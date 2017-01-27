3a147838ecbd5a35742c93ace5ef0f93a2f051be
Alexander Gabriel <alex@gabriel-software.ch>
2017-01-27 15:54:38 +0100

    test proj4

--- src/modules/isPointInsidePolygon.js
+++ src/modules/isPointInsidePolygon.js
@@ -1,5 +1,5 @@
 import inside from 'turf-inside'
-// import proj4 from 'proj4'
+import proj4 from 'proj4'
 // proj4.defs(`EPSG:21781`, `+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=600000 +y_0=200000 +ellps=bessel +towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs`)
 // proj4.defs(`EPSG:2056`, `+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=2600000 +y_0=1200000 +ellps=bessel +towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs`)
 // proj4.defs(`EPSG:4326`, `+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs`)
@@ -8,12 +8,12 @@ export default (polygon, x, y) => {
   const koordLv03 = [x, y]
 
   // convert koordinates to wgs84
-  /*const koordWgs84 = proj4(
+  const koordWgs84 = proj4(
     `EPSG:21781`,
     `EPSG:4326`,
     koordLv03
-  )*/
-  const koordWgs84 = `nothing`
+  )
+  // const koordWgs84 = `nothing`
 
   // convert coordinates into a geoJson point
   const koordPt = {
