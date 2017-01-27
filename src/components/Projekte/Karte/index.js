055d89ad7fdde630b59f5a9b2f69065583a49db9
Alexander Gabriel <alex.barbalex@gmail.com>
2017-01-27 17:57:33 +0100

    work on css

--- src/components/Projekte/Karte/index.js
+++ src/components/Projekte/Karte/index.js
@@ -12,8 +12,8 @@ import styled from 'styled-components'
 import compose from 'recompose/compose'
 // import Proj4leaflet from 'proj4leaflet'
 import Leaflet from 'leaflet'
-// import Proj4 from 'proj4'
-// import Proj4leaflet from 'proj4leaflet'
+import Proj4 from 'proj4'
+import Proj4leaflet from 'proj4leaflet'
 
 import OsmColorLayer from './layers/OsmColor'
 import OsmBwLayer from './layers/OsmBw'
@@ -33,18 +33,23 @@ const enhance = compose(
 class Karte extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     const position = [47.295, 8.58]
+    // const position = [685994, 238600]
+    // const position = [238600, 685994]
     // this does not work
     // see issue on proj4js: https://github.com/proj4js/proj4js/issues/214
-    const crs = (
+    const crs = new window.L.Proj.CRS(
       `EPSG:21781`,
-      `+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=600000 +y_0=200000 +ellps=bessel +towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs `
+      `+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=600000 +y_0=200000 +ellps=bessel +towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs`,
+      {
+        resolutions: [8192, 4096, 2048] // 3 example zoom level resolutions
+      }
     )
 
     return (
       <StyledMap
         center={position}
         zoom={13}
-        // CRS={crs}
+        // crs={crs}
         onClick={(e) => {
           console.log(`Lat, Lon : ` + e.latlng.lat + `, ` + e.latlng.lng)
         }}
