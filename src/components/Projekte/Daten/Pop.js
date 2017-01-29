8a77b7150fd83364f2b58732fecf68c8ed604bbb
Alexander Gabriel <alex@gabriel-software.ch>
2017-01-29 21:29:25 +0100

    add scrollbars

--- src/components/Projekte/Daten/Pop.js
+++ src/components/Projekte/Daten/Pop.js
@@ -3,6 +3,7 @@ import { observer, inject } from 'mobx-react'
 import styled from 'styled-components'
 import compose from 'recompose/compose'
 import withProps from 'recompose/withProps'
+import { Scrollbars } from 'react-custom-scrollbars'
 
 import TextField from '../../shared/TextField'
 import InfoWithPopover from '../../shared/InfoWithPopover'
@@ -17,9 +18,7 @@ const Container = styled.div`
 const FieldsContainer = styled.div`
   padding-left: 10px;
   padding-right: 10px;
-  overflow-x: auto;
-  height: 100%;
-  padding-bottom: 95px;
+  padding-bottom: 45px;
 `
 const PopoverRow = styled.div`
   padding: 2px 5px 2px 5px;
@@ -63,78 +62,80 @@ const Pop = ({
   return (
     <Container>
       <FormTitle title="Population" />
-      <FieldsContainer>
-        <TextField
-          label="Nr."
-          fieldName="PopNr"
-          value={activeDataset.row.PopNr}
-          errorText={activeDataset.valid.PopNr}
-          type="number"
-          updateProperty={store.updateProperty}
-          updatePropertyInDb={store.updatePropertyInDb}
-        />
-        <FieldWithInfoContainer>
+      <Scrollbars>
+        <FieldsContainer>
           <TextField
-            label="Name"
-            fieldName="PopName"
-            value={activeDataset.row.PopName}
-            errorText={activeDataset.valid.PopName}
-            type="text"
+            label="Nr."
+            fieldName="PopNr"
+            value={activeDataset.row.PopNr}
+            errorText={activeDataset.valid.PopNr}
+            type="number"
             updateProperty={store.updateProperty}
             updatePropertyInDb={store.updatePropertyInDb}
           />
-          <InfoWithPopover>
-            <PopoverContentRow>
-              Dieses Feld möglichst immer ausfüllen
-            </PopoverContentRow>
-          </InfoWithPopover>
-        </FieldWithInfoContainer>
-        <Status
-          apJahr={apJahr}
-          herkunftFieldName="PopHerkunft"
-          herkunftValue={activeDataset.row.PopHerkunft}
-          bekanntSeitFieldName="PopBekanntSeit"
-          bekanntSeitValue={activeDataset.row.PopBekanntSeit}
-          bekanntSeitValid={activeDataset.valid.PopBekanntSeit}
-          updateProperty={store.updateProperty}
-          updatePropertyInDb={store.updatePropertyInDb}
-        />
-        <Label label="Status unklar" />
-        <RadioButton
-          fieldName="PopHerkunftUnklar"
-          value={activeDataset.row.PopHerkunftUnklar}
-          updatePropertyInDb={store.updatePropertyInDb}
-        />
-        <TextField
-          label="Begründung"
-          fieldName="PopHerkunftUnklarBegruendung"
-          value={activeDataset.row.PopHerkunftUnklarBegruendung}
-          errorText={activeDataset.valid.PopHerkunftUnklarBegruendung}
-          type="text"
-          multiLine
-          fullWidth
-          updateProperty={store.updateProperty}
-          updatePropertyInDb={store.updatePropertyInDb}
-        />
-        <TextField
-          label="X-Koordinaten"
-          fieldName="PopXKoord"
-          value={activeDataset.row.PopXKoord}
-          errorText={activeDataset.valid.PopXKoord}
-          type="number"
-          updateProperty={store.updateProperty}
-          updatePropertyInDb={store.updatePropertyInDb}
-        />
-        <TextField
-          label="Y-Koordinaten"
-          fieldName="PopYKoord"
-          value={activeDataset.row.PopYKoord}
-          errorText={activeDataset.valid.PopYKoord}
-          type="number"
-          updateProperty={store.updateProperty}
-          updatePropertyInDb={store.updatePropertyInDb}
-        />
-      </FieldsContainer>
+          <FieldWithInfoContainer>
+            <TextField
+              label="Name"
+              fieldName="PopName"
+              value={activeDataset.row.PopName}
+              errorText={activeDataset.valid.PopName}
+              type="text"
+              updateProperty={store.updateProperty}
+              updatePropertyInDb={store.updatePropertyInDb}
+            />
+            <InfoWithPopover>
+              <PopoverContentRow>
+                Dieses Feld möglichst immer ausfüllen
+              </PopoverContentRow>
+            </InfoWithPopover>
+          </FieldWithInfoContainer>
+          <Status
+            apJahr={apJahr}
+            herkunftFieldName="PopHerkunft"
+            herkunftValue={activeDataset.row.PopHerkunft}
+            bekanntSeitFieldName="PopBekanntSeit"
+            bekanntSeitValue={activeDataset.row.PopBekanntSeit}
+            bekanntSeitValid={activeDataset.valid.PopBekanntSeit}
+            updateProperty={store.updateProperty}
+            updatePropertyInDb={store.updatePropertyInDb}
+          />
+          <Label label="Status unklar" />
+          <RadioButton
+            fieldName="PopHerkunftUnklar"
+            value={activeDataset.row.PopHerkunftUnklar}
+            updatePropertyInDb={store.updatePropertyInDb}
+          />
+          <TextField
+            label="Begründung"
+            fieldName="PopHerkunftUnklarBegruendung"
+            value={activeDataset.row.PopHerkunftUnklarBegruendung}
+            errorText={activeDataset.valid.PopHerkunftUnklarBegruendung}
+            type="text"
+            multiLine
+            fullWidth
+            updateProperty={store.updateProperty}
+            updatePropertyInDb={store.updatePropertyInDb}
+          />
+          <TextField
+            label="X-Koordinaten"
+            fieldName="PopXKoord"
+            value={activeDataset.row.PopXKoord}
+            errorText={activeDataset.valid.PopXKoord}
+            type="number"
+            updateProperty={store.updateProperty}
+            updatePropertyInDb={store.updatePropertyInDb}
+          />
+          <TextField
+            label="Y-Koordinaten"
+            fieldName="PopYKoord"
+            value={activeDataset.row.PopYKoord}
+            errorText={activeDataset.valid.PopYKoord}
+            type="number"
+            updateProperty={store.updateProperty}
+            updatePropertyInDb={store.updatePropertyInDb}
+          />
+        </FieldsContainer>
+      </Scrollbars>
     </Container>
   )
 }
