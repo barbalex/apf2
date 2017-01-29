e9efc449a49ae087c1f19136a08698f60095d723
Alexander Gabriel <alex@gabriel-software.ch>
2017-01-29 21:37:52 +0100

    add flow

--- src/components/Projekte/Karte/layers/BingAerial.js
+++ src/components/Projekte/Karte/layers/BingAerial.js
@@ -1,11 +1,12 @@
-    import React from 'react'
-    import { BingLayer } from 'react-leaflet-bing'
+// @flow
+import React from 'react'
+import { BingLayer } from 'react-leaflet-bing'
 
-    const bingKey = `AjGOtB_ygBplpxXtKiiHtm-GERjSg9TFEoCmuBI_Yz4VWy0unRGUDo9GOZHA46Pf`
+const bingKey = `AjGOtB_ygBplpxXtKiiHtm-GERjSg9TFEoCmuBI_Yz4VWy0unRGUDo9GOZHA46Pf`
 
-    const BingAerialLayer = () =>
-      <BingLayer
-        bingkey={bingKey}
-      />
+const BingAerialLayer = () =>
+  <BingLayer
+    bingkey={bingKey}
+  />
 
-    export default BingAerialLayer
+export default BingAerialLayer
