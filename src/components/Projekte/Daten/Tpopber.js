a5302b3648b412a5bbbecacc868731cdb8468aa0
Alexander Gabriel <alex@gabriel-software.ch>
2017-01-29 21:29:05 +0100

    add scrollbars

--- src/components/Projekte/Daten/Tpopber.js
+++ src/components/Projekte/Daten/Tpopber.js
@@ -4,6 +4,7 @@ import sortBy from 'lodash/sortBy'
 import styled from 'styled-components'
 import compose from 'recompose/compose'
 import withProps from 'recompose/withProps'
+import { Scrollbars } from 'react-custom-scrollbars'
 
 import RadioButtonGroup from '../../shared/RadioButtonGroup'
 import Label from '../../shared/Label'
@@ -16,9 +17,7 @@ const Container = styled.div`
 const FieldsContainer = styled.div`
   padding-left: 10px;
   padding-right: 10px;
-  overflow-x: auto;
-  height: 100%;
-  padding-bottom: 95px;
+  padding-bottom: 45px;
 `
 
 const enhance = compose(
@@ -46,36 +45,38 @@ const Tpopber = ({
   return (
     <Container>
       <FormTitle title="Kontroll-Bericht Teil-Population" />
-      <FieldsContainer>
-        <TextField
-          label="Jahr"
-          fieldName="TPopBerJahr"
-          value={activeDataset.row.TPopBerJahr}
-          errorText={activeDataset.valid.TPopBerJahr}
-          type="number"
-          updateProperty={store.updateProperty}
-          updatePropertyInDb={store.updatePropertyInDb}
-        />
-        <Label label="Entwicklung" />
-        <RadioButtonGroup
-          fieldName="TPopBerEntwicklung"
-          value={activeDataset.row.TPopBerEntwicklung}
-          errorText={activeDataset.valid.TPopBerEntwicklung}
-          dataSource={tpopEntwicklungWerte}
-          updatePropertyInDb={store.updatePropertyInDb}
-        />
-        <TextField
-          label="Bemerkungen"
-          fieldName="TPopBerTxt"
-          value={activeDataset.row.TPopBerTxt}
-          errorText={activeDataset.valid.TPopBerTxt}
-          type="text"
-          multiLine
-          fullWidth
-          updateProperty={store.updateProperty}
-          updatePropertyInDb={store.updatePropertyInDb}
-        />
-      </FieldsContainer>
+      <Scrollbars>
+        <FieldsContainer>
+          <TextField
+            label="Jahr"
+            fieldName="TPopBerJahr"
+            value={activeDataset.row.TPopBerJahr}
+            errorText={activeDataset.valid.TPopBerJahr}
+            type="number"
+            updateProperty={store.updateProperty}
+            updatePropertyInDb={store.updatePropertyInDb}
+          />
+          <Label label="Entwicklung" />
+          <RadioButtonGroup
+            fieldName="TPopBerEntwicklung"
+            value={activeDataset.row.TPopBerEntwicklung}
+            errorText={activeDataset.valid.TPopBerEntwicklung}
+            dataSource={tpopEntwicklungWerte}
+            updatePropertyInDb={store.updatePropertyInDb}
+          />
+          <TextField
+            label="Bemerkungen"
+            fieldName="TPopBerTxt"
+            value={activeDataset.row.TPopBerTxt}
+            errorText={activeDataset.valid.TPopBerTxt}
+            type="text"
+            multiLine
+            fullWidth
+            updateProperty={store.updateProperty}
+            updatePropertyInDb={store.updatePropertyInDb}
+          />
+        </FieldsContainer>
+      </Scrollbars>
     </Container>
   )
 }
