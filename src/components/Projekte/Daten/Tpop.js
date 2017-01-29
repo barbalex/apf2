9de0b1484a8875150f73d2f8d0f6f5ffdc6e3b6f
Alexander Gabriel <alex@gabriel-software.ch>
2017-01-29 01:47:22 +0100

    use scrollbars

--- src/components/Projekte/Daten/Tpop.js
+++ src/components/Projekte/Daten/Tpop.js
@@ -5,6 +5,7 @@ import AutoComplete from 'material-ui/AutoComplete'
 import styled from 'styled-components'
 import compose from 'recompose/compose'
 import withProps from 'recompose/withProps'
+import { Scrollbars } from 'react-custom-scrollbars'
 
 import TextField from '../../shared/TextField'
 import InfoWithPopover from '../../shared/InfoWithPopover'
@@ -21,9 +22,7 @@ const Container = styled.div`
 const FieldsContainer = styled.div`
   padding-left: 10px;
   padding-right: 10px;
-  overflow-x: auto;
-  height: 100%;
-  padding-bottom: 95px;
+  padding-bottom: 45px;
 `
 const FieldWithInfoContainer = styled.div`
   display: flex;
@@ -71,222 +70,224 @@ const Tpop = ({
   return (
     <Container>
       <FormTitle title="Teil-Population" />
-      <FieldsContainer>
-        <TextField
-          label="Nr."
-          fieldName="TPopNr"
-          value={activeDataset.row.TPopNr}
-          errorText={activeDataset.valid.TPopNr}
-          type="number"
-          updateProperty={store.updateProperty}
-          updatePropertyInDb={store.updatePropertyInDb}
-        />
-        <FieldWithInfoContainer>
+      <Scrollbars>
+        <FieldsContainer>
           <TextField
-            label="Flurname"
-            fieldName="TPopFlurname"
-            value={activeDataset.row.TPopFlurname}
-            errorText={activeDataset.valid.TPopFlurname}
+            label="Nr."
+            fieldName="TPopNr"
+            value={activeDataset.row.TPopNr}
+            errorText={activeDataset.valid.TPopNr}
+            type="number"
+            updateProperty={store.updateProperty}
+            updatePropertyInDb={store.updatePropertyInDb}
+          />
+          <FieldWithInfoContainer>
+            <TextField
+              label="Flurname"
+              fieldName="TPopFlurname"
+              value={activeDataset.row.TPopFlurname}
+              errorText={activeDataset.valid.TPopFlurname}
+              type="text"
+              updateProperty={store.updateProperty}
+              updatePropertyInDb={store.updatePropertyInDb}
+            />
+            <InfoWithPopover>
+              <PopoverContentRow>
+                Dieses Feld möglichst immer ausfüllen
+              </PopoverContentRow>
+            </InfoWithPopover>
+          </FieldWithInfoContainer>
+          <Status
+            apJahr={apJahr}
+            herkunftFieldName="TPopHerkunft"
+            herkunftValue={activeDataset.row.TPopHerkunft}
+            bekanntSeitFieldName="TPopBekanntSeit"
+            bekanntSeitValue={activeDataset.row.TPopBekanntSeit}
+            bekanntSeitValid={activeDataset.valid.TPopBekanntSeit}
+            updateProperty={store.updateProperty}
+            updatePropertyInDb={store.updatePropertyInDb}
+          />
+          <Label label="Status unklar" />
+          <RadioButton
+            fieldName="TPopHerkunftUnklar"
+            value={activeDataset.row.TPopHerkunftUnklar}
+            updatePropertyInDb={store.updatePropertyInDb}
+          />
+          <TextField
+            label="Begründung"
+            fieldName="TPopHerkunftUnklarBegruendung"
+            value={activeDataset.row.TPopHerkunftUnklarBegruendung}
+            errorText={activeDataset.valid.TPopHerkunftUnklarBegruendung}
+            type="text"
+            multiLine
+            fullWidth
+            updateProperty={store.updateProperty}
+            updatePropertyInDb={store.updatePropertyInDb}
+          />
+          <Label label="Für AP-Bericht relevant" />
+          <RadioButtonGroupWithInfo
+            fieldName="TPopApBerichtRelevant"
+            value={activeDataset.row.TPopApBerichtRelevant}
+            dataSource={tpopApBerichtRelevantWerte}
+            updatePropertyInDb={store.updatePropertyInDb}
+            popover={TpopAbBerRelevantInfoPopover}
+          />
+          <TextField
+            label="X-Koordinaten"
+            fieldName="TPopXKoord"
+            value={activeDataset.row.TPopXKoord}
+            errorText={activeDataset.valid.TPopXKoord}
+            type="number"
+            updateProperty={store.updateProperty}
+            updatePropertyInDb={store.updatePropertyInDb}
+          />
+          <TextField
+            label="Y-Koordinaten"
+            fieldName="TPopYKoord"
+            value={activeDataset.row.TPopYKoord}
+            errorText={activeDataset.valid.TPopYKoord}
+            type="number"
+            updateProperty={store.updateProperty}
+            updatePropertyInDb={store.updatePropertyInDb}
+          />
+          <AutoComplete
+            hintText={gemeinden.length === 0 ? `lade Daten...` : ``}
+            fullWidth
+            floatingLabelText="Gemeinde"
+            openOnFocus
+            dataSource={gemeinden}
+            searchText={activeDataset.row.TPopGemeinde || ``}
+            filter={AutoComplete.caseInsensitiveFilter}
+            maxSearchResults={20}
+            onNewRequest={val =>
+              store.updatePropertyInDb(`TPopGemeinde`, val)
+            }
+            onBlur={e =>
+              store.updatePropertyInDb(`TPopGemeinde`, e.target.value)
+            }
+            value={activeDataset.row.TPopGemeinde || ``}
+          />
+          <TextField
+            label="Radius (m)"
+            fieldName="TPopRadius"
+            value={activeDataset.row.TPopRadius}
+            errorText={activeDataset.valid.TPopRadius}
+            type="number"
+            updateProperty={store.updateProperty}
+            updatePropertyInDb={store.updatePropertyInDb}
+          />
+          <TextField
+            label="Höhe (m.ü.M.)"
+            fieldName="TPopHoehe"
+            value={activeDataset.row.TPopHoehe}
+            errorText={activeDataset.valid.TPopHoehe}
+            type="number"
+            updateProperty={store.updateProperty}
+            updatePropertyInDb={store.updatePropertyInDb}
+          />
+          <TextField
+            label="Exposition, Besonnung"
+            fieldName="TPopExposition"
+            value={activeDataset.row.TPopExposition}
+            errorText={activeDataset.valid.TPopExposition}
             type="text"
             updateProperty={store.updateProperty}
             updatePropertyInDb={store.updatePropertyInDb}
           />
-          <InfoWithPopover>
-            <PopoverContentRow>
-              Dieses Feld möglichst immer ausfüllen
-            </PopoverContentRow>
-          </InfoWithPopover>
-        </FieldWithInfoContainer>
-        <Status
-          apJahr={apJahr}
-          herkunftFieldName="TPopHerkunft"
-          herkunftValue={activeDataset.row.TPopHerkunft}
-          bekanntSeitFieldName="TPopBekanntSeit"
-          bekanntSeitValue={activeDataset.row.TPopBekanntSeit}
-          bekanntSeitValid={activeDataset.valid.TPopBekanntSeit}
-          updateProperty={store.updateProperty}
-          updatePropertyInDb={store.updatePropertyInDb}
-        />
-        <Label label="Status unklar" />
-        <RadioButton
-          fieldName="TPopHerkunftUnklar"
-          value={activeDataset.row.TPopHerkunftUnklar}
-          updatePropertyInDb={store.updatePropertyInDb}
-        />
-        <TextField
-          label="Begründung"
-          fieldName="TPopHerkunftUnklarBegruendung"
-          value={activeDataset.row.TPopHerkunftUnklarBegruendung}
-          errorText={activeDataset.valid.TPopHerkunftUnklarBegruendung}
-          type="text"
-          multiLine
-          fullWidth
-          updateProperty={store.updateProperty}
-          updatePropertyInDb={store.updatePropertyInDb}
-        />
-        <Label label="Für AP-Bericht relevant" />
-        <RadioButtonGroupWithInfo
-          fieldName="TPopApBerichtRelevant"
-          value={activeDataset.row.TPopApBerichtRelevant}
-          dataSource={tpopApBerichtRelevantWerte}
-          updatePropertyInDb={store.updatePropertyInDb}
-          popover={TpopAbBerRelevantInfoPopover}
-        />
-        <TextField
-          label="X-Koordinaten"
-          fieldName="TPopXKoord"
-          value={activeDataset.row.TPopXKoord}
-          errorText={activeDataset.valid.TPopXKoord}
-          type="number"
-          updateProperty={store.updateProperty}
-          updatePropertyInDb={store.updatePropertyInDb}
-        />
-        <TextField
-          label="Y-Koordinaten"
-          fieldName="TPopYKoord"
-          value={activeDataset.row.TPopYKoord}
-          errorText={activeDataset.valid.TPopYKoord}
-          type="number"
-          updateProperty={store.updateProperty}
-          updatePropertyInDb={store.updatePropertyInDb}
-        />
-        <AutoComplete
-          hintText={gemeinden.length === 0 ? `lade Daten...` : ``}
-          fullWidth
-          floatingLabelText="Gemeinde"
-          openOnFocus
-          dataSource={gemeinden}
-          searchText={activeDataset.row.TPopGemeinde || ``}
-          filter={AutoComplete.caseInsensitiveFilter}
-          maxSearchResults={20}
-          onNewRequest={val =>
-            store.updatePropertyInDb(`TPopGemeinde`, val)
-          }
-          onBlur={e =>
-            store.updatePropertyInDb(`TPopGemeinde`, e.target.value)
-          }
-          value={activeDataset.row.TPopGemeinde || ``}
-        />
-        <TextField
-          label="Radius (m)"
-          fieldName="TPopRadius"
-          value={activeDataset.row.TPopRadius}
-          errorText={activeDataset.valid.TPopRadius}
-          type="number"
-          updateProperty={store.updateProperty}
-          updatePropertyInDb={store.updatePropertyInDb}
-        />
-        <TextField
-          label="Höhe (m.ü.M.)"
-          fieldName="TPopHoehe"
-          value={activeDataset.row.TPopHoehe}
-          errorText={activeDataset.valid.TPopHoehe}
-          type="number"
-          updateProperty={store.updateProperty}
-          updatePropertyInDb={store.updatePropertyInDb}
-        />
-        <TextField
-          label="Exposition, Besonnung"
-          fieldName="TPopExposition"
-          value={activeDataset.row.TPopExposition}
-          errorText={activeDataset.valid.TPopExposition}
-          type="text"
-          updateProperty={store.updateProperty}
-          updatePropertyInDb={store.updatePropertyInDb}
-        />
-        <TextField
-          label="Klima"
-          fieldName="TPopKlima"
-          value={activeDataset.row.TPopKlima}
-          errorText={activeDataset.valid.TPopKlima}
-          type="text"
-          updateProperty={store.updateProperty}
-          updatePropertyInDb={store.updatePropertyInDb}
-        />
-        <TextField
-          label="Hangneigung"
-          fieldName="TPopNeigung"
-          value={activeDataset.row.TPopNeigung}
-          errorText={activeDataset.valid.TPopNeigung}
-          type="text"
-          updateProperty={store.updateProperty}
-          updatePropertyInDb={store.updatePropertyInDb}
-        />
-        <TextField
-          label="Beschreibung"
-          fieldName="TPopBeschr"
-          value={activeDataset.row.TPopBeschr}
-          errorText={activeDataset.valid.TPopBeschr}
-          type="text"
-          multiline
-          updateProperty={store.updateProperty}
-          updatePropertyInDb={store.updatePropertyInDb}
-        />
-        <TextField
-          label="Kataster-Nr."
-          fieldName="TPopKatNr"
-          value={activeDataset.row.TPopKatNr}
-          errorText={activeDataset.valid.TPopKatNr}
-          type="text"
-          updateProperty={store.updateProperty}
-          updatePropertyInDb={store.updatePropertyInDb}
-        />
-        <TextField
-          label="EigentümerIn"
-          fieldName="TPopEigen"
-          value={activeDataset.row.TPopEigen}
-          errorText={activeDataset.valid.TPopEigen}
-          type="text"
-          updateProperty={store.updateProperty}
-          updatePropertyInDb={store.updatePropertyInDb}
-        />
-        <TextField
-          label="Kontakt vor Ort"
-          fieldName="TPopKontakt"
-          value={activeDataset.row.TPopKontakt}
-          errorText={activeDataset.valid.TPopKontakt}
-          type="text"
-          updateProperty={store.updateProperty}
-          updatePropertyInDb={store.updatePropertyInDb}
-        />
-        <TextField
-          label="Nutzungszone"
-          fieldName="TPopNutzungszone"
-          value={activeDataset.row.TPopNutzungszone}
-          errorText={activeDataset.valid.TPopNutzungszone}
-          type="text"
-          updateProperty={store.updateProperty}
-          updatePropertyInDb={store.updatePropertyInDb}
-        />
-        <TextField
-          label="BewirtschafterIn"
-          fieldName="TPopBewirtschafterIn"
-          value={activeDataset.row.TPopBewirtschafterIn}
-          errorText={activeDataset.valid.TPopBewirtschafterIn}
-          type="text"
-          updateProperty={store.updateProperty}
-          updatePropertyInDb={store.updatePropertyInDb}
-        />
-        <TextField
-          label="Bewirtschaftung"
-          fieldName="TPopBewirtschaftung"
-          value={activeDataset.row.TPopBewirtschaftung}
-          errorText={activeDataset.valid.TPopBewirtschaftung}
-          type="text"
-          updateProperty={store.updateProperty}
-          updatePropertyInDb={store.updatePropertyInDb}
-        />
-        <TextField
-          label="Bemerkungen"
-          fieldName="TPopTxt"
-          value={activeDataset.row.TPopTxt}
-          errorText={activeDataset.valid.TPopTxt}
-          type="text"
-          multiline
-          updateProperty={store.updateProperty}
-          updatePropertyInDb={store.updatePropertyInDb}
-        />
-      </FieldsContainer>
+          <TextField
+            label="Klima"
+            fieldName="TPopKlima"
+            value={activeDataset.row.TPopKlima}
+            errorText={activeDataset.valid.TPopKlima}
+            type="text"
+            updateProperty={store.updateProperty}
+            updatePropertyInDb={store.updatePropertyInDb}
+          />
+          <TextField
+            label="Hangneigung"
+            fieldName="TPopNeigung"
+            value={activeDataset.row.TPopNeigung}
+            errorText={activeDataset.valid.TPopNeigung}
+            type="text"
+            updateProperty={store.updateProperty}
+            updatePropertyInDb={store.updatePropertyInDb}
+          />
+          <TextField
+            label="Beschreibung"
+            fieldName="TPopBeschr"
+            value={activeDataset.row.TPopBeschr}
+            errorText={activeDataset.valid.TPopBeschr}
+            type="text"
+            multiline
+            updateProperty={store.updateProperty}
+            updatePropertyInDb={store.updatePropertyInDb}
+          />
+          <TextField
+            label="Kataster-Nr."
+            fieldName="TPopKatNr"
+            value={activeDataset.row.TPopKatNr}
+            errorText={activeDataset.valid.TPopKatNr}
+            type="text"
+            updateProperty={store.updateProperty}
+            updatePropertyInDb={store.updatePropertyInDb}
+          />
+          <TextField
+            label="EigentümerIn"
+            fieldName="TPopEigen"
+            value={activeDataset.row.TPopEigen}
+            errorText={activeDataset.valid.TPopEigen}
+            type="text"
+            updateProperty={store.updateProperty}
+            updatePropertyInDb={store.updatePropertyInDb}
+          />
+          <TextField
+            label="Kontakt vor Ort"
+            fieldName="TPopKontakt"
+            value={activeDataset.row.TPopKontakt}
+            errorText={activeDataset.valid.TPopKontakt}
+            type="text"
+            updateProperty={store.updateProperty}
+            updatePropertyInDb={store.updatePropertyInDb}
+          />
+          <TextField
+            label="Nutzungszone"
+            fieldName="TPopNutzungszone"
+            value={activeDataset.row.TPopNutzungszone}
+            errorText={activeDataset.valid.TPopNutzungszone}
+            type="text"
+            updateProperty={store.updateProperty}
+            updatePropertyInDb={store.updatePropertyInDb}
+          />
+          <TextField
+            label="BewirtschafterIn"
+            fieldName="TPopBewirtschafterIn"
+            value={activeDataset.row.TPopBewirtschafterIn}
+            errorText={activeDataset.valid.TPopBewirtschafterIn}
+            type="text"
+            updateProperty={store.updateProperty}
+            updatePropertyInDb={store.updatePropertyInDb}
+          />
+          <TextField
+            label="Bewirtschaftung"
+            fieldName="TPopBewirtschaftung"
+            value={activeDataset.row.TPopBewirtschaftung}
+            errorText={activeDataset.valid.TPopBewirtschaftung}
+            type="text"
+            updateProperty={store.updateProperty}
+            updatePropertyInDb={store.updatePropertyInDb}
+          />
+          <TextField
+            label="Bemerkungen"
+            fieldName="TPopTxt"
+            value={activeDataset.row.TPopTxt}
+            errorText={activeDataset.valid.TPopTxt}
+            type="text"
+            multiline
+            updateProperty={store.updateProperty}
+            updatePropertyInDb={store.updatePropertyInDb}
+          />
+        </FieldsContainer>
+      </Scrollbars>
     </Container>
   )
 }
