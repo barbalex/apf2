36e853864e0543ec6194bab5ad3a85dec924bff2
Alexander Gabriel <alex@gabriel-software.ch>
2017-01-29 21:29:21 +0100

    add scrollbars

--- src/components/Projekte/Daten/Popber.js
+++ src/components/Projekte/Daten/Popber.js
@@ -4,6 +4,7 @@ import sortBy from 'lodash/sortBy'
 import styled from 'styled-components'
 import compose from 'recompose/compose'
 import withProps from 'recompose/withProps'
+import { Scrollbars } from 'react-custom-scrollbars'
 
 import RadioButtonGroup from '../../shared/RadioButtonGroup'
 import Label from '../../shared/Label'
@@ -16,9 +17,7 @@ const Container = styled.div`
 const FieldsContainer = styled.div`
   padding-left: 10px;
   padding-right: 10px;
-  overflow-x: auto;
-  height: 100%;
-  padding-bottom: 95px;
+  padding-bottom: 45px;
 `
 
 const enhance = compose(
@@ -44,36 +43,38 @@ const Popber = ({
   return (
     <Container>
       <FormTitle title="Kontroll-Bericht Population" />
-      <FieldsContainer>
-        <TextField
-          label="Jahr"
-          fieldName="PopBerJahr"
-          value={activeDataset.row.PopBerJahr}
-          errorText={activeDataset.valid.PopBerJahr}
-          type="number"
-          updateProperty={store.updateProperty}
-          updatePropertyInDb={store.updatePropertyInDb}
-        />
-        <Label label="Entwicklung" />
-        <RadioButtonGroup
-          fieldName="PopBerEntwicklung"
-          value={activeDataset.row.PopBerEntwicklung}
-          errorText={activeDataset.valid.PopBerEntwicklung}
-          dataSource={popEntwicklungWerte}
-          updatePropertyInDb={store.updatePropertyInDb}
-        />
-        <TextField
-          label="Bemerkungen"
-          fieldName="PopBerTxt"
-          value={activeDataset.row.PopBerTxt}
-          errorText={activeDataset.valid.PopBerTxt}
-          type="text"
-          multiLine
-          fullWidth
-          updateProperty={store.updateProperty}
-          updatePropertyInDb={store.updatePropertyInDb}
-        />
-      </FieldsContainer>
+      <Scrollbars>
+        <FieldsContainer>
+          <TextField
+            label="Jahr"
+            fieldName="PopBerJahr"
+            value={activeDataset.row.PopBerJahr}
+            errorText={activeDataset.valid.PopBerJahr}
+            type="number"
+            updateProperty={store.updateProperty}
+            updatePropertyInDb={store.updatePropertyInDb}
+          />
+          <Label label="Entwicklung" />
+          <RadioButtonGroup
+            fieldName="PopBerEntwicklung"
+            value={activeDataset.row.PopBerEntwicklung}
+            errorText={activeDataset.valid.PopBerEntwicklung}
+            dataSource={popEntwicklungWerte}
+            updatePropertyInDb={store.updatePropertyInDb}
+          />
+          <TextField
+            label="Bemerkungen"
+            fieldName="PopBerTxt"
+            value={activeDataset.row.PopBerTxt}
+            errorText={activeDataset.valid.PopBerTxt}
+            type="text"
+            multiLine
+            fullWidth
+            updateProperty={store.updateProperty}
+            updatePropertyInDb={store.updatePropertyInDb}
+          />
+        </FieldsContainer>
+      </Scrollbars>
     </Container>
   )
 }
