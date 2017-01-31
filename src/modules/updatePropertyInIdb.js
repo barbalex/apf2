5638db86c842d844e0745fdaee8af401ec9988d7
Alexander Gabriel <alex@gabriel-software.ch>
2017-01-31 23:00:45 +0100

    optimize

--- src/modules/updatePropertyInIdb.js
+++ src/modules/updatePropertyInIdb.js
@@ -1,11 +1,9 @@
 // @flow
 import app from 'ampersand-app'
-import clone from 'lodash/clone'
 
 export default (store:Object, table:string, datasetId:string|number, key:string, value:string|number|null) => {
   app.db[table].get(datasetId)
-    .then((dataset) => {
-      const ds = clone(dataset)
+    .then((ds) => {
       ds[key] = value
       return app.db[table].put(ds)
     })
