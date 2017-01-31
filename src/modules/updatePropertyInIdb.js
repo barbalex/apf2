e0a744ff6e136571533c4623e533715bbeccfe97
Alexander Gabriel <alex@gabriel-software.ch>
2017-01-31 18:13:29 +0100

    dont change passed value

--- src/modules/updatePropertyInIdb.js
+++ src/modules/updatePropertyInIdb.js
@@ -1,11 +1,13 @@
 // @flow
 import app from 'ampersand-app'
+import clone from 'lodash/clone'
 
 export default (store:Object, table:string, datasetId:string|number, key:string, value:string|number|null) => {
   app.db[table].get(datasetId)
     .then((dataset) => {
-      dataset[key] = value
-      return app.db[table].put(dataset)
+      const ds = clone(dataset)
+      ds[key] = value
+      return app.db[table].put(ds)
     })
     .catch(error =>
       store.listError(error)
