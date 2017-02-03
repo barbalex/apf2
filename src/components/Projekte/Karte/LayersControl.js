c43888de65729ce6b3959b2198ab0eb63542321c
Alexander Gabriel <alex@gabriel-software.ch>
2017-02-04 00:03:17 +0100

    add layers

--- src/components/Projekte/Karte/LayersControl.js
+++ src/components/Projekte/Karte/LayersControl.js
@@ -10,10 +10,21 @@ import 'proj4leaflet'
 import OsmColorLayer from './layers/OsmColor'
 import OsmBwLayer from './layers/OsmBw'
 import SwissTopoPixelFarbeLayer from './layers/SwisstopoPixelFarbe'
+import SwissTopoPixelGrauLayer from './layers/SwisstopoPixelGrau'
 import BingAerialLayer from './layers/BingAerial'
 import DetailplaeneLayer from './layers/Detailplaene'
-import SvoColorLayer from './layers/SvoColor'
-import LrVegKartierungenZh from './layers/LrVegKartierungenZh'
+import ZhSvoColorLayer from './layers/ZhSvoColor'
+import ZhSvoGreyLayer from './layers/ZhSvoGrey'
+import ZhLrVegKartierungen from './layers/ZhLrVegKartierungen'
+import ZhLichteWaelder from './layers/ZhLichteWaelder'
+import ZhWaelderVegetation from './layers/ZhWaelderVegetation'
+import ZhOrtho from './layers/ZhOrtho'
+import ZhOrthoIr from './layers/ZhOrthoIr'
+import ZhOrtho2015 from './layers/ZhOrtho2015'
+import ZhOrtho2015Ir from './layers/ZhOrtho2015Ir'
+import ZhUep from './layers/ZhUep'
+import ZhUepOverlay from './layers/ZhUepOverlay'
+import ZhDtm from './layers/ZhDtm'
 
 const { BaseLayer, Overlay } = LayersControl
 
@@ -24,6 +35,9 @@ const enhance = compose(
 
 const MyLayersControl = ({ store }) =>
   <LayersControl>
+    <BaseLayer checked name="ZH Höhenmodell">
+      <ZhDtm />
+    </BaseLayer>
     <BaseLayer checked name="OpenStreetMap farbig">
       <OsmColorLayer />
     </BaseLayer>
@@ -33,17 +47,47 @@ const MyLayersControl = ({ store }) =>
     <BaseLayer name="Swisstopo Karte">
       <SwissTopoPixelFarbeLayer />
     </BaseLayer>
+    <BaseLayer name="Swisstopo Karte grau">
+      <SwissTopoPixelGrauLayer />
+    </BaseLayer>
+    <BaseLayer name="ZH Übersichtsplan">
+      <ZhUep />
+    </BaseLayer>
     <BaseLayer name="Bing Luftbild">
       <BingAerialLayer />
     </BaseLayer>
+    <BaseLayer name="ZH Orthofoto Sommer RGB">
+      <ZhOrtho />
+    </BaseLayer>
+    <BaseLayer name="ZH Orthofoto Sommer Falschfarbeninfrarot">
+      <ZhOrthoIr />
+    </BaseLayer>
+    <BaseLayer name="ZH Orthofoto Frühjahr 2015/16 RGB">
+      <ZhOrtho2015 />
+    </BaseLayer>
+    <BaseLayer name="ZH Orthofoto Frühjahr 2015/16 Falschfarbeninfrarot">
+      <ZhOrtho2015Ir />
+    </BaseLayer>
+    <Overlay name="ZH Übersichtsplan zum darüberlegen">
+      <ZhUepOverlay />
+    </Overlay>
     <Overlay name="Detailplaene">
       <DetailplaeneLayer />
     </Overlay>
     <Overlay name="SVO farbig">
-      <SvoColorLayer />
+      <ZhSvoColorLayer />
+    </Overlay>
+    <Overlay name="SVO grau">
+      <ZhSvoGreyLayer />
     </Overlay>
     <Overlay name="Lebensraum- und Vegetationskartierungen">
-      <LrVegKartierungenZh />
+      <ZhLrVegKartierungen />
+    </Overlay>
+    <Overlay name="Wälder: lichte">
+      <ZhLichteWaelder />
+    </Overlay>
+    <Overlay name="Wälder: Vegetation">
+      <ZhWaelderVegetation />
     </Overlay>
   </LayersControl>
 
---
+++ src/components/Projekte/Karte/layers/ZhUepOverlay.js
@@ -0,0 +1,14 @@
+// @flow
+import React from 'react'
+import { WMSTileLayer } from 'react-leaflet'
+
+const UepLayer = () =>
+  <WMSTileLayer
+    url="//wms.zh.ch/upwms"
+    layers="Uebersichtsplan"
+    transparent={true}
+    version="1.3.0"
+    format="image/png"
+  />
+
+export default UepLayer
