4ee13778b5a3b89c5b6e9ed38e03a5f1a44e46d5
Alexander Gabriel <alex@gabriel-software.ch>
2017-02-06 00:13:58 +0100

    add export to png control

--- src/components/Projekte/Karte/PngControl.js
+++ src/components/Projekte/Karte/PngControl.js
@@ -1,29 +1,72 @@
-import React, { Component, PropTypes } from 'react'
+import React, { PropTypes } from 'react'
 import 'leaflet'
 import leafletImage from 'leaflet-image'
 import fileSaver from 'file-saver'
+import Control from 'react-leaflet-control'
+import FontIcon from 'material-ui/FontIcon'
+import styled from 'styled-components'
+import compose from 'recompose/compose'
+import withHandlers from 'recompose/withHandlers'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
 
-class PngControl extends Component { // eslint-disable-line react/prefer-stateless-function
-
-  static propTypes = {
-    map: PropTypes.object,
+const StyledIcon = styled(FontIcon)`
+  /*font-size: 20px !important;*/
+`
+const StyledButton = styled.button`
+  background-color: white;
+  width: 47.6px;
+  height: 47.6px;
+  border-radius: 5px;
+  border: 2px solid rgba(0,0,0,0.2);
+  background-clip: padding-box;
+  span {
+    color: rgba(0, 0, 0, 0.54) !important;
   }
+`
 
-  componentDidMount() {
-    const { map } = this.props
-    leafletImage(map, function(error, canvas) {
-      console.log(`canvas:`, canvas)
-      canvas.toBlob(function (blob) {
-        fileSaver.saveAs(blob, 'map.png');
+const theme = Object.assign({}, darkBaseTheme, {
+  appBar: {
+    height: 51,
+  },
+})
+
+const enhance = compose(
+  withHandlers({
+    savePng: props => () => {
+      const { map } = props
+      leafletImage(map, function(error, canvas) {
+        canvas.toBlob(function (blob) {
+          fileSaver.saveAs(blob, 'map.png');
+        })
       })
-    })
-  }
+    },
+  })
+)
 
-  render() {
-    return (
-      <div style={{ display: `none` }} />
-    )
-  }
+const PngControl = ({ map, savePng }) =>
+  <Control position="topright">
+    <StyledButton
+      onClick={savePng}
+      title="Karte als png speichern"
+    >
+      <MuiThemeProvider
+        muiTheme={getMuiTheme(theme)}
+      >
+        <StyledIcon
+          id="karteAlsPngSpeichern"
+          className="material-icons"
+        >
+          file_download
+        </StyledIcon>
+      </MuiThemeProvider>
+    </StyledButton>
+  </Control>
+
+PngControl.propTypes = {
+  map: PropTypes.object.isRequired,
+  savePng: PropTypes.func.isRequired,
 }
 
-export default PngControl
+export default enhance(PngControl)
