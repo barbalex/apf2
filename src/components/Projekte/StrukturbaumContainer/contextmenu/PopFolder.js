eb3f80dc5a5eaf4a344be5bd2d7f0f594f2e231f
Alexander Gabriel <alex@gabriel-software.ch>
2017-02-09 13:54:28 +0100

    enhance context menu

--- src/components/Projekte/StrukturbaumContainer/contextmenu/PopFolder.js
+++ src/components/Projekte/StrukturbaumContainer/contextmenu/PopFolder.js
@@ -3,17 +3,31 @@ import React, { PropTypes } from 'react'
 import { ContextMenu, MenuItem } from 'react-contextmenu'
 import { inject, observer } from 'mobx-react'
 import compose from 'recompose/compose'
+import withState from 'recompose/withState'
+import withHandlers from 'recompose/withHandlers'
 
 const enhance = compose(
   inject(`store`),
+  withState(`label`, `changeLabel`, ``),
+  withHandlers({
+    // according to https://github.com/vkbansal/react-contextmenu/issues/65
+    // this is how to pass data from ContextMenuTrigger to ContextMenu
+    onShow: props => (event) =>
+      props.changeLabel(event.detail.data.nodeLabel)
+    },
+  ),
   observer
 )
 
 const PopFolder = (
-  { onClick, store }:
-  {onClick:() => void, store:Object}
+  { onClick, store, changeLabel, label, onShow }:
+  {onClick:()=>void,store:Object,changeLabel:()=>{},label:string|number,onShow:()=>void}
 ) =>
-  <ContextMenu id="popFolder" >
+  <ContextMenu
+    id="popFolder"
+    collect={props => props}
+    onShow={onShow}
+  >
     <div className="react-contextmenu-title">Populationen</div>
     <MenuItem
       onClick={onClick}
@@ -22,8 +36,10 @@ const PopFolder = (
         table: `pop`,
       }}
     >
-      neu
+      erstelle neue
     </MenuItem>
+    <div className="react-contextmenu-divider" />
+    <div className="react-contextmenu-title">Karte</div>
     <MenuItem
       onClick={onClick}
       data={{
@@ -32,7 +48,7 @@ const PopFolder = (
         idTable: `ap`,
       }}
     >
-      {`Populationen in Karte ${store.map.pop.visible ? `ausblenden` : `zeigen`}`}
+      {`blende Populationen ${store.map.pop.visible ? `aus` : `ein`}`}
     </MenuItem>
     <MenuItem
       onClick={onClick}
@@ -42,12 +58,15 @@ const PopFolder = (
         idTable: `ap`,
       }}
     >
-      {`Teil-Populationen in Karte ${store.map.tpop.visible ? `ausblenden` : `zeigen`}`}
+      {`blende Teil-Populationen ${store.map.tpop.visible ? `aus` : `ein`}`}
     </MenuItem>
   </ContextMenu>
 
 PopFolder.propTypes = {
   onClick: PropTypes.func.isRequired,
+  changeLabel: PropTypes.func.isRequired,
+  label: PropTypes.any.isRequired,
+  onShow: PropTypes.func.isRequired,
 }
 
 export default enhance(PopFolder)
