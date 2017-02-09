56542e8b6940fb4ea14f6d8b338a5697e8b5fc9b
Alexander Gabriel <alex@gabriel-software.ch>
2017-02-09 18:47:50 +0100

    enhance context menu

--- src/components/Projekte/StrukturbaumContainer/contextmenu/TpopmassnberFolder.js
+++ src/components/Projekte/StrukturbaumContainer/contextmenu/TpopmassnberFolder.js
@@ -4,7 +4,7 @@ import { ContextMenu, MenuItem } from 'react-contextmenu'
 
 const TpopmassnberFolder = ({ onClick }:{onClick:() => void}) =>
   <ContextMenu id="tpopmassnberFolder" >
-    <div className="react-contextmenu-title" style={{ width: `185px` }}>Massnahmen-Berichte</div>
+    <div className="react-contextmenu-title">Massnahmen-Berichte</div>
     <MenuItem
       onClick={onClick}
       data={{
@@ -12,7 +12,7 @@ const TpopmassnberFolder = ({ onClick }:{onClick:() => void}) =>
         table: `tpopmassnber`,
       }}
     >
-      neu
+      erstelle neuen
     </MenuItem>
   </ContextMenu>
 
