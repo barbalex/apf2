2e03eb9aee4916a6538e50f4cbe1e1352937ab67
Alexander Gabriel <alex@gabriel-software.ch>
2017-02-09 12:17:33 +0100

    comment

--- src/modules/setLoginFromIdb.js
+++ src/modules/setLoginFromIdb.js
@@ -1,11 +1,16 @@
 // @flow
 import app from 'ampersand-app'
 
+// store.user.name default value is set to something random
+// because if it is empty, the login form appears for
+// just a very short time until currentUser was
+// fetched from idb
+// so need to set store.user.name to `` if something
+// goes wrong or no user name is received
 export default (store:Object) => {
   app.db.currentUser
   .toArray()
   .then((users) => {
-    console.log(`setLoginFromIdb: users:`, users)
     if (users[0] && users[0].name) {
       store.user.name = users[0].name
     } else {
@@ -13,7 +18,7 @@ export default (store:Object) => {
     }
   })
   .catch((error) => {
-    console.log(error)
+    store.listError(error)
     store.user.name = ``
   })
 }
