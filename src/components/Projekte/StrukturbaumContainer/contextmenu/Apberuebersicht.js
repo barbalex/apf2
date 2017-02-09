ea853be0e0196072028bc114bcf0c118766f8ac2
Alexander Gabriel <alex@gabriel-software.ch>
2017-02-09 13:54:54 +0100

    enhance context menu

--- src/components/Projekte/StrukturbaumContainer/contextmenu/Apberuebersicht.js
+++ src/components/Projekte/StrukturbaumContainer/contextmenu/Apberuebersicht.js
@@ -1,9 +1,30 @@
 // @flow
 import React, { PropTypes } from 'react'
 import { ContextMenu, MenuItem } from 'react-contextmenu'
+import compose from 'recompose/compose'
+import withState from 'recompose/withState'
+import withHandlers from 'recompose/withHandlers'
 
-const Apberuebersicht = ({ onClick }:{onClick:() => void}) =>
-  <ContextMenu id="apberuebersicht" >
+const enhance = compose(
+  withState(`label`, `changeLabel`, ``),
+  withHandlers({
+    // according to https://github.com/vkbansal/react-contextmenu/issues/65
+    // this is how to pass data from ContextMenuTrigger to ContextMenu
+    onShow: props => (event) =>
+      props.changeLabel(event.detail.data.nodeLabel)
+    ,
+  })
+)
+
+const Apberuebersicht = (
+  { onClick, changeLabel, label, onShow }:
+  {onClick:()=>void,changeLabel:()=>{},label:string|number,onShow:()=>{}}
+) =>
+  <ContextMenu
+    id="apberuebersicht"
+    collect={props => props}
+    onShow={onShow}
+  >
     <div className="react-contextmenu-title">AP-Bericht</div>
     <MenuItem
       onClick={onClick}
@@ -12,7 +33,7 @@ const Apberuebersicht = ({ onClick }:{onClick:() => void}) =>
         table: `apberuebersicht`,
       }}
     >
-      neu
+      erstelle neuen
     </MenuItem>
     <MenuItem
       onClick={onClick}
@@ -21,12 +42,15 @@ const Apberuebersicht = ({ onClick }:{onClick:() => void}) =>
         table: `apberuebersicht`,
       }}
     >
-      löschen
+      {`lösche "${label}"`}
     </MenuItem>
   </ContextMenu>
 
 Apberuebersicht.propTypes = {
   onClick: PropTypes.func.isRequired,
+  onShow: PropTypes.func.isRequired,
+  changeLabel: PropTypes.func.isRequired,
+  label: PropTypes.any.isRequired,
 }
 
-export default Apberuebersicht
+export default enhance(Apberuebersicht)
