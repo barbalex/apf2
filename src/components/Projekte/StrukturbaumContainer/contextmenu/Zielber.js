9e3ad5cdf753825c8bb2c401524deea2986fed6b
Alexander Gabriel <alex@gabriel-software.ch>
2017-02-09 18:47:39 +0100

    enhance context menu

--- src/components/Projekte/StrukturbaumContainer/contextmenu/Zielber.js
+++ src/components/Projekte/StrukturbaumContainer/contextmenu/Zielber.js
@@ -1,9 +1,30 @@
 // @flow
 import React, { PropTypes } from 'react'
 import { ContextMenu, MenuItem } from 'react-contextmenu'
+import compose from 'recompose/compose'
+import withState from 'recompose/withState'
+import withHandlers from 'recompose/withHandlers'
 
-const Zielber = ({ onClick }:{onClick:() => void}) =>
-  <ContextMenu id="zielber" >
+const enhance = compose(
+  withState(`label`, `changeLabel`, ``),
+  withHandlers({
+    // according to https://github.com/vkbansal/react-contextmenu/issues/65
+    // this is how to pass data from ContextMenuTrigger to ContextMenu
+    onShow: props => (event) =>
+      props.changeLabel(event.detail.data.nodeLabel)
+    ,
+  })
+)
+
+const Zielber = (
+  { onClick, changeLabel, label, onShow }:
+  {onClick:()=>void,changeLabel:()=>{},label:string|number,onShow:()=>void}
+) =>
+  <ContextMenu
+    id="zielber"
+    collect={props => props}
+    onShow={onShow}
+  >
     <div className="react-contextmenu-title">Bericht</div>
     <MenuItem
       onClick={onClick}
@@ -12,7 +33,7 @@ const Zielber = ({ onClick }:{onClick:() => void}) =>
         table: `zielber`,
       }}
     >
-      neu
+      erstelle neuen
     </MenuItem>
     <MenuItem
       onClick={onClick}
@@ -21,12 +42,15 @@ const Zielber = ({ onClick }:{onClick:() => void}) =>
         table: `zielber`,
       }}
     >
-      löschen
+      {`lösche "${label}"`}
     </MenuItem>
   </ContextMenu>
 
 Zielber.propTypes = {
   onClick: PropTypes.func.isRequired,
+  changeLabel: PropTypes.func.isRequired,
+  label: PropTypes.any.isRequired,
+  onShow: PropTypes.func.isRequired,
 }
 
-export default Zielber
+export default enhance(Zielber)
