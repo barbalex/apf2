0bb983a01e48dd34d988f994b200773c5f21e94c
Alexander Gabriel <alex@gabriel-software.ch>
2017-02-09 13:15:25 +0100

    enhance context menu

--- src/components/Projekte/StrukturbaumContainer/contextmenu/Apber.js
+++ src/components/Projekte/StrukturbaumContainer/contextmenu/Apber.js
@@ -1,9 +1,30 @@
 // @flow
 import React, { PropTypes } from 'react'
 import { ContextMenu, MenuItem } from 'react-contextmenu'
+import compose from 'recompose/compose'
+import withState from 'recompose/withState'
+import withHandlers from 'recompose/withHandlers'
 
-const Apber = ({ onClick }:{onClick:() => void}) =>
-  <ContextMenu id="apber" >
+const enhance = compose(
+  withState(`label`, `changeLabel`, ``),
+  withHandlers({
+    // according to https://github.com/vkbansal/react-contextmenu/issues/65
+    // this is how to pass data from ContextMenuTrigger to ContextMenu
+    onShow: props => (event) =>
+      props.changeLabel(event.detail.data.nodeLabel)
+    ,
+  })
+)
+
+const Apber = (
+  { onClick, changeLabel, label, onShow }:
+  {onClick:()=>void,changeLabel:()=>{},label:string,onShow:()=>{}}
+) =>
+  <ContextMenu
+    id="apber"
+    collect={props => props}
+    onShow={onShow}
+  >
     <div className="react-contextmenu-title">AP-Bericht</div>
     <MenuItem
       onClick={onClick}
@@ -12,7 +33,7 @@ const Apber = ({ onClick }:{onClick:() => void}) =>
         table: `apber`,
       }}
     >
-      neu
+      erstelle neuen
     </MenuItem>
     <MenuItem
       onClick={onClick}
@@ -21,12 +42,14 @@ const Apber = ({ onClick }:{onClick:() => void}) =>
         table: `apber`,
       }}
     >
-      löschen
+      {`lösche "${label}"`}
     </MenuItem>
   </ContextMenu>
 
 Apber.propTypes = {
   onClick: PropTypes.func.isRequired,
+  changeLabel: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired,
 }
 
-export default Apber
+export default enhance(Apber)
