8ae3b7a9934dfc608bec304764ca94b2c35ad005
Alexander Gabriel <alex@gabriel-software.ch>
2017-02-09 13:54:25 +0100

    enhance context menu

--- src/components/Projekte/StrukturbaumContainer/contextmenu/Pop.js
+++ src/components/Projekte/StrukturbaumContainer/contextmenu/Pop.js
@@ -9,19 +9,21 @@ import withHandlers from 'recompose/withHandlers'
 const enhance = compose(
   inject(`store`),
   withState(`id`, `changeId`, 0),
+  withState(`label`, `changeLabel`, ``),
   withHandlers({
     // according to https://github.com/vkbansal/react-contextmenu/issues/65
     // this is how to pass data from ContextMenuTrigger to ContextMenu
-    onShow: props => (event) =>
+    onShow: props => (event) => {
       props.changeId(event.detail.data.nodeId)
-    ,
+      props.changeLabel(event.detail.data.nodeLabel)
+    },
   }),
   observer
 )
 
 const Pop = (
-  { onClick, store, changeId, id, onShow }:
-  {onClick:() => void,store:Object,changeId:()=>{},id:number,onShow:()=>{}}
+  { onClick, store, changeId, id, changeLabel, label, onShow }:
+  {onClick:()=>void,store:Object,changeId:()=>{},id:number,changeLabel:()=>{},label:string,onShow:()=>void}
 ) =>
   <ContextMenu
     id="pop"
@@ -36,7 +38,7 @@ const Pop = (
         table: `pop`,
       }}
     >
-      neu
+      erstelle neue
     </MenuItem>
     <MenuItem
       onClick={onClick}
@@ -45,8 +47,10 @@ const Pop = (
         table: `pop`,
       }}
     >
-      löschen
+      {`lösche "${label}"`}
     </MenuItem>
+    <div className="react-contextmenu-divider" />
+    <div className="react-contextmenu-title">Karte</div>
     <MenuItem
       onClick={onClick}
       data={{
@@ -56,9 +60,9 @@ const Pop = (
       }}
     >
       {
-        store.map.pop.highlightedIds.includes(id) ?
-        `Hervorhebung der Population aufheben` :
-        `Population in Karte hervorheben`
+        store.map.pop.highlightedIds.includes(id) && store.map.pop.visible ?
+        `Hebe Markierung von "${label}" auf` :
+        `Markiere "${label}"`
       }
     </MenuItem>
     <MenuItem
@@ -69,7 +73,7 @@ const Pop = (
         idTable: `ap`,
       }}
     >
-      {`Teil-Populationen in Karte ${store.map.tpop.visible ? `ausblenden` : `zeigen`}`}
+      {`blende Teil-Populationen ${store.map.tpop.visible ? `aus` : `ein`}`}
     </MenuItem>
   </ContextMenu>
 
@@ -77,8 +81,10 @@ Pop.propTypes = {
   onClick: PropTypes.func.isRequired,
   store: PropTypes.object.isRequired,
   changeId: PropTypes.func.isRequired,
-  onShow: PropTypes.func.isRequired,
   id: PropTypes.number.isRequired,
+  changeLabel: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired,
+  onShow: PropTypes.func.isRequired,
 }
 
 export default enhance(Pop)
