fe570562312b8326c428e15c830eee0f7cb1a9a0
Alexander Gabriel <alex@gabriel-software.ch>
2017-02-16 14:35:34 +0100

    Dont load popmassn_erfbeurt_werte

--- src/modules/fetchStammdatenTables.js
+++ src/modules/fetchStammdatenTables.js
@@ -3,7 +3,10 @@ import tables from './tables'
 import fetchStammdatenTable from './fetchStammdatenTable'
 
 export default async (store:Object) => {
-  const stammdatenTablesMetadata = tables.filter(t => t.stammdaten)
+  const stammdatenTablesMetadata = tables
+    .filter(t => t.stammdaten)
+    // this table is listed but does not exist in db
+    .filter(t => t.table !== `popmassn_erfbeurt_werte`)
   stammdatenTablesMetadata.forEach(t =>
     fetchStammdatenTable(store, t)
   )
