7f899b8f7a503df030aa40602a8961c6b1952dc4
Alexander Gabriel <alex@gabriel-software.ch>
2017-02-23 21:26:34 +0100

    get correct length

--- src/modules/nodes/apFolder.js
+++ src/modules/nodes/apFolder.js
@@ -1,13 +1,13 @@
 import findIndex from 'lodash/findIndex'
 
 export default (store) => {
-  const { activeUrlElements } = store
+  const { activeUrlElements, table } = store
   // fetch sorting indexes of parents
   const projId = activeUrlElements.projekt
   if (!projId) return []
   const projIndex = findIndex(store.table.filteredAndSorted.projekt, { ProjId: projId })
   // build label
-  const apNodesLength = store.node.node.ap.length
+  const apNodesLength = table.filteredAndSorted.ap.length
   let message = apNodesLength
   if (store.table.apLoading) {
     message = `...`
