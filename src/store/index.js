d8c32a64741a71572cd9d1c0f9efa97d2451343e
Alexander Gabriel <alex@gabriel-software.ch>
2017-03-08 12:37:04 +0100

    work on pop

--- src/store/index.js
+++ src/store/index.js
@@ -512,6 +512,26 @@ function Store() {
       // sort by label and return
       return sortBy(zielbere, `label`)
     }),
+    pop: computed(() => {
+      const { activeUrlElements, table, node } = this
+      // grab pop as array and sort them by year
+      let pop = Array.from(table.pop.values())
+      // show only nodes of active ap
+      pop = pop.filter(a => a.ApArtId === activeUrlElements.ap)
+      pop = sortBy(pop, `PopNr`)
+      // map through all projekt and create array of nodes
+      pop.forEach((el) => {
+        el.label = `${el.PopNr || `(keine Nr)`}: ${el.PopName || `(kein Name)`}`
+      })
+      // filter by node.nodeLabelFilter
+      const filterString = node.nodeLabelFilter.get(`pop`)
+      if (filterString) {
+        pop = pop.filter(p =>
+          p.label.toLowerCase().includes(filterString.toLowerCase())
+        )
+      }
+      return pop
+    }),
   })
   this.valuesForWhichTableDataWasFetched = {}
   this.qk = observable.map()
