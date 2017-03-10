4b2026ff4bb73d560959eb509183200c6f7a2735
Alexander Gabriel <alex@gabriel-software.ch>
2017-03-10 17:57:29 +0100

    Get Zuordnung to work

--- src/modules/insertBeobzuordnung.js
+++ src/modules/insertBeobzuordnung.js
@@ -15,40 +15,45 @@ const updateBeobzuordnungData = (store, beobBereitgestellt, newKey, newValue) =>
   store.updatePropertyInDb(`QuelleId`, beobBereitgestellt.QuelleId)
 }
 
+const continueWithBeobBereitgestellt = (store, beobBereitgestellt, newKey, newValue) => {
+  const { projekt, ap } = store.activeUrlElements
+  // set new url
+  const query = `${Object.keys(store.urlQuery).length > 0 ? `?${queryString.stringify(store.urlQuery)}` : ``}`
+  if (newKey === `BeobNichtZuordnen`) {
+    const newUrl = `/Projekte/${projekt}/Arten/${ap}/nicht-zuzuordnende-Beobachtungen/${beobBereitgestellt.BeobId}${query}`
+    store.history.push(newUrl)
+    updateBeobzuordnungData(store, beobBereitgestellt, newKey, newValue)
+  } else if (newKey === `TPopId`) {
+    // ouch. Need to get url for this tpop
+    // Nice: tpop was already loaded for building tpop list
+    const tpop = store.table.tpop.get(newValue)
+    const newUrl = `/Projekte/${projekt}/Arten/${ap}/Populationen/${tpop.PopId}/Teil-Populationen/${newValue}/Beobachtungen/${beobBereitgestellt.BeobId}${query}`
+    store.history.push(newUrl)
+    updateBeobzuordnungData(store, beobBereitgestellt, newKey, newValue)
+  }
+}
+
 export default (store:Object, newKey:string, newValue:number) => {
   /**
    * newKey is either BeobNichtZuordnen or TPopId
    */
   // get data from beob_bereitgestellt in activeDataset
   const beobBereitgestellt = store.activeDataset.row
+  // check if a corresponding beobzuordnung already exists
+  const beobzuordnungExists = !!store.table.beobzuordnung.get(beobBereitgestellt.BeobId)
+  if (beobzuordnungExists) {
+    return continueWithBeobBereitgestellt(store, beobBereitgestellt, newKey, newValue)
+  }
   // insert new dataset in db and fetch id
   const url = `${apiBaseUrl}/apflora/beobzuordnung/NO_NOTE/${beobBereitgestellt.BeobId}`
   axios.post(url)
-    .then((result) => {
-      const { projekt, ap } = store.activeUrlElements
-      const row = result.data
+    .then(({ data }) => {
+      const row = data
       // insert this dataset in store.table
       store.table.beobzuordnung.set(row.NO_NOTE, row)
       // insert this dataset in idb
       insertDatasetInIdb(store, `beobzuordnung`, row)
-      // set new url
-      const query = `${Object.keys(store.urlQuery).length > 0 ? `?${queryString.stringify(store.urlQuery)}` : ``}`
-      if (newKey === `BeobNichtZuordnen`) {
-        const newUrl = `${appBaseUrl}/Projekte/${projekt}/Arten/${ap}/nicht-zuzuordnende-Beobachtungen/${beobBereitgestellt.BeobId}${query}`
-        store.history.push(newUrl)
-        updateBeobzuordnungData(store, beobBereitgestellt, newKey, newValue)
-      } else if (newKey === `TPopId`) {
-        // ouch. Need to get url to this tpop
-        // fetch tpop to get needed data to build new url
-        const url = `${apiBaseUrl}/apflora/tpop/TPopId/${newValue}`
-        axios.post(url)
-          .then(({ data }) => {
-            const newUrl = `${appBaseUrl}/Projekte/${projekt}/Arten/${ap}/Populationen/${data.PopId}/Teil-Populationen/${newValue}/Beobachtungen/${beobBereitgestellt.BeobId}${query}`
-            store.history.push(newUrl)
-            updateBeobzuordnungData(store, beobBereitgestellt, newKey, newValue)
-          })
-          .catch(error => store.listError(error))
-      }
+      continueWithBeobBereitgestellt(store, beobBereitgestellt, newKey, newValue)
     })
     .catch(error => store.listError(error))
 }
