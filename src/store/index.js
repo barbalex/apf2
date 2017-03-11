61400bb15e370987ab5b13f9a8f62c286440a150
Alexander Gabriel <alex@gabriel-software.ch>
2017-03-11 18:23:28 +0100

    modularize

--- src/store/index.js
+++ src/store/index.js
@@ -105,6 +105,7 @@ import tpopmassnberNode from '../modules/nodes/tpopmassnber'
 import tpopmassnFolderNode from '../modules/nodes/tpopmassnFolder'
 import tpopmassnNode from '../modules/nodes/tpopmassn'
 import filteredAndSortedProjekt from './table/filteredAndSorted/projekt'
+import filteredAndSortedTpopmassn from './table/filteredAndSorted/tpopmassn'
 
 import TableStore from './table'
 import ObservableHistory from './ObservableHistory'
@@ -820,30 +821,7 @@ function Store() {
       // sort by label and return
       return sortBy(tpopmassnber, `label`)
     }),
-    tpopmassn: computed(() => {
-      const { activeUrlElements, table, node } = this
-      // grab tpopmassn as array and sort them by year
-      let tpopmassn = Array.from(table.tpopmassn.values())
-      // show only nodes of active ap
-      tpopmassn = tpopmassn.filter(a => a.TPopId === activeUrlElements.tpop)
-      // get erfkritWerte
-      const tpopmassntypWerte = Array.from(table.tpopmassn_typ_werte.values())
-      // map through all projekt and create array of nodes
-      tpopmassn.forEach((el) => {
-        const tpopmassntypWert = tpopmassntypWerte.find(e => e.MassnTypCode === el.TPopMassnTyp)
-        const massnTypTxt = tpopmassntypWert ? tpopmassntypWert.MassnTypTxt : null
-        el.label = `${el.TPopMassnJahr || `(kein Jahr)`}: ${massnTypTxt || `(kein Typ)`}`
-      })
-      // filter by node.nodeLabelFilter
-      const filterString = node.nodeLabelFilter.get(`tpopmassn`)
-      if (filterString) {
-        tpopmassn = tpopmassn.filter(p =>
-          p.label.toLowerCase().includes(filterString.toLowerCase())
-        )
-      }
-      // sort by label and return
-      return sortBy(tpopmassn, `label`)
-    }),
+    tpopmassn: computed(() => filteredAndSortedTpopmassn(this)),
   })
   this.valuesForWhichTableDataWasFetched = {}
   this.qk = observable.map()
