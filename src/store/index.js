8d57978f6b054dfef6cd1ddeec90284f73150c6d
Alexander Gabriel <alex@gabriel-software.ch>
2017-03-11 21:59:06 +0100

    modularize

--- src/store/index.js
+++ src/store/index.js
@@ -105,6 +105,7 @@ import tpopmassnberNode from '../modules/nodes/tpopmassnber'
 import tpopmassnFolderNode from '../modules/nodes/tpopmassnFolder'
 import tpopmassnNode from '../modules/nodes/tpopmassn'
 import filteredAndSortedProjekt from './table/filteredAndSorted/projekt'
+import filteredAndSortedPopmassnber from './table/filteredAndSorted/popmassnber'
 import filteredAndSortedPopber from './table/filteredAndSorted/popber'
 import filteredAndSortedTpop from './table/filteredAndSorted/tpop'
 import filteredAndSortedTpopbeob from './table/filteredAndSorted/tpopbeob'
@@ -582,30 +583,7 @@ function Store() {
       }
       return pop
     }),
-    popmassnber: computed(() => {
-      const { activeUrlElements, table, node } = this
-      // grab popmassnber as array and sort them by year
-      let popmassnber = Array.from(table.popmassnber.values())
-      // show only nodes of active pop
-      popmassnber = popmassnber.filter(a => a.PopId === activeUrlElements.pop)
-      // get erfkritWerte
-      const tpopmassnErfbeurtWerte = Array.from(table.tpopmassn_erfbeurt_werte.values())
-      // map through all projekt and create array of nodes
-      popmassnber.forEach((el) => {
-        const tpopmassnErfbeurtWert = tpopmassnErfbeurtWerte.find(e => e.BeurteilId === el.PopMassnBerErfolgsbeurteilung)
-        const beurteilTxt = tpopmassnErfbeurtWert ? tpopmassnErfbeurtWert.BeurteilTxt : null
-        el.label = `${el.PopMassnBerJahr || `(kein Jahr)`}: ${beurteilTxt || `(nicht beurteilt)`}`
-      })
-      // filter by node.nodeLabelFilter
-      const filterString = node.nodeLabelFilter.get(`popmassnber`)
-      if (filterString) {
-        popmassnber = popmassnber.filter(p =>
-          p.label.toLowerCase().includes(filterString.toLowerCase())
-        )
-      }
-      // sort by label and return
-      return sortBy(popmassnber, `label`)
-    }),
+    popmassnber: computed(() => filteredAndSortedPopmassnber(this)),
     popber: computed(() => filteredAndSortedPopber(this)),
     tpop: computed(() => filteredAndSortedTpop(this)),
     tpopbeob: computed(() => filteredAndSortedTpopbeob(this)),
