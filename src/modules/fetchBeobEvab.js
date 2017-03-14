869e09f232c6cdbef0992c97bc4e18a25134334b
Alexander Gabriel <alex@gabriel-software.ch>
2017-03-14 18:04:05 +0100

    work on tpop beob assign

--- src/modules/fetchBeobEvab.js
+++ src/modules/fetchBeobEvab.js
@@ -10,48 +10,48 @@ import recordValuesForWhichTableDataWasFetched from './recordValuesForWhichTable
 const writeToStore = (store, data) => {
   runInAction(() => {
     data.forEach((d) => {
-      d.beobzuordnung = computed(() => store.table.beobzuordnung.get(d.BeobId))
-      store.table.beob_bereitgestellt.set(d.BeobId, d)
+      d.beob_bereitgestellt = computed(() => store.table.beob_bereitgestellt.get(d.NO_NOTE_PROJET))
+      store.table.beob_evab.set(d.NO_NOTE_PROJET, d)
     })
   })
 }
 
 export default (store:Object, apArtId:number) => {
   if (!apArtId) {
-    return new Error(`action fetchBeobBereitgestellt: apArtId must be passed`)
+    return new Error(`action fetchBeobEvab: apArtId must be passed`)
   }
 
   // only fetch if not yet fetched
   const { valuesForWhichTableDataWasFetched } = store
   if (
-    valuesForWhichTableDataWasFetched.beob_bereitgestellt &&
-    valuesForWhichTableDataWasFetched.beob_bereitgestellt.NO_ISFS &&
-    valuesForWhichTableDataWasFetched.beob_bereitgestellt.NO_ISFS.includes(apArtId)
+    valuesForWhichTableDataWasFetched.beob_evab &&
+    valuesForWhichTableDataWasFetched.beob_evab.NO_ISFS &&
+    valuesForWhichTableDataWasFetched.beob_evab.NO_ISFS.includes(apArtId)
   ) {
     return
   }
 
-  const url = `${apiBaseUrl}/schema/beob/table/beob_bereitgestellt/field/NO_ISFS/value/${apArtId}`
-  store.loading.push(`beob_bereitgestellt`)
-  app.db.beob_bereitgestellt
+  const url = `${apiBaseUrl}/schema/beob/table/beob_evab/field/NO_ISFS/value/${apArtId}`
+  store.loading.push(`beob_evab`)
+  app.db.beob_evab
     .toArray()
     .then((data) => {
       writeToStore(store, data)
-      recordValuesForWhichTableDataWasFetched({ store, table: `beob_bereitgestellt`, field: `NO_ISFS`, value: apArtId })
+      recordValuesForWhichTableDataWasFetched({ store, table: `beob_evab`, field: `NO_ISFS`, value: apArtId })
     })
     .then(() => axios.get(url))
     .then(({ data }) => {
-      store.loading = store.loading.filter(el => el !== `beob_bereitgestellt`)
+      store.loading = store.loading.filter(el => el !== `beob_evab`)
       // leave ui react before this happens
       // leave ui react before this happens
       // copy array without the individual objects being references
       // otherwise the computed values are passed to idb
       // and this creates errors, as they can't be cloned
       setTimeout(() => writeToStore(store, cloneDeep(data)))
-      setTimeout(() => app.db.beob_bereitgestellt.bulkPut(data))
+      setTimeout(() => app.db.beob_evab.bulkPut(data))
     })
     .catch(error => {
-      store.loading = store.loading.filter(el => el !== `beob_bereitgestellt`)
+      store.loading = store.loading.filter(el => el !== `beob_evab`)
       store.listError(error)
     })
 }
---
+++ src/modules/getBeobForMap.js
@@ -0,0 +1,29 @@
+// @flow
+import epsg21781to4326 from './epsg21781to4326'
+
+export default (store:Object) => {
+  const { table, activeUrlElements, node } = store
+  const myApArtId = activeUrlElements.ap || store.map.pop.apArtId
+  // get beob of this ap
+  const beobOfActiveAp = Array.from(table.beob_bereitgestellt.values())
+    .filter(beob => beob.NO_ISFS === myApArtId)
+  const beobIdsOfActiveAp = beobOfActiveAp.map(beob => beob.BeobId)
+  // get beob_evab and beob_infospezies of this ap
+  let tpops = Array.from(table.tpop.values())
+    .filter(p => popIdsOfActiveAp.includes(p.PopId))
+    // omit tpops without coordinates
+    .filter(p => p.TPopXKoord && p.TPopYKoord)
+  // filter them by nodeLabelFilter
+  const tpopFilterString = node.nodeLabelFilter.get(`tpop`)
+  if (tpopFilterString) {
+    tpops = tpops.filter((p) => {
+      const label = `${p.TPopNr || `(keine Nr)`}: ${p.TPopFlurname || `(kein Flurname)`}`
+      return label.toLowerCase().includes(tpopFilterString.toLowerCase())
+    })
+  }
+  tpops = tpops.map((p) => {
+    p.TPopKoordWgs84 = epsg21781to4326(p.TPopXKoord, p.TPopYKoord)
+    return p
+  })
+  return tpops
+}
