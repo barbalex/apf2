5239e68c08a97f195949e71a90079b5121c7acc2
Alexander Gabriel <alex@gabriel-software.ch>
2017-03-17 17:17:08 +0100

    modularize

---
+++ src/components/Projekte/Karte/LayersControl/Overlays.js
@@ -0,0 +1,74 @@
+import React, { PropTypes } from 'react'
+import { toJS } from 'mobx'
+import styled from 'styled-components'
+import FontIcon from 'material-ui/FontIcon'
+
+const CardContent = styled.div`
+  color: rgb(48, 48, 48);
+  padding-left: 5px;
+  padding-right: 5px;
+`
+const DragHandle = styled(FontIcon)`
+  font-size: 18px !important;
+  color: rgb(48, 48, 48) !important;
+  cursor: grab;
+`
+const LayerDiv = styled.div`
+  border-bottom: 1px solid #ececec;
+  display: flex;
+  justify-content: space-between;
+  padding-top: 4px;
+`
+const Input = styled.input`
+  margin-right: 4px;
+  /*vertical-align: -2px;*/
+`
+const Label = styled.label`
+  padding-right: 4px;
+  user-select: none;
+`
+/**
+ * don't know why but passing store
+ * with mobx inject does not work here
+ * so passed in from parent
+ */
+
+const Overlays = ({ store }) => {
+  const activeOverlays = toJS(store.map.activeOverlays)
+
+  return (
+    <CardContent>
+      {
+        store.map.overlays.map((o, index) =>
+          <LayerDiv key={index}>
+            <Label>
+              <Input
+                type="checkbox"
+                value={o.value}
+                checked={activeOverlays.includes(o.value)}
+                onChange={() => {
+                  if (activeOverlays.includes(o.value)) {
+                    return store.map.removeActiveOverlay(o.value)
+                  }
+                  return store.map.addActiveOverlay(o.value)
+                }}
+              />
+              {o.label}
+            </Label>
+            <div>
+              <DragHandle className="material-icons">
+                drag_handle
+              </DragHandle>
+            </div>
+          </LayerDiv>
+        )
+      }
+    </CardContent>
+  )
+}
+
+Overlays.propTypes = {
+  store: PropTypes.object.isRequired,
+}
+
+export default Overlays
--- src/components/Projekte/Karte/LayersControl/index.js
+++ src/components/Projekte/Karte/LayersControl/index.js
@@ -1,6 +1,5 @@
 import React, { PropTypes } from 'react'
 import Control from 'react-leaflet-control'
-import { toJS } from 'mobx'
 import { observer, inject } from 'mobx-react'
 import styled from 'styled-components'
 import compose from 'recompose/compose'
@@ -10,6 +9,8 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
 import FontIcon from 'material-ui/FontIcon'
+
+import Overlays from './Overlays'
 
 const theme = Object.assign({}, baseTheme)
 
@@ -45,18 +46,8 @@ const StyledFontIcon = styled(FontIcon)`
   font-size: 18px !important;
   color: rgb(48, 48, 48) !important;
 `
-const DragHandle = styled(FontIcon)`
-  font-size: 18px !important;
-  color: rgb(48, 48, 48) !important;
-  cursor: grab;
-`
-const LayerDiv = styled.div`
-  border-bottom: 1px solid #ececec;
-  display: flex;
-  justify-content: space-between;
-  padding-top: 4px;
-`
 const Input = styled.input`
+  margin-right: 4px;
   /*vertical-align: -2px;*/
 `
 const Label = styled.label`
@@ -105,7 +96,6 @@ const LayersControl = ({
     { label: `ZH Orthofoto Frühjahr 2015/16 RGB`, value: `ZhOrtho2015` },
     { label: `ZH Orthofoto Frühjahr 2015/16 infrarot`, value: `ZhOrtho2015Ir` },
   ]
-  const activeOverlays = toJS(store.map.activeOverlays)
 
   return (
     <Control position="topright">
@@ -153,33 +143,7 @@ const LayersControl = ({
             </CardHeader>
             {
               overlaysExpanded &&
-              <CardContent>
-                {
-                  store.map.overlays.map((o, index) =>
-                    <LayerDiv key={index}>
-                      <Label>
-                        <Input
-                          type="checkbox"
-                          value={o.value}
-                          checked={activeOverlays.includes(o.value)}
-                          onChange={() => {
-                            if (activeOverlays.includes(o.value)) {
-                              return store.map.removeActiveOverlay(o.value)
-                            }
-                            return store.map.addActiveOverlay(o.value)
-                          }}
-                        />
-                        {o.label}
-                      </Label>
-                      <div>
-                        <DragHandle className="material-icons">
-                          drag_handle
-                        </DragHandle>
-                      </div>
-                    </LayerDiv>
-                  )
-                }
-              </CardContent>
+              <Overlays store={store} />
             }
           </Card>
         </CardContainer>
