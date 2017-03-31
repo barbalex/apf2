266f4422785cee3a1fd3fde7a5b6ba86491d389d
Alexander Gabriel <alex@gabriel-software.ch>
2017-03-31 17:17:49 +0200

    Move filteredAndSorted to node state

--- src/modules/nodes/tpop.js
+++ src/modules/nodes/tpop.js
@@ -1,19 +1,19 @@
 import findIndex from 'lodash/findIndex'
 
 export default (store) => {
-  const { activeUrlElements, table } = store
+  const { activeUrlElements, node } = store
   // fetch sorting indexes of parents
   const projId = activeUrlElements.projekt
   if (!projId) return []
-  const projIndex = findIndex(table.filteredAndSorted.projekt, { ProjId: projId })
+  const projIndex = findIndex(node.filteredAndSorted.projekt, { ProjId: projId })
   const apArtId = activeUrlElements.ap
   if (!apArtId) return []
-  const apIndex = findIndex(table.filteredAndSorted.ap, { ApArtId: apArtId })
+  const apIndex = findIndex(node.filteredAndSorted.ap, { ApArtId: apArtId })
   const popId = activeUrlElements.pop
   if (!popId) return []
-  const popIndex = findIndex(table.filteredAndSorted.pop, { PopId: popId })
+  const popIndex = findIndex(node.filteredAndSorted.pop, { PopId: popId })
 
-  return table.filteredAndSorted.tpop.map((el, index) => ({
+  return node.filteredAndSorted.tpop.map((el, index) => ({
     nodeType: `table`,
     menuType: `tpop`,
     id: el.TPopId,
