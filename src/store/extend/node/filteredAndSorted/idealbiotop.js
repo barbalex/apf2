b6fce7bb80bbacd363b83f2b393f9a42834c283e
Alexander Gabriel <alex@gabriel-software.ch>
2017-03-31 23:11:02 +0200

    rename node to tree

--- src/store/extend/node/filteredAndSorted/idealbiotop.js
+++
@@ -1,8 +0,0 @@
-export default (store) => {
-  const { activeUrlElements, table } = store
-  // grab assozart as array and sort them by year
-  let idealbiotop = Array.from(table.idealbiotop.values())
-  // show only nodes of active ap
-  idealbiotop = idealbiotop.filter(a => a.IbApArtId === activeUrlElements.ap)
-  return idealbiotop
-}
