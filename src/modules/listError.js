391ca1bcdf1d32a099dfc83b2ba682f155b4a5cc
Alexander Gabriel <alex@gabriel-software.ch>
2017-03-31 19:06:25 +0200

    refactor actions

--- src/modules/listError.js
+++
@@ -1,8 +0,0 @@
-// @flow
-export default (store:Object, error:Object) => {
-  store.app.errors.unshift(error)
-  setTimeout(() => {
-    store.app.errors.pop()
-  }, 1000 * 10)
-  console.log(`Error:`, error)  // eslint-disable-line no-console
-}
