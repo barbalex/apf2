d47b2795c36fe9df93e4dd36bfea2c5c6ffed0ed
Alexander Gabriel <alex@gabriel-software.ch>
2017-03-31 19:06:18 +0200

    refactor actions

--- src/modules/insertDatasetInIdb.js
+++
@@ -1,9 +0,0 @@
-// @flow
-import app from 'ampersand-app'
-
-export default (store:Object, table:string, dataset:Object) => {
-  app.db[table].put(dataset)
-    .catch(error =>
-      store.listError(error)
-    )
-}
