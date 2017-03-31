f5a5e24ba37dcba3f11a836156e177f4ae6bd11e
Alexander Gabriel <alex@gabriel-software.ch>
2017-03-31 17:18:06 +0200

    Move filteredAndSorted to node state

--- src/modules/nodes/tpopfreiwkontrzaehlFolder.js
+++ src/modules/nodes/tpopfreiwkontrzaehlFolder.js
@@ -6,21 +6,21 @@ export default (store) => {
   // fetch sorting indexes of parents
   const projId = activeUrlElements.projekt
   if (!projId) return []
-  const projIndex = findIndex(table.filteredAndSorted.projekt, { ProjId: projId })
+  const projIndex = findIndex(node.filteredAndSorted.projekt, { ProjId: projId })
   const apArtId = activeUrlElements.ap
   if (!apArtId) return []
-  const apIndex = findIndex(table.filteredAndSorted.ap, { ApArtId: apArtId })
+  const apIndex = findIndex(node.filteredAndSorted.ap, { ApArtId: apArtId })
   const popId = activeUrlElements.pop
   if (!popId) return []
-  const popIndex = findIndex(table.filteredAndSorted.pop, { PopId: popId })
+  const popIndex = findIndex(node.filteredAndSorted.pop, { PopId: popId })
   const tpopId = activeUrlElements.tpop
   if (!tpopId) return []
-  const tpopIndex = findIndex(table.filteredAndSorted.tpop, { TPopId: tpopId })
+  const tpopIndex = findIndex(node.filteredAndSorted.tpop, { TPopId: tpopId })
   const tpopfreiwkontrId = activeUrlElements.tpopfreiwkontr
   if (!tpopfreiwkontrId) return []
-  const tpopfreiwkontrIndex = findIndex(table.filteredAndSorted.tpopfreiwkontr, { TPopKontrId: tpopfreiwkontrId })
+  const tpopfreiwkontrIndex = findIndex(node.filteredAndSorted.tpopfreiwkontr, { TPopKontrId: tpopfreiwkontrId })
 
-  const childrenLength = table.filteredAndSorted.tpopfreiwkontrzaehl.length
+  const childrenLength = node.filteredAndSorted.tpopfreiwkontrzaehl.length
 
   let message = childrenLength
   if (table.tpopkontrLoading) {
