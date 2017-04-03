fa395b92a9acb6aa2b0da6a6b96b2ce3addfd775
Alexander Gabriel <alex@gabriel-software.ch>
2017-04-03 14:35:58 +0200

    prepare tree2

--- src/store/extend/tree/filteredAndSorted/idealbiotop.js
+++
@@ -1,9 +0,0 @@
-export default (store) => {
-  const { table, tree } = store
-  const { activeNodes } = tree
-  // grab assozart as array and sort them by year
-  let idealbiotop = Array.from(table.idealbiotop.values())
-  // show only nodes of active ap
-  idealbiotop = idealbiotop.filter(a => a.IbApArtId === activeNodes.ap)
-  return idealbiotop
-}
