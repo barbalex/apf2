4a97ddeff94f954d7cf61cba43805bfc3128b7c5
Alexander Gabriel <alex@gabriel-software.ch>
2017-04-03 14:35:44 +0200

    prepare tree2

---
+++ src/store/compute/filteredAndSorted/zielber.js
@@ -0,0 +1,22 @@
+import sortBy from 'lodash/sortBy'
+
+export default (store, tree) => {
+  const { table } = store
+  const { activeNodes, nodeLabelFilter } = tree
+  // grab zielbere as array and sort them by year
+  let zielbere = Array.from(table.zielber.values())
+  zielbere = zielbere.filter(a => a.ZielId === activeNodes.ziel)
+  // map through all and create array of nodes
+  zielbere.forEach((el) => {
+    el.label = `${el.ZielBerJahr || `(kein Jahr)`}: ${el.ZielBerErreichung || `(keine Entwicklung)`}`
+  })
+  // filter by nodeLabelFilter
+  const filterString = nodeLabelFilter.get(`zielber`)
+  if (filterString) {
+    zielbere = zielbere.filter(p =>
+      p.label.toLowerCase().includes(filterString.toLowerCase())
+    )
+  }
+  // sort by label and return
+  return sortBy(zielbere, `label`)
+}
