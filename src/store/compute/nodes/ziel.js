cf0375107b891d62697850f778504aa1e72bc10b
Alexander Gabriel <alex@gabriel-software.ch>
2017-04-03 12:02:35 +0200

    move activeNodes and activeNodeArray state to tree

--- src/store/compute/nodes/ziel.js
+++ src/store/compute/nodes/ziel.js
@@ -1,7 +1,8 @@
 import findIndex from 'lodash/findIndex'
 
 export default (store, jahr) => {
-  const { activeNodes, tree } = store
+  const { tree } = store
+  const { activeNodes } = tree
   // fetch sorting indexes of parents
   const projId = activeNodes.projekt
   if (!projId) return []
--- src/store/compute/nodes/zielber.js
+++ src/store/compute/nodes/zielber.js
@@ -1,7 +1,8 @@
 import findIndex from 'lodash/findIndex'
 
 export default (store, zielId) => {
-  const { activeNodes, tree } = store
+  const { tree } = store
+  const { activeNodes } = tree
   // fetch sorting indexes of parents
   const projId = activeNodes.projekt
   if (!projId) return []
