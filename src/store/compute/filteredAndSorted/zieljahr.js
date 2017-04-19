bb0eab4308a6dea0f17ee9cc2816cea9aacec2e3
Alexander Gabriel <alex@gabriel-software.ch>
2017-04-19 15:33:05 +0200

    work on refactoring tree

--- src/store/compute/filteredAndSorted/zieljahr.js
+++ src/store/compute/filteredAndSorted/zieljahr.js
@@ -2,16 +2,15 @@ import uniq from 'lodash/uniq'
 
 export default (store, tree) => {
   const { table } = store
-  const { activeNodes, nodeLabelFilter } = tree
+  const { nodeLabelFilter } = tree
   // grab ziele as array
   let ziele = Array.from(table.ziel.values())
-  // show only nodes of active ap
-  ziele = ziele.filter(a => a.ApArtId === activeNodes.ap)
+
   // filter by nodeLabelFilter
   const filterString = nodeLabelFilter.get(`ziel`)
   const zieltypWerte = Array.from(table.ziel_typ_werte.values())
   if (filterString) {
-    ziele = ziele.filter((p) => {
+    ziele = ziele.filter(p => {
       const zielWert = zieltypWerte.find(e => e.ZieltypId === p.ZielTyp)
       const zieltypTxt = zielWert ? zielWert.ZieltypTxt : `kein Zieltyp`
       const label = `${p.ZielBezeichnung || `(kein Ziel)`} (${zieltypTxt})`
--- src/store/compute/nodes/allNodes.js
+++ src/store/compute/nodes/allNodes.js
@@ -4,6 +4,12 @@
 // to be sorted before those that have
 // that is why there is if (a !== 0 && !a)
 
+import projektNodes from '../../../modules/nodes/projekt'
+import apFolderNodes from '../../../modules/nodes/apFolder'
+import apberuebersichtFolderNodes
+  from '../../../modules/nodes/apberuebersichtFolder'
+import apberuebersichtNodes from '../../../modules/nodes/apberuebersicht'
+
 const compare = (a, b) => {
   // sort a before, if it has no value at this index
   if (a !== 0 && !a) return -1
@@ -14,183 +20,20 @@ const compare = (a, b) => {
 }
 
 export default (store, tree) => {
-  const { table } = store
-  const {
-    projekt,
-    apFolder,
-    apberuebersichtFolder,
-    apberuebersicht,
-    ap,
-    qkFolder,
-    assozartFolder,
-    assozart,
-    idealbiotopFolder,
-    beobNichtZuzuordnenFolder,
-    beobNichtZuzuordnen,
-    beobzuordnungFolder,
-    beobzuordnung,
-    berFolder,
-    ber,
-    apberFolder,
-    apber,
-    erfkritFolder,
-    erfkrit,
-    zieljahrFolder,
-    zieljahr,
-    ziel,
-    zielberFolder,
-    zielber,
-    popFolder,
-    pop,
-    popmassnberFolder,
-    popmassnber,
-    popberFolder,
-    popber,
-    tpopFolder,
-    tpop,
-    tpopbeobFolder,
-    tpopbeob,
-    tpopberFolder,
-    tpopber,
-    tpopfreiwkontrFolder,
-    tpopfreiwkontr,
-    tpopfreiwkontrzaehlFolder,
-    tpopfreiwkontrzaehl,
-    tpopfeldkontrFolder,
-    tpopfeldkontr,
-    tpopfeldkontrzaehlFolder,
-    tpopfeldkontrzaehl,
-    tpopmassnberFolder,
-    tpopmassnber,
-    tpopmassnFolder,
-    tpopmassn,
-  } = tree.node
-  const { activeNodes } = tree
+  const { openNodes } = tree
 
-  let nodes = projekt
+  let nodes = projektNodes(store, tree)
 
-  if (activeNodes.projekt) {
-    nodes = nodes.concat(apFolder)
-    nodes = nodes.concat(apberuebersichtFolder)
-    if (activeNodes.apberuebersichtFolder) {
-      nodes = nodes.concat(apberuebersicht)
+  openNodes.forEach(openNode => {
+    const projId = openNode.length > 1 ? openNode[1] : null
+    if (openNode.length === 2) {
+      nodes = [...nodes, ...apFolderNodes(store, tree, projId)]
+      nodes = [...nodes, ...apberuebersichtFolderNodes(store, tree, projId)]
     }
-    if (activeNodes.apFolder) {
-      nodes = nodes.concat(ap)
-      if (
-        activeNodes.ap &&
-        // make sure a pops lower levels are not shown
-        // if an apFilter exists
-        // and the species is not ap species
-        (
-          !tree.apFilter ||
-          (
-            tree.apFilter &&
-            table &&
-            table.ap &&
-            table.ap.get(activeNodes.ap) &&
-            [1, 2, 3].includes(table.ap.get(activeNodes.ap).ApStatus)
-          )
-        )
-      ) {
-        nodes = nodes.concat(qkFolder)
-        nodes = nodes.concat(assozartFolder)
-        if (activeNodes.assozartFolder) {
-          nodes = nodes.concat(assozart)
-        }
-        nodes = nodes.concat(idealbiotopFolder)
-        nodes = nodes.concat(beobNichtZuzuordnenFolder)
-        if (activeNodes.beobNichtZuzuordnenFolder) {
-          nodes = nodes.concat(beobNichtZuzuordnen)
-        }
-        nodes = nodes.concat(beobzuordnungFolder)
-        if (activeNodes.beobzuordnungFolder) {
-          nodes = nodes.concat(beobzuordnung)
-        }
-        nodes = nodes.concat(berFolder)
-        if (activeNodes.berFolder) {
-          nodes = nodes.concat(ber)
-        }
-        nodes = nodes.concat(apberFolder)
-        if (activeNodes.apberFolder) {
-          nodes = nodes.concat(apber)
-        }
-        nodes = nodes.concat(erfkritFolder)
-        if (activeNodes.erfkritFolder) {
-          nodes = nodes.concat(erfkrit)
-        }
-        nodes = nodes.concat(zieljahrFolder)
-        if (activeNodes.zielFolder) {
-          nodes = nodes.concat(zieljahr)
-          if (activeNodes.zieljahr) {
-            nodes = nodes.concat(ziel)
-            if (activeNodes.ziel) {
-              nodes = nodes.concat(zielberFolder)
-              if (activeNodes.zielberFolder) {
-                nodes = nodes.concat(zielber)
-              }
-            }
-          }
-        }
-        nodes = nodes.concat(popFolder)
-        if (activeNodes.popFolder) {
-          nodes = nodes.concat(pop)
-          if (activeNodes.pop) {
-            nodes = nodes.concat(popmassnberFolder)
-            if (activeNodes.popmassnberFolder) {
-              nodes = nodes.concat(popmassnber)
-            }
-            nodes = nodes.concat(popberFolder)
-            if (activeNodes.popberFolder) {
-              nodes = nodes.concat(popber)
-            }
-            nodes = nodes.concat(tpopFolder)
-            if (activeNodes.tpopFolder) {
-              nodes = nodes.concat(tpop)
-              if (activeNodes.tpop) {
-                nodes = nodes.concat(tpopbeobFolder)
-                if (activeNodes.tpopbeobFolder) {
-                  nodes = nodes.concat(tpopbeob)
-                }
-                nodes = nodes.concat(tpopberFolder)
-                if (activeNodes.tpopberFolder) {
-                  nodes = nodes.concat(tpopber)
-                }
-                nodes = nodes.concat(tpopfreiwkontrFolder)
-                if (activeNodes.tpopfreiwkontrFolder) {
-                  nodes = nodes.concat(tpopfreiwkontr)
-                  if (activeNodes.tpopfreiwkontr) {
-                    nodes = nodes.concat(tpopfreiwkontrzaehlFolder)
-                    if (activeNodes.tpopfreiwkontrzaehlFolder) {
-                      nodes = nodes.concat(tpopfreiwkontrzaehl)
-                    }
-                  }
-                }
-                nodes = nodes.concat(tpopfeldkontrFolder)
-                if (activeNodes.tpopfeldkontrFolder) {
-                  nodes = nodes.concat(tpopfeldkontr)
-                  if (activeNodes.tpopfeldkontr) {
-                    nodes = nodes.concat(tpopfeldkontrzaehlFolder)
-                    if (activeNodes.tpopfeldkontrzaehlFolder) {
-                      nodes = nodes.concat(tpopfeldkontrzaehl)
-                    }
-                  }
-                }
-                nodes = nodes.concat(tpopmassnberFolder)
-                if (activeNodes.tpopmassnberFolder) {
-                  nodes = nodes.concat(tpopmassnber)
-                }
-                nodes = nodes.concat(tpopmassnFolder)
-                if (activeNodes.tpopmassnFolder) {
-                  nodes = nodes.concat(tpopmassn)
-                }
-              }
-            }
-          }
-        }
-      }
+    if (openNode.length === 3 && openNode[2] === 'AP-Berichte') {
+      nodes = [...nodes, ...apberuebersichtNodes(store, tree, projId)]
     }
-  }
+  })
 
   /**
    * As all nodes are now in one flat list,
@@ -208,17 +51,18 @@ export default (store, tree) => {
    * if both array elements at this index are same,
    * compare values at next index
    */
-  return nodes.sort((a, b) => (
-    compare(a.sort[0], b.sort[0]) ||
-    compare(a.sort[1], b.sort[1]) ||
-    compare(a.sort[2], b.sort[2]) ||
-    compare(a.sort[3], b.sort[3]) ||
-    compare(a.sort[4], b.sort[4]) ||
-    compare(a.sort[5], b.sort[5]) ||
-    compare(a.sort[6], b.sort[6]) ||
-    compare(a.sort[7], b.sort[7]) ||
-    compare(a.sort[8], b.sort[8]) ||
-    compare(a.sort[9], b.sort[9]) ||
-    compare(a.sort[10], b.sort[10])
-  ))
+  return nodes.sort(
+    (a, b) =>
+      compare(a.sort[0], b.sort[0]) ||
+      compare(a.sort[1], b.sort[1]) ||
+      compare(a.sort[2], b.sort[2]) ||
+      compare(a.sort[3], b.sort[3]) ||
+      compare(a.sort[4], b.sort[4]) ||
+      compare(a.sort[5], b.sort[5]) ||
+      compare(a.sort[6], b.sort[6]) ||
+      compare(a.sort[7], b.sort[7]) ||
+      compare(a.sort[8], b.sort[8]) ||
+      compare(a.sort[9], b.sort[9]) ||
+      compare(a.sort[10], b.sort[10])
+  )
 }
