952fc743567f4ccc640a2a6aa7707fe0b666d372
Alexander Gabriel <alex@gabriel-software.ch>
2017-04-20 17:59:53 +0200

    enhance flow

--- src/modules/apiBaseUrl.js
+++ src/modules/apiBaseUrl.js
@@ -3,6 +3,8 @@
 
 const hostnameWithoutWww = window.location.hostname.replace('www.', '')
 const isLocalhost = hostnameWithoutWww === 'localhost'
-const apiHost = isLocalhost ? `http://localhost:4001` : `https://${window.location.hostname}/api`
+const apiHost = isLocalhost
+  ? `http://localhost:4001`
+  : `https://${window.location.hostname}/api`
 
 export default apiHost
--- src/modules/beobIdsFromServerInsideFeatureCollection.js
+++ src/modules/beobIdsFromServerInsideFeatureCollection.js
@@ -3,12 +3,15 @@ import within from '@turf/within'
 
 import epsg21781to4326 from './epsg21781to4326notReverse'
 
-export default (store: Object, beobs:Array<Object>) => {
+export default (
+  store: Object,
+  beobs: Array<Object>
+): Array<string | number> => {
   /**
    * data is passed from view fetched from the server
    * so need to filter to data with coordinates first...
    */
-  const beobsToUse = beobs.filter((b) => {
+  const beobsToUse = beobs.filter(b => {
     if (!b.BeobId) return false
     if (b.X && b.Y) return true
     return false
@@ -16,15 +19,15 @@ export default (store: Object, beobs:Array<Object>) => {
   const points = {
     type: `FeatureCollection`,
     // build an array of geoJson points
-    features: beobsToUse.map((b) => ({
+    features: beobsToUse.map(b => ({
       type: `Feature`,
       properties: {
-        BeobId: b.BeobId,
+        BeobId: b.BeobId
       },
       geometry: {
         type: `Point`,
-        coordinates: epsg21781to4326(b.X, b.Y),
-      },
+        coordinates: epsg21781to4326(b.X, b.Y)
+      }
     }))
   }
 
