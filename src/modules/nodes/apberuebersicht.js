9d2622bb934da4bc8c11df376d0b55ec8f422eb1
Alexander Gabriel <alex@gabriel-software.ch>
2017-04-20 18:00:01 +0200

    enhance flow

--- src/modules/nodes/apberuebersicht.js
+++ src/modules/nodes/apberuebersicht.js
@@ -2,11 +2,6 @@
 import findIndex from 'lodash/findIndex'
 
 export default (store: Object, tree: Object, projId: number): Array<Object> => {
-  // check passed variables
-  if (!store) return store.listError(new Error('no store passed'))
-  if (!tree) return store.listError(new Error('no tree passed'))
-  if (!projId) return store.listError(new Error('no projId passed'))
-
   // fetch sorting indexes of parents
   const projIndex = findIndex(tree.filteredAndSorted.projekt, {
     ProjId: projId
