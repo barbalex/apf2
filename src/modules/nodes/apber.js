e1cd2d74c5b1536df06b657561d43ae5b080d88d
Alexander Gabriel <alex@gabriel-software.ch>
2017-04-20 11:05:08 +0200

    add apberNodes

---
+++ src/modules/nodes/apber.js
@@ -0,0 +1,30 @@
+import findIndex from 'lodash/findIndex'
+
+export default (store, tree, projId, apArtId) => {
+  // check passed variables
+  if (!store) return store.listError(new Error('no store passed'))
+  if (!tree) return store.listError(new Error('no tree passed'))
+  if (!apArtId) return store.listError(new Error('no apArtId passed'))
+  if (!projId) return store.listError(new Error('no projId passed'))
+
+  // fetch sorting indexes of parents
+  const projIndex = findIndex(tree.filteredAndSorted.projekt, {
+    ProjId: projId
+  })
+  const apIndex = findIndex(tree.filteredAndSorted.ap, { ApArtId: apArtId })
+
+  // map through all projekt and create array of nodes
+  return tree.filteredAndSorted.apber
+    .filter(p => p.ApArtId === apArtId)
+    .map((el, index) => ({
+      nodeType: `table`,
+      menuType: `apber`,
+      id: el.JBerId,
+      parentId: el.ApArtId,
+      urlLabel: el.JBerId,
+      label: el.label,
+      url: [`Projekte`, projId, `Arten`, el.ApArtId, `AP-Berichte`, el.JBerId],
+      sort: [projIndex, 1, apIndex, 4, index],
+      hasChildren: false
+    }))
+}
--- src/store/compute/nodes/allNodes.js
+++ src/store/compute/nodes/allNodes.js
@@ -34,6 +34,7 @@ import beobNichtZuzuordnenNodes
 import beobzuordnungNodes from '../../../modules/nodes/beobzuordnung'
 import assozartNodes from '../../../modules/nodes/assozart'
 import berNodes from '../../../modules/nodes/ber'
+import apberNodes from '../../../modules/nodes/apber'
 
 const compare = (a, b) => {
   // sort a before, if it has no value at this index
@@ -182,6 +183,14 @@ export default (store, tree) => {
     ) {
       const apArtId = node[3]
       nodes = [...nodes, ...berNodes(store, tree, projId, apArtId)]
+    }
+    if (
+      node.length === 5 &&
+      node[4] === 'AP-Berichte' &&
+      isNodeOpen(openNodes, node.slice(0, node.length - 1))
+    ) {
+      const apArtId = node[3]
+      nodes = [...nodes, ...apberNodes(store, tree, projId, apArtId)]
     }
   })
 
