a527da327c86f62576b49984d6c9dec92ac9363d
Alexander Gabriel <alex@gabriel-software.ch>
2017-04-20 18:01:28 +0200

    enhance flow

--- src/modules/nodes/zielber.js
+++ src/modules/nodes/zielber.js
@@ -9,14 +9,6 @@ export default (
   zieljahr: number,
   zielId: number
 ): Array<Object> => {
-  // check passed variables
-  if (!store) return store.listError(new Error('no store passed'))
-  if (!tree) return store.listError(new Error('no tree passed'))
-  if (!apArtId) return store.listError(new Error('no apArtId passed'))
-  if (!projId) return store.listError(new Error('no projId passed'))
-  if (!zieljahr) return store.listError(new Error('no zieljahr passed'))
-  if (!zielId) return store.listError(new Error('no zielId passed'))
-
   // fetch sorting indexes of parents
   const projIndex = findIndex(tree.filteredAndSorted.projekt, {
     ProjId: projId
