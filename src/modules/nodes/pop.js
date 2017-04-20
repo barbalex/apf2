13f0c772ea190504d3a4b124b271c289cb94a72d
Alexander Gabriel <alex@gabriel-software.ch>
2017-04-20 12:35:53 +0200

    filter correctly

--- src/modules/nodes/pop.js
+++ src/modules/nodes/pop.js
@@ -17,7 +17,10 @@ export default (
   const projIndex = findIndex(tree.filteredAndSorted.projekt, {
     ProjId: projId
   })
-  const apIndex = findIndex(tree.filteredAndSorted.ap, { ApArtId: apArtId })
+  const apIndex = findIndex(
+    tree.filteredAndSorted.ap.filter(a => a.ProjId === projId),
+    { ApArtId: apArtId }
+  )
 
   // map through all pop and create array of nodes
   return tree.filteredAndSorted.pop
