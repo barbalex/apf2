70ed180196e436440f8eaaf239cb3dcae2b02d93
Alexander Gabriel <alex@gabriel-software.ch>
2017-04-20 18:37:45 +0200

    optimize

--- src/store/action/toggleNode.js
+++ src/store/action/toggleNode.js
@@ -1,20 +1,25 @@
 // @flow
 import clone from 'lodash/clone'
-import union from 'lodash/union'
 import { toJS } from 'mobx'
 
 import isNodeOpen from '../../modules/isNodeOpen'
+import isNodeInActiveNodePath from '../../modules/isNodeInActiveNodePath'
 
 export default (store: Object, tree: Object, node: Object): any => {
   if (!node.url) return store.listError(new Error('passed node has no url'))
 
   const newActiveNodeArray = clone(node.url)
-  if (isNodeOpen(toJS(tree.openNodes), node.url)) {
+  if (
+    isNodeOpen(toJS(tree.openNodes), node.url) &&
+    isNodeInActiveNodePath(node, tree.activeNodeArray)
+  ) {
     // shorten activeNodeArray
     // but don't close node
     newActiveNodeArray.pop()
   } else {
-    tree.openNodes = union([...tree.openNodes, node.url])
+    if (!isNodeOpen(toJS(tree.openNodes), node.url)) {
+      tree.openNodes.push(node.url)
+    }
   }
   tree.setActiveNodeArray(newActiveNodeArray)
 }
