0e82dc1ec1919cc3a1b0af13e27285b38f622634
Alexander Gabriel <alex@gabriel-software.ch>
2017-04-20 18:02:27 +0200

    enhance flow

--- src/store/compute/filteredAndSorted/idealbiotop.js
+++ src/store/compute/filteredAndSorted/idealbiotop.js
@@ -1,4 +1,4 @@
-export default (store, tree) => {
+export default (store: Object, tree: Object): Object => {
   const { table } = store
   // grab assozart as array and sort them by year
   let idealbiotop = Array.from(table.idealbiotop.values())
