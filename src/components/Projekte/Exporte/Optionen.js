e92b6dd61559575db95998908b56c9ae5526f089
Alexander Gabriel <alex@gabriel-software.ch>
2017-04-21 18:35:07 +0200

    format

--- src/components/Projekte/Exporte/Optionen.js
+++ src/components/Projekte/Exporte/Optionen.js
@@ -9,58 +9,41 @@ const FirstLevelCard = styled(Card)`
   margin-bottom: 10px;
 `
 
-const enhance = compose(
-  inject(`store`),
-  observer
-)
+const enhance = compose(inject(`store`), observer)
 
-const Optionen = (
-  { store }:
-  { store: Object }
-) =>
+const Optionen = ({ store }: { store: Object }) => (
   <FirstLevelCard initiallyExpanded>
-    <CardHeader
-      title="Optionen"
-      actAsExpander
-      showExpandableButton
-    />
-    <CardText
-      expandable
-    >
+    <CardHeader title="Optionen" actAsExpander showExpandableButton />
+    <CardText expandable>
       <Checkbox
         label={
-          (
-            store.map.mapFilter.filter.features.length > 0 ?
-            `Karten-Filter anwenden` :
-            `Karten-Filter anwenden (verfügbar, wenn ein Karten-Filter erstellt wurde)`
-          )
+          store.map.mapFilter.filter.features.length > 0
+            ? `Karten-Filter anwenden`
+            : `Karten-Filter anwenden (verfügbar, wenn ein Karten-Filter erstellt wurde)`
         }
         value={store.export.applyMapFilterToExport}
         checked={store.export.applyMapFilterToExport}
         onCheck={store.export.toggleApplyMapFilterToExport}
         disabled={!(store.map.mapFilter.filter.features.length > 0)}
       />
-      {
-        false &&
+      {false &&
         <Checkbox
           label={`Strukturbaum-Filter anwenden`}
           value={store.export.applyNodeLabelFilterToExport}
           checked={store.export.applyNodeLabelFilterToExport}
           onCheck={store.export.toggleApplyNodeLabelFilterToExport}
           disabled
-        />
-      }
-      {
-        false &&
+        />}
+      {false &&
         <Checkbox
           label={`Nach den aktuell im Strukturbaum gewählten Elementen filtern`}
           value={store.export.applyActiveNodeFilterToExport}
           checked={store.export.applyActiveNodeFilterToExport}
           onCheck={store.export.toggleApplyActiveNodeFilterToExport}
           disabled
-        />
-      }
+        />}
     </CardText>
   </FirstLevelCard>
+)
 
 export default enhance(Optionen)
