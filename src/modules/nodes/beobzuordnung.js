08957432fc4cdcec4df9b1988f47f0cf3ff02f1a
Alexander Gabriel <alex@gabriel-software.ch>
2017-05-15 15:58:30 +0200

    style

--- src/modules/nodes/beobzuordnung.js
+++ src/modules/nodes/beobzuordnung.js
@@ -5,35 +5,35 @@ export default (
   store: Object,
   tree: Object,
   projId: number,
-  apArtId: number
+  apArtId: number,
 ): Array<Object> => {
   const { filteredAndSorted } = tree
   // fetch sorting indexes of parents
   const projIndex = findIndex(filteredAndSorted.projekt, { ProjId: projId })
   const apIndex = findIndex(
     filteredAndSorted.ap.filter(a => a.ProjId === projId),
-    { ApArtId: apArtId }
+    { ApArtId: apArtId },
   )
 
   // map through all and create array of nodes
   return filteredAndSorted.beobzuordnung
     .filter(b => b.ArtId === apArtId)
     .map((el, index) => ({
-      nodeType: `table`,
-      menuType: `beobzuordnung`,
+      nodeType: 'table',
+      menuType: 'beobzuordnung',
       id: el.id,
       parentId: apArtId,
       urlLabel: el.id,
       label: el.label,
       url: [
-        `Projekte`,
+        'Projekte',
         projId,
-        `Arten`,
+        'Arten',
         apArtId,
-        `nicht-beurteilte-Beobachtungen`,
-        el.id
+        'nicht-beurteilte-Beobachtungen',
+        el.id,
       ],
       sort: [projIndex, 1, apIndex, 8, index],
-      hasChildren: false
+      hasChildren: false,
     }))
 }
--- src/modules/nodes/ber.js
+++ src/modules/nodes/ber.js
@@ -5,29 +5,29 @@ export default (
   store: Object,
   tree: Object,
   projId: number,
-  apArtId: number
+  apArtId: number,
 ): Array<Object> => {
   // fetch sorting indexes of parents
   const projIndex = findIndex(tree.filteredAndSorted.projekt, {
-    ProjId: projId
+    ProjId: projId,
   })
   const apIndex = findIndex(
     tree.filteredAndSorted.ap.filter(a => a.ProjId === projId),
-    { ApArtId: apArtId }
+    { ApArtId: apArtId },
   )
 
   // map through all projekt and create array of nodes
   return tree.filteredAndSorted.ber
     .filter(p => p.ApArtId === apArtId)
     .map((el, index) => ({
-      nodeType: `table`,
-      menuType: `ber`,
+      nodeType: 'table',
+      menuType: 'ber',
       id: el.BerId,
       parentId: el.ApArtId,
       urlLabel: el.BerId,
       label: el.label,
-      url: [`Projekte`, projId, `Arten`, el.ApArtId, `Berichte`, el.BerId],
+      url: ['Projekte', projId, 'Arten', el.ApArtId, 'Berichte', el.BerId],
       sort: [projIndex, 1, apIndex, 5, index],
-      hasChildren: false
+      hasChildren: false,
     }))
 }
