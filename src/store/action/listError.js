e19ef120c609f008f916a531e004428508f09018
Alexander Gabriel <alex@gabriel-software.ch>
2017-05-17 18:14:23 +0200

    correct idb error

--- src/store/action/listError.js
+++ src/store/action/listError.js
@@ -1,5 +1,15 @@
 // @flow
+
 export default (store: Object, error: Object): void => {
+  // reinitialize db if db has problem
+  // happens after resfactoring db structure
+  if (
+    error.message.includes('IDBDatabase') &&
+    error.message.includes('One of the specified object stores was not found')
+  ) {
+    window.indexedDB.deleteDatabase('apflora')
+    window.open(window.location.href, '_self')
+  }
   store.app.errors.unshift(error)
   setTimeout(() => {
     store.app.errors.pop()
