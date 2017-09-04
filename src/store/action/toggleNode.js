92db4b035cd903a49a9cb43f33881f73f40ae3d2
Alexander Gabriel <alex@gabriel-software.ch>
2017-09-04 11:29:00 +0200

    optimize tree clicking behaviour

--- src/store/action/toggleNode.js
+++ src/store/action/toggleNode.js
@@ -6,32 +6,30 @@ import isNodeOpen from '../../modules/isNodeOpen'
 import isNodeInActiveNodePath from '../../modules/isNodeInActiveNodePath'
 
 export default (store: Object, tree: Object, node: Object): any => {
-  if (!node.url) return store.listError(new Error('passed node has no url'))
+  if (!node.url) {
+    return store.listError(new Error('passed node has no url'))
+  }
 
   const newActiveNodeArray = clone(node.url)
   const nodeIsOpen = isNodeOpen(toJS(tree.openNodes), node.url)
-  if (
-    nodeIsOpen &&
-    isNodeInActiveNodePath(node, tree.activeNodeArray) &&
+  if (nodeIsOpen && isNodeInActiveNodePath(node, tree.activeNodeArray)) {
     // need to check if node is last in activeNodePath
-    node.url.length === tree.activeNodeArray.length
-  ) {
-    // shorten activeNodeArray
-    // but don't close node
-    newActiveNodeArray.pop()
-  } else if (nodeIsOpen && isNodeInActiveNodePath(node, tree.activeNodeArray)) {
-    // leave newActiveNodeArray as it is
-  } else {
-    if (!nodeIsOpen) {
-      tree.openNodes.push(node.url)
-      // automatically open zaehlFolder of tpopfeldkontr or tpopfreiwkontr
-      if (['tpopfeldkontr', 'tpopfreiwkontr'].includes(node.menuType)) {
-        tree.openNodes.push([...node.url, 'Zaehlungen'])
-      }
-      // automatically open zielberFolder of ziel
-      if (node.menuType === 'ziel') {
-        tree.openNodes.push([...node.url, 'Berichte'])
-      }
+    if (node.url.length === tree.activeNodeArray.length) {
+      // shorten activeNodeArray
+      // but don't close node
+      newActiveNodeArray.pop()
+    } else {
+      // leave newActiveNodeArray as it is
+    }
+  } else if (!nodeIsOpen) {
+    tree.openNodes.push(node.url)
+    // automatically open zaehlFolder of tpopfeldkontr or tpopfreiwkontr
+    if (['tpopfeldkontr', 'tpopfreiwkontr'].includes(node.menuType)) {
+      tree.openNodes.push([...node.url, 'Zaehlungen'])
+    }
+    // automatically open zielberFolder of ziel
+    if (node.menuType === 'ziel') {
+      tree.openNodes.push([...node.url, 'Berichte'])
     }
   }
   tree.setLastClickedNode(node.url)
