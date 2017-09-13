7ac3333f6f12e2c059552886fd19a18f72889636
Alexander Gabriel <alex@gabriel-software.ch>
2017-09-13 11:39:40 +0200

    optimize

--- src/store/action/fetchKtZh.js
+++ src/store/action/fetchKtZh.js
@@ -2,13 +2,11 @@
 import axios from 'axios'
 
 export default (store: Object): void => {
-  if (!store.app.ktZh) {
-    const url = '/geojson/ktZh.json'
-    axios
-      .get(url)
-      .then(({ data }) => {
-        store.app.ktZh = data
-      })
-      .catch(error => store.listError(error))
-  }
+  if (store.app.ktZh) return
+  axios
+    .get('/geojson/ktZh.json')
+    .then(({ data }) => {
+      store.app.ktZh = data
+    })
+    .catch(error => store.listError(error))
 }
