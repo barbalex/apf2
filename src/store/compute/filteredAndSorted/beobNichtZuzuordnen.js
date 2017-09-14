385773de8b68145de4264249c693fc15ca95f5b7
Alexander Gabriel <alex@gabriel-software.ch>
2017-09-14 23:35:45 +0200

    correct error

--- src/store/compute/filteredAndSorted/beobNichtZuzuordnen.js
+++ src/store/compute/filteredAndSorted/beobNichtZuzuordnen.js
@@ -13,7 +13,7 @@ export default (store: Object, tree: Object): Array<Object> => {
         return true
       }
       let ap
-      const beob = table.beob.get(b.id)
+      const beob = table.beob.get(b.BeobId)
       if (beob) {
         ap = table.ap.get(beob.ArtId)
       }
@@ -38,13 +38,14 @@ export default (store: Object, tree: Object): Array<Object> => {
     }
     const quelle = table.beob_quelle.get(el.QuelleId)
     const quelleName = quelle && quelle.name ? quelle.name : ''
-    el.label = `${datum || '(kein Datum)'}: ${autor || '(kein Autor)'} (${quelleName})`
+    el.label = `${datum || '(kein Datum)'}: ${autor ||
+      '(kein Autor)'} (${quelleName})`
   })
   // filter by nodeLabelFilter
   const filterString = nodeLabelFilter.get('beobNichtZuzuordnen')
   if (filterString) {
     beobNichtZuzuordnen = beobNichtZuzuordnen.filter(p =>
-      p.label.toLowerCase().includes(filterString.toLowerCase()),
+      p.label.toLowerCase().includes(filterString.toLowerCase())
     )
   }
   // sort by label
