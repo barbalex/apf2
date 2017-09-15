c4ace07fa97d16a26c1ef3defa471dd990e7ea16
Alexander Gabriel <alex@gabriel-software.ch>
2017-09-15 02:20:19 +0200

    remove v2

--- src/store/action/getActiveNodeArrayFromPathname.js
+++ src/store/action/getActiveNodeArrayFromPathname.js
@@ -2,10 +2,6 @@
 export default (): Array<number | string> => {
   const pathName = window.location.pathname.replace('/', '')
   const pathElements = pathName.split('/')
-  if (pathElements[0] === 'v2') {
-    // get rid of empty element(s) at start
-    pathElements.shift()
-  }
   if (pathElements[0] === '') {
     // get rid of empty element(s) at start
     pathElements.shift()
