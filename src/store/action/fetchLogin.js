8148a83abeb930e72ac919bb68074f7edcc3a4e5
Alexander Gabriel <alex@gabriel-software.ch>
2017-09-15 11:44:19 +0200

    prevent login error

--- src/store/action/fetchLogin.js
+++ src/store/action/fetchLogin.js
@@ -16,14 +16,22 @@ export default (store: Object, name: string, password: string): any => {
   store.loading.push('user')
   axios
     .get(url)
-    .then(({ data }) => {
+    .then(({ data, status, statusText }) => {
+      //console.log('status:', status)
+      //console.log('statusText:', statusText)
+      //console.log('data:', data)
       if (data && data.length > 0) {
+        //console.log('data.length:', data.length)
+        //console.log('data.length > 0:', data.length > 0)
         const readOnly = data[0].NurLesen === -1
         store.user.readOnly = readOnly
         store.user.name = name
         app.db.currentUser.clear()
         app.db.currentUser.put({ name, readOnly })
-      } else {
+      } else if (status !== 200) {
+        // somehow fetchLogin sometimes gets called 3 times consecutively
+        // and the second time data is an empty array
+        // but: status is 200
         store.listError(new Error('Anmeldung gescheitert'))
       }
       store.loading = store.loading.filter(el => el !== 'user')
