711845e4747f16303864c716a861b6af619670fb
Alexander Gabriel <alex@gabriel-software.ch>
2017-09-25 19:09:47 +0200

    change setting

--- src/components/Projekte/Karte/FullScreenControl.js
+++ src/components/Projekte/Karte/FullScreenControl.js
@@ -18,7 +18,7 @@ class FullScreenControl extends Component {
       titleCancel: 'Karte verkleinern', // change the title of the button when fullscreen is on, default Exit Full Screen
       content: null, // change the content of the button, can be HTML, default null
       forceSeparateButton: false, // force seperate button to detach from zoom buttons, default false
-      forcePseudoFullscreen: true, // force use of pseudo full screen even if full screen API is available, default false
+      forcePseudoFullscreen: false, // force use of pseudo full screen even if full screen API is available, default false
       fullscreenElement: false, // Dom element to render in full screen, false by default, fallback to map._container
     }
     const fullscreenControl = window.L.control.fullscreen(options)
