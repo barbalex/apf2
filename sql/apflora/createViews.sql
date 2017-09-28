413850635bc918349c3ca453764519dd1be37246
Alexander Gabriel <alex@gabriel-software.ch>
2017-09-28 15:09:51 +0200

    correct error

--- sql/apflora/createViews.sql
+++ sql/apflora/createViews.sql
@@ -6107,55 +6107,7 @@ GROUP BY
   apflora.tpopkontr."TPopKontrId"
 HAVING
   apflora.tpopkontr."TPopKontrJahr" IS NULL
-ORDER BY
-  apflora.ap."ApArtId",
-  apflora.pop."PopNr",
-  apflora.tpop."TPopNr",
-  apflora.tpopkontr."TPopKontrJahr",
-  apflora.tpopkontr."TPopKontrId";
-
-DROP VIEW IF EXISTS apflora.v_qk_freiwkontr_ohnejahr CASCADE;
-CREATE OR REPLACE VIEW apflora.v_qk_freiwkontr_ohnejahr AS
-SELECT
-  apflora.ap."ApArtId",
-  'Freiwilligen-Kontrolle ohne Jahr:'::text AS "hw",
-  concat(
-    '<a href="http://apflora.ch/index.html?ap=',
-    apflora.ap."ApArtId",
-    '&pop=',
-    apflora.pop."PopId",
-    '&tpop=',
-    apflora.tpop."TPopId",
-    '&tpopfreiwkontr=',
-    apflora.tpopkontr."TPopKontrId",
-    '" target="_blank">',
-    COALESCE(
-      concat('Pop: ', apflora.pop."PopNr"),
-      concat('Pop.-ID: ', apflora.pop."PopId")
-    ),
-    COALESCE(
-      concat(' > TPop: ', apflora.tpop."TPopNr"),
-      concat(' > TPop.-ID: ', apflora.tpop."TPopId")
-    ),
-    COALESCE(
-      concat(' > KontrJahr: ', apflora.tpopkontr."TPopKontrJahr"),
-      concat(' > Kontr.-ID: ', apflora.tpopkontr."TPopKontrId")
-    ),
-    '</a>'
-  ) AS "link"
-FROM
-  apflora.ap
-  INNER JOIN
-    (apflora.pop
-    INNER JOIN
-      (apflora.tpop
-      INNER JOIN
-        apflora.tpopkontr
-        ON apflora.tpop."TPopId" = apflora.tpopkontr."TPopId")
-      ON apflora.pop."PopId" = apflora.tpop."PopId")
-    ON apflora.ap."ApArtId" = apflora.pop."ApArtId"
-WHERE
-  apflora.tpopkontr."TPopKontrJahr" IS NULL
+  AND apflora.tpopkontr."TPopKontrTyp" <> 'Freiwilligen-Erfolgskontrolle'
 ORDER BY
   apflora.ap."ApArtId",
   apflora.pop."PopNr",
@@ -6188,6 +6140,7 @@ GROUP BY
   apflora.tpopkontr."TPopKontrId"
 HAVING
   apflora.tpopkontr."TPopKontrJahr" IS NULL
+  AND apflora.tpopkontr."TPopKontrTyp" = 'Freiwilligen-Erfolgskontrolle'
 ORDER BY
   apflora.ap."ApArtId",
   apflora.pop."PopNr",
