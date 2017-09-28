dac28ba123838ff0400e2e38db0ddec9b1b9a05e
Alexander Gabriel <alex@gabriel-software.ch>
2017-09-28 15:00:28 +0200

    remove unneeded

--- sql/apflora/createViews.sql
+++ sql/apflora/createViews.sql
@@ -5631,34 +5631,6 @@ ORDER BY
   apflora.ap."ApArtId",
   apflora.pop."PopName";
 
-DROP VIEW IF EXISTS apflora.v_qk_pop_ohnepopname CASCADE;
-CREATE OR REPLACE VIEW apflora.v_qk_pop_ohnepopname AS
-SELECT
-  apflora.ap."ApArtId" AS "ApArtId",
-  'Population ohne Name:'::text AS "hw",
-  concat(
-    '<a href="http://apflora.ch/index.html?ap=',
-    apflora.ap."ApArtId",
-    '&pop=',
-    apflora.pop."PopId",
-    '" target="_blank">',
-    COALESCE(
-      concat('Pop: ', apflora.pop."PopNr"),
-      concat('Pop: id=', apflora.pop."PopId")
-    ),
-    '</a>'
-  ) AS "link"
-FROM
-  apflora.ap
-  INNER JOIN
-    apflora.pop
-    ON apflora.ap."ApArtId" = apflora.pop."ApArtId"
-WHERE
-  apflora.pop."PopName" IS NULL
-ORDER BY
-  apflora.ap."ApArtId",
-  apflora.pop."PopNr";
-
 DROP VIEW IF EXISTS apflora.v_qk2_pop_ohnepopname CASCADE;
 CREATE OR REPLACE VIEW apflora.v_qk2_pop_ohnepopname AS
 SELECT
