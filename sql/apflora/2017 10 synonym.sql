61e3545ce3d0c925f587ec929270135942439575
Alexander Gabriel <alex@gabriel-software.ch>
2017-10-04 10:38:52 +0200

    add id to beob_art

--- sql/apflora/2017 10 synonym.sql
+++ sql/apflora/2017 10 synonym.sql
@@ -1,16 +1,19 @@
 -- create table
 DROP TABLE IF EXISTS apflora.beob_art;
 CREATE TABLE apflora.beob_art (
+  "BeobArtId" SERIAL PRIMARY KEY,
   "TaxonomieId" INTEGER DEFAULT NULL REFERENCES apflora.adb_eigenschaften ("TaxonomieId") ON DELETE SET NULL ON UPDATE CASCADE,
   "ApArtId" integer DEFAULT NULL REFERENCES apflora.ap ("ApArtId") ON DELETE CASCADE ON UPDATE CASCADE,
   "MutWann" date DEFAULT NULL,
   "MutWer" varchar(20) DEFAULT NULL
 );
+COMMENT ON COLUMN apflora.beob_art."BeobArtId" IS 'Primärschlüssel der Tabelle "beob_art"';
 COMMENT ON COLUMN apflora.beob_art."TaxonomieId" IS 'Zugehörige Art. Fremdschlüssel aus der Tabelle "adb_eigenschaften"';
 COMMENT ON COLUMN apflora.beob_art."ApArtId" IS 'Zugehöriger Aktionsplan. Fremdschlüssel aus der Tabelle "ap"';
 COMMENT ON COLUMN apflora.beob_art."MutWann" IS 'Wann wurde der Datensatz zuletzt geändert?';
 COMMENT ON COLUMN apflora.beob_art."MutWer" IS 'Wer hat den Datensatz zuletzt geändert?';
 CREATE INDEX ON apflora.beob_art USING btree ("ApArtId", "TaxonomieId");
+SELECT setval(pg_get_serial_sequence('apflora.beob_art', 'BeobArtId'), coalesce(max("BeobArtId"), 0) + 1, false) FROM apflora.beob_art;
 
 -- insert ap species
 insert into apflora.beob_art ("ApArtId", "TaxonomieId")
