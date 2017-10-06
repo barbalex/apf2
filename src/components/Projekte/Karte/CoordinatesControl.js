ba53458c5a027992e25ef16f0f76f3d65b401df9
Alexander Gabriel <alex@gabriel-software.ch>
2017-10-06 15:36:33 +0200

    work on to to coordinates

--- src/components/Projekte/Karte/CoordinatesControl.js
+++ src/components/Projekte/Karte/CoordinatesControl.js
@@ -4,36 +4,81 @@ import { inject, observer } from 'mobx-react'
 import Control from 'react-leaflet-control'
 import styled from 'styled-components'
 import compose from 'recompose/compose'
+import withHandlers from 'recompose/withHandlers'
+import withState from 'recompose/withState'
 
-const StyledControl = styled(Control)`
+/**
+ * TODO:
+ * onClick coordinates container: render coordinate-field-pair and go-to button
+ * onBlur coordinate-field-pair-container: render coordinates
+ * onBlur coordinate field: validate coordinates
+ * onClick go-to button:
+ * - do nothing if coordinates are invalid, else:
+ * - move to coordinates
+ * - render coordinates
+ */
+
+const CoordinatesControl = styled(Control)`
   margin-bottom: 2px !important;
   margin-right: 5px !important;
+  cursor: pointer !important;
 `
 const StyledDiv = styled.div`
   background-color: transparent;
   color: rgb(48, 48, 48);
   font-weight: 700;
-  text-shadow: 0 1px 0 white, -0 -1px 0 white, 1px 0 0 white, -1px 0 0 white, 0 2px 1px white, -0 -2px 1px white, 2px 0 1px white, -2px 0 1px white, 0 3px 2px white, -0 -3px 2px white, 3px 0 2px white, -3px 0 2px white;
+  text-shadow: 0 1px 0 white, -0 -1px 0 white, 1px 0 0 white, -1px 0 0 white,
+    0 2px 1px white, -0 -2px 1px white, 2px 0 1px white, -2px 0 1px white,
+    0 3px 2px white, -0 -3px 2px white, 3px 0 2px white, -3px 0 2px white;
+`
+const GotoControl = styled(Control)`
+  margin-bottom: 2px !important;
+  margin-right: 5px !important;
 `
 
-const enhance = compose(inject('store'), observer)
+const enhance = compose(
+  inject('store'),
+  withState('controlType', 'changeControlType', 'coordinates'),
+  withHandlers({
+    onClickCoordinates: props => () => {
+      console.log('coordinates clicked')
+      props.changeControlType('goto')
+    },
+    onClickGoto: props => () => {
+      console.log('goto clicked')
+    },
+  }),
+  observer
+)
 
-const CoordinatesControl = ({ store }: { store: Object }) => {
-  let [x, y] = store.map.mouseCoordEpsg21781
-  let coord = ''
-  if (x && y) {
-    x = parseInt(x, 10).toLocaleString('de-ch')
-    y = parseInt(y, 10).toLocaleString('de-ch')
-    coord = `${x}, ${y}`
+const MyControl = ({
+  store,
+  controlType,
+  onClickCoordinates,
+}: {
+  store: Object,
+  controlType: string,
+  onClickCoordinates: () => void,
+}) => {
+  if (controlType === 'coordinates') {
+    let [x, y] = store.map.mouseCoordEpsg21781
+    let coord = ''
+    if (x && y) {
+      x = parseInt(x, 10).toLocaleString('de-ch')
+      y = parseInt(y, 10).toLocaleString('de-ch')
+      coord = `${x}, ${y}`
+    }
+    return (
+      <CoordinatesControl position="bottomright">
+        <StyledDiv onClick={onClickCoordinates}>{coord}</StyledDiv>
+      </CoordinatesControl>
+    )
   }
-
   return (
-    <StyledControl position="bottomright">
-      <StyledDiv>
-        {coord}
-      </StyledDiv>
-    </StyledControl>
+    <GotoControl position="bottomright">
+      <StyledDiv>"go to"</StyledDiv>
+    </GotoControl>
   )
 }
 
-export default enhance(CoordinatesControl)
+export default enhance(MyControl)
