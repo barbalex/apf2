89c0b9407e39021c1e9f7e7f2618eddd9ce644e5
Alexander Gabriel <alex@gabriel-software.ch>
2017-10-11 21:22:48 +0200

    add maxZoom

--- src/components/Projekte/Karte/layers/BingAerial.js
+++ src/components/Projekte/Karte/layers/BingAerial.js
@@ -5,6 +5,8 @@ import { BingLayer } from 'react-leaflet-bing'
 const bingKey =
   'AjGOtB_ygBplpxXtKiiHtm-GERjSg9TFEoCmuBI_Yz4VWy0unRGUDo9GOZHA46Pf'
 
-const BingAerialLayer = () => <BingLayer bingkey={bingKey} />
+const BingAerialLayer = () => (
+  <BingLayer bingkey={bingKey} maxNativeZoom={18} minZoom={0} maxZoom={22} />
+)
 
 export default BingAerialLayer
