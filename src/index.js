7ee0502ec6e3b134f0a33abd13f6d2059cfd16b7
Alexander Gabriel <alex@gabriel-software.ch>
2017-10-13 12:21:19 +0200

    make sure auth is fetched from idb before first data request

--- src/index.js
+++ src/index.js
@@ -38,6 +38,7 @@ import Messages from './components/Messages'
 import DownloadMessages from './components/DownloadMessages'
 import getActiveNodeArrayFromPathname from './store/action/getActiveNodeArrayFromPathname'
 import getUrlQuery from './store/action/getUrlQuery'
+import setLoginFromIdb from './store/action/setLoginFromIdb'
 
 // service worker
 import registerServiceWorker from './registerServiceWorker'
@@ -47,74 +48,79 @@ import apiBaseUrl from './modules/apiBaseUrl'
 //import updateFromSocket from './modules/updateFromSocket'
 
 import './index.css'
+;(async () => {
+  try {
+    registerServiceWorker(store)
 
-registerServiceWorker(store)
+    // prevent changing values in number inputs when scrolling pages!
+    // see: http://stackoverflow.com/a/38589039/712005
+    // and: https://stackoverflow.com/a/42058469/712005
+    document.addEventListener('wheel', function(event) {
+      if (window.document.activeElement.type === 'number') {
+        event.preventDefault()
+      }
+    })
 
-// prevent changing values in number inputs when scrolling pages!
-// see: http://stackoverflow.com/a/38589039/712005
-// and: https://stackoverflow.com/a/42058469/712005
-document.addEventListener('wheel', function(event) {
-  if (window.document.activeElement.type === 'number') {
-    event.preventDefault()
+    app.extend({
+      init() {
+        this.db = initializeDb()
+        this.store = store
+      },
+    })
+    app.init()
+
+    const theme = Object.assign({}, uiTheme, {
+      appBar: {
+        height: 51,
+      },
+    })
+
+    // make store accessible in dev
+    window.app = app
+
+    axios.defaults.baseURL = apiBaseUrl
+
+    await setLoginFromIdb(store)
+
+    // turned off because of errors in production
+    // const socket = window.io(apiBaseUrl)
+    // socket.on('tabelle_update', payload => updateFromSocket(store, payload))
+
+    const AppContainer = styled.div`
+      display: flex;
+      flex-direction: column;
+    `
+
+    // initiate activeNodeArray
+    const activeNodeArrayFromUrl = getActiveNodeArrayFromPathname()
+    store.tree.setActiveNodeArray(activeNodeArrayFromUrl)
+    store.tree.setLastClickedNode(activeNodeArrayFromUrl)
+    // need to set openNodes
+    store.tree.setOpenNodesFromActiveNodeArray()
+    // clone tree2 in case tree2 is open
+    store.tree.cloneActiveNodeArrayToTree2()
+    const urlQuery = getUrlQuery(window.location.search)
+    store.setUrlQuery(urlQuery)
+
+    ReactDOM.render(
+      <Provider store={store}>
+        <MuiThemeProvider muiTheme={getMuiTheme(theme)}>
+          <AppContainer>
+            {false && <DevTools />}
+            <AppBar />
+            <Projekte />
+            <User />
+            <Deletions />
+            <Errors />
+            <UpdateAvailable />
+            <Messages />
+            <DownloadMessages />
+          </AppContainer>
+        </MuiThemeProvider>
+      </Provider>,
+      document.getElementById('root')
+    )
+  } catch (error) {
+    console.log('Error in index.js:', error)
   }
-})
-
-app.extend({
-  init() {
-    this.db = initializeDb()
-    this.store = store
-  },
-})
-app.init()
-
-const theme = Object.assign({}, uiTheme, {
-  appBar: {
-    height: 51,
-  },
-})
-
-// make store accessible in dev
-window.app = app
-
-axios.defaults.baseURL = apiBaseUrl
-
-store.setLoginFromIdb()
-
-// turned off because of errors in production
-// const socket = window.io(apiBaseUrl)
-// socket.on('tabelle_update', payload => updateFromSocket(store, payload))
-
-const AppContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-`
-
-// initiate activeNodeArray
-const activeNodeArrayFromUrl = getActiveNodeArrayFromPathname()
-store.tree.setActiveNodeArray(activeNodeArrayFromUrl)
-store.tree.setLastClickedNode(activeNodeArrayFromUrl)
-// need to set openNodes
-store.tree.setOpenNodesFromActiveNodeArray()
-// clone tree2 in case tree2 is open
-store.tree.cloneActiveNodeArrayToTree2()
-const urlQuery = getUrlQuery(window.location.search)
-store.setUrlQuery(urlQuery)
-
-ReactDOM.render(
-  <Provider store={store}>
-    <MuiThemeProvider muiTheme={getMuiTheme(theme)}>
-      <AppContainer>
-        {false && <DevTools />}
-        <AppBar />
-        <Projekte />
-        <User />
-        <Deletions />
-        <Errors />
-        <UpdateAvailable />
-        <Messages />
-        <DownloadMessages />
-      </AppContainer>
-    </MuiThemeProvider>
-  </Provider>,
-  document.getElementById('root')
-)
+})()
