48d7177123629eb952bd95761ff7325027f59249
Alexander Gabriel <alex@gabriel-software.ch>
2017-11-03 14:45:35 +0100

    flow

--- src/modules/bufferBoundsTo50m.js
+++ src/modules/bufferBoundsTo50m.js
@@ -1,5 +1,5 @@
 // @flow
-export default (bounds: Array) => {
+export default (bounds: Array<Array<number>>) => {
   // only buffer if two points are identical
   if (bounds.length === 2) {
     const b1 = bounds[0]
