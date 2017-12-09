802857e90cea9305e19cdcee5b434e9f62a4a444
Alexander Gabriel <alex.barbalex@gmail.com>
2017-12-09 12:36:56 +0100

    document create db

--- sql/apflora/create_databases.sql
+++ sql/apflora/create_databases.sql
@@ -1,8 +1,14 @@
 CREATE DATABASE apflora encoding 'UTF8';
-CREATE SCHEMA apflora;
--- CREATE EXTENSION pgcrypto;
+CREATE EXTENSION pgcrypto;
 CREATE EXTENSION "uuid-ossp";
 
 -- create role reader in pgAdmin, then:
-GRANT USAGE ON SCHEMA apflora TO read;
-GRANT SELECT ON ALL TABLES IN SCHEMA apflora TO read;
+create role apflora_reader;
+create role apflora_artverantwortlich in group apflora_reader;
+create role apflora_freiwillig;
+create role anon;
+create role authenticator with login password 'secret' noinherit;
+--GRANT USAGE ON SCHEMA apflora TO apflora_reader;
+--GRANT SELECT ON ALL TABLES IN SCHEMA apflora TO apflora_reader;
+
+ALTER DATABASE apflora SET "app.jwt_secret" TO 'secret';
