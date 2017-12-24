06dcd17eabe75c2f4332e578b3019c96d8937163
Alexander Gabriel <alex.barbalex@gmail.com>
2017-12-24 16:26:55 +0100

    get installation to work

--- sql/apflora/create_databases.sql
+++ sql/apflora/create_databases.sql
@@ -8,7 +8,14 @@ create role apflora_artverantwortlich in group apflora_reader;
 create role apflora_freiwillig;
 create role anon;
 create role authenticator with login password 'secret' noinherit;
+grant connect on database apflora to authenticator;
+grant connect on database apflora to anon;
+grant anon to authenticator;
 --GRANT USAGE ON SCHEMA apflora TO apflora_reader;
 --GRANT SELECT ON ALL TABLES IN SCHEMA apflora TO apflora_reader;
 
 ALTER DATABASE apflora SET "app.jwt_secret" TO 'secret';
+
+-- restore from db.backup
+
+-- add roles
--- src/store/action/fetchLogin.js
+++ src/store/action/fetchLogin.js
@@ -9,20 +9,26 @@ export default (store: Object, name: string, password: string): any => {
   store.loading.push('user')
   axios
     .post('/rpc/login', { username: name, pass: password })
-    .then(({ data, status, statusText, message }) => {
-      if (data && data[0] && data[0].token) {
-        const token = data[0].token
-        const tokenDecoded = jwtDecode(token)
-        const { username, role } = tokenDecoded
-        processLogin({ store, name: username, role, token })
-        // refresh currentUser in idb
-        app.db.currentUser.clear()
-        app.db.currentUser.put({ name: username, token, role })
-      } else if (status !== 200) {
-        // somehow fetchLogin sometimes gets called 3 times consecutively
-        // and the second time data is an empty array
-        // but: status is 200
-        store.listError(new Error('Anmeldung gescheitert'))
+    .then(result => {
+      if (result) {
+        const { data, status } = result
+        if (data && data[0] && data[0].token) {
+          const token = data[0].token
+          const tokenDecoded = jwtDecode(token)
+          const { username, role } = tokenDecoded
+          processLogin({ store, name: username, role, token })
+          // refresh currentUser in idb
+          app.db.currentUser.clear()
+          app.db.currentUser.put({ name: username, token, role })
+        } else if (status !== 200) {
+          // somehow fetchLogin sometimes gets called 3 times consecutively
+          // and the second time data is an empty array
+          // but: status is 200
+          store.listError(new Error('Anmeldung gescheitert'))
+        }
+      } else {
+        // somehow then happens with result undefined if user is unauthorized
+        return store.listError(new Error('Anmeldung gescheitert'))
       }
       store.loading = store.loading.filter(el => el !== 'user')
     })
