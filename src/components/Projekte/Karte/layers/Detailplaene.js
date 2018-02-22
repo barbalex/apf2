802b2b24f534657e222364bee6e8945384b0c257
Alexander Gabriel <alex.barbalex@gmail.com>
2018-02-22 17:30:59 +0100

    optimize

--- src/components/Projekte/Karte/layers/Detailplaene.js
+++ src/components/Projekte/Karte/layers/Detailplaene.js
@@ -3,14 +3,13 @@ import React from 'react'
 import { GeoJSON } from 'react-leaflet'
 import { inject } from 'mobx-react'
 
+const style = () => ({ fill: false, color: 'red', weight: 1 })
+
 const DetailplaeneLayer = ({ store }) => {
   store.map.fetchDetailplaene()
   return (
     store.map.detailplaene && (
-      <GeoJSON
-        data={store.map.detailplaene}
-        style={() => ({ fill: false, color: 'red', weight: 1 })}
-      />
+      <GeoJSON data={store.map.detailplaene} style={style} />
     )
   )
 }
