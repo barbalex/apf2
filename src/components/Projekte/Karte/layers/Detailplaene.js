908ef88b9a904ea4c4e6bf569c2fff35d8ed0446
Alexander Gabriel <alex.barbalex@gmail.com>
2018-02-22 19:07:35 +0100

    add popups

--- src/components/Projekte/Karte/layers/Detailplaene.js
+++ src/components/Projekte/Karte/layers/Detailplaene.js
@@ -4,13 +4,35 @@ import { GeoJSON } from 'react-leaflet'
 import { inject } from 'mobx-react'
 import { toJS } from 'mobx'
 
+const popupFromProperties = p =>
+  `<div>
+    ${Object.entries(p)
+      .filter(e => !!e[1])
+      .sort()
+      .map(
+        entry =>
+          `<div style="display:flex;justify-content:space-between;">
+        <div>${entry[0]}:</div><div>${entry[1]}</div>
+      </div>`
+      )
+      .join('')}
+  </div>`
 const style = () => ({ fill: false, color: 'red', weight: 1 })
+const onEachFeature = (feature, layer) => {
+  if (feature.properties) {
+    //console.log('feature.properties:', feature.properties)
+    //layer.bindPopup(JSON.stringify(feature.properties, 5))
+    layer.bindPopup(popupFromProperties(feature.properties))
+  }
+}
 
 const DetailplaeneLayer = ({ store }) => {
-  store.map.fetchDetailplaene()
   const data = toJS(store.map.detailplaene)
+  !data && store.map.fetchDetailplaene()
 
-  return data && <GeoJSON data={data} style={style} />
+  return (
+    data && <GeoJSON data={data} style={style} onEachFeature={onEachFeature} />
+  )
 }
 
 export default inject('store')(DetailplaeneLayer)
