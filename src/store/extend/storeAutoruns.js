a276baacd6d131c8414ddc742ccdaac633e56129
Alexander Gabriel <alex.barbalex@gmail.com>
2018-03-14 21:51:00 +0100

    remove autorunAsync

--- src/store/extend/storeAutoruns.js
+++ src/store/extend/storeAutoruns.js
@@ -2,7 +2,7 @@
 // don't know why but combining this with extendStore call
 // creates an error in an autorun
 // maybe needed actions are not part of Store yet?
-import { extendObservable, autorun, autorunAsync } from 'mobx'
+import { extendObservable, autorun } from 'mobx'
 
 import fetchDataForOpenNodes from '../action/fetchDataForOpenNodes'
 import manipulateUrl from '../action/manipulateUrl'
@@ -10,7 +10,7 @@ import manipulateUrl from '../action/manipulateUrl'
 export default (store: Object): void => {
   extendObservable(store, {
     manipulateUrl: autorun(() => manipulateUrl(store)),
-    fetchDataWhenTreeOpenNodesChanges: autorunAsync(
+    fetchDataWhenTreeOpenNodesChanges: autorun(
       'fetchDataWhenTreeOpenNodesChanges',
       () => {
         // need to pass visibility of layers to make data fetched on changing layers
@@ -34,11 +34,12 @@ export default (store: Object): void => {
           showBeobNichtBeurteilt,
           showBeobNichtZuzuordnen
         )
-      }
+      },
+      { delay: 0 }
     ),
   })
   extendObservable(store.tree2, {
-    fetchDataWhenTreeOpenNodesChanges: autorunAsync(
+    fetchDataWhenTreeOpenNodesChanges: autorun(
       'fetchDataWhenTreeOpenNodesChanges',
       () => {
         // need to pass visibility of layers to make data fetched on changing layers
@@ -62,7 +63,8 @@ export default (store: Object): void => {
           showBeobNichtBeurteilt,
           showBeobNichtZuzuordnen
         )
-      }
+      },
+      { delay: 0 }
     ),
   })
 }
