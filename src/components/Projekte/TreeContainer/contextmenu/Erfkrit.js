16696306cbe6187a017f2d3fac1b23bad27d0a47
Alexander Gabriel <alex.barbalex@gmail.com>
2018-03-16 14:58:48 +0100

    add error boundaries

--- src/components/Projekte/TreeContainer/contextmenu/Erfkrit.js
+++ src/components/Projekte/TreeContainer/contextmenu/Erfkrit.js
@@ -5,13 +5,15 @@ import compose from 'recompose/compose'
 import withState from 'recompose/withState'
 import withHandlers from 'recompose/withHandlers'
 
+import ErrorBoundary from '../../../shared/ErrorBoundary'
+
 const enhance = compose(
   withState('label', 'changeLabel', ''),
   withHandlers({
     // according to https://github.com/vkbansal/react-contextmenu/issues/65
     // this is how to pass data from ContextMenuTrigger to ContextMenu
     onShow: props => event => props.changeLabel(event.detail.data.nodeLabel),
-  }),
+  })
 )
 
 const Erfkrit = ({
@@ -27,31 +29,33 @@ const Erfkrit = ({
   label: string | number,
   onShow: () => void,
 }) => (
-  <ContextMenu
-    id={`${tree.name}erfkrit`}
-    collect={props => props}
-    onShow={onShow}
-  >
-    <div className="react-contextmenu-title">AP-Erfolgskriterium</div>
-    <MenuItem
-      onClick={onClick}
-      data={{
-        action: 'insert',
-        table: 'erfkrit',
-      }}
+  <ErrorBoundary>
+    <ContextMenu
+      id={`${tree.name}erfkrit`}
+      collect={props => props}
+      onShow={onShow}
     >
-      erstelle neues
-    </MenuItem>
-    <MenuItem
-      onClick={onClick}
-      data={{
-        action: 'delete',
-        table: 'erfkrit',
-      }}
-    >
-      lösche
-    </MenuItem>
-  </ContextMenu>
+      <div className="react-contextmenu-title">AP-Erfolgskriterium</div>
+      <MenuItem
+        onClick={onClick}
+        data={{
+          action: 'insert',
+          table: 'erfkrit',
+        }}
+      >
+        erstelle neues
+      </MenuItem>
+      <MenuItem
+        onClick={onClick}
+        data={{
+          action: 'delete',
+          table: 'erfkrit',
+        }}
+      >
+        lösche
+      </MenuItem>
+    </ContextMenu>
+  </ErrorBoundary>
 )
 
 export default enhance(Erfkrit)
--- src/components/Projekte/TreeContainer/contextmenu/TpopfreiwkontrzaehlFolder.js
+++ src/components/Projekte/TreeContainer/contextmenu/TpopfreiwkontrzaehlFolder.js
@@ -2,6 +2,8 @@
 import React from 'react'
 import { ContextMenu, MenuItem } from 'react-contextmenu'
 
+import ErrorBoundary from '../../../shared/ErrorBoundary'
+
 const TpopfreiwkontrzaehlFolder = ({
   onClick,
   tree,
@@ -9,18 +11,20 @@ const TpopfreiwkontrzaehlFolder = ({
   onClick: () => void,
   tree: Object,
 }) => (
-  <ContextMenu id={`${tree.name}tpopfreiwkontrzaehlFolder`}>
-    <div className="react-contextmenu-title">Zählungen</div>
-    <MenuItem
-      onClick={onClick}
-      data={{
-        action: 'insert',
-        table: 'tpopfreiwkontrzaehl',
-      }}
-    >
-      erstelle neue
-    </MenuItem>
-  </ContextMenu>
+  <ErrorBoundary>
+    <ContextMenu id={`${tree.name}tpopfreiwkontrzaehlFolder`}>
+      <div className="react-contextmenu-title">Zählungen</div>
+      <MenuItem
+        onClick={onClick}
+        data={{
+          action: 'insert',
+          table: 'tpopfreiwkontrzaehl',
+        }}
+      >
+        erstelle neue
+      </MenuItem>
+    </ContextMenu>
+  </ErrorBoundary>
 )
 
 export default TpopfreiwkontrzaehlFolder
