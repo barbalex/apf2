1fee79fb3160e769ef1023991935a874e08c5c07
Alexander Gabriel <alex.barbalex@gmail.com>
2018-03-16 14:57:38 +0100

    add error boundaries

--- src/components/Projekte/TreeContainer/contextmenu/BerFolder.js
+++ src/components/Projekte/TreeContainer/contextmenu/BerFolder.js
@@ -2,6 +2,8 @@
 import React from 'react'
 import { ContextMenu, MenuItem } from 'react-contextmenu'
 
+import ErrorBoundary from '../../../shared/ErrorBoundary'
+
 const BerFolder = ({
   onClick,
   tree,
@@ -9,18 +11,20 @@ const BerFolder = ({
   onClick: () => void,
   tree: Object,
 }) => (
-  <ContextMenu id={`${tree.name}berFolder`}>
-    <div className="react-contextmenu-title">Bericht</div>
-    <MenuItem
-      onClick={onClick}
-      data={{
-        action: 'insert',
-        table: 'ber',
-      }}
-    >
-      erstelle neuen
-    </MenuItem>
-  </ContextMenu>
+  <ErrorBoundary>
+    <ContextMenu id={`${tree.name}berFolder`}>
+      <div className="react-contextmenu-title">Bericht</div>
+      <MenuItem
+        onClick={onClick}
+        data={{
+          action: 'insert',
+          table: 'ber',
+        }}
+      >
+        erstelle neuen
+      </MenuItem>
+    </ContextMenu>
+  </ErrorBoundary>
 )
 
 export default BerFolder
--- src/components/Projekte/TreeContainer/contextmenu/ErfkritFolder.js
+++ src/components/Projekte/TreeContainer/contextmenu/ErfkritFolder.js
@@ -2,6 +2,8 @@
 import React from 'react'
 import { ContextMenu, MenuItem } from 'react-contextmenu'
 
+import ErrorBoundary from '../../../shared/ErrorBoundary'
+
 const ErfkritFolder = ({
   onClick,
   tree,
@@ -9,18 +11,20 @@ const ErfkritFolder = ({
   onClick: () => void,
   tree: Object,
 }) => (
-  <ContextMenu id={`${tree.name}erfkritFolder`}>
-    <div className="react-contextmenu-title">AP-Erfolgskriterien</div>
-    <MenuItem
-      onClick={onClick}
-      data={{
-        action: 'insert',
-        table: 'erfkrit',
-      }}
-    >
-      erstelle neues
-    </MenuItem>
-  </ContextMenu>
+  <ErrorBoundary>
+    <ContextMenu id={`${tree.name}erfkritFolder`}>
+      <div className="react-contextmenu-title">AP-Erfolgskriterien</div>
+      <MenuItem
+        onClick={onClick}
+        data={{
+          action: 'insert',
+          table: 'erfkrit',
+        }}
+      >
+        erstelle neues
+      </MenuItem>
+    </ContextMenu>
+  </ErrorBoundary>
 )
 
 export default ErfkritFolder
