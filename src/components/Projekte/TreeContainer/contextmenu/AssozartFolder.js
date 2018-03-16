2eca458714551d24c9e9de774862694b25f5b502
Alexander Gabriel <alex.barbalex@gmail.com>
2018-03-16 14:57:08 +0100

    add error boundaries

--- src/components/Projekte/TreeContainer/contextmenu/AssozartFolder.js
+++ src/components/Projekte/TreeContainer/contextmenu/AssozartFolder.js
@@ -2,6 +2,8 @@
 import React from 'react'
 import { ContextMenu, MenuItem } from 'react-contextmenu'
 
+import ErrorBoundary from '../../../shared/ErrorBoundary'
+
 const AssozartFolder = ({
   onClick,
   tree,
@@ -9,18 +11,20 @@ const AssozartFolder = ({
   onClick: () => void,
   tree: Object,
 }) => (
-  <ContextMenu id={`${tree.name}assozartFolder`}>
-    <div className="react-contextmenu-title">assoziierte Art</div>
-    <MenuItem
-      onClick={onClick}
-      data={{
-        action: 'insert',
-        table: 'assozart',
-      }}
-    >
-      erstelle neue
-    </MenuItem>
-  </ContextMenu>
+  <ErrorBoundary>
+    <ContextMenu id={`${tree.name}assozartFolder`}>
+      <div className="react-contextmenu-title">assoziierte Art</div>
+      <MenuItem
+        onClick={onClick}
+        data={{
+          action: 'insert',
+          table: 'assozart',
+        }}
+      >
+        erstelle neue
+      </MenuItem>
+    </ContextMenu>
+  </ErrorBoundary>
 )
 
 export default AssozartFolder
--- src/components/Projekte/TreeContainer/contextmenu/Beobart.js
+++ src/components/Projekte/TreeContainer/contextmenu/Beobart.js
@@ -2,6 +2,8 @@
 import React from 'react'
 import { ContextMenu, MenuItem } from 'react-contextmenu'
 
+import ErrorBoundary from '../../../shared/ErrorBoundary'
+
 const BeobartFolder = ({
   onClick,
   tree,
@@ -9,27 +11,29 @@ const BeobartFolder = ({
   onClick: () => void,
   tree: Object,
 }) => (
-  <ContextMenu id={`${tree.name}beobart`}>
-    <div className="react-contextmenu-title">Art für Beobachtungen</div>
-    <MenuItem
-      onClick={onClick}
-      data={{
-        action: 'insert',
-        table: 'beobart',
-      }}
-    >
-      erstelle neue
-    </MenuItem>
-    <MenuItem
-      onClick={onClick}
-      data={{
-        action: 'delete',
-        table: 'beobart',
-      }}
-    >
-      lösche
-    </MenuItem>
-  </ContextMenu>
+  <ErrorBoundary>
+    <ContextMenu id={`${tree.name}beobart`}>
+      <div className="react-contextmenu-title">Art für Beobachtungen</div>
+      <MenuItem
+        onClick={onClick}
+        data={{
+          action: 'insert',
+          table: 'beobart',
+        }}
+      >
+        erstelle neue
+      </MenuItem>
+      <MenuItem
+        onClick={onClick}
+        data={{
+          action: 'delete',
+          table: 'beobart',
+        }}
+      >
+        lösche
+      </MenuItem>
+    </ContextMenu>
+  </ErrorBoundary>
 )
 
 export default BeobartFolder
