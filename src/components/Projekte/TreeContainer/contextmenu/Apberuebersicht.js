e00bca195b6cd385036895d8f2453e397cf7e657
Alexander Gabriel <alex.barbalex@gmail.com>
2018-03-16 14:56:45 +0100

    add error boundaries

--- src/components/Projekte/TreeContainer/contextmenu/Apberuebersicht.js
+++ src/components/Projekte/TreeContainer/contextmenu/Apberuebersicht.js
@@ -2,6 +2,8 @@
 import React from 'react'
 import { ContextMenu, MenuItem } from 'react-contextmenu'
 
+import ErrorBoundary from '../../../shared/ErrorBoundary'
+
 const Apberuebersicht = ({
   onClick,
   tree,
@@ -9,27 +11,29 @@ const Apberuebersicht = ({
   onClick: () => void,
   tree: Object,
 }) => (
-  <ContextMenu id={`${tree.name}apberuebersicht`}>
-    <div className="react-contextmenu-title">AP-Bericht</div>
-    <MenuItem
-      onClick={onClick}
-      data={{
-        action: 'insert',
-        table: 'apberuebersicht',
-      }}
-    >
-      erstelle neuen
-    </MenuItem>
-    <MenuItem
-      onClick={onClick}
-      data={{
-        action: 'delete',
-        table: 'apberuebersicht',
-      }}
-    >
-      lösche
-    </MenuItem>
-  </ContextMenu>
+  <ErrorBoundary>
+    <ContextMenu id={`${tree.name}apberuebersicht`}>
+      <div className="react-contextmenu-title">AP-Bericht</div>
+      <MenuItem
+        onClick={onClick}
+        data={{
+          action: 'insert',
+          table: 'apberuebersicht',
+        }}
+      >
+        erstelle neuen
+      </MenuItem>
+      <MenuItem
+        onClick={onClick}
+        data={{
+          action: 'delete',
+          table: 'apberuebersicht',
+        }}
+      >
+        lösche
+      </MenuItem>
+    </ContextMenu>
+  </ErrorBoundary>
 )
 
 export default Apberuebersicht
--- src/components/Projekte/TreeContainer/contextmenu/Assozart.js
+++ src/components/Projekte/TreeContainer/contextmenu/Assozart.js
@@ -2,6 +2,8 @@
 import React from 'react'
 import { ContextMenu, MenuItem } from 'react-contextmenu'
 
+import ErrorBoundary from '../../../shared/ErrorBoundary'
+
 const AssozartFolder = ({
   onClick,
   tree,
@@ -9,27 +11,29 @@ const AssozartFolder = ({
   onClick: () => void,
   tree: Object,
 }) => (
-  <ContextMenu id={`${tree.name}assozart`}>
-    <div className="react-contextmenu-title">assoziierte Art</div>
-    <MenuItem
-      onClick={onClick}
-      data={{
-        action: 'insert',
-        table: 'assozart',
-      }}
-    >
-      erstelle neue
-    </MenuItem>
-    <MenuItem
-      onClick={onClick}
-      data={{
-        action: 'delete',
-        table: 'assozart',
-      }}
-    >
-      lösche
-    </MenuItem>
-  </ContextMenu>
+  <ErrorBoundary>
+    <ContextMenu id={`${tree.name}assozart`}>
+      <div className="react-contextmenu-title">assoziierte Art</div>
+      <MenuItem
+        onClick={onClick}
+        data={{
+          action: 'insert',
+          table: 'assozart',
+        }}
+      >
+        erstelle neue
+      </MenuItem>
+      <MenuItem
+        onClick={onClick}
+        data={{
+          action: 'delete',
+          table: 'assozart',
+        }}
+      >
+        lösche
+      </MenuItem>
+    </ContextMenu>
+  </ErrorBoundary>
 )
 
 export default AssozartFolder
