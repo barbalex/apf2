a7f7d66451d8c3a24d511d65dbace77e59750ff6
Alexander Gabriel <alex.barbalex@gmail.com>
2018-03-16 13:56:46 +0100

    add error boundaries

--- src/components/Projekte/Daten/Popber.js
+++ src/components/Projekte/Daten/Popber.js
@@ -7,6 +7,7 @@ import compose from 'recompose/compose'
 import RadioButtonGroup from '../../shared/RadioButtonGroup'
 import TextField from '../../shared/TextField'
 import FormTitle from '../../shared/FormTitle'
+import ErrorBoundary from '../../shared/ErrorBoundary'
 
 const Container = styled.div`
   height: 100%;
@@ -25,44 +26,46 @@ const Popber = ({ store, tree }: { store: Object, tree: Object }) => {
   const { activeDataset } = tree
 
   return (
-    <Container>
-      <FormTitle tree={tree} title="Kontroll-Bericht Population" />
-      <FieldsContainer>
-        <TextField
-          key={`${activeDataset.row.PopberId}PopBerJahr`}
-          tree={tree}
-          label="Jahr"
-          fieldName="PopBerJahr"
-          value={activeDataset.row.PopBerJahr}
-          errorText={activeDataset.valid.PopBerJahr}
-          type="number"
-          updateProperty={store.updateProperty}
-          updatePropertyInDb={store.updatePropertyInDb}
-        />
-        <RadioButtonGroup
-          tree={tree}
-          fieldName="PopBerEntwicklung"
-          label="Entwicklung"
-          value={activeDataset.row.PopBerEntwicklung}
-          errorText={activeDataset.valid.PopBerEntwicklung}
-          dataSource={store.dropdownList.popEntwicklungWerte}
-          updatePropertyInDb={store.updatePropertyInDb}
-        />
-        <TextField
-          key={`${activeDataset.row.PopberId}PopBerTxt`}
-          tree={tree}
-          label="Bemerkungen"
-          fieldName="PopBerTxt"
-          value={activeDataset.row.PopBerTxt}
-          errorText={activeDataset.valid.PopBerTxt}
-          type="text"
-          multiLine
-          fullWidth
-          updateProperty={store.updateProperty}
-          updatePropertyInDb={store.updatePropertyInDb}
-        />
-      </FieldsContainer>
-    </Container>
+    <ErrorBoundary>
+      <Container>
+        <FormTitle tree={tree} title="Kontroll-Bericht Population" />
+        <FieldsContainer>
+          <TextField
+            key={`${activeDataset.row.PopberId}PopBerJahr`}
+            tree={tree}
+            label="Jahr"
+            fieldName="PopBerJahr"
+            value={activeDataset.row.PopBerJahr}
+            errorText={activeDataset.valid.PopBerJahr}
+            type="number"
+            updateProperty={store.updateProperty}
+            updatePropertyInDb={store.updatePropertyInDb}
+          />
+          <RadioButtonGroup
+            tree={tree}
+            fieldName="PopBerEntwicklung"
+            label="Entwicklung"
+            value={activeDataset.row.PopBerEntwicklung}
+            errorText={activeDataset.valid.PopBerEntwicklung}
+            dataSource={store.dropdownList.popEntwicklungWerte}
+            updatePropertyInDb={store.updatePropertyInDb}
+          />
+          <TextField
+            key={`${activeDataset.row.PopberId}PopBerTxt`}
+            tree={tree}
+            label="Bemerkungen"
+            fieldName="PopBerTxt"
+            value={activeDataset.row.PopBerTxt}
+            errorText={activeDataset.valid.PopBerTxt}
+            type="text"
+            multiLine
+            fullWidth
+            updateProperty={store.updateProperty}
+            updatePropertyInDb={store.updatePropertyInDb}
+          />
+        </FieldsContainer>
+      </Container>
+    </ErrorBoundary>
   )
 }
 
