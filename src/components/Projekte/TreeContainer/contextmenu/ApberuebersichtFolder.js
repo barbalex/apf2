50b79a94f7df637d876d078d17d18e44677c471c
Alexander Gabriel <alex.barbalex@gmail.com>
2018-03-16 14:57:20 +0100

    add error boundaries

--- src/components/Projekte/TreeContainer/contextmenu/ApberuebersichtFolder.js
+++ src/components/Projekte/TreeContainer/contextmenu/ApberuebersichtFolder.js
@@ -2,6 +2,8 @@
 import React from 'react'
 import { ContextMenu, MenuItem } from 'react-contextmenu'
 
+import ErrorBoundary from '../../../shared/ErrorBoundary'
+
 const ApberuebersichtFolder = ({
   onClick,
   tree,
@@ -9,18 +11,20 @@ const ApberuebersichtFolder = ({
   onClick: () => void,
   tree: Object,
 }) => (
-  <ContextMenu id={`${tree.name}apberuebersichtFolder`}>
-    <div className="react-contextmenu-title">AP-Bericht</div>
-    <MenuItem
-      onClick={onClick}
-      data={{
-        action: 'insert',
-        table: 'apberuebersicht',
-      }}
-    >
-      erstelle neuen
-    </MenuItem>
-  </ContextMenu>
+  <ErrorBoundary>
+    <ContextMenu id={`${tree.name}apberuebersichtFolder`}>
+      <div className="react-contextmenu-title">AP-Bericht</div>
+      <MenuItem
+        onClick={onClick}
+        data={{
+          action: 'insert',
+          table: 'apberuebersicht',
+        }}
+      >
+        erstelle neuen
+      </MenuItem>
+    </ContextMenu>
+  </ErrorBoundary>
 )
 
 export default ApberuebersichtFolder
