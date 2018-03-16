29f7758246bd2c7680126bf1582daf3cbc3d1185
Alexander Gabriel <alex.barbalex@gmail.com>
2018-03-16 14:58:18 +0100

    add error boundaries

--- src/components/Projekte/TreeContainer/contextmenu/Tpopber.js
+++ src/components/Projekte/TreeContainer/contextmenu/Tpopber.js
@@ -5,13 +5,15 @@ import compose from 'recompose/compose'
 import withState from 'recompose/withState'
 import withHandlers from 'recompose/withHandlers'
 
+import ErrorBoundary from '../../../shared/ErrorBoundary'
+
 const enhance = compose(
   withState('label', 'changeLabel', ''),
   withHandlers({
     // according to https://github.com/vkbansal/react-contextmenu/issues/65
     // this is how to pass data from ContextMenuTrigger to ContextMenu
     onShow: props => event => props.changeLabel(event.detail.data.nodeLabel),
-  }),
+  })
 )
 
 const Tpopber = ({
@@ -27,31 +29,33 @@ const Tpopber = ({
   label: string | number,
   onShow: () => void,
 }) => (
-  <ContextMenu
-    id={`${tree.name}tpopber`}
-    collect={props => props}
-    onShow={onShow}
-  >
-    <div className="react-contextmenu-title">Kontroll-Bericht</div>
-    <MenuItem
-      onClick={onClick}
-      data={{
-        action: 'insert',
-        table: 'tpopber',
-      }}
+  <ErrorBoundary>
+    <ContextMenu
+      id={`${tree.name}tpopber`}
+      collect={props => props}
+      onShow={onShow}
     >
-      erstelle neuen
-    </MenuItem>
-    <MenuItem
-      onClick={onClick}
-      data={{
-        action: 'delete',
-        table: 'tpopber',
-      }}
-    >
-      lösche
-    </MenuItem>
-  </ContextMenu>
+      <div className="react-contextmenu-title">Kontroll-Bericht</div>
+      <MenuItem
+        onClick={onClick}
+        data={{
+          action: 'insert',
+          table: 'tpopber',
+        }}
+      >
+        erstelle neuen
+      </MenuItem>
+      <MenuItem
+        onClick={onClick}
+        data={{
+          action: 'delete',
+          table: 'tpopber',
+        }}
+      >
+        lösche
+      </MenuItem>
+    </ContextMenu>
+  </ErrorBoundary>
 )
 
 export default enhance(Tpopber)
--- src/components/Projekte/TreeContainer/contextmenu/Tpopfeldkontrzaehl.js
+++ src/components/Projekte/TreeContainer/contextmenu/Tpopfeldkontrzaehl.js
@@ -5,13 +5,15 @@ import compose from 'recompose/compose'
 import withState from 'recompose/withState'
 import withHandlers from 'recompose/withHandlers'
 
+import ErrorBoundary from '../../../shared/ErrorBoundary'
+
 const enhance = compose(
   withState('label', 'changeLabel', ''),
   withHandlers({
     // according to https://github.com/vkbansal/react-contextmenu/issues/65
     // this is how to pass data from ContextMenuTrigger to ContextMenu
     onShow: props => event => props.changeLabel(event.detail.data.nodeLabel),
-  }),
+  })
 )
 
 const Tpopfeldkontrzaehl = ({
@@ -27,31 +29,33 @@ const Tpopfeldkontrzaehl = ({
   label: string | number,
   onShow: () => void,
 }) => (
-  <ContextMenu
-    id={`${tree.name}tpopfeldkontrzaehl`}
-    collect={props => props}
-    onShow={onShow}
-  >
-    <div className="react-contextmenu-title">Zählung</div>
-    <MenuItem
-      onClick={onClick}
-      data={{
-        action: 'insert',
-        table: 'tpopfeldkontrzaehl',
-      }}
+  <ErrorBoundary>
+    <ContextMenu
+      id={`${tree.name}tpopfeldkontrzaehl`}
+      collect={props => props}
+      onShow={onShow}
     >
-      erstelle neue
-    </MenuItem>
-    <MenuItem
-      onClick={onClick}
-      data={{
-        action: 'delete',
-        table: 'tpopfeldkontrzaehl',
-      }}
-    >
-      lösche
-    </MenuItem>
-  </ContextMenu>
+      <div className="react-contextmenu-title">Zählung</div>
+      <MenuItem
+        onClick={onClick}
+        data={{
+          action: 'insert',
+          table: 'tpopfeldkontrzaehl',
+        }}
+      >
+        erstelle neue
+      </MenuItem>
+      <MenuItem
+        onClick={onClick}
+        data={{
+          action: 'delete',
+          table: 'tpopfeldkontrzaehl',
+        }}
+      >
+        lösche
+      </MenuItem>
+    </ContextMenu>
+  </ErrorBoundary>
 )
 
 export default enhance(Tpopfeldkontrzaehl)
