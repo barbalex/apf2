4c574150926f701f35daee87ef213a84b520eeda
Alexander Gabriel <alex.barbalex@gmail.com>
2018-04-10 17:16:01 +0200

    migrate

--- src/modules/nodes/zielberFolder.js
+++ src/modules/nodes/zielberFolder.js
@@ -7,7 +7,7 @@ export default (
   projId: number,
   apArtId: number,
   zieljahr: number,
-  zielId: number,
+  zielId: number
 ): Array<Object> => {
   // fetch sorting indexes of parents
   const projIndex = findIndex(tree.filteredAndSorted.projekt, {
@@ -15,24 +15,24 @@ export default (
   })
   const apIndex = findIndex(
     tree.filteredAndSorted.ap.filter(a => a.ProjId === projId),
-    { ApArtId: apArtId },
+    { ApArtId: apArtId }
   )
   const zieljahrIndex = findIndex(tree.filteredAndSorted.zieljahr, {
     jahr: zieljahr,
   })
   const zielIndex = findIndex(
     tree.filteredAndSorted.ziel.filter(z => z.ZielJahr === zieljahr),
-    { ZielId: zielId },
+    { ZielId: zielId }
   )
   const zielIsVisible = !!tree.filteredAndSorted.ziel.filter(
-    z => z.ZielJahr === zieljahr && z.ZielId === zielId,
+    z => z.ZielJahr === zieljahr && z.ZielId === zielId
   )
 
   // prevent folder from showing when nodeFilter is set
   if (!zielIsVisible) return []
 
   const zielberNodesLength = tree.filteredAndSorted.zielber.filter(
-    z => z.ZielId === zielId,
+    z => z.ziel_id === zielId
   ).length
 
   let message = zielberNodesLength
