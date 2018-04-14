0d380eaf8a1dad48be6ad75ea2865775fc30ef67
Alexander Gabriel <alex.barbalex@gmail.com>
2018-04-14 23:54:37 +0200

    migrate ap

--- sql/migration/0041-ap.sql
+++ sql/migration/0041-ap.sql
@@ -335,12 +335,201 @@ $ap_insert_add_apart$ LANGUAGE plpgsql;
 CREATE TRIGGER ap_insert_add_apart AFTER INSERT ON apflora.ap
   FOR EACH ROW EXECUTE PROCEDURE apflora.ap_insert_add_apart();
 
+
+
+/*
+ * Sicherstellen, das pro Pop/TPop jährlich maximal ein Bericht erstellt wird (massnber, popber, tpopber)
+ */
+-- drop triggers before creating functions because triggers depend on functions
+DROP TRIGGER IF EXISTS tpop_max_one_massnber_per_year ON apflora.tpopmassnber;
+DROP FUNCTION IF EXISTS apflora.tpop_max_one_massnber_per_year();
+CREATE FUNCTION apflora.tpop_max_one_massnber_per_year() RETURNS trigger AS $tpop_max_one_massnber_per_year$
+  BEGIN
+    -- check if a tpopmassnber already exists for this year
+    IF
+      (
+        NEW.jahr > 0
+        AND NEW.jahr IN
+          (
+            SELECT
+              jahr
+            FROM
+              apflora.tpopmassnber
+            WHERE
+              tpop_id = NEW.tpop_id
+              AND id <> NEW.id
+          )
+      )
+    THEN
+      RAISE EXCEPTION  'Pro Teilpopulation und Jahr darf maximal ein Massnahmenbericht erfasst werden';
+    END IF;
+    RETURN NEW;
+  END;
+$tpop_max_one_massnber_per_year$ LANGUAGE plpgsql;
+CREATE TRIGGER tpop_max_one_massnber_per_year BEFORE UPDATE OR INSERT ON apflora.tpopmassnber
+  FOR EACH ROW EXECUTE PROCEDURE apflora.tpop_max_one_massnber_per_year();
+
+DROP TRIGGER IF EXISTS pop_max_one_massnber_per_year ON apflora.popmassnber;
+DROP FUNCTION IF EXISTS apflora.pop_max_one_massnber_per_year();
+CREATE FUNCTION apflora.pop_max_one_massnber_per_year() RETURNS trigger AS $pop_max_one_massnber_per_year$
+  BEGIN
+    IF
+      (
+        NEW.jahr > 0
+        AND NEW.jahr IN
+          (
+            SELECT
+              jahr
+            FROM
+              apflora.popmassnber
+            WHERE
+              pop_id = NEW.pop_id
+              AND id <> NEW.id
+          )
+      )
+    THEN
+      RAISE EXCEPTION 'Pro Population und Jahr darf maximal ein Massnahmenbericht erfasst werden';
+    END IF;
+    RETURN NEW;
+  END;
+$pop_max_one_massnber_per_year$ LANGUAGE plpgsql;
+
+CREATE TRIGGER pop_max_one_massnber_per_year BEFORE INSERT OR UPDATE ON apflora.popmassnber
+  FOR EACH ROW EXECUTE PROCEDURE apflora.pop_max_one_massnber_per_year();
+
+DROP TRIGGER IF EXISTS pop_max_one_popber_per_year ON apflora.popber;
+DROP FUNCTION IF EXISTS apflora.pop_max_one_popber_per_year();
+CREATE FUNCTION apflora.pop_max_one_popber_per_year() RETURNS trigger AS $pop_max_one_popber_per_year$
+  BEGIN
+    IF
+      (
+        NEW.jahr > 0
+        AND NEW.jahr IN
+          (
+            SELECT
+              jahr
+            FROM
+              apflora.popber
+            WHERE
+              pop_id = NEW.pop_id
+              AND id <> NEW.id
+          )
+      )
+    THEN
+      RAISE EXCEPTION 'Pro Population und Jahr darf maximal ein Populationsbericht erfasst werden';
+    END IF;
+    RETURN NEW;
+  END;
+$pop_max_one_popber_per_year$ LANGUAGE plpgsql;
+
+CREATE TRIGGER pop_max_one_popber_per_year BEFORE INSERT OR UPDATE ON apflora.popber
+  FOR EACH ROW EXECUTE PROCEDURE apflora.pop_max_one_popber_per_year();
+
+
+DROP TRIGGER IF EXISTS tpop_max_one_tpopber_per_year ON apflora.tpopber;
+DROP FUNCTION IF EXISTS apflora.tpop_max_one_tpopber_per_year();
+CREATE FUNCTION apflora.tpop_max_one_tpopber_per_year() RETURNS trigger AS $tpop_max_one_tpopber_per_year$
+  BEGIN
+    -- check if a tpopber already exists for this year
+    IF
+      (
+        NEW.jahr > 0
+        AND NEW.jahr IN
+        (
+          SELECT
+            jahr
+          FROM
+            apflora.tpopber
+          WHERE
+            tpop_id = NEW.tpop_id
+            AND id <> NEW.id
+        )
+      )
+    THEN
+      RAISE EXCEPTION 'Pro Teilpopulation und Jahr darf maximal ein Teilpopulationsbericht erfasst werden';
+    END IF;
+    RETURN NEW;
+  END;
+$tpop_max_one_tpopber_per_year$ LANGUAGE plpgsql;
+
+CREATE TRIGGER tpop_max_one_tpopber_per_year BEFORE UPDATE OR INSERT ON apflora.tpopber
+  FOR EACH ROW EXECUTE PROCEDURE apflora.tpop_max_one_tpopber_per_year();
+
+-- when ap is inserted
+-- ensure idealbiotop is created too
+DROP TRIGGER IF EXISTS ap_insert_add_idealbiotop ON apflora.ap;
+DROP FUNCTION IF EXISTS apflora.ap_insert_add_idealbiotop();
+CREATE FUNCTION apflora.ap_insert_add_idealbiotop() RETURNS trigger AS $ap_insert_add_idealbiotop$
+BEGIN
+  INSERT INTO
+    apflora.idealbiotop (ap_id)
+  VALUES (NEW.id);
+  RETURN NEW;
+END;
+$ap_insert_add_idealbiotop$ LANGUAGE plpgsql;
+
+CREATE TRIGGER ap_insert_add_idealbiotop AFTER INSERT ON apflora.ap
+  FOR EACH ROW EXECUTE PROCEDURE apflora.ap_insert_add_idealbiotop();
+
+-- in case this trigger was not working
+-- add idealbiotop where they are missing
+insert into apflora.idealbiotop (ap_id)
+select apflora.ap.id from apflora.ap
+left join apflora.idealbiotop
+on apflora.idealbiotop.ap_id = apflora.ap.id
+where apflora.idealbiotop.ap_id is null;
+
+-- when ap is inserted
+-- ensure apart is created too
+DROP TRIGGER IF EXISTS ap_insert_add_beobart ON apflora.ap;
+DROP TRIGGER IF EXISTS ap_insert_add_apart ON apflora.ap;
+DROP FUNCTION IF EXISTS apflora.ap_insert_add_beobart();
+DROP FUNCTION IF EXISTS apflora.ap_insert_add_apart();
+CREATE FUNCTION apflora.ap_insert_add_apart() RETURNS trigger AS $ap_insert_add_apart$
+BEGIN
+  INSERT INTO
+    apflora.apart (ap_id, taxid)
+  VALUES (NEW.id, NEW.id);
+  RETURN NEW;
+END;
+$ap_insert_add_apart$ LANGUAGE plpgsql;
+
+CREATE TRIGGER ap_insert_add_apart AFTER INSERT ON apflora.ap
+  FOR EACH ROW EXECUTE PROCEDURE apflora.ap_insert_add_apart();
+
+-- when ap is inserted
+-- ensure apart is created too
+DROP TRIGGER IF EXISTS ap_insert_add_beobart ON apflora.ap;
+DROP TRIGGER IF EXISTS ap_insert_add_apart ON apflora.ap;
+DROP FUNCTION IF EXISTS apflora.ap_insert_add_beobart();
+DROP FUNCTION IF EXISTS apflora.ap_insert_add_apart();
+CREATE FUNCTION apflora.ap_insert_add_apart() RETURNS trigger AS $ap_insert_add_apart$
+BEGIN
+  INSERT INTO
+    apflora.apart (ap_id, taxid)
+  VALUES (NEW.id, NEW.id);
+  RETURN NEW;
+END;
+$ap_insert_add_apart$ LANGUAGE plpgsql;
+
+CREATE TRIGGER ap_insert_add_apart AFTER INSERT ON apflora.ap
+  FOR EACH ROW EXECUTE PROCEDURE apflora.ap_insert_add_apart();
+
+
+
+
+
+
+
+
+
+
 CREATE OR REPLACE FUNCTION apflora.qk2_tpop_ohne_tpopber(apid integer, berichtjahr integer)
   RETURNS table("ProjId" integer, ap_id integer, hw text, url text[], text text[]) AS
   $$
   -- 3. "TPop ohne verlangten TPop-Bericht im Berichtjahr" ermitteln und in Qualitätskontrollen auflisten:
   SELECT DISTINCT
-    apflora.ap."ProjId",
+    apflora.ap.proj_id,
     apflora.pop.ap_id,
     'Teilpopulation mit Kontrolle (im Berichtjahr) aber ohne Teilpopulations-Bericht (im Berichtjahr):' AS hw,
     ARRAY['Projekte', 1 , 'Arten', apflora.ap.id, 'Populationen', apflora.pop.id, 'Teil-Populationen', apflora.tpop.id]::text[] AS "url",
@@ -385,7 +574,7 @@ CREATE OR REPLACE FUNCTION apflora.qk2_pop_ohne_popmassnber(apid integer, berich
   $$
   -- 5. "Pop ohne verlangten Pop-Massn-Bericht im Berichtjahr" ermitteln und in Qualitätskontrollen auflisten:
   SELECT DISTINCT
-    apflora.ap."ProjId",
+    apflora.ap.proj_id,
     apflora.pop.ap_id,
     'Population mit angesiedelten Teilpopulationen (vor dem Berichtjahr), die (im Berichtjahr) kontrolliert wurden, aber ohne Massnahmen-Bericht (im Berichtjahr):' AS hw,
     ARRAY['Projekte', 1 , 'Arten', apflora.ap.id, 'Populationen', apflora.pop.id]::text[] AS "url",
@@ -448,12 +637,11 @@ CREATE OR REPLACE FUNCTION apflora.qk2_pop_ohne_popmassnber(apid integer, berich
   LANGUAGE sql STABLE;
 ALTER FUNCTION apflora.qk2_pop_ohne_popmassnber(apid integer, berichtjahr integer)
   OWNER TO postgres;
-
 CREATE OR REPLACE FUNCTION apflora.qk2_pop_ohne_popber(apid integer, berichtjahr integer)
   RETURNS table("ProjId" integer, ap_id integer, hw text, url text[], text text[]) AS
   $$
   SELECT DISTINCT
-    apflora.ap."ProjId",
+    apflora.ap.proj_id,
     apflora.pop.ap_id,
     'Population mit angesiedelten Teilpopulationen (vor dem Berichtjahr), die (im Berichtjahr) kontrolliert wurden, aber ohne Populations-Bericht (im Berichtjahr):' AS hw,
     ARRAY['Projekte', 1 , 'Arten', apflora.ap.id, 'Populationen', apflora.pop.id]::text[] AS "url",
@@ -516,4 +704,5 @@ CREATE OR REPLACE FUNCTION apflora.qk2_pop_ohne_popber(apid integer, berichtjahr
   LANGUAGE sql STABLE;
 ALTER FUNCTION apflora.qk2_pop_ohne_popber(apid integer, berichtjahr integer)
   OWNER TO postgres;
+
 
