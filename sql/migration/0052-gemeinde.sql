cf8487d523d049db30df7c18eb62a2f54556e076
Alexander Gabriel <alex.barbalex@gmail.com>
2018-04-19 11:27:27 +0200

    migrate gemeinde

--- sql/migration/0052-gemeinde.sql
+++ sql/migration/0052-gemeinde.sql
@@ -1,23 +1,21 @@
 ALTER TABLE apflora.gemeinde ADD COLUMN id UUID UNIQUE DEFAULT uuid_generate_v1mc();
-ALTER TABLE apflora.gemeinde RENAME "BfsNr" TO id_old;
 ALTER TABLE apflora.gemeinde RENAME "GmdName" TO name;
 
 -- change primary key
 ALTER TABLE apflora.gemeinde DROP CONSTRAINT gemeinde_pkey cascade;
 ALTER TABLE apflora.gemeinde ADD PRIMARY KEY (id);
-ALTER TABLE apflora.gemeinde ALTER COLUMN id_old DROP NOT NULL;
-ALTER TABLE apflora.gemeinde ALTER COLUMN id_old SET DEFAULT null;
 
 -- drop existing indexes
 DROP index IF EXISTS apflora.apflora."gemeinde_BfsNr_idx";
 DROP index IF EXISTS apflora.apflora."gemeinde_GmdName_idx";
 -- don't need new
+ALTER TABLE apflora.gemeinde drop COLUMN "BfsNr" cascade;
 
 -- done: make sure createTable is correct
 -- done: rename in sql
 -- done: rename in js
 -- done: add all views, functions, triggers containing this table to this file
--- TODO: run migration sql in dev
--- TODO: restart postgrest and test app
+-- done: run migration sql in dev
+-- done: restart postgrest and test app
 -- TODO: update js and run this file on server
 -- TODO: restart postgrest
