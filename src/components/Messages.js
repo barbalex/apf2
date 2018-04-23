c97b52a77174790852d6466822cde4033c39eed9
Alexander Gabriel <alex.barbalex@gmail.com>
2018-04-23 10:25:22 +0200

    use m v1

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,7 +1,8 @@
 // @flow
 import React from 'react'
 import { observer, inject } from 'mobx-react'
-import Dialog from 'material-ui/Dialog'
+//import Dialog from 'material-ui/Dialog'
+import Dialog, { DialogTitle } from 'material-ui-next/Dialog'
 import Button from 'material-ui-next/Button'
 import compose from 'recompose/compose'
 import withHandlers from 'recompose/withHandlers'
@@ -10,8 +11,18 @@ import styled from 'styled-components'
 import ErrorBoundary from './shared/ErrorBoundary'
 
 const StyledDialog = styled(Dialog)`
-  > div > div > div > div {
-    overflow: auto !important;
+  > div > div {
+    max-width: ${window.innerWidth * 0.8}px;
+    min-width: 400px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding-right: 24px;
+    padding-left: 24px;
+    overflow: auto;
+    > div {
+      padding-bottom: 8px;
+    }
   }
 `
 const MessageContainer = styled.div`
@@ -51,15 +62,9 @@ const UserMessages = ({
   return (
     <ErrorBoundary>
       <StyledDialog
-        title="Letzte Anpassungen:"
         open={store.messages.messages.length > 0 && !!store.user.name}
-        contentStyle={{
-          maxWidth: window.innerWidth * 0.8,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-        }}
       >
+        <DialogTitle>Letzte Anpassungen:</DialogTitle>
         <div>
           <AllOkButton onClick={onClickReadAll}>alle o.k.</AllOkButton>
           {store.messages.messages.sort(m => m.time).map((m, index) => {
