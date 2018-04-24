1d2d4f02f0809eb7a9dc988d753c5f4061208da3
Alexander Gabriel <alex.barbalex@gmail.com>
2018-04-24 12:49:28 +0200

    use m v1

--- src/components/Projekte/Exporte/Optionen.js
+++ src/components/Projekte/Exporte/Optionen.js
@@ -1,7 +1,8 @@
 // @flow
 import React from 'react'
 import { Card, CardHeader, CardText } from 'material-ui/Card'
-import Checkbox from 'material-ui/Checkbox'
+import { FormControlLabel } from 'material-ui-next/Form'
+import Checkbox from 'material-ui-next/Checkbox'
 import { observer, inject } from 'mobx-react'
 import compose from 'recompose/compose'
 import styled from 'styled-components'
@@ -10,49 +11,51 @@ const FirstLevelCard = styled(Card)`
   margin-bottom: 10px;
   background-color: #fff8e1 !important;
 `
+const StyledCardText = styled(CardText)`
+  padding-top: 5px !important;
+`
+const StyledFormControlLabel = styled(FormControlLabel)`
+  margin-left: 0 !important;
+`
+const StyledCheckbox = styled(Checkbox)`
+  width: 30px !important;
+  height: 30px !important;
+`
 
 const enhance = compose(inject('store'), observer)
 
 const Optionen = ({ store }: { store: Object }) => (
   <FirstLevelCard initiallyExpanded>
     <CardHeader title="Optionen" actAsExpander showExpandableButton />
-    <CardText expandable>
-      <Checkbox
+    <StyledCardText expandable>
+      <StyledFormControlLabel
+        control={
+          <StyledCheckbox
+            checked={store.export.fileType === 'csv'}
+            onChange={store.export.toggleFileType}
+            value={store.export.fileType}
+            color="primary"
+          />
+        }
         label="Dateien im .csv-Format exportieren (Standard ist das xlsx-Format von Excel)"
-        value={store.export.fileType}
-        checked={store.export.fileType === 'csv'}
-        onCheck={store.export.toggleFileType}
       />
-      <Checkbox
+      <StyledFormControlLabel
+        control={
+          <StyledCheckbox
+            checked={store.export.applyMapFilterToExport}
+            onChange={store.export.toggleApplyMapFilterToExport}
+            value={store.export.applyMapFilterToExport}
+            color="primary"
+          />
+        }
         label={
           store.map.mapFilter.filter.features.length > 0
             ? 'Karten-Filter anwenden'
             : 'Karten-Filter anwenden (verfügbar, wenn ein Karten-Filter erstellt wurde)'
         }
-        value={store.export.applyMapFilterToExport}
-        checked={store.export.applyMapFilterToExport}
-        onCheck={store.export.toggleApplyMapFilterToExport}
         disabled={!(store.map.mapFilter.filter.features.length > 0)}
       />
-      {false && (
-        <Checkbox
-          label={'Strukturbaum-Filter anwenden'}
-          value={store.export.applyNodeLabelFilterToExport}
-          checked={store.export.applyNodeLabelFilterToExport}
-          onCheck={store.export.toggleApplyNodeLabelFilterToExport}
-          disabled
-        />
-      )}
-      {false && (
-        <Checkbox
-          label={'Nach den aktuell im Strukturbaum gewählten Elementen filtern'}
-          value={store.export.applyActiveNodeFilterToExport}
-          checked={store.export.applyActiveNodeFilterToExport}
-          onCheck={store.export.toggleApplyActiveNodeFilterToExport}
-          disabled
-        />
-      )}
-    </CardText>
+    </StyledCardText>
   </FirstLevelCard>
 )
 
