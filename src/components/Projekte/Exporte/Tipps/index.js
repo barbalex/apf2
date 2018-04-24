6642c481b69fcac0ee12e4aaf44442c13a74c71a
Alexander Gabriel <alex.barbalex@gmail.com>
2018-04-24 18:08:00 +0200

    use m v1

---
+++ src/components/Projekte/Exporte/Tipps/index.js
@@ -0,0 +1,76 @@
+// @flow
+import React from 'react'
+import Card, { CardActions, CardContent } from 'material-ui-next/Card'
+import Collapse from 'material-ui-next/transitions/Collapse'
+import IconButton from 'material-ui-next/IconButton'
+import Icon from 'material-ui-next/Icon'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import compose from 'recompose/compose'
+import withState from 'recompose/withState'
+import styled from 'styled-components'
+
+import WasIstCsv from './WasIstCsv'
+import CsvOeffnen from './CsvOeffnen'
+import CsvProgramm from './CsvProgramm'
+import DatenChaotisch from './DatenChaotisch'
+import ZuVieleDaten from './ZuVieleDaten'
+
+const StyledCard = styled(Card)`
+  margin: 10px 0;
+  background-color: #fff8e1 !important;
+`
+const StyledCardActions = styled(CardActions)`
+  justify-content: space-between;
+  cursor: pointer;
+  height: auto !important;
+`
+const CardActionIconButton = styled(IconButton)`
+  transform: ${props => (props['data-expanded'] ? 'rotate(180deg)' : 'none')};
+`
+const CardActionTitle = styled.div`
+  padding-left: 8px;
+  font-weight: bold;
+  word-break: break-word;
+`
+const StyledCardContent = styled(CardContent)`
+  margin: -15px 0 0 0;
+`
+
+const enhance = compose(withState('expanded', 'setExpanded', false))
+
+const Tipps = ({
+  expanded,
+  setExpanded,
+}: {
+  expanded: Boolean,
+  setExpanded: () => void,
+}) => (
+  <StyledCard>
+    <StyledCardActions
+      disableActionSpacing
+      onClick={() => setExpanded(!expanded)}
+    >
+      <CardActionTitle>Tipps und Tricks</CardActionTitle>
+      <CardActionIconButton
+        data-expanded={expanded}
+        aria-expanded={expanded}
+        aria-label="öffnen"
+      >
+        <Icon title={expanded ? 'schliessen' : 'öffnen'}>
+          <ExpandMoreIcon />
+        </Icon>
+      </CardActionIconButton>
+    </StyledCardActions>
+    <Collapse in={expanded} timeout="auto" unmountOnExit>
+      <StyledCardContent>
+        <WasIstCsv />
+        <CsvOeffnen />
+        <CsvProgramm />
+        <DatenChaotisch />
+        <ZuVieleDaten />
+      </StyledCardContent>
+    </Collapse>
+  </StyledCard>
+)
+
+export default enhance(Tipps)
