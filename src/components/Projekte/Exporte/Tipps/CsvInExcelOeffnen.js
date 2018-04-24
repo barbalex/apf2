a2c8037abb3fed39b331503c31c2a7593a54ab62
Alexander Gabriel <alex.barbalex@gmail.com>
2018-04-24 18:08:05 +0200

    use m v1

---
+++ src/components/Projekte/Exporte/Tipps/CsvInExcelOeffnen.js
@@ -0,0 +1,100 @@
+// @flow
+import React from 'react'
+import Card, { CardActions, CardContent } from 'material-ui-next/Card'
+import Collapse from 'material-ui-next/transitions/Collapse'
+import IconButton from 'material-ui-next/IconButton'
+import Icon from 'material-ui-next/Icon'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import compose from 'recompose/compose'
+import withState from 'recompose/withState'
+import styled from 'styled-components'
+
+const StyledCard = styled(Card)`
+  margin: 10px 0;
+  background-color: #fff8e1 !important;
+`
+const StyledCardActions = styled(CardActions)`
+  justify-content: space-between;
+  cursor: pointer;
+  height: auto !important;
+`
+const CardActionIconButton = styled(IconButton)`
+  transform: ${props => (props['data-expanded'] ? 'rotate(180deg)' : 'none')};
+`
+const CardActionTitle = styled.div`
+  padding-left: 8px;
+  font-weight: bold;
+  word-break: break-word;
+`
+const StyledCardContent = styled(CardContent)`
+  margin: -15px 0 0 0;
+  ol {
+    -webkit-padding-start: 16px;
+  }
+  li {
+    margin-top: 4px;
+  }
+`
+
+const enhance = compose(withState('expanded', 'setExpanded', false))
+
+const CsvInExcelOeffnen = ({
+  expanded,
+  setExpanded,
+}: {
+  expanded: Boolean,
+  setExpanded: () => void,
+}) => (
+  <StyledCard>
+    <StyledCardActions
+      disableActionSpacing
+      onClick={() => setExpanded(!expanded)}
+    >
+      <CardActionTitle>
+        Sie wollen die .csv-Datei direkt in Excel öffnen? Das wird nicht
+        empfohlen, aber hier erfahren Sie, wie es funktionieren kann:
+      </CardActionTitle>
+      <CardActionIconButton
+        data-expanded={expanded}
+        aria-expanded={expanded}
+        aria-label="öffnen"
+      >
+        <Icon title={expanded ? 'schliessen' : 'öffnen'}>
+          <ExpandMoreIcon />
+        </Icon>
+      </CardActionIconButton>
+    </StyledCardActions>
+    <Collapse in={expanded} timeout="auto" unmountOnExit>
+      <StyledCardContent>
+        <ol>
+          <li>Excel öffnen</li>
+          <li>{'"Daten" > "Externe Daten abrufen" > "Aus Text" wählen'}</li>
+          <li>
+            {'Nun erscheit der Textkonvertierungs-Assistent.'}
+            <br />
+            {
+              'Im Schritt 1 als Dateiursprung statt dem vorgegebenen "Windows (ANSI)" dies hier wählen: "65001 : Unicode (UTF-8)". Excel versteht sonst partout keine Umlaute.'
+            }
+          </li>
+          <li>
+            {'Vorsicht: Excel vermanscht regelmässig importierte Daten!'}
+            <br />
+            {
+              'Beim Importieren interpretiert es nämlich in jedem Feld die obersten paar Datensätze - und bestimmt einen Datentyp, ohne Sie zu fragen.'
+            }
+            <br />
+            {
+              'Auch wenn weiter unten in vielen Datensätzen die Daten bei der nun nötigen Umwandlung in diesen Datentyp in diesem Feld dadurch verändert oder gelöscht werden, weil sie nicht diesem Datentyp entsprechen.'
+            }
+            <br />
+            {
+              'Daher bitte Excel nur für die Auswertung von Daten benutzten - nicht um .csv-Dateien zu öffnen.'
+            }
+          </li>
+        </ol>
+      </StyledCardContent>
+    </Collapse>
+  </StyledCard>
+)
+
+export default enhance(CsvInExcelOeffnen)
