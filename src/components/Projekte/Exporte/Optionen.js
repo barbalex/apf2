caf0006440cce6a148cac482f77bff3b4ff6ad25
Alexander Gabriel <alex.barbalex@gmail.com>
2018-04-25 10:31:23 +0200

    use m v1

--- src/components/Projekte/Exporte/Optionen.js
+++ src/components/Projekte/Exporte/Optionen.js
@@ -1,18 +1,41 @@
 // @flow
 import React from 'react'
-import { Card, CardHeader, CardText } from 'material-ui/Card'
+import { observer, inject } from 'mobx-react'
+import Card, { CardActions, CardContent } from 'material-ui-next/Card'
+import Collapse from 'material-ui-next/transitions/Collapse'
+import IconButton from 'material-ui-next/IconButton'
+import Icon from 'material-ui-next/Icon'
 import { FormControlLabel } from 'material-ui-next/Form'
 import Checkbox from 'material-ui-next/Checkbox'
-import { observer, inject } from 'mobx-react'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import compose from 'recompose/compose'
+import withState from 'recompose/withState'
 import styled from 'styled-components'
 
-const FirstLevelCard = styled(Card)`
-  margin-bottom: 10px;
+const StyledCard = styled(Card)`
+  margin: 10px 0;
   background-color: #fff8e1 !important;
 `
-const StyledCardText = styled(CardText)`
-  padding-top: 5px !important;
+const StyledCardActions = styled(CardActions)`
+  justify-content: space-between;
+  cursor: pointer;
+  height: auto !important;
+`
+const CardActionIconButton = styled(IconButton)`
+  transform: ${props => (props['data-expanded'] ? 'rotate(180deg)' : 'none')};
+`
+const CardActionTitle = styled.div`
+  padding-left: 8px;
+  font-weight: bold;
+  word-break: break-word;
+`
+const StyledCardContent = styled(CardContent)`
+  margin: -15px 0 0 0;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: stretch;
+  justify-content: stretch;
+  align-content: stretch;
 `
 const StyledFormControlLabel = styled(FormControlLabel)`
   margin-left: 0 !important;
@@ -22,41 +45,71 @@ const StyledCheckbox = styled(Checkbox)`
   height: 30px !important;
 `
 
-const enhance = compose(inject('store'), observer)
+const enhance = compose(
+  inject('store'),
+  withState('expanded', 'setExpanded', true),
+  observer
+)
 
-const Optionen = ({ store }: { store: Object }) => (
-  <FirstLevelCard initiallyExpanded>
-    <CardHeader title="Optionen" actAsExpander showExpandableButton />
-    <StyledCardText expandable>
-      <StyledFormControlLabel
-        control={
-          <StyledCheckbox
-            checked={store.export.fileType === 'csv'}
-            onChange={store.export.toggleFileType}
-            value={store.export.fileType}
-            color="primary"
-          />
-        }
-        label="Dateien im .csv-Format exportieren (Standard ist das xlsx-Format von Excel)"
-      />
-      <StyledFormControlLabel
-        control={
-          <StyledCheckbox
-            checked={store.export.applyMapFilterToExport}
-            onChange={store.export.toggleApplyMapFilterToExport}
-            value={store.export.applyMapFilterToExport}
-            color="primary"
-          />
-        }
-        label={
-          store.map.mapFilter.filter.features.length > 0
-            ? 'Karten-Filter anwenden'
-            : 'Karten-Filter anwenden (verfügbar, wenn ein Karten-Filter erstellt wurde)'
-        }
-        disabled={!(store.map.mapFilter.filter.features.length > 0)}
-      />
-    </StyledCardText>
-  </FirstLevelCard>
+const Optionen = ({
+  store,
+  expanded,
+  setExpanded,
+  downloadFromView,
+}: {
+  store: Object,
+  expanded: Boolean,
+  setExpanded: () => void,
+  downloadFromView: () => void,
+}) => (
+  <StyledCard>
+    <StyledCardActions
+      disableActionSpacing
+      onClick={() => setExpanded(!expanded)}
+    >
+      <CardActionTitle>Optionen</CardActionTitle>
+      <CardActionIconButton
+        data-expanded={expanded}
+        aria-expanded={expanded}
+        aria-label="öffnen"
+      >
+        <Icon title={expanded ? 'schliessen' : 'öffnen'}>
+          <ExpandMoreIcon />
+        </Icon>
+      </CardActionIconButton>
+    </StyledCardActions>
+    <Collapse in={expanded} timeout="auto" unmountOnExit>
+      <StyledCardContent>
+        <StyledFormControlLabel
+          control={
+            <StyledCheckbox
+              checked={store.export.fileType === 'csv'}
+              onChange={store.export.toggleFileType}
+              value={store.export.fileType}
+              color="primary"
+            />
+          }
+          label="Dateien im .csv-Format exportieren (Standard ist das xlsx-Format von Excel)"
+        />
+        <StyledFormControlLabel
+          control={
+            <StyledCheckbox
+              checked={store.export.applyMapFilterToExport}
+              onChange={store.export.toggleApplyMapFilterToExport}
+              value={store.export.applyMapFilterToExport}
+              color="primary"
+            />
+          }
+          label={
+            store.map.mapFilter.filter.features.length > 0
+              ? 'Karten-Filter anwenden'
+              : 'Karten-Filter anwenden (verfügbar, wenn ein Karten-Filter erstellt wurde)'
+          }
+          disabled={!(store.map.mapFilter.filter.features.length > 0)}
+        />
+      </StyledCardContent>
+    </Collapse>
+  </StyledCard>
 )
 
 export default enhance(Optionen)
