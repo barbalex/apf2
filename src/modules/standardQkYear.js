df742d36739e60dc4772947bed0e183579b05bb9
Alexander Gabriel <alex.barbalex@gmail.com>
2018-04-25 13:46:23 +0200

    clean

--- src/modules/standardQkYear.js
+++ src/modules/standardQkYear.js
@@ -1,9 +1,7 @@
 // @flow
-//import dateFns from 'date-fns'
 
 export default () => {
   const refDate = new Date()
   refDate.setMonth(refDate.getMonth() - 6)
-  // return parseInt(dateFns.format(refDate, 'YYYY'), 10)
   return refDate.getFullYear()
 }
