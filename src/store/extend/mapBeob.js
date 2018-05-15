b787a5936509f039f7f39a53c63d9593529e35f9
Alexander Gabriel <alex.barbalex@gmail.com>
2018-05-15 14:50:33 +0200

    remove unneeded

--- src/store/extend/mapBeob.js
+++ src/store/extend/mapBeob.js
@@ -1,18 +1,12 @@
 // @flow
 import { extendObservable, computed, action } from 'mobx'
 
-import getBeobMarkersClustered from '../action/getBeobMarkersClustered'
-import getBeobMarkers from '../action/getBeobMarkers'
 import getBeobForMap from '../action/getBeobForMap'
 
 export default (store: Object): void => {
   extendObservable(store.map.beob, {
     highlightedIds: [],
     beobs: computed(() => getBeobForMap(store), { name: 'mapBeobBeobs' }),
-    markersClustered: computed(() => getBeobMarkersClustered(store), {
-      name: 'mapBeobMarkersClustered',
-    }),
-    markers: computed(() => getBeobMarkers(store), { name: 'mapBeobMarkers' }),
     assigning: false,
     toggleAssigning: action(
       'toggleAssigning',
