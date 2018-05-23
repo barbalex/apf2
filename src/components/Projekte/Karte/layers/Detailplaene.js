85392d54474d37d5fdfee711507f28247f8c31d1
Alexander Gabriel <alex.barbalex@gmail.com>
2018-05-23 18:49:20 +0200

    revert

--- src/components/Projekte/Karte/layers/Detailplaene.js
+++ src/components/Projekte/Karte/layers/Detailplaene.js
@@ -1,11 +1,10 @@
 // @flow
 import React from 'react'
 import { GeoJSON } from 'react-leaflet'
-import axios from 'axios'
-import gql from 'graphql-tag'
+import { inject } from 'mobx-react'
+import { toJS } from 'mobx'
 
 import popupFromProperties from './popupFromProperties'
-import staticFilesBaseUrl from '../../../../modules/staticFilesBaseUrl'
 
 const style = () => ({ fill: false, color: 'red', weight: 1 })
 const onEachFeature = (feature, layer) => {
@@ -14,54 +13,13 @@ const onEachFeature = (feature, layer) => {
   }
 }
 
-const fetchDetailplaene = (client, treeName) => {
-  const baseURL = staticFilesBaseUrl
-    const url = `/detailplaeneWgs84neu.json`
-    axios
-      .get(url, { baseURL })
-      .then(({ data }) => {
-        console.log('Detailplaene, data fetched:', data)
-        client.mutate({
-          mutation: gql`
-            mutation setTreeMapKey($value: String!, $tree: String!, $key: String!) {
-              setTreeMapKey(tree: $tree, key: $key, value: $value) @client {
-                tree @client {
-                  map {
-                    detailplaene @client
-                    __typename: Map
-                  }
-                  __typename: Tree
-                }
-                tree2 @client {
-                  map {
-                    detailplaene @client
-                    __typename: Map
-                  }
-                  __typename: Tree2
-                }
-              }
-            }
-          `,
-          variables: {
-            value: data,
-            tree: treeName,
-            key: 'detailplaene'
-          }
-        })
-      })
-}
+const DetailplaeneLayer = ({ store }) => {
+  const data = toJS(store.map.detailplaene)
+  !data && store.map.fetchDetailplaene()
 
-const DetailplaeneLayer = ({ client, treeName, detailplaene }) => {
-  !detailplaene && fetchDetailplaene(client, treeName)
-
-  if (!detailplaene) return null
   return (
-    <GeoJSON
-      data={detailplaene}
-      style={style}
-      onEachFeature={onEachFeature}
-    />
+    data && <GeoJSON data={data} style={style} onEachFeature={onEachFeature} />
   )
 }
 
-export default DetailplaeneLayer
+export default inject('store')(DetailplaeneLayer)
