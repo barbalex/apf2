bc9f76e8e77d8698439fb5351d75320c95418a5b
Alexander Gabriel <alex.barbalex@gmail.com>
2018-05-26 00:22:40 +0200

    modularize

--- src/components/Projekte/Daten/Beobzuordnung/index.js
+++ src/components/Projekte/Daten/Beobzuordnung/index.js
@@ -18,6 +18,7 @@ import ErrorBoundary from '../../../shared/ErrorBoundary'
 import dataGql from './data.graphql'
 import updateBeobByIdGql from './updateBeobById.graphql'
 import setTreeKeyGql from './setTreeKey.graphql'
+import saveNichtZuordnenToDb from './saveNichtZuordnenToDb'
 
 const Container = styled.div`
   height: 100%;
@@ -177,44 +178,16 @@ const Beobzuordnung = ({
                       key={`${row.id}nichtZuordnen`}
                       label="Nicht zuordnen"
                       value={row.nichtZuordnen}
-                      saveToDb={async value => {
-                        const variables = {
+                      saveToDb={value => saveNichtZuordnenToDb({
+                          value,
                           id,
-                          nichtZuordnen: value,
-                        }
-                        // if true, empty tpopId
-                        if (value) variables.tpopId = null
-                        updateBeob({ variables })
-                        // need to update activeNodeArray and openNodes
-                        const { activeNodeArray, openNodes } = tree
-                        let newActiveNodeArray = clone(activeNodeArray)
-                        newActiveNodeArray[4] = value
-                          ? 'nicht-zuzuordnende-Beobachtungen'
-                          : 'nicht-beurteilte-Beobachtungen'
-                        newActiveNodeArray[5] = id
-                        newActiveNodeArray = newActiveNodeArray.slice(0, 6)
-                        const oldParentNodeUrl = clone(activeNodeArray)
-                        oldParentNodeUrl.pop()
-                        const newParentNodeUrl = clone(newActiveNodeArray)
-                        newParentNodeUrl.pop()
-                        const newOpenNodes = openNodes.map(n => {
-                          if (isEqual(n, activeNodeArray)) return newActiveNodeArray
-                          if (isEqual(n, oldParentNodeUrl)) return newParentNodeUrl
-                          return n
+                          updateBeob,
+                          tree,
+                          client,
+                          refetch,
+                          refetchTree
                         })
-                        await client.mutate({
-                          mutation: setTreeKeyGql,
-                          variables: {
-                            tree: tree.name,
-                            value1: newActiveNodeArray,
-                            key1: 'activeNodeArray',
-                            value2: newOpenNodes,
-                            key2: 'openNodes'
-                          }
-                        })
-                        refetch()
-                        refetchTree()
-                      }}
+                      }
                       popover={nichtZuordnenPopover}
                     />
                     <ZuordnenDiv>
---
+++ src/components/Projekte/Daten/Beobzuordnung/saveNichtZuordnenToDb.js
@@ -0,0 +1,44 @@
+//@flow
+import clone from 'lodash/clone'
+import isEqual from 'lodash/isEqual'
+
+import setTreeKeyGql from './setTreeKey.graphql'
+
+export default async ({ value, id, updateBeob, tree, client, refetch, refetchTree }) => {
+  const variables = {
+    id,
+    nichtZuordnen: value,
+  }
+  // if true, empty tpopId
+  if (value) variables.tpopId = null
+  updateBeob({ variables })
+  // need to update activeNodeArray and openNodes
+  const { activeNodeArray, openNodes } = tree
+  let newActiveNodeArray = clone(activeNodeArray)
+  newActiveNodeArray[4] = value
+    ? 'nicht-zuzuordnende-Beobachtungen'
+    : 'nicht-beurteilte-Beobachtungen'
+  newActiveNodeArray[5] = id
+  newActiveNodeArray = newActiveNodeArray.slice(0, 6)
+  const oldParentNodeUrl = clone(activeNodeArray)
+  oldParentNodeUrl.pop()
+  const newParentNodeUrl = clone(newActiveNodeArray)
+  newParentNodeUrl.pop()
+  const newOpenNodes = openNodes.map(n => {
+    if (isEqual(n, activeNodeArray)) return newActiveNodeArray
+    if (isEqual(n, oldParentNodeUrl)) return newParentNodeUrl
+    return n
+  })
+  await client.mutate({
+    mutation: setTreeKeyGql,
+    variables: {
+      tree: tree.name,
+      value1: newActiveNodeArray,
+      key1: 'activeNodeArray',
+      value2: newOpenNodes,
+      key2: 'openNodes'
+    }
+  })
+  refetch()
+  refetchTree()
+}
