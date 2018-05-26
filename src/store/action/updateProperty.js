d5541c5d77912b0496ac9190ae6796dd1505b60f
Alexander Gabriel <alex.barbalex@gmail.com>
2018-05-26 12:01:42 +0200

    remmove old store parts

--- src/store/action/updateProperty.js
+++ src/store/action/updateProperty.js
@@ -6,18 +6,5 @@ export default (
   key: String,
   valuePassed: String | Number
 ): void => {
-  /*
-  const { table, row } = tree.activeDataset
-  let value = valuePassed
-  // ensure primary data exists
-  if (!key || !table || !row) {
-    return store.listError(
-      new Error(
-        `change was not saved as one or more of the following values were not passed:
-        field: "${key}", table: "${table}", value: "${value}"`
-      )
-    )
-  }
-  row[key] = value
-  */
+  
 }
--- src/store/action/updatePropertyInDb.js
+++ src/store/action/updatePropertyInDb.js
@@ -1,12 +1,4 @@
 // @flow
-/*
-import axios from 'axios'
-import objectValues from 'lodash/values'
-import clone from 'lodash/clone'
-import { toJS } from 'mobx'
-
-import tables from '../../modules/tables'
-*/
 
 export default async (
   store: Object,
@@ -14,65 +6,5 @@ export default async (
   key: String,
   valuePassed: String | Number
 ): any => {
-  /*
-  const { row, valid } = tree.activeDataset
-  let value = valuePassed
-  let table = tree.activeDataset.table
-
-  // ensure primary data exists
-  if (!key || !table || !row) {
-    return
-  }
-
-  // convert undefined and '' to null
-  if (value === undefined || value === '') {
-    value = null
-  }
-
-  // ensure derived data exists
-  const tabelle: {
-    table: string,
-    dbTable: ?string,
-    idField: string,
-  } = tables.find(t => t.table === table)
-  // in tpopfeldkontr and tpopfreiwkontr need to find dbTable
-  if (tabelle.dbTable) {
-    table = tabelle.dbTable
-  }
-  const idField = tabelle ? tabelle.idField : undefined
-  if (!idField) {
-    return store.listError(
-      new Error(
-        `change was not saved:
-        field: ${key}, table: ${table}, value: ${value}
-        Reason: idField was not found`
-      )
-    )
-  }
-  const tabelleId = row[idField]
-  if (!tabelleId && tabelleId !== 0) {
-    return store.listError(
-      new Error(
-        `change was not saved:
-        field: ${key}, table: ${table}, value: ${value}
-        Reason: tabelleId was not found`
-      )
-    )
-  }
-
-  // update if no validation messages exist
-  const combinedValidationMessages = objectValues(valid).join('')
-  if (combinedValidationMessages.length === 0) {
-    const oldValue = row[key]
-    row[key] = value
-    const url = `/${table}?${idField}=eq.${tabelleId}`
-    try {
-      await axios.patch(url, { [key]: value })
-    } catch (error) {
-      // revert change in store
-      row[key] = oldValue
-      store.listError(error)
-    }
-  }
-  */
+  
 }
