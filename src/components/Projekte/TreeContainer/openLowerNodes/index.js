22bf645db26e192010410b948acec89b296339e4
Alexander Gabriel <alex.barbalex@gmail.com>
2018-05-28 18:49:33 +0200

    use gql

--- src/components/Projekte/TreeContainer/openLowerNodes/index.js
+++ src/components/Projekte/TreeContainer/openLowerNodes/index.js
@@ -1,39 +1,6 @@
 // @flow
 import isEqual from 'lodash/isEqual'
 
-import isNodeOpen from '../isNodeOpen'
-import openNode from '../openNode'
-
-const openLowerNodes = ({
-  tree,
-  node,
-  nodes
-}: {
-  tree: Object,
-  node: Object,
-  nodes: Array < Object >
-}) => {
-  /**
-   * 1. load all data
-   * 2. build openNodes for all data
-   * 3. activeNodeArray stays same
-   * 4. refresh tree
-   */
-
-
-
-  const lowerNodes = nodes.filter(n => {
-    const levelNeeded = n.url.length === node.url.length + 1
-    if (!levelNeeded) return false
-    const activeNodeArrayPartWithEqualLength = n.url.slice(0, node.url.length)
-    return isEqual(activeNodeArrayPartWithEqualLength, node.url)
-  })
-  lowerNodes.forEach(lowerNode => openNode({
-    tree,
-    node: lowerNode
-  }))
-}
-
 export default ({
   tree,
   id,
@@ -45,27 +12,35 @@ export default ({
   menuType: String,
   nodes: Array < Object >
 }) => {
-  const node = nodes.find(
-    n => n.id === id && n.menuType === menuType
-  )
-  // if node is not open, toggle it
-  if (!isNodeOpen(tree.openNodes, node.url)) {
-    openNode({
-      tree,
-      node,
-      nodes
-    })
-    // wait a while for node to load
-    setTimeout(() => openLowerNodes({
-      tree,
-      node,
-      nodes
-    }), 1000)
-  } else {
-    openLowerNodes({
-      tree,
-      node,
-      nodes
-    })
+   /**
+   * 1. load all data
+   * 2. build openNodes for all data using setOpenNodesFromActiveNodeArray
+   * 3. add these nodes to existing openNodes
+   * 4. make sure every nodeArray is unique in openNodes
+   * 5. activeNodeArray stays same
+   * 6. refresh tree
+   */
+  switch (menuType) {
+    case 'popFolder':
+      // TODO
+      break;
+    case 'pop':
+      // TODO
+      break;
+    case 'tpopFolder':
+      // TODO
+      break;
+    case 'tpop':
+      // TODO
+      break;
+    case 'tpopfeldkontrFolder':
+      // TODO
+      break;
+    case 'tpopfreiwkontrFolder':
+      // TODO
+      break;
+    default:
+      // do nothing
+      break;
   }
 }
