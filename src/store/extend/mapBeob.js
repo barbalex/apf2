afe96d532bb3faa95ab9fbc9b6c7ae94657c4f66
Alexander Gabriel <alex.barbalex@gmail.com>
2018-06-03 15:33:29 +0200

    refactor map store

--- src/store/extend/mapBeob.js
+++ src/store/extend/mapBeob.js
@@ -1,11 +1,8 @@
 // @flow
-import { extendObservable, computed } from 'mobx'
-
-import getBeobForMap from '../action/getBeobForMap'
+import { extendObservable } from 'mobx'
 
 export default (store: Object): void => {
   extendObservable(store.map.beob, {
     highlightedIds: [],
-    beobs: computed(() => getBeobForMap(store), { name: 'mapBeobBeobs' }),
   })
 }
