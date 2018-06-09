850aadb4c5a6637a560963c7a0167c415b20d6d5
Alexander Gabriel <alex.barbalex@gmail.com>
2018-06-09 16:14:49 +0200

    only update if value changed

--- src/components/Projekte/Daten/Zielber/index.js
+++ src/components/Projekte/Daten/Zielber/index.js
@@ -28,7 +28,19 @@ const FieldsContainer = styled.div`
 const enhance = compose(
   withState('errors', 'setErrors', ({})),
   withHandlers({
-    saveToDb: ({ setErrors, errors }) => async ({ row, field, value, updateZielber }) => {
+    saveToDb: ({
+      setErrors,
+      errors,
+    }) => async ({
+      row,
+      field,
+      value,
+      updateZielber,
+    }) => {
+      /**
+       * only save if value changed
+       */
+      if (row[field] === value) return
       try {
         await updateZielber({
           variables: {
