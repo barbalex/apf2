32c64969f0255ee76c033601362c3c9e505f9d56
Alexander Gabriel <alex.barbalex@gmail.com>
2018-06-10 14:43:37 +0200

    modularize

--- src/components/Projekte/Exporte/Teilpopulationen/index.js
+++ src/components/Projekte/Exporte/Teilpopulationen/index.js
@@ -123,53 +123,7 @@ const Teilpopulationen = ({
                       setMessage('Export "Teilpopulationen" wird vorbereitet...')
                       try {
                         const { data } = await client.query({
-                          query: gql`
-                            query view {
-                              allVTpops {
-                                nodes {
-                                  apId
-                                  familie
-                                  artname
-                                  apBearbeitung
-                                  apStartJahr
-                                  apUmsetzung
-                                  popId
-                                  popNr
-                                  popName
-                                  popStatus
-                                  popBekanntSeit
-                                  popStatusUnklar
-                                  popStatusUnklarBegruendung
-                                  popX
-                                  popY
-                                  id
-                                  nr
-                                  gemeinde
-                                  flurname
-                                  status
-                                  bekanntSeit
-                                  statusUnklar
-                                  statusUnklarGrund
-                                  x
-                                  y
-                                  radius
-                                  hoehe
-                                  exposition
-                                  klima
-                                  neigung
-                                  beschreibung
-                                  katasterNr
-                                  apberRelevant
-                                  eigentuemer
-                                  kontakt
-                                  nutzungszone
-                                  bewirtschafter
-                                  bewirtschaftung
-                                  changed
-                                  changedBy
-                                }
-                              }
-                            }`
+                          query: await import('./allVTpops.graphql')
                         })
                         exportModule({
                           data: get(data, 'allVTpops.nodes', []),
