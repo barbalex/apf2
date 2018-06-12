3e976da5503c3518d9a71412dfa4970a07a3800a
Alexander Gabriel <alex.barbalex@gmail.com>
2018-06-12 14:08:24 +0200

    also pass projId

--- sql/apflora/functions/qPopOhnePopber.sql
+++ sql/apflora/functions/qPopOhnePopber.sql
@@ -1,5 +1,5 @@
-DROP FUNCTION IF EXISTS apflora.q_pop_ohne_popber(apid uuid, berichtjahr integer);
-CREATE OR REPLACE FUNCTION apflora.q_pop_ohne_popber(apid uuid, berichtjahr integer)
+DROP FUNCTION IF EXISTS apflora.q_pop_ohne_popber(projid uuid, apid uuid, berichtjahr integer);
+CREATE OR REPLACE FUNCTION apflora.q_pop_ohne_popber(projid uuid, apid uuid, berichtjahr integer)
   RETURNS setof apflora.q_pop_ohne_popber AS
   $$
   SELECT DISTINCT
@@ -18,12 +18,18 @@ CREATE OR REPLACE FUNCTION apflora.q_pop_ohne_popber(apid uuid, berichtjahr inte
         apflora.tpop.pop_id
       FROM
         apflora.tpop
+        inner join apflora.pop
+          inner join apflora.ap
+          on apflora.ap.id = apflora.pop.ap_id
+        on apflora.pop.id = apflora.tpop.pop_id
       WHERE
         apflora.tpop.apber_relevant = 1
+        and apflora.ap.id = $2
+        and apflora.ap.proj_id = $1
       GROUP BY
         apflora.tpop.pop_id
     )
-    AND apflora.pop.id IN (
+    and apflora.pop.id IN (
       -- 3. "Pop mit TPop mit verlangten TPopBer im Berichtjahr" ermitteln:
       SELECT DISTINCT
         apflora.tpop.pop_id
@@ -33,35 +39,58 @@ CREATE OR REPLACE FUNCTION apflora.q_pop_ohne_popber(apid uuid, berichtjahr inte
         apflora.tpop.id IN (
           -- 1. "TPop mit Ansiedlungen/Ansaaten vor dem Berichtjahr" ermitteln:
           SELECT DISTINCT
-          apflora.tpopmassn.tpop_id
+            apflora.tpopmassn.tpop_id
           FROM
             apflora.tpopmassn
+            inner join apflora.tpop
+              inner join apflora.pop
+                inner join apflora.ap
+                on apflora.ap.id = apflora.pop.ap_id
+              on apflora.pop.id = apflora.tpop.pop_id
+            on apflora.tpop.id = apflora.tpopmassn.tpop_id
           WHERE
             apflora.tpopmassn.typ in (1, 2, 3)
-            AND apflora.tpopmassn.jahr < $2
+            and apflora.tpopmassn.jahr < $3
+            and apflora.ap.id = $2
+            and apflora.ap.proj_id = $1
         )
-        AND apflora.tpop.id IN (
+        and apflora.tpop.id IN (
           -- 2. "TPop mit Kontrolle im Berichtjahr" ermitteln:
           SELECT DISTINCT
             apflora.tpopkontr.tpop_id
           FROM
             apflora.tpopkontr
+            inner join apflora.tpop
+              inner join apflora.pop
+                inner join apflora.ap
+                on apflora.ap.id = apflora.pop.ap_id
+              on apflora.pop.id = apflora.tpop.pop_id
+            on apflora.tpop.id = apflora.tpopkontr.tpop_id
           WHERE
             apflora.tpopkontr.typ NOT IN ('Zwischenziel', 'Ziel')
-            AND apflora.tpopkontr.jahr = $2
+            and apflora.tpopkontr.jahr = $3
+            and apflora.ap.id = $2
+            and apflora.ap.proj_id = $1
         )
     )
-    AND apflora.pop.id NOT IN (
+    and apflora.pop.id NOT IN (
       -- 4. "Pop mit PopBer im Berichtjahr" ermitteln:
       SELECT DISTINCT
         apflora.popber.pop_id
       FROM
         apflora.popber
+          inner join apflora.pop
+            inner join apflora.ap
+            on apflora.ap.id = apflora.pop.ap_id
+          on apflora.pop.id = apflora.popber.pop_id
       WHERE
-        apflora.popber.jahr = $2
+        apflora.popber.jahr = $3
+        and apflora.ap.id = $2
+        and apflora.ap.proj_id = $1
     )
-    AND apflora.pop.ap_id = $1
+    and apflora.pop.ap_id = $2
+    and apflora.ap.proj_id = $1
   $$
   LANGUAGE sql STABLE;
-ALTER FUNCTION apflora.q_pop_ohne_popber(apid uuid, berichtjahr integer)
+ALTER FUNCTION apflora.q_pop_ohne_popber(projid uuid, apid uuid, berichtjahr integer)
   OWNER TO postgres;
