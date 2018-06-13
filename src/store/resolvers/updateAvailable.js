8070f738706dde4cd3c77b795cbee80b3d1cb4a6
Alexander Gabriel <alex.barbalex@gmail.com>
2018-06-14 00:49:41 +0200

    log

--- src/store/resolvers/updateAvailable.js
+++ src/store/resolvers/updateAvailable.js
@@ -3,6 +3,7 @@
 export default {
   Mutation: {
     setUpdateAvailable: (_, { value }, { cache }) => {
+      console.log('resolvers: updateAvailable, value:', value)
       cache.writeData({
         data: {
           updateAvailable: value
