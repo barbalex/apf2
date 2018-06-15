5bebfdf6f1414aef472f834f8b7ff6d41b3e0f25
Alexander Gabriel <alex.barbalex@gmail.com>
2018-06-15 21:08:39 +0200

    explain setup

--- sql/apflora/createDatabase.sql
+++ sql/apflora/createDatabase.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE apflora encoding 'UTF8';
-CREATE EXTENSION pgcrypto;
-CREATE EXTENSION "uuid-ossp";
+CREATE EXTENSION if not exists pgcrypto;
+CREATE EXTENSION if not exists "uuid-ossp";
 
 -- create role reader in pgAdmin, then:
 create role apflora_reader;
@@ -14,7 +14,10 @@ grant anon to authenticator;
 --GRANT USAGE ON SCHEMA apflora TO apflora_reader;
 --GRANT SELECT ON ALL TABLES IN SCHEMA apflora TO apflora_reader;
 
+-- restore from backup, then:
+-- run these two once with real secret (see how_to.md)
 ALTER DATABASE apflora SET "app.jwt_secret" TO 'secret';
+ALTER USER "authenticator" WITH PASSWORD 'secret';
 
 -- restore from db.backup
 
