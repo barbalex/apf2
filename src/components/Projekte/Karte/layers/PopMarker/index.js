4988d15643c837e2c0cd6e7ff7a657d8b1a31291
Alexander Gabriel <alex.barbalex@gmail.com>
2018-06-18 17:05:06 +0200

    enliven pop marker

--- src/components/Projekte/Karte/layers/PopMarker/index.js
+++ src/components/Projekte/Karte/layers/PopMarker/index.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import { Query } from 'react-apollo'
 import get from 'lodash/get'
 
-import dataGql from './data.graphql'
 import buildMarkers from './buildMarkers'
 import PopMarkerCluster from './Cluster'
 
 const PmcComponent = ({
   tree,
+  data,
   activeNodes,
   apfloraLayers,
   activeApfloraLayers,
@@ -15,43 +14,34 @@ const PmcComponent = ({
   mapIdsFiltered,
 }:{
   tree: Object,
+  data: Object,
   activeNodes: Array<Object>,
   apfloraLayers: Array<Object>,
   activeApfloraLayers: Array<String>,
   popLabelUsingNr: Boolean,
   mapIdsFiltered: Array<String>,
-}) =>
-  <Query query={dataGql}
-    variables={{
-      apId: activeNodes.ap,
-      projId: activeNodes.projekt,
-    }}
-  >
-    {({ loading, error, data }) => {
-      if (error) return `Fehler: ${error.message}`
-
-      const popFilterString = get(tree, 'nodeLabelFilter.pop')
-      const pops = get(data, 'projektById.apsByProjId.nodes[0].popsByApId.nodes', [])
-        // filter them by nodeLabelFilter
-        .filter(p => {
-          if (!popFilterString) return true
-          return `${p.nr || '(keine Nr)'}: ${p.name || '(kein Name)'}`
-            .toLowerCase()
-            .includes(popFilterString.toLowerCase())
-        })
-      const popMarkers = buildMarkers({
-        pops,
-        activeNodes,
-        apfloraLayers,
-        activeApfloraLayers,
-        data,
-        popLabelUsingNr,
-        mapIdsFiltered,
-      })
-      return <PopMarkerCluster markers={popMarkers} />
+}) => {
+  const popFilterString = get(tree, 'nodeLabelFilter.pop')
+  const pops = get(data, 'popForMapMarkers.apsByProjId.nodes[0].popsByApId.nodes', [])
+    // filter them by nodeLabelFilter
+    .filter(p => {
+      if (!popFilterString) return true
+      return `${p.nr || '(keine Nr)'}: ${p.name || '(kein Name)'}`
+        .toLowerCase()
+        .includes(popFilterString.toLowerCase())
+    })
+  const popMarkers = buildMarkers({
+    pops,
+    activeNodes,
+    apfloraLayers,
+    activeApfloraLayers,
+    data,
+    popLabelUsingNr,
+    mapIdsFiltered,
+  })
+  return <PopMarkerCluster markers={popMarkers} />
     
-  }}
-</Query>
+}
 
 
 export default PmcComponent
