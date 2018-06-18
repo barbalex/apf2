4b68837378c3103a27b46b0a5726486735beb46d
Alexander Gabriel <alex.barbalex@gmail.com>
2018-06-18 16:44:43 +0200

    enliven beob nicht  beurteilt markers

--- src/components/Projekte/Karte/layers/BeobNichtBeurteilt/index.js
+++ src/components/Projekte/Karte/layers/BeobNichtBeurteilt/index.js
@@ -1,10 +1,8 @@
 import React from 'react'
-import { Query } from 'react-apollo'
 import get from 'lodash/get'
 import flatten from 'lodash/flatten'
 import format from 'date-fns/format'
 
-import dataGql from './data.graphql'
 import buildMarkers from './buildMarkers'
 import buildMarkersClustered from './buildMarkersClustered'
 import Marker from './Marker'
@@ -12,6 +10,7 @@ import MarkerCluster from './MarkerCluster'
 
 const BeobNichtBeurteiltMarker = ({
   tree,
+  data,
   activeNodes,
   apfloraLayers,
   clustered,
@@ -19,58 +18,48 @@ const BeobNichtBeurteiltMarker = ({
   mapIdsFiltered,
 } : {
   tree: Object,
+  data: Object,
   activeNodes: Array<Object>,
   apfloraLayers: Array<Object>,
   clustered: Boolean,
   refetchTree: () => void,
   mapIdsFiltered: Array<String>,
-}) =>
-  <Query query={dataGql}
-    variables={{
-      apId: activeNodes.ap,
-      projId: activeNodes.projekt,
-    }}
-  >
-    {({ loading, error, data, client }) => {
-      if (error) return `Fehler: ${error.message}`
+}) => {
+  const beobNichtBeurteiltFilterString = get(tree, 'nodeLabelFilter.beobNichtBeurteilt')
+  const aparts = get(data, 'beobNichtBeurteiltForMapMarkers.apsByProjId.nodes[0].apartsByApId.nodes', [])
+  const beobs = flatten(aparts.map(a => get(a, 'aeEigenschaftenByArtId.beobsByArtId.nodes', [])))
+    // filter them by nodeLabelFilter
+    .filter(el => {
+      if (!beobNichtBeurteiltFilterString) return true
+      const datum = el.datum ? format(el.datum, 'YYYY.MM.DD') : '(kein Datum)'
+      const autor = el.autor || '(kein Autor)'
+      const quelle = get(el, 'beobQuelleWerteByQuelleId.name', '')
+      return `${datum}: ${autor} (${quelle})`
+        .toLowerCase()
+        .includes(beobNichtBeurteiltFilterString.toLowerCase())
+    })
 
-      const beobNichtBeurteiltFilterString = get(tree, 'nodeLabelFilter.beobNichtBeurteilt')
-      const aparts = get(data, 'projektById.apsByProjId.nodes[0].apartsByApId.nodes', [])
-      const beobs = flatten(aparts.map(a => get(a, 'aeEigenschaftenByArtId.beobsByArtId.nodes', [])))
-        // filter them by nodeLabelFilter
-        .filter(el => {
-          if (!beobNichtBeurteiltFilterString) return true
-          const datum = el.datum ? format(el.datum, 'YYYY.MM.DD') : '(kein Datum)'
-          const autor = el.autor || '(kein Autor)'
-          const quelle = get(el, 'beobQuelleWerteByQuelleId.name', '')
-          return `${datum}: ${autor} (${quelle})`
-            .toLowerCase()
-            .includes(beobNichtBeurteiltFilterString.toLowerCase())
-        })
-
-      if (clustered) {
-        const markers = buildMarkersClustered({
-          beobs,
-          activeNodes,
-          apfloraLayers,
-          data,
-          mapIdsFiltered,
-        })
-        return <MarkerCluster markers={markers} />
-      }
-      const markers = buildMarkers({
-        beobs,
-        tree,
-        activeNodes,
-        apfloraLayers,
-        data,
-        mapIdsFiltered,
-        refetchTree,
-      })
-      return <Marker markers={markers} />
-    
-  }}
-</Query>
+  if (clustered) {
+    const markers = buildMarkersClustered({
+      beobs,
+      activeNodes,
+      apfloraLayers,
+      data,
+      mapIdsFiltered,
+    })
+    return <MarkerCluster markers={markers} />
+  }
+  const markers = buildMarkers({
+    beobs,
+    tree,
+    activeNodes,
+    apfloraLayers,
+    data,
+    mapIdsFiltered,
+    refetchTree,
+  })
+  return <Marker markers={markers} />
+}
 
 
 export default BeobNichtBeurteiltMarker
