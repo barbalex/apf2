f8c1b5380c0c3dcf07242f0b9d1138df5d57f7ef
Alexander Gabriel <alex.barbalex@gmail.com>
2018-07-08 00:47:39 +0200

    remove unused

--- sql/apflora/2017_12_01_apber_fields.sql
+++
@@ -1,5 +0,0 @@
-alter table apflora.apber
-add column "JBerCAktivApbearb" text,
-add column "JBerCVerglAusfPl" text;
-COMMENT ON COLUMN apflora.apber."JBerCAktivApbearb" IS 'Bemerkungen zum Aussagebereich C: Weitere Aktivitäten der Aktionsplan-Verantwortlichen';
-COMMENT ON COLUMN apflora.apber."JBerCVerglAusfPl" IS 'Bemerkungen zum Aussagebereich C: Vergleich Ausführung/Planung';
