93104354582d98c114f85786267457aba2be9b42
Alexander Gabriel <alex.barbalex@gmail.com>
2018-07-10 22:18:10 +0200

    work on print

--- src/components/Print/ApberForYear/ErfolgList.js
+++ src/components/Print/ApberForYear/ErfolgList.js
@@ -1,5 +1,5 @@
 // @flow
-import React from 'react'
+import React, { Fragment } from 'react'
 import styled from 'styled-components'
 import get from 'lodash/get'
 import flatten from 'lodash/flatten'
@@ -17,11 +17,12 @@ const Container = styled.div`
 `
 const Table = styled.div`
   display: grid;
-  grid-template-rows: 6cm | repeat(11, 1cm);
+  grid-template-columns: 9cm repeat(11, 1fr);
   grid-column-gap: 1px;
   grid-row-gap: 1px;
   justify-items: stretch;
   align-items: stretch;
+  justify-content: stretch;
 `
 const OverallTitle = styled.p`
   font-size: 12px;
@@ -35,9 +36,56 @@ const ApTitle = styled(Title)`
   grid-column: 1 / span 1;
   grid-row: 1 / span 2;
 `
+const Ap = styled.div`
+  grid-column: 1 / span 1;
+`
 const ErfolgSpanningTitle = styled(Title)`
   grid-column: 2 / span 7;
   grid-row: 1 / span 1;
+  justify-self: center;
+`
+const ErfolgNicht = styled.div`
+  grid-column: 2 / span 1;
+  justify-self: center;
+`
+const ErfolgWenig = styled.div`
+  grid-column: 3 / span 1;
+  justify-self: center;
+`
+const ErfolgMaessig = styled.div`
+  grid-column: 4 / span 1;
+  justify-self: center;
+`
+const ErfolgGut = styled.div`
+  grid-column: 5 / span 1;
+  justify-self: center;
+`
+const ErfolgSehr = styled.div`
+  grid-column: 6 / span 1;
+  justify-self: center;
+`
+const ErfolgVeraenderung = styled.div`
+  grid-column: 7 / span 1;
+  justify-self: center;
+`
+const ErfolgUnsicher = styled.div`
+  grid-column: 8 / span 1;
+  justify-self: center;
+`
+const ErfolgNichtBeurteilt = styled.div`
+  grid-column: 9 / span 1;
+  justify-self: center;
+`
+const KeineMassnahme = styled.div`
+  grid-column: 10 / span 1;
+  justify-self: center;
+`
+const KefArt = styled.div`
+  grid-column: 11 / span 1;
+  justify-self: center;
+`
+const KefKontrolle = styled.div`
+  grid-column: 12 / span 1;
   justify-self: center;
 `
 const KefSpanningTitle = styled(Title)`
@@ -110,25 +158,33 @@ const ErfolgList = ({
   jahr: Number
 }) => {
   const aps = get(data, 'projektById.apsByProjId.nodes', [])
-  const pops = flatten(
-    aps.map(ap => get(ap, 'popsByApId.nodes', []))
-  ).filter(p => get(p, 'tpopsByPopId.totalCount') > 0)
-  const popsUrspr = pops.filter(p => p.status === 100).length
-  const popsAnges = pops.filter(p => p.status === 200).length
-  const popsTotal = pops.filter(p => [100, 200].includes(p.status)).length
   const apRows = sortBy(
-      aps.map(ap => ({
-      ap: get(ap, 'aeEigenschaftenByArtId.artname'),
-      urspr: get(ap, 'popsByApId.nodes', [])
-        .filter(p => p.status === 100)
-        .length,
-      anges: get(ap, 'popsByApId.nodes', [])
-        .filter(p => p.status === 200)
-        .length,
-      total: get(ap, 'popsByApId.nodes', [])
-        .filter(p => [100, 200].includes(p.status))
-        .length
-    })),
+    aps.map(ap => {
+      const beurteilung = get(ap, 'apbersByApId.nodes[0].beurteilung')
+      const pops = get(ap, 'popsByApId.nodes', [])
+      const tpops = flatten(
+        pops.map(p => get(p, 'tpopsByPopId.nodes', []))
+      )
+      const anzMassn = flatten(
+        tpops.map(t => get(t, 'tpopmassnsByTpopId.nodes', []))
+      ).length
+      const kefKontrollJahr = get(ap, 'aeEigenschaftenByArtId.kefkontrolljahr')
+      const isKefKontrollJahr = !!kefKontrollJahr && window.Math.floor((jahr - kefKontrollJahr) / 4) === (jahr - kefKontrollJahr) / 4
+      return ({
+        ap: get(ap, 'aeEigenschaftenByArtId.artname'),
+        erfolgNicht: beurteilung === 3 ? 'X' : '',
+        erfolgWenig: beurteilung === 6 ? 'X' : '',
+        erfolgMaessig: beurteilung === 5 ? 'X' : '',
+        erfolgGut: beurteilung === 1 ? 'X' : '',
+        erfolgSehr: beurteilung === 4 ? 'X' : '',
+        erfolgUnsicher: beurteilung === 1168274204 ? 'X' : '',
+        nichtBeurteilt: ![1, 3, 4, 5, 6, 1168274204].includes(beurteilung) ? 'X' : '',
+        veraenderung: get(ap, 'apbersByApId.nodes[0].veraenderungZumVorjahr', ''),
+        keineMassnahme: anzMassn === 0 ? 'X' : '',
+        kefArt: !!get(ap, 'aeEigenschaftenByArtId.kefart') ? 'X' : '',
+        kefKontrolle: isKefKontrollJahr ? 'X' : ''
+      })
+    }),
     'ap'
   )
 
@@ -151,6 +207,24 @@ const ErfolgList = ({
           <ErfolgNichtBeurteiltTitle>nicht beurteilt</ErfolgNichtBeurteiltTitle>
           <KefArtTitle>Art</KefArtTitle>
           <KefKontrolleTitle>Kontrolle</KefKontrolleTitle>
+          {
+            apRows.map(row =>
+              <Fragment key={row.ap}>
+                <Ap>{row.ap}</Ap>
+                <ErfolgNicht>{row.erfolgNicht}</ErfolgNicht>
+                <ErfolgWenig>{row.erfolgWenig}</ErfolgWenig>
+                <ErfolgMaessig>{row.erfolgMaessig}</ErfolgMaessig>
+                <ErfolgGut>{row.erfolgGut}</ErfolgGut>
+                <ErfolgSehr>{row.erfolgSehr}</ErfolgSehr>
+                <ErfolgVeraenderung>{row.veraenderung}</ErfolgVeraenderung>
+                <ErfolgUnsicher>{row.erfolgUnsicher}</ErfolgUnsicher>
+                <ErfolgNichtBeurteilt>{row.nichtBeurteilt}</ErfolgNichtBeurteilt>
+                <KeineMassnahme>{row.keineMassnahme}</KeineMassnahme>
+                <KefArt>{row.kefArt}</KefArt>
+                <KefKontrolle>{row.kefKontrolle}</KefKontrolle>
+              </Fragment>
+            )
+          }
         </Table>
       </Container>
     </ErrorBoundary>
