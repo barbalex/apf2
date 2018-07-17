cf109407042479b4dbe5ad7214e45856cfb14a69
Alexander Gabriel <alex.barbalex@gmail.com>
2018-07-17 12:47:08 +0200

    modularize

---
+++ src/components/Projekte/Daten/Tpopfreiwkontr/Map.js
@@ -0,0 +1,109 @@
+// @flow
+import React from 'react'
+import styled from 'styled-components'
+import get from 'lodash/get'
+
+import RadioButton from '../../../shared/RadioButton'
+
+const Area = styled.div`
+  border: 1px solid rgba(0, 0, 0, 0.5);
+  border-radius: 6px;
+  padding: 10px;
+`
+const Container = styled(Area)`
+  grid-area: map;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-areas:
+    'mapLabel0 mapLabel1 mapVal1'
+    'mapLabel0 mapLabel2 mapVal2';
+  align-items: center;
+`
+const Label = styled.div`
+  font-weight: 700;
+  padding-right: 4px;
+`
+const MapLabel0 = styled(Label)`
+  grid-area: mapLabel0;
+  padding-right: 15px;
+`
+const MapLabel1 = styled(Label)`
+  grid-area: mapLabel1;
+`
+const MapLabel2 = styled(Label)`
+  grid-area: mapLabel2;
+`
+const MapVal1 = styled(Label)`
+  grid-area: mapVal1;
+  > fieldset {
+    margin-top: -5px;
+    padding-bottom: 0 !important;
+  }
+  > fieldset > legend {
+    padding-top: 0 !important;
+  }
+`
+const MapVal2 = styled(Label)`
+  grid-area: mapVal2;
+  > fieldset {
+    margin-top: -5px;
+    padding-bottom: 0 !important;
+  }
+  > fieldset > legend {
+    padding-top: 0 !important;
+  }
+`
+
+const Date = ({
+  saveToDb,
+  errors,
+  data,
+  updateTpopkontr,
+}: {
+  saveToDb: () => void,
+  errors: Object,
+  data: Object,
+  updateTpopkontr: () => void,
+}) => {
+  const row = get(data, 'tpopkontrById')
+
+  return (
+    <Container>
+      <MapLabel0>Plan ergänzt</MapLabel0>
+      <MapLabel1>ja</MapLabel1>
+      <MapVal1>
+        <RadioButton
+          key={`${row.id}planVorhanden`}
+          value={row.planVorhanden}
+          saveToDb={value =>
+            saveToDb({
+              row,
+              field: 'planVorhanden',
+              value,
+              updateTpopkontr,
+            })
+          }
+          error={errors.planVorhanden}
+        />
+      </MapVal1>
+      <MapLabel2>nein</MapLabel2>
+      <MapVal2>
+        <RadioButton
+          key={`${row.id}planVorhanden`}
+          value={!row.planVorhanden}
+          saveToDb={value =>
+            saveToDb({
+              row,
+              field: 'planVorhanden',
+              value: !value,
+              updateTpopkontr,
+            })
+          }
+          error={errors.planVorhanden}
+        />
+      </MapVal2>
+    </Container>
+  )
+}
+
+export default Date
--- src/components/Projekte/Daten/Tpopfreiwkontr/index.js
+++ src/components/Projekte/Daten/Tpopfreiwkontr/index.js
@@ -18,6 +18,7 @@ import veghoeheImg from './veghoehe.png'
 import Headdata from './Headdata'
 import Besttime from './Besttime'
 import Date from './Date'
+import Map from './Map'
 
 const LadeContainer = styled.div`
   height: 100%;
@@ -68,45 +69,6 @@ const Image = styled(Area)`
 const Label = styled.div`
   font-weight: 700;
   padding-right: 4px;
-`
-const Map = styled(Area)`
-  grid-area: map;
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-template-areas:
-    'mapLabel0 mapLabel1 mapVal1'
-    'mapLabel0 mapLabel2 mapVal2';
-  align-items: center;
-`
-const MapLabel0 = styled(Label)`
-  grid-area: mapLabel0;
-  padding-right: 15px;
-`
-const MapLabel1 = styled(Label)`
-  grid-area: mapLabel1;
-`
-const MapLabel2 = styled(Label)`
-  grid-area: mapLabel2;
-`
-const MapVal1 = styled(Label)`
-  grid-area: mapVal1;
-  > fieldset {
-    margin-top: -5px;
-    padding-bottom: 0 !important;
-  }
-  > fieldset > legend {
-    padding-top: 0 !important;
-  }
-`
-const MapVal2 = styled(Label)`
-  grid-area: mapVal2;
-  > fieldset {
-    margin-top: -5px;
-    padding-bottom: 0 !important;
-  }
-  > fieldset > legend {
-    padding-top: 0 !important;
-  }
 `
 const Count1 = styled(Area)`
   grid-area: count1;
@@ -400,41 +362,12 @@ const Tpopfreiwkontr = ({
                   data={data}
                   updateTpopkontr={updateTpopkontr}
                 />
-                <Map>
-                  <MapLabel0>Plan ergänzt</MapLabel0>
-                  <MapLabel1>ja</MapLabel1>
-                  <MapVal1>
-                    <RadioButton
-                      key={`${row.id}planVorhanden`}
-                      value={row.planVorhanden}
-                      saveToDb={value =>
-                        saveToDb({
-                          row,
-                          field: 'planVorhanden',
-                          value,
-                          updateTpopkontr,
-                        })
-                      }
-                      error={errors.planVorhanden}
-                    />
-                  </MapVal1>
-                  <MapLabel2>nein</MapLabel2>
-                  <MapVal2>
-                    <RadioButton
-                      key={`${row.id}planVorhanden`}
-                      value={!row.planVorhanden}
-                      saveToDb={value =>
-                        saveToDb({
-                          row,
-                          field: 'planVorhanden',
-                          value: !value,
-                          updateTpopkontr,
-                        })
-                      }
-                      error={errors.planVorhanden}
-                    />
-                  </MapVal2>
-                </Map>
+                <Map
+                  saveToDb={saveToDb}
+                  errors={errors}
+                  data={data}
+                  updateTpopkontr={updateTpopkontr}
+                />
                 <Image>Image</Image>
                 <Count1>count1</Count1>
                 <Count2>count2</Count2>
