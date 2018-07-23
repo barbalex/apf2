a1add307108023ed4b494dacf3e750c3c4b94eda
Alexander Gabriel <alex.barbalex@gmail.com>
2018-07-23 11:48:47 +0200

    use .graphql file instead of gql

--- src/components/Projekte/Daten/Tpopfreiwkontr/data.js
+++ src/components/Projekte/Daten/Tpopfreiwkontr/data.js
@@ -1,90 +1,12 @@
 // @flow
 import { graphql } from 'react-apollo'
-import gql from 'graphql-tag'
 
-export default graphql(
-  gql`
-    query tpopkontrByIdQuery($id: UUID!) {
-      isPrint @client
-      tpopkontrById(id: $id) {
-        id
-        typ
-        ekfVerifiziert
-        datum
-        jahr
-        bemerkungen
-        flaecheUeberprueft
-        deckungVegetation
-        deckungNackterBoden
-        deckungApArt
-        vegetationshoeheMaximum
-        vegetationshoeheMittel
-        gefaehrdung
-        tpopId
-        bearbeiter
-        adresseByBearbeiter {
-          id
-          name
-        }
-        planVorhanden
-        jungpflanzenVorhanden
-        tpopByTpopId {
-          id
-          nr
-          flurname
-          x
-          y
-          status
-          popByPopId {
-            id
-            apId
-            nr
-            name
-            apByApId {
-              id
-              ekfzaehleinheitsByApId {
-                nodes {
-                  id
-                  tpopkontrzaehlEinheitWerteByZaehleinheitId {
-                    id
-                    code
-                    text
-                    sort
-                  }
-                }
-              }
-            }
-          }
-        }
-        tpopkontrzaehlsByTpopkontrId {
-          nodes {
-            id
-            anzahl
-            einheit
-          }
-        }
-      }
-      allTpopkontrzaehlEinheitWertes {
-        nodes {
-          id
-          code
-          text
-          sort
-        }
-      }
-      allAdresses {
-        nodes {
-          id
-          name
-        }
-      }
-    }
-  `,
-  {
-    options: ({ id }) => ({
-      variables: {
-        id,
-      },
-    }),
-  }
-)
+import dataGql from './data.graphql'
+
+export default graphql(dataGql, {
+  options: ({ id }) => ({
+    variables: {
+      id,
+    },
+  }),
+})
