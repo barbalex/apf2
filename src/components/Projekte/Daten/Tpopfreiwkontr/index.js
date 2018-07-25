32d4840782f3a377744e02b0328234749af07abd
Alexander Gabriel <alex.barbalex@gmail.com>
2018-07-25 19:06:09 +0200

    optimize message

--- src/components/Projekte/Daten/Tpopfreiwkontr/index.js
+++ src/components/Projekte/Daten/Tpopfreiwkontr/index.js
@@ -289,7 +289,7 @@ const enhance = compose(
       if (bearbeiter && !userCount && !errors.bearbeiter) {
         setErrors({
           bearbeiter:
-            'Es ist kein Benutzer mit dieser Adresse verbunden. Damit dieser Benutzer Kontrollen erfassen kann, muss er ein Benutzerkonto haben, in dem die soeben gewählte Adresse als zugehörig erfasst wurde.',
+            'Es ist kein Benutzer mit dieser Adresse verbunden. Damit dieser Benutzer Kontrollen erfassen kann, muss er ein Benutzerkonto haben, in dem die oben gwählte Adresse als zugehörig erfasst wurde.',
         })
       }
     },
