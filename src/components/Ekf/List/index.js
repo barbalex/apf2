a161fc2f0870378d6cd94a011562201866be7c6d
Alexander Gabriel <alex.barbalex@gmail.com>
2018-07-27 12:53:33 +0200

    style list

--- src/components/Ekf/List/index.js
+++ src/components/Ekf/List/index.js
@@ -3,13 +3,24 @@ import React from 'react'
 import { FixedSizeList as List } from 'react-window'
 import get from 'lodash/get'
 import uniq from 'lodash/uniq'
+import sortBy from 'lodash/sortBy'
 import styled from 'styled-components'
 
+const Container = styled.div`
+  height: 100%;
+  border-right: 1px solid rgb(46, 125, 50);
+`
 const OuterContainer = styled.div`
-  border-bottom: 1px solid;
+  border-bottom: 1px solid rgba(46, 125, 50, 0.5);
+  cursor: pointer;
+  &:hover {
+    background-color: rgb(255, 250, 198);
+    border-top: 1px solid rgba(46, 125, 50, 0.5);
+    margin-top: -1px;
+  }
 `
 const InnerContainer = styled.div`
-  height: 81px;
+  height: ${props => props.height}px;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -28,7 +39,7 @@ const EkfList = ({
   data: Object,
   dimensions: Object,
 }) => {
-  const ekf = get(
+  let ekf = get(
     data,
     'userByName.adresseByAdresseId.tpopkontrsByBearbeiter.nodes',
     []
@@ -48,33 +59,45 @@ const EkfList = ({
       'tpopByTpopId.popByPopId.name',
       '(kein Name)'
     )}`,
+    popSort: get(e, 'tpopByTpopId.popByPopId.nr', '(keine Nr)'),
     tpop: `${get(e, 'tpopByTpopId.nr', '(keine Nr)')}: ${get(
       e,
       'tpopByTpopId.flurname',
       '(kein Flurname)'
     )}`,
+    tpopSort: get(e, 'tpopByTpopId.nr', '(keine Nr)'),
   }))
+  ekf = sortBy(ekf, ['projekt', 'art', 'popSort', 'tpopSort'])
   const height = isNaN(dimensions.height) ? 250 : dimensions.height
-  const width = isNaN(dimensions.width) ? 250 : dimensions.width
+  const width = isNaN(dimensions.width) ? 250 : dimensions.width - 1
   const projektCount = uniq(ekf.map(e => e.projekt)).length
+  const itemSize = projektCount > 1 ? 110 : 91
+  const innerContainerHeight = projektCount > 1 ? 81 : 62
 
   return (
-    <List height={height} itemCount={ekf.length} itemSize={110} width={width}>
-      {({ index, style }) => {
-        const row = ekf[index]
+    <Container>
+      <List
+        height={height}
+        itemCount={ekf.length}
+        itemSize={itemSize}
+        width={width}
+      >
+        {({ index, style }) => {
+          const row = ekf[index]
 
-        return (
-          <OuterContainer style={style}>
-            <InnerContainer>
-              {projektCount > 1 && <div>{row.projekt}</div>}
-              <div>{row.art}</div>
-              <div>{row.pop}</div>
-              <div>{row.tpop}</div>
-            </InnerContainer>
-          </OuterContainer>
-        )
-      }}
-    </List>
+          return (
+            <OuterContainer style={style}>
+              <InnerContainer height={innerContainerHeight}>
+                {projektCount > 1 && <div>{row.projekt}</div>}
+                <div>{row.art}</div>
+                <div>{row.pop}</div>
+                <div>{row.tpop}</div>
+              </InnerContainer>
+            </OuterContainer>
+          )
+        }}
+      </List>
+    </Container>
   )
 }
 
