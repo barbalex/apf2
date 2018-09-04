b22c98cb75891149e24b108a8dc497e34d9336b5
Alexander Gabriel <alex.barbalex@gmail.com>
2018-09-04 13:02:58 +0200

    build hoc for state

---
+++ src/state/withErrorState.js
@@ -0,0 +1,18 @@
+/**
+ * inspired from:
+ * https://www.reddit.com/r/reactjs/comments/8t3di7/how_do_you_access_your_store_in_lifecycle/
+ * example: https://github.com/jaredpalmer/react-fns/blob/052ea56092c53946c7937e1d32adf78a7d37d6f3/src/GeoPosition/withGeoPosition.tsx
+ */
+// @flow
+import React from 'react'
+import { Subscribe } from 'unstated'
+
+import ErrorState from './Error'
+
+export function withErrorState(WrappedComponent) {
+  return props => (
+    <Subscribe to={[ErrorState]}>
+      {value => <WrappedComponent errorState={value} {...props} />}
+    </Subscribe>
+  )
+}
