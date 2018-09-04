820020cbd04ae93528ff77ddd039eeb0018c239a
Alexander Gabriel <alex.barbalex@gmail.com>
2018-09-05 00:08:17 +0200

    style

--- src/components/shared/FormTitle.js
+++ src/components/shared/FormTitle.js
@@ -2,11 +2,13 @@
 import React from 'react'
 import styled from 'styled-components'
 import FilterIcon from '@material-ui/icons/FilterList'
-import ListIcon from '@material-ui/icons/List'
+import EditIcon from '@material-ui/icons/Edit'
 import compose from 'recompose/compose'
+import withHandlers from 'recompose/withHandlers'
 
 import TestdataMessage from './TestdataMessage'
 import withNodeFilterState from '../../state/withNodeFilter'
+import { Icon } from '@material-ui/core'
 
 const Container = styled.div`
   background-color: #388e3c;
@@ -24,41 +26,57 @@ const Title = styled.div`
   color: white;
   font-weight: bold;
 `
+const IconDiv = styled.div``
 const StyledFilterIcon = styled(FilterIcon)`
   cursor: pointer;
   pointer-events: auto;
-  padding: 10px;
+  padding-top: 5px;
+  padding-right: 10px;
   padding-bottom: 0;
   color: white;
+  vertical-align: middle;
 `
-const StyledListIcon = styled(ListIcon)`
+const StyledEditIcon = styled(EditIcon)`
   cursor: pointer;
   pointer-events: auto;
-  padding: 10px;
+  padding-top: 5px;
+  padding-right: 10px;
   padding-bottom: 0;
   color: white;
+  vertical-align: middle;
 `
 
-const enhance = compose(withNodeFilterState)
+const enhance = compose(
+  withNodeFilterState,
+  withHandlers({
+    onToggleShow: ({ nodeFilterState }) => () => nodeFilterState.toggleShow(),
+  }),
+)
 
 const FormTitle = ({
   tree,
   title,
   apId,
   nodeFilterState,
+  onToggleShow,
 }: {
   tree: Object,
   title: String,
   apId: String,
   nodeFilterState: Object,
+  onToggleShow: () => void,
 }) => (
   <Container>
     <TitleRow>
       <Title>{title}</Title>
-      {nodeFilterState.show ? (
-        <StyledListIcon onClick={nodeFilterState.toggleShow} />
+      {nodeFilterState.state.show ? (
+        <IconDiv title="Daten anzeigen und bearbeiten">
+          <StyledEditIcon onClick={onToggleShow} />
+        </IconDiv>
       ) : (
-        <StyledFilterIcon onClick={nodeFilterState.toggleShow} />
+        <IconDiv title="Daten filtern (TODO)">
+          <StyledFilterIcon onClick={onToggleShow} />
+        </IconDiv>
       )}
     </TitleRow>
     <TestdataMessage tree={tree} apId={apId} />
