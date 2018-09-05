e385b944a4c215ad90763bf544592f23c559aa7f
Alexander Gabriel <alex.barbalex@gmail.com>
2018-09-05 19:21:05 +0200

    comment

--- src/components/shared/Loading.js
+++ src/components/shared/Loading.js
@@ -10,9 +10,13 @@ const LoadingComponent = ({ isLoading, loading, error }) => {
   if (isLoading || loading) {
     return <Container>Lade...</Container>
   } else if (error) {
+    // uncomment following when researching errors
+    // throw error
     return (
       <Container>
-        {`Entschuldigung, die Seite konnte nicht geladen werden. Fehler: ${error.message}`}
+        {`Entschuldigung, die Seite konnte nicht geladen werden. Fehler: ${
+          error.message
+        }`}
       </Container>
     )
   } else {
