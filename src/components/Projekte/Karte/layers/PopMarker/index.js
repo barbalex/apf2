fb6a517d457fa9178007ef0fc968622a1e0b98b9
Alexander Gabriel <alex.barbalex@gmail.com>
2018-09-07 16:50:19 +0200

    get map pops to follow tree pops

--- src/components/Projekte/Karte/layers/PopMarker/index.js
+++ src/components/Projekte/Karte/layers/PopMarker/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import get from 'lodash/get'
+import compose from 'recompose/compose'
 
 import buildMarkers from './buildMarkers'
 import PopMarkerCluster from './Cluster'
+import filterNodesByNodeFilterArray from '../../../TreeContainer/filterNodesByNodeFilterArray'
+import withNodeFilter from '../../../../../state/withNodeFilter'
+
+const enhance = compose(withNodeFilter)
 
 const PmcComponent = ({
   tree,
@@ -12,7 +17,8 @@ const PmcComponent = ({
   activeApfloraLayers,
   popLabelUsingNr,
   mapIdsFiltered,
-}:{
+  nodeFilterState,
+}: {
   tree: Object,
   data: Object,
   activeNodes: Array<Object>,
@@ -20,9 +26,17 @@ const PmcComponent = ({
   activeApfloraLayers: Array<String>,
   popLabelUsingNr: Boolean,
   mapIdsFiltered: Array<String>,
+  nodeFilterState: Object,
 }) => {
   const popFilterString = get(tree, 'nodeLabelFilter.pop')
-  const pops = get(data, 'popForMapMarkers.apsByProjId.nodes[0].popsByApId.nodes', [])
+  const nodeFilterArray = Object.entries(
+    nodeFilterState.state[tree.name].pop,
+  ).filter(([key, value]) => value || value === 0)
+  const pops = get(
+    data,
+    'popForMapMarkers.apsByProjId.nodes[0].popsByApId.nodes',
+    [],
+  )
     // filter them by nodeLabelFilter
     .filter(p => {
       if (!popFilterString) return true
@@ -30,6 +44,12 @@ const PmcComponent = ({
         .toLowerCase()
         .includes(popFilterString.toLowerCase())
     })
+    // filter by nodeFilter
+    // TODO: would be much better to filter this in query
+    // this is done
+    // but unfortunately query does not immediatly update
+    .filter(node => filterNodesByNodeFilterArray({ node, nodeFilterArray }))
+
   const popMarkers = buildMarkers({
     pops,
     activeNodes,
@@ -40,8 +60,6 @@ const PmcComponent = ({
     mapIdsFiltered,
   })
   return <PopMarkerCluster markers={popMarkers} />
-    
 }
 
-
-export default PmcComponent
+export default enhance(PmcComponent)
