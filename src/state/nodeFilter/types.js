f0558c90f0a8fd275271a3d4443ac37e1de0db35
Alexander Gabriel <alex.barbalex@gmail.com>
2018-09-10 21:22:04 +0200

    collect all types in object

---
+++ src/state/nodeFilter/types.js
@@ -0,0 +1,16 @@
+// @flow
+import { type as ap } from './ap'
+import { type as pop } from './pop'
+import { type as tpop } from './tpop'
+import { type as tpopfeldkontr } from './tpopfeldkontr'
+import { type as tpopfreiwkontr } from './tpopfreiwkontr'
+import { type as tpopmassn } from './tpopmassn'
+
+export default {
+  ap,
+  pop,
+  tpop,
+  tpopfeldkontr,
+  tpopfreiwkontr,
+  tpopmassn,
+}
