a7dcc7bf219bea67935f4dc0a3dc01ec2b299ffe
Alexander Gabriel <alex.barbalex@gmail.com>
2018-09-10 21:22:55 +0200

    directly compare some types

--- src/components/Projekte/TreeContainer/filterNodesByNodeFilterArray.js
+++ src/components/Projekte/TreeContainer/filterNodesByNodeFilterArray.js
@@ -3,16 +3,30 @@
 // TODO: would be much better to filter this in query
 // this is done
 // but unfortunately query does not immediatly update
+// TODO:
+// if key is of type number or boolean, do not cast to string
+
+import types from '../../../state/nodeFilter/types'
+
 export default ({
   node,
   nodeFilterArray,
+  table,
 }: {
   node: Object,
   nodeFilterArray: Array<Object>,
+  table: string,
 }) => {
   if (nodeFilterArray.length === 0) return true
+  let type = 'string'
   return nodeFilterArray.every(([key, value]) => {
     if (node[key] === null || node[key] === undefined) return false
+    if (table && types[table] && types[table][key]) {
+      type = types[table][key]
+    }
+    if (['number', 'uuid', 'boolean'].includes(type)) {
+      return node[key] === value
+    }
     return node[key]
       .toString()
       .toLowerCase()
