545b6cefe1799841a66f2828f7d01c98b0324a33
Alexander Gabriel <alex.barbalex@gmail.com>
2018-09-20 17:24:30 +0200

    optimize

--- src/components/Projekte/ProjektContainer/withPopForMap.js
+++ src/components/Projekte/ProjektContainer/withPopForMap.js
@@ -2,22 +2,11 @@
 import { graphql } from 'react-apollo'
 
 import query from './popForMap.graphql'
-import buildVariables from './variables'
 
 export default graphql(query, {
-  options: ({ dataLocal, treeName, nodeFilterState }) => {
-    const variables = buildVariables({
-      data: dataLocal,
-      treeName,
-      nodeFilter: nodeFilterState.state[treeName],
-    })
-
-    return {
-      variables: {
-        apIsActiveInMap: variables.apIsActiveInMap,
-        ap: variables.ap,
-      },
-    }
-  },
+  options: ({ apIsActiveInMap, ap }) => ({
+    apIsActiveInMap,
+    ap,
+  }),
   name: 'dataPopForMap',
 })
--- src/components/Projekte/ProjektContainer/withPopbers.js
+++ src/components/Projekte/ProjektContainer/withPopbers.js
@@ -2,22 +2,11 @@
 import { graphql } from 'react-apollo'
 
 import query from './popbers.graphql'
-import buildVariables from './variables'
 
 export default graphql(query, {
-  options: ({ dataLocal, treeName, nodeFilterState }) => {
-    const variables = buildVariables({
-      data: dataLocal,
-      treeName,
-      nodeFilter: nodeFilterState.state[treeName],
-    })
-
-    return {
-      variables: {
-        isPop: variables.isPop,
-        pop: variables.pop,
-      },
-    }
-  },
+  options: ({ isPop, pop }) => ({
+    isPop,
+    pop,
+  }),
   name: 'dataPopbers',
 })
