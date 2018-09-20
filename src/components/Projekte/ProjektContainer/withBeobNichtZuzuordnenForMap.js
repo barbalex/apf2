1cafe1a51bdf6077f1772eb92872a97bf322fbe5
Alexander Gabriel <alex.barbalex@gmail.com>
2018-09-20 16:53:01 +0200

    calc less

--- src/components/Projekte/ProjektContainer/withBeobNichtZuzuordnenForMap.js
+++ src/components/Projekte/ProjektContainer/withBeobNichtZuzuordnenForMap.js
@@ -1,21 +1,39 @@
 // @flow
 import { graphql } from 'react-apollo'
+import get from 'lodash/get'
+import uniq from 'lodash/uniq'
 
 import query from './beobNichtZuzuordnenForMap.graphql'
-import buildVariables from './variables'
 
 export default graphql(query, {
   options: ({ dataLocal, treeName, nodeFilterState }) => {
-    const variables = buildVariables({
-      data: dataLocal,
-      treeName,
-      nodeFilter: nodeFilterState.state[treeName],
-    })
+    const openNodes = get(dataLocal, `${treeName}.openNodes`)
+    const ap = uniq(
+      openNodes
+        .map(
+          a =>
+            a.length > 3 &&
+            a[0] === 'Projekte' &&
+            decodeURIComponent(a[2]) === 'Aktionspläne'
+              ? a[3]
+              : null,
+        )
+        .filter(v => v !== null),
+    )
+    const projekteTabs = get(dataLocal, 'urlQuery.projekteTabs', [])
+    const mapIsActive =
+      projekteTabs.includes('karte') || projekteTabs.includes('karte2')
+    const isProjekt = openNodes.some(
+      nArray => nArray[0] === 'Projekte' && nArray[1],
+    )
+    const isAp =
+      isProjekt &&
+      openNodes.some(nArray => nArray[2] === 'Aktionspläne' && nArray[3])
 
     return {
       variables: {
-        ap: variables.ap,
-        apIsActiveInMap: variables.apIsActiveInMap,
+        ap,
+        apIsActiveInMap: mapIsActive && isAp,
       },
     }
   },
