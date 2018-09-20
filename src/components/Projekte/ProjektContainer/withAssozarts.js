d25af8165afa6dc57740853a0621421d525fe989
Alexander Gabriel <alex.barbalex@gmail.com>
2018-09-20 16:44:46 +0200

    calc less

--- src/components/Projekte/ProjektContainer/withAssozarts.js
+++ src/components/Projekte/ProjektContainer/withAssozarts.js
@@ -1,21 +1,36 @@
 // @flow
 import { graphql } from 'react-apollo'
+import get from 'lodash/get'
+import uniq from 'lodash/uniq'
 
 import query from './assozarts.graphql'
-import buildVariables from './variables'
 
 export default graphql(query, {
-  options: ({ dataLocal, treeName, nodeFilterState }) => {
-    const variables = buildVariables({
-      data: dataLocal,
-      treeName,
-      nodeFilter: nodeFilterState.state[treeName],
-    })
+  options: ({ dataLocal, treeName }) => {
+    const openNodes = get(dataLocal, `${treeName}.openNodes`)
+    const isProjekt = openNodes.some(
+      nArray => nArray[0] === 'Projekte' && nArray[1],
+    )
+    const isAp =
+      isProjekt &&
+      openNodes.some(nArray => nArray[2] === 'Aktionspläne' && nArray[3])
+    const ap = uniq(
+      openNodes
+        .map(
+          a =>
+            a.length > 3 &&
+            a[0] === 'Projekte' &&
+            decodeURIComponent(a[2]) === 'Aktionspläne'
+              ? a[3]
+              : null,
+        )
+        .filter(v => v !== null),
+    )
 
     return {
       variables: {
-        isAp: variables.isAp,
-        ap: variables.ap,
+        isAp,
+        ap,
       },
     }
   },
--- src/components/Projekte/ProjektContainer/withBeobAssignLines.js
+++ src/components/Projekte/ProjektContainer/withBeobAssignLines.js
@@ -1,23 +1,46 @@
 // @flow
 import { graphql } from 'react-apollo'
+import get from 'lodash/get'
 
 import query from './beobAssignLines.graphql'
-import buildVariables from './variables'
 
 export default graphql(query, {
   options: ({ dataLocal, treeName, nodeFilterState }) => {
-    const variables = buildVariables({
-      data: dataLocal,
-      treeName,
-      nodeFilter: nodeFilterState.state[treeName],
-    })
+    const openNodes = get(dataLocal, `${treeName}.openNodes`)
+    const isProjekt = openNodes.some(
+      nArray => nArray[0] === 'Projekte' && nArray[1],
+    )
+    const isAp =
+      isProjekt &&
+      openNodes.some(nArray => nArray[2] === 'Aktionspläne' && nArray[3])
+    const activeNodeArray = get(dataLocal, `${treeName}.activeNodeArray`)
+    const projId =
+      activeNodeArray.length > 0 &&
+      activeNodeArray[0] === 'Projekte' &&
+      activeNodeArray.length > 1
+        ? activeNodeArray[1]
+        : '99999999-9999-9999-9999-999999999999'
+
+    const projektFolder =
+      (activeNodeArray.length > 0 && activeNodeArray[0] === 'Projekte') || false
+    const projekt =
+      projektFolder && activeNodeArray.length > 1 ? activeNodeArray[1] : null
+    const apFolder =
+      (projekt &&
+        activeNodeArray.length > 2 &&
+        decodeURIComponent(activeNodeArray[2]) === 'Aktionspläne') ||
+      false
+    const apId =
+      apFolder && activeNodeArray.length > 3
+        ? activeNodeArray[3]
+        : '99999999-9999-9999-9999-999999999999'
 
     return {
       variables: {
-        projId: variables.projId,
-        isProjekt: variables.isProjekt,
-        apId: variables.apId,
-        isAp: variables.isAp,
+        projId,
+        isProjekt,
+        apId,
+        isAp,
       },
     }
   },
