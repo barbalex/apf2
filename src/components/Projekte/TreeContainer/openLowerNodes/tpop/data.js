cc86b5f4bb0e95bf70eb26c774ef082aa96e8677
Alexander Gabriel <alex.barbalex@gmail.com>
2018-10-02 15:40:59 +0200

    use gql

---
+++ src/components/Projekte/TreeContainer/openLowerNodes/tpop/data.js
@@ -0,0 +1,53 @@
+import gql from 'graphql-tag'
+
+export default gql`
+  query Query($id: UUID!) {
+    tpopById(id: $id) {
+      id
+      tpopmassnsByTpopId {
+        nodes {
+          id
+        }
+      }
+      tpopmassnbersByTpopId {
+        nodes {
+          id
+        }
+      }
+      tpopfeldkontrs: tpopkontrsByTpopId(
+        filter: { typ: { notEqualTo: "Freiwilligen-Erfolgskontrolle" } }
+      ) {
+        nodes {
+          id
+          tpopkontrzaehlsByTpopkontrId {
+            nodes {
+              id
+            }
+          }
+        }
+      }
+      tpopfreiwkontrs: tpopkontrsByTpopId(
+        filter: { typ: { equalTo: "Freiwilligen-Erfolgskontrolle" } }
+      ) {
+        nodes {
+          id
+          tpopkontrzaehlsByTpopkontrId {
+            nodes {
+              id
+            }
+          }
+        }
+      }
+      tpopbersByTpopId {
+        nodes {
+          id
+        }
+      }
+      beobsByTpopId {
+        nodes {
+          id
+        }
+      }
+    }
+  }
+`
