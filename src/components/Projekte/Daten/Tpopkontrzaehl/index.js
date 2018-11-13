faf60662fa4ccec3282e7038545a574d845e2a57
Alexander Gabriel <alex.barbalex@gmail.com>
2018-11-13 15:55:09 +0100

    optimize

--- src/components/Projekte/Daten/Tpopkontrzaehl/index.js
+++ src/components/Projekte/Daten/Tpopkontrzaehl/index.js
@@ -1,20 +1,20 @@
 // @flow
 import React from 'react'
 import styled from 'styled-components'
-import { Query, Mutation } from 'react-apollo'
 import get from 'lodash/get'
 import sortBy from 'lodash/sortBy'
 import compose from 'recompose/compose'
 import withHandlers from 'recompose/withHandlers'
 import withState from 'recompose/withState'
 import withLifecycle from '@hocs/with-lifecycle'
+import app from 'ampersand-app'
 
 import RadioButtonGroup from '../../../shared/RadioButtonGroup'
 import TextField from '../../../shared/TextField'
 import FormTitle from '../../../shared/FormTitle'
 import Select from '../../../shared/Select'
 import ErrorBoundary from '../../../shared/ErrorBoundary'
-import dataGql from './data'
+import withData from './withData'
 import updateTpopkontrzaehlByIdGql from './updateTpopkontrzaehlById'
 import withAllTpopkontrzaehlEinheitWertes from './withAllTpopkontrzaehlEinheitWertes'
 
@@ -30,21 +30,22 @@ const FieldsContainer = styled.div`
 `
 
 const enhance = compose(
+  withData,
   withAllTpopkontrzaehlEinheitWertes,
   withState('errors', 'setErrors', {}),
   withHandlers({
-    saveToDb: ({ refetchTree, setErrors, errors }) => async ({
-      row,
-      field,
-      value,
-      updateTpopkontrzaehl,
-    }) => {
+    saveToDb: ({ refetchTree, setErrors, errors, data }) => async event => {
+      const field = event.target.name
+      let value = event.target.value
+      if (value === undefined) value = null
+      const row = get(data, 'tpopkontrzaehlById', {})
       /**
        * only save if value changed
        */
       if (row[field] === value) return
       try {
-        await updateTpopkontrzaehl({
+        await app.client.mutate({
+          mutation: updateTpopkontrzaehlByIdGql,
           variables: {
             id: row.id,
             [field]: value,
@@ -90,111 +91,87 @@ const Tpopkontrzaehl = ({
   errors,
   treeName,
   dataAllTpopkontrzaehlEinheitWertes,
+  data,
 }: {
   id: string,
   saveToDb: () => void,
   errors: Object,
   treeName: string,
   dataAllTpopkontrzaehlEinheitWertes: Object,
-}) => (
-  <Query query={dataGql} variables={{ id }}>
-    {({ loading, error, data }) => {
-      if (loading || dataAllTpopkontrzaehlEinheitWertes.loading)
-        return (
-          <Container>
-            <FieldsContainer>Lade...</FieldsContainer>
-          </Container>
-        )
-      if (error) return `Fehler: ${error.message}`
-      if (dataAllTpopkontrzaehlEinheitWertes.error)
-        return `Fehler: ${dataAllTpopkontrzaehlEinheitWertes.error.message}`
+  data: Object,
+}) => {
+  if (data.loading || dataAllTpopkontrzaehlEinheitWertes.loading)
+    return (
+      <Container>
+        <FieldsContainer>Lade...</FieldsContainer>
+      </Container>
+    )
+  if (data.error) return `Fehler: ${data.error.message}`
+  if (dataAllTpopkontrzaehlEinheitWertes.error)
+    return `Fehler: ${dataAllTpopkontrzaehlEinheitWertes.error.message}`
 
-      const row = get(data, 'tpopkontrzaehlById', {})
-      let zaehleinheitWerte = get(
-        dataAllTpopkontrzaehlEinheitWertes,
-        'allTpopkontrzaehlEinheitWertes.nodes',
-        [],
-      )
-      zaehleinheitWerte = sortBy(zaehleinheitWerte, 'sort').map(el => ({
-        value: el.code,
-        label: el.text,
-      }))
-      let methodeWerte = get(data, 'allTpopkontrzaehlMethodeWertes.nodes', [])
-      methodeWerte = sortBy(methodeWerte, 'sort')
-      methodeWerte = methodeWerte.map(el => ({
-        value: el.code,
-        label: el.text,
-      }))
+  const row = get(data, 'tpopkontrzaehlById', {})
+  let zaehleinheitWerte = get(
+    dataAllTpopkontrzaehlEinheitWertes,
+    'allTpopkontrzaehlEinheitWertes.nodes',
+    [],
+  )
+  zaehleinheitWerte = sortBy(zaehleinheitWerte, 'sort').map(el => ({
+    value: el.code,
+    label: el.text,
+  }))
+  let methodeWerte = get(data, 'allTpopkontrzaehlMethodeWertes.nodes', [])
+  methodeWerte = sortBy(methodeWerte, 'sort')
+  methodeWerte = methodeWerte.map(el => ({
+    value: el.code,
+    label: el.text,
+  }))
 
-      return (
-        <ErrorBoundary>
-          <Container>
-            <FormTitle
-              apId={get(
-                data,
-                'tpopkontrzaehlById.tpopkontrByTpopkontrId.tpopByTpopId.popByPopId.apId',
-              )}
-              title="Zählung"
-              treeName={treeName}
-              table="tpopkontrzaehl"
-            />
-            <Mutation mutation={updateTpopkontrzaehlByIdGql}>
-              {(updateTpopkontrzaehl, { data }) => (
-                <FieldsContainer>
-                  <Select
-                    key={`${row.id}einheit`}
-                    value={row.einheit}
-                    field="einheit"
-                    label="Einheit"
-                    options={zaehleinheitWerte}
-                    saveToDb={value =>
-                      saveToDb({
-                        row,
-                        field: 'einheit',
-                        value,
-                        updateTpopkontrzaehl,
-                      })
-                    }
-                    error={errors.einheit}
-                  />
-                  <TextField
-                    key={`${row.id}anzahl`}
-                    label="Anzahl (nur ganze Zahlen)"
-                    value={row.anzahl}
-                    type="number"
-                    saveToDb={value =>
-                      saveToDb({
-                        row,
-                        field: 'anzahl',
-                        value,
-                        updateTpopkontrzaehl,
-                      })
-                    }
-                    error={errors.anzahl}
-                  />
-                  <RadioButtonGroup
-                    key={`${row.id}methode`}
-                    label="Methode"
-                    value={row.methode}
-                    dataSource={methodeWerte}
-                    saveToDb={value =>
-                      saveToDb({
-                        row,
-                        field: 'methode',
-                        value,
-                        updateTpopkontrzaehl,
-                      })
-                    }
-                    error={errors.methode}
-                  />
-                </FieldsContainer>
-              )}
-            </Mutation>
-          </Container>
-        </ErrorBoundary>
-      )
-    }}
-  </Query>
-)
+  return (
+    <ErrorBoundary>
+      <Container>
+        <FormTitle
+          apId={get(
+            data,
+            'tpopkontrzaehlById.tpopkontrByTpopkontrId.tpopByTpopId.popByPopId.apId',
+          )}
+          title="Zählung"
+          treeName={treeName}
+          table="tpopkontrzaehl"
+        />
+        <FieldsContainer>
+          <Select
+            key={`${row.id}einheit`}
+            name="einheit"
+            value={row.einheit}
+            field="einheit"
+            label="Einheit"
+            options={zaehleinheitWerte}
+            saveToDb={saveToDb}
+            error={errors.einheit}
+          />
+          <TextField
+            key={`${row.id}anzahl`}
+            name="anzahl"
+            label="Anzahl (nur ganze Zahlen)"
+            value={row.anzahl}
+            type="number"
+            saveToDb={saveToDb}
+            error={errors.anzahl}
+          />
+          <RadioButtonGroup
+            key={`${row.id}methode`}
+            name="methode"
+            label="Methode"
+            value={row.methode}
+            dataSource={methodeWerte}
+            saveToDb={saveToDb}
+            error={errors.methode}
+          />
+        </FieldsContainer>
+      </Container>
+    </ErrorBoundary>
+  )
+}
 
 export default enhance(Tpopkontrzaehl)
