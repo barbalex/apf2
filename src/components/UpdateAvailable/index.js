e698ac6372f663a400fbb3fc8e9ebf2df08ddbfe
Alexander Gabriel <alex.barbalex@gmail.com>
2018-11-14 18:05:47 +0100

    optimize

--- src/components/UpdateAvailable/index.js
+++ src/components/UpdateAvailable/index.js
@@ -13,11 +13,12 @@ import React from 'react'
 import Snackbar from '@material-ui/core/Snackbar'
 import Button from '@material-ui/core/Button'
 import styled from 'styled-components'
-import { Query } from 'react-apollo'
 import get from 'lodash/get'
+import compose from 'recompose/compose'
+import app from 'ampersand-app'
 
 import ErrorBoundary from '../shared/ErrorBoundary'
-import dataGql from './data'
+import withLocalData from './withLocalData'
 import setUpdateAvailable from './setUpdateAvailable'
 
 const StyledSnackbar = styled(Snackbar)`
@@ -26,49 +27,45 @@ const StyledSnackbar = styled(Snackbar)`
   }
 `
 
-const UpdateAvailable = () => (
-  <Query query={dataGql}>
-    {({ loading, error, data, client }) => {
-      if (error) return `Fehler: ${error.message}`
-      const updateAvailable = get(data, 'updateAvailable')
+const enhance = compose(withLocalData)
 
-      return (
-        <ErrorBoundary>
-          <StyledSnackbar
-            open={updateAvailable}
-            message={
-              <span id="message-id">Ein Update steht zur Verfügung</span>
-            }
-            SnackbarContentProps={{
-              'aria-describedby': 'message-id',
+const UpdateAvailable = ({ localData }: { localData: Object }) => {
+  if (localData.error) return `Fehler: ${localData.error.message}`
+  const updateAvailable = get(localData, 'updateAvailable')
+
+  return (
+    <ErrorBoundary>
+      <StyledSnackbar
+        open={updateAvailable}
+        message={<span id="message-id">Ein Update steht zur Verfügung</span>}
+        SnackbarContentProps={{
+          'aria-describedby': 'message-id',
+        }}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+        action={
+          <Button
+            color="primary"
+            size="small"
+            onClick={event => {
+              event.preventDefault()
+              window.location.reload(false)
             }}
-            anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
-            action={
-              <Button
-                color="primary"
-                size="small"
-                onClick={event => {
-                  event.preventDefault()
-                  window.location.reload(false)
-                }}
-              >
-                installieren
-              </Button>
-            }
-            autoHideDuration={1000 * 60}
-            onClose={() => {
-              client.mutate({
-                mutation: setUpdateAvailable,
-                variables: { value: false },
-              })
-            }}
-          />
-        </ErrorBoundary>
-      )
-    }}
-  </Query>
-)
+          >
+            installieren
+          </Button>
+        }
+        autoHideDuration={1000 * 60}
+        onClose={() => {
+          app.client.mutate({
+            mutation: setUpdateAvailable,
+            variables: { value: false },
+          })
+        }}
+      />
+    </ErrorBoundary>
+  )
+}
 
 UpdateAvailable.displayName = 'UpdateAvailable'
 
-export default UpdateAvailable
+export default enhance(UpdateAvailable)
