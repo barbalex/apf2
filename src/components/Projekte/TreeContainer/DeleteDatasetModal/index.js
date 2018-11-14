46b296c74d140c0fc5b26aeafccdd7a7463d72d0
Alexander Gabriel <alex.barbalex@gmail.com>
2018-11-14 18:02:48 +0100

    optimize

--- src/components/Projekte/TreeContainer/DeleteDatasetModal/index.js
+++ src/components/Projekte/TreeContainer/DeleteDatasetModal/index.js
@@ -4,12 +4,11 @@ import Dialog from '@material-ui/core/Dialog'
 import DialogActions from '@material-ui/core/DialogActions'
 import Button from '@material-ui/core/Button'
 import styled from 'styled-components'
-import { Query } from 'react-apollo'
 import compose from 'recompose/compose'
 
 import tables from '../../../../modules/tables'
 import ErrorBoundary from '../../../shared/ErrorBoundary'
-import dataGql from './data'
+import withLocalData from './withLocalData'
 import deleteDataset from './delete'
 import withDeleteState from '../../../../state/withDeleteState'
 import withErrorState from '../../../../state/withErrorState'
@@ -21,6 +20,7 @@ const StyledDialog = styled(Dialog)`
 `
 
 const enhance = compose(
+  withLocalData,
   withDeleteState,
   withErrorState,
 )
@@ -28,62 +28,58 @@ const enhance = compose(
 const DatasetDeleteModal = ({
   deleteState,
   errorState,
+  localData,
 }: {
   deleteState: Object,
   errorState: Object,
-}) => (
-  <Query query={dataGql}>
-    {({ loading, error, data, client }) => {
-      if (error) {
-        if (
-          error.message.includes('permission denied') ||
-          error.message.includes('keine Berechtigung')
-        ) {
-          // ProjektContainer returns helpful screen
-          return null
-        }
-        return `Fehler: ${error.message}`
-      }
-      const datasetToDelete = deleteState.state.toDelete
-      const table = tables.find(t => t.table === datasetToDelete.table)
-      let tableName = null
-      if (table && table.labelSingular) {
-        tableName = table.labelSingular
-      }
-      let question = `${tableName ? `${tableName} "` : ''}${
-        datasetToDelete.label
-      }${tableName ? '"' : ''} löschen?`
-      if (!datasetToDelete.label) {
-        question = `${tableName} löschen?`
-      }
+  localData: Object,
+}) => {
+  if (localData.error) {
+    if (
+      localData.error.message.includes('permission denied') ||
+      localData.error.message.includes('keine Berechtigung')
+    ) {
+      // ProjektContainer returns helpful screen
+      return null
+    }
+    return `Fehler: ${localData.error.message}`
+  }
+  const datasetToDelete = deleteState.state.toDelete
+  const table = tables.find(t => t.table === datasetToDelete.table)
+  let tableName = null
+  if (table && table.labelSingular) {
+    tableName = table.labelSingular
+  }
+  let question = `${tableName ? `${tableName} "` : ''}${datasetToDelete.label}${
+    tableName ? '"' : ''
+  } löschen?`
+  if (!datasetToDelete.label) {
+    question = `${tableName} löschen?`
+  }
 
-      return (
-        <ErrorBoundary>
-          <StyledDialog open={!!datasetToDelete.table}>
-            {question}
-            <DialogActions>
-              <Button onClick={() => deleteState.emptyToDelete()}>
-                Abbrechen
-              </Button>
-              <Button
-                color="primary"
-                onClick={() =>
-                  deleteDataset({
-                    dataPassedIn: data,
-                    deleteState,
-                    errorState,
-                  })
-                }
-              >
-                Löschen
-              </Button>
-              ,
-            </DialogActions>
-          </StyledDialog>
-        </ErrorBoundary>
-      )
-    }}
-  </Query>
-)
+  return (
+    <ErrorBoundary>
+      <StyledDialog open={!!datasetToDelete.table}>
+        {question}
+        <DialogActions>
+          <Button onClick={() => deleteState.emptyToDelete()}>Abbrechen</Button>
+          <Button
+            color="primary"
+            onClick={() =>
+              deleteDataset({
+                dataPassedIn: localData,
+                deleteState,
+                errorState,
+              })
+            }
+          >
+            Löschen
+          </Button>
+          ,
+        </DialogActions>
+      </StyledDialog>
+    </ErrorBoundary>
+  )
+}
 
 export default enhance(DatasetDeleteModal)
