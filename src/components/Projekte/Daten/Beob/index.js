9cd0841c90484b4013c3a19a6eb0fa5bfd114ca3
Alexander Gabriel <alex.barbalex@gmail.com>
2018-11-15 13:29:04 +0100

    use hooks

--- src/components/Projekte/Daten/Beob/index.js
+++ src/components/Projekte/Daten/Beob/index.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { createRef, Component } from 'react'
+import React, { useMemo } from 'react'
 import styled from 'styled-components'
 import get from 'lodash/get'
 import compose from 'recompose/compose'
@@ -19,50 +19,45 @@ const Container = styled.div`
 
 const enhance = compose(withData)
 
-type Props = {
-  treeName: String,
-  id: String,
+const Beob = ({
+  id,
+  data,
+  dimensions = { width: 380 },
+}: {
+  id: string,
   dimensions: Object,
   data: Object,
-}
+}) => {
+  if (data.loading) return <Container>Lade...</Container>
+  if (data.error) return `Fehler: ${data.error.message}`
 
-class Beob extends Component<Props> {
-  constructor(props) {
-    super(props)
-    // TODO: what is this ref used for?
-    this.container = createRef()
-  }
+  const row = get(data, 'beobById', {})
+  if (!row) return null
 
-  render() {
-    const { data, dimensions = { width: 380 } } = this.props
+  const beobFields = useMemo(
+    () =>
+      Object.entries(JSON.parse(row.data)).filter(
+        ([key, value]) => value || value === 0 || value === false,
+      ),
+    [row.data],
+  )
+  if (!beobFields || beobFields.length === 0) return null
 
-    if (data.loading) return <Container>Lade...</Container>
-    if (data.error) return `Fehler: ${data.error.message}`
-
-    const row = get(data, 'beobById', {})
-    if (!row) return null
-
-    const beobFields = Object.entries(JSON.parse(row.data)).filter(
-      ([key, value]) => value || value === 0 || value === false,
-    )
-    if (!beobFields || beobFields.length === 0) return null
-
-    return (
-      <ErrorBoundary>
-        <div ref={this.container}>
-          <Container
-            data-width={isNaN(dimensions.width) ? 380 : dimensions.width}
-          >
-            {beobFields.map(([key, value]) => (
-              <div key={key}>
-                <TextFieldNonUpdatable label={key} value={value} />
-              </div>
-            ))}
-          </Container>
-        </div>
-      </ErrorBoundary>
-    )
-  }
+  return (
+    <ErrorBoundary>
+      <div>
+        <Container
+          data-width={isNaN(dimensions.width) ? 380 : dimensions.width}
+        >
+          {beobFields.map(([key, value]) => (
+            <div key={key}>
+              <TextFieldNonUpdatable label={key} value={value} />
+            </div>
+          ))}
+        </Container>
+      </div>
+    </ErrorBoundary>
+  )
 }
 
 export default enhance(Beob)
