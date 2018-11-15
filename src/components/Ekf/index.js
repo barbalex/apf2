8e7d7ed80b96fc15f8fadcf7bdf3b880d83f87c9
Alexander Gabriel <alex.barbalex@gmail.com>
2018-11-15 17:30:24 +0100

    dont lazy load

--- src/components/Ekf/index.js
+++ src/components/Ekf/index.js
@@ -1,11 +1,14 @@
 // @flow
-import React, { lazy, Suspense } from 'react'
+/**
+ * Stopped lazy loading Tpopfreiwkontr
+ * because Reflex would often not show layout
+ */
+import React from 'react'
 import styled from 'styled-components'
 import { ReflexContainer, ReflexSplitter, ReflexElement } from 'react-reflex'
 import { Query } from 'react-apollo'
 import get from 'lodash/get'
 import merge from 'lodash/merge'
-import intersection from 'lodash/intersection'
 import compose from 'recompose/compose'
 import jwtDecode from 'jwt-decode'
 
@@ -19,16 +22,7 @@ import dataWithDateByUserNameGql from './dataWithDateByUserName'
 import dataWithDateByAdresseIdGql from './dataWithDateByAdresseId'
 import withErrorState from '../../state/withErrorState'
 import EkfList from './List'
-import Fallback from '../shared/Fallback'
-
-/**
- * TODO:
- * on mount
- * check if ekfAdresseId exists
- * if so: use byAdresse queries
- */
-
-const Tpopfreiwkontr = lazy(() => import('../Projekte/Daten/Tpopfreiwkontr'))
+import Tpopfreiwkontr from '../Projekte/Daten/Tpopfreiwkontr'
 
 const Container = styled.div`
   display: flex;
@@ -45,7 +39,6 @@ const ReflexElementForEKF = styled(ReflexElement)`
     border-left: 1px solid rgb(46, 125, 50);
   }
 `
-const treeTabValues = ['tree', 'daten', 'karte', 'exporte']
 
 const enhance = compose(
   withLocalData,
@@ -61,14 +54,6 @@ const EkfContainer = ({
 }) => {
   if (localData.error) return `Fehler: ${localData.error.message}`
   const userName = get(localData, 'user.name')
-  const projekteTabs = [...get(localData, 'urlQuery.projekteTabs', [])]
-  const tabs = intersection(treeTabValues, projekteTabs)
-  const treeFlex =
-    projekteTabs.length === 2 && tabs.length === 2
-      ? 0.33
-      : tabs.length === 0
-      ? 1
-      : 1 / tabs.length
   const isPrint = get(localData, 'isPrint')
   const jahr = get(localData, 'ekfYear')
   const ekfAdresseId = get(localData, 'ekfAdresseId')
@@ -96,62 +81,51 @@ const EkfContainer = ({
         const tpopkontrId = activeNodeArray[9]
         const treeName = 'tree'
 
-        if (isPrint && tpopkontrId)
+        if (isPrint && tpopkontrId) {
           return (
-            <Suspense fallback={<Fallback />}>
-              <Tpopfreiwkontr
-                id={activeNodeArray[9]}
-                activeNodeArray={activeNodeArray}
-                treeName={treeName}
-                refetchTree={refetch}
-                errorState={errorState}
-                role={role}
-                dimensions={{ width: 1000 }}
-              />
-            </Suspense>
+            <Tpopfreiwkontr
+              id={activeNodeArray[9]}
+              activeNodeArray={activeNodeArray}
+              treeName={treeName}
+              refetchTree={refetch}
+              errorState={errorState}
+              role={role}
+              dimensions={{ width: 1000 }}
+            />
           )
+        }
 
         return (
           <Container data-loading={loading}>
             <ErrorBoundary>
               <ReflexContainer orientation="vertical">
-                {tabs.includes('tree') && (
-                  <ReflexElement
-                    flex={treeFlex}
-                    propagateDimensions={true}
-                    renderOnResizeRate={200}
-                    renderOnResize={true}
-                  >
-                    <EkfList data={data} loading={loading} />
-                  </ReflexElement>
-                )}
-                {tabs.includes('tree') && tabs.includes('daten') && (
-                  <ReflexSplitter />
-                )}
-                {tabs.includes('daten') && (
-                  <ReflexElementForEKF
-                    flex={1 - treeFlex}
-                    propagateDimensions={true}
-                    renderOnResizeRate={100}
-                    renderOnResize={true}
-                  >
-                    {tpopkontrId ? (
-                      <Suspense fallback={<Fallback />}>
-                        <Tpopfreiwkontr
-                          id={activeNodeArray[9]}
-                          activeNodeArray={activeNodeArray}
-                          treeName={treeName}
-                          refetchTree={refetch}
-                          errorState={errorState}
-                          role={role}
-                          dimensions={{ width: 1000 }}
-                        />
-                      </Suspense>
-                    ) : (
-                      <div />
-                    )}
-                  </ReflexElementForEKF>
-                )}
+                <ReflexElement
+                  flex={0.33}
+                  propagateDimensions={true}
+                  renderOnResizeRate={200}
+                  renderOnResize={true}
+                >
+                  <EkfList data={data} loading={loading} />
+                </ReflexElement>
+                <ReflexSplitter />
+                <ReflexElementForEKF
+                  flex={0.67}
+                  propagateDimensions={true}
+                  renderOnResizeRate={100}
+                  renderOnResize={true}
+                >
+                  {tpopkontrId && (
+                    <Tpopfreiwkontr
+                      id={activeNodeArray[9]}
+                      activeNodeArray={activeNodeArray}
+                      treeName={treeName}
+                      refetchTree={refetch}
+                      errorState={errorState}
+                      role={role}
+                      dimensions={{ width: 1000 }}
+                    />
+                  )}
+                </ReflexElementForEKF>
               </ReflexContainer>
             </ErrorBoundary>
           </Container>
