769ecd3efc3dcdfe2afad2b25d0caa53f492c75f
Alexander Gabriel <alex.barbalex@gmail.com>
2018-11-20 20:35:30 +0100

    use withApollo

--- src/components/Projekte/TreeContainer/openLowerNodes/zielFolder/index.js
+++ src/components/Projekte/TreeContainer/openLowerNodes/zielFolder/index.js
@@ -5,7 +5,6 @@
  * 3. update openNodes
  * 4. refresh tree
  */
-import app from 'ampersand-app'
 import get from 'lodash/get'
 import groupBy from 'lodash/groupBy'
 
@@ -17,13 +16,14 @@ export default async ({
   activeNodes,
   id,
   refetchTree,
+  client,
 }: {
   tree: Object,
   activeNodes: Object,
   id: String,
   refetchTree: () => void,
+  client: Object,
 }) => {
-  const { client } = app
   const { projekt } = activeNodes
   const { openNodes } = tree
   // 1. load all data
