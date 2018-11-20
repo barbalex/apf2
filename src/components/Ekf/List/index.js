a8f7a2fc396a6d701562cb917aa33a54005348fb
Alexander Gabriel <alex.barbalex@gmail.com>
2018-11-20 17:14:53 +0100

    use withApollo

--- src/components/Ekf/List/index.js
+++ src/components/Ekf/List/index.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { FixedSizeList as List } from 'react-window'
 import get from 'lodash/get'
 import uniq from 'lodash/uniq'
@@ -8,6 +8,7 @@ import styled from 'styled-components'
 import { withApollo } from 'react-apollo'
 
 import initiateDataFromUrl from './initiateDataFromUrl'
+import Item from './Item'
 
 const Container = styled.div`
   height: 100%;
@@ -15,30 +16,6 @@ const Container = styled.div`
 `
 const NoDataContainer = styled.div`
   padding 15px;
-`
-const OuterContainer = styled.div`
-  border-bottom: 1px solid rgba(46, 125, 50, 0.5);
-  cursor: pointer;
-  background-color: ${props => (props.active ? 'rgb(255, 250, 198)' : 'unset')};
-  border-top: ${props =>
-    props.active ? '1px solid rgba(46, 125, 50, 0.5)' : 'unset'};
-  &:hover {
-    background-color: rgb(255, 250, 198);
-    border-top: 1px solid rgba(46, 125, 50, 0.5);
-    margin-top: -1px;
-  }
-`
-const InnerContainer = styled.div`
-  height: ${props => props.height}px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  padding: 15px;
-  > div {
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-  }
 `
 
 const getEkfFromData = data => {
@@ -93,8 +70,19 @@ const EkfList = ({
   dimensions: Object,
   client: Object,
 }) => {
-  const [initialKontrId, setInitialKontrId] = useState(null)
   const ekf = getEkfFromData(data)
+
+  if (!loading && ekf.length === 0) {
+    return (
+      <NoDataContainer>
+        {`Für das Jahr ${get(
+          data,
+          'ekfYear',
+        )} existieren offenbar keine Erfolgskontrollen mit Ihnen als BearbeiterIn`}
+      </NoDataContainer>
+    )
+  }
+
   const activeNodeArray = get(data, 'tree.activeNodeArray')
   const activeTpopkontrId = activeNodeArray[9]
 
@@ -102,13 +90,7 @@ const EkfList = ({
     () => {
       // set initial kontrId so form is shown for first ekf
       // IF none is choosen yet
-      if (
-        ekf &&
-        ekf.length &&
-        ekf.length > 0 &&
-        !activeTpopkontrId &&
-        !initialKontrId
-      ) {
+      if (ekf && ekf.length && ekf.length > 0 && !activeTpopkontrId) {
         const row = ekf[0]
         const url = [
           'Projekte',
@@ -122,28 +104,16 @@ const EkfList = ({
           'Freiwilligen-Kontrollen',
           row.id,
         ]
-        initiateDataFromUrl({ url, client })
-        setInitialKontrId(row.id)
+        initiateDataFromUrl({ activeNodeArray: url, client })
       }
     },
-    [initialKontrId],
+    [loading],
   )
 
   const height = isNaN(dimensions.height) ? 250 : dimensions.height
   const width = isNaN(dimensions.width) ? 250 : dimensions.width - 1
   const projektCount = uniq(ekf.map(e => e.projekt)).length
   const itemSize = projektCount > 1 ? 110 : 91
-  const innerContainerHeight = projektCount > 1 ? 81 : 62
-
-  if (!loading && ekf.length === 0)
-    return (
-      <NoDataContainer>
-        {`Für das Jahr ${get(
-          data,
-          'ekfYear',
-        )} existieren offenbar keine Erfolgskontrollen mit Ihnen als BearbeiterIn`}
-      </NoDataContainer>
-    )
 
   return (
     <Container>
@@ -155,34 +125,14 @@ const EkfList = ({
       >
         {({ index, style }) => {
           const row = ekf[index]
-          const url = [
-            'Projekte',
-            row.projId,
-            'Aktionspläne',
-            row.apId,
-            'Populationen',
-            row.popId,
-            'Teil-Populationen',
-            row.tpopId,
-            'Freiwilligen-Kontrollen',
-            row.id,
-          ]
 
           return (
-            <OuterContainer
+            <Item
+              activeTpopkontrId={activeTpopkontrId}
+              projektCount={projektCount}
               style={style}
-              onClick={() =>
-                initiateDataFromUrl({ activeNodeArray: url, client })
-              }
-              active={activeTpopkontrId === row.id}
-            >
-              <InnerContainer height={innerContainerHeight}>
-                {projektCount > 1 && <div>{row.projekt}</div>}
-                <div>{row.art}</div>
-                <div>{row.pop}</div>
-                <div>{row.tpop}</div>
-              </InnerContainer>
-            </OuterContainer>
+              row={row}
+            />
           )
         }}
       </List>
--- src/modules/historyListen.js
+++ src/modules/historyListen.js
@@ -1,19 +1,19 @@
 //@flow
 import isEqual from 'lodash/isEqual'
 import gql from 'graphql-tag'
-import app from 'ampersand-app'
 
 import getActiveNodeArrayFromPathname from './getActiveNodeArrayFromPathname'
 
-export default ( {
+export default ({
   location,
   action,
-}:{
+  client,
+}: {
   location: Object,
   action: String,
+  client: Object,
 }) => {
   const { pathname, state } = location
-  const { client } = app
   //console.log(action, location.pathname, location.state)
   // prevent never ending loop if user clicks back right after initial loading
   if (!(pathname === '/Projekte' && action === 'PUSH')) {
@@ -36,14 +36,17 @@ export default ( {
       variables: {
         value: activeNodeArray,
         tree: 'tree',
-        key: 'activeNodeArray'
-      }
+        key: 'activeNodeArray',
+      },
     })
     if (
       state &&
       state.openNodes &&
       // this would prevent single project from opening at first load
-      !(state.openNodes.length === 1 && isEqual(state.openNodes[0], ['Projekte']))
+      !(
+        state.openNodes.length === 1 &&
+        isEqual(state.openNodes[0], ['Projekte'])
+      )
     ) {
       client.mutate({
         mutation: gql`
@@ -63,8 +66,8 @@ export default ( {
         variables: {
           value: state.openNodes,
           tree: 'tree',
-          key: 'openNodes'
-        }
+          key: 'openNodes',
+        },
       })
     }
   }
