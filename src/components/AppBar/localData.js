06a34f82b686261a9f0d96d9aea852025b4b130c
Alexander Gabriel <alex.barbalex@gmail.com>
2018-11-21 15:52:34 +0100

    move user state to mst

--- src/components/AppBar/localData.js
+++ src/components/AppBar/localData.js
@@ -4,10 +4,6 @@ export default gql`
   query AppBarQuery {
     view @client
     ekfYear @client
-    user @client {
-      name
-      token
-    }
     urlQuery @client {
       projekteTabs
     }
