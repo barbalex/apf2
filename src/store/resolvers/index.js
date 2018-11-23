52ba8405232d4f7d06b7ab20c77543d365449eec
Alexander Gabriel <alex.barbalex@gmail.com>
2018-11-23 22:35:13 +0100

    clean

--- src/store/resolvers/index.js
+++ src/store/resolvers/index.js
@@ -4,14 +4,12 @@ import merge from 'lodash/merge'
 import treeNodeLabelFilterKey from './treeNodeLabelFilterKey'
 import treeNodeLabelFilter from './treeNodeLabelFilter'
 import treeNodeLabelFilterResetExceptAp from './treeNodeLabelFilterResetExceptAp'
-import cloneTree2From1 from './cloneTree2From1'
 
 export default ({ mobxStore }) => {
   const resolvers = merge(
     treeNodeLabelFilterKey,
     treeNodeLabelFilter,
     treeNodeLabelFilterResetExceptAp,
-    cloneTree2From1,
   )
   return resolvers
 }
