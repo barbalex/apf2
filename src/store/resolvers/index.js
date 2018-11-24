47c357801f9cf59cb88061d0729eaea8c75a8159
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2018-11-24 14:55:34 +0100

    remove unused

--- src/store/resolvers/index.js
+++ src/store/resolvers/index.js
@@ -2,13 +2,11 @@
 import merge from 'lodash/merge'
 
 import treeNodeLabelFilterKey from './treeNodeLabelFilterKey'
-import treeNodeLabelFilter from './treeNodeLabelFilter'
 import treeNodeLabelFilterResetExceptAp from './treeNodeLabelFilterResetExceptAp'
 
 export default ({ mobxStore }) => {
   const resolvers = merge(
     treeNodeLabelFilterKey,
-    treeNodeLabelFilter,
     treeNodeLabelFilterResetExceptAp,
   )
   return resolvers
