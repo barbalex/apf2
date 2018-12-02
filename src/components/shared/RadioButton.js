537f601e685d375a271b0657756055581dfa9362
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2018-12-02 17:35:08 +0100

    use hooks

--- src/components/shared/RadioButton.js
+++ src/components/shared/RadioButton.js
@@ -1,14 +1,13 @@
 // @flow
-import React from 'react'
+import React, { useCallback } from 'react'
 import Radio from '@material-ui/core/Radio'
 import RadioGroup from '@material-ui/core/RadioGroup'
 import FormLabel from '@material-ui/core/FormLabel'
 import FormControl from '@material-ui/core/FormControl'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 import FormHelperText from '@material-ui/core/FormHelperText'
-import withHandlers from 'recompose/withHandlers'
-import compose from 'recompose/compose'
 import styled from 'styled-components'
+import { observer } from 'mobx-react-lite'
 
 // without slight padding radio is slightly cut off!
 const StyledFormControl = styled(FormControl)`
@@ -27,9 +26,21 @@ const StyledRadio = styled(Radio)`
   height: 2px !important;
 `
 
-const enhance = compose(
-  withHandlers({
-    onClickButton: ({ saveToDb, value, name }) => () => {
+const RadioButton = ({
+  label,
+  name,
+  value,
+  error,
+  saveToDb,
+}: {
+  label: String,
+  name: String,
+  value: Boolean,
+  error: String,
+  saveToDb: () => void,
+}) => {
+  const onClickButton = useCallback(
+    () => {
       const fakeEvent = {
         target: {
           value: !value,
@@ -38,45 +49,34 @@ const enhance = compose(
       }
       saveToDb(fakeEvent)
     },
-  }),
-)
+    [value, name],
+  )
 
-const RadioButton = ({
-  label,
-  name,
-  value,
-  error,
-  onClickButton,
-}: {
-  label: String,
-  name: String,
-  value: Boolean,
-  error: String,
-  onClickButton: () => void,
-}) => (
-  <StyledFormControl
-    component="fieldset"
-    error={!!error}
-    aria-describedby={`${label}ErrorText`}
-  >
-    <StyledFormLabel component="legend">{label}</StyledFormLabel>
-    <RadioGroup
-      aria-label={label}
-      value={value === null ? 'false' : value.toString()}
+  return (
+    <StyledFormControl
+      component="fieldset"
+      error={!!error}
+      aria-describedby={`${label}ErrorText`}
     >
-      <FormControlLabel
-        value="true"
-        control={<StyledRadio onClick={onClickButton} color="primary" />}
-      />
-    </RadioGroup>
-    {!!error && (
-      <FormHelperText id={`${label}ErrorText`}>{error}</FormHelperText>
-    )}
-  </StyledFormControl>
-)
+      <StyledFormLabel component="legend">{label}</StyledFormLabel>
+      <RadioGroup
+        aria-label={label}
+        value={value === null ? 'false' : value.toString()}
+      >
+        <FormControlLabel
+          value="true"
+          control={<StyledRadio onClick={onClickButton} color="primary" />}
+        />
+      </RadioGroup>
+      {!!error && (
+        <FormHelperText id={`${label}ErrorText`}>{error}</FormHelperText>
+      )}
+    </StyledFormControl>
+  )
+}
 
 RadioButton.defaultProps = {
   value: 'false',
 }
 
-export default enhance(RadioButton)
+export default observer(RadioButton)
