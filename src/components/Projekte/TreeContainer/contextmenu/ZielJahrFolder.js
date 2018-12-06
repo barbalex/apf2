b19e352cc4eb0ff1ae41cf28f04a5bfff3644840
Alexander Gabriel <alex.barbalex@gmail.com>
2018-12-06 18:22:46 +0100

    optimize

--- src/components/Projekte/TreeContainer/contextmenu/ZielJahrFolder.js
+++ src/components/Projekte/TreeContainer/contextmenu/ZielJahrFolder.js
@@ -7,6 +7,18 @@ import ErrorBoundary from '../../../shared/ErrorBoundary'
 import userIsReadOnly from '../../../../modules/userIsReadOnly'
 import mobxStoreContext from '../../../../mobxStoreContext'
 
+// create objects outside render
+const openLowerNodesData = {
+  action: 'openLowerNodes',
+}
+const closeLowerNodesData = {
+  action: 'closeLowerNodes',
+}
+const insertData = {
+  action: 'insert',
+  table: 'ziel',
+}
+
 const ZielJahrFolder = ({
   onClick,
   treeName,
@@ -20,31 +32,15 @@ const ZielJahrFolder = ({
     <ErrorBoundary>
       <ContextMenu id={`${treeName}zieljahrFolder`}>
         <div className="react-contextmenu-title">Ziele</div>
-        <MenuItem
-          onClick={onClick}
-          data={{
-            action: 'openLowerNodes',
-          }}
-        >
+        <MenuItem onClick={onClick} data={openLowerNodesData}>
           alle öffnen
         </MenuItem>
-        <MenuItem
-          onClick={onClick}
-          data={{
-            action: 'closeLowerNodes',
-          }}
-        >
+        <MenuItem onClick={onClick} data={closeLowerNodesData}>
           alle schliessen
         </MenuItem>
         {!userIsReadOnly(user.token) && (
           <>
-            <MenuItem
-              onClick={onClick}
-              data={{
-                action: 'insert',
-                table: 'ziel',
-              }}
-            >
+            <MenuItem onClick={onClick} data={insertData}>
               erstelle neues
             </MenuItem>
           </>
