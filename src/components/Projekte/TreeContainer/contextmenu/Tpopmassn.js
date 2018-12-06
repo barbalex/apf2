13a3bf0b6b57f3f0c10f19a58a255de0ae8293c6
Alexander Gabriel <alex.barbalex@gmail.com>
2018-12-06 18:12:57 +0100

    optimize

--- src/components/Projekte/TreeContainer/contextmenu/Tpopmassn.js
+++ src/components/Projekte/TreeContainer/contextmenu/Tpopmassn.js
@@ -7,6 +7,27 @@ import ErrorBoundary from '../../../shared/ErrorBoundary'
 import userIsReadOnly from '../../../../modules/userIsReadOnly'
 import mobxStoreContext from '../../../../mobxStoreContext'
 
+// create objects outside render
+const insertData = {
+  action: 'insert',
+  table: 'tpopmassn',
+}
+const deleteData = {
+  action: 'delete',
+  table: 'tpopmassn',
+}
+const markForMovingData = {
+  action: 'markForMoving',
+  table: 'tpopmassn',
+}
+const markForCopyingData = {
+  action: 'markForCopying',
+  table: 'tpopmassn',
+}
+const resetCopyingData = {
+  action: 'resetCopying',
+}
+
 const Tpopmassn = ({
   treeName,
   onClick,
@@ -33,49 +54,20 @@ const Tpopmassn = ({
         <div className="react-contextmenu-title">Massnahme</div>
         {!userIsReadOnly(user.token) && (
           <>
-            <MenuItem
-              onClick={onClick}
-              data={{
-                action: 'insert',
-                table: 'tpopmassn',
-              }}
-            >
+            <MenuItem onClick={onClick} data={insertData}>
               erstelle neue
             </MenuItem>
-            <MenuItem
-              onClick={onClick}
-              data={{
-                action: 'delete',
-                table: 'tpopmassn',
-              }}
-            >
+            <MenuItem onClick={onClick} data={deleteData}>
               lösche
             </MenuItem>
-            <MenuItem
-              onClick={onClick}
-              data={{
-                action: 'markForMoving',
-                table: 'tpopmassn',
-              }}
-            >
+            <MenuItem onClick={onClick} data={markForMovingData}>
               verschiebe
             </MenuItem>
-            <MenuItem
-              onClick={onClick}
-              data={{
-                action: 'markForCopying',
-                table: 'tpopmassn',
-              }}
-            >
+            <MenuItem onClick={onClick} data={markForCopyingData}>
               kopiere
             </MenuItem>
             {copying.table && (
-              <MenuItem
-                onClick={onClick}
-                data={{
-                  action: 'resetCopying',
-                }}
-              >
+              <MenuItem onClick={onClick} data={resetCopyingData}>
                 Kopieren aufheben
               </MenuItem>
             )}
