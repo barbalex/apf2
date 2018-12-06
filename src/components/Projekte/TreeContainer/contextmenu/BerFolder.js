8b6fd2cc8fc76edd3cb2e6fe6c7bf4cba26e0af4
Alexander Gabriel <alex.barbalex@gmail.com>
2018-12-06 12:11:01 +0100

    optimize

--- src/components/Projekte/TreeContainer/contextmenu/BerFolder.js
+++ src/components/Projekte/TreeContainer/contextmenu/BerFolder.js
@@ -7,6 +7,12 @@ import ErrorBoundary from '../../../shared/ErrorBoundary'
 import userIsReadOnly from '../../../../modules/userIsReadOnly'
 import mobxStoreContext from '../../../../mobxStoreContext'
 
+// create objects outside render
+const insertData = {
+  action: 'insert',
+  table: 'ber',
+}
+
 const BerFolder = ({
   onClick,
   treeName,
@@ -21,13 +27,7 @@ const BerFolder = ({
       <ContextMenu id={`${treeName}berFolder`}>
         <div className="react-contextmenu-title">Bericht</div>
         {!userIsReadOnly(user.token) && (
-          <MenuItem
-            onClick={onClick}
-            data={{
-              action: 'insert',
-              table: 'ber',
-            }}
-          >
+          <MenuItem onClick={onClick} data={insertData}>
             erstelle neuen
           </MenuItem>
         )}
