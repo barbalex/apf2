6e75680d38c98f7de9313c36532a427269db9bff
Alexander Gabriel <alex.barbalex@gmail.com>
2018-12-07 22:18:29 +0100

    use hooks

--- src/components/Projekte/Karte/MeasureControl.js
+++ src/components/Projekte/Karte/MeasureControl.js
@@ -1,35 +1,31 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
 import 'leaflet'
 import { withLeaflet } from 'react-leaflet'
-import compose from 'recompose/compose'
 // regular import results in error,
 // see: https://github.com/ljagis/leaflet-measure/issues/68
 // import 'leaflet-measure'
 import '../../../../node_modules/leaflet-measure/dist/leaflet-measure'
 
-const enhance = compose(withLeaflet)
+const options = {
+  primaryLengthUnit: 'meters',
+  secondaryLengthUnit: 'kilometers',
+  primaryAreaUnit: 'sqmeters',
+  secondaryAreaUnit: 'hectares',
+  localization: 'de_CH',
+  activeColor: '#f45942',
+  completedColor: '#b22c25',
+  thousandsSep: `'`,
+  decPoint: '.',
+}
+const style = { display: 'none' }
 
-class MeasureControl extends Component {
-  componentDidMount() {
-    const { leaflet } = this.props
-    const options = {
-      primaryLengthUnit: 'meters',
-      secondaryLengthUnit: 'kilometers',
-      primaryAreaUnit: 'sqmeters',
-      secondaryAreaUnit: 'hectares',
-      localization: 'de_CH',
-      activeColor: '#f45942',
-      completedColor: '#b22c25',
-      thousandsSep: `'`,
-      decPoint: '.',
-    }
+const MeasureControl = ({ leaflet }: { leaflet: Object }) => {
+  useEffect(() => {
     const measureControl = new window.L.Control.Measure(options)
     measureControl.addTo(leaflet.map)
-  }
+  }, [])
 
-  render() {
-    return <div style={{ display: 'none' }} />
-  }
+  return <div style={style} />
 }
 
-export default enhance(MeasureControl)
+export default withLeaflet(MeasureControl)
