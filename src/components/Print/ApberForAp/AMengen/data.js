f8ffadccf9f5bdf06ac0dfeda9dd4f8d158df7a4
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2018-12-30 12:24:41 +0100

    correct error

--- src/components/Print/ApberForAp/AMengen/data.js
+++ src/components/Print/ApberForAp/AMengen/data.js
@@ -91,7 +91,7 @@ export default gql`
       sevenLPop: popsByApId(
         filter: {
           status: { equalTo: 101 }
-          and: [
+          or: [
             { status: { equalTo: 202 } }
             {
               or: [
