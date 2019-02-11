1c531779c18f3cef2a52a59b45b99ca336a0465a
Alexander Gabriel <alex.barbalex@gmail.com>
2019-02-11 18:35:23 +0100

    correct error

--- src/components/Projekte/TreeContainer/allParentNodesAreOpen.js
+++ src/components/Projekte/TreeContainer/allParentNodesAreOpen.js
@@ -3,15 +3,17 @@ import isNodeOpen from './isNodeOpen'
 
 export default (
   openNodes: Array<Array<String>>,
-  nodePassed: Array<String>
+  urlPassed: Array<String>,
 ): Boolean => {
   let parentNodes = []
-  const node = [...nodePassed]
-  for (let i = 1; i < node.length; i++) {
-    parentNodes.push(node.slice(0, i))
+  const url = [...urlPassed]
+  for (let i = 1; i < url.length; i++) {
+    parentNodes.push(url.slice(0, i))
   }
   // remove 'Projekte' as that is not contained in openNodes
-  parentNodes = parentNodes.filter(n => !(n.length === 1 && n[0] === 'Projekte'))
+  parentNodes = parentNodes.filter(
+    n => !(n.length === 1 && n[0] === 'Projekte'),
+  )
   if (parentNodes.length === 0) return true
   return parentNodes.every(n => isNodeOpen(openNodes, n))
 }
