e7c50fc1dbf15f1d8327ae74109162bf7c16ed0a
Alexander Gabriel <alex.barbalex@gmail.com>
2019-03-06 20:32:41 +0100

    add test

--- cypress/integration/navigate.js
+++ cypress/integration/navigate.js
@@ -10,15 +10,18 @@ describe('Navigation', function() {
     cy.url().should('include', '/Projekte/e57f56f4-4376-11e8-ab21-4314b6749d13')
     cy.get('.nav-strukturbaum').click()
   })
-  it('shows Strukturbaum', function() {
-    cy.visit('http://localhost:3000')
-    cy.url().should('include', '/Projekte/e57f56f4-4376-11e8-ab21-4314b6749d13')
-    cy.get('.nav-strukturbaum').click()
-  })
   it('opens Aktionspläne', function() {
     cy.visit('http://localhost:3000')
     cy.url().should('include', '/Projekte/e57f56f4-4376-11e8-ab21-4314b6749d13')
     cy.contains('Aktionspläne').click()
     cy.contains('Abies alba Mill.').click()
+  })
+  it('updates ap Abies alba', function() {
+    cy.visit('http://localhost:3000')
+    cy.url().should('include', '/Projekte/e57f56f4-4376-11e8-ab21-4314b6749d13')
+    cy.contains('Aktionspläne').click()
+    cy.contains('Abies alba Mill.').click()
+    cy.contains('kein Eintrag').click()
+    cy.contains('kein AP').click()
   })
 })
