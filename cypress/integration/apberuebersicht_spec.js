b6b0eeba8382d176bb36819b950740b52d4284e0
Alexander Gabriel <alex.barbalex@gmail.com>
2019-03-07 23:13:00 +0100

    test

---
+++ cypress/integration/apberuebersicht_spec.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-undef */
+
+describe('Adresse form', () => {
+  before(() => {
+    cy.visit(
+      '/Projekte/e57f56f4-4376-11e8-ab21-4314b6749d13/AP-Berichte/15e116db-3cb0-11e8-b0c5-fb9823ff7051',
+    )
+  })
+  it('has Title AP-Bericht', () => {
+    cy.get('[data-id=form-title]').should(
+      'contain',
+      'AP-Bericht Jahresübersicht',
+    )
+  })
+  it('updates Bemerkungen', () => {
+    const typedText = `Zusammenfassende Beurteilung
+    Im Jahr 2006 wurden...`
+    cy.get('#bemerkungen')
+      .clear()
+      .type(typedText)
+      .should('have.value', typedText)
+  })
+})
