d1425ef6511f5d3badab84c1490fbc2807bf8f64
Alexander Gabriel <alex.barbalex@gmail.com>
2019-03-08 17:17:39 +0100

    test

---
+++ cypress/integration/ziel.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-undef */
+
+describe('Ziel form', () => {
+  before(() => {
+    cy.visit(
+      '/Projekte/e57f56f4-4376-11e8-ab21-4314b6749d13/Aktionspläne/6c52d174-4f62-11e7-aebe-67a303eb0640/AP-Ziele/1/609d6760-14db-11e9-97a0-1f2adbd510e6',
+    )
+  })
+  it('has Title Ziel', () => {
+    cy.get('[data-id=form-title]').should('contain', 'Ziel')
+  })
+  it('updates jahr', () => {
+    const typedText = '1'
+    cy.get('#jahr')
+      .clear()
+      .type(typedText)
+      .blur()
+      .should('have.value', typedText)
+  })
+  it('updates typ', () => {
+    cy.get('[data-id=typ_1170775556] input')
+      .check()
+      .get('[data-id=typ_1] input')
+      .check()
+      .should('have.value', '1')
+  })
+  it('updates ziel', () => {
+    const typedText = 'test'
+    cy.get('#bezeichnung')
+      .clear()
+      .type(typedText)
+      .blur()
+      .should('have.value', typedText)
+  })
+})
