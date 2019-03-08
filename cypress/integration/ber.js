a7ff44a85ca7a5221bfdc3dd43d1a2b77533c34b
Alexander Gabriel <alex.barbalex@gmail.com>
2019-03-08 16:18:39 +0100

    test

---
+++ cypress/integration/ber.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-undef */
+
+describe('Bericht form', () => {
+  before(() => {
+    cy.visit(
+      '/Projekte/e57f56f4-4376-11e8-ab21-4314b6749d13/Aktionspläne/6c52d174-4f62-11e7-aebe-67a303eb0640/Berichte/2e5d097b-3cbe-11e8-b0c5-3feaa0b4b947',
+    )
+  })
+  it('has Title Bericht', () => {
+    cy.get('[data-id=form-title]').should('contain', 'Bericht')
+  })
+  it('shows testdata-message', () => {
+    cy.get('[data-id=testdata-message]').should('contain', 'Test-Aktionsplan')
+  })
+  it('updates autor', () => {
+    const typedText = 'test'
+    cy.get('#autor')
+      .clear()
+      .type(typedText)
+      .blur()
+      .should('have.value', typedText)
+  })
+  it('updates jahr', () => {
+    const typedText = '2000'
+    cy.get('#jahr')
+      .clear()
+      .type(typedText)
+      .blur()
+      .should('have.value', typedText)
+  })
+  it('updates titel', () => {
+    const typedText = 'test'
+    cy.get('#titel')
+      .clear()
+      .type(typedText)
+      .blur()
+      .should('have.value', typedText)
+  })
+  it('updates url', () => {
+    const typedText = 'https://apflora.ch'
+    cy.get('#url')
+      .clear()
+      .type(typedText)
+      .blur()
+      .should('have.value', typedText)
+  })
+})
