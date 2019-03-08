9723e071ea459fc87d058c7e4a63b0cf3bc9da87
Alexander Gabriel <alex.barbalex@gmail.com>
2019-03-08 17:17:33 +0100

    test

---
+++ cypress/integration/erfkrit.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-undef */
+
+describe('Erfolgs-Kriterien form', () => {
+  before(() => {
+    cy.visit(
+      '/Projekte/e57f56f4-4376-11e8-ab21-4314b6749d13/Aktionspläne/6c52d174-4f62-11e7-aebe-67a303eb0640/AP-Erfolgskriterien/124b46ae-3cc3-11e8-b0c5-8b5949b25bb7',
+    )
+  })
+  it('has Title Erfolgs-Kriterium', () => {
+    cy.get('[data-id=form-title]').should('contain', 'Erfolgs-Kriterium')
+  })
+  it('updates Beurteilung', () => {
+    cy.get('[data-id=erfolg_4] input')
+      .check()
+      .get('[data-id=erfolg_1] input')
+      .check()
+      .should('have.value', '1')
+  })
+  it('updates kriterien', () => {
+    const typedText = 'test'
+    cy.get('#kriterien')
+      .clear()
+      .type(typedText)
+      .blur()
+      .should('have.value', typedText)
+  })
+})
