7adf1b97bd1c0742295759107353590173daaa18
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2019-03-09 21:13:45 +0100

    add test

---
+++ cypress/integration/tpopmassnber.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-undef */
+
+describe('Massnahmen-Bericht Teil-Population form', () => {
+  before(() => {
+    cy.visit(
+      '/Projekte/e57f56f4-4376-11e8-ab21-4314b6749d13/Aktionspläne/6c52d174-4f62-11e7-aebe-67a303eb0640/Populationen/70d2b98f-4f62-11e7-aebe-d3b09a4611dd/Teil-Populationen/76c7fe44-4f62-11e7-aebe-6b56ab796555/Massnahmen-Berichte/711ae39a-3be7-11e8-b0c5-4b1fe3662e10',
+    )
+  })
+  it('has correct Title', () => {
+    cy.get('[data-id=form-title]').should(
+      'contain',
+      'Massnahmen-Bericht Teil-Population',
+    )
+  })
+  it('updates jahr', () => {
+    const typedText = '1'
+    cy.get('#jahr')
+      .clear()
+      .type(typedText)
+      .blur()
+      .should('have.value', typedText)
+  })
+  it('updates entwicklung', () => {
+    cy.get('[data-id=beurteilung_3] input')
+      .check()
+      .get('[data-id=beurteilung_2] input')
+      .check()
+      .should('have.value', '2')
+  })
+  it('updates bemerkungen', () => {
+    const typedText = 'test, bitte nicht löschen'
+    cy.get('#bemerkungen')
+      .clear()
+      .type(typedText)
+      .blur()
+      .should('have.value', typedText)
+  })
+})
