75f8f51dda84db0c0d68713667e7220385d98a53
Alexander Gabriel <alex.barbalex@gmail.com>
2019-03-09 13:31:37 +0100

    test

---
+++ cypress/integration/tpopber.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-undef */
+
+describe('Kontroll-Bericht Teil-Population form', () => {
+  before(() => {
+    cy.visit(
+      '/Projekte/e57f56f4-4376-11e8-ab21-4314b6749d13/Aktionspläne/6c52d174-4f62-11e7-aebe-67a303eb0640/Populationen/70d2b98f-4f62-11e7-aebe-d3b09a4611dd/Teil-Populationen/76c7fe44-4f62-11e7-aebe-6b56ab796555/Kontroll-Berichte/8ecfea40-3bf6-11e8-b0c5-d796ed2e0bd2',
+    )
+  })
+  it('has correct Title', () => {
+    cy.get('[data-id=form-title]').should(
+      'contain',
+      'Kontroll-Bericht Teil-Population',
+    )
+  })
+  it('updates jahr', () => {
+    const typedText = '2000'
+    cy.get('#jahr')
+      .clear()
+      .type(typedText)
+      .blur()
+      .should('have.value', typedText)
+  })
+  it('updates entwicklung', () => {
+    cy.get('[data-id=entwicklung_3] input')
+      .check()
+      .get('[data-id=entwicklung_2] input')
+      .check()
+      .should('have.value', '2')
+  })
+  it('updates bemerkungen', () => {
+    const typedText = 'test, bitte nicht löschen'
+    cy.get('#bemerkungen')
+      .clear()
+      .type(typedText)
+      .blur()
+      .should('have.value', typedText)
+  })
+})
