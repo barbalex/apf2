1ed806a98c6c9de1d6f4104f6d6ccf5c3dc7cc24
Alexander Gabriel <alex.barbalex@gmail.com>
2019-03-10 18:27:51 +0100

    test

---
+++ cypress/integration/tpopfeldkontr.js
@@ -0,0 +1,152 @@
+/* eslint-disable no-undef */
+
+describe('Teil-Population Feldkontrolle form', () => {
+  before(() => {
+    cy.visit(
+      '/Projekte/e57f56f4-4376-11e8-ab21-4314b6749d13/Aktionspläne/6c52d174-4f62-11e7-aebe-67a303eb0640/Populationen/70d2b98f-4f62-11e7-aebe-d3b09a4611dd/Teil-Populationen/76c7fe44-4f62-11e7-aebe-6b56ab796555/Feld-Kontrollen/68364eb3-8be6-11e7-a848-0b73d4b76fcd',
+    )
+  })
+  it('has correct Title', () => {
+    cy.get('[data-id=form-title]').should('contain', 'Feld-Kontrolle')
+  })
+  it('updates jahr', () => {
+    const typedText = '1'
+    cy.get('#jahr')
+      .clear()
+      .type(typedText)
+      .blur()
+      .should('have.value', typedText)
+  })
+  it('datum was nulled when updating jahr', () => {
+    cy.get('[data-id=datum] input').should('have.value', '')
+  })
+  it('datum only accepts valid values', () => {
+    const typedText = '01.02.1001'
+    cy.get('[data-id=datum] input')
+      .clear()
+      .type(typedText)
+      .blur()
+    cy.contains('[data-id=datum] p', 'minimal')
+  })
+  it('updates datum', () => {
+    const typedText = '01.02.2000'
+    cy.get('[data-id=datum] input')
+      .clear()
+      .type(typedText)
+      .blur()
+      .should('have.value', typedText)
+  })
+  it('jahr was set to jahr of datum', () => {
+    const typedText = '2000'
+    cy.get('#jahr').should('have.value', typedText)
+  })
+  it('updates typ', () => {
+    cy.get('[data-id=typ_Ausgangszustand] input')
+      .check()
+      .get('[data-id=typ_Zwischenbeurteilung] input')
+      .check()
+      .should('have.value', 'Zwischenbeurteilung')
+  })
+  it('updates bearbeiter', () => {
+    cy.setSelectOption({
+      selector: '[data-id=bearbeiter]',
+      option: 'Adrienne Frei',
+      value: 'dbc6b98a-4375-11e8-ab21-63812d703dd9',
+    })
+    cy.get('#bemerkungen').focus()
+  })
+  it('updates jungpflanzenAnzahl', () => {
+    const typedText = '5'
+    cy.get('#jungpflanzenAnzahl')
+      .clear()
+      .type(typedText)
+      .blur()
+      .should('have.value', typedText)
+  })
+  it('updates vitalitaet', () => {
+    const typedText = 'test, bitte nicht löschen'
+    cy.get('#vitalitaet')
+      .clear()
+      .type(typedText)
+      .blur()
+      .should('have.value', typedText)
+  })
+  it('updates ueberlebensrate', () => {
+    const typedText = '50'
+    cy.get('#ueberlebensrate')
+      .clear()
+      .type(typedText)
+      .blur()
+      .should('have.value', typedText)
+  })
+  it('updates entwicklung', () => {
+    cy.get('[data-id=entwicklung_3] input')
+      .check()
+      .get('[data-id=entwicklung_2] input')
+      .check()
+      .should('have.value', '2')
+  })
+  it('updates ursachen', () => {
+    const typedText = 'test, bitte nicht löschen'
+    cy.get('#ursachen')
+      .clear()
+      .type(typedText)
+      .blur()
+      .should('have.value', typedText)
+  })
+  it('updates erfolgsbeurteilung', () => {
+    const typedText = 'test, bitte nicht löschen'
+    cy.get('#erfolgsbeurteilung')
+      .clear()
+      .type(typedText)
+      .blur()
+      .should('have.value', typedText)
+  })
+  it('updates umsetzungAendern', () => {
+    const typedText = 'test, bitte nicht löschen'
+    cy.get('#umsetzungAendern')
+      .clear()
+      .type(typedText)
+      .blur()
+      .should('have.value', typedText)
+  })
+  it('updates kontrolleAendern', () => {
+    const typedText = 'test, bitte nicht löschen'
+    cy.get('#kontrolleAendern')
+      .clear()
+      .type(typedText)
+      .blur()
+      .should('have.value', typedText)
+  })
+  it('updates bemerkungen', () => {
+    const typedText = 'test, bitte nicht löschen'
+    cy.get('#bemerkungen')
+      .clear()
+      .type(typedText)
+      .blur()
+      .should('have.value', typedText)
+  })
+  it('has filter icon', () => {
+    cy.get('[data-id=daten-filtern]').should('exist')
+  })
+  it('opens filter form', () => {
+    cy.get('[data-id=daten-filtern]')
+      .click()
+      .get('[data-id=form-title]')
+      .should('contain', 'Filter')
+  })
+  it('closes filter form', () => {
+    cy.get('[data-id=daten-anzeigen]')
+      .click()
+      .get('[data-id=form-title]')
+      .should('not.contain', 'Filter')
+  })
+  it('opens info when info icon is clicked', () => {
+    cy.get('[data-id=info-icon]')
+      .first()
+      .click()
+      .get('[data-id=info-icon-popover')
+      .should('exist')
+    cy.get('#jahr').focus()
+  })
+})
