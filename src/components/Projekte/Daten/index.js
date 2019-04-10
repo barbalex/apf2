ba4895ac5ea1704a5a5dff9d39c35e3b9511b2b2
Alexander Gabriel <alex.barbalex@gmail.com>
2019-04-10 14:08:53 +0200

    work on optimizing

--- src/components/Projekte/Daten/index.js
+++ src/components/Projekte/Daten/index.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { lazy, Suspense, useContext } from 'react'
+import React, { lazy, Suspense, useContext, useCallback } from 'react'
 import styled from 'styled-components'
 import { observer } from 'mobx-react-lite'
 import get from 'lodash/get'
@@ -8,20 +8,9 @@ import Fallback from '../../shared/Fallback'
 import getTableNameFromActiveNode from '../../../modules/getTableNameFromActiveNode'
 import mobxStoreContext from '../../../mobxStoreContext'
 
-const Projekt = lazy(() => import('./Projekt'))
-const Ap = lazy(() => import('./Ap'))
 const User = lazy(() => import('./User'))
-const CurrentIssue = lazy(() => import('./CurrentIssue'))
-const Adresse = lazy(() => import('./Adresse'))
-const Apberuebersicht = lazy(() => import('./Apberuebersicht'))
-const Erfkrit = lazy(() => import('./Erfkrit'))
-const Apber = lazy(() => import('./Apber'))
 const Pop = lazy(() => import('./Pop'))
-const Assozart = lazy(() => import('./Assozart'))
-const Ekfzaehleinheit = lazy(() => import('./Ekfzaehleinheit'))
-const Apart = lazy(() => import('./Apart'))
 const Idealbiotop = lazy(() => import('./Idealbiotop'))
-const Ber = lazy(() => import('./Ber'))
 const Ziel = lazy(() => import('./Ziel'))
 const Zielber = lazy(() => import('./Zielber'))
 const Popmassnber = lazy(() => import('./Popmassnber'))
@@ -35,7 +24,6 @@ const Tpopfreiwkontr = lazy(() => import('./Tpopfreiwkontr'))
 const Tpopkontrzaehl = lazy(() => import('./Tpopkontrzaehl'))
 const Exporte = lazy(() => import('../Exporte'))
 const Qk = lazy(() => import('./Qk'))
-const Beobzuordnung = lazy(() => import('./Beobzuordnung'))
 
 const Container = styled.div`
   height: 100%;
@@ -89,17 +77,101 @@ const Daten = ({
 
   const fOKey = activeTable ? activeTable : key ? key : ''
   console.log('Daten', { fOKey })
+  let form
+  switch (fOKey) {
+    case 'adresse': {
+      const Adresse = lazy(() => import('./Adresse'))
+      form = <Adresse treeName={treeName} />
+      break
+    }
+    case 'ap': {
+      const Ap = lazy(() => import('./Ap'))
+      form = <Ap treeName={treeName} />
+      break
+    }
+    case 'apberuebersicht': {
+      const Apberuebersicht = lazy(() => import('./Apberuebersicht'))
+      form = <Apberuebersicht treeName={treeName} />
+      break
+    }
+    case 'apart': {
+      const Apart = lazy(() => import('./Apart'))
+      form = <Apart treeName={treeName} />
+      break
+    }
+    case 'apber': {
+      const Apber = lazy(() => import('./Apber'))
+      form = <Apber dimensions={dimensions} treeName={treeName} />
+      break
+    }
+    case 'assozart': {
+      const Assozart = lazy(() => import('./Assozart'))
+      form = <Assozart treeName={treeName} />
+      break
+    }
+    case 'beobNichtBeurteilt': {
+      const Beobzuordnung = lazy(() => import('./Beobzuordnung'))
+      form = (
+        <Beobzuordnung
+          dimensions={dimensions}
+          treeName={treeName}
+          type="nichtBeurteilt"
+        />
+      )
+      break
+    }
+    case 'beobNichtZuzuordnen': {
+      const Beobzuordnung = lazy(() => import('./Beobzuordnung'))
+      form = (
+        <Beobzuordnung
+          dimensions={dimensions}
+          treeName={treeName}
+          type="nichtZuzuordnen"
+        />
+      )
+      break
+    }
+    case 'beobZugeordnet': {
+      const Beobzuordnung = lazy(() => import('./Beobzuordnung'))
+      form = (
+        <Beobzuordnung
+          dimensions={dimensions}
+          treeName={treeName}
+          type="zugeordnet"
+        />
+      )
+      break
+    }
+    case 'ber': {
+      const Ber = lazy(() => import('./Ber'))
+      form = <Ber treeName={treeName} />
+      break
+    }
+    case 'currentIssue': {
+      const CurrentIssue = lazy(() => import('./CurrentIssue'))
+      form = <CurrentIssue treeName={treeName} />
+      break
+    }
+    case 'ekfzaehleinheit': {
+      const Ekfzaehleinheit = lazy(() => import('./Ekfzaehleinheit'))
+      form = <Ekfzaehleinheit treeName={treeName} />
+      break
+    }
+    case 'erfkrit': {
+      const Erfkrit = lazy(() => import('./Erfkrit'))
+      form = <Erfkrit treeName={treeName} />
+      break
+    }
+    case 'projekt': {
+      const Projekt = lazy(() => import('./Projekt'))
+      form = <Projekt treeName={treeName} />
+      break
+    }
+    default:
+      form = null
+  }
   const formObject = {
-    projekt: <Projekt treeName={treeName} />,
-    apberuebersicht: <Apberuebersicht treeName={treeName} />,
-    ap: <Ap treeName={treeName} />,
-    assozart: <Assozart treeName={treeName} />,
-    ekfzaehleinheit: <Ekfzaehleinheit treeName={treeName} />,
-    apart: <Apart treeName={treeName} />,
     idealbiotop: <Idealbiotop dimensions={dimensions} treeName={treeName} />,
-    erfkrit: <Erfkrit treeName={treeName} />,
-    apber: <Apber dimensions={dimensions} treeName={treeName} />,
-    ber: <Ber treeName={treeName} />,
     ziel: <Ziel dimensions={dimensions} treeName={treeName} />,
     zielber: <Zielber dimensions={dimensions} treeName={treeName} />,
     pop: <Pop dimensions={dimensions} treeName={treeName} />,
@@ -118,32 +190,8 @@ const Daten = ({
     tpopkontrzaehl: <Tpopkontrzaehl treeName={treeName} />,
     exporte: <Exporte />,
     qk: <Qk treeName={treeName} />,
-    beobNichtZuzuordnen: (
-      <Beobzuordnung
-        dimensions={dimensions}
-        treeName={treeName}
-        type="nichtZuzuordnen"
-      />
-    ),
-    beobNichtBeurteilt: (
-      <Beobzuordnung
-        dimensions={dimensions}
-        treeName={treeName}
-        type="nichtBeurteilt"
-      />
-    ),
-    beobZugeordnet: (
-      <Beobzuordnung
-        dimensions={dimensions}
-        treeName={treeName}
-        type="zugeordnet"
-      />
-    ),
     user: <User treeName={treeName} />,
-    currentIssue: <CurrentIssue treeName={treeName} />,
-    adresse: <Adresse treeName={treeName} />,
   }
-  const form = formObject[fOKey]
 
   console.log('Daten', {
     dimensions,
