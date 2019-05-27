8ad50880093b1a4659dced077af5aba7bc6ae652
Alexander Gabriel <alex.barbalex@gmail.com>
2019-05-27 13:39:02 +0200

    correct background color printing

--- src/pages/Daten.js
+++ src/pages/Daten.js
@@ -1,21 +1,21 @@
-import React, { useEffect, useContext } from "react"
-import styled from "styled-components"
-import { observer } from "mobx-react-lite"
+import React, { useEffect, useContext } from 'react'
+import styled from 'styled-components'
+import { observer } from 'mobx-react-lite'
 
-import ErrorBoundary from "../components/shared/ErrorBoundary"
-import Layout from "../components/Layout"
-import storeContext from "../storeContext"
-import idbContext from "../idbContext"
-import getActiveNodeArrayFromPathname from "../modules/getActiveNodeArrayFromPathname"
-import setUserFromIdb from "../modules/setUserFromIdb"
-import Projekte from "../components/Projekte"
-import User from "../components/User"
-import Errors from "../components/Errors"
-import UpdateAvailable from "../components/UpdateAvailable"
-import Messages from "../components/Messages"
-import Ekf from "../components/Ekf"
-import Deletions from "../components/Deletions"
-import initiateDataFromUrl from "../modules/initiateDataFromUrl"
+import ErrorBoundary from '../components/shared/ErrorBoundary'
+import Layout from '../components/Layout'
+import storeContext from '../storeContext'
+import idbContext from '../idbContext'
+import getActiveNodeArrayFromPathname from '../modules/getActiveNodeArrayFromPathname'
+import setUserFromIdb from '../modules/setUserFromIdb'
+import Projekte from '../components/Projekte'
+import User from '../components/User'
+import Errors from '../components/Errors'
+import UpdateAvailable from '../components/UpdateAvailable'
+import Messages from '../components/Messages'
+import Ekf from '../components/Ekf'
+import Deletions from '../components/Deletions'
+import initiateDataFromUrl from '../modules/initiateDataFromUrl'
 
 const Container = styled.div`
   height: calc(100vh - 64px);
@@ -25,6 +25,7 @@ const Container = styled.div`
     margin-top: 0;
     height: auto;
     overflow: visible !important;
+    background-color: white;
   }
 `
 
@@ -54,8 +55,8 @@ const DatenPage = ({ location }) => {
         <Container>
           {!!user.token && (
             <>
-              {view === "ekf" && <Ekf />}
-              {view === "normal" && <Projekte />}
+              {view === 'ekf' && <Ekf />}
+              {view === 'normal' && <Projekte />}
               <Errors />
               <UpdateAvailable />
               <Messages />
