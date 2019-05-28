166ae5a4f148a2f403197d679e7b922376a089fa
Alexander Gabriel <alex.barbalex@gmail.com>
2019-05-28 16:25:04 +0200

    sort by sort

--- src/components/Projekte/Daten/Tpop/queryLists.js
+++ src/components/Projekte/Daten/Tpop/queryLists.js
@@ -2,7 +2,7 @@ import gql from 'graphql-tag'
 
 export default gql`
   query TpopListsQuery {
-    allTpopApberrelevantGrundWertes(orderBy: CODE_ASC) {
+    allTpopApberrelevantGrundWertes(orderBy: SORT_ASC) {
       nodes {
         value: code
         label: text
