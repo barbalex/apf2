e96ae289114a09b03cc34ab3b9253786c30b086f
Alexander Gabriel <alex.barbalex@gmail.com>
2019-05-31 16:36:46 +0200

    flip coordinates

--- sql/apflora/2019-05-29-wgs84.sql
+++ sql/apflora/2019-05-29-wgs84.sql
@@ -7,9 +7,10 @@ select AddGeometryColumn ('apflora', 'beob', 'geom_point', 4326, 'POINT', 2);
 -- select ST_Transform(ST_SetSRID(ST_MakePoint(x, y), 2056), 4326) from apflora.tpop where x is not null and y is not null;
 
 -- 2. update from existing data
-update apflora.tpop set geom_point = ST_Transform(ST_SetSRID(ST_MakePoint(x, y), 2056), 4326) where x is not null and y is not null;
-update apflora.pop set geom_point = ST_Transform(ST_SetSRID(ST_MakePoint(x, y), 2056), 4326) where x is not null and y is not null;
-update apflora.beob set geom_point = ST_Transform(ST_SetSRID(ST_MakePoint(x, y), 2056), 4326) where x is not null and y is not null;
+-- need to flip weirdly as x and y are wrong way round
+update apflora.tpop set geom_point = ST_Transform(ST_FlipCoordinates(ST_SetSRID(ST_MakePoint(y, x), 2056)), 4326) where x is not null and y is not null;
+update apflora.pop set geom_point = ST_Transform(ST_FlipCoordinates(ST_SetSRID(ST_MakePoint(y, x), 2056)), 4326) where x is not null and y is not null;
+update apflora.beob set geom_point = ST_Transform(ST_FlipCoordinates(ST_SetSRID(ST_MakePoint(y, x), 2056)), 4326) where x is not null and y is not null;
 
 -- query to check
 -- select geom_point, round(ST_X(ST_Transform(geom_point, 2056))), x, round(ST_Y(ST_Transform(geom_point, 2056))), y, ST_AsKML(geom_point) from apflora.tpop;
