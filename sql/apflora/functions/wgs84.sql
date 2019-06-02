04b68280f8080ddb20e2d4bede8ff7bd06322abf
Alexander Gabriel <alex.barbalex@gmail.com>
2019-06-02 16:20:59 +0200

    prrevent invalid values

--- sql/apflora/functions/wgs84.sql
+++ sql/apflora/functions/wgs84.sql
@@ -8,23 +8,23 @@
 CREATE OR REPLACE FUNCTION wgs84_lat(apflora.tpop)
   RETURNS float8 AS
 $func$
-    select ST_Y(geom_point) from apflora.tpop where apflora.tpop.id = $1.id
+    select ST_Y(geom_point) from apflora.tpop where apflora.tpop.id = $1.id and is_valid_coordinates(geom_point) = true
 $func$ LANGUAGE SQL STABLE;
 -- drop function if exists wgs84_long(apflora.tpop);
 CREATE OR REPLACE FUNCTION wgs84_long(apflora.tpop)
   RETURNS float8 AS
 $func$
-    select ST_X(geom_point) from apflora.tpop where apflora.tpop.id = $1.id
+    select ST_X(geom_point) from apflora.tpop where apflora.tpop.id = $1.id and is_valid_coordinates(geom_point) = true
 $func$ LANGUAGE SQL STABLE;
 
 -- this works for postgraphile
 -- drop function if exists apflora.tpop_wgs84lat(tpop apflora.tpop);
 CREATE OR REPLACE FUNCTION apflora.tpop_wgs84lat(tpop apflora.tpop) RETURNS float8 AS $$
-  select ST_Y(tpop.geom_point)
+  select ST_Y(tpop.geom_point) where is_valid_coordinates(tpop.geom_point) = true
 $$ LANGUAGE sql STABLE;
 -- drop function if exists apflora.tpop_wgs84long(tpop apflora.tpop);
 CREATE OR REPLACE FUNCTION apflora.tpop_wgs84long(tpop apflora.tpop) RETURNS float8 AS $$
-  select ST_X(tpop.geom_point)
+  select ST_X(tpop.geom_point) where is_valid_coordinates(tpop.geom_point) = true
 $$ LANGUAGE sql STABLE;
 
 -- 2. pop
@@ -32,23 +32,23 @@ $$ LANGUAGE sql STABLE;
 CREATE OR REPLACE FUNCTION wgs84_lat(apflora.pop)
   RETURNS float8 AS
 $func$
-    select ST_Y(geom_point) from apflora.pop where apflora.pop.id = $1.id
+    select ST_Y(geom_point) from apflora.pop where apflora.pop.id = $1.id and is_valid_coordinates(geom_point) = true
 $func$ LANGUAGE SQL STABLE;
 -- drop function if exists wgs84_long(apflora.pop);
 CREATE OR REPLACE FUNCTION wgs84_long(apflora.pop)
   RETURNS float8 AS
 $func$
-    select ST_X(geom_point) from apflora.pop where apflora.pop.id = $1.id
+    select ST_X(geom_point) from apflora.pop where apflora.pop.id = $1.id and is_valid_coordinates(geom_point) = true
 $func$ LANGUAGE SQL STABLE;
 
 -- this works for postgraphile
 -- drop function if exists apflora.pop_wgs84lat(pop apflora.pop);
 CREATE OR REPLACE FUNCTION apflora.pop_wgs84lat(pop apflora.pop) RETURNS float8 AS $$
-  select ST_Y(pop.geom_point)
+  select ST_Y(pop.geom_point) where is_valid_coordinates(pop.geom_point) = true
 $$ LANGUAGE sql STABLE;
 -- drop function if exists apflora.pop_wgs84long(pop apflora.pop);
 CREATE OR REPLACE FUNCTION apflora.pop_wgs84long(pop apflora.pop) RETURNS float8 AS $$
-  select ST_X(pop.geom_point)
+  select ST_X(pop.geom_point) where is_valid_coordinates(pop.geom_point) = true
 $$ LANGUAGE sql STABLE;
 
 -- 3. beob
@@ -56,21 +56,21 @@ $$ LANGUAGE sql STABLE;
 CREATE OR REPLACE FUNCTION wgs84_lat(apflora.beob)
   RETURNS float8 AS
 $func$
-    select ST_Y(geom_point) from apflora.beob where apflora.beob.id = $1.id
+    select ST_Y(geom_point) from apflora.beob where apflora.beob.id = $1.id and is_valid_coordinates(geom_point) = true
 $func$ LANGUAGE SQL STABLE;
 -- drop function if exists wgs84_long(apflora.beob);
 CREATE OR REPLACE FUNCTION wgs84_long(apflora.beob)
   RETURNS float8 AS
 $func$
-    select ST_X(geom_point) from apflora.beob where apflora.beob.id = $1.id
+    select ST_X(geom_point) from apflora.beob where apflora.beob.id = $1.id and is_valid_coordinates(geom_point) = true
 $func$ LANGUAGE SQL STABLE;
 
 -- this works for postgraphile
 -- drop function if exists apflora.beob_wgs84lat(beob apflora.beob);
 CREATE OR REPLACE FUNCTION apflora.beob_wgs84lat(beob apflora.beob) RETURNS float8 AS $$
-  select ST_Y(beob.geom_point)
+  select ST_Y(beob.geom_point) where is_valid_coordinates(beob.geom_point) = true
 $$ LANGUAGE sql STABLE;
 -- drop function if exists apflora.beob_wgs84long(beob apflora.beob);
 CREATE OR REPLACE FUNCTION apflora.beob_wgs84long(beob apflora.beob) RETURNS float8 AS $$
-  select ST_X(beob.geom_point)
+  select ST_X(beob.geom_point) where is_valid_coordinates(beob.geom_point) = true
 $$ LANGUAGE sql STABLE;
