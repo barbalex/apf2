0a49edf495605e587e7676ea5aeddc11a6d41aeb
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2019-06-03 11:28:41 +0200

    update creation instructions

--- sql/apflora/createDatabase.sql
+++ sql/apflora/createDatabase.sql
@@ -11,12 +11,10 @@ create role apflora_manager in group apflora_reader;
 create role apflora_artverantwortlich in group apflora_reader;
 create role apflora_freiwillig;
 create role anon;
-create role authenticator with login password 'USE REAL SECRET HERE' noinherit;
+create role authenticator with login password 'INSERT PASSWORD' noinherit;
 grant connect on database apflora to authenticator;
 grant connect on database apflora to anon;
 grant anon to authenticator;
---GRANT USAGE ON SCHEMA apflora TO apflora_reader;
---GRANT SELECT ON ALL TABLES IN SCHEMA apflora TO apflora_reader;
 
 -- restore from backup, then:
 -- run these two once with real secret (see how_to.md)
