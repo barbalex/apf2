1e18c7ed7dc3d2907b64ea819deb60db579af51e
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2019-06-03 11:27:30 +0200

    update create instructions

--- sql/apflora/createDatabase.sql
+++ sql/apflora/createDatabase.sql
@@ -1,15 +1,17 @@
+-- 1. create db apflora
+-- usually directly in pgAdmin
 CREATE DATABASE apflora encoding 'UTF8';
 create extension if not exists postgis;
 CREATE EXTENSION if not exists pgcrypto;
 CREATE EXTENSION if not exists "uuid-ossp";
 
--- create role reader in pgAdmin, then:
+-- 2. create role reader in pgAdmin, then:
 create role apflora_reader;
 create role apflora_manager in group apflora_reader;
 create role apflora_artverantwortlich in group apflora_reader;
 create role apflora_freiwillig;
 create role anon;
-create role authenticator with login password 'secret' noinherit;
+create role authenticator with login password 'USE REAL SECRET HERE' noinherit;
 grant connect on database apflora to authenticator;
 grant connect on database apflora to anon;
 grant anon to authenticator;
@@ -19,8 +21,3 @@ grant anon to authenticator;
 -- restore from backup, then:
 -- run these two once with real secret (see how_to.md)
 ALTER DATABASE apflora SET "app.jwt_secret" TO 'secret';
-ALTER USER "authenticator" WITH PASSWORD 'secret';
-
--- restore from db.backup
-
--- add roles
