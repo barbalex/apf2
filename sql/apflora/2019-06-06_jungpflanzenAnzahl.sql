86aa1c59c522b2bff8347a5a46b732c11499ae32
Alexander Gabriel <alex.barbalex@gmail.com>
2019-06-06 23:31:14 +0200

    communicate

--- sql/apflora/2019-06-06_jungpflanzenAnzahl.sql
+++ sql/apflora/2019-06-06_jungpflanzenAnzahl.sql
@@ -6,3 +6,6 @@ ALTER TABLE apflora.tpopkontr DROP COLUMN jungpflanzen_anzahl cascade;
 -- 3. rerun all views
 
 -- 4. restart postgraphile
+
+-- 5. communicate
+Das Feld "Anzahl Jungpflanzen" im Formular der Feld-Kontrolle wurde entfernt. Stattdessen: 1. Wird die Anzahl Jungpflanzen nur noch als Zählung erfasst (wie allen anderen Anzahlen) 2. Ist das Feld "Jungpflanzen vorhanden" auch bei Feld-Kontrollen sichtbar (bisher nur bei EKF)
