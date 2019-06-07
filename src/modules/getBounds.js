f694d0091eaffff71e3c0a8376f3455edba25a67
Alexander Gabriel <alex.barbalex@gmail.com>
2019-06-07 15:40:33 +0200

    swap bound coordinates

--- src/modules/getBounds.js
+++ src/modules/getBounds.js
@@ -2,8 +2,8 @@ import bufferBoundsTo50m from './bufferBoundsTo50m'
 
 export default os => {
   if (os.length === 0) return []
-  const xKoords = os.map(p => p.wgs84Long)
-  const yKoords = os.map(p => p.wgs84Lat)
+  const xKoords = os.map(p => p.wgs84Lat)
+  const yKoords = os.map(p => p.wgs84Long)
   const maxX = Math.max(...xKoords)
   const minX = Math.min(...xKoords)
   const maxY = Math.max(...yKoords)
