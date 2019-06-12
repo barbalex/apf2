902391a604458b7a0b5299ff1c22f11a4f81dffb
Alexander Gabriel <alex.barbalex@gmail.com>
2019-06-12 14:15:45 +0200

    refactor file structure

--- src/pages/Dokumentation/Benutzer.js
+++ src/pages/Dokumentation/Benutzer.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import styled from 'styled-components'
 
-import Layout from '../../../components/Layout'
-import Sidebar from '../../../templates/Sidebar'
-import ErrorBoundary from '../../../components/shared/ErrorBoundary'
+import Layout from '../../components/Layout'
+import Sidebar from '../../templates/Sidebar'
+import ErrorBoundary from '../../components/shared/ErrorBoundary'
 
 const Container = styled.div`
   margin-top: 64px;
