0c4328bdc4b3b4d554205c03a76239d7a35f6a33
Alexander Gabriel <alex.barbalex@gmail.com>
2019-06-18 16:29:39 +0200

    add 404

---
+++ src/pages/404.js
@@ -0,0 +1,97 @@
+import React, { useCallback } from 'react'
+import Typography from '@material-ui/core/Typography'
+import { navigate } from 'gatsby'
+import styled from 'styled-components'
+import Img from 'gatsby-image'
+import Button from '@material-ui/core/Button'
+import { graphql } from 'gatsby'
+
+import ErrorBoundary from '../components/shared/ErrorBoundary'
+import Layout from '../components/Layout'
+
+const Container = styled.div`
+  margin-top: 64px;
+  min-height: calc(100vh - 64px);
+`
+const TextContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  font-weight: 700 !important;
+  text-shadow: 2px 2px 3px white, -2px -2px 3px white, 2px -2px 3px white,
+    -2px 2px 3px white;
+`
+const PageTitle = styled(Typography)`
+  font-size: 2em !important;
+  padding: 15px;
+  font-weight: 700 !important;
+`
+const Text = styled(Typography)`
+  font-size: 1.5em !important;
+  padding: 15px;
+  font-weight: 700 !important;
+`
+const StyledButton = styled(Button)`
+  text-shadow: 2px 2px 3px white, -2px -2px 3px white, 2px -2px 3px white,
+    -2px 2px 3px white;
+  border-color: white !important;
+  margin-top: 10px !important;
+`
+
+const bgImageStyle = {
+  position: 'absolute',
+  left: 0,
+  top: 0,
+  width: '100%',
+  height: '100%',
+  zIndex: -1,
+}
+
+const FourOFour = ({ data }) => {
+  const onClickBack = useCallback(() => navigate('/'))
+
+  return (
+    <ErrorBoundary>
+      <Container>
+        <Layout>
+          <Img
+            sizes={data.file.childImageSharp.sizes}
+            fluid={data.file.childImageSharp.fluid}
+            style={bgImageStyle}
+          />
+          <TextContainer>
+            <PageTitle align="center" variant="h6">
+              Oh je
+            </PageTitle>
+          </TextContainer>
+          <TextContainer>
+            <Text align="center" variant="h6">
+              Diese Seite ist nicht verfügbar.
+            </Text>
+          </TextContainer>
+          <TextContainer>
+            <StyledButton variant="outlined" onClick={onClickBack}>
+              Zurück zur Startseite
+            </StyledButton>
+          </TextContainer>
+        </Layout>
+      </Container>
+    </ErrorBoundary>
+  )
+}
+
+export default FourOFour
+
+export const query = graphql`
+  query FourOFourQuery {
+    file(relativePath: { eq: "ophr-ara.jpg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+        sizes {
+          ...GatsbyImageSharpSizes
+        }
+      }
+    }
+  }
+`
