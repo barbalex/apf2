9b9974d2303bbaa5bd386391b8369b897f2f73ae
Alexander Gabriel <alex.barbalex@gmail.com>
2019-06-25 11:37:40 +0200

    use formik

--- src/components/Projekte/Daten/Idealbiotop/index.js
+++ src/components/Projekte/Daten/Idealbiotop/index.js
@@ -1,20 +1,20 @@
-import React, { useState, useCallback, useEffect, useContext } from 'react'
+import React, { useState, useCallback, useContext } from 'react'
 import styled from 'styled-components'
 import get from 'lodash/get'
 import { observer } from 'mobx-react-lite'
 import { useApolloClient, useQuery } from 'react-apollo-hooks'
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
+import { Formik, Form, Field } from 'formik'
 
-import TextField from '../../../shared/TextField2'
-import DateFieldWithPicker from '../../../shared/DateFieldWithPicker'
+import TextField from '../../../shared/TextFieldFormik'
+import DateFieldWithPicker from '../../../shared/DateFieldWithPickerFormik'
 import FormTitle from '../../../shared/FormTitle'
 import constants from '../../../../modules/constants'
 import ErrorBoundary from '../../../shared/ErrorBoundary'
 import query from './query'
 import updateIdealbiotopByIdGql from './updateIdealbiotopById'
 import storeContext from '../../../../storeContext'
-import ifIsNumericAsNumber from '../../../../modules/ifIsNumericAsNumber'
 import Files from '../../../shared/Files'
 import setUrlQueryValue from '../../../../modules/setUrlQueryValue'
 
@@ -62,7 +62,6 @@ const Idealbiotop = ({ treeName }) => {
   const { urlQuery, setUrlQuery } = store
   const client = useApolloClient()
 
-  const [errors, setErrors] = useState({})
   const [tab, setTab] = useState(get(urlQuery, 'idealbiotopTab', 'idealbiotop'))
   const { activeNodeArray, datenWidth } = store[treeName]
 
@@ -77,18 +76,32 @@ const Idealbiotop = ({ treeName }) => {
 
   const row = get(data, 'allIdealbiotops.nodes[0]', {})
 
-  useEffect(() => setErrors({}), [row])
-
-  const saveToDb = useCallback(
-    async event => {
-      const field = event.target.name
-      const value = ifIsNumericAsNumber(event.target.value)
+  const onSubmit = useCallback(
+    async (values, { setErrors }) => {
       try {
         await client.mutate({
           mutation: updateIdealbiotopByIdGql,
           variables: {
             id: row.id,
-            [field]: value,
+            apId: values.apId,
+            erstelldatum: values.erstelldatum,
+            hoehenlage: values.hoehenlage,
+            region: values.region,
+            exposition: values.exposition,
+            besonnung: values.besonnung,
+            hangneigung: values.hangneigung,
+            bodenTyp: values.bodenTyp,
+            bodenKalkgehalt: values.bodenKalkgehalt,
+            bodenDurchlaessigkeit: values.bodenDurchlaessigkeit,
+            bodenHumus: values.bodenHumus,
+            bodenNaehrstoffgehalt: values.bodenNaehrstoffgehalt,
+            wasserhaushalt: values.wasserhaushalt,
+            konkurrenz: values.konkurrenz,
+            moosschicht: values.moosschicht,
+            krautschicht: values.krautschicht,
+            strauchschicht: values.strauchschicht,
+            baumschicht: values.baumschicht,
+            bemerkungen: values.bemerkungen,
             changedBy: store.user.name,
           },
           optimisticResponse: {
@@ -96,36 +109,25 @@ const Idealbiotop = ({ treeName }) => {
             updateIdealbiotopById: {
               idealbiotop: {
                 id: row.id,
-                apId: field === 'apId' ? value : row.apId,
-                erstelldatum:
-                  field === 'erstelldatum' ? value : row.erstelldatum,
-                hoehenlage: field === 'hoehenlage' ? value : row.hoehenlage,
-                region: field === 'region' ? value : row.region,
-                exposition: field === 'exposition' ? value : row.exposition,
-                besonnung: field === 'besonnung' ? value : row.besonnung,
-                hangneigung: field === 'hangneigung' ? value : row.hangneigung,
-                bodenTyp: field === 'bodenTyp' ? value : row.bodenTyp,
-                bodenKalkgehalt:
-                  field === 'bodenKalkgehalt' ? value : row.bodenKalkgehalt,
-                bodenDurchlaessigkeit:
-                  field === 'bodenDurchlaessigkeit'
-                    ? value
-                    : row.bodenDurchlaessigkeit,
-                bodenHumus: field === 'bodenHumus' ? value : row.bodenHumus,
-                bodenNaehrstoffgehalt:
-                  field === 'bodenNaehrstoffgehalt'
-                    ? value
-                    : row.bodenNaehrstoffgehalt,
-                wasserhaushalt:
-                  field === 'wasserhaushalt' ? value : row.wasserhaushalt,
-                konkurrenz: field === 'konkurrenz' ? value : row.konkurrenz,
-                moosschicht: field === 'moosschicht' ? value : row.moosschicht,
-                krautschicht:
-                  field === 'krautschicht' ? value : row.krautschicht,
-                strauchschicht:
-                  field === 'strauchschicht' ? value : row.strauchschicht,
-                baumschicht: field === 'baumschicht' ? value : row.baumschicht,
-                bemerkungen: field === 'bemerkungen' ? value : row.bemerkungen,
+                apId: values.apId,
+                erstelldatum: values.erstelldatum,
+                hoehenlage: values.hoehenlage,
+                region: values.region,
+                exposition: values.exposition,
+                besonnung: values.besonnung,
+                hangneigung: values.hangneigung,
+                bodenTyp: values.bodenTyp,
+                bodenKalkgehalt: values.bodenKalkgehalt,
+                bodenDurchlaessigkeit: values.bodenDurchlaessigkeit,
+                bodenHumus: values.bodenHumus,
+                bodenNaehrstoffgehalt: values.bodenNaehrstoffgehalt,
+                wasserhaushalt: values.wasserhaushalt,
+                konkurrenz: values.konkurrenz,
+                moosschicht: values.moosschicht,
+                krautschicht: values.krautschicht,
+                strauchschicht: values.strauchschicht,
+                baumschicht: values.baumschicht,
+                bemerkungen: values.bemerkungen,
                 __typename: 'Idealbiotop',
               },
               __typename: 'Idealbiotop',
@@ -133,7 +135,43 @@ const Idealbiotop = ({ treeName }) => {
           },
         })
       } catch (error) {
-        return setErrors({ [field]: error.message })
+        const { message } = error
+        const field = message.includes('$erstelldatum')
+          ? 'erstelldatum'
+          : message.includes('$hoehenlage')
+          ? 'hoehenlage'
+          : message.includes('$region')
+          ? 'region'
+          : message.includes('$exposition')
+          ? 'exposition'
+          : message.includes('$besonnung')
+          ? 'besonnung'
+          : message.includes('$hangneigung')
+          ? 'hangneigung'
+          : message.includes('$bodenTyp')
+          ? 'bodenTyp'
+          : message.includes('$bodenKalkgehalt')
+          ? 'bodenKalkgehalt'
+          : message.includes('$bodenDurchlaessigkeit')
+          ? 'bodenDurchlaessigkeit'
+          : message.includes('$bodenHumus')
+          ? 'bodenHumus'
+          : message.includes('$bodenNaehrstoffgehalt')
+          ? 'bodenNaehrstoffgehalt'
+          : message.includes('$wasserhaushalt')
+          ? 'wasserhaushalt'
+          : message.includes('$konkurrenz')
+          ? 'konkurrenz'
+          : message.includes('$moosschicht')
+          ? 'moosschicht'
+          : message.includes('$krautschicht')
+          ? 'krautschicht'
+          : message.includes('$strauchschicht')
+          ? 'strauchschicht'
+          : message.includes('$baumschicht')
+          ? 'baumschicht'
+          : 'bemerkungen'
+        return setErrors({ [field]: message })
       }
       setErrors({})
     },
@@ -148,6 +186,8 @@ const Idealbiotop = ({ treeName }) => {
     })
     setTab(value)
   })
+
+  console.log('Idealbiotop rendering')
 
   if (loading) {
     return (
@@ -183,187 +223,143 @@ const Idealbiotop = ({ treeName }) => {
           </Tabs>
           {tab === 'idealbiotop' && (
             <FormContainer data-width={datenWidth}>
-              <DateFieldWithPicker
-                key={`${row.id}erstelldatum`}
-                name="erstelldatum"
-                label="Erstelldatum"
-                value={row.erstelldatum}
-                saveToDb={saveToDb}
-                error={errors.erstelldatum}
-              />
-              <Section>Lage</Section>
-              <TextField
-                key={`${row.id}hoehenlage`}
-                name="hoehenlage"
-                label="Höhe"
-                row={row}
-                type="text"
-                multiLine
-                saveToDb={saveToDb}
-                errors={errors}
-              />
-              <TextField
-                key={`${row.id}region`}
-                name="region"
-                label="Region"
-                row={row}
-                type="text"
-                multiLine
-                saveToDb={saveToDb}
-                errors={errors}
-              />
-              <TextField
-                key={`${row.id}exposition`}
-                name="exposition"
-                label="Exposition"
-                row={row}
-                type="text"
-                multiLine
-                saveToDb={saveToDb}
-                errors={errors}
-              />
-              <TextField
-                key={`${row.id}besonnung`}
-                name="besonnung"
-                label="Besonnung"
-                row={row}
-                type="text"
-                multiLine
-                saveToDb={saveToDb}
-                errors={errors}
-              />
-              <TextField
-                key={`${row.id}hangneigung`}
-                name="hangneigung"
-                label="Hangneigung"
-                row={row}
-                type="text"
-                multiLine
-                saveToDb={saveToDb}
-                errors={errors}
-              />
-              <Section>Boden</Section>
-              <TextField
-                key={`${row.id}bodenTyp`}
-                name="bodenTyp"
-                label="Typ"
-                row={row}
-                type="text"
-                multiLine
-                saveToDb={saveToDb}
-                errors={errors}
-              />
-              <TextField
-                key={`${row.id}bodenKalkgehalt`}
-                name="bodenKalkgehalt"
-                label="Kalkgehalt"
-                row={row}
-                type="text"
-                multiLine
-                saveToDb={saveToDb}
-                errors={errors}
-              />
-              <TextField
-                key={`${row.id}bodenDurchlaessigkeit`}
-                name="bodenDurchlaessigkeit"
-                label="Durchlässigkeit"
-                row={row}
-                type="text"
-                multiLine
-                saveToDb={saveToDb}
-                errors={errors}
-              />
-              <TextField
-                key={`${row.id}bodenHumus`}
-                name="bodenHumus"
-                label="Humus"
-                row={row}
-                type="text"
-                multiLine
-                saveToDb={saveToDb}
-                errors={errors}
-              />
-              <TextField
-                key={`${row.id}bodenNaehrstoffgehalt`}
-                name="bodenNaehrstoffgehalt"
-                label="Nährstoffgehalt"
-                row={row}
-                type="text"
-                multiLine
-                saveToDb={saveToDb}
-                errors={errors}
-              />
-              <TextField
-                key={`${row.id}wasserhaushalt`}
-                name="wasserhaushalt"
-                label="Wasserhaushalt"
-                row={row}
-                type="text"
-                multiLine
-                saveToDb={saveToDb}
-                errors={errors}
-              />
-              <Section>Vegetation</Section>
-              <TextField
-                key={`${row.id}konkurrenz`}
-                name="konkurrenz"
-                label="Konkurrenz"
-                row={row}
-                type="text"
-                multiLine
-                saveToDb={saveToDb}
-                errors={errors}
-              />
-              <TextField
-                key={`${row.id}moosschicht`}
-                name="moosschicht"
-                label="Moosschicht"
-                row={row}
-                type="text"
-                multiLine
-                saveToDb={saveToDb}
-                errors={errors}
-              />
-              <TextField
-                key={`${row.id}Krautschicht`}
-                name="krautschicht"
-                label="Krautschicht"
-                row={row}
-                type="text"
-                multiLine
-                saveToDb={saveToDb}
-                errors={errors}
-              />
-              <TextField
-                key={`${row.id}Strauchschicht`}
-                name="strauchschicht"
-                label="Strauchschicht"
-                row={row}
-                type="text"
-                multiLine
-                saveToDb={saveToDb}
-                errors={errors}
-              />
-              <TextField
-                key={`${row.id}baumschicht`}
-                name="baumschicht"
-                label="Baumschicht"
-                row={row}
-                type="text"
-                multiLine
-                saveToDb={saveToDb}
-                errors={errors}
-              />
-              <TextField
-                key={`${row.id}bemerkungen`}
-                name="bemerkungen"
-                label="Bemerkungen"
-                row={row}
-                type="text"
-                multiLine
-                saveToDb={saveToDb}
-                errors={errors}
-              />
+              <Formik
+                initialValues={row}
+                onSubmit={onSubmit}
+                enableReinitialize
+              >
+                {({ isSubmitting, handleSubmit, dirty }) => (
+                  <Form onBlur={() => dirty && handleSubmit()}>
+                    <Field
+                      name="erstelldatum"
+                      label="Erstelldatum"
+                      component={DateFieldWithPicker}
+                    />
+                    <Section>Lage</Section>
+                    <Field
+                      name="hoehenlage"
+                      label="Höhe"
+                      type="text"
+                      component={TextField}
+                      multiLine
+                    />
+                    <Field
+                      name="region"
+                      label="Region"
+                      type="text"
+                      component={TextField}
+                      multiLine
+                    />
+                    <Field
+                      name="exposition"
+                      label="Exposition"
+                      type="text"
+                      component={TextField}
+                      multiLine
+                    />
+                    <Field
+                      name="besonnung"
+                      label="Besonnung"
+                      type="text"
+                      component={TextField}
+                      multiLine
+                    />
+                    <Field
+                      name="hangneigung"
+                      label="Hangneigung"
+                      type="text"
+                      component={TextField}
+                      multiLine
+                    />
+                    <Section>Boden</Section>
+                    <Field
+                      name="bodenTyp"
+                      label="Typ"
+                      type="text"
+                      component={TextField}
+                      multiLine
+                    />
+                    <Field
+                      name="bodenKalkgehalt"
+                      label="Kalkgehalt"
+                      type="text"
+                      component={TextField}
+                      multiLine
+                    />
+                    <Field
+                      name="bodenDurchlaessigkeit"
+                      label="Durchlässigkeit"
+                      type="text"
+                      component={TextField}
+                      multiLine
+                    />
+                    <Field
+                      name="bodenHumus"
+                      label="Humus"
+                      type="text"
+                      component={TextField}
+                      multiLine
+                    />
+                    <Field
+                      name="bodenNaehrstoffgehalt"
+                      label="Nährstoffgehalt"
+                      type="text"
+                      component={TextField}
+                      multiLine
+                    />
+                    <Field
+                      name="wasserhaushalt"
+                      label="Wasserhaushalt"
+                      type="text"
+                      component={TextField}
+                      multiLine
+                    />
+                    <Section>Vegetation</Section>
+                    <Field
+                      name="konkurrenz"
+                      label="Konkurrenz"
+                      type="text"
+                      component={TextField}
+                      multiLine
+                    />
+                    <Field
+                      name="moosschicht"
+                      label="Moosschicht"
+                      type="text"
+                      component={TextField}
+                      multiLine
+                    />
+                    <Field
+                      name="krautschicht"
+                      label="Krautschicht"
+                      type="text"
+                      component={TextField}
+                      multiLine
+                    />
+                    <Field
+                      name="strauchschicht"
+                      label="Strauchschicht"
+                      type="text"
+                      component={TextField}
+                      multiLine
+                    />
+                    <Field
+                      name="baumschicht"
+                      label="Baumschicht"
+                      type="text"
+                      component={TextField}
+                      multiLine
+                    />
+                    <Field
+                      name="bemerkungen"
+                      label="Bemerkungen"
+                      type="text"
+                      component={TextField}
+                      multiLine
+                    />
+                  </Form>
+                )}
+              </Formik>
             </FormContainer>
           )}
           {tab === 'dateien' && (
