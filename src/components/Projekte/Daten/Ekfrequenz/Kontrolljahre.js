7bd93ccd8b85178492e5c6a73991d5bc6c13b67d
Alexander Gabriel <alex.barbalex@gmail.com>
2019-07-09 00:20:24 +0200

    work on kontrolljahre

--- src/components/Projekte/Daten/Ekfrequenz/Kontrolljahre.js
+++ src/components/Projekte/Daten/Ekfrequenz/Kontrolljahre.js
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from 'react'
 
+/**
+ * build an array of fields
+ * set state on mount
+ * write to db on blur
+ */
 const Kontrolljahre = kontrolljahre => {
   console.log('Kontrolljahre:', kontrolljahre)
-  return <div>Kontrolljahre</div>
+  return <div>{'Kontrolljahre (TODO)'}</div>
 }
 
 export default Kontrolljahre
