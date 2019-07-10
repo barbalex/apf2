e2054866298cc82bd9433ad9c922a9acc29cf924
Alexander Gabriel <alex.barbalex@gmail.com>
2019-07-10 13:23:35 +0200

    catch graphql errors

--- src/client.js
+++ src/client.js
@@ -4,11 +4,13 @@ import { BatchHttpLink } from 'apollo-link-batch-http'
 import { setContext } from 'apollo-link-context'
 import { InMemoryCache, defaultDataIdFromObject } from 'apollo-cache-inmemory'
 import { ApolloLink } from 'apollo-link'
+import { onError } from 'apollo-link-error'
 import jwtDecode from 'jwt-decode'
 
 import graphQlUri from './modules/graphQlUri'
 
 export default ({ idb, store }) => {
+  const { enqueNotification } = store
   const authLink = setContext((_, { headers }) => {
     const { token } = store.user
     if (token) {
@@ -28,6 +30,30 @@ export default ({ idb, store }) => {
     return { headers }
   })
 
+  const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors)
+      graphQLErrors.map(({ message, locations, path }) => {
+        console.log(
+          `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+        )
+        return enqueNotification({
+          message: `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+          options: {
+            variant: 'error',
+          },
+        })
+      })
+    if (networkError) {
+      console.log(`[Network error]: ${networkError}`)
+      enqueNotification({
+        message: `[Network error]: ${networkError}`,
+        options: {
+          variant: 'error',
+        },
+      })
+    }
+  })
+
   const cache = new InMemoryCache({
     dataIdFromObject: object => {
       if (object.id && isNaN(object.id)) return object.id
@@ -41,7 +67,7 @@ export default ({ idb, store }) => {
   })*/
   const batchHttpLink = new BatchHttpLink({ uri: graphQlUri() })
   const client = new ApolloClient({
-    link: ApolloLink.from([authLink, batchHttpLink]),
+    link: ApolloLink.from([errorLink, authLink, batchHttpLink]),
     cache,
     defaultOptions: { fetchPolicy: 'cache-and-network' },
   })
