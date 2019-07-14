3105113b13103f9f5d72673ef539c12fae72b0e0
Alexander Gabriel <alex.barbalex@gmail.com>
2019-07-14 23:55:14 +0200

    correct error

--- src/components/Projekte/TreeContainer/Tree/index.js
+++ src/components/Projekte/TreeContainer/Tree/index.js
@@ -8,7 +8,7 @@ import { observer } from 'mobx-react-lite'
 import { useQuery } from 'react-apollo-hooks'
 import jwtDecode from 'jwt-decode'
 
-import Row from '.'
+import Row from './Row'
 
 import storeContext from '../../../../storeContext'
 import buildVariables from './buildVariables'
