01f03c2fccbf1610b1f6ca8a34f322e85dca83c5
Alexander Gabriel <alex.barbalex@gmail.com>
2019-08-31 12:30:11 +0200

    work on ekplan

--- src/components/EkPlan/Table/setEkplans/index.js
+++ src/components/EkPlan/Table/setEkplans/index.js
@@ -1,6 +1,7 @@
 import get from 'lodash/get'
 
 import queryEkplans from './queryEkplans'
+import queryEkfrequenz from './queryEkfrequenz'
 import mutationDeleteEkplans from './mutationDeleteEkplans'
 
 export default async ({
@@ -17,9 +18,9 @@ export default async ({
     ekfrequenzStartjahr,
   })
   // 1. query all ekplans beginning with ekfrequenzStartJahr
-  let ekplansResult
+  let ekplansToDeleteResult
   try {
-    ekplansResult = await client.query({
+    ekplansToDeleteResult = await client.query({
       query: queryEkplans,
       variables: {
         tpopId,
@@ -28,25 +29,55 @@ export default async ({
     })
   } catch (error) {
     return store.enqueNotification({
-      message: error.message,
+      message: `Fehler beim Abfragen der bisherigen EK-Pläne: ${error.message}`,
       options: {
         variant: 'error',
       },
     })
   }
-  const ekplansToDelete = get(ekplansResult, 'data.allEkplans.nodes').map(
-    e => e.id,
-  )
+  const ekplansToDelete = get(
+    ekplansToDeleteResult,
+    'data.allEkplans.nodes',
+  ).map(e => e.id)
   // 2. delete them
-  ekplansToDelete.map(id =>
-    client.mutate({
-      mutation: mutationDeleteEkplans,
-      variables: {
-        id,
-      },
-    }),
-  )
+  ekplansToDelete.forEach(id => {
+    try {
+      client.mutate({
+        mutation: mutationDeleteEkplans,
+        variables: {
+          id,
+        },
+      })
+    } catch (error) {
+      return store.enqueNotification({
+        message: `Fehler beim Löschen der bisherigen EK-Pläne: ${error.message}`,
+        options: {
+          variant: 'error',
+        },
+      })
+    }
+  })
   // 3. fetch ekfrequenz.kontrolljahre for this tpop.ekfrequenz
+  let ekfrequenzsResult
+  try {
+    ekfrequenzsResult = await client.query({
+      query: queryEkfrequenz,
+      variables: {
+        code: ekfrequenzCode,
+      },
+    })
+  } catch (error) {
+    return store.enqueNotification({
+      message: `Fehler beim Abfragen der Kontrolljahre: ${error.message}`,
+      options: {
+        variant: 'error',
+      },
+    })
+  }
+  const kontrolljahre = get(ekfrequenzsResult, 'data.allEkfrequenzs.nodes').map(
+    e => e.kontrolljahre,
+  )
+  console.log('setEkplans, kontrolljahre:', kontrolljahre)
   // 4. add kontrolljahre to ekplan
   // 5. tell user how it went
   setTimeout(() => refetchTpop())
--- src/components/EkPlan/Table/setEkplans/queryEkfrequenz.js
+++ src/components/EkPlan/Table/setEkplans/queryEkfrequenz.js
@@ -1,15 +1,11 @@
 import gql from 'graphql-tag'
 
 export default gql`
-  query EkfrequenzQuery($code: Int!) {
-    allEkplans(
-      filter: {
-        jahr: { greaterThanOrEqualTo: $jahr }
-        tpopId: { equalTo: $tpopId }
-      }
-    ) {
+  query EkfrequenzQuery($code: String!) {
+    allEkfrequenzs(filter: { code: { equalTo: $code } }) {
       nodes {
         id
+        kontrolljahre
       }
     }
   }
