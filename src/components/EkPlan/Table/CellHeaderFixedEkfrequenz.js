655748c4d5a871c1be4f644cfd06a66e068e11fd
Alexander Gabriel <alex.barbalex@gmail.com>
2019-09-01 18:50:33 +0200

    work on filtering

--- src/components/EkPlan/Table/CellHeaderFixedEkfrequenz.js
+++ src/components/EkPlan/Table/CellHeaderFixedEkfrequenz.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
 
 import { StyledFixedHeaderCell } from './index'
 
@@ -8,12 +10,32 @@ import { StyledFixedHeaderCell } from './index'
  */
 
 const CellHeaderFixed = ({ style, column }) => {
+  const [anchorEl, setAnchorEl] = useState(null)
+  const closeMenu = useCallback(() => setAnchorEl(null), [])
+  const handleClick = useCallback(e => setAnchorEl(e.currentTarget), [])
+
   const { label } = column
 
   return (
-    <StyledFixedHeaderCell style={style}>
-      <span>{label}</span>
-    </StyledFixedHeaderCell>
+    <>
+      <StyledFixedHeaderCell
+        style={style}
+        aria-controls="ekfrequenzHeaderMenu"
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        <span>{label}</span>
+      </StyledFixedHeaderCell>
+      <Menu
+        id="ekfrequenzHeaderMenu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={closeMenu}
+      >
+        <MenuItem onClick={closeMenu}>Filter: Leerwerte</MenuItem>
+      </Menu>
+    </>
   )
 }
 
