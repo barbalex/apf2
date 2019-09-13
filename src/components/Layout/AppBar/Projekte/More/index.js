199abde5207758bfbd438f9fd1358c70590c76cc
Alexander Gabriel <alex.barbalex@gmail.com>
2019-09-13 18:53:11 +0200

    up version

--- src/components/Layout/AppBar/Projekte/More/index.js
+++ src/components/Layout/AppBar/Projekte/More/index.js
@@ -126,7 +126,7 @@ const AppbarMore = ({ onClickExporte: passedOnClickExporte, role }) => {
         <MenuItem onClick={onClickReload}>
           Cache leeren und apflora neu laden
         </MenuItem>
-        <Version>Version: 1.10.6 vom 13.09.2019</Version>
+        <Version>Version: 1.10.7 vom 13.09.2019</Version>
       </Menu>
     </Container>
   )
