eb63aacf18d6d7dc7bb7f0d3e5c09dd2426d2f4f
Alexander Gabriel <alex.barbalex@gmail.com>
2019-09-14 16:16:56 +0200

    use other provider

--- src/components/Projekte/Karte/layers/OsmColor.js
+++ src/components/Projekte/Karte/layers/OsmColor.js
@@ -3,10 +3,10 @@ import { TileLayer } from 'react-leaflet'
 
 const OsmColorLayer = () => (
   <TileLayer
-    //url="//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"  // only loads sporadic, 2018.06.06
+    url="//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" // only loads sporadic, 2018.06.06. Reinstated 2019.09.14 because supports cors
     //url="//{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png"  // did not work
     //url="//{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"  // did not work
-    url="//tiles.wmflabs.org/osm/{z}/{x}/{y}.png"
+    //url="//tiles.wmflabs.org/osm/{z}/{x}/{y}.png"
     attribution='&copy; <a href="//osm.org/copyright">OpenStreetMap</a>'
     maxNativeZoom={19}
     minZoom={0}
