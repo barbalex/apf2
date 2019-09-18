340fa89b52c0d1ccf4e15bec096558737353a17e
Alexander Gabriel <alex.barbalex@gmail.com>
2019-09-18 17:38:54 +0200

    catch objects

--- src/modules/objectsFindChangedKey.js
+++ src/modules/objectsFindChangedKey.js
@@ -5,9 +5,13 @@
  */
 export default (a, b) => {
   for (const [key, value] of Object.entries(a)) {
+    // DANGER
+    // if query fetched dependent data, there will be objects with that data contained
+    // these objects are always inequal so wrong field will be returned!
+    // so do not compare object values!
     // forms convert everything to strings!!!
     // eslint-disable-next-line eqeqeq
-    if (value != b[key]) return key
+    if (typeof value !== 'object' && value != b[key]) return key
   }
   return null
 }
