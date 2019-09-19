f9c0d8edadcf2e2220ac8cef5caa557c6a19f2eb
Alexander Gabriel <alex.barbalex@gmail.com>
2019-09-19 17:01:48 +0200

    clean

--- src/components/Layout/AppBar/Projekte/More/index.js
+++ src/components/Layout/AppBar/Projekte/More/index.js
@@ -4,7 +4,6 @@ import MenuItem from '@material-ui/core/MenuItem'
 import Button from '@material-ui/core/Button'
 import styled from 'styled-components'
 import { observer } from 'mobx-react-lite'
-import localForage from 'localforage'
 
 import isMobilePhone from '../../../../../modules/isMobilePhone'
 import logout from '../../../../../modules/logout'
