073f8dcaa485f35ab604aaac9a9ceb1efe465b62
Alexander Gabriel <alex.barbalex@gmail.com>
2019-09-23 18:10:48 +0200

    respect null

--- src/modules/objectsFindChangedKey.js
+++ src/modules/objectsFindChangedKey.js
@@ -9,7 +9,10 @@ export default (a, b) => {
     // if query fetched dependent data, there will be objects with that data contained
     // these objects are always inequal so wrong field will be returned!
     // so do not compare object values!
+    // BUT do respect null (which is an object!)
     // forms convert everything to strings!!!
+    // eslint-disable-next-line eqeqeq
+    if (value === null && value != b[key]) return key
     // eslint-disable-next-line eqeqeq
     if (typeof value !== 'object' && value != b[key]) return key
   }
