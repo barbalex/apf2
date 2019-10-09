b8a4c4827429e4eec34e121055056094bfb34099
Alexander Gabriel <alex.barbalex@gmail.com>
2019-10-09 13:46:55 +0200

    add browser-update

--- src/App.js
+++ src/App.js
@@ -68,6 +68,22 @@ const App = ({ element }) => {
           }
         }),
     )
+
+    const browserUpdateConfiguration = {
+      required: { e: -2, f: -2, o: -2, s: -2, c: -2 },
+      text: {
+        msg: 'Ihr Browser ({brow_name}) ist veraltet.',
+        msgmore:
+          'Aktualisieren Sie ihn für mehr Sicherheit, Geschwindigkeit und weil apflora einen aktuellen Browser voraussetzt.',
+        bupdate: 'Browser aktualisieren',
+        bignore: 'Ignorieren',
+      },
+      style: 'bottom',
+      //test: true,
+    }
+    import('browser-update').then(module =>
+      module.default(browserUpdateConfiguration),
+    )
   }
 
   //onPatch(store, patch => console.log(patch))
