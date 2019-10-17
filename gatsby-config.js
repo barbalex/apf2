c86e4745c6be2edcd8e7e6b375ac1db9f040c2d3
Alexander Gabriel <alex.barbalex@gmail.com>
2019-10-17 15:53:40 +0200

    up version

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'apflora v1.18.0',
+    title: 'apflora v1.18.1',
   },
   plugins: [
     {
--- src/components/Layout/AppBar/Projekte/More/index.js
+++ src/components/Layout/AppBar/Projekte/More/index.js
@@ -112,7 +112,7 @@ const AppbarMore = ({ onClickExporte: passedOnClickExporte, role }) => {
           onClick={onClickLogout}
           data-id="appbar-more-logout"
         >{`${user.name} abmelden (und Cache leeren)`}</MenuItem>
-        <Version>Version: 1.18.0 vom 16.10.2019</Version>
+        <Version>Version: 1.18.1 vom 16.10.2019</Version>
       </Menu>
     </Container>
   )
