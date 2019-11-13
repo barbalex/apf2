2a4dccc7b580fcc18fdbcfda8e75e9c83c1f78c9
Alexander Gabriel <alex.barbalex@gmail.com>
2019-11-13 13:55:18 +0100

    prepare import of ekplan

---
+++ sql/apflora/2019-11-13_import_ekplan.sql
@@ -0,0 +1,6 @@
+-- 1. import all arten to tmp_ekplan_ap
+-- 2. import importdata to tmp_ekplan
+-- 3. copy all ekfrequenz from Abies alba to all ap's in tmp_ekplan_ap and all ap's with ap.bearbeitung < 4
+-- 4. import ekplan data from tmp_ekplan in 11 queries (one for every year)
+-- delete tmp_ekplan_ap and tmp_ekplan
+
