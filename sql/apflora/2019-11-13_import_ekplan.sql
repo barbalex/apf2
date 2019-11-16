f949b536263d4f3dd5ea769f2ae516ead5748020
Alexander Gabriel <alex.barbalex@gmail.com>
2019-11-16 12:42:17 +0100

    import ekplan

--- sql/apflora/2019-11-13_import_ekplan.sql
+++ sql/apflora/2019-11-13_import_ekplan.sql
@@ -1,5 +1,5 @@
--- 1. import all arten to tmp_ekplan_ap. Done
--- 2. import importdata to tmp_ekplan. Done
+-- 1. import all arten to tmp_ekplan_ap. done
+-- 2. import importdata to tmp_ekplan. done
 -- 3. copy all ekfrequenz from Abies alba to all ap's in tmp_ekplan_ap and all ap's with ap.bearbeitung < 4
 select * from apflora.ekfrequenz where ap_id = '6c52d174-4f62-11e7-aebe-67a303eb0640'
 select id from apflora.ap where bearbeitung < 4 union select ap_id from apflora.tmp_ekplan_ap
@@ -40,6 +40,12 @@ select
   f.ektyp,
   f.kontrolljahre_ab
 from ids, f 
+-- done
+
+-- check if all tpop_id exist
+SELECT tpop_id 
+FROM apflora.tmp_ekplan
+where tmp_ekplan.tpop_id not in (select id from apflora.tpop)
 
 -- 4. import tpop.ekfrequenz
 with ekfrequenzs as (
@@ -47,13 +53,13 @@ select
   tmp_ekplan.tpop_id,
   (select id from apflora.ekfrequenz where code = tmp_ekplan.ekfrequenz_code and ap_id = ap.id) as ekfrequenzid
 from
-  apflora.tmp_ekplan tmp_ekplan
+  apflora.tmp_ekplan
   inner join apflora.tpop
-    inner join apflora.pop pop
-      inner join apflora.ap ap
-      on ap.id = pop.ap_id
-    on pop.id = apflora.tpop.pop_id
-  on apflora.tpop.id = tmp_ekplan.tpop_id
+    inner join apflora.pop
+      inner join apflora.ap
+      on apflora.ap.id = apflora.pop.ap_id
+    on apflora.pop.id = apflora.tpop.pop_id
+  on apflora.tpop.id = apflora.tmp_ekplan.tpop_id
 )
 update apflora.tpop 
 set ekfrequenz = ekfrequenzs.ekfrequenzid
@@ -61,22 +67,106 @@ from ekfrequenzs
 where ekfrequenzs.tpop_id = apflora.tpop.id;
 
 -- 5. import ekplan data from tmp_ekplan in 11 queries (one for every year)
-select 
-  tpop_id,
-  typ,
-  2018 as jahr,
-  '2019-11-13' as changed,
-  'ag' as changed_by
-from
-  apflora.tmp_ekplan tmp_ekplan
-  inner join apflora.tpop tpop
-    inner join apflora.pop pop
-      inner join apflora.ap ap
-        inner join apflora.ekfrequenz ekfrequenz
-        on ekfrequenz.ap_id = ap.id and ekfrequenz.code = tmp_ekplan.code
-      on ap.id = pop.ap_id
-    on pop.id = tpop.pop_id
-  on tpop.id = tmp_ekplan.tpop_id
+with ekplans as (
+  select 
+    tmp_ekplan.tpop_id,
+    tmp_ekplan.typ,
+    2018 as jahr,
+    '2019-11-16' as changed,
+    'ag' as changed_by
+  from apflora.tmp_ekplan
+  where tmp_ekplan.y2018 = 1
+  union select 
+    tmp_ekplan.tpop_id,
+    tmp_ekplan.typ,
+    2019 as jahr,
+    '2019-11-16' as changed,
+    'ag' as changed_by
+  from apflora.tmp_ekplan
+  where tmp_ekplan.y2019 = 1
+  union select 
+    tmp_ekplan.tpop_id,
+    tmp_ekplan.typ,
+    2020 as jahr,
+    '2019-11-16' as changed,
+    'ag' as changed_by
+  from apflora.tmp_ekplan
+  where tmp_ekplan.y2020 = 1
+  union select 
+    tmp_ekplan.tpop_id,
+    tmp_ekplan.typ,
+    2021 as jahr,
+    '2019-11-16' as changed,
+    'ag' as changed_by
+  from apflora.tmp_ekplan
+  where tmp_ekplan.y2021 = 1
+  union select 
+    tmp_ekplan.tpop_id,
+    tmp_ekplan.typ,
+    2022 as jahr,
+    '2019-11-16' as changed,
+    'ag' as changed_by
+  from apflora.tmp_ekplan
+  where tmp_ekplan.y2022 = 1
+  union select 
+    tmp_ekplan.tpop_id,
+    tmp_ekplan.typ,
+    2023 as jahr,
+    '2019-11-16' as changed,
+    'ag' as changed_by
+  from apflora.tmp_ekplan
+  where tmp_ekplan.y2023 = 1
+  union select 
+    tmp_ekplan.tpop_id,
+    tmp_ekplan.typ,
+    2024 as jahr,
+    '2019-11-16' as changed,
+    'ag' as changed_by
+  from apflora.tmp_ekplan
+  where tmp_ekplan.y2024 = 1
+  union select 
+    tmp_ekplan.tpop_id,
+    tmp_ekplan.typ,
+    2025 as jahr,
+    '2019-11-16' as changed,
+    'ag' as changed_by
+  from apflora.tmp_ekplan
+  where tmp_ekplan.y2025 = 1
+  union select 
+    tmp_ekplan.tpop_id,
+    tmp_ekplan.typ,
+    2026 as jahr,
+    '2019-11-16' as changed,
+    'ag' as changed_by
+  from apflora.tmp_ekplan
+  where tmp_ekplan.y2026 = 1
+  union select 
+    tmp_ekplan.tpop_id,
+    tmp_ekplan.typ,
+    2027 as jahr,
+    '2019-11-16' as changed,
+    'ag' as changed_by
+  from apflora.tmp_ekplan
+  where tmp_ekplan.y2027 = 1
+  union select 
+    tmp_ekplan.tpop_id,
+    tmp_ekplan.typ,
+    2028 as jahr,
+    '2019-11-16' as changed,
+    'ag' as changed_by
+  from apflora.tmp_ekplan
+  where tmp_ekplan.y2028 = 1
+)
+insert into apflora.ekplan(tpop_id,typ,jahr,changed,changed_by)
+select
+  ekplans.tpop_id,
+  ekplans.typ::ek_type,
+  ekplans.jahr,
+  ekplans.changed::date,
+  ekplans.changed_by
+from ekplans
+  inner join apflora.tpop
+  on apflora.tpop.id = ekplans.tpop_id;
 
--- delete tmp_ekplan_ap and tmp_ekplan
+-- eventually delete tmp_ekplan_ap and tmp_ekplan
 
