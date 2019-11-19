d8b18aad30bfae729fd672b1b9eb10fded6f3167
Alexander Gabriel <alex.barbalex@gmail.com>
2019-11-19 23:20:29 +0100

    clear storage on update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,6 +4,7 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 import React from 'react'
+import localForage from 'localforage'
 
 import App from './src/App'
 
@@ -16,6 +17,8 @@ export const wrapRootElement = ({ element }) => <App element={element} />
 
 // https://github.com/gatsbyjs/gatsby/issues/9087#issuecomment-459105021
 export const onServiceWorkerUpdateReady = () => {
+  // clear local storage in case db structure was changed
+  localForage.clear()
   if (
     window.confirm('apflora neu laden, um die neuste Version zu installieren?')
   ) {
