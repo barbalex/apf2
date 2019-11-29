a1c40a8b4cf02975316ee12e6bf59f7b9bb3cfa4
Alexander Gabriel <alex.barbalex@gmail.com>
2019-11-29 16:28:32 +0100

    update queries

--- sql/apflora/2019-11-29_pop_sums.sql
+++ sql/apflora/2019-11-29_pop_sums.sql
@@ -2,31 +2,37 @@ select *
 from
   apflora.tpop
 
-select distinct on (apflora.tpop.id, apflora.tpopkontrzaehl_einheit_werte.text)
-  apflora.pop.id as pop_id,
-  apflora.tpop.id as tpop_id,
-  apflora.tpopkontrzaehl_einheit_werte.text as zaehleinheit,
-  apflora.tpopkontrzaehl.anzahl as anzahl
-from
-  apflora.tpopkontrzaehl
-  inner join apflora.tpopkontrzaehl_einheit_werte
-  on apflora.tpopkontrzaehl_einheit_werte.code = apflora.tpopkontrzaehl.einheit
-  inner join apflora.tpopkontr
-    inner join apflora.tpop
-      inner join apflora.pop
-      on apflora.pop.id = apflora.tpop.pop_id
-    on apflora.tpop.id = apflora.tpopkontr.tpop_id
-  on apflora.tpopkontrzaehl.tpopkontr_id = apflora.tpopkontr.id
-where
-  -- nur Kontrollen mit Jahr berücksichtigen
-  apflora.tpopkontr.jahr is not null
-  -- nur Zählungen mit Anzahl berücksichtigen
-  and apflora.tpopkontrzaehl.anzahl is not null
-order by
-  apflora.tpop.id,
-  apflora.tpopkontrzaehl_einheit_werte.text,
-  apflora.tpopkontr.jahr desc,
-  apflora.tpopkontr.datum desc
+-- get sums per pop
+with tpop_sums as (
+  select distinct on (apflora.tpop.id, apflora.tpopkontrzaehl_einheit_werte.text)
+    apflora.pop.id as pop_id,
+    apflora.tpop.id as tpop_id,
+    apflora.tpopkontrzaehl_einheit_werte.text as zaehleinheit,
+    apflora.tpopkontrzaehl.anzahl as anzahl
+  from
+    apflora.tpopkontrzaehl
+    inner join apflora.tpopkontrzaehl_einheit_werte
+    on apflora.tpopkontrzaehl_einheit_werte.code = apflora.tpopkontrzaehl.einheit
+    inner join apflora.tpopkontr
+      inner join apflora.tpop
+        inner join apflora.pop
+        on apflora.pop.id = apflora.tpop.pop_id
+      on apflora.tpop.id = apflora.tpopkontr.tpop_id
+    on apflora.tpopkontrzaehl.tpopkontr_id = apflora.tpopkontr.id
+  where
+    -- nur Kontrollen mit Jahr berücksichtigen
+    apflora.tpopkontr.jahr is not null
+    -- nur Zählungen mit Anzahl berücksichtigen
+    and apflora.tpopkontrzaehl.anzahl is not null
+  order by
+    apflora.tpop.id,
+    apflora.tpopkontrzaehl_einheit_werte.text,
+    apflora.tpopkontr.jahr desc,
+    apflora.tpopkontr.datum desc
+)
+select pop_id, zaehleinheit, sum(anzahl) as anzahl
+from tpop_sums
+group by pop_id, zaehleinheit
 
 
 
@@ -41,30 +47,36 @@ select
 from crosstab($$
   select pop_id, zaehleinheit, anzahl
   from 
-    (select distinct on (apflora.pop.id, apflora.tpopkontrzaehl_einheit_werte.text)
-      apflora.pop.id as pop_id,
-      apflora.tpopkontrzaehl_einheit_werte.text as zaehleinheit,
-      apflora.tpopkontrzaehl.anzahl as anzahl
-    from
-      apflora.tpopkontrzaehl
-      inner join apflora.tpopkontrzaehl_einheit_werte
-      on apflora.tpopkontrzaehl_einheit_werte.code = apflora.tpopkontrzaehl.einheit
-      inner join apflora.tpopkontr
-        inner join apflora.tpop
-          inner join apflora.pop
-          on apflora.pop.id = apflora.tpop.pop_id
-        on apflora.tpop.id = apflora.tpopkontr.tpop_id
-      on apflora.tpopkontrzaehl.tpopkontr_id = apflora.tpopkontr.id
-    where
-      -- nur Kontrollen mit Jahr berücksichtigen
-      apflora.tpopkontr.jahr is not null
-      -- nur Zählungen mit Anzahl berücksichtigen
-      and apflora.tpopkontrzaehl.anzahl is not null
-    order by
-      apflora.pop.id,
-      apflora.tpopkontrzaehl_einheit_werte.text,
-      apflora.tpopkontr.jahr desc,
-      apflora.tpopkontr.datum desc) as tbl
+    (with tpop_sums as (
+      select distinct on (apflora.tpop.id, apflora.tpopkontrzaehl_einheit_werte.text)
+        apflora.pop.id as pop_id,
+        apflora.tpop.id as tpop_id,
+        apflora.tpopkontrzaehl_einheit_werte.text as zaehleinheit,
+        apflora.tpopkontrzaehl.anzahl as anzahl
+      from
+        apflora.tpopkontrzaehl
+        inner join apflora.tpopkontrzaehl_einheit_werte
+        on apflora.tpopkontrzaehl_einheit_werte.code = apflora.tpopkontrzaehl.einheit
+        inner join apflora.tpopkontr
+          inner join apflora.tpop
+            inner join apflora.pop
+            on apflora.pop.id = apflora.tpop.pop_id
+          on apflora.tpop.id = apflora.tpopkontr.tpop_id
+        on apflora.tpopkontrzaehl.tpopkontr_id = apflora.tpopkontr.id
+      where
+        -- nur Kontrollen mit Jahr berücksichtigen
+        apflora.tpopkontr.jahr is not null
+        -- nur Zählungen mit Anzahl berücksichtigen
+        and apflora.tpopkontrzaehl.anzahl is not null
+      order by
+        apflora.tpop.id,
+        apflora.tpopkontrzaehl_einheit_werte.text,
+        apflora.tpopkontr.jahr desc,
+        apflora.tpopkontr.datum desc
+    )
+    select pop_id, zaehleinheit, sum(anzahl) as anzahl
+    from tpop_sums
+    group by pop_id, zaehleinheit) as tbl
   order by 1,2,3
   $$,
   $$SELECT unnest('{Pflanzen, Pflanzen (ohne Jungpflanzen), Triebe, Triebe Beweidung, Keimlinge, Rosetten, Jungpflanzen, Blätter, blühende Pflanzen, blühende Triebe, Blüten, Fertile Pflanzen, fruchtende Triebe, Blütenstände, Fruchtstände, Gruppen, Deckung (%), Pflanzen/5m2, Triebe in 30 m2, Triebe/50m2, Triebe Mähfläche, Fläche (m2), Pflanzstellen, Stellen, andere Zaehleinheit, Art ist vorhanden}'::text[])$$
