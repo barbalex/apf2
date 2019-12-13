6eefe01a54624d79b648fa84931c5318e956559a
Alexander Gabriel <alex.barbalex@gmail.com>
2019-12-13 14:21:57 +0100

    respect first massn year

--- src/components/EkPlan/Table/yearsFromTpops.js
+++ src/components/EkPlan/Table/yearsFromTpops.js
@@ -1,14 +1,18 @@
 import get from 'lodash/get'
 import minBy from 'lodash/minBy'
+import min from 'lodash/min'
 
 export default tpops => {
   const ekplans = tpops.flatMap(tpop => get(tpop, 'ekplansByTpopId.nodes'))
   const kontrs = tpops.flatMap(tpop => get(tpop, 'tpopkontrsByTpopId.nodes'))
+  const massns = tpops.flatMap(tpop => get(tpop, 'tpopmassnsByTpopId.nodes'))
   const firstEk = minBy([...ekplans, ...kontrs], 'jahr')
+  const firstMassn = minBy([...ekplans, ...massns], 'jahr')
   const currentYear = new Date().getFullYear()
-  const firstEkYear = firstEk ? firstEk.jahr : null
+  const firstEkYear = firstEk ? firstEk.jahr : 999999
+  const firstMassnYear = firstEk ? firstMassn.jahr : 999999
   // ensure never before 1993
-  let firstYear = firstEkYear || currentYear
+  let firstYear = min([firstEkYear, firstMassnYear, currentYear])
   const lastYear = currentYear + 15
   const years = []
   while (firstYear <= lastYear) {
