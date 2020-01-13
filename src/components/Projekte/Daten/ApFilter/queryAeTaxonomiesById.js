2800103a7a0a19235b71c4351d9c5f79c6f7f5ce
Alexander Gabriel <alex.barbalex@gmail.com>
2020-01-13 18:03:50 +0100

    name better

--- src/components/Projekte/Daten/ApFilter/queryAeTaxonomiesById.js
+++ src/components/Projekte/Daten/ApFilter/queryAeTaxonomiesById.js
@@ -1,7 +1,7 @@
 import gql from 'graphql-tag'
 
 export default gql`
-  query ApAeTaxonomiesQuery($id: UUID!, $run: Boolean!) {
+  query ApFilterAeTaxonomiesByIdQuery($id: UUID!, $run: Boolean!) {
     aeTaxonomyById(id: $id) @include(if: $run) {
       id
       artname
