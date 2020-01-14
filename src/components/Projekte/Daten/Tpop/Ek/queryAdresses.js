6fd3053e0ce3c9ed6dc07f97bd4f43b8cf35845c
Alexander Gabriel <alex.barbalex@gmail.com>
2020-01-14 10:31:40 +0100

    name unique

--- src/components/Projekte/Daten/Tpop/Ek/queryAdresses.js
+++ src/components/Projekte/Daten/Tpop/Ek/queryAdresses.js
@@ -1,7 +1,7 @@
 import gql from 'graphql-tag'
 
 export default gql`
-  query TpopfeldkontrAdressesQuery {
+  query TpopfeldkontrAdressesQueryForEk {
     allAdresses(orderBy: NAME_ASC, filter: { usersByAdresseIdExist: true }) {
       nodes {
         value: id
