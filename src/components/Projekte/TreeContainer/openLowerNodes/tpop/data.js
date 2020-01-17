a30ef7b0ec721da7f646b6619f3a52d85ffbb84b
Alexander Gabriel <alex.barbalex@gmail.com>
2020-01-17 10:18:45 +0100

    name unique

--- src/components/Projekte/TreeContainer/openLowerNodes/tpop/data.js
+++ src/components/Projekte/TreeContainer/openLowerNodes/tpop/data.js
@@ -1,7 +1,7 @@
 import gql from 'graphql-tag'
 
 export default gql`
-  query tpopFolderOpenLowerNodesQuery($id: UUID!) {
+  query treeContainerOpenLowerNodesQuery($id: UUID!) {
     tpopById(id: $id) {
       id
       tpopmassnsByTpopId {
