0f3f467522c92d6b503787f1f18ca376dbe97d0a
Alexander Gabriel <alex.barbalex@gmail.com>
2020-03-16 10:45:08 +0100

    update deps

--- src/modules/exportCsv.js
+++ src/modules/exportCsv.js
@@ -3,6 +3,8 @@ import fileDownload from 'js-file-download'
 import format from 'date-fns/format'
 
 export default ({ fileName, data }) => {
+  // maybe use "AsyncParser" if the synchronous "parse" blocks the ui to badly
+  // see: https://mircozeiss.com/json2csv/#json2csv-async-parser-(streaming-api)
   const csvData = parse(data)
   const file = `${fileName}_${format(new Date(), 'yyyy-MM-dd_HH-mm-ss')}`
   fileDownload(csvData, `${file}.csv`)
