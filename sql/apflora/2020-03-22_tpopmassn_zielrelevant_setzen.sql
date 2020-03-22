c39c2639f8f81832b751936b71994afa3e44b814
Alexander Gabriel <alex.barbalex@gmail.com>
2020-03-22 16:16:17 +0100

    update massn

--- sql/apflora/2020-03-22_tpopmassn_zielrelevant_setzen.sql
+++ sql/apflora/2020-03-22_tpopmassn_zielrelevant_setzen.sql
@@ -3,38 +3,56 @@
 -- zusätzlich: wenn tpopmassn.typ anpflanzung ist (2, 3)
 
 --1. triebe
-select massn.id, massn.anz_triebe, eh.text as zieleinheit_text, eh.code as zieleinheit_code
-from
-  apflora.tpopmassn massn
-  inner join apflora.tpop tpop
-    inner join apflora.pop pop
-      inner join apflora.ap ap
-        inner join apflora.ekzaehleinheit ekze
-          inner join apflora.tpopkontrzaehl_einheit_werte eh
-          on ekze.zaehleinheit_id = eh.id and eh.id = 'ddf9e946-3dc8-11e8-acca-fb443f718fbd' -- Triebe total
-        on ap.id = ekze.ap_id and ekze.zielrelevant = true
-      on ap.id = pop.ap_id
-    on pop.id = tpop.pop_id
-  on tpop.id = massn.tpop_id
-where
-  massn.typ in (2, 3)
-  and massn.anz_triebe is not null
+with massn_mit_trieben as (
+  select massn.id, massn.anz_triebe, eh.text as zieleinheit_text, eh.code as zieleinheit_code
+  from
+    apflora.tpopmassn massn
+    inner join apflora.tpop tpop
+      inner join apflora.pop pop
+        inner join apflora.ap ap
+          inner join apflora.ekzaehleinheit ekze
+            inner join apflora.tpopkontrzaehl_einheit_werte eh
+            on ekze.zaehleinheit_id = eh.id and eh.id = 'ddf9e946-3dc8-11e8-acca-fb443f718fbd' -- Triebe total
+          on ap.id = ekze.ap_id and ekze.zielrelevant = true
+        on ap.id = pop.ap_id
+      on pop.id = tpop.pop_id
+    on tpop.id = massn.tpop_id
+  where
+    massn.typ in (2, 3)
+    and massn.anz_triebe is not null
+)
+update apflora.tpopmassn
+set
+  zieleinheit_einheit = massn_mit_trieben.zieleinheit_code,
+  zieleinheit_anzahl = massn_mit_trieben.anz_triebe
+from massn_mit_trieben
+where apflora.tpopmassn.id = massn_mit_trieben.id;
+
+select * from apflora.tpopmassn where zieleinheit_einheit is not null;
 
 
 --2. pflanzen
-select massn.id, massn.anz_pflanzen, eh.text as zieleinheit_text, eh.code as zieleinheit_code
-from
-  apflora.tpopmassn massn
-  inner join apflora.tpop tpop
-    inner join apflora.pop pop
-      inner join apflora.ap ap
-        inner join apflora.ekzaehleinheit ekze
-          inner join apflora.tpopkontrzaehl_einheit_werte eh
-          on ekze.zaehleinheit_id = eh.id and eh.id = 'ddf9e944-3dc8-11e8-acca-a74bfdad0083' -- Pflanzen total
-        on ap.id = ekze.ap_id and ekze.zielrelevant = true
-      on ap.id = pop.ap_id
-    on pop.id = tpop.pop_id
-  on tpop.id = massn.tpop_id
-where
-  massn.typ in (2, 3)
-  and massn.anz_pflanzen is not null
+with massn_mit_pflanzen as (
+  select massn.id, massn.anz_pflanzen, eh.text as zieleinheit_text, eh.code as zieleinheit_code
+  from
+    apflora.tpopmassn massn
+    inner join apflora.tpop tpop
+      inner join apflora.pop pop
+        inner join apflora.ap ap
+          inner join apflora.ekzaehleinheit ekze
+            inner join apflora.tpopkontrzaehl_einheit_werte eh
+            on ekze.zaehleinheit_id = eh.id and eh.id = 'ddf9e944-3dc8-11e8-acca-a74bfdad0083' -- Pflanzen total
+          on ap.id = ekze.ap_id and ekze.zielrelevant = true
+        on ap.id = pop.ap_id
+      on pop.id = tpop.pop_id
+    on tpop.id = massn.tpop_id
+  where
+    massn.typ in (2, 3)
+    and massn.anz_pflanzen is not null
+)
+update apflora.tpopmassn
+set
+  zieleinheit_einheit = massn_mit_pflanzen.zieleinheit_code,
+  zieleinheit_anzahl = massn_mit_pflanzen.anz_pflanzen
+from massn_mit_pflanzen
+where apflora.tpopmassn.id = massn_mit_pflanzen.id;
