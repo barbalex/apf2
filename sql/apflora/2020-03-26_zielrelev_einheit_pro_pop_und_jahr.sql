ac17bcc0803efffd1efed0294700142c77fcbbe7
Alexander Gabriel <alex.barbalex@gmail.com>
2020-03-26 18:20:09 +0100

    work on ausw

--- sql/apflora/2020-03-26_zielrelev_einheit_pro_pop_und_jahr.sql
+++ sql/apflora/2020-03-26_zielrelev_einheit_pro_pop_und_jahr.sql
@@ -9,16 +9,16 @@ massnjahre as (
   select distinct on (tpop0.id, massn0.jahr)
     tpop0.id as tpop_id,
     massn0.jahr,
-    massn0.id as massn_id,
+    massn0.id,
     ze0.text as zaehleinheit,
     massn0.zieleinheit_anzahl as anzahl
   from 
     apflora.tpopmassn massn0
       inner join apflora.tpopmassn_typ_werte tw
       on tw.code = massn0.typ and tw.anpflanzung = true
-    inner join apflora.tpop tpop0
-      inner join apflora.pop pop0
-        inner join apflora.ap ap0
+    inner join apflora.tpop_history tpop0
+      inner join apflora.pop_history pop0
+        inner join apflora.ap_history ap0
           inner join apflora.ekzaehleinheit ekze0
             inner join apflora.tpopkontrzaehl_einheit_werte ze0
             on ze0.id = ekze0.zaehleinheit_id
@@ -28,6 +28,9 @@ massnjahre as (
     on tpop0.id = massn0.tpop_id
   where
     massn0.jahr is not null
+    and ap0.year = massn0.jahr
+    and pop0.year = massn0.jahr
+    and tpop0.year = massn0.jahr
     and tpop0.status in (200, 201)
     and massn0.zieleinheit_einheit = ze0.code
     and massn0.zieleinheit_anzahl is not null
@@ -40,25 +43,28 @@ zaehljahre as (
   select distinct on (tpop2.id, kontr2.jahr)
     tpop2.id as tpop_id,
     kontr2.jahr,
-    kontr2.id as kontr_id,
+    kontr2.id,
     ze2.text as zaehleinheit,
     zaehl2.anzahl
   from
     apflora.tpopkontrzaehl zaehl2
     inner join apflora.tpopkontr kontr2
-      inner join apflora.tpop tpop2
-        inner join apflora.pop pop2
-          inner join apflora.ap ap2
+      inner join apflora.tpop_history tpop2
+        inner join apflora.pop_history pop2
+          inner join apflora.ap_history ap2
             inner join apflora.ekzaehleinheit ekze2
               inner join apflora.tpopkontrzaehl_einheit_werte ze2
               on ze2.id = ekze2.zaehleinheit_id
             on ekze2.ap_id = ap2.id and ekze2.zielrelevant = true
           on ap2.id = pop2.ap_id
-        on pop2.id = tpop2.pop_id
-      on tpop2.id = kontr2.tpop_id
+        on pop2.id = tpop2.pop_id 
+      on tpop2.id = kontr2.tpop_id 
     on zaehl2.tpopkontr_id = kontr2.id and zaehl2.einheit = ze2.code
   where
     kontr2.jahr is not null
+    and ap2.year = kontr2.jahr
+    and pop2.year = kontr2.jahr
+    and tpop2.year = kontr2.jahr
     and tpop2.status in (100, 200, 201)
     and zaehl2.anzahl is not null
     -- nur Zählungen mit der Ziel-Einheit
@@ -68,113 +74,52 @@ zaehljahre as (
     kontr2.jahr desc,
     kontr2.datum desc
 ),
-jahre as (
-  select generate_series(
-    (select jahr from massnjahre union select jahr from zaehljahre order by jahr asc limit 1)::int,
-    extract(year from now())::int
-  ) as jahr
-),
-tpop_ids as (
-  select tpop_id from massnjahre 
-  union select tpop_id from zaehljahre 
-  order by tpop_id
-),
-tpops_with_year as (
-  select tpop_id, jahr from tpop_ids, jahre
-  order by tpop_id, jahr
-),
-letzte_zaehlung_oder_kontrolle as (
+tpop_mit_letzten as (
   select
-    ty.jahr,
-    ty.tpop_id,
-    (select jahr from massnjahre where jahr <= ty.jahr and tpop_id = ty.tpop_id order by jahr desc limit 1) as last_massnahmenjahr,
-    (select jahr from zaehljahre where jahr <= ty.jahr and tpop_id = ty.tpop_id order by jahr desc limit 1) as last_zaehlungjahr
-  from 
-    tpops_with_year ty
-    left join massnjahre mj
-    on mj.jahr = ty.jahr and mj.tpop_id = ty.tpop_id
-    left join zaehljahre zj
-    on zj.jahr = ty.jahr and zj.tpop_id = ty.tpop_id
-)
-select * from letzte_zaehlung_oder_kontrolle
-
-
-
-letzte_ansiedlung as (
-  select distinct on (tpop4.id, massn4.jahr)
-    tpop4.id as tpop_id,
-    massn4.jahr,
-    massn4.id as massn_id,
-    ze4.text as zaehleinheit,
-    massn4.zieleinheit_anzahl as anzahl
+    tpop3.id as tpop_id,
+    tpop3.year as jahr,
+    case
+      when zj.jahr is not null and mj.jahr is not null and zj.jahr >= mj.jahr then 'zaehlung'
+      when zj.jahr is not null and mj.jahr is not null and zj.jahr < mj.jahr then 'massnahme'
+      when zj.jahr is not null then 'zaehlung'
+      when mj.jahr is not null then 'massnahme'
+      else null
+    end as typ,
+  case
+    when zj.jahr is not null and mj.jahr is not null and zj.jahr >= mj.jahr then zj.id
+    when zj.jahr is not null and mj.jahr is not null and zj.jahr < mj.jahr then mj.id
+    when zj.jahr is not null then zj.id
+    when mj.jahr is not null then mj.id
+    else null
+  end as id,
+  case
+    when zj.jahr is not null and mj.jahr is not null and zj.jahr >= mj.jahr then zj.zaehleinheit
+    when zj.jahr is not null and mj.jahr is not null and zj.jahr < mj.jahr then mj.zaehleinheit
+    when zj.jahr is not null then zj.zaehleinheit
+    when mj.jahr is not null then mj.zaehleinheit
+    else null
+  end as zaehleinheit,
+  case
+    when zj.jahr is not null and mj.jahr is not null and zj.jahr >= mj.jahr then zj.anzahl
+    when zj.jahr is not null and mj.jahr is not null and zj.jahr < mj.jahr then mj.anzahl
+    when zj.jahr is not null then zj.anzahl
+    when mj.jahr is not null then mj.anzahl
+    else null
+  end as anzahl
   from
-    apflora.tpopmassn massn4
-    inner join apflora.tpop tpop4
-      inner join apflora.pop pop4
-        inner join apflora.ap ap4
-          inner join apflora.ekzaehleinheit ekze4
-            inner join apflora.tpopkontrzaehl_einheit_werte ze4
-            on ze4.id = ekze4.zaehleinheit_id
-          on ekze4.ap_id = ap4.id and ekze4.zielrelevant = true
-        on ap4.id = pop4.ap_id
-      on pop4.id = tpop4.pop_id
-    on tpop4.id = massn4.tpop_id
-    inner join apflora.tpopmassn_typ_werte
-    on apflora.tpopmassn_typ_werte.code = massn4.typ
-  where
-    massn4.jahr is not null
-    and tpop4.status in (200, 201)
-    and tpopmassn_typ_werte.anpflanzung = true
-    and massn4.zieleinheit_einheit = ze4.code
-    and massn4.zieleinheit_anzahl is not null
+    apflora.tpop_history as tpop3
+    left join massnjahre as mj
+    on mj.tpop_id = tpop3.id and mj.jahr = (select max(jahr) from massnjahre where massnjahre.jahr <= tpop3.year and massnjahre.tpop_id = tpop3.id)
+    left join zaehljahre as zj
+    on zj.tpop_id = tpop3.id and zj.jahr = (select max(jahr) from zaehljahre where zaehljahre.jahr <= tpop3.year and zaehljahre.tpop_id = tpop3.id)
   order by
-    tpop4.id,
-    massn4.jahr desc,
-    massn4.datum desc
-), 
-letzte_zaehlung as (
-  select distinct on (tpop5.id, kontr5.jahr)
-    tpop5.id as tpop_id,
-    kontr5.jahr,
-    kontr5.id as kontr_id,
-    apflora.tpopkontrzaehl_einheit_werte.text as zaehleinheit,
-    zaehl5.anzahl
-  from
-    apflora.tpopkontrzaehl zaehl5
-    inner join apflora.tpopkontrzaehl_einheit_werte
-    on apflora.tpopkontrzaehl_einheit_werte.code = zaehl5.einheit
-    inner join apflora.tpopkontr kontr5
-      inner join apflora.tpop tpop5
-        inner join apflora.pop pop5
-          inner join apflora.ap ap5
-            inner join apflora.ekzaehleinheit ekze5
-              inner join apflora.tpopkontrzaehl_einheit_werte ze5
-              on ze5.id = ekze5.zaehleinheit_id
-            on ekze5.ap_id = ap5.id and ekze5.zielrelevant = true
-          on ap5.id = pop5.ap_id
-        on pop5.id = tpop5.pop_id
-      on tpop5.id = kontr5.tpop_id
-    on zaehl5.tpopkontr_id = kontr5.id
-  where
-    -- nur Kontrollen mit Jahr berücksichtigen
-    kontr5.jahr is not null
-    -- nur Zählungen mit der Ziel-Einheit
-    and ze5.code = zaehl5.einheit
-    -- nur Zählungen mit Anzahl berücksichtigen
-    and zaehl5.anzahl is not null
-  order by
-    tpop5.id,
-    kontr5.jahr desc,
-    kontr5.datum desc
+    tpop3.id,
+    tpop3.year
 )
-select
-  tpop.id
-from
-  apflora.tpop tpop
-  left join letzte_ansiedlung la
-  on la.tpop_id = tpop.id
-  left join letzte_zaehlung lz
-  on lz.tpop_id = tpop.id
+select * from tpop_mit_letzten
+
+
+
 
 
 
