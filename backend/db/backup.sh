f16944acabad0a1d1a3d977583b201c486fa51f0
Alexander Gabriel <alex.barbalex@gmail.com>
2020-04-04 18:41:24 +0200

    work on backup

--- backend/db/backup.sh
+++ backend/db/backup.sh
@@ -2,10 +2,9 @@
 # stellt dem Filenamen das Datum voran
 # kopiert das File auf die dropbox
 # entfernt das File
-#PATH=$PATH:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
-#SHELL=/bin/sh PATH=/bin:/sbin:/usr/bin:/usr/sbin:/root/
+echo "will back up apflora"
 FILENAME=$(date +"%Y-%m-%d_%H-%M-%S_apflora.backup")
-PGPASSWORD=Wy5doZyg0z5B pg_dump -U postgres -h localhost --file=/shared/$FILENAME -Fc -Z9 apflora
-/dropbox_uploader.sh upload /shared/$FILENAME $FILENAME
-rm /shared/$FILENAME
-echo "backed up $(FILENAME)" >> /var/log/cron.log 2>&1
+PGPASSWORD=Wy5doZyg0z5B pg_dump -U postgres -h localhost --file=/sik_data/$FILENAME -Fc -Z9 apflora
+/dropbox_uploader.sh upload /sik_data/$FILENAME $FILENAME
+rm /sik_data/$FILENAME
+echo "backed up $FILENAME" >> /var/log/cron.log 2>&1
