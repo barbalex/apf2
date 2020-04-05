5de5f9031581e9cca56c5c7f11ce66193bb4dbd3
Alexander Gabriel <alex.barbalex@gmail.com>
2020-04-06 00:02:14 +0200

    optimize views

--- sql/drop views.sql
+++ sql/drop views.sql
@@ -1,5 +1,5 @@
 -- create list like this:
-SELECT 'DROP VIEW if exists  apflora.' || table_name || ' cascade;'
+SELECT 'DROP VIEW if exists apflora.' || table_name || ' cascade;'
   FROM information_schema.views
  WHERE table_schema NOT IN ('pg_catalog', 'information_schema')
    AND table_name !~ '^pg_';
