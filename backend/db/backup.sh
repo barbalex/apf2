046aea2a6dc1e4a88b709d5c486b18880338afe6
Alexander Gabriel <alex.barbalex@gmail.com>
2020-04-05 13:01:19 +0200

    rename pw

--- backend/db/backup.sh
+++ backend/db/backup.sh
@@ -4,9 +4,8 @@
 # entfernt das File
 echo "creating dump file..."
 FILENAME=$(date +"%Y-%m-%d_%H-%M-%S_apflora.backup")
-PGPASSWORD=$PGPASSWORD pg_dump -U postgres -h localhost --file=/sik_data/$FILENAME -Fc -Z9 apflora
+PGPASSWORD=$POSTGRES_PASSWORD pg_dump -U postgres -h localhost --file=/sik_data/$FILENAME -Fc -Z9 apflora
 echo "uploading dump file..."
-# echo OAUTH_ACCESS_TOKEN=$DROPBOX_OAUTH_ACCESS_TOKEN 
 /dropbox_uploader.sh -f dropbox_uploader.conf upload /sik_data/$FILENAME $FILENAME
 rm /sik_data/$FILENAME
 echo "backed up $FILENAME" >> /var/log/cron.log 2>&1
