3ab4ceebfecbdf002c4ad8b0f4adcaeb1ca94cf1
Alexander Gabriel <alex.barbalex@gmail.com>
2020-04-07 12:02:01 +0200

    enable files for more tables

--- src/store/UrlQuery.js
+++ src/store/UrlQuery.js
@@ -5,14 +5,16 @@ export default types
   .model('UrlQuery', {
     projekteTabs: types.optional(types.array(types.string), ['tree', 'daten']),
     apTab: types.optional(types.maybeNull(types.string), 'ap'),
+    popTab: types.optional(types.maybeNull(types.string), 'pop'),
     tpopTab: types.optional(types.maybeNull(types.string), 'tpop'),
+    tpopmassnTab: types.optional(types.maybeNull(types.string), 'tpopmassn'),
     feldkontrTab: types.optional(types.maybeNull(types.string), 'entwicklung'),
     idealbiotopTab: types.optional(
       types.maybeNull(types.string),
       'idealbiotop',
     ),
   })
-  .actions(self => ({
+  .actions((self) => ({
     addProjekteTab(tab) {
       self.projekteTabs = uniq([...self.projekteTabs, tab])
     },
@@ -20,7 +22,9 @@ export default types
 
 export const defaultValue = {
   projekteTabs: ['tree', 'daten'],
+  popTab: 'pop',
   tpopTab: 'tpop',
+  tpopmassnTab: 'tpopmassn',
   apTab: 'ap',
   feldkontrTab: 'entwicklung',
   idealbiotopTab: 'idealbiotop',
