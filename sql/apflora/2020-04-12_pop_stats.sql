7b8e30d47cca296e6f16bfdab5c53ac55a5ec49e
Alexander Gabriel <alex.barbalex@gmail.com>
2020-04-12 12:50:38 +0200

    work on prio

--- sql/apflora/2020-04-12_pop_stats.sql
+++ sql/apflora/2020-04-12_pop_stats.sql
@@ -12,37 +12,99 @@ with last_year as (
   limit 1
   offset 1
 ),
-count_urspr_last as (),
-count_anges_last as (),
-count_total_last as (),
-count_urspr_prev as (),
-count_anges_prev as (),
-count_total_prev as (),
+count_urspr_last as (
+  select
+    apflora.pop_history.ap_id,
+    count(apflora.pop_history.*) as anzahl
+  from
+    apflora.pop_history
+    inner join last_year
+    on last_year.year = apflora.pop_history.year
+  where
+    apflora.pop_history.status = 100
+  group by
+    apflora.pop_history.ap_id
+),
+count_anges_last as (
+  select
+    apflora.pop_history.ap_id,
+    count(apflora.pop_history.*) as anzahl
+  from
+    apflora.pop_history
+    inner join last_year
+    on last_year.year = apflora.pop_history.year
+  where
+    apflora.pop_history.status = 200
+  group by
+    apflora.pop_history.ap_id
+),
+count_total_last as (
+  select
+    apflora.pop_history.ap_id,
+    count(apflora.pop_history.*) as anzahl
+  from
+    apflora.pop_history
+    inner join last_year
+    on last_year.year = apflora.pop_history.year
+  where
+    apflora.pop_history.status in (100, 200)
+  group by
+    apflora.pop_history.ap_id),
+count_urspr_prev as (
+  select
+    apflora.pop_history.ap_id,
+    count(apflora.pop_history.*) as anzahl
+  from
+    apflora.pop_history
+    inner join previous_year
+    on previous_year.year = apflora.pop_history.year
+  where
+    apflora.pop_history.status = 100
+  group by
+    apflora.pop_history.ap_id
+),
+count_anges_prev as (
+  select
+    apflora.pop_history.ap_id,
+    count(apflora.pop_history.*) as anzahl
+  from
+    apflora.pop_history
+    inner join previous_year
+    on previous_year.year = apflora.pop_history.year
+  where
+    apflora.pop_history.status = 200
+  group by
+    apflora.pop_history.ap_id
+),
+count_total_prev as (
+  select
+    apflora.pop_history.ap_id,
+    count(apflora.pop_history.*) as anzahl
+  from
+    apflora.pop_history
+    inner join previous_year
+    on previous_year.year = apflora.pop_history.year
+  where
+    apflora.pop_history.status in (100, 200)
+  group by
+    apflora.pop_history.ap_id
+),
 diff_urspr as (),
 diff_anges as (),
 diff_total as (),
-data as (
-  select
-    ap_id,
-    year,
-    status,
-    count(*) as anzahl
-    from
-      apflora.pop_history
-    where status is not null
-    group by
-      ap_id,
-      year,
-      status
-    order by
-      ap_id,
-      year,
-      status
-)
 select
-  ap_id,
-  year as jahr,
-  json_object_agg(status, anzahl) as values
-from data
-group by ap_id, year
-order by ap_id, year;
+  apflora.ap_history.id as ap_id,
+  last_year.year as zuletzt,
+  previous_year.year as zuvor,
+  count_urspr_last.anzahl as anz_pop_urspr_zuletzt,
+  count_anges_last.anzahl as anz_pop_anges_zuletzt
+from
+  apflora.ap_history
+  left join count_urspr_last
+  on count_urspr_last.ap_id = apflora.ap_history.id
+  left join count_anges_last
+  on count_anges_last.ap_id = apflora.ap_history.id,
+  last_year, 
+  previous_year
+where
+  apflora.ap_history.year = last_year.year;
