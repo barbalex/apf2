8250151f1f5e763a9216d43c641ab3a2f4b1b057
Alexander Gabriel <alex.barbalex@gmail.com>
2020-05-04 00:22:48 +0200

    flesh out ss server

--- backend/ss/server.js
+++ backend/ss/server.js
@@ -3,38 +3,57 @@
 
 const Hapi = require('@hapi/hapi')
 const axios = require('axios')
+const querystring = require('querystring')
 
 const server = new Hapi.Server({
   host: '0.0.0.0',
   port: 7000,
 })
 
+const karteMassnahmenBaseUrl = 'https://maps.zh.ch/wfs/FnsAPFloraWFS'
+const karteMassnahmenParams = {
+  service: 'WFS',
+  version: '2.0.0',
+  request: 'getFeature',
+  typeName: 'ms:massnahmenflaechen',
+  srsName: 'EPSG::4326',
+  maxFeatures: 3000,
+  // weird output format according to https://maps.zh.ch/wfs/FnsAPFloraWFS?SERVICE=WFS&Request=GetCapabilities
+  outputFormat: 'application/json; subtype=geojson',
+}
+const karteMassnahmenUrl = `${karteMassnahmenBaseUrl}?${querystring.stringify(
+  karteMassnahmenParams,
+)}`
+
+const karteBetreuungsgebieteBaseUrl = 'https://maps.zh.ch/wfs/OGDZHWFS'
+const karteBetreuungsgebieteParams = {
+  service: 'WFS',
+  version: '2.0.0',
+  request: 'getFeature',
+  typeName: 'ms:ogd-0428_aln_fns_betreuungsgebiete_f',
+  srsName: 'EPSG:4326',
+  // weird output format according to https://maps.zh.ch/wfs/OGDZHWFS?SERVICE=WFS&Request=GetCapabilities
+  outputFormat: 'application/json; subtype=geojson',
+}
+const karteBetreuungsgebieteUrl = `${karteBetreuungsgebieteBaseUrl}?${querystring.stringify(
+  karteBetreuungsgebieteParams,
+)}`
+
 async function start() {
   server.route({
     method: 'GET',
     path: '/karte/massnahmen',
     handler: async (req, h) => {
-      const baseUrl = 'https://maps.zh.ch/wfs/FnsAPFloraWFS'
-      const params = {
-        service: 'WFS',
-        version: '1.0.0',
-        request: 'getFeature',
-        typeName: 'ms:massnahmenflaechen',
-        maxFeatures: 3000,
-        outputFormat: 'application/json',
-      }
-      const url = `${baseUrl}${window.L.Util.getParamString(params)}`
-      axios({
+      return axios({
         method: 'get',
-        url,
+        url: karteMassnahmenUrl,
         auth: {
           username: process.env.MAPS_ZH_CH_USER,
           password: process.env.MAPS_ZH_CH_SECRET,
         },
       })
         .then((response) => {
-          console.log('Massnahmen, response:', response)
-          h.response(response).code(200)
+          return h.response(response.data).code(200)
         })
         .catch((error) => {
           console.log('Massnahmen, error:', error)
@@ -42,9 +61,31 @@ async function start() {
         })
     },
   })
+  server.route({
+    method: 'GET',
+    path: '/karte/betreuungsgebiete',
+    handler: async (req, h) => {
+      //console.log('karteBetreuungsgebieteUrl:', karteBetreuungsgebieteUrl)
+      return axios({
+        method: 'get',
+        url: karteBetreuungsgebieteUrl,
+        auth: {
+          username: process.env.MAPS_ZH_CH_USER,
+          password: process.env.MAPS_ZH_CH_SECRET,
+        },
+      })
+        .then((response) => {
+          return h.response(response.data).code(200)
+        })
+        .catch((error) => {
+          console.log('Betreuungsgebiete, error:', error)
+          return h.response(error.message).code(500)
+        })
+    },
+  })
 
   await server.start()
-  console.log('JSON-API-Server running at:', server.info.uri)
+  console.log('ss-Server running at:', server.info.uri)
 }
 
 process.on('unhandledRejection', (err) => {
