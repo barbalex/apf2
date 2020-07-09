7857cdbee2353c7f26f599cd8cb31ff0b68fcaa9
Alexander Gabriel <alex.barbalex@gmail.com>
2020-07-09 09:50:45 +0200

    refactor react-contextmenu

--- src/components/Projekte/TreeContainer/contextmenu/TpopberFolder.js
+++ src/components/Projekte/TreeContainer/contextmenu/TpopberFolder.js
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react'
-import { ContextMenu, MenuItem } from 'react-contextmenu'
 import { observer } from 'mobx-react-lite'
 
 import userIsReadOnly from '../../../../modules/userIsReadOnly'
 import storeContext from '../../../../storeContext'
 import ErrorBoundary from '../../../shared/ErrorBoundary'
+import { ContextMenu, MenuItem } from '../../../../modules/react-contextmenu'
 
 // create objects outside render
 const insertData = {
