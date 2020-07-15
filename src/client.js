3e62cd690b23f0e4c243d78a35ad531311ce0d90
Alexander Gabriel <alex.barbalex@gmail.com>
2020-07-15 10:34:45 +0200

    migrate apollo

--- src/client.js
+++ src/client.js
@@ -1,9 +1,8 @@
 import { ApolloClient } from 'apollo-client'
-//import { createHttpLink } from "apollo-link-http"
 import { BatchHttpLink } from 'apollo-link-batch-http'
 import { setContext } from 'apollo-link-context'
 import { InMemoryCache, defaultDataIdFromObject } from 'apollo-cache-inmemory'
-import { ApolloLink } from 'apollo-link'
+import { ApolloLink } from '@apollo/client'
 import { onError } from 'apollo-link-error'
 import jwtDecode from 'jwt-decode'
 import uniqBy from 'lodash/uniqBy'
@@ -90,16 +89,12 @@ export default ({ idb, store }) => {
   })
 
   const cache = new InMemoryCache({
-    dataIdFromObject: object => {
+    dataIdFromObject: (object) => {
       if (object.id && isNaN(object.id)) return object.id
       return defaultDataIdFromObject(object)
     },
   })
-  // use httpLink _instead_ of batchHttpLink to _not_ batch
 
-  /*const httpLink = createHttpLink({
-    uri: graphQlUri(),
-  })*/
   const batchHttpLink = new BatchHttpLink({ uri: graphQlUri() })
   const client = new ApolloClient({
     link: ApolloLink.from([errorLink, authLink, batchHttpLink]),
