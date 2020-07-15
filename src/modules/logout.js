423c1eeb4431d17ed61afeeb264dacaf73384f2b
Alexander Gabriel <alex.barbalex@gmail.com>
2020-07-15 10:34:05 +0200

    lint

--- src/modules/logout.js
+++ src/modules/logout.js
@@ -4,6 +4,7 @@ export default async (idb) => {
   console.log('LOGGING OUT')
   if (typeof window !== 'undefined') {
     localForage.clear()
+    // eslint-disable-next-line no-unused-expressions
     idb?.delete?.()
     window.location.reload(true)
   }
