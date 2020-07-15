72e287e94d30ec20a0a53c354180114753c54b8d
Alexander Gabriel <alex.barbalex@gmail.com>
2020-07-15 12:54:04 +0200

    migrate apollo

--- src/client.js
+++ src/client.js
@@ -1,4 +1,4 @@
-import { ApolloClient } from 'apollo-client'
+import { ApolloClient } from '@apollo/client'
 import { BatchHttpLink } from '@apollo/client/link/batch-http'
 import { setContext } from '@apollo/client/link/context'
 import { InMemoryCache, defaultDataIdFromObject } from 'apollo-cache-inmemory'
