f7e9b4d0200e6be62d98332771d57f4733d3f1b1
Alexander Gabriel <alex.barbalex@gmail.com>
2020-09-27 17:44:10 +0200

    refactor markdownfield

--- src/components/shared/MarkdownFieldFormik/index.js
+++ src/components/shared/MarkdownFieldFormik/index.js
@@ -1,27 +1,22 @@
 import React from 'react'
 import { FocusWithin } from 'react-focus-within'
 
-import TextField from '../TextField'
-import MarkdownField from './Field'
+import Editor from './Editor'
+import Presenter from './Presenter'
 
 // need to lazy import because react-markdown-editor-lite calls navigator which busts gatsby build
 const MdFieldFormik = (props) => {
-  const { value, name } = props.field
+  const { label } = props
+  const { value } = props.field
 
   return (
     <FocusWithin>
       {({ isFocused, focusProps }) => (
         <div {...focusProps}>
           {isFocused ? (
-            <MarkdownField {...props} />
+            <Editor {...props} />
           ) : (
-            <TextField
-              value={value}
-              label={props.label}
-              name={name}
-              multiLine={true}
-              saveToDb={props.saveToDb}
-            />
+            <Presenter value={value} label={label} />
           )}
         </div>
       )}
