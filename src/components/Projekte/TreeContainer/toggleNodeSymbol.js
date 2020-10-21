94044ac2770f7a813029245622b71b0d6448d998
Alexander Gabriel <alex.barbalex@gmail.com>
2020-10-21 17:24:27 +0200

    enhance tree behaviour

--- src/components/Projekte/TreeContainer/toggleNodeSymbol.js
+++ src/components/Projekte/TreeContainer/toggleNodeSymbol.js
@@ -1,14 +1,27 @@
 import isEqual from 'lodash/isEqual'
 
 import isNodeOpen from './isNodeOpen'
+import isNodeInActiveNodePath from './isNodeInActiveNodePath'
 
 export default ({ treeName, node, store }) => {
   if (!node.url) throw new Error('passed node has no url')
-  const { openNodes, setOpenNodes } = store[treeName]
+  const {
+    openNodes,
+    setOpenNodes,
+    activeNodeArray,
+    setActiveNodeArray,
+  } = store[treeName]
 
   let newOpenNodes = [...openNodes]
   if (isNodeOpen({ openNodes, url: node.url })) {
     newOpenNodes = newOpenNodes.filter((n) => !isEqual(n, node.url))
+    if (isNodeInActiveNodePath({ node, activeNodeArray })) {
+      // when a user closes a folder in the active node path
+      // the active node should swith to the node's parent
+      const newActiveNodeArray = [...node.url]
+      newActiveNodeArray.pop()
+      setActiveNodeArray(newActiveNodeArray)
+    }
   } else {
     newOpenNodes.push(node.url)
   }
