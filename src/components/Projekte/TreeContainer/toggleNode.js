0165bb4eff47a209b461bc4749093f5f0c2b8f5f
Alexander Gabriel <alex.barbalex@gmail.com>
2020-10-21 17:28:39 +0200

    optimize

--- src/components/Projekte/TreeContainer/toggleNode.js
+++ src/components/Projekte/TreeContainer/toggleNode.js
@@ -4,7 +4,9 @@ import openNode from './openNode'
 export default ({ treeName, node, store }) => {
   if (!node.url) throw new Error('passed node has no url')
   const { dataFilterSetActiveTable } = store
-  const { openNodes, setActiveNodeArray, dataFilter } = store[treeName]
+  const { openNodes, setActiveNodeArray, activeNodeArray, dataFilter } = store[
+    treeName
+  ]
 
   if (dataFilter) {
     const show = !!dataFilter.activeTable
@@ -14,7 +16,25 @@ export default ({ treeName, node, store }) => {
   }
 
   if (!isNodeOpen({ openNodes, url: node.url })) {
+    // node is closed
+    // open it and make it the active node
     openNode({ treeName, node, openNodes, store })
+    const newActiveNodeArray = [...node.url]
+    setActiveNodeArray(newActiveNodeArray)
+    // some elements are numbers but they are contained in url as text
+    // eslint-disable-next-line eqeqeq
+  } else if (node.urlLabel == activeNodeArray.slice(-1)[0]) {
+    // the node is open
+    // AND it is the active node
+    // make it's parent the new active node
+    const newActiveNodeArray = [...node.url]
+    newActiveNodeArray.pop()
+    return setActiveNodeArray(newActiveNodeArray)
+  } else {
+    // the node is open
+    // but not the active node
+    // make it the new active node
+    const newActiveNodeArray = [...node.url]
+    setActiveNodeArray(newActiveNodeArray)
   }
-  setActiveNodeArray([...node.url])
 }
