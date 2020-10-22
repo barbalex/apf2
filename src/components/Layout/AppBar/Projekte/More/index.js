dee8046b6a476abaa9e05be519bfee4169f48735
Alexander Gabriel <alex.barbalex@gmail.com>
2020-10-22 14:50:22 +0200

    up version

--- src/components/Layout/AppBar/Projekte/More/index.js
+++ src/components/Layout/AppBar/Projekte/More/index.js
@@ -99,7 +99,7 @@ const AppbarMore = ({ onClickExporte: passedOnClickExporte, role }) => {
           onClick={onClickLogout}
           data-id="appbar-more-logout"
         >{`${user.name} abmelden (und Cache leeren)`}</MenuItem>
-        <Version>Version: 1.43.6 vom 21.10.2020</Version>
+        <Version>Version: 1.43.7 vom 22.10.2020</Version>
       </Menu>
     </Container>
   )
