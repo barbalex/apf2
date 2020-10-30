2157504183a5fad84f3d018b5c1287f0c398fe9c
Alexander Gabriel <alex.barbalex@gmail.com>
2020-10-30 17:12:56 +0100

    work on errors

--- src/components/shared/Error.js
+++ src/components/shared/Error.js
@@ -16,6 +16,7 @@ const LogoutButton = styled(Button)`
 `*/
 
 const Error = ({ errors: errorsPassed, error }) => {
+  const { idb } = useContext(idbContext)
   // allow user to pass single error or multiple errors
   let errors = errorsPassed
   if (error && !errorsPassed) errors = [error]
@@ -23,12 +24,9 @@ const Error = ({ errors: errorsPassed, error }) => {
   // something passes in an object instead of an array
   // so need to check and extract the errors array from the object if necessary
   const errorsToUse = errors.map ? errors : errors.errors
-  const { idb } = useContext(idbContext)
-  //console.log('Error, errors:', errors)
-  //console.log('Error, errorsToUse:', errorsToUse)
 
   if (existsPermissionError(errorsToUse)) {
-    console.log('Error will log out')
+    console.log('Permission error exists, will log out', { errorsToUse })
     // during login don't show permission error
     return logout(idb)
     /*// if token is not accepted, ask user to logout
@@ -47,15 +45,13 @@ const Error = ({ errors: errorsPassed, error }) => {
       </ErrorContainer>
     )*/
   }
-  //console.log('Error will list messages')
+
   const errorMessages = errorsToUse.map((e) => e.message)
-  //console.log('Error errorMessages:', errorMessages)
   const uniqueMessages = uniq(errorMessages)
-  //console.log('Error uniqueMessages:', uniqueMessages)
   if (uniqueMessages.length === 1) {
     return <ErrorContainer>{`Fehler: ${uniqueMessages[0]}`}</ErrorContainer>
   }
-  //console.log('Error returning all uniqueMessages')
+
   return (
     <ErrorContainer>
       <h5>Fehler:</h5>
