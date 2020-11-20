9dcc4f1576a5b93e57d449d3c3041bc668980b20
Alexander Gabriel <alex.barbalex@gmail.com>
2020-11-20 12:56:29 +0100

    prevent errors

--- src/components/EkPlan/Table/yearsFromTpops.js
+++ src/components/EkPlan/Table/yearsFromTpops.js
@@ -1,16 +1,15 @@
-import get from 'lodash/get'
 import minBy from 'lodash/minBy'
 import min from 'lodash/min'
 
-export default ({ tpops, pastYears }) => {
-  const ekplans = tpops.flatMap((tpop) => get(tpop, 'ekplansByTpopId.nodes'))
-  const kontrs = tpops.flatMap((tpop) => get(tpop, 'tpopkontrsByTpopId.nodes'))
-  const massns = tpops.flatMap((tpop) => get(tpop, 'tpopmassnsByTpopId.nodes'))
+const yearsFromTpops = ({ tpops, pastYears }) => {
+  const ekplans = tpops.flatMap((tpop) => tpop?.ekplansByTpopId?.nodes ?? [])
+  const kontrs = tpops.flatMap((tpop) => tpop?.tpopkontrsByTpopId?.nodes ?? [])
+  const massns = tpops.flatMap((tpop) => tpop?.tpopmassnsByTpopId?.nodes ?? [])
   const firstEk = minBy([...ekplans, ...kontrs], 'jahr')
   const firstMassn = minBy([...ekplans, ...massns], 'jahr')
   const currentYear = new Date().getFullYear()
-  const firstEkYear = firstEk ? firstEk.jahr : 999999
-  const firstMassnYear = firstEk ? firstMassn.jahr : 999999
+  const firstEkYear = firstEk?.jahr ?? 999999
+  const firstMassnYear = firstMassn?.jahr ?? 999999
   // ensure never before 1993
   let firstYear = min([firstEkYear, firstMassnYear])
   let lastYear = currentYear + 15
@@ -22,3 +21,5 @@ export default ({ tpops, pastYears }) => {
   }
   return years
 }
+
+export default yearsFromTpops
