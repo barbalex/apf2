eda8f0caee123338b24639f2bc0e4bff88274a26
Alexander Gabriel <alex.barbalex@gmail.com>
2020-12-16 21:22:59 +0100

    comment

--- src/components/Projekte/Daten/Ap/Auswertung/TpopKontrolliert/query.js
+++ src/components/Projekte/Daten/Ap/Auswertung/TpopKontrolliert/query.js
@@ -1,5 +1,6 @@
 import { gql } from '@apollo/client'
 
+// query: v_ap_ausw_tpop_kontrolliert
 export default gql`
   query apAuswertungTpopKontrolliert($id: UUID!) {
     allVApAuswTpopKontrollierts(
