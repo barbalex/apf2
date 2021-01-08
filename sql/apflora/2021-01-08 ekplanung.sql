42659565eea0416eabb25bc3522cc07eb93598ea
Alexander Gabriel <alex.barbalex@gmail.com>
2021-01-08 17:03:40 +0100

    plan ek

--- sql/apflora/2021-01-08 ekplanung.sql
+++ sql/apflora/2021-01-08 ekplanung.sql
@@ -248,3 +248,46 @@ from apflora.v_tpop_ekfrequenz_to_set
 -- nur noch dieses mal?
 where ap_id in ('6c52d173-4f62-11e7-aebe-2bd3a2ea4576', '6c52d126-4f62-11e7-aebe-cbb8319e1712', '6c52d134-4f62-11e7-aebe-f78ab946ed4e', '6c52d14b-4f62-11e7-aebe-13080d6c3ca2', '6c52d15d-4f62-11e7-aebe-d3ebe63c98ce', '6c52d16c-4f62-11e7-aebe-bf479a922be9', '6c52d33a-4f62-11e7-aebe-c362ab7bf00c', '6c52d17a-4f62-11e7-aebe-735510824f5c', '6c52d17c-4f62-11e7-aebe-1fc9d4d8081b', '6c52d1ab-4f62-11e7-aebe-0bac47bfd0e2', '6c52d1c5-4f62-11e7-aebe-0ba4179b00ef', '6c52d1d2-4f62-11e7-aebe-0f4a6fd6302d', '6c52d1d5-4f62-11e7-aebe-efe7798714be', '6c52d1d6-4f62-11e7-aebe-ff9a1728b3c2', '6c52d335-4f62-11e7-aebe-5325f98ff13e', '6c52d1e6-4f62-11e7-aebe-d365c4f34069', '6c52d1f6-4f62-11e7-aebe-dfe3eaf910cc', '6c52d1fb-4f62-11e7-aebe-1f005b242e3b', '6c52d1ff-4f62-11e7-aebe-ebb98d6fc3cd', '6c52d200-4f62-11e7-aebe-334a20278a47', '6c52d223-4f62-11e7-aebe-e3ef9e657ec9', '6c52d245-4f62-11e7-aebe-0319a140fe46', '6c52d24a-4f62-11e7-aebe-3b1fea21f435', '6c52d250-4f62-11e7-aebe-a71b014ac715', '6c52d25d-4f62-11e7-aebe-4719a1363629', '6c52d28b-4f62-11e7-aebe-1772be4a5746', '6c52d28e-4f62-11e7-aebe-7f7763f755a8', '6c52d28f-4f62-11e7-aebe-af6c305c3118', '6c52d290-4f62-11e7-aebe-4b95be9a1ab4', '6c52d29a-4f62-11e7-aebe-df2a375e3003', '6c52d2aa-4f62-11e7-aebe-07d8f7f7a15f', '6c52d336-4f62-11e7-aebe-b3f4fb153660', '6c52d2f0-4f62-11e7-aebe-138e69662b58', '6c52d2c5-4f62-11e7-aebe-1bae8687d749', '6c52d2d8-4f62-11e7-aebe-9b85b2bde977', '6c52d2f1-4f62-11e7-aebe-c357c17496bc', '6c52d2e6-4f62-11e7-aebe-7f6df0551226', '6c52d2e9-4f62-11e7-aebe-333ee57f5b45', '6c52d2fa-4f62-11e7-aebe-631cf4b57c2b', '6c52d2ff-4f62-11e7-aebe-dfaa30dbe005', '6c52d300-4f62-11e7-aebe-37a15e739517', '6c52d142-4f62-11e7-aebe-13d6fdd1c0e7', '6c52d30b-4f62-11e7-aebe-73528305a058', '6c52d344-4f62-11e7-aebe-8fd26f1c01cf', '6c52d1e2-4f62-11e7-aebe-13e4d5d74aaf', '6c52d31a-4f62-11e7-aebe-774437534c00', '6c52d225-4f62-11e7-aebe-b3e2d7efa4b5', '6c52d345-4f62-11e7-aebe-ef496a1afa23', '6c52d347-4f62-11e7-aebe-8b160a649947', '6c52d30a-4f62-11e7-aebe-17661b30ebf2', '6c52d346-4f62-11e7-aebe-c79e12a0ff58', '6c52d348-4f62-11e7-aebe-9be0ec6227b2', '6c52d30c-4f62-11e7-aebe-2fdf30155710')
 and apflora.tpop.id = apflora.v_tpop_ekfrequenz_to_set.tpop_id;
+
+-- Wäre es möglich, dass bei den dieses Jahr neu erloschenen Teilpopulationen die EK-Frequenz "nie" gesetzt wird?
+drop view if exists apflora.v_tpop_ekfrequenz_to_set_nie cascade;
+create or replace view apflora.v_tpop_ekfrequenz_to_set_nie as
+select 
+  tax.artname,
+  pop.nr,
+  tpop.id,
+  tpop.nr,
+  tpop.status as status_code,
+  status_werte.text as status_text,
+  tpophist.status as status_code_2019,
+  status_werte_hist.text as status_text_2019,
+  tpop.ekfrequenz as ekfrequenz_id,
+  ekfrequenz.code as ekfrequenz_code
+from apflora.tpop tpop
+  inner join apflora.tpop_history tpophist
+    inner join apflora.pop_status_werte status_werte_hist
+    on tpophist.status = status_werte_hist.code
+  on tpop.id = tpophist.id and tpophist.year = 2019
+  left join apflora.ekfrequenz ekfrequenz
+  on ekfrequenz.id = tpop.ekfrequenz
+  inner join apflora.pop_status_werte status_werte
+  on tpop.status = status_werte.code
+  inner join apflora.pop pop
+    inner join apflora.ap ap
+      inner join apflora.ae_taxonomies tax
+      on tax.id = ap.art_id
+    on ap.id = pop.ap_id
+  on pop.id = tpop.pop_id
+WHERE
+  tpop.status in (101, 202)
+  and tpophist.status not in (101, 202)
+  and (
+    -- ensure they are still named like this:
+    ekfrequenz.code not in ('nie (EK)', 'nie (EKF)')
+    or tpop.ekfrequenz is null
+  )
+order by
+  tax.artname,
+  pop.nr,
+  tpop.id,
+  tpop.nr;
