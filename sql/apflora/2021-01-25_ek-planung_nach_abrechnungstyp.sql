5baaac325c46147ccbb56a56df1780a91ca8b41e
Alexander Gabriel <alex.barbalex@gmail.com>
2021-01-25 11:00:12 +0100

    create query

---
+++ sql/apflora/2021-01-25_ek-planung_nach_abrechnungstyp.sql
@@ -0,0 +1,34 @@
+select
+  tax.artname,
+  adresse.name as artverantwortlich,
+  ekplan.jahr,
+  ek_abrechnungstyp_werte.text as ek_abrechnungstyp,
+  count(ekplan.id) as anzahl
+from
+  apflora.ekplan ekplan
+  inner join apflora.tpop tpop
+    inner join apflora.pop pop
+      inner join apflora.ap ap
+        inner join apflora.ae_taxonomies tax
+        on tax.id = ap.art_id
+        left join apflora.adresse adresse
+        on adresse.id = ap.bearbeiter
+      on ap.id = pop.ap_id
+    on pop.id = tpop.pop_id
+  on tpop.id = ekplan.tpop_id
+  inner join apflora.ekfrequenz ekfrequenz
+    inner join apflora.ek_abrechnungstyp_werte ek_abrechnungstyp_werte
+    on ek_abrechnungstyp_werte.code = ekfrequenz.ek_abrechnungstyp
+  on tpop.ekfrequenz = ekfrequenz.id
+where
+  tax.taxid > 150
+group by
+  tax.artname,
+  adresse.name,
+  ekplan.jahr,
+  ek_abrechnungstyp_werte.text
+order by
+  tax.artname,
+  ekplan.jahr,
+  ek_abrechnungstyp_werte.text;
+  
