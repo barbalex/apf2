a66037d732fe6922f820683ab14416f6cb57246c
Alexander Gabriel <alex.barbalex@gmail.com>
2021-02-01 21:15:51 +0100

    work on beob

--- sql/apflora/2021-02-01_beob_quelle.sql
+++ sql/apflora/2021-02-01_beob_quelle.sql
@@ -9,6 +9,7 @@ create function apflora.beob_label(beob apflora.beob) returns text as $$
   select to_char(beob.datum, 'YYYY.MM.DD') || ': ' || coalesce(beob.autor, '(kein Autor)') || beob.quelle || ')'
 $$ language sql stable;
 
+DROP VIEW IF EXISTS apflora.v_beob;
 CREATE OR REPLACE VIEW apflora.v_beob AS
 SELECT
   apflora.beob.id,
@@ -70,7 +71,7 @@ ORDER BY
   apflora.tpop.nr ASC,
   apflora.beob.datum DESC;
 
-
+drop view apflora.v_beob_art_changed;
 CREATE OR REPLACE VIEW apflora.v_beob_art_changed AS
 SELECT
   apflora.beob.id,
@@ -142,25 +143,37 @@ ORDER BY
   apflora.tpop.nr ASC,
   apflora.beob.datum DESC;
 
-
+drop VIEW apflora.v_apbeob;
 CREATE OR REPLACE VIEW apflora.v_apbeob AS
 select
-  apflora.beob.*,
-  apflora.beob.wgs84_lat,
-  apflora.beob.wgs84_long,
+  beob.id,
+  beob.quelle,
+  beob.id_field,
+  beob.art_id,
+  beob.datum,
+  beob.autor,
+  beob.geom_point,
+  beob.data,
+  beob.tpop_id,
+  beob.nicht_zuordnen,
+  beob.infoflora_informiert_datum,
+  beob.bemerkungen,
+  beob.changed,
+  beob.changed_by,
+  beob.wgs84_lat,
+  beob.wgs84_long,
   apflora.apart.ap_id,
-  apflora.beob.quelle,
-  to_char(apflora.beob.datum, 'YYYY.MM.DD') || ': ' || coalesce(apflora.beob.autor, '(kein Autor)') || ' (' || apflora.beob.quelle || ')' as label
+  to_char(beob.datum, 'YYYY.MM.DD') || ': ' || coalesce(beob.autor, '(kein Autor)') || ' (' || beob.quelle || ')' as label
 from
-  apflora.beob
+  apflora.beob beob
   inner join apflora.apart
-  on apflora.apart.art_id = apflora.beob.art_id
+  on apflora.apart.art_id = beob.art_id
 order by
-  apflora.beob.datum desc,
-  apflora.beob.autor asc,
-  apflora.beob.quelle asc;
+  beob.datum desc,
+  beob.autor asc,
+  beob.quelle asc;
 
-
+drop VIEW apflora.v_beob_art_changed;
 CREATE OR REPLACE VIEW apflora.v_beob_art_changed AS
 SELECT
   apflora.beob.id,
@@ -177,21 +190,17 @@ SELECT
   apflora.pop.nr as pop_nr,
   apflora.tpop.id AS tpop_id,
   apflora.tpop.nr AS tpop_nr,
-  apflora.beob.x,
-  apflora.beob.y,
+  pop_status_werte.text AS tpop_status,
+  apflora.tpop.gemeinde AS tpop_gemeinde,
+  apflora.tpop.flurname AS tpop_flurname,
+  apflora.beob.lv95_x as x,
+  apflora.beob.lv95_y as y,
   CASE
     WHEN
-      apflora.beob.x > 0
-      AND apflora.tpop.x > 0
-      AND apflora.beob.y > 0
-      AND apflora.tpop.y > 0
+      apflora.beob.lv95_x > 0
+      AND apflora.tpop.lv95_x > 0
     THEN
-      round(
-        sqrt(
-          power((apflora.beob.x - apflora.tpop.x), 2) +
-          power((apflora.beob.y - apflora.tpop.y), 2)
-        )
-      )
+      round(ST_Distance(ST_Transform(apflora.beob.geom_point, 2056), ST_Transform(apflora.tpop.geom_point, 2056)))
     ELSE
       NULL
   END AS distanz_zur_teilpopulation,
@@ -222,6 +231,9 @@ FROM
     apflora.tpop
     ON apflora.tpop.id = apflora.beob.tpop_id
     LEFT JOIN
+      apflora.pop_status_werte AS pop_status_werte
+      ON apflora.tpop.status = pop_status_werte.code
+    LEFT JOIN
       apflora.pop
       ON apflora.pop.id = apflora.tpop.pop_id
 WHERE
@@ -233,11 +245,29 @@ ORDER BY
   apflora.tpop.nr ASC,
   apflora.beob.datum DESC;
 
--- 3. correct all instances of beobQuelleWerte
--- 4. correct all instances of quelle_id, quelleId
+-- 3. correct all instances of beobQuelleWerte: done
+-- 4. correct all instances of quelle_id, quelleId: done
+-- 5. fill quelle field with data from beob_quelle_werte.name
+update apflora.beob beob
+set quelle = 'EvAB 2016'
+from apflora.beob_quelle_werte werte
+where
+  werte.id = beob.quelle_id
+  and werte.name = 'evab';
+  
+update apflora.beob beob
+set quelle = 'Info Flora 2017'
+from apflora.beob_quelle_werte werte
+where
+  werte.id = beob.quelle_id
+  and werte.name = 'infospezies';
 
-
-
+update apflora.beob beob
+set quelle = 'FloZ 2017'
+from apflora.beob_quelle_werte werte
+where
+  werte.id = beob.quelle_id
+  and werte.name = 'FloZ';
 
 -- zuletzt:
 alter table apflora.beob drop column quelle_id;
