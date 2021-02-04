9b6c4b067652900c330b65623997bbc931d3f25f
Alexander Gabriel <alex.barbalex@gmail.com>
2021-02-04 15:39:07 +0100

    work on jber query

--- sql/apflora/2021-02-04_jber_bereiche_a_b_c.sql
+++ sql/apflora/2021-02-04_jber_bereiche_a_b_c.sql
@@ -46,20 +46,134 @@ with a3LPop as (
     and tpop.bekannt_seit < ap.start_jahr
   group by
     pop.ap_id
+), a4LTPop as (
+  select
+    pop.ap_id,
+    count(distinct tpop.id) as count
+  from apflora.ap ap
+    inner join apflora.pop pop
+      inner join apflora.tpop tpop
+      on pop.id = tpop.pop_id
+    on pop.ap_id = ap.id
+  where
+    pop.status < 300
+    and pop.bekannt_seit <= 2020
+    and tpop.status = 200
+    and tpop.apber_relevant = true
+    and tpop.bekannt_seit <= 2020
+    and tpop.bekannt_seit < ap.start_jahr
+  group by
+    pop.ap_id
+), a5LPop as (
+  select
+    pop.ap_id,
+    count(distinct pop.id) as count
+  from apflora.ap ap
+    inner join apflora.pop pop
+      inner join apflora.tpop tpop
+      on pop.id = tpop.pop_id
+    on pop.ap_id = ap.id
+  where
+    pop.status = 200
+    and pop.bekannt_seit <= 2020
+    and pop.bekannt_seit >= ap.start_jahr
+    and tpop.apber_relevant = true
+    and tpop.bekannt_seit <= 2020
+    and tpop.bekannt_seit >= ap.start_jahr
+  group by
+    pop.ap_id
+), a5LTpop as (
+  select
+    pop.ap_id,
+    count(distinct tpop.id) as count
+  from apflora.ap ap
+    inner join apflora.pop pop
+      inner join apflora.tpop tpop
+      on pop.id = tpop.pop_id
+    on pop.ap_id = ap.id
+  where
+    pop.status < 300
+    and pop.bekannt_seit <= 2020
+    and tpop.status = 200
+    and tpop.apber_relevant = true
+    and tpop.bekannt_seit <= 2020
+    and tpop.bekannt_seit >= ap.start_jahr
+  group by
+    pop.ap_id
+), a7LPop as (
+  select
+    pop.ap_id,
+    count(distinct pop.id) as count
+  from apflora.ap ap
+    inner join apflora.pop pop
+      inner join apflora.tpop tpop
+      on pop.id = tpop.pop_id
+    on pop.ap_id = ap.id
+  where
+    (
+      pop.status = 101
+      or (
+        pop.status = 202
+        and pop.bekannt_seit < ap.start_jahr
+      )
+    )
+    and pop.bekannt_seit <= 2020
+    and tpop.apber_relevant = true
+    and tpop.bekannt_seit <= 2020
+  group by
+    pop.ap_id
+), a7LTpop as (
+  select
+    pop.ap_id,
+    count(distinct tpop.id) as count
+  from apflora.ap ap
+    inner join apflora.pop pop
+      inner join apflora.tpop tpop
+      on pop.id = tpop.pop_id
+    on pop.ap_id = ap.id
+  where
+    pop.status < 300
+    and pop.bekannt_seit <= 2020
+    and (
+      tpop.status = 101
+      or (
+        tpop.status = 202
+        and tpop.bekannt_seit < ap.start_jahr
+      )
+    )
+    and tpop.apber_relevant = true
+    and tpop.bekannt_seit <= 2020
+  group by
+    pop.ap_id
 )
 select
   tax.artname,
   ap.id,
   a3LPop.count as a_3_l_pop,
   a3LTpop.count as a_3_l_tpop,
-  a4LPop.count as a_4_l_pop
+  a4LPop.count as a_4_l_pop,
+  a4LTpop.count as a_4_l_tpop,
+  a5LPop.count as a_5_l_pop,
+  a5LTpop.count as a_5_l_tpop,
+  a7LPop.count as a_7_l_pop,
+  a7LTpop.count as a_7_l_tpop
 from apflora.ap
-  inner join a3LPop on
+  left join a3LPop on
   a3LPop.ap_id = ap.id
-  inner join a3LTpop on
+  left join a3LTpop on
   a3LTpop.ap_id = ap.id
-  inner join a4LPop on
+  left join a4LPop on
   a4LPop.ap_id = ap.id
+  left join a4LTpop on
+  a4LTpop.ap_id = ap.id
+  left join a5LPop on
+  a5LPop.ap_id = ap.id
+  left join a5LTpop on
+  a5LTpop.ap_id = ap.id
+  left join a7LPop on
+  a7LPop.ap_id = ap.id
+  left join a7LTpop on
+  a7LTpop.ap_id = ap.id
   inner join apflora.ae_taxonomies tax
   on tax.id = ap.art_id
 where
