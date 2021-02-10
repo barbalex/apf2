f2dafa29495b1f9d368b4828d6db276460dbcf76
Alexander Gabriel <alex.barbalex@gmail.com>
2021-02-10 11:19:01 +0100

    up version

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'apflora v1.62.12',
+    title: 'apflora v1.62.13',
   },
   plugins: [
     {
--- src/components/Layout/AppBar/Projekte/More/index.js
+++ src/components/Layout/AppBar/Projekte/More/index.js
@@ -107,7 +107,7 @@ const AppbarMore = ({ onClickExporte: passedOnClickExporte, role }) => {
         <MenuItem onClick={onClickUptime}>
           Verfügbarkeit der Server von apflora.ch
         </MenuItem>
-        <Version>Version: 1.62.12 vom 9.02.2021</Version>
+        <Version>Version: 1.62.13 vom 10.02.2021</Version>
       </Menu>
     </Container>
   )
