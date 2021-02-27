9e43147025669114d79184b3f01b020ee3dfdd63
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2021-02-27 11:14:04 +0100

    remove unneeded

--- src/components/shared/Fallback.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styled from 'styled-components'
-
-const LoadingContainer = styled.div`
-  padding: 10px;
-`
-
-export default () => <LoadingContainer>Lade...</LoadingContainer>
