bbe7fe02d4d3cde0479ad7045eec5606b0e78a67
Alexander Gabriel <alex.barbalex@gmail.com>
2021-07-21 12:24:10 +0200

    update deps

--- src/utils/materialTheme.js
+++ src/utils/materialTheme.js
@@ -1,11 +1,11 @@
-import { createMuiTheme } from "@material-ui/core/styles"
+import { createTheme } from '@material-ui/core/styles'
 //import orange from "@material-ui/core/colors/orange"
-import red from "@material-ui/core/colors/red"
-import green from "@material-ui/core/colors/green"
+import red from '@material-ui/core/colors/red'
+import green from '@material-ui/core/colors/green'
 
 // All the following keys are optional.
 // We try our best to provide a great default value.
-const theme = createMuiTheme({
+const theme = createTheme({
   palette: {
     primary: { main: green[800] },
     error: {
