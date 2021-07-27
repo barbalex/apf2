57b25d4c23504cafc5f2b0967aa6a9bd14bbca9d
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2021-07-27 16:36:37 +0200

    update deps

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'apflora v1.63.12',
+    title: 'apflora v1.63.13',
   },
   plugins: [
     {
--- src/components/Layout/AppBar/Projekte/More/index.js
+++ src/components/Layout/AppBar/Projekte/More/index.js
@@ -107,7 +107,7 @@ const AppbarMore = ({ onClickExporte: passedOnClickExporte, role }) => {
         <MenuItem onClick={onClickUptime}>
           Verfügbarkeit der Server von apflora.ch
         </MenuItem>
-        <Version>Version: 1.63.12 vom 22.07.2021</Version>
+        <Version>Version: 1.63.13 vom 27.07.2021</Version>
       </Menu>
     </Container>
   )
