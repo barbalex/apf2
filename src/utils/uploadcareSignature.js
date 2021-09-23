ad4413df05964428748c94cb2e7aa20a685ebf6c
Alexander Gabriel <alex.barbalex@gmail.com>
2021-09-23 14:53:30 +0200

    remove md5-hex

--- src/utils/uploadcareSignature.js
+++ src/utils/uploadcareSignature.js
@@ -1,7 +1,7 @@
-import md5Hex from 'md5-hex'
+import md5 from 'blueimp-md5'
 
 const secret = process.env.GATSBY_UPLOADCARE_SECRET_KEY
 // Expire in 30 min e.g. 1454903856
 export const expire = Math.round(new Date().getTime() / 1000) + 60 * 30
 const toSign = secret + expire
-export const signature = md5Hex(toSign)
+export const signature = md5(toSign)
