c323f985e4db83c6229da604e2efe520eb2a612c
Alexander Gabriel <alex.barbalex@gmail.com>
2021-11-17 22:19:13 +0100

    add role

--- backend/db/init/01_pre_restore.sql
+++ backend/db/init/01_pre_restore.sql
@@ -1,18 +1,19 @@
 CREATE DATABASE apflora WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';
-\c apflora
-
--- not necessary as done by restore.sh
+\ c apflora --
+-- not necessary as done by restore.sh:
 --CREATE DATABASE apflora encoding 'UTF8';
 --create extension if not exists postgis;
 --CREATE EXTENSION if not exists pgcrypto;
 --CREATE EXTENSION if not exists "uuid-ossp";
 --create extension if not exists tablefunc;
 --CREATE EXTENSION intarray;
-
+--
 create role apflora_reader;
 create role apflora_ap_reader;
 create role apflora_manager in group apflora_reader;
 create role apflora_ap_writer in group apflora_reader;
+-- seems this role though not used any more is needed when restoring a backup:
+create role apflora_artverantwortlich;
 -- TODO: drop role apflora_artverantwortlich;
 create role apflora_freiwillig;
 create role anon;
