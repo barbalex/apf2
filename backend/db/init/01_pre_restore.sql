71f6c362ceb3e73db951e884ba9ecb5182de4d83
Alexander Gabriel <alex.barbalex@gmail.com>
2021-11-18 10:37:32 +0100

    optimize

--- backend/db/init/01_pre_restore.sql
+++ backend/db/init/01_pre_restore.sql
@@ -1,13 +1,5 @@
 CREATE DATABASE apflora WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';
-\ c apflora --
--- not necessary as done by restore.sh:
---CREATE DATABASE apflora encoding 'UTF8';
---create extension if not exists postgis;
---CREATE EXTENSION if not exists pgcrypto;
---CREATE EXTENSION if not exists "uuid-ossp";
---create extension if not exists tablefunc;
---CREATE EXTENSION intarray;
---
+\ c apflora;
 create role apflora_reader;
 create role apflora_ap_reader;
 create role apflora_manager in group apflora_reader;
