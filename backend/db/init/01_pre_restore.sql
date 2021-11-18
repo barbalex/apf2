2db56c611d331a70196304681bd47009d1db159a
Alexander Gabriel <alex.barbalex@gmail.com>
2021-11-18 10:37:53 +0100

    comment

--- backend/db/init/01_pre_restore.sql
+++ backend/db/init/01_pre_restore.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE apflora WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';
 \ c apflora;
+-- not sure if this hyphen is correct
 create role apflora_reader;
 create role apflora_ap_reader;
 create role apflora_manager in group apflora_reader;
