a9e991bc6cd27a4e657c51f86e5acf96ad5aadff
Alexander Gabriel <alex.barbalex@gmail.com>
2021-11-19 13:19:38 +0100

    format

--- backend/db/init/01_pre_restore.sql
+++ backend/db/init/01_pre_restore.sql
@@ -1,10 +1,21 @@
 CREATE DATABASE apflora WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';
-\ c apflora;
+
+c apflora;
+
 -- not sure if this hyphen is correct
-create role apflora_reader;
-create role apflora_ap_reader;
-create role apflora_manager in group apflora_reader;
-create role apflora_ap_writer in group apflora_reader;
-create role apflora_freiwillig;
-create role anon;
-create role authenticator with login password '${AUTHENTICATOR_PASSWORD}' noinherit;
+CREATE ROLE apflora_reader;
+
+CREATE ROLE apflora_ap_reader;
+
+CREATE ROLE apflora_manager IN
+GROUP apflora_reader;
+
+CREATE ROLE apflora_ap_writer IN
+GROUP apflora_reader;
+
+CREATE ROLE apflora_freiwillig;
+
+CREATE ROLE anon;
+
+CREATE ROLE authenticator WITH LOGIN PASSWORD '${AUTHENTICATOR_PASSWORD}' noinherit;
+
