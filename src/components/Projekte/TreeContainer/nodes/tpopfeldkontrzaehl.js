5cbc6252e4015ed526ca79887e84cc086bfb1b78
Alexander Gabriel <alex.barbalex@gmail.com>
2021-12-01 15:46:20 +0100

    optimize

--- src/components/Projekte/TreeContainer/nodes/tpopfeldkontrzaehl.js
+++ src/components/Projekte/TreeContainer/nodes/tpopfeldkontrzaehl.js
@@ -1,11 +1,8 @@
 import findIndex from 'lodash/findIndex'
-import get from 'lodash/get'
-import memoizeOne from 'memoize-one'
 
 const tpopfeldkontrzaehlNodes = ({
   nodes: nodesPassed,
   data,
-  treeName,
   projektNodes,
   apNodes,
   popNodes,
@@ -16,7 +13,6 @@ const tpopfeldkontrzaehlNodes = ({
   popId,
   tpopId,
   tpopkontrId,
-  store,
 }) => {
   // fetch sorting indexes of parents
   const projIndex = findIndex(projektNodes, {
@@ -28,58 +24,56 @@ const tpopfeldkontrzaehlNodes = ({
   const tpopkontrIndex = findIndex(tpopfeldkontrNodes, { id: tpopkontrId })
 
   // map through all elements and create array of nodes
-  const nodes = memoizeOne(() =>
-    get(data, 'allTpopkontrzaehls.nodes', [])
-      // only show if parent node exists
-      .filter((el) =>
-        nodesPassed
-          .map((n) => n.id)
-          .includes(`${el.tpopkontrId}TpopfeldkontrzaehlFolder`),
-      )
-      // only show nodes of this parent
-      .filter((el) => el.tpopkontrId === tpopkontrId)
-      .map((el) => ({
-        nodeType: 'table',
-        menuType: 'tpopfeldkontrzaehl',
-        filterTable: 'tpopkontrzaehl',
-        id: el.id,
-        parentId: `${el.tpopkontrId}TpopfeldkontrzaehlFolder`,
-        parentTableId: el.tpopkontrId,
-        urlLabel: el.id,
-        label: el.label,
-        url: [
-          'Projekte',
-          projId,
-          'Aktionspläne',
-          apId,
-          'Populationen',
-          popId,
-          'Teil-Populationen',
-          tpopId,
-          'Feld-Kontrollen',
-          tpopkontrId,
-          'Zaehlungen',
-          el.id,
-        ],
-        hasChildren: false,
-      }))
-      .map((el, index) => {
-        el.sort = [
-          projIndex,
-          1,
-          apIndex,
-          1,
-          popIndex,
-          1,
-          tpopIndex,
-          3,
-          tpopkontrIndex,
-          1,
-          index,
-        ]
-        return el
-      }),
-  )()
+  const nodes = (data?.allTpopkontrzaehls?.nodes ?? [])
+    // only show if parent node exists
+    .filter((el) =>
+      nodesPassed
+        .map((n) => n.id)
+        .includes(`${el.tpopkontrId}TpopfeldkontrzaehlFolder`),
+    )
+    // only show nodes of this parent
+    .filter((el) => el.tpopkontrId === tpopkontrId)
+    .map((el) => ({
+      nodeType: 'table',
+      menuType: 'tpopfeldkontrzaehl',
+      filterTable: 'tpopkontrzaehl',
+      id: el.id,
+      parentId: `${el.tpopkontrId}TpopfeldkontrzaehlFolder`,
+      parentTableId: el.tpopkontrId,
+      urlLabel: el.id,
+      label: el.label,
+      url: [
+        'Projekte',
+        projId,
+        'Aktionspläne',
+        apId,
+        'Populationen',
+        popId,
+        'Teil-Populationen',
+        tpopId,
+        'Feld-Kontrollen',
+        tpopkontrId,
+        'Zaehlungen',
+        el.id,
+      ],
+      hasChildren: false,
+    }))
+    .map((el, index) => {
+      el.sort = [
+        projIndex,
+        1,
+        apIndex,
+        1,
+        popIndex,
+        1,
+        tpopIndex,
+        3,
+        tpopkontrIndex,
+        1,
+        index,
+      ]
+      return el
+    })
 
   return nodes
 }
