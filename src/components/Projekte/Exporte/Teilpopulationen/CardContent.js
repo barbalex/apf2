767677fed3fbef73585cbbdcd6582309258ca19c
Alexander Gabriel <alex.barbalex@gmail.com>
2021-12-22 16:17:22 +0100

    refactor exports

--- src/components/Projekte/Exporte/Teilpopulationen/CardContent.js
+++ src/components/Projekte/Exporte/Teilpopulationen/CardContent.js
@@ -14,6 +14,8 @@ import LetzteZaehlungenInklAnpflanzungen from './LetzteZaehlungenInklAnpflanzung
 import TPopInklBerichte from './TPopInklBerichte'
 import AnzMassnahmen from './AnzMassnahmen'
 import TPopOhneApberRelevant from './TPopOhneApberRelevant'
+import TPopOhneBekanntSeit from './TPopOhneBekanntSeit'
+import TPopFuerGEArtname from './TPopFuerGEArtname'
 import { StyledCardContent, DownloadCardButton } from '../index'
 
 const Teilpopulationen = () => {
@@ -123,101 +125,8 @@ const Teilpopulationen = () => {
         <div>Teilpopulationen für Google Earth</div>
         <div>(beschriftet mit PopNr/TPopNr)</div>
       </DownloadCardButton>
-      <DownloadCardButton
-        color="inherit"
-        onClick={async () => {
-          const notif = enqueNotification({
-            message: `Export "TeilpopulationenNachNamen" wird vorbereitet...`,
-            options: {
-              variant: 'info',
-              persist: true,
-            },
-          })
-          let result
-          try {
-            result = await client.query({
-              query: await import('./queryTpopKmlNamen').then((m) => m.default),
-            })
-          } catch (error) {
-            enqueNotification({
-              message: error.message,
-              options: { variant: 'error' },
-            })
-          }
-          const rows = (result.data?.allTpops?.nodes ?? []).map((z) => ({
-            art: z?.vTpopKmlnamenById?.nodes?.[0]?.art ?? '',
-            label: z?.vTpopKmlnamenById?.nodes?.[0]?.label ?? '',
-            inhalte: z?.vTpopKmlnamenById?.nodes?.[0]?.inhalte ?? '',
-            id: z?.vTpopKmlnamenById?.nodes?.[0]?.id ?? '',
-            wgs84Lat: z?.vTpopKmlnamenById?.nodes?.[0]?.wgs84Lat ?? '',
-            wgs84Long: z?.vTpopKmlnamenById?.nodes?.[0]?.wgs84Long ?? '',
-            url: z?.vTpopKmlnamenById?.nodes?.[0]?.url ?? '',
-          }))
-          removeNotification(notif)
-          closeSnackbar(notif)
-          if (rows.length === 0) {
-            return enqueNotification({
-              message: 'Die Abfrage retournierte 0 Datensätze',
-              options: {
-                variant: 'warning',
-              },
-            })
-          }
-          exportModule({
-            data: sortBy(rows, ['art', 'label']),
-            fileName: 'TeilpopulationenNachNamen',
-            store,
-            kml: true,
-          })
-        }}
-      >
-        <div>Teilpopulationen für Google Earth</div>
-        <div>(beschriftet mit Artname, PopNr/TPopNr)</div>
-      </DownloadCardButton>
-      <DownloadCardButton
-        color="inherit"
-        onClick={async () => {
-          const notif = enqueNotification({
-            message: `Export "TeilpopulationenVonApArtenOhneBekanntSeit" wird vorbereitet...`,
-            options: {
-              variant: 'info',
-              persist: true,
-            },
-          })
-          let result
-          try {
-            result = await client.query({
-              query: await import('./allVTpopOhnebekanntseits').then(
-                (m) => m.default,
-              ),
-            })
-          } catch (error) {
-            enqueNotification({
-              message: error.message,
-              options: { variant: 'error' },
-            })
-          }
-          const rows = result.data?.allVTpopOhnebekanntseits?.nodes ?? []
-          removeNotification(notif)
-          closeSnackbar(notif)
-          if (rows.length === 0) {
-            return enqueNotification({
-              message: 'Die Abfrage retournierte 0 Datensätze',
-              options: {
-                variant: 'warning',
-              },
-            })
-          }
-          exportModule({
-            data: rows,
-            fileName: 'TeilpopulationenVonApArtenOhneBekanntSeit',
-            store,
-          })
-        }}
-      >
-        <div>Teilpopulationen von AP-Arten</div>
-        <div>{'ohne "Bekannt seit"'}</div>
-      </DownloadCardButton>
+      <TPopFuerGEArtname />
+      <TPopOhneBekanntSeit />
       <TPopOhneApberRelevant />
       <AnzMassnahmen />
       <Wollmilchsau />
