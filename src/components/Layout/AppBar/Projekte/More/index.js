805ecd8fb75d55fb6943adcdc0f81e8b2897d72a
Alexander Gabriel <alex.barbalex@gmail.com>
2021-12-22 21:29:16 +0100

    up version

--- src/components/Layout/AppBar/Projekte/More/index.js
+++ src/components/Layout/AppBar/Projekte/More/index.js
@@ -107,7 +107,7 @@ const AppbarMore = ({ onClickExporte: passedOnClickExporte, role }) => {
         <MenuItem onClick={onClickUptime}>
           Verfügbarkeit der Server von apflora.ch
         </MenuItem>
-        <Version>Version: 1.66.24 vom 22.12.2021</Version>
+        <Version>Version: 1.66.25 vom 22.12.2021</Version>
       </Menu>
     </Container>
   )
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -31,7 +31,7 @@ const Layout = ({ children }) => {
   return (
     <Container>
       <Helmet
-        title="apflora v1.66.24"
+        title="apflora v1.66.25"
         meta={[
           {
             name: 'description',
