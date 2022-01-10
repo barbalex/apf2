ea2789ca1b5f12e3bb766049b3b04d09392164a0
Alexander Gabriel <alex.barbalex@gmail.com>
2022-01-10 11:06:52 +0100

    comment

--- src/components/Projekte/Karte/layers/SwisstopoPixelGrau.js
+++ src/components/Projekte/Karte/layers/SwisstopoPixelGrau.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { TileLayer } from 'react-leaflet'
 
+// https://wmts.geo.admin.ch/EPSG/3857/1.0.0/WMTSCapabilities.xml
+
 const SwissTopoPixelFarbeLayer = () => (
   <TileLayer
     url="//wmts.geo.admin.ch/1.0.0/ch.swisstopo.pixelkarte-grau/default/current/3857/{z}/{x}/{y}.jpeg"
