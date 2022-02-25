130dc48b55f962a01571414d3569730f2142407b
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2022-02-25 11:28:29 +0100

    solve 526

--- src/components/Layout/AppBar/Projekte/More/index.js
+++ src/components/Layout/AppBar/Projekte/More/index.js
@@ -107,7 +107,7 @@ const AppbarMore = ({ onClickExporte: passedOnClickExporte, role }) => {
         <MenuItem onClick={onClickUptime}>
           Verfügbarkeit der Server von apflora.ch
         </MenuItem>
-        <Version>Version: 1.68.5 vom 15.02.2022</Version>
+        <Version>Version: 1.68.6 vom 25.02.2022</Version>
       </Menu>
     </Container>
   )
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -31,7 +31,7 @@ const Layout = ({ children }) => {
   return (
     <Container>
       <Helmet
-        title="apflora v1.68.5"
+        title="apflora v1.68.6"
         meta={[
           {
             name: 'description',
