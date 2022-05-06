69a29c4c9af1fa5b3ff61722cffd94c27b319b9e
Alexander Gabriel <alex.barbalex@gmail.com>
2022-05-06 22:44:18 +0200

    optimize

--- sql/apflora/one_offs/2022-05-06_check_apart.sql
+++ sql/apflora/one_offs/2022-05-06_check_apart.sql
@@ -1,11 +1,9 @@
 WITH multiple_used_taxa AS (
   SELECT
-    apart.art_id,
+    apart.art_id AS apart_art_id,
     count(apart.id)
   FROM
     apflora.apart apart
-    INNER JOIN apflora.ae_taxonomies tax ON tax.id = apart.art_id
-    INNER JOIN apflora.ap ap ON ap.id = apart.ap_id
   WHERE
     apart.art_id IS NOT NULL
   GROUP BY
@@ -14,12 +12,15 @@ WITH multiple_used_taxa AS (
     count(apart.id) > 1
 )
 SELECT
-  multiple_used_taxa.art_id as apart_id,
+  apart.art_id AS apart_art_id,
   tax.tax_art_name AS apart_artname,
-  multiple_used_taxa.count as apart_count,
-  apart.ap_id
+  --multiple_used_taxa.count AS apart_count,
+  apart.ap_id AS apart_ap_id,
+  ap_tax.tax_art_name AS ap_artname
 FROM
   apflora.apart apart
   INNER JOIN apflora.ae_taxonomies tax ON tax.id = apart.art_id
-  INNER JOIN multiple_used_taxa ON multiple_used_taxa.art_id = apart.art_id;
+  INNER JOIN multiple_used_taxa ON multiple_used_taxa.apart_art_id = apart.art_id
+  INNER JOIN apflora.ap ap ON ap.id = apart.ap_id
+  INNER JOIN apflora.ae_taxonomies ap_tax ON ap_tax.id = ap.art_id;
 
