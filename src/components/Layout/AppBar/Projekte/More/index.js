48eff18a74a170ac18c6181c7f5a2dc3af09118a
Alexander Gabriel <alex.barbalex@gmail.com>
2022-05-11 14:05:57 +0200

    solve 1.68.18

--- src/components/Layout/AppBar/Projekte/More/index.js
+++ src/components/Layout/AppBar/Projekte/More/index.js
@@ -107,7 +107,7 @@ const AppbarMore = ({ onClickExporte: passedOnClickExporte, role }) => {
         <MenuItem onClick={onClickUptime}>
           Verfügbarkeit der Server von apflora.ch
         </MenuItem>
-        <Version>Version: 1.68.18 vom 6.05.2022</Version>
+        <Version>Version: 1.68.19 vom 11.05.2022</Version>
       </Menu>
     </Container>
   )
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -31,7 +31,7 @@ const Layout = ({ children }) => {
   return (
     <Container>
       <Helmet
-        title="apflora v1.68.18"
+        title="apflora v1.68.19"
         meta={[
           {
             name: 'description',
