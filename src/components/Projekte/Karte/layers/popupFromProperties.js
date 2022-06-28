08c74490788d1928b75b32273791f98928762c99
Alexander Gabriel <alex.barbalex@gmail.com>
2022-06-28 17:18:53 +0200

    work on popups

--- src/components/Projekte/Karte/layers/popupFromProperties.js
+++ src/components/Projekte/Karte/layers/popupFromProperties.js
@@ -1,7 +1,7 @@
 // alternative: renderToString
 // see: https://gis.stackexchange.com/a/356513/13491
-const PopupFromProperties = ({ properties, layerName }) =>
-  `<div>
+const PopupFromProperties = ({ properties, layerName }) => {
+  return `<div>
   ${Object.entries(properties)
     .filter((e) => !!e[1])
     .sort()
@@ -13,5 +13,6 @@ const PopupFromProperties = ({ properties, layerName }) =>
     )
     .join('')}
 </div>`
+}
 
 export default PopupFromProperties
