d06977e7d47ffd5367658af5fa39e65ba79c9ccb
Alexander Gabriel <alex.barbalex@gmail.com>
2022-07-02 16:56:11 +0200

    add id

--- src/components/Projekte/Daten/ApFilter/queryAeTaxonomies.js
+++ src/components/Projekte/Daten/ApFilter/queryAeTaxonomies.js
@@ -4,6 +4,7 @@ export default gql`
   query ApFilterAeTaxonomiesQuery($filter: AeTaxonomyFilter!) {
     allAeTaxonomies(first: 8, filter: $filter, orderBy: ARTNAME_ASC) {
       nodes {
+        id
         value: id
         label: artname
       }
