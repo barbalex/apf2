4505ce1c59cc2a3ca3eec3a538fea2f9228497bb
Alexander Gabriel <alex.barbalex@gmail.com>
2022-07-04 14:56:14 +0200

    work on pop or filter

--- src/components/Projekte/Daten/PopFilter/queryPops.js
+++ src/components/Projekte/Daten/PopFilter/queryPops.js
@@ -3,22 +3,14 @@ import { gql } from '@apollo/client'
 export default gql`
   query allPopsQueryForPopFilter(
     $popFilter: PopFilter!
-    $popApFilter: PopFilter!
-    $allPopsFilter: PopFilter!
     $apId: UUID
     $apIdExists: Boolean!
   ) {
-    allPops(filter: $allPopsFilter) @include(if: $apIdExists) {
-      totalCount
-    }
-    popsFiltered: allPops(filter: $popFilter) @include(if: $apIdExists) {
-      totalCount
-    }
-    popsOfAp: allPops(filter: { apId: { equalTo: $apId } })
+    pops: allPops(filter: { apId: { equalTo: $apId } })
       @include(if: $apIdExists) {
       totalCount
     }
-    popsOfApFiltered: allPops(filter: $popApFilter) @include(if: $apIdExists) {
+    popsFiltered: allPops(filter: $popFilter) @include(if: $apIdExists) {
       totalCount
     }
   }
