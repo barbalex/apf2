474adf26b051559b06df438a0675bc3dff9467f5
Alexander Gabriel <alex.barbalex@gmail.com>
2022-07-05 12:06:25 +0200

    up version

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -31,7 +31,7 @@ const Layout = ({ children }) => {
   return (
     <Container>
       <Helmet
-        title="apflora v1.74.0"
+        title="apflora v1.74.2"
         meta={[
           {
             name: 'description',
