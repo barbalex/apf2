fcd4b09ea1eb630f3285682d57735f8b0bcdfbd0
Alexander Gabriel <alex.barbalex@gmail.com>
2022-07-12 16:54:21 +0200

    work on beobGqlFilter

--- src/components/Projekte/Karte/layers/BeobZugeordnet/index.js
+++ src/components/Projekte/Karte/layers/BeobZugeordnet/index.js
@@ -1,9 +1,8 @@
-import React, { useContext, useMemo, useEffect, useState } from 'react'
-import flatten from 'lodash/flatten'
+import React, { useContext } from 'react'
 import { observer } from 'mobx-react-lite'
 import { useQuery } from '@apollo/client'
 import MarkerClusterGroup from '@changey/react-leaflet-markercluster'
-import { useMap } from 'react-leaflet'
+// import { useMap } from 'react-leaflet'
 
 import Marker from './Marker'
 import storeContext from '../../../../../storeContext'
@@ -26,48 +25,30 @@ const iconCreateFunction = function (cluster) {
 }
 
 const BeobZugeordnetMarker = ({ treeName, clustered }) => {
-  const leafletMap = useMap()
+  // const leafletMap = useMap()
   const store = useContext(storeContext)
   const { setRefetchKey, enqueNotification } = store
   const tree = store[treeName]
-  const { apIdInActiveNodeArray, projIdInActiveNodeArray } = tree
-
-  const projId =
-    projIdInActiveNodeArray ?? '99999999-9999-9999-9999-999999999999'
-  const apId = apIdInActiveNodeArray ?? '99999999-9999-9999-9999-999999999999'
-
-  const beobFilter = {
-    tpopId: { isNull: false },
-    nichtZuordnen: { equalTo: false },
-    wgs84Lat: { isNull: false },
-    // 2021.08.16: needed to remove this filter
-    // because icons where added every time a tpop left, then reentered the bbox
-    // geomPoint: { within: myBbox },
-  }
-  if (tree.nodeLabelFilter.beob) {
-    beobFilter.label = {
-      includesInsensitive: tree.nodeLabelFilter.beob,
-    }
-  }
+  const { beobGqlFilter } = tree
 
   var { data, error, refetch } = useQuery(query, {
-    variables: { projId, apId, beobFilter },
+    variables: { beobFilter: beobGqlFilter('nichtZugeordnet').filtered },
   })
   setRefetchKey({ key: 'beobZugeordnetForMap', value: refetch })
 
   // eslint-disable-next-line no-unused-vars
-  const [refetchProvoker, setRefetchProvoker] = useState(1)
-  useEffect(() => {
-    // DO NOT use:
-    // leafletMap.on('zoomend dragend', refetch
-    // see: https://github.com/apollographql/apollo-client/issues/1291#issuecomment-367911441
-    // Also: leafletMap.on('zoomend dragend', ()=> refetch()) never refetches!!??
-    // Also: use dragend, not moveend because moveend fires on zoomend as well
-    leafletMap.on('zoomend dragend', () => setRefetchProvoker(Math.random()))
-    return () => {
-      leafletMap.off('zoomend dragend', () => setRefetchProvoker(Math.random()))
-    }
-  }, [leafletMap])
+  // const [refetchProvoker, setRefetchProvoker] = useState(1)
+  // useEffect(() => {
+  //   // DO NOT use:
+  //   // leafletMap.on('zoomend dragend', refetch
+  //   // see: https://github.com/apollographql/apollo-client/issues/1291#issuecomment-367911441
+  //   // Also: leafletMap.on('zoomend dragend', ()=> refetch()) never refetches!!??
+  //   // Also: use dragend, not moveend because moveend fires on zoomend as well
+  //   leafletMap.on('zoomend dragend', () => setRefetchProvoker(Math.random()))
+  //   return () => {
+  //     leafletMap.off('zoomend dragend', () => setRefetchProvoker(Math.random()))
+  //   }
+  // }, [leafletMap])
 
   if (error) {
     enqueNotification({
@@ -78,19 +59,7 @@ const BeobZugeordnetMarker = ({ treeName, clustered }) => {
     })
   }
 
-  const aparts = useMemo(
-    () => data?.projektById?.apsByProjId?.nodes?.[0]?.apartsByApId?.nodes ?? [],
-    [data?.projektById?.apsByProjId?.nodes],
-  )
-  let beobs = useMemo(
-    () =>
-      flatten(
-        aparts.map((a) => a?.aeTaxonomyByArtId?.beobsByArtId?.nodes ?? []),
-      ),
-    [aparts],
-  )
-
-  const beobMarkers = beobs.map((beob) => (
+  const beobMarkers = (data?.allBeobs?.nodes ?? []).map((beob) => (
     <Marker key={beob.id} treeName={treeName} beob={beob} />
   ))
 
