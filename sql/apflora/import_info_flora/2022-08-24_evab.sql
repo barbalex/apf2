424f57551e2e667c98bdc60f4a4f6ac2e8c63a62
Alexander Gabriel <alex.barbalex@gmail.com>
2022-08-24 18:22:53 +0200

    work on beob import

--- sql/apflora/import_info_flora/2022-08-24_evab.sql
+++ sql/apflora/import_info_flora/2022-08-24_evab.sql
@@ -17,17 +17,192 @@ WHERE
 ORDER BY
   beob_from_evab.id_evab_lc;
 
--- 1000 guids
+-- 3891 guids
 --
 -- 5 cases:
 -- 1 both tpop_id null
 --   delete evab beob
+WITH dat AS (
+  SELECT
+    beob_from_evab.id_evab_lc AS guid,
+    beob.id AS if_id,
+    beob.quelle AS if_quelle,
+    beob.tpop_id AS if_tpop_id,
+    beob_from_evab.id AS evab_id,
+    beob_from_evab.quelle AS evab_quelle,
+    beob_from_evab.tpop_id AS evab_tpop_id
+  FROM
+    apflora.beob beob
+    INNER JOIN apflora.beob beob_from_evab ON beob_from_evab.id_evab_lc = beob.data ->> 'guid'
+  WHERE
+    beob.quelle <> 'EvAB 2016'
+    AND beob_from_evab.quelle = 'EvAB 2016'
+    --AND beob.id <> beob_from_evab.id
+  ORDER BY
+    beob_from_evab.id_evab_lc
+),
+todelete AS (
+  SELECT
+    evab_id
+  FROM
+    dat
+  WHERE
+    if_tpop_id IS NULL
+    AND evab_tpop_id IS NULL)
+DELETE FROM apflora.beob
+WHERE id IN (
+    SELECT
+      *
+    FROM
+      todelete);
+
+-- 373
+--
 -- 2 both tpop_id set - same tpop
 --   delete evab beob
+WITH dat AS (
+  SELECT
+    beob_from_evab.id_evab_lc AS guid,
+    beob.id AS if_id,
+    beob.quelle AS if_quelle,
+    beob.tpop_id AS if_tpop_id,
+    beob_from_evab.id AS evab_id,
+    beob_from_evab.quelle AS evab_quelle,
+    beob_from_evab.tpop_id AS evab_tpop_id
+  FROM
+    apflora.beob beob
+    INNER JOIN apflora.beob beob_from_evab ON beob_from_evab.id_evab_lc = beob.data ->> 'guid'
+  WHERE
+    beob.quelle <> 'EvAB 2016'
+    AND beob_from_evab.quelle = 'EvAB 2016'
+    --AND beob.id <> beob_from_evab.id
+  ORDER BY
+    beob_from_evab.id_evab_lc
+),
+todelete AS (
+  SELECT
+    evab_id
+  FROM
+    dat
+  WHERE
+    if_tpop_id IS NOT NULL
+    AND evab_tpop_id IS NOT NULL
+    AND if_tpop_id = evab_tpop_id)
+DELETE FROM apflora.beob
+WHERE id IN (
+    SELECT
+      *
+    FROM
+      todelete);
+
+-- 1485
+--
 -- 3 both tpop_id set - different tpop
 --   list and tell topos
--- 4 if_tpop_id set
+WITH dat AS (
+  SELECT
+    beob_from_evab.id_evab_lc AS guid,
+    beob.id AS if_id,
+    beob.quelle AS if_quelle,
+    beob.tpop_id AS if_tpop_id,
+    beob_from_evab.id AS evab_id,
+    beob_from_evab.quelle AS evab_quelle,
+    beob_from_evab.tpop_id AS evab_tpop_id
+  FROM
+    apflora.beob beob
+    INNER JOIN apflora.beob beob_from_evab ON beob_from_evab.id_evab_lc = beob.data ->> 'guid'
+  WHERE
+    beob.quelle <> 'EvAB 2016'
+    AND beob_from_evab.quelle = 'EvAB 2016'
+    --AND beob.id <> beob_from_evab.id
+  ORDER BY
+    beob_from_evab.id_evab_lc
+)
+SELECT
+  *
+FROM
+  dat
+WHERE
+  if_tpop_id IS NOT NULL
+  AND evab_tpop_id IS NOT NULL
+  AND if_tpop_id <> evab_tpop_id;
+
+-- 86
+--
+-- 4 only if_tpop_id set
 --   delete evab beob
--- 5 evab_tpop_id set
+WITH dat AS (
+  SELECT
+    beob_from_evab.id_evab_lc AS guid,
+    beob.id AS if_id,
+    beob.quelle AS if_quelle,
+    beob.tpop_id AS if_tpop_id,
+    beob_from_evab.id AS evab_id,
+    beob_from_evab.quelle AS evab_quelle,
+    beob_from_evab.tpop_id AS evab_tpop_id
+  FROM
+    apflora.beob beob
+    INNER JOIN apflora.beob beob_from_evab ON beob_from_evab.id_evab_lc = beob.data ->> 'guid'
+  WHERE
+    beob.quelle <> 'EvAB 2016'
+    AND beob_from_evab.quelle = 'EvAB 2016'
+    --AND beob.id <> beob_from_evab.id
+  ORDER BY
+    beob_from_evab.id_evab_lc
+),
+todelete AS (
+  SELECT
+    evab_id
+  FROM
+    dat
+  WHERE
+    if_tpop_id IS NOT NULL
+    AND evab_tpop_id IS NULL)
+DELETE FROM apflora.beob
+WHERE id IN (
+    SELECT
+      *
+    FROM
+      todelete);
+
+-- 69
+--
+-- 5 only evab_tpop_id set
 --   set tpop_id (and other fields) in if
 --   delete evab beob
+-- TODO: first set tpop_id (and other fields) in if
+WITH dat AS (
+  SELECT
+    beob_from_evab.id_evab_lc AS guid,
+    beob.id AS if_id,
+    beob.quelle AS if_quelle,
+    beob.tpop_id AS if_tpop_id,
+    beob_from_evab.id AS evab_id,
+    beob_from_evab.quelle AS evab_quelle,
+    beob_from_evab.tpop_id AS evab_tpop_id
+  FROM
+    apflora.beob beob
+    INNER JOIN apflora.beob beob_from_evab ON beob_from_evab.id_evab_lc = beob.data ->> 'guid'
+  WHERE
+    beob.quelle <> 'EvAB 2016'
+    AND beob_from_evab.quelle = 'EvAB 2016'
+    --AND beob.id <> beob_from_evab.id
+  ORDER BY
+    beob_from_evab.id_evab_lc
+),
+todelete AS (
+  SELECT
+    evab_id
+  FROM
+    dat
+  WHERE
+    if_tpop_id IS NULL
+    AND evab_tpop_id IS NOT NULL)
+DELETE FROM apflora.beob
+WHERE id IN (
+    SELECT
+      *
+    FROM
+      todelete);
+
+-- 1878
