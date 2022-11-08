d81415644b64bfd3037c1fddb7b2659f5072c2ee
Alexander Gabriel <alex.barbalex@gmail.com>
2022-11-08 16:02:52 +0100

    up version

--- src/components/Head.js
+++ src/components/Head.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Head = () => (
   <>
-    <title>apflora v1.79.0</title>
+    <title>apflora v1.80.0</title>
     <meta name="description" content="Bedrohte Pflanzenarten fördern" />
     <meta name="keywords" content="Naturschutz, Artenschutz, Flora, Pflanzen" />
   </>
