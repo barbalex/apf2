c078b85b5ab930b327e7fd7d1eb08ce5fc781696
Alexander Gabriel <alex.barbalex@gmail.com>
2023-01-29 21:34:40 +0100

    add nodes

--- src/components/Projekte/TreeContainer/nodes/ap.js
+++ src/components/Projekte/TreeContainer/nodes/ap.js
@@ -1,36 +1,90 @@
 import { gql } from '@apollo/client'
 
+import popFolder from './popFolder'
+
 const ap = async ({ projId, store, treeQueryVariables }) => {
   const { data } = await store.client.query({
     query: gql`
-      query TreeApFolderQuery($apsFilter: ApFilter!) {
+      query TreeApsQuery($apsFilter: ApFilter!, $popsFilter: PopFilter!) {
         allAps(filter: $apsFilter, orderBy: LABEL_ASC) {
           nodes {
             id
             label
+            popsByApId(filter: $popsFilter) {
+              totalCount
+            }
           }
         }
       }
     `,
-    variables: { apsFilter: treeQueryVariables.apsFilter },
+    variables: {
+      apsFilter: treeQueryVariables.apsFilter,
+      popsFilter: treeQueryVariables.popsFilter,
+    },
   })
 
   const aps = data?.allAps?.nodes ?? []
 
   // map through all elements and create array of nodes
-  const nodes = aps.map((el) => ({
-    nodeType: 'table',
-    menuType: 'ap',
-    filterTable: 'ap',
-    id: el.id,
-    parentId: projId,
-    parentTableId: projId,
-    urlLabel: el.id,
-    label: el.label,
-    url: ['Projekte', projId, 'Arten', el.id],
-    hasChildren: true,
-  }))
+  let nodes = []
+  for (const ap of aps) {
+    if (store.tree.openAps.includes(ap.id)) {
+      // 1. fetch counts for children
+      const { data } = await store.client.query({
+        query: gql`
+          query TreeApQuery($id: UUID!, $popsFilter: PopFilter!) {
+            apById(id: $id) {
+              id
+              label
+              popsByApId(filter: $popsFilter) {
+                totalCount
+              }
+            }
+          }
+        `,
+        variables: {
+          id: ap.id,
+          popsFilter: treeQueryVariables.popsFilter,
+        },
+      })
+      // 2. build children
+      const popFolderNode = popFolder({
+        projId,
+        apId: ap.id,
+        store,
+        count: data?.apById?.popsByApId?.totalCount ?? 0,
+      })
 
+      nodes.push({
+        nodeType: 'table',
+        menuType: 'ap',
+        filterTable: 'ap',
+        id: ap.id,
+        parentId: projId,
+        parentTableId: projId,
+        urlLabel: ap.id,
+        label: ap.label,
+        url: ['Projekte', projId, 'Arten', ap.id],
+        hasChildren: true,
+        children: [popFolderNode],
+      })
+      continue
+    }
+    nodes.push({
+      nodeType: 'table',
+      menuType: 'ap',
+      filterTable: 'ap',
+      id: ap.id,
+      parentId: projId,
+      parentTableId: projId,
+      urlLabel: ap.id,
+      label: ap.label,
+      url: ['Projekte', projId, 'Arten', ap.id],
+      hasChildren: true,
+    })
+  }
+
+  // console.log('nodes, ap, nodes:', nodes)
   return nodes
 }
 
--- src/components/Projekte/TreeContainer/nodes/popFolder.js
+++ src/components/Projekte/TreeContainer/nodes/popFolder.js
@@ -1,52 +1,21 @@
-import findIndex from 'lodash/findIndex'
-
-const popFolderNode = ({
-  nodes: nodesPassed,
-  data,
-  loading,
-  projektNodes,
-  projId,
-  apNodes,
-  apId,
-  store,
-}) => {
-  const ap = (data?.openAps?.nodes ?? [])?.find((a) => a.id === apId)
-  const count = ap?.popsByApId?.totalCount ?? 0
-
-  // fetch sorting indexes of parents
-  const projIndex = findIndex(projektNodes, {
-    id: projId,
-  })
-  const apIndex = findIndex(apNodes, {
-    id: apId,
-  })
+const popFolderNode = ({ projId, apId, store, count }) => {
   const nodeLabelFilterString = store.tree?.nodeLabelFilter?.pop ?? ''
 
-  const message = loading
-    ? '...'
-    : nodeLabelFilterString
-    ? `${count} gefiltert`
-    : count
+  const message = nodeLabelFilterString ? `${count} gefiltert` : count
 
   const url = ['Projekte', projId, 'Arten', apId, 'Populationen']
 
-  // only show if parent node exists
-  if (!nodesPassed.map((n) => n.id).includes(apId)) return []
-
-  return [
-    {
-      nodeType: 'folder',
-      menuType: 'popFolder',
-      filterTable: 'pop',
-      id: `${apId}PopFolder`,
-      tableId: apId,
-      urlLabel: 'Populationen',
-      label: `Populationen (${message})`,
-      url,
-      sort: [projIndex, 1, apIndex, 1],
-      hasChildren: count > 0,
-    },
-  ]
+  return {
+    nodeType: 'folder',
+    menuType: 'popFolder',
+    filterTable: 'pop',
+    id: `${apId}PopFolder`,
+    tableId: apId,
+    urlLabel: 'Populationen',
+    label: `Populationen (${message})`,
+    url,
+    hasChildren: count > 0,
+  }
 }
 
 export default popFolderNode
