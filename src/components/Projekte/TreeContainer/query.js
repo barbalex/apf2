e8158af184cd7221056b0691107938e971f189a0
Alexander Gabriel <alex.barbalex@gmail.com>
2023-02-02 12:00:47 +0100

    add nodes

--- src/components/Projekte/TreeContainer/query.js
+++ src/components/Projekte/TreeContainer/query.js
@@ -2,23 +2,9 @@ import { gql } from '@apollo/client'
 
 export default gql`
   query TreeAllQuery(
-    $ekAbrechnungstypWertesFilter: EkAbrechnungstypWerteFilter!
     $tpopkontrzaehlEinheitWertesFilter: TpopkontrzaehlEinheitWerteFilter!
     $isWerteListen: Boolean!
   ) {
-    ekAbrechnungstypWertesUnfiltered: allEkAbrechnungstypWertes {
-      totalCount
-    }
-    allEkAbrechnungstypWertes(
-      filter: $ekAbrechnungstypWertesFilter
-      orderBy: SORT_ASC
-    ) @include(if: $isWerteListen) {
-      totalCount
-      nodes {
-        id
-        label
-      }
-    }
     tpopkontrzaehlEinheitWertesUnfiltered: allTpopkontrzaehlEinheitWertes {
       totalCount
     }
