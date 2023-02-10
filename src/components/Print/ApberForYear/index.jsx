c008918f360977b424aecb9981a92163f5a749cd
Alexander Gabriel <alex.barbalex@gmail.com>
2023-02-10 15:57:43 +0100

    optimize

--- src/components/Print/ApberForYear/index.jsx
+++ src/components/Print/ApberForYear/index.jsx
@@ -1,7 +1,6 @@
 import React, { useContext, useState, useEffect } from 'react'
 import { observer } from 'mobx-react-lite'
-import { useApolloClient } from '@apollo/client'
-import { gql } from '@apollo/client'
+import { useApolloClient, gql } from '@apollo/client'
 import { useParams } from 'react-router-dom'
 
 import ApberForYear from './ApberForYear'
