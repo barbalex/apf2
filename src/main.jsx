81782b88c5095ced9ed2350f858a8c2d36c1c92a
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2023-02-15 21:13:44 +0100

    register sw

--- src/main.jsx
+++ src/main.jsx
@@ -5,22 +5,7 @@ import { registerSW } from 'virtual:pwa-register'
 import App from './App'
 
 // https://vite-plugin-pwa.netlify.app/guide/prompt-for-update.html#runtime
-registerSW({
-  onNeedRefresh() {
-    if (
-      window.confirm(
-        'capturing.app neu laden, um die neuste Version zu installieren?',
-      )
-    ) {
-      // TODO: empty everything but the login?
-      // seems that is what is happening
-      window.location.reload(true)
-    }
-  },
-  onOfflineReady() {
-    console.log('the service worker is offline ready')
-  },
-})
+registerSW({ immediate: true })
 
 console.log('main rendering')
 
