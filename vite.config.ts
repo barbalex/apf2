c688b20e0dd7b72270edfad852e55917174914fb
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2023-02-16 21:42:51 +0100

    work on build

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,16 @@ import { splitVendorChunkPlugin } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true,
+    // rollupOptions: {
+    //   output: {
+    //     manualChunks: {
+    //       reactDatepicker: ['react-datepicker'],
+    //     },
+    //   },
+    // },
+  },
   // jsc: {
   //   experimental: {
   //     plugins: [
