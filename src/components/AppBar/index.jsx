6fdfb1dfb6896a1f9dbe8a875d27bb46add716e8
Alexander Gabriel <alex.barbalex@gmail.com>
2023-02-28 23:24:01 +0100

    clean

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy } from 'react'
+import React, { Suspense } from 'react'
 import styled from '@emotion/styled'
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
