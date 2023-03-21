665d87662608c5ac9f7ffa48ba4c6cac6ba30fb2
Alexander Gabriel <alex.barbalex@gmail.com>
2023-03-21 16:33:29 +0100

    sort labels case insensitive

--- sql/apflora/createComputedLabels.sql
+++ sql/apflora/createComputedLabels.sql
@@ -1,10 +1,14 @@
+-- https://github.com/graphile/postgraphile/issues/1087#issuecomment-1477958319
+-- need citext to guarantee case-insensitive sorting
+CREATE EXTENSION IF NOT EXISTS citext;
+
 -- see:
 -- https://www.graphile.org/postgraphile/computed-columns/
 -- https://github.com/graphile/postgraphile/issues/119#issuecomment-479445316
 DROP FUNCTION IF EXISTS apflora.adresse_label (adresse apflora.adresse);
 
-CREATE FUNCTION apflora.adresse_label (adresse apflora.adresse)
-  RETURNS text
+CREATE OR REPLACE FUNCTION apflora.adresse_label (adresse apflora.adresse)
+  RETURNS citext
   AS $$
   SELECT
     coalesce(adresse.name, '(kein Name)')
@@ -17,8 +21,8 @@ COMMENT ON FUNCTION apflora.adresse_label (apflora.adresse) IS e'@sortable';
 
 DROP FUNCTION IF EXISTS apflora.tpop_apberrelevant_grund_werte_label (tpop_apberrelevant_grund_werte apflora.tpop_apberrelevant_grund_werte);
 
-CREATE FUNCTION apflora.tpop_apberrelevant_grund_werte_label (tpop_apberrelevant_grund_werte apflora.tpop_apberrelevant_grund_werte)
-  RETURNS text
+CREATE OR REPLACE FUNCTION apflora.tpop_apberrelevant_grund_werte_label (tpop_apberrelevant_grund_werte apflora.tpop_apberrelevant_grund_werte)
+  RETURNS citext
   AS $$
   SELECT
     coalesce(tpop_apberrelevant_grund_werte.text, '(kein Name)')
@@ -31,8 +35,8 @@ COMMENT ON FUNCTION apflora.tpop_apberrelevant_grund_werte_label (apflora.tpop_a
 
 DROP FUNCTION IF EXISTS apflora.tpopkontrzaehl_einheit_werte_label (tpopkontrzaehl_einheit_werte apflora.tpopkontrzaehl_einheit_werte);
 
-CREATE FUNCTION apflora.tpopkontrzaehl_einheit_werte_label (tpopkontrzaehl_einheit_werte apflora.tpopkontrzaehl_einheit_werte)
-  RETURNS text
+CREATE OR REPLACE FUNCTION apflora.tpopkontrzaehl_einheit_werte_label (tpopkontrzaehl_einheit_werte apflora.tpopkontrzaehl_einheit_werte)
+  RETURNS citext
   AS $$
   SELECT
     coalesce(tpopkontrzaehl_einheit_werte.text, '(keine Einheit)')
@@ -45,8 +49,8 @@ COMMENT ON FUNCTION apflora.tpopkontrzaehl_einheit_werte_label (apflora.tpopkont
 
 DROP FUNCTION IF EXISTS apflora.ek_abrechnungstyp_werte_label (ek_abrechnungstyp_werte apflora.ek_abrechnungstyp_werte);
 
-CREATE FUNCTION apflora.ek_abrechnungstyp_werte_label (ek_abrechnungstyp_werte apflora.ek_abrechnungstyp_werte)
-  RETURNS text
+CREATE OR REPLACE FUNCTION apflora.ek_abrechnungstyp_werte_label (ek_abrechnungstyp_werte apflora.ek_abrechnungstyp_werte)
+  RETURNS citext
   AS $$
   SELECT
     coalesce(ek_abrechnungstyp_werte.text, '(keine Einheit)')
@@ -59,8 +63,8 @@ COMMENT ON FUNCTION apflora.ek_abrechnungstyp_werte_label (apflora.ek_abrechnung
 
 DROP FUNCTION IF EXISTS apflora.ap_label (ap apflora.ap);
 
-CREATE FUNCTION apflora.ap_label (ap apflora.ap)
-  RETURNS text
+CREATE OR REPLACE FUNCTION apflora.ap_label (ap apflora.ap)
+  RETURNS citext
   AS $$
   SELECT
     coalesce((
@@ -79,7 +83,7 @@ COMMENT ON FUNCTION apflora.ap_label (apflora.ap) IS e'@sortable';
 DROP FUNCTION IF EXISTS apflora.apart_label (apart apflora.apart);
 
 CREATE OR REPLACE FUNCTION apflora.apart_label (apart apflora.apart)
-  RETURNS text
+  RETURNS citext
   AS $$
   SELECT
     coalesce((
@@ -97,8 +101,8 @@ COMMENT ON FUNCTION apflora.apart_label (apflora.apart) IS e'@sortable';
 
 DROP FUNCTION IF EXISTS apflora.apber_label (apber apflora.apber);
 
-CREATE FUNCTION apflora.apber_label (apber apflora.apber)
-  RETURNS text
+CREATE OR REPLACE FUNCTION apflora.apber_label (apber apflora.apber)
+  RETURNS citext
   AS $$
   SELECT
     coalesce(lpad(apber.jahr::text, 4, '0'), '(kein Jahr)')
@@ -111,8 +115,8 @@ COMMENT ON FUNCTION apflora.apber_label (apflora.apber) IS e'@sortable';
 
 DROP FUNCTION IF EXISTS apflora.apberuebersicht_label (apberuebersicht apflora.apberuebersicht);
 
-CREATE FUNCTION apflora.apberuebersicht_label (apberuebersicht apflora.apberuebersicht)
-  RETURNS text
+CREATE OR REPLACE FUNCTION apflora.apberuebersicht_label (apberuebersicht apflora.apberuebersicht)
+  RETURNS citext
   AS $$
   SELECT
     coalesce(lpad(apberuebersicht.jahr::text, 4, '0'), '(kein Jahr)')
@@ -126,7 +130,7 @@ COMMENT ON FUNCTION apflora.apberuebersicht_label (apflora.apberuebersicht) IS e
 DROP FUNCTION IF EXISTS apflora.ekzaehleinheit_label (ekzaehleinheit apflora.ekzaehleinheit);
 
 CREATE OR REPLACE FUNCTION apflora.ekzaehleinheit_label (ekzaehleinheit apflora.ekzaehleinheit)
-  RETURNS text
+  RETURNS citext
   AS $$
   SELECT
     coalesce((
@@ -143,8 +147,8 @@ COMMENT ON FUNCTION apflora.ekzaehleinheit_label (apflora.ekzaehleinheit) IS e'@
 
 DROP FUNCTION IF EXISTS apflora.erfkrit_label (erfkrit apflora.erfkrit);
 
-CREATE FUNCTION apflora.erfkrit_label (erfkrit apflora.erfkrit)
-  RETURNS text
+CREATE OR REPLACE FUNCTION apflora.erfkrit_label (erfkrit apflora.erfkrit)
+  RETURNS citext
   AS $$
   SELECT
     coalesce((
@@ -162,8 +166,8 @@ COMMENT ON FUNCTION apflora.erfkrit_label (apflora.erfkrit) IS e'@sortable';
 
 DROP FUNCTION IF EXISTS apflora.ziel_label (ziel apflora.ziel);
 
-CREATE FUNCTION apflora.ziel_label (ziel apflora.ziel)
-  RETURNS text
+CREATE OR REPLACE FUNCTION apflora.ziel_label (ziel apflora.ziel)
+  RETURNS citext
   AS $$
   SELECT
     coalesce(ziel.bezeichnung, '(kein Ziel)') || ' (' || coalesce((
@@ -181,8 +185,8 @@ COMMENT ON FUNCTION apflora.ziel_label (apflora.ziel) IS e'@sortable';
 
 DROP FUNCTION IF EXISTS apflora.zielber_label (zielber apflora.zielber);
 
-CREATE FUNCTION apflora.zielber_label (zielber apflora.zielber)
-  RETURNS text
+CREATE OR REPLACE FUNCTION apflora.zielber_label (zielber apflora.zielber)
+  RETURNS citext
   AS $$
   SELECT
     coalesce(lpad(zielber.jahr::text, 4, '0'), '(kein Jahr)') || ': ' || coalesce(zielber.erreichung, '(nicht beurteilt)')
@@ -196,7 +200,7 @@ COMMENT ON FUNCTION apflora.zielber_label (apflora.zielber) IS e'@sortable';
 DROP FUNCTION IF EXISTS apflora.assozart_label (assozart apflora.assozart);
 
 CREATE OR REPLACE FUNCTION apflora.assozart_label (assozart apflora.assozart)
-  RETURNS text
+  RETURNS citext
   AS $$
   SELECT
     coalesce((
@@ -214,8 +218,8 @@ COMMENT ON FUNCTION apflora.assozart_label (apflora.assozart) IS e'@sortable';
 
 DROP FUNCTION IF EXISTS apflora.currentissue_label (currentissue apflora.currentissue);
 
-CREATE FUNCTION apflora.currentissue_label (currentissue apflora.currentissue)
-  RETURNS text
+CREATE OR REPLACE FUNCTION apflora.currentissue_label (currentissue apflora.currentissue)
+  RETURNS citext
   AS $$
   SELECT
     coalesce(currentissue.title, '(kein Titel)')
@@ -228,8 +232,8 @@ COMMENT ON FUNCTION apflora.currentissue_label (apflora.currentissue) IS e'@sort
 
 DROP FUNCTION IF EXISTS apflora.pop_label (pop apflora.pop);
 
-CREATE FUNCTION apflora.pop_label (pop apflora.pop)
-  RETURNS text
+CREATE OR REPLACE FUNCTION apflora.pop_label (pop apflora.pop)
+  RETURNS citext
   AS $$
   SELECT
     coalesce(pop.nr::text, '(keine Nr)') || ': ' || coalesce(pop.name, '(kein Name)')
@@ -242,8 +246,8 @@ COMMENT ON FUNCTION apflora.pop_label (apflora.pop) IS e'@sortable';
 
 DROP FUNCTION IF EXISTS apflora.popber_label (popber apflora.popber);
 
-CREATE FUNCTION apflora.popber_label (popber apflora.popber)
-  RETURNS text
+CREATE OR REPLACE FUNCTION apflora.popber_label (popber apflora.popber)
+  RETURNS citext
   AS $$
   SELECT
     coalesce(lpad(popber.jahr::text, 4, '0'), '(kein Jahr)') || ': ' || coalesce((
@@ -261,8 +265,8 @@ COMMENT ON FUNCTION apflora.popber_label (apflora.popber) IS e'@sortable';
 
 DROP FUNCTION IF EXISTS apflora.popmassnber_label (popmassnber apflora.popmassnber);
 
-CREATE FUNCTION apflora.popmassnber_label (popmassnber apflora.popmassnber)
-  RETURNS text
+CREATE OR REPLACE FUNCTION apflora.popmassnber_label (popmassnber apflora.popmassnber)
+  RETURNS citext
   AS $$
   SELECT
     coalesce(lpad(popmassnber.jahr::text, 4, '0'), '(kein Jahr)') || ': ' || coalesce((
@@ -280,8 +284,8 @@ COMMENT ON FUNCTION apflora.popmassnber_label (apflora.popmassnber) IS e'@sortab
 
 DROP FUNCTION IF EXISTS apflora.projekt_label (projekt apflora.projekt);
 
-CREATE FUNCTION apflora.projekt_label (projekt apflora.projekt)
-  RETURNS text
+CREATE OR REPLACE FUNCTION apflora.projekt_label (projekt apflora.projekt)
+  RETURNS citext
   AS $$
   SELECT
     coalesce(projekt.name, '(kein Name)')
@@ -294,8 +298,8 @@ COMMENT ON FUNCTION apflora.projekt_label (apflora.projekt) IS e'@sortable';
 
 DROP FUNCTION IF EXISTS apflora.tpop_label (tpop apflora.tpop);
 
-CREATE FUNCTION apflora.tpop_label (tpop apflora.tpop)
-  RETURNS text
+CREATE OR REPLACE FUNCTION apflora.tpop_label (tpop apflora.tpop)
+  RETURNS citext
   AS $$
   SELECT
     coalesce(tpop.nr::text, '(keine Nr)') || ': ' || coalesce(tpop.flurname, '(kein Flurname)')
@@ -308,8 +312,8 @@ COMMENT ON FUNCTION apflora.tpop_label (apflora.tpop) IS e'@sortable';
 
 DROP FUNCTION IF EXISTS apflora.tpop_ap_name (apflora.tpop);
 
-CREATE FUNCTION apflora.tpop_ap_name (tpop apflora.tpop)
-  RETURNS text
+CREATE OR REPLACE FUNCTION apflora.tpop_ap_name (tpop apflora.tpop)
+  RETURNS citext
   AS $$
   SELECT
     coalesce((
@@ -329,8 +333,8 @@ COMMENT ON FUNCTION apflora.tpop_ap_name (apflora.tpop) IS e'@sortable';
 
 DROP FUNCTION IF EXISTS apflora.tpopber_label (tpopber apflora.tpopber);
 
-CREATE FUNCTION apflora.tpopber_label (tpopber apflora.tpopber)
-  RETURNS text
+CREATE OR REPLACE FUNCTION apflora.tpopber_label (tpopber apflora.tpopber)
+  RETURNS citext
   AS $$
   SELECT
     coalesce(lpad(tpopber.jahr::text, 4, '0'), '(kein Jahr)') || ': ' || coalesce((
@@ -348,8 +352,8 @@ COMMENT ON FUNCTION apflora.tpopber_label (apflora.tpopber) IS e'@sortable';
 
 DROP FUNCTION IF EXISTS apflora.tpopmassnber_label (tpopmassnber apflora.tpopmassnber);
 
-CREATE FUNCTION apflora.tpopmassnber_label (tpopmassnber apflora.tpopmassnber)
-  RETURNS text
+CREATE OR REPLACE FUNCTION apflora.tpopmassnber_label (tpopmassnber apflora.tpopmassnber)
+  RETURNS citext
   AS $$
   SELECT
     coalesce(lpad(tpopmassnber.jahr::text, 4, '0'), '(kein Jahr)') || ': ' || coalesce((
@@ -367,8 +371,8 @@ COMMENT ON FUNCTION apflora.tpopmassnber_label (apflora.tpopmassnber) IS e'@sort
 
 DROP FUNCTION IF EXISTS apflora.tpopkontr_label_ek (tpopkontr apflora.tpopkontr);
 
-CREATE FUNCTION apflora.tpopkontr_label_ek (tpopkontr apflora.tpopkontr)
-  RETURNS text
+CREATE OR REPLACE FUNCTION apflora.tpopkontr_label_ek (tpopkontr apflora.tpopkontr)
+  RETURNS citext
   AS $$
   SELECT
     coalesce(lpad(tpopkontr.jahr::text, 4, '0'), '(kein Jahr)') || ': ' || coalesce(tpopkontr.typ, '(kein Typ)')
@@ -381,8 +385,8 @@ COMMENT ON FUNCTION apflora.tpopkontr_label_ek (apflora.tpopkontr) IS e'@sortabl
 
 DROP FUNCTION IF EXISTS apflora.tpopkontr_label_ekf (tpopkontr apflora.tpopkontr);
 
-CREATE FUNCTION apflora.tpopkontr_label_ekf (tpopkontr apflora.tpopkontr)
-  RETURNS text
+CREATE OR REPLACE FUNCTION apflora.tpopkontr_label_ekf (tpopkontr apflora.tpopkontr)
+  RETURNS citext
   AS $$
   SELECT
     coalesce(lpad(tpopkontr.jahr::text, 4, '0'), '(kein Jahr)')
@@ -395,8 +399,8 @@ COMMENT ON FUNCTION apflora.tpopkontr_label_ekf (apflora.tpopkontr) IS e'@sortab
 
 DROP FUNCTION IF EXISTS apflora.tpopkontr_ap_name (apflora.tpopkontr);
 
-CREATE FUNCTION apflora.tpopkontr_ap_name (tpopkontr apflora.tpopkontr)
-  RETURNS text
+CREATE OR REPLACE FUNCTION apflora.tpopkontr_ap_name (tpopkontr apflora.tpopkontr)
+  RETURNS citext
   AS $$
   SELECT
     coalesce((
@@ -417,7 +421,7 @@ COMMENT ON FUNCTION apflora.tpopkontr_ap_name (apflora.tpopkontr) IS e'@sortable
 
 DROP FUNCTION IF EXISTS apflora.tpopkontr_pop_nr (apflora.tpopkontr);
 
-CREATE FUNCTION apflora.tpopkontr_pop_nr (tpopkontr apflora.tpopkontr)
+CREATE OR REPLACE FUNCTION apflora.tpopkontr_pop_nr (tpopkontr apflora.tpopkontr)
   RETURNS integer
   AS $$
   SELECT
@@ -438,7 +442,7 @@ COMMENT ON FUNCTION apflora.tpopkontr_pop_nr (apflora.tpopkontr) IS e'@sortable'
 DROP FUNCTION IF EXISTS apflora.tpopkontrzaehl_label (tpopkontrzaehl apflora.tpopkontrzaehl);
 
 CREATE OR REPLACE FUNCTION apflora.tpopkontrzaehl_label (tpopkontrzaehl apflora.tpopkontrzaehl)
-  RETURNS text
+  RETURNS citext
   AS $$
   SELECT
     coalesce((
@@ -460,8 +464,8 @@ COMMENT ON FUNCTION apflora.tpopkontrzaehl_label (apflora.tpopkontrzaehl) IS e'@
 
 DROP FUNCTION IF EXISTS apflora.tpopmassn_label (tpopmassn apflora.tpopmassn);
 
-CREATE FUNCTION apflora.tpopmassn_label (tpopmassn apflora.tpopmassn)
-  RETURNS text
+CREATE OR REPLACE FUNCTION apflora.tpopmassn_label (tpopmassn apflora.tpopmassn)
+  RETURNS citext
   AS $$
   SELECT
     coalesce(lpad(tpopmassn.jahr::text, 4, '0'), '(kein Jahr)') || ': ' || coalesce((
@@ -479,8 +483,8 @@ COMMENT ON FUNCTION apflora.tpopmassn_label (apflora.tpopmassn) IS e'@sortable';
 
 DROP FUNCTION IF EXISTS apflora.tpopmassn_ap_name (apflora.tpopmassn);
 
-CREATE FUNCTION apflora.tpopmassn_ap_name (tpopmassn apflora.tpopmassn)
-  RETURNS text
+CREATE OR REPLACE FUNCTION apflora.tpopmassn_ap_name (tpopmassn apflora.tpopmassn)
+  RETURNS citext
   AS $$
   SELECT
     coalesce((
@@ -501,7 +505,7 @@ COMMENT ON FUNCTION apflora.tpopmassn_ap_name (apflora.tpopmassn) IS e'@sortable
 
 DROP FUNCTION IF EXISTS apflora.tpopmassn_pop_nr (apflora.tpopmassn);
 
-CREATE FUNCTION apflora.tpopmassn_pop_nr (tpopmassn apflora.tpopmassn)
+CREATE OR REPLACE FUNCTION apflora.tpopmassn_pop_nr (tpopmassn apflora.tpopmassn)
   RETURNS integer
   AS $$
   SELECT
@@ -521,8 +525,8 @@ COMMENT ON FUNCTION apflora.tpopmassn_pop_nr (apflora.tpopmassn) IS e'@sortable'
 
 DROP FUNCTION IF EXISTS apflora.user_label (u apflora.user);
 
-CREATE FUNCTION apflora.user_label (u apflora.user)
-  RETURNS text
+CREATE OR REPLACE FUNCTION apflora.user_label (u apflora.user)
+  RETURNS citext
   AS $$
   SELECT
     coalesce(u.name || ' (' || replace(u.role, 'apflora_', '') || ')', '(kein Name)')
@@ -536,7 +540,7 @@ COMMENT ON FUNCTION apflora.user_label (apflora.user) IS e'@sortable';
 DROP FUNCTION IF EXISTS apflora.beob_label (beob apflora.beob);
 
 CREATE OR REPLACE FUNCTION apflora.beob_label (beob apflora.beob)
-  RETURNS text
+  RETURNS citext
   AS $$
   SELECT
     to_char(beob.datum, 'YYYY.MM.DD') || ': ' || coalesce(beob.autor, '(kein Autor)') || ' (' || beob.quelle || ')'
