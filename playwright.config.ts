d8b21254aba442ab76c5626a342cdfea6373f9b5
Alexander Gabriel <alex.barbalex@gmail.com>
2023-03-24 20:44:36 +0100

    test

--- playwright.config.ts
+++ playwright.config.ts
@@ -40,46 +40,32 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
+    { name: 'setup', testMatch: /.*\.setup\.ts/ },
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: {
+        ...devices['Desktop Chrome'],
+        storageState: './playwright/.auth/user.json',
+      },
+      dependencies: ['setup'],
     },
 
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      use: {
+        ...devices['Desktop Firefox'],
+        storageState: './playwright/.auth/user.json',
+      },
+      dependencies: ['setup'],
     },
 
     {
       name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      use: {
+        ...devices['Desktop Safari'],
+        storageState: './playwright/.auth/user.json',
+      },
+      dependencies: ['setup'],
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ..devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 })
--- tests/all.spec.ts
+++ tests/all.spec.ts
@@ -15,8 +15,13 @@ test('Docs link', async ({ page }) => {
 })
 
 test('Daten link', async ({ page }) => {
-  await page.goto(`/`)
-  await page.getByRole('link', { name: 'Arten bearbeiten' }).click()
-  await expect(page).toHaveURL(/Daten/)
-  await expect(page.getByLabel('Name')).toBeFocused()
+  await page.goto(`/Daten/Projekte/e57f56f4-4376-11e8-ab21-4314b6749d13`)
+  await expect(page.getByLabel('Name')).toBeInViewport()
+})
+
+test('Navigate to Mitteilungen', async ({ page }) => {
+  await page.goto(`/Daten/Projekte/e57f56f4-4376-11e8-ab21-4314b6749d13`)
+  await page.getByText('Aktuelle Fehler').click()
+  await page.getByText('Fehlt hier was?').click()
+  await expect(page.getByText('Aktueller Fehler')).toBeInViewport()
 })
