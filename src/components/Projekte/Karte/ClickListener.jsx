95786e2a2c9ff26f7830de6bed41f91e6ca3709e
Alexander Gabriel <alex.barbalex@gmail.com>
2023-04-08 14:13:11 +0200

    plan

--- src/components/Projekte/Karte/ClickListener.jsx
+++ src/components/Projekte/Karte/ClickListener.jsx
@@ -10,18 +10,24 @@ const ClickListener = () => {
   // idea 1:
   // get all layers
   // run onEachFeature on all layers
+  // not possible because onEachFeature is not called when layer is added
+
   // idea 2:
   // get all layers
   // fetch all layers features using turf.inside
   // https://gis.stackexchange.com/a/277207/13491
   // turf.intersect(myPt.toGeoJSON(), myPoly.toGeoJSON()); if (intersection != undefined){ // must be inside }
+  // possible but not efficient
+
   // idea 3:
   // use a FeatureGroup https://leafletjs.com/reference.html#featuregroup
+  // does not seem to work
+
   // idea 4:
   // get all active layers
   // filter queryable ones
   // directly query them
-  // and build popup from responses
+  // build popup from responses
 
   return null
 }
