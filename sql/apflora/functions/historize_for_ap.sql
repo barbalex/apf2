36ff3c0687d9a9b33242eb196c591834033b235f
Alexander Gabriel <alex.barbalex@gmail.com>
2023-04-12 22:22:56 +0200

    add function

---
+++ sql/apflora/functions/historize_for_ap.sql
@@ -0,0 +1,125 @@
+CREATE OR REPLACE FUNCTION apflora.historize_for_ap(year integer, ap_id uuid)
+  RETURNS boolean
+  AS $$
+BEGIN
+  -- empty history tables (rows of passed year)
+  DELETE FROM apflora.tpop_history
+  WHERE apflora.tpop_history.year = $1
+    AND pop_id IN(
+      SELECT
+        id
+      FROM
+        apflora.pop_history
+      WHERE
+        apflora.pop_history.year = $1
+        AND apflora.pop_history.ap_id = $2);
+  DELETE FROM apflora.pop_history
+  WHERE apflora.pop_history.year = $1
+    AND apflora.pop_history.ap_id = $2;
+  DELETE FROM apflora.ap_history
+  WHERE apflora.ap_history.year = $1
+    AND apflora.ap_history.id = $2;
+  --
+  -- insert ap_history
+  INSERT INTO apflora.ap_history(year, id, art_id, proj_id, bearbeitung, start_jahr, umsetzung, bearbeiter, ekf_beobachtungszeitpunkt, created_at, updated_at, changed_by)
+  SELECT
+    $1 AS year,
+    id,
+    art_id,
+    proj_id,
+    bearbeitung,
+    start_jahr,
+    umsetzung,
+    bearbeiter,
+    ekf_beobachtungszeitpunkt,
+    created_at,
+    updated_at,
+    changed_by
+  FROM
+    apflora.ap
+  WHERE
+    apflora.ap.id = $2;
+  --
+  -- insert pop_history
+  INSERT INTO apflora.pop_history(year, id, ap_id, nr, name, status, status_unklar, status_unklar_begruendung, bekannt_seit, geom_point, created_at, updated_at, changed_by)
+  SELECT
+    $1 AS year,
+    id,
+    apflora.pop.ap_id,
+    nr,
+    name,
+    status,
+    status_unklar,
+    status_unklar_begruendung,
+    bekannt_seit,
+    geom_point,
+    created_at,
+    updated_at,
+    changed_by
+  FROM
+    apflora.pop
+  WHERE
+    apflora.pop.ap_id = $2;
+  --
+  -- insert tpop_history
+  INSERT INTO apflora.tpop_history(year, id, pop_id, nr, gemeinde, flurname, geom_point, radius, hoehe, exposition, klima, neigung, boden_typ, boden_kalkgehalt, boden_durchlaessigkeit, boden_humus, boden_naehrstoffgehalt, boden_abtrag, wasserhaushalt, beschreibung, kataster_nr, status, status_unklar, status_unklar_grund, apber_relevant, apber_relevant_grund, bekannt_seit, eigentuemer, kontakt, nutzungszone, bewirtschafter, bewirtschaftung, ekfrequenz, ekfrequenz_startjahr, ekfrequenz_abweichend, ekf_kontrolleur, bemerkungen, created_at, updated_at, changed_by)
+  SELECT
+    $1 AS year,
+    id,
+    pop_id,
+    nr,
+    gemeinde,
+    flurname,
+    geom_point,
+    radius,
+    hoehe,
+    exposition,
+    klima,
+    neigung,
+    boden_typ,
+    boden_kalkgehalt,
+    boden_durchlaessigkeit,
+    boden_humus,
+    boden_naehrstoffgehalt,
+    boden_abtrag,
+    wasserhaushalt,
+    beschreibung,
+    kataster_nr,
+    status,
+    status_unklar,
+    status_unklar_grund,
+    apber_relevant,
+    apber_relevant_grund,
+    bekannt_seit,
+    eigentuemer,
+    kontakt,
+    nutzungszone,
+    bewirtschafter,
+    bewirtschaftung,
+    ekfrequenz,
+    ekfrequenz_startjahr,
+    ekfrequenz_abweichend,
+    ekf_kontrolleur,
+    bemerkungen,
+    created_at,
+    updated_at,
+    changed_by
+  FROM
+    apflora.tpop
+  WHERE
+    pop_id IN(
+      SELECT
+        id
+      FROM
+        apflora.pop_history
+      WHERE
+        apflora.pop_history.year = $1
+        AND apflora.pop_history.ap_id = $2);
+  RETURN FOUND;
+END;
+$$
+LANGUAGE plpgsql
+SECURITY DEFINER;
+
+ALTER FUNCTION apflora.historize_for_ap(year integer, ap_id uuid) OWNER TO postgres;
+
