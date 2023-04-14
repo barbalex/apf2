ddd868e24e55d0de5f2ece0851fd2673378ee27a
Alexander Gabriel <alex.barbalex@gmail.com>
2023-04-14 11:01:59 +0200

    document

--- src/components/Docs/docs/BeobVerwalten/index.jsx
+++ src/components/Docs/docs/BeobVerwalten/index.jsx
@@ -1,22 +1,22 @@
-import { DokuDate } from '../..'
-import beobTable from './beobTable.png'
+import { DokuDate, Code } from '../..'
+import beobTable from './beobTable.webp'
 
 const BeobVerwalten = () => (
   <>
     <h1>Beobachtungen verwalten</h1>
-    <DokuDate>27.12.2021</DokuDate>
+    <DokuDate>14.04.2023</DokuDate>
     <h2>1. Datenstruktur</h2>
     <p>
-      Beobachtungen werden in der Tabelle <code>beob</code> gespeichert:
+      Beobachtungen werden in der Tabelle <Code>beob</Code> gespeichert:
       <br />
-      <img src={beobTable} alt="Tabelle beob" />
+      <img src={beobTable} alt="Tabelle beob" width="280" height="575" />
       <br />
     </p>
     <p>Ein Datensatz besteht aus jeweils zwei Teilen:</p>
     <ol>
       <li>
         Den unveränderten Originaldaten der Beobachtung, enthalten im JSONB-Feld{' '}
-        <code>data</code>
+        <Code>data</Code>
       </li>
       <li>
         In den übrigen Feldern: Extrahierten bzw. abgeleiteteten Daten, welche
@@ -33,7 +33,7 @@ const BeobVerwalten = () => (
         Die Struktur von Beobachtungsdaten ist im Prinzip unerheblich.
         Änderungen daran auch. Wichtig ist einzig, dass zum Zeitpunkt des
         Imports klar ist, wie aus den Beobachtungsdaten die abgeleiteten Felder
-        in Tabelle <code>beob</code> generiert werden können
+        in Tabelle <Code>beob</Code> generiert werden können
       </li>
       <li>
         Somit können jederzeit Beobachtungen unabhängig von ihrer Datenstruktur
@@ -41,8 +41,8 @@ const BeobVerwalten = () => (
       </li>
       <li>
         Schon vorhandene Beobachtungen können bei erneutem Import mit
-        aktuelleren ersetzt werden (<code>quelle</code> und{' '}
-        <code>id_field</code> vergleichen)
+        aktuelleren ersetzt werden (<Code>quelle</Code> und{' '}
+        <Code>id_field</Code> vergleichen)
       </li>
       <li>
         Mit Hilfe der abgeleiteten Felder können gebaut werden:
@@ -54,63 +54,86 @@ const BeobVerwalten = () => (
         </ul>
       </li>
     </ul>
-    <p>Struktur der Tabelle &quot;beob&quot;:</p>
+    <p>Felder der Tabelle &quot;beob&quot;:</p>
     <ul>
       <li>
-        id: id dieser Tabelle. Ohne Bezug zu id&#39;s in den Beobachtungsdaten
-      </li>
-      <li>data: Unveränderte Originaldaten im JSONB Format</li>
-      <li>
-        id_field: Feld in den Originaldaten, welches die Original-ID enthält.
-        Dient dazu, gemeinsam mit dem Feld <code>quelle</code> jederzeit mit
-        neuen Versionen von Originaldaten verbinden zu können
+        <Code>id</Code>: id dieser Tabelle. Ohne Bezug zu id&#39;s in den
+        Beobachtungsdaten
       </li>
       <li>
-        obs_id: Wie id_field. Weil ab 2022 Importe sowieso nur von Info Flora
-        stammen sollen, dies ihre ID ist und komplexe Abfragen beim Import mit
-        Hilfe dieses Feldes vereinfacht und beschleunigt werden
+        <Code>data</Code>: Unveränderte Originaldaten im JSONB Format
       </li>
       <li>
-        quelle: Woher die Beobachtung stammt. Möglichst kurz und klar, ähnlich
-        Literaturzitaten. Beispiel: Info Spezies 2017
+        <Code>id_field</Code>: Feld in den Originaldaten, welches die
+        Original-ID enthält. Dient dazu, gemeinsam mit dem Feld{' '}
+        <Code>quelle</Code> jederzeit mit neuen Versionen von Originaldaten
+        verbinden zu können
       </li>
       <li>
-        art_id: beschreibt die Art. Fremdschlüssel aus Tabelle{' '}
-        <code>ae_taxonomies</code>
+        <Code>obs_id</Code>: Wie id_field. Weil ab 2022 Importe sowieso nur von
+        Info Flora stammen sollen, dies ihre ID ist und komplexe Abfragen beim
+        Import mit Hilfe dieses Feldes vereinfacht und beschleunigt werden
       </li>
       <li>
-        art_id_original: Am Unterschied zwischen art_id_original und art_id wird
-        erkenntlich, wenn die Art-Bestimmung verändert wurde
+        <Code>quelle</Code>
+        {`: Woher die Beobachtung stammt. Möglichst kurz und klar, ähnlich
+        Literaturzitaten. Beispiel: "Info Spezies 2017"`}
       </li>
-      <li>autor: Autor der Beobachtung</li>
-      <li>datum: Datum der Beobachtung</li>
-      <li>geom_point: Die Geometrie der Beobachtung</li>
-      <li>tpop_id: dieser Teilpopulation wird die Beobachtung zugeordnet</li>
       <li>
-        nicht_zuordnen: Ja oder nein. Wird ja gesetzt, wenn eine Beobachtung
+        <Code>art_id</Code>: beschreibt die Art. Fremdschlüssel aus Tabelle{' '}
+        <Code>ae_taxonomies</Code>
+      </li>
+      <li>
+        <Code>art_id_original</Code>: Am Unterschied zwischen art_id_original
+        (unverändert) und art_id (verändert) wird erkenntlich, wenn die
+        Zuordnung des Taxon verändert wurde
+      </li>
+      <li>
+        <Code>autor</Code>: Autor der Beobachtung
+      </li>
+      <li>
+        <Code>datum</Code>: Datum der Beobachtung
+      </li>
+      <li>
+        <Code>geom_point</Code>: Die Geometrie der Beobachtung
+      </li>
+      <li>
+        <Code>tpop_id</Code>: dieser Teilpopulation wird die Beobachtung
+        zugeordnet
+      </li>
+      <li>
+        <Code>nicht_zuordnen</Code>
+        {`: "Ja" oder "nein". Wird "ja" gesetzt, wenn eine Beobachtung
         keiner Teilpopulation zugeordnet werden kann. Sollte im Bemerkungsfeld
         begründet werden. In der Regel ist die Artbestimmung zweifelhaft. Oder
-        die Beobachtung ist nicht (genau genug) lokalisierbar
-      </li>
-      <li>bemerkungen: Bemerkungen zur Zuordnung</li>
-      <li>
-        infoflora_informiert_datum: Wann ein Email an Info Flora generiert wurde
+        die Beobachtung ist nicht (genau genug) lokalisierbar`}
       </li>
       <li>
-        created_at, update_at, changed_by: Dokumentiert die letzte Änderung am
-        Datensatz
+        <Code>bemerkungen</Code>: Bemerkungen zur Zuordnung
+      </li>
+      <li>
+        <Code>infoflora_informiert_datum</Code>: Wann ein Email an Info Flora
+        generiert wurde
+      </li>
+      <li>
+        <Code>created_at</Code>, <Code>update_at</Code>, <Code>changed_by</Code>
+        : Dokumentiert die letzte Änderung am Datensatz
+      </li>
+      <li>
+        {`id_evab, id_evab_lc: Relikte aus der Zeit, als Beobachtungen aus EvAB
+        importiert werden mussten. Die lc-Variante steht für lower case und wurde generiert, damit eine GUID wirklich immer gleich ist. Wieso? Weil aus EvAB GUID's manchmal upper-case und manchmal lower-case ankamen, womit dieselbe GUID nicht immer gleich war...`}
       </li>
     </ul>
     <h2>2. Beobachtungen von Info Spezies importieren</h2>
     <p>
       Ist in der jeweils aktuellsten Abfrage dokumentiert. Beispiel:{' '}
       <a
-        href="https://github.com/barbalex/apf2/blob/master/sql/apflora/import_info_flora/2022-03-30_import_info_flora.sql"
+        href="https://github.com/barbalex/apf2/blob/master/sql/apflora/import_info_flora/2023-02-10.sql"
         target="_blank"
         className="url"
         rel="noreferrer"
       >
-        https://github.com/barbalex/apf2/blob/master/sql/apflora/import_info_flora/2022-03-30_import_info_flora.sql
+        https://github.com/barbalex/apf2/blob/master/sql/apflora/import_info_flora/2023-02-10.sql
       </a>
     </p>
   </>
