544b9803676d6a7600bfe2fbb6d1089852669aa6
Alexander Gabriel <alex.barbalex@gmail.com>
2023-07-14 16:13:07 +0200

    use modularized react-leaflet-bing

---
+++ src/components/shared/react-leaflet-bing-v2/Bing.jsx
@@ -0,0 +1,11 @@
+import { createLayerComponent } from '@react-leaflet/core'
+// import {bingLayer} from './leaflet.bing';
+import './leaflet.bing'
+
+const createLeafletElement = (props) => {
+  const instance = L.bingLayer(props.bingkey, props)
+
+  return { instance }
+}
+
+export default createLayerComponent(createLeafletElement)
