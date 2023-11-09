3fdb18580ba7e3dfd08a9a0b35088f39a50bf74a
Alexander Gabriel <alex.barbalex@gmail.com>
2023-11-09 09:50:09 +0100

    up version

--- src/components/AppBar/Bar/Projekte/More/index.jsx
+++ src/components/AppBar/Bar/Projekte/More/index.jsx
@@ -114,7 +114,7 @@ const AppbarMore = ({ onClickExporte: passedOnClickExporte, role }) => {
         <MenuItem onClick={onClickUptime}>
           Verfügbarkeit der Server von apflora.ch
         </MenuItem>
-        <Version>Version: 1.103.5 vom 5.11.2023</Version>
+        <Version>Version: 1.103.6 vom 9.11.2023</Version>
       </Menu>
     </Container>
   )
