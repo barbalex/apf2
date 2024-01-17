09cf4eadf872ad6931e932bdf3aa41298d609922
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-17 12:33:46 +0100

    solve 662

--- src/components/Projekte/TreeContainer/Tree/IntoViewScroller.jsx
+++ src/components/Projekte/TreeContainer/Tree/IntoViewScroller.jsx
@@ -3,7 +3,6 @@ import { observer } from 'mobx-react-lite'
 
 import storeContext from '../../../../storeContext'
 import isElementInViewport from '../../../../modules/isElementInViewport'
-import getLastIdFromUrl from '../../../../modules/getLastIdFromUrl'
 
 const IntoViewScroller = () => {
   const store = useContext(storeContext)
@@ -15,40 +14,25 @@ const IntoViewScroller = () => {
   const urlToFocus = lastTouchedNode.length ? lastTouchedNode : activeNodeArray
 
   // 2024.01.17
-  // Problem: this is not good enough
-  // it works for nodes with id's that correspond with dataset ids
-  // it does not work for folders
-  // solution 1: folders must be given ids in the form: `${dataset-id}/${folder name in url}`
-  // better solution: every node gets its url in data-url. Then get element by: document.querySelector('[data-url="url"]')
+  // Problem: this was not good enough
+  // it worked for nodes with id's that correspond with dataset ids
+  // it did not work for folders
+  // better solution: every node gets its url in data-url. Then get element by that
   const scroller = useCallback(() => {
-    // console.log('IntoViewScroller running')
-    // 1. Get id from url
-    const id = getLastIdFromUrl(urlToFocus)
-    if (!id) return setTimeout(scroller, 150)
-    // 2. Get its element
-    const element = document.getElementById(id)
-    // 3. No element yet? Tree may still be loading > try later
+    // 1. Get element with the url to focus
+    const selector = `[data-url='${JSON.stringify(urlToFocus)}']`
+    const element = document.querySelector(selector)
+    // const element = document.getElementById(id)
+    // 2. No element yet? Tree may still be loading > try later
     if (!element) return setTimeout(scroller, 150)
-    console.log('IntoViewScroller, scroller', {
-      id,
-      element,
-      isElementInViewport: isElementInViewport(element),
-    })
-    // 4. Got an element but it is visible? do not scroll
+    // 3. If the element is visible, do not scroll
     if (isElementInViewport(element)) return
-    // console.log('IntoViewScroller, will scroll id into view:', id)
-    // 5. Got an element and it is not visible? scroll it into view
+    // 4. The element is not visible? scroll it into view
     element?.scrollIntoView?.({
       block: 'center',
       inline: 'center',
     })
   }, [urlToFocus])
-
-  console.log('IntoViewScroller', {
-    urlToFocus,
-    lastTouchedNode,
-    activeNodeArray,
-  })
 
   useEffect(() => {
     scroller()
