17c2d3579277bedb2fa2010ff17c8949aca2eddd
Alexander Gabriel <alex.barbalex@gmail.com>
2024-05-14 14:23:39 +0200

    style

--- src/components/Projekte/TreeContainer/Tree/index.jsx
+++ src/components/Projekte/TreeContainer/Tree/index.jsx
@@ -1,14 +1,16 @@
 import styled from '@emotion/styled'
 
-import Root from './Root'
-import IntoViewScroller from './IntoViewScroller'
-import Menu from './Menu'
+import Root from './Root/index.jsx'
+import IntoViewScroller from './IntoViewScroller.jsx'
+import Menu from './Menu/index.jsx'
 
 const Container = styled.div`
   height: calc(100% - 53px - 8px);
   width: 100%;
   overflow: auto;
+  scrollbar-width: thin;
   position: relative;
+  contain: paint layout style;
   ul {
     margin: 0;
     list-style: none;
