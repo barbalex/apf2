0881c36fca55d6e6c6ce5464f87d81ed72aa030a
Alexander Gabriel <alex.barbalex@gmail.com>
2024-05-14 15:33:32 +0200

    import using complete path

--- src/components/shared/Date.jsx
+++ src/components/shared/Date.jsx
@@ -35,7 +35,9 @@ const StyledDatePicker = styled(DatePicker)`
   color: #495057;
   background-color: #fff;
   background-clip: padding-box;
-  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+  transition:
+    border-color 0.15s ease-in-out,
+    box-shadow 0.15s ease-in-out;
   border-bottom: 1px solid rgba(0, 0, 0, 0.1);
   border-top: none;
   border-left: none;
