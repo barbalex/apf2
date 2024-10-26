909707062263d0659331e84621ff93d8c064e114
Alexander Gabriel <alex.barbalex@gmail.com>
2024-10-26 22:35:17 +0200

    use named export

--- src/components/EkPlan/Table/CellForEkfrequenz.jsx
+++ src/components/EkPlan/Table/CellForEkfrequenz.jsx
@@ -28,168 +28,160 @@ const Option = styled.option`
   font-size: 0.85rem;
 `
 
-const CellForEkfrequenz = ({ row, field, style, refetchTpop, ekfrequenzs }) => {
-  const client = useApolloClient()
-  const store = useContext(StoreContext)
-  const { enqueNotification } = store
-  const { hovered } = store.ekPlan
-  const className = hovered.tpopId === row.id ? 'tpop-hovered' : ''
+export const CellForEkfrequenz = observer(
+  ({ row, field, style, refetchTpop, ekfrequenzs }) => {
+    const client = useApolloClient()
+    const store = useContext(StoreContext)
+    const { enqueNotification } = store
+    const { hovered } = store.ekPlan
+    const className = hovered.tpopId === row.id ? 'tpop-hovered' : ''
 
-  const ekfOptionsGroupedPerAp = useMemo(() => {
-    const longestAnwendungsfall = max(
-      ekfrequenzs.map((a) => (a.anwendungsfall || '').length),
+    const ekfOptionsGroupedPerAp = useMemo(() => {
+      const longestAnwendungsfall = max(
+        ekfrequenzs.map((a) => (a.anwendungsfall || '').length),
+      )
+      const options = ekfrequenzs.map((o) => {
+        const code = (o.code || '').padEnd(9, '\xA0')
+        const anwendungsfall =
+          `${(o.anwendungsfall || '').padEnd(longestAnwendungsfall, '\xA0')}` ||
+          ''
+        return {
+          value: o.id,
+          label: `${code}: ${anwendungsfall}`,
+          anwendungsfall,
+          apId: o.apId,
+        }
+      })
+      const os = groupBy(options, 'apId')
+      return os
+    }, [ekfrequenzs])
+
+    const [focused, setFocused] = useState(false)
+
+    const onMouseEnter = useCallback(
+      () => hovered.setTpopId(row.id),
+      [hovered, row.id],
     )
-    const options = ekfrequenzs.map((o) => {
-      const code = (o.code || '').padEnd(9, '\xA0')
-      const anwendungsfall =
-        `${(o.anwendungsfall || '').padEnd(longestAnwendungsfall, '\xA0')}` ||
-        ''
-      return {
-        value: o.id,
-        label: `${code}: ${anwendungsfall}`,
-        anwendungsfall,
-        apId: o.apId,
-      }
-    })
-    const os = groupBy(options, 'apId')
-    return os
-  }, [ekfrequenzs])
-
-  const [focused, setFocused] = useState(false)
-
-  const onMouseEnter = useCallback(
-    () => hovered.setTpopId(row.id),
-    [hovered, row.id],
-  )
-  const onChange = useCallback(
-    async (e) => {
-      const value = e.target.value || null
-      try {
-        await client.mutate({
-          mutation: gql`
-            mutation updateTpopEkfrequenz(
-              $id: UUID!
-              $ekfrequenz: UUID
-              $changedBy: String
-            ) {
-              updateTpopById(
-                input: {
-                  id: $id
-                  tpopPatch: {
+    const onChange = useCallback(
+      async (e) => {
+        const value = e.target.value || null
+        try {
+          await client.mutate({
+            mutation: gql`
+              mutation updateTpopEkfrequenz(
+                $id: UUID!
+                $ekfrequenz: UUID
+                $changedBy: String
+              ) {
+                updateTpopById(
+                  input: {
                     id: $id
-                    ekfrequenz: $ekfrequenz
-                    changedBy: $changedBy
+                    tpopPatch: {
+                      id: $id
+                      ekfrequenz: $ekfrequenz
+                      changedBy: $changedBy
+                    }
+                  }
+                ) {
+                  tpop {
+                    ...TpopFields
                   }
                 }
-              ) {
-                tpop {
-                  ...TpopFields
-                }
               }
-            }
-            ${tpop}
-          `,
-          variables: {
-            id: row.id,
-            ekfrequenz: value,
-            changedBy: store.user.name,
-          },
-          refetchQueries: ['EkplanTpopQuery'],
-        })
-      } catch (error) {
-        enqueNotification({
-          message: error.message,
-          options: {
-            variant: 'error',
-          },
-        })
-      }
-      // set EK-Frequenz Startjahr
-      let ekfrequenzStartjahr
-      if (value) {
-        ekfrequenzStartjahr = await setStartjahr({
-          row,
-          ekfrequenz: value,
-          client,
-          store,
-        })
-      }
-      // set ekplans if startjahr exists
-      // TODO: or ekfrequenz has no kontrolljahre
-      if (!!ekfrequenzStartjahr && !!value) {
-        setEkplans({
-          tpopId: row.id,
-          ekfrequenz: value,
-          ekfrequenzStartjahr,
-          refetchTpop,
-          client,
-          store,
-        })
-      }
-    },
-    [row, client, store, enqueNotification, refetchTpop],
-  )
-  const onFocus = useCallback(() => {
-    setFocused(true)
-  }, [])
-  const onBlur = useCallback(() => {
-    setFocused(false)
-  }, [])
-  const optionsGrouped = ekfOptionsGroupedPerAp[row.apId]
-  const ekfrequenz = ekfrequenzs.find((f) => f.id === field.value)
-  const valueToShow = ekfrequenz ? ekfrequenz.code : ''
-
-  /*focused &&
-    console.log('CellForEkfrequenz', {
-      focused,
-      optionsGrouped,
-      valueToShow,
-      ekfOptionsGroupedPerAp,
-    })*/
-
-  return (
-    <StyledCellForSelect
-      style={style}
-      onMouseEnter={onMouseEnter}
-      onMouseLeave={hovered.reset}
-      className={className}
-      data-isodd={row.isOdd}
-    >
-      <Select
-        value={valueToShow}
-        onChange={onChange}
-        onFocus={onFocus}
-        onBlur={onBlur}
-      >
-        {focused ?
-          optionsGrouped ?
-            <>
-              <Option
-                key="option1"
-                value={null}
-              >
-                {''}
-              </Option>
-              {optionsGrouped.map((o) => (
-                <Option
-                  key={o.value}
-                  value={o.value}
-                >
-                  {o.label}
-                </Option>
-              ))}
-            </>
-          : null
-        : <Option
-            key="option1"
-            value={field.value}
-          >
-            {valueToShow}
-          </Option>
+              ${tpop}
+            `,
+            variables: {
+              id: row.id,
+              ekfrequenz: value,
+              changedBy: store.user.name,
+            },
+            refetchQueries: ['EkplanTpopQuery'],
+          })
+        } catch (error) {
+          enqueNotification({
+            message: error.message,
+            options: {
+              variant: 'error',
+            },
+          })
         }
-      </Select>
-    </StyledCellForSelect>
-  )
-}
+        // set EK-Frequenz Startjahr
+        let ekfrequenzStartjahr
+        if (value) {
+          ekfrequenzStartjahr = await setStartjahr({
+            row,
+            ekfrequenz: value,
+            client,
+            store,
+          })
+        }
+        // set ekplans if startjahr exists
+        // TODO: or ekfrequenz has no kontrolljahre
+        if (!!ekfrequenzStartjahr && !!value) {
+          setEkplans({
+            tpopId: row.id,
+            ekfrequenz: value,
+            ekfrequenzStartjahr,
+            refetchTpop,
+            client,
+            store,
+          })
+        }
+      },
+      [row, client, store, enqueNotification, refetchTpop],
+    )
+    const onFocus = useCallback(() => {
+      setFocused(true)
+    }, [])
+    const onBlur = useCallback(() => {
+      setFocused(false)
+    }, [])
+    const optionsGrouped = ekfOptionsGroupedPerAp[row.apId]
+    const ekfrequenz = ekfrequenzs.find((f) => f.id === field.value)
+    const valueToShow = ekfrequenz ? ekfrequenz.code : ''
 
-export default observer(CellForEkfrequenz)
+    return (
+      <StyledCellForSelect
+        style={style}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={hovered.reset}
+        className={className}
+        data-isodd={row.isOdd}
+      >
+        <Select
+          value={valueToShow}
+          onChange={onChange}
+          onFocus={onFocus}
+          onBlur={onBlur}
+        >
+          {focused ?
+            optionsGrouped ?
+              <>
+                <Option
+                  key="option1"
+                  value={null}
+                >
+                  {''}
+                </Option>
+                {optionsGrouped.map((o) => (
+                  <Option
+                    key={o.value}
+                    value={o.value}
+                  >
+                    {o.label}
+                  </Option>
+                ))}
+              </>
+            : null
+          : <Option
+              key="option1"
+              value={field.value}
+            >
+              {valueToShow}
+            </Option>
+          }
+        </Select>
+      </StyledCellForSelect>
+    )
+  },
+)
