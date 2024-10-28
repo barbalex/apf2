01f9350b62e1a861f40b1a9ed4e95dc023459150
Alexander Gabriel <alex.barbalex@gmail.com>
2024-10-28 20:15:02 +0100

    correct

--- src/components/EkPlan/Table/CellHeaderFixed/BooleanFilter.jsx
+++ src/components/EkPlan/Table/CellHeaderFixed/BooleanFilter.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useCallback, useContext } from 'react'
+import { useCallback, useContext, forwardRef } from 'react'
 import MenuItem from '@mui/material/MenuItem'
 import upperFirst from 'lodash/upperFirst'
 
@@ -8,29 +8,30 @@ import { StoreContext } from '../../../../storeContext.js'
 // need to forward ref from Menu to MenuItem
 // see: https://github.com/mui-org/material-ui/issues/15903#issuecomment-496313450
 // and: https://reactjs.org/docs/forwarding-refs.html
-export const BooleanFilter = React.forwardRef(
-  ({ column, closeMenu }, ref) => {
-    const store = useContext(StoreContext)
-    const { name } = column
+export const BooleanFilter = forwardRef(({ column, closeMenu }, ref) => {
+  const store = useContext(StoreContext)
+  const { name } = column
 
-    const storeValue = store.ekPlan?.[`filter${upperFirst(name)}`]
-    const storeSetFunction = store.ekPlan?.[`setFilter${upperFirst(name)}`]
+  const storeValue = store.ekPlan?.[`filter${upperFirst(name)}`]
+  const storeSetFunction = store.ekPlan?.[`setFilter${upperFirst(name)}`]
 
-    let valueText = 'wahre Werte'
-    if (name === 'ekfrequenzAbweichend') {
-      valueText = 'abweichende'
-    }
+  let valueText = 'wahre Werte'
+  if (name === 'ekfrequenzAbweichend') {
+    valueText = 'abweichende'
+  }
 
-    const onClick = useCallback(() => {
-      storeSetFunction(!storeValue)
-      closeMenu()
-    }, [])
+  const onClick = useCallback(() => {
+    storeSetFunction(!storeValue)
+    closeMenu()
+  }, [])
 
-    return (
-      <MenuItem ref={ref} onClick={onClick} dense>
-        {!storeValue ? `${valueText} filtern` : `${valueText} nicht filtern`}
-      </MenuItem>
-    )
-  },
-)
-
+  return (
+    <MenuItem
+      ref={ref}
+      onClick={onClick}
+      dense
+    >
+      {!storeValue ? `${valueText} filtern` : `${valueText} nicht filtern`}
+    </MenuItem>
+  )
+})
