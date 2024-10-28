f1c9947ba08accb7ac2e6fbb82d8654279fc8d29
Alexander Gabriel <alex.barbalex@gmail.com>
2024-10-28 11:56:35 +0100

    use named export

--- src/components/Projekte/Daten/Tpopfreiwkontr/Form/Files.jsx
+++ src/components/Projekte/Daten/Tpopfreiwkontr/Form/Files.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import { memo } from 'react'
 import styled from '@emotion/styled'
 import { observer } from 'mobx-react-lite'
 
-import { Files } from '../../../../shared/Files/index.jsx'
+import { Files as SharedFiles } from '../../../../shared/Files/index.jsx'
 
 const Container = styled.div`
   border: 1px solid rgba(0, 0, 0, 0.5);
@@ -19,16 +19,16 @@ const RemarksVal = styled.div`
   margin-bottom: -10px;
 `
 
-const EkfFiles = ({ row }) => (
-  <Container>
-    <RemarksLabel>Dateien</RemarksLabel>
-    <RemarksVal>
-      <Files
-        parentId={row.id}
-        parent="tpopkontr"
-      />
-    </RemarksVal>
-  </Container>
+export const Files = memo(
+  observer(({ row }) => (
+    <Container>
+      <RemarksLabel>Dateien</RemarksLabel>
+      <RemarksVal>
+        <SharedFiles
+          parentId={row.id}
+          parent="tpopkontr"
+        />
+      </RemarksVal>
+    </Container>
+  )),
 )
-
-export default observer(EkfFiles)
