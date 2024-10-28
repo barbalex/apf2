1766a03e69be090d8f3b1e9499eb9f8b1124ba87
Alexander Gabriel <alex.barbalex@gmail.com>
2024-10-28 17:13:53 +0100

    use named export

--- src/components/Projekte/Exporte/Teilpopulationen/TPopFuerWebgisBun.jsx
+++ src/components/Projekte/Exporte/Teilpopulationen/TPopFuerWebgisBun.jsx
@@ -6,7 +6,7 @@ import { exportModule } from '../../../../modules/export.js'
 import { StoreContext } from '../../../../storeContext.js'
 import { DownloadCardButton, StyledProgressText } from '../index.jsx'
 
-const Teilpopulationen = () => {
+export const TPopFuerWebgisBun = observer(() => {
   const client = useApolloClient()
   const store = useContext(StoreContext)
   const { enqueNotification } = store
@@ -100,11 +100,9 @@ const Teilpopulationen = () => {
       }}
     >
       Teilpopulationen für WebGIS BUN
-      {queryState ? (
+      {queryState ?
         <StyledProgressText>{queryState}</StyledProgressText>
-      ) : null}
+      : null}
     </DownloadCardButton>
   )
-}
-
-export default observer(Teilpopulationen)
+})
