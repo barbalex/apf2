c809fec4f8882ee5a99ec3d9c5a22d73123554cd
Alexander Gabriel <alex.barbalex@gmail.com>
2024-10-30 17:13:39 +0100

    use named export

--- src/components/Projekte/TreeContainer/Tree/Root/Projekt/ApFolder/Aps/Folders/Pop/Pop/Folders/index.jsx
+++ src/components/Projekte/TreeContainer/Tree/Root/Projekt/ApFolder/Aps/Folders/Pop/Pop/Folders/index.jsx
@@ -5,9 +5,9 @@ import { useApolloClient } from '@apollo/client'
 import { observer } from 'mobx-react-lite'
 
 import { StoreContext } from '../../../../../../../../../../../../storeContext.js'
-import TpopFolder from './Tpop'
-import PopBerFolder from './PopBer'
-import PopMassnBerFolder from './PopMassnBer'
+import TpopFolder from './Tpop/index.jsx'
+import PopBerFolder from './PopBer/index.jsx'
+import PopMassnBerFolder from './PopMassnBer/index.jsx'
 
 export const PopFolders = observer(({ projekt, ap, pop }) => {
   const client = useApolloClient()
