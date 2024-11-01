5c1294d8a39dfd6db96d0acf26046db5b5480c99
Alexander Gabriel <alex.barbalex@gmail.com>
2024-11-01 15:59:19 +0100

    work on toolbar

--- src/components/shared/Toolbar/index.jsx
+++ src/components/shared/Toolbar/index.jsx
@@ -10,13 +10,29 @@ import { useResizeDetector } from 'react-resize-detector'
 import styled from 'styled-components'
 
 const Container = styled.div`
+  width: 100%;
   overflow: hidden;
-  max-height: 42px;
+  padding: 5px;
 `
 const ToolDiv = styled.div`
   display: inline;
+  padding: 0 10px 0 0;
 `
 
+const Tools = () => [
+  <ToolDiv key={1}>Tool</ToolDiv>,
+  <ToolDiv key={2}>Tool</ToolDiv>,
+  <ToolDiv key={3}>Tool</ToolDiv>,
+  <ToolDiv key={4}>Tool</ToolDiv>,
+  <ToolDiv key={5}>Tool</ToolDiv>,
+  <ToolDiv key={6}>Tool</ToolDiv>,
+  <ToolDiv key={7}>Tool</ToolDiv>,
+  <ToolDiv key={8}>Tool</ToolDiv>,
+  <ToolDiv key={9}>Tool</ToolDiv>,
+  <ToolDiv key={10}>Tool</ToolDiv>,
+]
+
+// TODO: pass in Tools as children?
 export const Toolbar = memo(() => {
   const [overflowing, setOverflowing] = useState(false)
   const containerRef = useRef(null)
@@ -24,14 +40,19 @@ export const Toolbar = memo(() => {
   const checkOverflow = useCallback(() => {
     const container = containerRef.current
     const isOverflowing = container.scrollWidth > container.clientWidth
-    if (isOverflowing !== overflowing) setOverflowing(isOverflowing)
+    if (isOverflowing !== overflowing) {
+      setOverflowing(isOverflowing)
+      if (isOverflowing) {
+        // TODO: send right tool into menu
+      }
+    }
   }, [overflowing])
 
   const { width } = useResizeDetector({
     onResize: checkOverflow,
     targetRef: containerRef,
     refreshMode: 'debounce',
-    refreshRate: 100,
+    refreshRate: 200,
     refreshOptions: { trailing: true },
   })
 
@@ -39,11 +60,7 @@ export const Toolbar = memo(() => {
 
   return (
     <Container ref={containerRef}>
-      <ToolDiv>Toolbar</ToolDiv>
-      <ToolDiv>Toolbar</ToolDiv>
-      <ToolDiv>Toolbar</ToolDiv>
-      <ToolDiv>Toolbar</ToolDiv>
-      <ToolDiv>Toolbar</ToolDiv>
+      <Tools />
     </Container>
   )
 })
