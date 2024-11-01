d2403f816f8edcda4eb61ebab8cc7dc7686a4bd1
Alexander Gabriel <alex.barbalex@gmail.com>
2024-11-01 18:06:06 +0100

    work on toolbar

--- src/components/shared/Toolbar/index.jsx
+++ src/components/shared/Toolbar/index.jsx
@@ -8,7 +8,19 @@ import {
 } from 'react'
 import { useResizeDetector } from 'react-resize-detector'
 import { IconButton } from '@mui/material'
-import { FaBars, FaAlignLeft } from 'react-icons/fa6'
+import {
+  FaBars,
+  FaAlignLeft,
+  FaAlignRight,
+  FaArrowDown,
+  FaArrowRight,
+  FaArrowLeft,
+  FaArrowUp,
+  FaCaretDown,
+  FaCaretLeft,
+  FaCaretRight,
+  FaCaretUp,
+} from 'react-icons/fa6'
 import styled from 'styled-components'
 
 const Container = styled.div`
@@ -23,19 +35,6 @@ const ToolDiv = styled.div`
   padding: 0 10px 0 0;
 `
 
-const Tools = () => [
-  <ToolDiv key={1}>Tool</ToolDiv>,
-  <ToolDiv key={2}>Tool</ToolDiv>,
-  <ToolDiv key={3}>Tool</ToolDiv>,
-  <ToolDiv key={4}>Tool</ToolDiv>,
-  <ToolDiv key={5}>Tool</ToolDiv>,
-  <ToolDiv key={6}>Tool</ToolDiv>,
-  <ToolDiv key={7}>Tool</ToolDiv>,
-  <ToolDiv key={8}>Tool</ToolDiv>,
-  <ToolDiv key={9}>Tool</ToolDiv>,
-  <ToolDiv key={10}>Tool</ToolDiv>,
-]
-
 const testTools = [
   {
     title: 'Tool 1',
@@ -44,51 +43,52 @@ const testTools = [
   },
   {
     title: 'Tool 2',
-    iconComponent: <FaAlignLeft />,
+    iconComponent: <FaArrowDown />,
     onClick: () => console.log('Tool 2'),
   },
   {
     title: 'Tool 3',
-    iconComponent: <FaAlignLeft />,
+    iconComponent: <FaArrowLeft />,
     onClick: () => console.log('Tool 3'),
   },
   {
     title: 'Tool 4',
-    iconComponent: <FaAlignLeft />,
+    iconComponent: <FaArrowRight />,
     onClick: () => console.log('Tool 4'),
   },
   {
     title: 'Tool 5',
-    iconComponent: <FaAlignLeft />,
+    iconComponent: <FaArrowUp />,
     onClick: () => console.log('Tool 5'),
   },
   {
     title: 'Tool 6',
-    iconComponent: <FaAlignLeft />,
+    iconComponent: <FaCaretDown />,
     onClick: () => console.log('Tool 6'),
   },
   {
     title: 'Tool 7',
-    iconComponent: <FaAlignLeft />,
+    iconComponent: <FaCaretLeft />,
     onClick: () => console.log('Tool 7'),
   },
   {
     title: 'Tool 8',
-    iconComponent: <FaAlignLeft />,
+    iconComponent: <FaCaretRight />,
     onClick: () => console.log('Tool 8'),
   },
   {
     title: 'Tool 9',
-    iconComponent: <FaAlignLeft />,
+    iconComponent: <FaCaretUp />,
     onClick: () => console.log('Tool 9'),
   },
   {
     title: 'Tool 10',
-    iconComponent: <FaAlignLeft />,
+    iconComponent: <FaAlignRight />,
     onClick: () => console.log('Tool 10'),
   },
 ]
 const buttonWidth = 40
+const gapWidth = 5
 
 // TODO: pass in Tools as children?
 // or rather: need info for menu AND button
@@ -102,10 +102,10 @@ export const Toolbar = memo(({ tools = testTools }) => {
   const onResize = useCallback(({ width }) => {
     console.log('onResize:', { width, testTools })
     // TODO: build menus
-    // width of one tool button is toolButtonWidth
-    // toolsWidth is tools.length * toolWidth + (tools.length - 1) * columnGapWidth
+    // width of one tool button is buttonWidth
+    // toolsWidth is tools.length * buttonWidth + (tools.length - 1) * gapWidth
     // if toolsWidth > container width
-    // fit tools into containerWidth - MenuButtonWidth - columnGapWidth
+    // fit tools into containerWidth - MenuButtonWidth - gapWidth
     // fit fitting tools into container
     // add overflowing tools to menu
     setButtons(testTools)
@@ -119,10 +119,16 @@ export const Toolbar = memo(({ tools = testTools }) => {
     refreshOptions: { trailing: true },
   })
 
+  // TODO: build menu from menuItems
   return (
     <Container ref={containerRef}>
       {buttons.map((tool) => (
-        <IconButton key={tool.title}>{tool.iconComponent}</IconButton>
+        <IconButton
+          key={tool.title}
+          title={tool.title}
+        >
+          {tool.iconComponent}
+        </IconButton>
       ))}
       <IconButton>
         <FaBars />
