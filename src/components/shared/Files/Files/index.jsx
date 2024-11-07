34082fba918914e2ffeb5c555f9316b2ea25f51c
Alexander Gabriel <alex.barbalex@gmail.com>
2024-11-07 12:06:53 +0100

    optimize

--- src/components/shared/Files/Files/index.jsx
+++ src/components/shared/Files/Files/index.jsx
@@ -10,19 +10,15 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   background-color: ${(props) => (props.showfilter ? '#ffd3a7' : 'unset')};
-  padding: 0 10px;
-`
-const Spacer = styled.div`
-  height: 10px;
+  padding: 10px;
 `
 
 export const Component = memo(() => {
-  const { parentId, parent, files, refetch } = useOutletContext()
+  const { parent, files, refetch } = useOutletContext()
 
   return (
     <ErrorBoundary>
       <Container>
-        <Spacer />
         {files.map((file) => (
           <File
             key={file.fileId}
