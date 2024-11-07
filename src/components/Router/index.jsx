99ce7e6fa463ad9a219b7e4c46f154c601036f5b
Alexander Gabriel <alex.barbalex@gmail.com>
2024-11-07 19:04:40 +0100

    update deps

--- src/components/Router/index.jsx
+++ src/components/Router/index.jsx
@@ -618,6 +618,15 @@ export const Router = () => {
         />
       </Route>,
     ),
+    {
+      future: {
+        v7_fetcherPersist: true,
+        v7_normalizeFormMethod: true,
+        v7_startTransition: true,
+        v7_partialHydration: true,
+        v7_skipActionStatusRevalidation: true,
+      },
+    },
   )
 
   return <RouterProvider router={router} />
