2a2300b3395b44caeec5bdf4eacfdb84bc344115
Alexander Gabriel <alex.barbalex@gmail.com>
2024-11-13 20:41:54 +0100

    use memo

--- src/components/LastTouchedNodeSetter.jsx
+++ src/components/LastTouchedNodeSetter.jsx
@@ -1,17 +1,19 @@
-import { useContext, useEffect } from 'react'
+import { memo, useContext, useEffect } from 'react'
 import { observer } from 'mobx-react-lite'
 import { getSnapshot } from 'mobx-state-tree'
 
 import { StoreContext } from '../storeContext.js'
 
-export const LastTouchedNodeSetter = observer(() => {
-  const store = useContext(StoreContext)
-  const { activeNodeArray, setLastTouchedNode } = store.tree
+export const LastTouchedNodeSetter = memo(
+  observer(() => {
+    const store = useContext(StoreContext)
+    const { activeNodeArray, setLastTouchedNode } = store.tree
 
-  useEffect(() => {
-    // set last touched node in case project is directly opened on it
-    setLastTouchedNode(getSnapshot(activeNodeArray))
-  }, [activeNodeArray, setLastTouchedNode])
+    useEffect(() => {
+      // set last touched node in case project is directly opened on it
+      setLastTouchedNode(getSnapshot(activeNodeArray))
+    }, [activeNodeArray, setLastTouchedNode])
 
-  return null
-})
+    return null
+  }),
+)
