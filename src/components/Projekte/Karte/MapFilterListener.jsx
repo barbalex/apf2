20decec5d7da90082a3526ca7ed87722d365e05e
Alexander Gabriel <alex.barbalex@gmail.com>
2024-11-14 09:42:31 +0100

    use memo

--- src/components/Projekte/Karte/MapFilterListener.jsx
+++ src/components/Projekte/Karte/MapFilterListener.jsx
@@ -1,19 +1,21 @@
-import { useContext, useEffect } from 'react'
+import { memo, useContext, useEffect } from 'react'
 import { useMap } from 'react-leaflet'
 import { observer } from 'mobx-react-lite'
 
 import { StoreContext } from '../../../storeContext.js'
 
-export const MapFilterListener = observer(() => {
-  const map = useMap()
-  const store = useContext(StoreContext)
-  const { mapFilter } = store.tree
+export const MapFilterListener = memo(
+  observer(() => {
+    const map = useMap()
+    const store = useContext(StoreContext)
+    const { mapFilter } = store.tree
 
-  useEffect(() => {
-    if (!mapFilter) {
-      map.fireEvent('draw:deletedFromOutside')
-    }
-  }, [map, mapFilter])
+    useEffect(() => {
+      if (!mapFilter) {
+        map.fireEvent('draw:deletedFromOutside')
+      }
+    }, [map, mapFilter])
 
-  return null
-})
+    return null
+  }),
+)
--- src/components/Projekte/Karte/MapResizer.jsx
+++ src/components/Projekte/Karte/MapResizer.jsx
@@ -1,52 +1,54 @@
-import { useEffect, useContext } from 'react'
+import { memo, useEffect, useContext } from 'react'
 import { useMap } from 'react-leaflet'
 import { observer } from 'mobx-react-lite'
 import { getSnapshot } from 'mobx-state-tree'
 
 import { StoreContext } from '../../../storeContext.js'
 
-export const MapResizer = observer(({ children, mapContainerRef }) => {
-  const map = useMap()
-  const store = useContext(StoreContext)
-  const { bounds: boundsRaw } = store
-  const bounds = getSnapshot(boundsRaw)
+export const MapResizer = memo(
+  observer(({ children, mapContainerRef }) => {
+    const map = useMap()
+    const store = useContext(StoreContext)
+    const { bounds: boundsRaw } = store
+    const bounds = getSnapshot(boundsRaw)
 
-  // need this to prevent map from greying out on resize
-  // https://github.com/PaulLeCam/react-leaflet/issues/1074
-  useEffect(() => {
-    const container = mapContainerRef?.current
-    if (!container) return
+    // need this to prevent map from greying out on resize
+    // https://github.com/PaulLeCam/react-leaflet/issues/1074
+    useEffect(() => {
+      const container = mapContainerRef?.current
+      if (!container) return
 
-    const resizeObserver = new ResizeObserver(() => {
-      if (map) {
-        // console.log('MapResizer, resizeObserver resizing map')
-        map.invalidateSize()
-      }
-    })
-
-    resizeObserver.observe(container)
-
-    return () => {
-      resizeObserver.unobserve(container)
-    }
-  }, [map, mapContainerRef])
-
-  // this is a bad hack to make sure bounds are set
-  // somehow on reload of the page they are not set
-  useEffect(() => {
-    if (map) {
-      console.log('MapResizer fitting bounds')
-      setTimeout(() => {
-        // Uncaught TypeError: Cannot read properties of undefined (reading '_leaflet_pos')
-        // thus: try/catch
-        try {
-          map.fitBounds(bounds)
-        } catch (error) {
-          console.log('MapResizer error fitting bounds:', error)
+      const resizeObserver = new ResizeObserver(() => {
+        if (map) {
+          // console.log('MapResizer, resizeObserver resizing map')
+          map.invalidateSize()
         }
-      }, 800)
-    }
-  }, [bounds, map])
+      })
 
-  return children
-})
+      resizeObserver.observe(container)
+
+      return () => {
+        resizeObserver.unobserve(container)
+      }
+    }, [map, mapContainerRef])
+
+    // this is a bad hack to make sure bounds are set
+    // somehow on reload of the page they are not set
+    useEffect(() => {
+      if (map) {
+        console.log('MapResizer fitting bounds')
+        setTimeout(() => {
+          // Uncaught TypeError: Cannot read properties of undefined (reading '_leaflet_pos')
+          // thus: try/catch
+          try {
+            map.fitBounds(bounds)
+          } catch (error) {
+            console.log('MapResizer error fitting bounds:', error)
+          }
+        }, 800)
+      }
+    }, [bounds, map])
+
+    return children
+  }),
+)
