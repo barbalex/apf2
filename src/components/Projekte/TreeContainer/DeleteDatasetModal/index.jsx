d1dcad5128f08ca95830e7819d4f75622eb819fe
Alexander Gabriel <alex.barbalex@gmail.com>
2024-11-14 10:14:54 +0100

    use memo

--- src/components/Projekte/TreeContainer/DeleteDatasetModal/index.jsx
+++ src/components/Projekte/TreeContainer/DeleteDatasetModal/index.jsx
@@ -1,4 +1,4 @@
-import { useContext, useCallback } from 'react'
+import { memo, useContext, useCallback } from 'react'
 import Dialog from '@mui/material/Dialog'
 import DialogActions from '@mui/material/DialogActions'
 import Button from '@mui/material/Button'
@@ -18,54 +18,56 @@ const StyledDialog = styled(Dialog)`
   }
 `
 
-export const DatasetDeleteModal = observer(() => {
-  const { search } = useLocation()
+export const DatasetDeleteModal = memo(
+  observer(() => {
+    const { search } = useLocation()
 
-  const client = useApolloClient()
-  const store = useContext(StoreContext)
-  const { toDeleteTable, toDeleteLabel, emptyToDelete, toDeleteId } = store
+    const client = useApolloClient()
+    const store = useContext(StoreContext)
+    const { toDeleteTable, toDeleteLabel, emptyToDelete, toDeleteId } = store
 
-  const table = tables.find((t) => t.table === toDeleteTable)
-  let tableName = null
-  if (table && table.labelSingular) {
-    tableName = table.labelSingular
-  }
-  let question = `${tableName ? `${tableName} "` : ''}${toDeleteLabel}${
-    tableName ? '"' : ''
-  } löschen?`
-  if (!toDeleteLabel) {
-    question = `${tableName} löschen?`
-  }
+    const table = tables.find((t) => t.table === toDeleteTable)
+    let tableName = null
+    if (table && table.labelSingular) {
+      tableName = table.labelSingular
+    }
+    let question = `${tableName ? `${tableName} "` : ''}${toDeleteLabel}${
+      tableName ? '"' : ''
+    } löschen?`
+    if (!toDeleteLabel) {
+      question = `${tableName} löschen?`
+    }
 
-  const onClickLoeschen = useCallback(
-    () =>
-      deleteModule({
-        client,
-        store,
-        search,
-      }),
-    [client, search, store],
-  )
+    const onClickLoeschen = useCallback(
+      () =>
+        deleteModule({
+          client,
+          store,
+          search,
+        }),
+      [client, search, store],
+    )
 
-  return (
-    <ErrorBoundary>
-      <StyledDialog open={!!toDeleteId}>
-        {question}
-        <DialogActions>
-          <Button
-            onClick={emptyToDelete}
-            color="inherit"
-          >
-            Abbrechen
-          </Button>
-          <Button
-            color="primary"
-            onClick={onClickLoeschen}
-          >
-            Löschen
-          </Button>
-        </DialogActions>
-      </StyledDialog>
-    </ErrorBoundary>
-  )
-})
+    return (
+      <ErrorBoundary>
+        <StyledDialog open={!!toDeleteId}>
+          {question}
+          <DialogActions>
+            <Button
+              onClick={emptyToDelete}
+              color="inherit"
+            >
+              Abbrechen
+            </Button>
+            <Button
+              color="primary"
+              onClick={onClickLoeschen}
+            >
+              Löschen
+            </Button>
+          </DialogActions>
+        </StyledDialog>
+      </ErrorBoundary>
+    )
+  }),
+)
