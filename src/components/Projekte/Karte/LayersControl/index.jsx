787ebb9ac1070ce7f9243db5c56ffa4101db1e0d
Alexander Gabriel <alex.barbalex@gmail.com>
2024-11-14 09:50:04 +0100

    use memo

--- src/components/Projekte/Karte/LayersControl/index.jsx
+++ src/components/Projekte/Karte/LayersControl/index.jsx
@@ -1,4 +1,11 @@
-import { useContext, useState, useCallback, useEffect, useRef } from 'react'
+import {
+  memo,
+  useContext,
+  useState,
+  useCallback,
+  useEffect,
+  useRef,
+} from 'react'
 import styled from '@emotion/styled'
 import { MdExpandMore, MdExpandLess } from 'react-icons/md'
 import { observer } from 'mobx-react-lite'
@@ -54,117 +61,119 @@ const StyledExpandMoreIcon = styled(ExpandMoreIcon)`
   height: 18px !important;
 `
 
-export const LayersControl = observer(() => {
-  const store = useContext(StoreContext)
-  const { apfloraLayers, overlays } = store
+export const LayersControl = memo(
+  observer(() => {
+    const store = useContext(StoreContext)
+    const { apfloraLayers, overlays } = store
 
-  const [baseLayersExpanded, setBaseLayersExpanded] = useState(true)
-  const [overlaysExpanded, setOverlaysExpanded] = useState(false)
-  const [apfloraLayersExpanded, setApfloraLayersExpanded] = useState(false)
+    const [baseLayersExpanded, setBaseLayersExpanded] = useState(true)
+    const [overlaysExpanded, setOverlaysExpanded] = useState(false)
+    const [apfloraLayersExpanded, setApfloraLayersExpanded] = useState(false)
 
-  const onToggleBaseLayersExpanded = useCallback(
-    (event) => {
-      event.stopPropagation()
-      setBaseLayersExpanded(!baseLayersExpanded)
-      if (overlaysExpanded) {
-        setOverlaysExpanded(!overlaysExpanded)
+    const onToggleBaseLayersExpanded = useCallback(
+      (event) => {
+        event.stopPropagation()
+        setBaseLayersExpanded(!baseLayersExpanded)
+        if (overlaysExpanded) {
+          setOverlaysExpanded(!overlaysExpanded)
+        }
+        if (apfloraLayersExpanded) {
+          setApfloraLayersExpanded(!apfloraLayersExpanded)
+        }
+      },
+      [baseLayersExpanded, overlaysExpanded, apfloraLayersExpanded],
+    )
+    const onToggleOverlaysExpanded = useCallback(() => {
+      setOverlaysExpanded(!overlaysExpanded)
+      if (baseLayersExpanded) {
+        setBaseLayersExpanded(!baseLayersExpanded)
       }
       if (apfloraLayersExpanded) {
         setApfloraLayersExpanded(!apfloraLayersExpanded)
       }
-    },
-    [baseLayersExpanded, overlaysExpanded, apfloraLayersExpanded],
-  )
-  const onToggleOverlaysExpanded = useCallback(() => {
-    setOverlaysExpanded(!overlaysExpanded)
-    if (baseLayersExpanded) {
-      setBaseLayersExpanded(!baseLayersExpanded)
-    }
-    if (apfloraLayersExpanded) {
+    }, [overlaysExpanded, baseLayersExpanded, apfloraLayersExpanded])
+    const onToggleApfloraLayersExpanded = useCallback(() => {
       setApfloraLayersExpanded(!apfloraLayersExpanded)
-    }
-  }, [overlaysExpanded, baseLayersExpanded, apfloraLayersExpanded])
-  const onToggleApfloraLayersExpanded = useCallback(() => {
-    setApfloraLayersExpanded(!apfloraLayersExpanded)
-    if (overlaysExpanded) {
-      setOverlaysExpanded(!overlaysExpanded)
-    }
-    if (baseLayersExpanded) {
-      setBaseLayersExpanded(!baseLayersExpanded)
-    }
-  }, [overlaysExpanded, baseLayersExpanded, apfloraLayersExpanded])
+      if (overlaysExpanded) {
+        setOverlaysExpanded(!overlaysExpanded)
+      }
+      if (baseLayersExpanded) {
+        setBaseLayersExpanded(!baseLayersExpanded)
+      }
+    }, [overlaysExpanded, baseLayersExpanded, apfloraLayersExpanded])
 
-  const ApfloraCard =
-    baseLayersExpanded || apfloraLayersExpanded || overlaysExpanded ?
-      CardTitle
-    : CardTitleApfloraOpen
+    const ApfloraCard =
+      baseLayersExpanded || apfloraLayersExpanded || overlaysExpanded ?
+        CardTitle
+      : CardTitleApfloraOpen
 
-  // hack to get control to show on first load
-  // depends on state being changed, so needs to be true above
-  // see: https://github.com/LiveBy/react-leaflet-control/issues/27#issuecomment-430564722
-  useEffect(() => {
-    setBaseLayersExpanded(false)
-  }, [])
+    // hack to get control to show on first load
+    // depends on state being changed, so needs to be true above
+    // see: https://github.com/LiveBy/react-leaflet-control/issues/27#issuecomment-430564722
+    useEffect(() => {
+      setBaseLayersExpanded(false)
+    }, [])
 
-  // prevent click propagation on to map
-  // https://stackoverflow.com/a/57013052/712005
-  const ref = useRef()
-  useEffect(() => {
-    window.L.DomEvent.disableClickPropagation(ref.current)
-    window.L.DomEvent.disableScrollPropagation(ref.current)
-  }, [])
+    // prevent click propagation on to map
+    // https://stackoverflow.com/a/57013052/712005
+    const ref = useRef()
+    useEffect(() => {
+      window.L.DomEvent.disableClickPropagation(ref.current)
+      window.L.DomEvent.disableScrollPropagation(ref.current)
+    }, [])
 
-  return (
-    <CardContainer ref={ref}>
-      <Card>
-        <CardHeader onClick={onToggleApfloraLayersExpanded}>
-          <ApfloraCard>apflora</ApfloraCard>
-          <div>
-            {apfloraLayersExpanded ?
-              <StyledExpandLessIcon />
-            : <StyledExpandMoreIcon />}
-          </div>
-        </CardHeader>
-        {apfloraLayersExpanded && (
-          <ApfloraLayers
-            /**
-             * overlaysString enforces rererender
-             * even when only the sorting changes
-             */
-            apfloraLayersString={apfloraLayers.map((o) => o.value).join()}
-          />
-        )}
-      </Card>
-      <Card>
-        <CardHeader onClick={onToggleOverlaysExpanded}>
-          <CardTitle>überlagernd</CardTitle>
-          <div>
-            {overlaysExpanded ?
-              <StyledExpandLessIcon />
-            : <StyledExpandMoreIcon />}
-          </div>
-        </CardHeader>
-        {overlaysExpanded && (
-          <Overlays
-            /**
-             * overlaysString enforces rererender
-             * even when only the sorting changes
-             */
-            overlaysString={overlays.map((o) => o.value).join()}
-          />
-        )}
-      </Card>
-      <Card>
-        <CardHeader onClick={onToggleBaseLayersExpanded}>
-          <CardTitle>Hintergrund</CardTitle>
-          <div>
-            {baseLayersExpanded ?
-              <StyledExpandLessIcon />
-            : <StyledExpandMoreIcon />}
-          </div>
-        </CardHeader>
-        {baseLayersExpanded && <BaseLayers />}
-      </Card>
-    </CardContainer>
-  )
-})
+    return (
+      <CardContainer ref={ref}>
+        <Card>
+          <CardHeader onClick={onToggleApfloraLayersExpanded}>
+            <ApfloraCard>apflora</ApfloraCard>
+            <div>
+              {apfloraLayersExpanded ?
+                <StyledExpandLessIcon />
+              : <StyledExpandMoreIcon />}
+            </div>
+          </CardHeader>
+          {apfloraLayersExpanded && (
+            <ApfloraLayers
+              /**
+               * overlaysString enforces rererender
+               * even when only the sorting changes
+               */
+              apfloraLayersString={apfloraLayers.map((o) => o.value).join()}
+            />
+          )}
+        </Card>
+        <Card>
+          <CardHeader onClick={onToggleOverlaysExpanded}>
+            <CardTitle>überlagernd</CardTitle>
+            <div>
+              {overlaysExpanded ?
+                <StyledExpandLessIcon />
+              : <StyledExpandMoreIcon />}
+            </div>
+          </CardHeader>
+          {overlaysExpanded && (
+            <Overlays
+              /**
+               * overlaysString enforces rererender
+               * even when only the sorting changes
+               */
+              overlaysString={overlays.map((o) => o.value).join()}
+            />
+          )}
+        </Card>
+        <Card>
+          <CardHeader onClick={onToggleBaseLayersExpanded}>
+            <CardTitle>Hintergrund</CardTitle>
+            <div>
+              {baseLayersExpanded ?
+                <StyledExpandLessIcon />
+              : <StyledExpandMoreIcon />}
+            </div>
+          </CardHeader>
+          {baseLayersExpanded && <BaseLayers />}
+        </Card>
+      </CardContainer>
+    )
+  }),
+)
--- src/components/Projekte/Karte/layers/Tpop/index.jsx
+++ src/components/Projekte/Karte/layers/Tpop/index.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from 'react'
+import { memo, useContext, useEffect } from 'react'
 import { observer } from 'mobx-react-lite'
 import MarkerClusterGroup from 'react-leaflet-markercluster'
 import { useApolloClient, useLazyQuery } from '@apollo/client'
@@ -25,161 +25,165 @@ const iconCreateFunction = (cluster) => {
   })
 }
 
-const ObservedTpop = observer(({ clustered }) => {
-  const client = useApolloClient()
+const ObservedTpop = memo(
+  observer(({ clustered }) => {
+    const client = useApolloClient()
 
-  const store = useContext(StoreContext)
-  const {
-    enqueNotification,
-    setIdOfTpopBeingLocalized,
-    idOfTpopBeingLocalized,
-  } = store
-  const { tpopGqlFilter } = store.tree
+    const store = useContext(StoreContext)
+    const {
+      enqueNotification,
+      setIdOfTpopBeingLocalized,
+      idOfTpopBeingLocalized,
+    } = store
+    const { tpopGqlFilter } = store.tree
 
-  const tpopFilter = cloneDeep(tpopGqlFilter.filtered)
-  tpopFilter.or.forEach((f) => (f.wgs84Lat = { isNull: false }))
+    const tpopFilter = cloneDeep(tpopGqlFilter.filtered)
+    tpopFilter.or.forEach((f) => (f.wgs84Lat = { isNull: false }))
 
-  const [fetchTpopDataForMap, { error: errorLoadingTpopForMap, data }] =
-    useLazyQuery(query, {
-      variables: {
-        tpopFilter,
-      },
-    })
-
-  const leafletMap = useMapEvents({
-    async dblclick(event) {
-      //console.log('doubleclick')
-
-      // since 2018 10 31 using idOfTpopBeingLocalized directly
-      // returns null, so need to use store.idOfTpopBeingLocalized
-      const { idOfTpopBeingLocalized } = store
-      //console.log('Tpop, on dblclick', { idOfTpopBeingLocalized })
-      if (!idOfTpopBeingLocalized) return
-      /**
-       * TODO
-       * When clicking on Layertool
-       * somehow Mapelement grabs the click event
-       * although Layertool lies _over_ map element ??!!
-       * So when localizing, if user wants to change base map,
-       * click on Layertool sets new coordinates!
-       */
-      const { lat, lng } = event.latlng
-      const geomPoint = {
-        type: 'Point',
-        coordinates: [lng, lat],
-        // need to add crs otherwise PostGIS v2.5 (on server) errors
-        crs: {
-          type: 'name',
-          properties: {
-            name: 'urn:ogc:def:crs:EPSG::4326',
-          },
+    const [fetchTpopDataForMap, { error: errorLoadingTpopForMap, data }] =
+      useLazyQuery(query, {
+        variables: {
+          tpopFilter,
         },
-      }
-      // TODO:
-      // Idea: set Gemeinde?
-      //console.log('Tpop, on dblclick', { lat, lng, geomPoint })
-      // DANGER:
-      // need to stop propagation of the event
-      // if not it is called a second time
-      // the crazy thing is:
-      // in some areas (not all) the second event
-      // has wrong coordinates!!!!
-      window.L.DomEvent.stopPropagation(event)
-      /**
-       * how to update a geometry value?
-       * v1: "SRID=4326;POINT(long lat)" https://github.com/graphile/postgraphile/issues/575#issuecomment-372030995
-       */
-      try {
-        await client.mutate({
-          mutation: updateTpopById,
-          variables: {
-            id: idOfTpopBeingLocalized,
-            geomPoint,
-          },
-        })
-        // refetch so it appears on map
-        // need to also refetch pop in case it was new
-        client.refetchQueries({
-          include: ['TpopForMapQuery', 'PopForMapQuery'],
-        })
-      } catch (error) {
-        enqueNotification({
-          message: error.message,
-          options: {
-            variant: 'error',
-          },
-        })
-      }
-      setIdOfTpopBeingLocalized(null)
-    },
-  })
+      })
 
-  useEffect(() => {
-    if (idOfTpopBeingLocalized) {
-      // see: https://stackoverflow.com/a/28724847/712005
-      // altering the maps css corrupted the map ui
-      window.L.DomUtil.addClass(
-        leafletMap._container,
-        'crosshair-cursor-enabled',
-      )
-    } else {
-      window.L.DomUtil.removeClass(
-        leafletMap._container,
-        'crosshair-cursor-enabled',
-      )
-    }
-  }, [idOfTpopBeingLocalized, leafletMap._container])
+    const leafletMap = useMapEvents({
+      async dblclick(event) {
+        //console.log('doubleclick')
 
-  if (errorLoadingTpopForMap) {
-    enqueNotification({
-      message: `Fehler beim Laden der Teil-Populationen für die Karte: ${errorLoadingTpopForMap.message}`,
-      options: {
-        variant: 'error',
+        // since 2018 10 31 using idOfTpopBeingLocalized directly
+        // returns null, so need to use store.idOfTpopBeingLocalized
+        const { idOfTpopBeingLocalized } = store
+        //console.log('Tpop, on dblclick', { idOfTpopBeingLocalized })
+        if (!idOfTpopBeingLocalized) return
+        /**
+         * TODO
+         * When clicking on Layertool
+         * somehow Mapelement grabs the click event
+         * although Layertool lies _over_ map element ??!!
+         * So when localizing, if user wants to change base map,
+         * click on Layertool sets new coordinates!
+         */
+        const { lat, lng } = event.latlng
+        const geomPoint = {
+          type: 'Point',
+          coordinates: [lng, lat],
+          // need to add crs otherwise PostGIS v2.5 (on server) errors
+          crs: {
+            type: 'name',
+            properties: {
+              name: 'urn:ogc:def:crs:EPSG::4326',
+            },
+          },
+        }
+        // TODO:
+        // Idea: set Gemeinde?
+        //console.log('Tpop, on dblclick', { lat, lng, geomPoint })
+        // DANGER:
+        // need to stop propagation of the event
+        // if not it is called a second time
+        // the crazy thing is:
+        // in some areas (not all) the second event
+        // has wrong coordinates!!!!
+        window.L.DomEvent.stopPropagation(event)
+        /**
+         * how to update a geometry value?
+         * v1: "SRID=4326;POINT(long lat)" https://github.com/graphile/postgraphile/issues/575#issuecomment-372030995
+         */
+        try {
+          await client.mutate({
+            mutation: updateTpopById,
+            variables: {
+              id: idOfTpopBeingLocalized,
+              geomPoint,
+            },
+          })
+          // refetch so it appears on map
+          // need to also refetch pop in case it was new
+          client.refetchQueries({
+            include: ['TpopForMapQuery', 'PopForMapQuery'],
+          })
+        } catch (error) {
+          enqueNotification({
+            message: error.message,
+            options: {
+              variant: 'error',
+            },
+          })
+        }
+        setIdOfTpopBeingLocalized(null)
       },
     })
-  }
 
-  useEffect(() => {
-    if (fetchTpopDataForMap !== undefined) {
-      fetchTpopDataForMap()
+    useEffect(() => {
+      if (idOfTpopBeingLocalized) {
+        // see: https://stackoverflow.com/a/28724847/712005
+        // altering the maps css corrupted the map ui
+        window.L.DomUtil.addClass(
+          leafletMap._container,
+          'crosshair-cursor-enabled',
+        )
+      } else {
+        window.L.DomUtil.removeClass(
+          leafletMap._container,
+          'crosshair-cursor-enabled',
+        )
+      }
+    }, [idOfTpopBeingLocalized, leafletMap._container])
+
+    if (errorLoadingTpopForMap) {
+      enqueNotification({
+        message: `Fehler beim Laden der Teil-Populationen für die Karte: ${errorLoadingTpopForMap.message}`,
+        options: {
+          variant: 'error',
+        },
+      })
     }
-  }, [fetchTpopDataForMap])
 
-  const tpopMarkers = (data?.allTpops?.nodes ?? []).map((tpop) => (
-    <Marker
-      key={tpop.id}
-      tpop={tpop}
-    />
-  ))
+    useEffect(() => {
+      if (fetchTpopDataForMap !== undefined) {
+        fetchTpopDataForMap()
+      }
+    }, [fetchTpopDataForMap])
 
-  if (clustered) {
-    return (
-      <MarkerClusterGroup
-        maxClusterRadius={66}
-        iconCreateFunction={iconCreateFunction}
-      >
-        {tpopMarkers}
-      </MarkerClusterGroup>
-    )
-  }
-  return tpopMarkers
-})
+    const tpopMarkers = (data?.allTpops?.nodes ?? []).map((tpop) => (
+      <Marker
+        key={tpop.id}
+        tpop={tpop}
+      />
+    ))
 
-export const Tpop = observer(({ clustered }) => {
-  const store = useContext(StoreContext)
-  const tree = store.tree
-  const { tpopGqlFilter } = tree
+    if (clustered) {
+      return (
+        <MarkerClusterGroup
+          maxClusterRadius={66}
+          iconCreateFunction={iconCreateFunction}
+        >
+          {tpopMarkers}
+        </MarkerClusterGroup>
+      )
+    }
+    return tpopMarkers
+  }),
+)
 
-  const { apId } = useParams()
+export const Tpop = memo(
+  observer(({ clustered }) => {
+    const store = useContext(StoreContext)
+    const tree = store.tree
+    const { tpopGqlFilter } = tree
 
-  // Problem: gqlFilter updates AFTER apId
-  // if navigating from ap to pop, apId is set before gqlFilter
-  // thus query fetches data for all aps
-  // Solution: do not return pop if apId exists but gqlFilter does not contain it (yet)
-  const gqlFilterHasApId = !!tpopGqlFilter.filtered?.or?.[0]?.popByPopId?.apId
-  const apIdExistsButGqlFilterDoesNotKnowYet = !!apId && !gqlFilterHasApId
+    const { apId } = useParams()
 
-  if (apIdExistsButGqlFilterDoesNotKnowYet) return null
+    // Problem: gqlFilter updates AFTER apId
+    // if navigating from ap to pop, apId is set before gqlFilter
+    // thus query fetches data for all aps
+    // Solution: do not return pop if apId exists but gqlFilter does not contain it (yet)
+    const gqlFilterHasApId = !!tpopGqlFilter.filtered?.or?.[0]?.popByPopId?.apId
+    const apIdExistsButGqlFilterDoesNotKnowYet = !!apId && !gqlFilterHasApId
 
-  return <ObservedTpop clustered={clustered} />
-})
+    if (apIdExistsButGqlFilterDoesNotKnowYet) return null
+
+    return <ObservedTpop clustered={clustered} />
+  }),
+)
