d0747cd5465cb93768368f51c7299c320c06d8da
Alexander Gabriel <alex.barbalex@gmail.com>
2024-11-14 09:37:19 +0100

    use memo

--- src/components/Projekte/Exporte/Populationen/PopsForGoogleEarth.jsx
+++ src/components/Projekte/Exporte/Populationen/PopsForGoogleEarth.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react'
+import { memo, useContext, useState } from 'react'
 import sortBy from 'lodash/sortBy'
 import { observer } from 'mobx-react-lite'
 import { useApolloClient, gql } from '@apollo/client'
@@ -7,83 +7,85 @@ import { exportModule } from '../../../../modules/export.js'
 import { StoreContext } from '../../../../storeContext.js'
 import { DownloadCardButton, StyledProgressText } from '../index.jsx'
 
-export const PopsForGoogleEarth = observer(() => {
-  const client = useApolloClient()
-  const store = useContext(StoreContext)
-  const { enqueNotification } = store
+export const PopsForGoogleEarth = memo(
+  observer(() => {
+    const client = useApolloClient()
+    const store = useContext(StoreContext)
+    const { enqueNotification } = store
 
-  const [queryState, setQueryState] = useState()
+    const [queryState, setQueryState] = useState()
 
-  return (
-    <DownloadCardButton
-      color="inherit"
-      disabled={!!queryState}
-      onClick={async () => {
-        setQueryState('lade Daten...')
-        let result
-        try {
-          result = await client.query({
-            query: gql`
-              query popKmlQuery {
-                allPops(filter: { vPopKmlsByIdExist: true }) {
-                  nodes {
-                    id
-                    vPopKmlsById {
-                      nodes {
-                        art
-                        label
-                        inhalte
-                        id
-                        wgs84Lat
-                        wgs84Long
-                        url
+    return (
+      <DownloadCardButton
+        color="inherit"
+        disabled={!!queryState}
+        onClick={async () => {
+          setQueryState('lade Daten...')
+          let result
+          try {
+            result = await client.query({
+              query: gql`
+                query popKmlQuery {
+                  allPops(filter: { vPopKmlsByIdExist: true }) {
+                    nodes {
+                      id
+                      vPopKmlsById {
+                        nodes {
+                          art
+                          label
+                          inhalte
+                          id
+                          wgs84Lat
+                          wgs84Long
+                          url
+                        }
                       }
                     }
                   }
                 }
-              }
-            `,
+              `,
+            })
+          } catch (error) {
+            enqueNotification({
+              message: error.message,
+              options: {
+                variant: 'error',
+              },
+            })
+          }
+          setQueryState('verarbeite...')
+          const rows = (result?.data?.allPops?.nodes ?? []).map((z) => ({
+            art: z?.vPopKmlsById?.nodes?.[0]?.art ?? '',
+            label: z?.vPopKmlsById?.nodes?.[0]?.label ?? '',
+            inhalte: z?.vPopKmlsById?.nodes?.[0]?.inhalte ?? '',
+            id: z?.vPopKmlsById?.nodes?.[0]?.id ?? '',
+            wgs84Lat: z?.vPopKmlsById?.nodes?.[0]?.wgs84Lat ?? '',
+            wgs84Long: z?.vPopKmlsById?.nodes?.[0]?.wgs84Long ?? '',
+            url: z?.vPopKmlsById?.nodes?.[0]?.url ?? '',
+          }))
+          if (rows.length === 0) {
+            setQueryState(undefined)
+            return enqueNotification({
+              message: 'Die Abfrage retournierte 0 Datensätze',
+              options: {
+                variant: 'warning',
+              },
+            })
+          }
+          exportModule({
+            data: sortBy(rows, ['art', 'label']),
+            fileName: 'Populationen',
+            store,
+            kml: true,
           })
-        } catch (error) {
-          enqueNotification({
-            message: error.message,
-            options: {
-              variant: 'error',
-            },
-          })
-        }
-        setQueryState('verarbeite...')
-        const rows = (result?.data?.allPops?.nodes ?? []).map((z) => ({
-          art: z?.vPopKmlsById?.nodes?.[0]?.art ?? '',
-          label: z?.vPopKmlsById?.nodes?.[0]?.label ?? '',
-          inhalte: z?.vPopKmlsById?.nodes?.[0]?.inhalte ?? '',
-          id: z?.vPopKmlsById?.nodes?.[0]?.id ?? '',
-          wgs84Lat: z?.vPopKmlsById?.nodes?.[0]?.wgs84Lat ?? '',
-          wgs84Long: z?.vPopKmlsById?.nodes?.[0]?.wgs84Long ?? '',
-          url: z?.vPopKmlsById?.nodes?.[0]?.url ?? '',
-        }))
-        if (rows.length === 0) {
           setQueryState(undefined)
-          return enqueNotification({
-            message: 'Die Abfrage retournierte 0 Datensätze',
-            options: {
-              variant: 'warning',
-            },
-          })
-        }
-        exportModule({
-          data: sortBy(rows, ['art', 'label']),
-          fileName: 'Populationen',
-          store,
-          kml: true,
-        })
-        setQueryState(undefined)
-      }}
-    >
-      {`Populationen für Google Earth (beschriftet mit PopNr)`}
-      {queryState ?
-        <StyledProgressText>{queryState}</StyledProgressText>
-      : null}
-    </DownloadCardButton>
-  )
-})
+        }}
+      >
+        {`Populationen für Google Earth (beschriftet mit PopNr)`}
+        {queryState ?
+          <StyledProgressText>{queryState}</StyledProgressText>
+        : null}
+      </DownloadCardButton>
+    )
+  }),
+)
--- src/components/Projekte/Exporte/Teilpopulationen/AnzMassnahmen.jsx
+++ src/components/Projekte/Exporte/Teilpopulationen/AnzMassnahmen.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react'
+import { memo, useContext, useState } from 'react'
 import sortBy from 'lodash/sortBy'
 import { observer } from 'mobx-react-lite'
 import { useApolloClient, gql } from '@apollo/client'
@@ -7,159 +7,163 @@ import { exportModule } from '../../../../modules/export.js'
 import { StoreContext } from '../../../../storeContext.js'
 import { DownloadCardButton, StyledProgressText } from '../index.jsx'
 
-export const AnzMassnahmen = observer(() => {
-  const client = useApolloClient()
-  const store = useContext(StoreContext)
-  const { enqueNotification } = store
+export const AnzMassnahmen = memo(
+  observer(() => {
+    const client = useApolloClient()
+    const store = useContext(StoreContext)
+    const { enqueNotification } = store
 
-  const [queryState, setQueryState] = useState()
+    const [queryState, setQueryState] = useState()
 
-  return (
-    <DownloadCardButton
-      color="inherit"
-      disabled={!!queryState}
-      onClick={async () => {
-        setQueryState('lade Daten...')
-        let result
-        try {
-          result = await client.query({
-            query: gql`
-              query tpopAnzMassnQuery {
-                allTpops(filter: { vTpopAnzmassnsByIdExist: true }) {
-                  totalCount
-                  nodes {
-                    id
-                    vTpopAnzmassnsById {
-                      nodes {
-                        apId
-                        familie
-                        artname
-                        apBearbeitung
-                        apStartJahr
-                        apUmsetzung
-                        popId
-                        popNr
-                        popName
-                        popStatus
-                        popBekanntSeit
-                        popStatusUnklar
-                        popStatusUnklarBegruendung
-                        popX
-                        popY
-                        id
-                        nr
-                        gemeinde
-                        flurname
-                        status
-                        bekanntSeit
-                        statusUnklar
-                        statusUnklarGrund
-                        x
-                        y
-                        radius
-                        hoehe
-                        exposition
-                        klima
-                        neigung
-                        beschreibung
-                        katasterNr
-                        apberRelevant
-                        apberRelevantGrund
-                        eigentuemer
-                        kontakt
-                        nutzungszone
-                        bewirtschafter
-                        ekfrequenz
-                        ekfrequenzAbweichend
-                        anzahlMassnahmen
+    return (
+      <DownloadCardButton
+        color="inherit"
+        disabled={!!queryState}
+        onClick={async () => {
+          setQueryState('lade Daten...')
+          let result
+          try {
+            result = await client.query({
+              query: gql`
+                query tpopAnzMassnQuery {
+                  allTpops(filter: { vTpopAnzmassnsByIdExist: true }) {
+                    totalCount
+                    nodes {
+                      id
+                      vTpopAnzmassnsById {
+                        nodes {
+                          apId
+                          familie
+                          artname
+                          apBearbeitung
+                          apStartJahr
+                          apUmsetzung
+                          popId
+                          popNr
+                          popName
+                          popStatus
+                          popBekanntSeit
+                          popStatusUnklar
+                          popStatusUnklarBegruendung
+                          popX
+                          popY
+                          id
+                          nr
+                          gemeinde
+                          flurname
+                          status
+                          bekanntSeit
+                          statusUnklar
+                          statusUnklarGrund
+                          x
+                          y
+                          radius
+                          hoehe
+                          exposition
+                          klima
+                          neigung
+                          beschreibung
+                          katasterNr
+                          apberRelevant
+                          apberRelevantGrund
+                          eigentuemer
+                          kontakt
+                          nutzungszone
+                          bewirtschafter
+                          ekfrequenz
+                          ekfrequenzAbweichend
+                          anzahlMassnahmen
+                        }
                       }
                     }
                   }
                 }
-              }
-            `,
+              `,
+            })
+          } catch (error) {
+            enqueNotification({
+              message: error.message,
+              options: { variant: 'error' },
+            })
+          }
+          setQueryState('verarbeite...')
+          const rows = (result.data?.allTpops?.nodes ?? []).map((n) => ({
+            ap_id: n?.vTpopAnzmassnsById?.nodes?.[0]?.apId ?? '',
+            familie: n?.vTpopAnzmassnsById?.nodes?.[0]?.familie ?? '',
+            artname: n?.vTpopAnzmassnsById?.nodes?.[0]?.artname ?? '',
+            ap_bearbeitung:
+              n?.vTpopAnzmassnsById?.nodes?.[0]?.apBearbeitung ?? '',
+            ap_start_jahr: n?.vTpopAnzmassnsById?.nodes?.[0]?.apStartJahr ?? '',
+            ap_umsetzung: n?.vTpopAnzmassnsById?.nodes?.[0]?.apUmsetzung ?? '',
+            pop_id: n?.vTpopAnzmassnsById?.nodes?.[0]?.popId ?? '',
+            pop_nr: n?.vTpopAnzmassnsById?.nodes?.[0]?.popNr ?? '',
+            pop_name: n?.vTpopAnzmassnsById?.nodes?.[0]?.popName ?? '',
+            pop_status: n?.vTpopAnzmassnsById?.nodes?.[0]?.popStatus ?? '',
+            pop_bekannt_seit:
+              n?.vTpopAnzmassnsById?.nodes?.[0]?.popBekanntSeit ?? '',
+            pop_status_unklar:
+              n?.vTpopAnzmassnsById?.nodes?.[0]?.popStatusUnklar ?? '',
+            pop_status_unklar_begruendung:
+              n?.vTpopAnzmassnsById?.nodes?.[0]?.popStatusUnklarBegruendung ??
+              '',
+            pop_x: n?.vTpopAnzmassnsById?.nodes?.[0]?.popX ?? '',
+            pop_y: n?.vTpopAnzmassnsById?.nodes?.[0]?.popY ?? '',
+            id: n?.vTpopAnzmassnsById?.nodes?.[0]?.id ?? '',
+            nr: n?.vTpopAnzmassnsById?.nodes?.[0]?.nr ?? '',
+            gemeinde: n?.vTpopAnzmassnsById?.nodes?.[0]?.gemeinde ?? '',
+            flurname: n?.vTpopAnzmassnsById?.nodes?.[0]?.flurname ?? '',
+            status: n?.vTpopAnzmassnsById?.nodes?.[0]?.status ?? '',
+            bekannt_seit: n?.vTpopAnzmassnsById?.nodes?.[0]?.bekanntSeit ?? '',
+            status_unklar:
+              n?.vTpopAnzmassnsById?.nodes?.[0]?.statusUnklar ?? '',
+            status_unklar_grund:
+              n?.vTpopAnzmassnsById?.nodes?.[0]?.statusUnklarGrund ?? '',
+            lv95X: n?.vTpopAnzmassnsById?.nodes?.[0]?.x ?? '',
+            lv95Y: n?.vTpopAnzmassnsById?.nodes?.[0]?.y ?? '',
+            radius: n?.vTpopAnzmassnsById?.nodes?.[0]?.radius ?? '',
+            hoehe: n?.vTpopAnzmassnsById?.nodes?.[0]?.hoehe ?? '',
+            exposition: n?.vTpopAnzmassnsById?.nodes?.[0]?.exposition ?? '',
+            klima: n?.vTpopAnzmassnsById?.nodes?.[0]?.klima ?? '',
+            neigung: n?.vTpopAnzmassnsById?.nodes?.[0]?.neigung ?? '',
+            beschreibung: n?.vTpopAnzmassnsById?.nodes?.[0]?.beschreibung ?? '',
+            kataster_nr: n?.vTpopAnzmassnsById?.nodes?.[0]?.katasterNr ?? '',
+            apber_relevant:
+              n?.vTpopAnzmassnsById?.nodes?.[0]?.apberRelevant ?? '',
+            apber_relevant_grund:
+              n?.vTpopAnzmassnsById?.nodes?.[0]?.apberRelevantGrund ?? '',
+            eigentuemer: n?.vTpopAnzmassnsById?.nodes?.[0]?.eigentuemer ?? '',
+            kontakt: n?.vTpopAnzmassnsById?.nodes?.[0]?.kontakt ?? '',
+            nutzungszone: n?.vTpopAnzmassnsById?.nodes?.[0]?.nutzungszone ?? '',
+            bewirtschafter:
+              n?.vTpopAnzmassnsById?.nodes?.[0]?.bewirtschafter ?? '',
+            ekfrequenz: n?.vTpopAnzmassnsById?.nodes?.[0]?.ekfrequenz ?? '',
+            ekfrequenz_abweichend:
+              n?.vTpopAnzmassnsById?.nodes?.[0]?.ekfrequenzAbweichend ?? '',
+            anzahlMassnahmen:
+              n?.vTpopAnzmassnsById?.nodes?.[0]?.anzahlMassnahmen ?? '',
+          }))
+          if (rows.length === 0) {
+            setQueryState(undefined)
+            return enqueNotification({
+              message: 'Die Abfrage retournierte 0 Datensätze',
+              options: {
+                variant: 'warning',
+              },
+            })
+          }
+          exportModule({
+            data: sortBy(rows, ['artname', 'pop_nr', 'nr']),
+            fileName: 'TeilpopulationenAnzahlMassnahmen',
+            store,
           })
-        } catch (error) {
-          enqueNotification({
-            message: error.message,
-            options: { variant: 'error' },
-          })
-        }
-        setQueryState('verarbeite...')
-        const rows = (result.data?.allTpops?.nodes ?? []).map((n) => ({
-          ap_id: n?.vTpopAnzmassnsById?.nodes?.[0]?.apId ?? '',
-          familie: n?.vTpopAnzmassnsById?.nodes?.[0]?.familie ?? '',
-          artname: n?.vTpopAnzmassnsById?.nodes?.[0]?.artname ?? '',
-          ap_bearbeitung:
-            n?.vTpopAnzmassnsById?.nodes?.[0]?.apBearbeitung ?? '',
-          ap_start_jahr: n?.vTpopAnzmassnsById?.nodes?.[0]?.apStartJahr ?? '',
-          ap_umsetzung: n?.vTpopAnzmassnsById?.nodes?.[0]?.apUmsetzung ?? '',
-          pop_id: n?.vTpopAnzmassnsById?.nodes?.[0]?.popId ?? '',
-          pop_nr: n?.vTpopAnzmassnsById?.nodes?.[0]?.popNr ?? '',
-          pop_name: n?.vTpopAnzmassnsById?.nodes?.[0]?.popName ?? '',
-          pop_status: n?.vTpopAnzmassnsById?.nodes?.[0]?.popStatus ?? '',
-          pop_bekannt_seit:
-            n?.vTpopAnzmassnsById?.nodes?.[0]?.popBekanntSeit ?? '',
-          pop_status_unklar:
-            n?.vTpopAnzmassnsById?.nodes?.[0]?.popStatusUnklar ?? '',
-          pop_status_unklar_begruendung:
-            n?.vTpopAnzmassnsById?.nodes?.[0]?.popStatusUnklarBegruendung ?? '',
-          pop_x: n?.vTpopAnzmassnsById?.nodes?.[0]?.popX ?? '',
-          pop_y: n?.vTpopAnzmassnsById?.nodes?.[0]?.popY ?? '',
-          id: n?.vTpopAnzmassnsById?.nodes?.[0]?.id ?? '',
-          nr: n?.vTpopAnzmassnsById?.nodes?.[0]?.nr ?? '',
-          gemeinde: n?.vTpopAnzmassnsById?.nodes?.[0]?.gemeinde ?? '',
-          flurname: n?.vTpopAnzmassnsById?.nodes?.[0]?.flurname ?? '',
-          status: n?.vTpopAnzmassnsById?.nodes?.[0]?.status ?? '',
-          bekannt_seit: n?.vTpopAnzmassnsById?.nodes?.[0]?.bekanntSeit ?? '',
-          status_unklar: n?.vTpopAnzmassnsById?.nodes?.[0]?.statusUnklar ?? '',
-          status_unklar_grund:
-            n?.vTpopAnzmassnsById?.nodes?.[0]?.statusUnklarGrund ?? '',
-          lv95X: n?.vTpopAnzmassnsById?.nodes?.[0]?.x ?? '',
-          lv95Y: n?.vTpopAnzmassnsById?.nodes?.[0]?.y ?? '',
-          radius: n?.vTpopAnzmassnsById?.nodes?.[0]?.radius ?? '',
-          hoehe: n?.vTpopAnzmassnsById?.nodes?.[0]?.hoehe ?? '',
-          exposition: n?.vTpopAnzmassnsById?.nodes?.[0]?.exposition ?? '',
-          klima: n?.vTpopAnzmassnsById?.nodes?.[0]?.klima ?? '',
-          neigung: n?.vTpopAnzmassnsById?.nodes?.[0]?.neigung ?? '',
-          beschreibung: n?.vTpopAnzmassnsById?.nodes?.[0]?.beschreibung ?? '',
-          kataster_nr: n?.vTpopAnzmassnsById?.nodes?.[0]?.katasterNr ?? '',
-          apber_relevant:
-            n?.vTpopAnzmassnsById?.nodes?.[0]?.apberRelevant ?? '',
-          apber_relevant_grund:
-            n?.vTpopAnzmassnsById?.nodes?.[0]?.apberRelevantGrund ?? '',
-          eigentuemer: n?.vTpopAnzmassnsById?.nodes?.[0]?.eigentuemer ?? '',
-          kontakt: n?.vTpopAnzmassnsById?.nodes?.[0]?.kontakt ?? '',
-          nutzungszone: n?.vTpopAnzmassnsById?.nodes?.[0]?.nutzungszone ?? '',
-          bewirtschafter:
-            n?.vTpopAnzmassnsById?.nodes?.[0]?.bewirtschafter ?? '',
-          ekfrequenz: n?.vTpopAnzmassnsById?.nodes?.[0]?.ekfrequenz ?? '',
-          ekfrequenz_abweichend:
-            n?.vTpopAnzmassnsById?.nodes?.[0]?.ekfrequenzAbweichend ?? '',
-          anzahlMassnahmen:
-            n?.vTpopAnzmassnsById?.nodes?.[0]?.anzahlMassnahmen ?? '',
-        }))
-        if (rows.length === 0) {
           setQueryState(undefined)
-          return enqueNotification({
-            message: 'Die Abfrage retournierte 0 Datensätze',
-            options: {
-              variant: 'warning',
-            },
-          })
-        }
-        exportModule({
-          data: sortBy(rows, ['artname', 'pop_nr', 'nr']),
-          fileName: 'TeilpopulationenAnzahlMassnahmen',
-          store,
-        })
-        setQueryState(undefined)
-      }}
-    >
-      Anzahl Massnahmen pro Teilpopulation
-      {queryState ?
-        <StyledProgressText>{queryState}</StyledProgressText>
-      : null}
-    </DownloadCardButton>
-  )
-})
+        }}
+      >
+        Anzahl Massnahmen pro Teilpopulation
+        {queryState ?
+          <StyledProgressText>{queryState}</StyledProgressText>
+        : null}
+      </DownloadCardButton>
+    )
+  }),
+)
