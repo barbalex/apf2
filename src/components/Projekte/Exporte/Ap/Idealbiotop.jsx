6c4235594dad2f035af882d9bbec8401f229014a
Alexander Gabriel <alex.barbalex@gmail.com>
2024-11-14 09:30:10 +0100

    update deps

--- src/components/Projekte/Exporte/Ap/Idealbiotop.jsx
+++ src/components/Projekte/Exporte/Ap/Idealbiotop.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState, useCallback } from 'react'
+import { memo, useContext, useState, useCallback } from 'react'
 import sortBy from 'lodash/sortBy'
 import { observer } from 'mobx-react-lite'
 import { useApolloClient, gql } from '@apollo/client'
@@ -7,136 +7,138 @@ import { exportModule } from '../../../../modules/export.js'
 import { StoreContext } from '../../../../storeContext.js'
 import { DownloadCardButton, StyledProgressText } from '../index.jsx'
 
-export const Idealbiotop = observer(() => {
-  const client = useApolloClient()
-  const store = useContext(StoreContext)
-  const { enqueNotification } = store
+export const Idealbiotop = memo(
+  observer(() => {
+    const client = useApolloClient()
+    const store = useContext(StoreContext)
+    const { enqueNotification } = store
 
-  const [queryState, setQueryState] = useState()
+    const [queryState, setQueryState] = useState()
 
-  const onClickIdealbiotop = useCallback(async () => {
-    setQueryState('lade Daten...')
-    let result
-    try {
-      result = await client.query({
-        query: gql`
-          query idealbiotopsForExportQuery {
-            allIdealbiotops {
-              nodes {
-                id
-                apId
-                apByApId {
+    const onClickIdealbiotop = useCallback(async () => {
+      setQueryState('lade Daten...')
+      let result
+      try {
+        result = await client.query({
+          query: gql`
+            query idealbiotopsForExportQuery {
+              allIdealbiotops {
+                nodes {
                   id
-                  aeTaxonomyByArtId {
+                  apId
+                  apByApId {
                     id
-                    artname
+                    aeTaxonomyByArtId {
+                      id
+                      artname
+                    }
+                    apBearbstandWerteByBearbeitung {
+                      id
+                      text
+                    }
+                    startJahr
+                    apUmsetzungWerteByUmsetzung {
+                      id
+                      text
+                    }
+                    adresseByBearbeiter {
+                      id
+                      name
+                    }
                   }
-                  apBearbstandWerteByBearbeitung {
-                    id
-                    text
-                  }
-                  startJahr
-                  apUmsetzungWerteByUmsetzung {
-                    id
-                    text
-                  }
-                  adresseByBearbeiter {
-                    id
-                    name
-                  }
+                  erstelldatum
+                  hoehenlage
+                  region
+                  exposition
+                  besonnung
+                  hangneigung
+                  bodenTyp
+                  bodenKalkgehalt
+                  bodenDurchlaessigkeit
+                  bodenHumus
+                  bodenNaehrstoffgehalt
+                  wasserhaushalt
+                  konkurrenz
+                  moosschicht
+                  krautschicht
+                  strauchschicht
+                  baumschicht
+                  bemerkungen
+                  createdAt
+                  updatedAt
+                  changedBy
                 }
-                erstelldatum
-                hoehenlage
-                region
-                exposition
-                besonnung
-                hangneigung
-                bodenTyp
-                bodenKalkgehalt
-                bodenDurchlaessigkeit
-                bodenHumus
-                bodenNaehrstoffgehalt
-                wasserhaushalt
-                konkurrenz
-                moosschicht
-                krautschicht
-                strauchschicht
-                baumschicht
-                bemerkungen
-                createdAt
-                updatedAt
-                changedBy
               }
             }
-          }
-        `,
+          `,
+        })
+      } catch (error) {
+        enqueNotification({
+          message: error.message,
+          options: {
+            variant: 'error',
+          },
+        })
+      }
+      setQueryState('verarbeite...')
+      const rows = (result.data?.allIdealbiotops?.nodes ?? []).map((z) => ({
+        ap_id: z.apId,
+        artname: z?.apByApId?.aeTaxonomyByArtId?.artname ?? '',
+        ap_bearbeitung: z?.apByApId?.apBearbstandWerteByBearbeitung?.text ?? '',
+        ap_start_jahr: z?.apByApId?.startJahr ?? '',
+        ap_umsetzung: z?.apByApId?.apUmsetzungWerteByUmsetzung?.text ?? '',
+        ap_bearbeiter: z?.apByApId?.adresseByBearbeiter?.name ?? '',
+        id: z.id,
+        erstelldatum: z.erstelldatum,
+        hoehenlage: z.hoehenlage,
+        region: z.region,
+        exposition: z.exposition,
+        besonnung: z.besonnung,
+        hangneigung: z.hangneigung,
+        boden_typ: z.bodenTyp,
+        boden_kalkgehalt: z.bodenKalkgehalt,
+        boden_durchlaessigkeit: z.bodenDurchlaessigkeit,
+        boden_humus: z.bodenHumus,
+        boden_naehrstoffgehalt: z.bodenNaehrstoffgehalt,
+        wasserhaushalt: z.wasserhaushalt,
+        konkurrenz: z.konkurrenz,
+        moosschicht: z.moosschicht,
+        krautschicht: z.krautschicht,
+        strauchschicht: z.strauchschicht,
+        baumschicht: z.baumschicht,
+        bemerkungen: z.bemerkungen,
+        created_at: z.createdAt,
+        updated_at: z.updatedAt,
+        changed_by: z.changedBy,
+      }))
+      if (rows.length === 0) {
+        setQueryState(undefined)
+        return enqueNotification({
+          message: 'Die Abfrage retournierte 0 Datensätze',
+          options: {
+            variant: 'warning',
+          },
+        })
+      }
+      exportModule({
+        data: sortBy(rows, 'artname'),
+        fileName: 'Idealbiotope',
+        store,
       })
-    } catch (error) {
-      enqueNotification({
-        message: error.message,
-        options: {
-          variant: 'error',
-        },
-      })
-    }
-    setQueryState('verarbeite...')
-    const rows = (result.data?.allIdealbiotops?.nodes ?? []).map((z) => ({
-      ap_id: z.apId,
-      artname: z?.apByApId?.aeTaxonomyByArtId?.artname ?? '',
-      ap_bearbeitung: z?.apByApId?.apBearbstandWerteByBearbeitung?.text ?? '',
-      ap_start_jahr: z?.apByApId?.startJahr ?? '',
-      ap_umsetzung: z?.apByApId?.apUmsetzungWerteByUmsetzung?.text ?? '',
-      ap_bearbeiter: z?.apByApId?.adresseByBearbeiter?.name ?? '',
-      id: z.id,
-      erstelldatum: z.erstelldatum,
-      hoehenlage: z.hoehenlage,
-      region: z.region,
-      exposition: z.exposition,
-      besonnung: z.besonnung,
-      hangneigung: z.hangneigung,
-      boden_typ: z.bodenTyp,
-      boden_kalkgehalt: z.bodenKalkgehalt,
-      boden_durchlaessigkeit: z.bodenDurchlaessigkeit,
-      boden_humus: z.bodenHumus,
-      boden_naehrstoffgehalt: z.bodenNaehrstoffgehalt,
-      wasserhaushalt: z.wasserhaushalt,
-      konkurrenz: z.konkurrenz,
-      moosschicht: z.moosschicht,
-      krautschicht: z.krautschicht,
-      strauchschicht: z.strauchschicht,
-      baumschicht: z.baumschicht,
-      bemerkungen: z.bemerkungen,
-      created_at: z.createdAt,
-      updated_at: z.updatedAt,
-      changed_by: z.changedBy,
-    }))
-    if (rows.length === 0) {
       setQueryState(undefined)
-      return enqueNotification({
-        message: 'Die Abfrage retournierte 0 Datensätze',
-        options: {
-          variant: 'warning',
-        },
-      })
-    }
-    exportModule({
-      data: sortBy(rows, 'artname'),
-      fileName: 'Idealbiotope',
-      store,
-    })
-    setQueryState(undefined)
-  }, [enqueNotification, client, store])
+    }, [enqueNotification, client, store])
 
-  return (
-    <DownloadCardButton
-      onClick={onClickIdealbiotop}
-      color="inherit"
-      disabled={!!queryState}
-    >
-      Idealbiotope
-      {queryState ?
-        <StyledProgressText>{queryState}</StyledProgressText>
-      : null}
-    </DownloadCardButton>
-  )
-})
+    return (
+      <DownloadCardButton
+        onClick={onClickIdealbiotop}
+        color="inherit"
+        disabled={!!queryState}
+      >
+        Idealbiotope
+        {queryState ?
+          <StyledProgressText>{queryState}</StyledProgressText>
+        : null}
+      </DownloadCardButton>
+    )
+  }),
+)
