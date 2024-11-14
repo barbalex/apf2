9b2a0d95883e61999a61b5d3b3b50d87034c0a14
Alexander Gabriel <alex.barbalex@gmail.com>
2024-11-14 09:34:58 +0100

    use memo

--- src/components/Projekte/Exporte/Populationen/ApOhneStatus.jsx
+++ src/components/Projekte/Exporte/Populationen/ApOhneStatus.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react'
+import { memo, useContext, useState } from 'react'
 import sortBy from 'lodash/sortBy'
 import { observer } from 'mobx-react-lite'
 import { useApolloClient, gql } from '@apollo/client'
@@ -7,87 +7,89 @@ import { exportModule } from '../../../../modules/export.js'
 import { StoreContext } from '../../../../storeContext.js'
 import { DownloadCardButton, StyledProgressText } from '../index.jsx'
 
-export const ApOhneStatus = observer(() => {
-  const client = useApolloClient()
-  const store = useContext(StoreContext)
-  const { enqueNotification } = store
+export const ApOhneStatus = memo(
+  observer(() => {
+    const client = useApolloClient()
+    const store = useContext(StoreContext)
+    const { enqueNotification } = store
 
-  const [queryState, setQueryState] = useState()
+    const [queryState, setQueryState] = useState()
 
-  return (
-    <DownloadCardButton
-      color="inherit"
-      disabled={!!queryState}
-      onClick={async () => {
-        setQueryState('lade Daten...')
-        let result
-        try {
-          result = await client.query({
-            query: gql`
-              query popVonApOhneStatusQuery {
-                allPops(filter: { vPopVonapohnestatusesByIdExist: true }) {
-                  nodes {
-                    id
-                    vPopVonapohnestatusesById {
-                      nodes {
-                        apId
-                        artname
-                        apBearbeitung
-                        id
-                        nr
-                        name
-                        status
-                        x
-                        y
+    return (
+      <DownloadCardButton
+        color="inherit"
+        disabled={!!queryState}
+        onClick={async () => {
+          setQueryState('lade Daten...')
+          let result
+          try {
+            result = await client.query({
+              query: gql`
+                query popVonApOhneStatusQuery {
+                  allPops(filter: { vPopVonapohnestatusesByIdExist: true }) {
+                    nodes {
+                      id
+                      vPopVonapohnestatusesById {
+                        nodes {
+                          apId
+                          artname
+                          apBearbeitung
+                          id
+                          nr
+                          name
+                          status
+                          x
+                          y
+                        }
                       }
                     }
                   }
                 }
-              }
-            `,
+              `,
+            })
+          } catch (error) {
+            enqueNotification({
+              message: error.message,
+              options: {
+                variant: 'error',
+              },
+            })
+          }
+          setQueryState('verarbeite...')
+          const rows = (result?.data?.allPops?.nodes ?? []).map((z) => ({
+            ap_id: z?.vPopVonapohnestatusesById?.nodes?.[0]?.apId ?? '',
+            artname: z?.vPopVonapohnestatusesById?.nodes?.[0]?.artname ?? '',
+            ap_bearbeitung:
+              z?.vPopVonapohnestatusesById?.nodes?.[0]?.apBearbeitung ?? '',
+            id: z?.vPopVonapohnestatusesById?.nodes?.[0]?.id ?? '',
+            nr: z?.vPopVonapohnestatusesById?.nodes?.[0]?.nr ?? '',
+            name: z?.vPopVonapohnestatusesById?.nodes?.[0]?.name ?? '',
+            status: z?.vPopVonapohnestatusesById?.nodes?.[0]?.status ?? '',
+            lv95X: z?.vPopVonapohnestatusesById?.nodes?.[0]?.x ?? '',
+            lv95Y: z?.vPopVonapohnestatusesById?.nodes?.[0]?.y ?? '',
+          }))
+          if (rows.length === 0) {
+            setQueryState(undefined)
+            return enqueNotification({
+              message: 'Die Abfrage retournierte 0 Datensätze',
+              options: {
+                variant: 'warning',
+              },
+            })
+          }
+          exportModule({
+            data: sortBy(rows, ['artname', 'nr']),
+            fileName: 'PopulationenVonApArtenOhneStatus',
+            store,
           })
-        } catch (error) {
-          enqueNotification({
-            message: error.message,
-            options: {
-              variant: 'error',
-            },
-          })
-        }
-        setQueryState('verarbeite...')
-        const rows = (result?.data?.allPops?.nodes ?? []).map((z) => ({
-          ap_id: z?.vPopVonapohnestatusesById?.nodes?.[0]?.apId ?? '',
-          artname: z?.vPopVonapohnestatusesById?.nodes?.[0]?.artname ?? '',
-          ap_bearbeitung:
-            z?.vPopVonapohnestatusesById?.nodes?.[0]?.apBearbeitung ?? '',
-          id: z?.vPopVonapohnestatusesById?.nodes?.[0]?.id ?? '',
-          nr: z?.vPopVonapohnestatusesById?.nodes?.[0]?.nr ?? '',
-          name: z?.vPopVonapohnestatusesById?.nodes?.[0]?.name ?? '',
-          status: z?.vPopVonapohnestatusesById?.nodes?.[0]?.status ?? '',
-          lv95X: z?.vPopVonapohnestatusesById?.nodes?.[0]?.x ?? '',
-          lv95Y: z?.vPopVonapohnestatusesById?.nodes?.[0]?.y ?? '',
-        }))
-        if (rows.length === 0) {
           setQueryState(undefined)
-          return enqueNotification({
-            message: 'Die Abfrage retournierte 0 Datensätze',
-            options: {
-              variant: 'warning',
-            },
-          })
-        }
-        exportModule({
-          data: sortBy(rows, ['artname', 'nr']),
-          fileName: 'PopulationenVonApArtenOhneStatus',
-          store,
-        })
-        setQueryState(undefined)
-      }}
-    >
-      Populationen von AP-Arten ohne Status
-      {queryState ?
-        <StyledProgressText>{queryState}</StyledProgressText>
-      : null}
-    </DownloadCardButton>
-  )
-})
+        }}
+      >
+        Populationen von AP-Arten ohne Status
+        {queryState ?
+          <StyledProgressText>{queryState}</StyledProgressText>
+        : null}
+      </DownloadCardButton>
+    )
+  }),
+)
--- src/components/Projekte/Exporte/Populationen/Berichte.jsx
+++ src/components/Projekte/Exporte/Populationen/Berichte.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react'
+import { memo, useContext, useState } from 'react'
 import sortBy from 'lodash/sortBy'
 import { observer } from 'mobx-react-lite'
 import { useApolloClient, gql } from '@apollo/client'
@@ -7,135 +7,139 @@ import { exportModule } from '../../../../modules/export.js'
 import { StoreContext } from '../../../../storeContext.js'
 import { DownloadCardButton, StyledProgressText } from '../index.jsx'
 
-export const Berichte = observer(() => {
-  const client = useApolloClient()
-  const store = useContext(StoreContext)
-  const { enqueNotification } = store
+export const Berichte = memo(
+  observer(() => {
+    const client = useApolloClient()
+    const store = useContext(StoreContext)
+    const { enqueNotification } = store
 
-  const [queryState, setQueryState] = useState()
+    const [queryState, setQueryState] = useState()
 
-  return (
-    <DownloadCardButton
-      color="inherit"
-      disabled={!!queryState}
-      onClick={async () => {
-        setQueryState('lade Daten...')
-        let result
-        try {
-          result = await client.query({
-            query: gql`
-              query popPopberUndMassnberQuery {
-                allPops(filter: { vPopPopberundmassnbersByPopIdExist: true }) {
-                  nodes {
-                    id
-                    vPopPopberundmassnbersByPopId {
-                      nodes {
-                        apId
-                        artname
-                        apBearbeitung
-                        apStartJahr
-                        apUmsetzung
-                        popId
-                        popNr
-                        popName
-                        popStatus
-                        popBekanntSeit
-                        popStatusUnklar
-                        popStatusUnklarBegruendung
-                        popX
-                        popY
-                        popCreatedAt
-                        popUpdatedAt
-                        popChangedBy
-                        jahr
-                        popberId
-                        popberJahr
-                        popberEntwicklung
-                        popberBemerkungen
-                        popberCreatedAt
-                        popberUpdatedAt
-                        popberChangedBy
-                        popmassnberId
-                        popmassnberJahr
-                        popmassnberEntwicklung
-                        popmassnberBemerkungen
-                        popmassnberCreatedAt
-                        popmassnberUpdatedAt
-                        popmassnberChangedBy
+    return (
+      <DownloadCardButton
+        color="inherit"
+        disabled={!!queryState}
+        onClick={async () => {
+          setQueryState('lade Daten...')
+          let result
+          try {
+            result = await client.query({
+              query: gql`
+                query popPopberUndMassnberQuery {
+                  allPops(
+                    filter: { vPopPopberundmassnbersByPopIdExist: true }
+                  ) {
+                    nodes {
+                      id
+                      vPopPopberundmassnbersByPopId {
+                        nodes {
+                          apId
+                          artname
+                          apBearbeitung
+                          apStartJahr
+                          apUmsetzung
+                          popId
+                          popNr
+                          popName
+                          popStatus
+                          popBekanntSeit
+                          popStatusUnklar
+                          popStatusUnklarBegruendung
+                          popX
+                          popY
+                          popCreatedAt
+                          popUpdatedAt
+                          popChangedBy
+                          jahr
+                          popberId
+                          popberJahr
+                          popberEntwicklung
+                          popberBemerkungen
+                          popberCreatedAt
+                          popberUpdatedAt
+                          popberChangedBy
+                          popmassnberId
+                          popmassnberJahr
+                          popmassnberEntwicklung
+                          popmassnberBemerkungen
+                          popmassnberCreatedAt
+                          popmassnberUpdatedAt
+                          popmassnberChangedBy
+                        }
                       }
                     }
                   }
                 }
-              }
-            `,
+              `,
+            })
+          } catch (error) {
+            enqueNotification({
+              message: error.message,
+              options: {
+                variant: 'error',
+              },
+            })
+          }
+          setQueryState('verarbeite...')
+          // need to flatmap because view delivers multiple rows per pop
+          const rows = (result?.data?.allPops?.nodes ?? []).flatMap((z0) =>
+            (z0?.vPopPopberundmassnbersByPopId?.nodes ?? []).map((z) => ({
+              ap_id: z.apId,
+              artname: z.artname,
+              ap_bearbeitung: z.apBearbeitung,
+              ap_start_jahr: z.apStartJahr,
+              ap_umsetzung: z.apUmsetzung,
+              pop_id: z.popId,
+              pop_nr: z.popNr,
+              pop_name: z.popName,
+              pop_status: z.popStatus,
+              pop_bekannt_seit: z.popBekanntSeit,
+              pop_status_unklar: z.popStatusUnklar,
+              pop_status_unklar_begruendung: z.popStatusUnklarBegruendung,
+              pop_x: z.popX,
+              pop_y: z.popY,
+              pop_created_at: z.popCreatedAt,
+              pop_updated_at: z.popUpdatedAt,
+              pop_changed_by: z.popChangedBy,
+              jahr: z.jahr,
+              popber_id: z.popberId,
+              popber_jahr: z.popberJahr,
+              popber_entwicklung: z.popberEntwicklung,
+              popber_bemerkungen: z.popberBemerkungen,
+              popber_created_at: z.popberCreatedAt,
+              popber_updated_at: z.popberUpdatedAt,
+              popber_changed_by: z.popberChangedBy,
+              popmassnber_id: z.popmassnberId,
+              popmassnber_jahr: z.popmassnberJahr,
+              popmassnber_entwicklung: z.popmassnberEntwicklung,
+              popmassnber_bemerkungen: z.popmassnberBemerkungen,
+              popmassnber_created_at: z.popmassnberCreatedAt,
+              popmassnber_updated_at: z.popmassnberUpdatedAt,
+              popmassnber_changed_by: z.popmassnberChangedBy,
+            })),
+          )
+          if (rows.length === 0) {
+            setQueryState(undefined)
+            return enqueNotification({
+              message: 'Die Abfrage retournierte 0 Datensätze',
+              options: {
+                variant: 'warning',
+              },
+            })
+          }
+          exportModule({
+            data: sortBy(rows, ['artname', 'pop_nr', 'jahr']),
+            fileName: 'PopulationenPopUndMassnBerichte',
+            store,
           })
-        } catch (error) {
-          enqueNotification({
-            message: error.message,
-            options: {
-              variant: 'error',
-            },
-          })
-        }
-        setQueryState('verarbeite...')
-        // need to flatmap because view delivers multiple rows per pop
-        const rows = (result?.data?.allPops?.nodes ?? []).flatMap((z0) =>
-          (z0?.vPopPopberundmassnbersByPopId?.nodes ?? []).map((z) => ({
-            ap_id: z.apId,
-            artname: z.artname,
-            ap_bearbeitung: z.apBearbeitung,
-            ap_start_jahr: z.apStartJahr,
-            ap_umsetzung: z.apUmsetzung,
-            pop_id: z.popId,
-            pop_nr: z.popNr,
-            pop_name: z.popName,
-            pop_status: z.popStatus,
-            pop_bekannt_seit: z.popBekanntSeit,
-            pop_status_unklar: z.popStatusUnklar,
-            pop_status_unklar_begruendung: z.popStatusUnklarBegruendung,
-            pop_x: z.popX,
-            pop_y: z.popY,
-            pop_created_at: z.popCreatedAt,
-            pop_updated_at: z.popUpdatedAt,
-            pop_changed_by: z.popChangedBy,
-            jahr: z.jahr,
-            popber_id: z.popberId,
-            popber_jahr: z.popberJahr,
-            popber_entwicklung: z.popberEntwicklung,
-            popber_bemerkungen: z.popberBemerkungen,
-            popber_created_at: z.popberCreatedAt,
-            popber_updated_at: z.popberUpdatedAt,
-            popber_changed_by: z.popberChangedBy,
-            popmassnber_id: z.popmassnberId,
-            popmassnber_jahr: z.popmassnberJahr,
-            popmassnber_entwicklung: z.popmassnberEntwicklung,
-            popmassnber_bemerkungen: z.popmassnberBemerkungen,
-            popmassnber_created_at: z.popmassnberCreatedAt,
-            popmassnber_updated_at: z.popmassnberUpdatedAt,
-            popmassnber_changed_by: z.popmassnberChangedBy,
-          })),
-        )
-        if (rows.length === 0) {
           setQueryState(undefined)
-          return enqueNotification({
-            message: 'Die Abfrage retournierte 0 Datensätze',
-            options: {
-              variant: 'warning',
-            },
-          })
-        }
-        exportModule({
-          data: sortBy(rows, ['artname', 'pop_nr', 'jahr']),
-          fileName: 'PopulationenPopUndMassnBerichte',
-          store,
-        })
-        setQueryState(undefined)
-      }}
-    >
-      Populationen inkl. Populations- und Massnahmen-Berichte
-      {queryState ?
-        <StyledProgressText>{queryState}</StyledProgressText>
-      : null}
-    </DownloadCardButton>
-  )
-})
+        }}
+      >
+        Populationen inkl. Populations- und Massnahmen-Berichte
+        {queryState ?
+          <StyledProgressText>{queryState}</StyledProgressText>
+        : null}
+      </DownloadCardButton>
+    )
+  }),
+)
