27bdd8b2e5ef4b4f73be55e7bf8bb2ecab12b823
Alexander Gabriel <alex.barbalex@gmail.com>
2024-11-16 14:48:53 +0100

    design list

---
+++ src/components/shared/List/index.jsx
@@ -0,0 +1,65 @@
+import { memo, useCallback } from 'react'
+import { useNavigate } from 'react-router-dom'
+import styled from '@emotion/styled'
+
+import { FormTitle } from '../FormTitle/index.jsx'
+import { ErrorBoundary } from '../ErrorBoundary.jsx'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  overflow: hidden;
+`
+const ListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  overflow: auto;
+  scrollbar-width: thin;
+`
+const Row = styled.div`
+  display: flex;
+  justify-content: center;
+  border-top: rgba(46, 125, 50, 0.5) solid 1px;
+  width: 100%;
+  height: 50px;
+  line-height: 50px;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  user-select: none;
+  &:hover {
+    background-color: rgba(103, 216, 101, 0.07);
+  }
+  &:last-child {
+    border-bottom: rgba(46, 125, 50, 0.5) solid 1px;
+  }
+`
+
+export const List = memo(({ items, title, totalCount, menuBar = null }) => {
+  const navigate = useNavigate()
+
+  const onClickRow = useCallback((item) => navigate(item.id), [navigate])
+
+  return (
+    <ErrorBoundary>
+      <Container>
+        <FormTitle
+          title={`${title} (${items.length}/${totalCount ?? items.length})`}
+          menuBar={menuBar}
+        />
+        <ListContainer>
+          {items.map((item) => (
+            <Row
+              key={item.id}
+              onClick={onClickRow.bind(this, item)}
+            >
+              {item.label}
+            </Row>
+          ))}
+        </ListContainer>
+      </Container>
+    </ErrorBoundary>
+  )
+})
