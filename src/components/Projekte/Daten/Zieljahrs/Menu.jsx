0e0b50048310d84eec0999d241383bccda59e8fa
Alexander Gabriel <alex.barbalex@gmail.com>
2024-11-22 15:07:10 +0100

    add list filters

--- src/components/Projekte/Daten/Zieljahrs/Menu.jsx
+++ src/components/Projekte/Daten/Zieljahrs/Menu.jsx
@@ -1,4 +1,4 @@
-import { memo, useCallback, useContext } from 'react'
+import { memo, useCallback, useContext, useMemo } from 'react'
 import { useApolloClient, gql } from '@apollo/client'
 import { useQueryClient } from '@tanstack/react-query'
 import { useParams, useNavigate, useLocation } from 'react-router-dom'
@@ -6,97 +6,115 @@ import { FaPlus, FaFolderTree } from 'react-icons/fa6'
 import { RiFolderCloseFill } from 'react-icons/ri'
 import IconButton from '@mui/material/IconButton'
 import Tooltip from '@mui/material/Tooltip'
+import { observer } from 'mobx-react-lite'
+import { useAtom } from 'jotai'
 
-import { MenuBar } from '../../../shared/MenuBar/index.jsx'
+import { MenuBar, buttonWidth } from '../../../shared/MenuBar/index.jsx'
 import { ErrorBoundary } from '../../../shared/ErrorBoundary.jsx'
-import { StoreContext } from '../../../../storeContext.js'
 import { openLowerNodes } from '../../TreeContainer/openLowerNodes/index.js'
 import { closeLowerNodes } from '../../TreeContainer/closeLowerNodes.js'
+import { StoreContext } from '../../../../storeContext.js'
+import { LabelFilter, labelFilterWidth } from '../../../shared/LabelFilter.jsx'
+import { listLabelFilterIsIconAtom } from '../../../../JotaiStore/index.js'
 
 const iconStyle = { color: 'white' }
 
-export const Menu = memo(() => {
-  const { search } = useLocation()
-  const navigate = useNavigate()
-  const client = useApolloClient()
-  const tanstackQueryClient = useQueryClient()
-  const { projId, apId } = useParams()
+export const Menu = memo(
+  observer(() => {
+    const { search } = useLocation()
+    const navigate = useNavigate()
+    const client = useApolloClient()
+    const tanstackQueryClient = useQueryClient()
+    const { projId, apId } = useParams()
+    const store = useContext(StoreContext)
 
-  const onClickAdd = useCallback(async () => {
-    let result
-    try {
-      result = await client.mutate({
-        mutation: gql`
-          mutation createZielForZielForm($apId: UUID!) {
-            createZiel(input: { ziel: { apId: $apId, jahr: 1 } }) {
-              ziel {
-                id
-                apId
+    const onClickAdd = useCallback(async () => {
+      let result
+      try {
+        result = await client.mutate({
+          mutation: gql`
+            mutation createZielForZielForm($apId: UUID!) {
+              createZiel(input: { ziel: { apId: $apId, jahr: 1 } }) {
+                ziel {
+                  id
+                  apId
+                }
               }
             }
-          }
-        `,
-        variables: { apId },
+          `,
+          variables: { apId },
+        })
+      } catch (error) {
+        return store.enqueNotification({
+          message: error.message,
+          options: {
+            variant: 'error',
+          },
+        })
+      }
+      tanstackQueryClient.invalidateQueries({
+        queryKey: [`treeZiel`],
       })
-    } catch (error) {
-      return store.enqueNotification({
-        message: error.message,
-        options: {
-          variant: 'error',
-        },
+      tanstackQueryClient.invalidateQueries({
+        queryKey: [`treeApFolders`],
       })
-    }
-    tanstackQueryClient.invalidateQueries({
-      queryKey: [`treeZiel`],
-    })
-    tanstackQueryClient.invalidateQueries({
-      queryKey: [`treeApFolders`],
-    })
-    const id = result?.data?.createZiel?.ziel?.id
-    navigate(`./1/${id}${search}`)
-  }, [client, store, tanstackQueryClient, navigate, search, apId])
+      const id = result?.data?.createZiel?.ziel?.id
+      navigate(`./1/${id}${search}`)
+    }, [client, store, tanstackQueryClient, navigate, search, apId])
 
-  const onClickOpenLowerNodes = useCallback(() => {
-    openLowerNodes({
-      id: apId,
-      projId,
-      apId,
-      client,
-      store,
-      menuType: 'zielFolder',
-    })
-  }, [projId, apId, client, store])
+    const onClickOpenLowerNodes = useCallback(() => {
+      openLowerNodes({
+        id: apId,
+        projId,
+        apId,
+        client,
+        store,
+        menuType: 'zielFolder',
+      })
+    }, [projId, apId, client, store])
 
-  const onClickCloseLowerNodes = useCallback(() => {
-    closeLowerNodes({
-      url: ['Projekte', projId, 'Arten', apId, 'AP-Ziele'],
-      store,
-      search,
-    })
-  }, [projId, apId, store, search])
+    const onClickCloseLowerNodes = useCallback(() => {
+      closeLowerNodes({
+        url: ['Projekte', projId, 'Arten', apId, 'AP-Ziele'],
+        store,
+        search,
+      })
+    }, [projId, apId, store, search])
 
-  return (
-    <ErrorBoundary>
-      <MenuBar
-        bgColor="#388e3c"
-        color="white"
-      >
-        <Tooltip title="Neues Ziel erstellen">
-          <IconButton onClick={onClickAdd}>
-            <FaPlus style={iconStyle} />
-          </IconButton>
-        </Tooltip>
-        <Tooltip title="Ordner im Navigationsbaum öffnen">
-          <IconButton onClick={onClickOpenLowerNodes}>
-            <FaFolderTree style={iconStyle} />
-          </IconButton>
-        </Tooltip>
-        <Tooltip title="Ordner im Navigationsbaum schliessen">
-          <IconButton onClick={onClickCloseLowerNodes}>
-            <RiFolderCloseFill style={iconStyle} />
-          </IconButton>
-        </Tooltip>
-      </MenuBar>
-    </ErrorBoundary>
-  )
-})
+    const [labelFilterIsIcon] = useAtom(listLabelFilterIsIconAtom)
+    const widths = useMemo(
+      () =>
+        labelFilterIsIcon ?
+          [buttonWidth, buttonWidth, buttonWidth]
+        : [labelFilterWidth, buttonWidth, buttonWidth],
+      [labelFilterIsIcon],
+    )
+
+    return (
+      <ErrorBoundary>
+        <MenuBar
+          bgColor="#388e3c"
+          color="white"
+          widths={widths}
+        >
+          <LabelFilter />
+          <Tooltip title="Neues Ziel erstellen">
+            <IconButton onClick={onClickAdd}>
+              <FaPlus style={iconStyle} />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Ordner im Navigationsbaum öffnen">
+            <IconButton onClick={onClickOpenLowerNodes}>
+              <FaFolderTree style={iconStyle} />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Ordner im Navigationsbaum schliessen">
+            <IconButton onClick={onClickCloseLowerNodes}>
+              <RiFolderCloseFill style={iconStyle} />
+            </IconButton>
+          </Tooltip>
+        </MenuBar>
+      </ErrorBoundary>
+    )
+  }),
+)
