96d1e09dd6a6c4f018f5a14e9bcae65cf5f2431a
Alexander Gabriel <alex.barbalex@gmail.com>
2024-11-24 10:02:14 +0100

    correct

--- src/components/Projekte/Daten/Tpopfreiwkontr/Menu.jsx
+++ src/components/Projekte/Daten/Tpopfreiwkontr/Menu.jsx
@@ -101,7 +101,7 @@ export const Menu = memo(
       try {
         result = await client.mutate({
           mutation: gql`
-            mutation deleteTpopkontr($id: UUID!) {
+            mutation deleteTpopkontrForTpopfreiwkontr($id: UUID!) {
               deleteTpopkontrById(input: { id: $id }) {
                 tpopkontr {
                   id
