0d72089979683a0ca9f9364d02e1a70e8297d3c8
Alexander Gabriel <alex.barbalex@gmail.com>
2024-11-26 12:10:30 +0100

    migrate

--- src/components/Router/index.jsx
+++ src/components/Router/index.jsx
@@ -823,17 +823,6 @@ export const Router = () => {
         />
       </Route>,
     ),
-    {
-      future: {
-        v7_fetcherPersist: true,
-        v7_normalizeFormMethod: true,
-        v7_startTransition: true,
-        v7_partialHydration: true,
-        v7_skipActionStatusRevalidation: true,
-        v7_skipActionErrorRevalidation: true,
-        v7_relativeSplatPath: true,
-      },
-    },
   )
 
   return <RouterProvider router={router} />
