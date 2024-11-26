cee0d846d8d88cf8c1d3ae505eee5b20181c3e7d
Alexander Gabriel <alex.barbalex@gmail.com>
2024-11-26 19:10:41 +0100

    work on it

--- src/components/Bookmarks/index.jsx
+++ src/components/Bookmarks/index.jsx
@@ -1,5 +1,10 @@
 import { memo } from 'react'
 
 export const Bookmarks = memo(() => {
+  // TODO:
+  // from top do bottom
+  // get: bookmarks (label, url) and children (array of same)
+  // build ui from bookmarks
+  // if children: render as menu
   return <div>{`Bookmarks. Window width: ${window.innerWidth}`}</div>
 })
