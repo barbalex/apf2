e5c30c92cb201cdb03e20d0242a2801290381ab7
Alexander Gabriel <alex.barbalex@gmail.com>
2024-11-27 14:28:58 +0100

    work on to nav

--- src/components/Bookmarks/NavTo/Navs/Daten.jsx
+++ src/components/Bookmarks/NavTo/Navs/Daten.jsx
@@ -1,11 +1,21 @@
 import { memo, useContext } from 'react'
 import { observer } from 'mobx-react-lite'
+import styled from '@emotion/styled'
 
 import { StoreContext } from '../../../../storeContext.js'
 import { useRootNavData } from '../../../../modules/useRootNavData.js'
 import { Spinner } from '../../../shared/Spinner.jsx'
 import { Error } from '../../../shared/Error.jsx'
 import { Nav } from '../Nav.jsx'
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  flex-wrap: nowrap;
+  height: 40px;
+  border-bottom: rgba(46, 125, 50, 0.5) solid 1px;
+`
 
 export const DatenNav = memo(
   observer(() => {
@@ -23,14 +33,14 @@ export const DatenNav = memo(
     // TODO: relative paths are not working using Link
     // use a with onClick instead
     return (
-      <div>
+      <Container>
         {navData.menus.map((item) => (
           <Nav
             key={item.id}
             item={item}
           />
         ))}
-      </div>
+      </Container>
     )
   }),
 )
--- src/components/Bookmarks/index.jsx
+++ src/components/Bookmarks/index.jsx
@@ -1,6 +1,17 @@
 import { memo } from 'react'
+import styled from '@emotion/styled'
 
 import { NavTo } from './NavTo/index.jsx'
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  flex-wrap: nowrap;
+  height: 40px;
+  border-top: rgba(46, 125, 50, 0.5) solid 1px;
+  border-bottom: rgba(46, 125, 50, 0.5) solid 1px;
+`
 
 export const Bookmarks = memo(() => {
   // TODO:
@@ -10,7 +21,7 @@ export const Bookmarks = memo(() => {
   // if children: render as menu
   return (
     <>
-      <div>{`Bookmarks. Window width: ${window.innerWidth}`}</div>
+      <Container>{`Bookmarks. Window width: ${window.innerWidth}`}</Container>
       <NavTo />
     </>
   )
