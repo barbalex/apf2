a8476560782cbf00c71f69c2a1d00f38e15b8649
Alexander Gabriel <alex.barbalex@gmail.com>
2024-11-28 18:39:38 +0100

    optimize

--- src/components/Projekte/Daten/Ekzaehleinheit/Menu.jsx
+++ src/components/Projekte/Daten/Ekzaehleinheit/Menu.jsx
@@ -19,12 +19,12 @@ import { MenuTitle } from '../../../shared/Files/Menu/index.jsx'
 const iconStyle = { color: 'white' }
 
 export const Menu = memo(
-  observer(({ row }) => {
+  observer(() => {
     const { search, pathname } = useLocation()
     const navigate = useNavigate()
     const client = useApolloClient()
     const queryClient = useQueryClient()
-    const { projId, apId } = useParams()
+    const { projId, apId, zaehleinheitId } = useParams()
     const store = useContext(StoreContext)
 
     const onClickAdd = useCallback(async () => {
@@ -79,7 +79,7 @@ export const Menu = memo(
               }
             }
           `,
-          variables: { id: row.id },
+          variables: { id: zaehleinheitId },
         })
       } catch (error) {
         return store.enqueNotification({
@@ -116,7 +116,7 @@ export const Menu = memo(
       search,
       apId,
       projId,
-      row,
+      zaehleinheitId,
       pathname,
     ])
 
