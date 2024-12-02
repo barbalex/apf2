0317bcd15308e4d6e4d15fb8694d39b4e18dd83c
Alexander Gabriel <alex.barbalex@gmail.com>
2024-12-02 23:03:31 +0100

    work on bookmarks

--- src/components/Projekte/Daten/Tpopkontrzaehls/index.jsx
+++ src/components/Projekte/Daten/Tpopkontrzaehls/index.jsx
@@ -1,11 +1,8 @@
 import { memo, useContext } from 'react'
-import { useApolloClient, gql } from '@apollo/client'
-import { useQuery } from '@tanstack/react-query'
 import { observer } from 'mobx-react-lite'
-import { useParams } from 'react-router'
 
 import { StoreContext } from '../../../../storeContext.js'
-import { createTpopkontrzaehlsQuery } from '../../../../modules/createTpopkontrzaehlsQuery.js'
+import { useTpopfeldkontrzaehlsNavData } from '../../../../modules/useTpopfeldkontrzaehlsNavData.js'
 import { List } from '../../../shared/List/index.jsx'
 import { Menu } from './Menu.jsx'
 import { Spinner } from '../../../shared/Spinner.jsx'
@@ -13,23 +10,10 @@ import { Error } from '../../../shared/Error.jsx'
 
 export const Component = memo(
   observer(() => {
-    const { tpopkontrId } = useParams()
-    const apolloClient = useApolloClient()
     const store = useContext(StoreContext)
-    const { tpopkontrzaehlGqlFilterForTree, nodeLabelFilter } = store.tree
+    const { nodeLabelFilter } = store.tree
 
-    const { data, isLoading, error } = useQuery(
-      createTpopkontrzaehlsQuery({
-        tpopkontrId,
-        tpopkontrzaehlGqlFilterForTree,
-        apolloClient,
-      }),
-    )
-    const tpopkontrzaehls =
-      data?.data?.tpopkontrById?.tpopkontrzaehlsByTpopkontrId?.nodes ?? []
-    const count = tpopkontrzaehls.length
-    const totalCount =
-      data?.data?.tpopkontrById?.tpopkontrzaehlsCount?.totalCount ?? 0
+    const { navData, isLoading, error } = useTpopfeldkontrzaehlsNavData()
 
     if (isLoading) return <Spinner />
 
@@ -37,8 +21,8 @@ export const Component = memo(
 
     return (
       <List
-        items={tpopkontrzaehls}
-        title={`Zählungen (${isLoading ? '...' : `${count}/${totalCount}`})`}
+        items={navData.menus}
+        title={navData.label}
         menuBar={<Menu />}
         highlightSearchString={nodeLabelFilter.tpopkontrzaehl}
       />
--- src/components/Projekte/TreeContainer/Tree/Root/Projekt/ApFolder/Aps/Folders/Pop/Pop/Folders/Tpop/Tpop/Folders/TpopFeldkontr/TpopFeldkontr/Zaehl/Zaehl.jsx
+++ src/components/Projekte/TreeContainer/Tree/Root/Projekt/ApFolder/Aps/Folders/Pop/Pop/Folders/Tpop/Tpop/Folders/TpopFeldkontr/TpopFeldkontr/Zaehl/Zaehl.jsx
@@ -1,60 +1,48 @@
-import { memo, useContext } from 'react'
-import { useQuery } from '@tanstack/react-query'
-import { useApolloClient, gql } from '@apollo/client'
-import { observer } from 'mobx-react-lite'
+import { memo } from 'react'
 
 import { Row } from '../../../../../../../../../../../../../../Row.jsx'
-import { StoreContext } from '../../../../../../../../../../../../../../../../../../storeContext.js'
-import { createTpopkontrzaehlsQuery } from '../../../../../../../../../../../../../../../../../../modules/createTpopkontrzaehlsQuery.js'
+import { useTpopfeldkontrzaehlsNavData } from '../../../../../../../../../../../../../../../../../../modules/useTpopfeldkontrzaehlsNavData.js'
 
-export const Zaehl = memo(
-  observer(({ projekt, ap, pop, tpop, tpopkontr }) => {
-    const apolloClient = useApolloClient()
-    const store = useContext(StoreContext)
-    const { tpopkontrzaehlGqlFilterForTree } = store.tree
+export const Zaehl = memo(({ projekt, ap, pop, tpop, tpopkontr }) => {
+  const { navData } = useTpopfeldkontrzaehlsNavData({
+    projId: projekt.id,
+    apId: ap.id,
+    popId: pop.id,
+    tpopId: tpop.id,
+    tpopkontrId: tpopkontr.id,
+  })
 
-    const { data } = useQuery(
-      createTpopkontrzaehlsQuery({
-        tpopkontrId: tpopkontr.id,
-        tpopkontrzaehlGqlFilterForTree,
-        apolloClient,
-      }),
-    )
+  return navData.menus.map((el) => {
+    const node = {
+      nodeType: 'table',
+      menuType: 'tpopfeldkontrzaehl',
+      id: el.id,
+      parentId: `${tpopkontr.id}TpopfeldkontrzaehlFolder`,
+      parentTableId: tpopkontr.id,
+      urlLabel: el.id,
+      label: el.label,
+      url: [
+        'Projekte',
+        projekt.id,
+        'Arten',
+        ap.id,
+        'Populationen',
+        pop.id,
+        'Teil-Populationen',
+        tpop.id,
+        'Feld-Kontrollen',
+        tpopkontr.id,
+        'Zaehlungen',
+        el.id,
+      ],
+      hasChildren: false,
+    }
 
     return (
-      data?.data?.tpopkontrById?.tpopkontrzaehlsByTpopkontrId?.nodes ?? []
-    ).map((el) => {
-      const node = {
-        nodeType: 'table',
-        menuType: 'tpopfeldkontrzaehl',
-        id: el.id,
-        parentId: `${tpopkontr.id}TpopfeldkontrzaehlFolder`,
-        parentTableId: tpopkontr.id,
-        urlLabel: el.id,
-        label: el.label,
-        url: [
-          'Projekte',
-          projekt.id,
-          'Arten',
-          ap.id,
-          'Populationen',
-          pop.id,
-          'Teil-Populationen',
-          tpop.id,
-          'Feld-Kontrollen',
-          tpopkontr.id,
-          'Zaehlungen',
-          el.id,
-        ],
-        hasChildren: false,
-      }
-
-      return (
-        <Row
-          key={el.id}
-          node={node}
-        />
-      )
-    })
-  }),
-)
+      <Row
+        key={el.id}
+        node={node}
+      />
+    )
+  })
+})
