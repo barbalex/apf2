c00c46a7b1555842a15719b614d0efc0e73e9f2e
Alexander Gabriel <alex.barbalex@gmail.com>
2024-12-02 12:23:36 +0100

    work on bookmarks

--- src/components/Projekte/Daten/Assozarts/index.jsx
+++ src/components/Projekte/Daten/Assozarts/index.jsx
@@ -1,11 +1,8 @@
 import { memo, useContext } from 'react'
-import { useApolloClient, gql } from '@apollo/client'
-import { useQuery } from '@tanstack/react-query'
 import { observer } from 'mobx-react-lite'
-import { useParams } from 'react-router'
 
 import { StoreContext } from '../../../../storeContext.js'
-import { createAssozartsQuery } from '../../../../modules/createAssozartsQuery.js'
+import { useAssozartsNavData } from '../../../../modules/useAssozartsNavData.js'
 import { List } from '../../../shared/List/index.jsx'
 import { Menu } from './Menu.jsx'
 import { Spinner } from '../../../shared/Spinner.jsx'
@@ -13,21 +10,10 @@ import { Error } from '../../../shared/Error.jsx'
 
 export const Component = memo(
   observer(() => {
-    const { apId } = useParams()
-    const apolloClient = useApolloClient()
     const store = useContext(StoreContext)
-    const { assozartGqlFilterForTree, nodeLabelFilter } = store.tree
+    const { nodeLabelFilter } = store.tree
 
-    const { data, isLoading, error } = useQuery(
-      createAssozartsQuery({
-        apId,
-        assozartGqlFilterForTree,
-        apolloClient,
-      }),
-    )
-    const assozarts = data?.data?.apById?.assozartsByApId?.nodes ?? []
-    const count = assozarts.length
-    const totalCount = data?.data?.apById?.assozartsCount?.totalCount ?? 0
+    const { navData, isLoading, error } = useAssozartsNavData()
 
     if (isLoading) return <Spinner />
 
@@ -35,8 +21,8 @@ export const Component = memo(
 
     return (
       <List
-        items={assozarts}
-        title={`Assoziierte Arten (${isLoading ? '...' : `${count}/${totalCount}`})`}
+        items={navData.menus}
+        title={navData.label}
         menuBar={<Menu />}
         highlightSearchString={nodeLabelFilter.assozart}
       />
