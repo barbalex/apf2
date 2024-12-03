ea0f1f7332b63f25c9bc3b2561c1b68f451e77cd
Alexander Gabriel <alex.barbalex@gmail.com>
2024-12-03 12:30:48 +0100

    work on menu filter

--- src/components/Bookmarks/Bookmark/Menu/index.jsx
+++ src/components/Bookmarks/Bookmark/Menu/index.jsx
@@ -1,9 +1,13 @@
 import { memo, useState, useCallback } from 'react'
 import IconButton from '@mui/material/IconButton'
 import MuiMenu from '@mui/material/Menu'
+import MenuList from '@mui/material/MenuList'
 import MenuItem from '@mui/material/MenuItem'
+import Tooltip from '@mui/material/Tooltip'
 import { BsCaretDown } from 'react-icons/bs'
+import { MdFilterAlt } from 'react-icons/md'
 import styled from '@emotion/styled'
+import { useResizeDetector } from 'react-resize-detector'
 
 import { Item } from './Item.jsx'
 
@@ -12,9 +16,30 @@ const StyledMenu = styled(MuiMenu)`
     scrollbar-width: thin !important;
   }
 `
-const MenuTitle = styled(MenuItem)`
+const MenuTitleRow = styled.div`
+  position: relative;
+  z-index: 1;
+  margin-top: -8px;
+`
+const MenuTitle = styled.div`
+  position: fixed;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: white;
+  border-bottom: 0.6666667px solid rgba(0, 0, 0, 0.12);
+  border-radius: 4px;
   font-weight: bold;
   opacity: 1 !important;
+  width: ${(props) => props.width}px;
+`
+const Title = styled.div`
+  padding-left: 16px;
+  user-select: none;
+  cursor: default;
+`
+const StyledMenuList = styled(MenuList)`
+  margin-top: 50px;
 `
 
 export const Menu = memo(({ navData }) => {
@@ -25,6 +50,13 @@ export const Menu = memo(({ navData }) => {
 
   const iconId = `${navData.id}/MenuIcon`
   const menuId = `${navData.id}/Menu`
+
+  const { width, ref } = useResizeDetector({
+    handleHeight: false,
+    refreshMode: 'debounce',
+    refreshRate: 300,
+    refreshOptions: { leading: false, trailing: true },
+  })
 
   // TODO:
   // add filter symbol right of title
@@ -49,20 +81,31 @@ export const Menu = memo(({ navData }) => {
           'aria-labelledby': iconId,
         }}
       >
-        <MenuTitle
-          divider
-          disabled
-        >
-          {navData.label}
-        </MenuTitle>
-        {navData.menus.map((menu) => (
-          <Item
-            key={menu.id}
-            menu={menu}
-            baseUrl={navData.url}
-            onClose={onClose}
-          />
-        ))}
+        <MenuTitleRow>
+          <MenuTitle width={width}>
+            <Title>{navData.label}</Title>
+            <Tooltip title="Filtern">
+              <IconButton
+                aria-label="Filtern"
+                onClick={() => {
+                  console.log('Filtern')
+                }}
+              >
+                <MdFilterAlt />
+              </IconButton>
+            </Tooltip>
+          </MenuTitle>
+        </MenuTitleRow>
+        <StyledMenuList ref={ref}>
+          {navData.menus.map((menu) => (
+            <Item
+              key={menu.id}
+              menu={menu}
+              baseUrl={navData.url}
+              onClose={onClose}
+            />
+          ))}
+        </StyledMenuList>
       </StyledMenu>
     </>
   )
