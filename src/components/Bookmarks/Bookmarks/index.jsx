09b479f12a116a8433385dfe42f43fc281c2bde0
Alexander Gabriel <alex.barbalex@gmail.com>
2024-12-04 21:03:23 +0100

    style

--- src/components/Bookmarks/Bookmarks/index.jsx
+++ src/components/Bookmarks/Bookmarks/index.jsx
@@ -13,7 +13,6 @@ const Container = styled.nav`
   fex-grow: 1;
   flex-shrink: 0;
   padding: 0 3px;
-  height: 40px;
   min-height: 40px;
   border-bottom: rgba(46, 125, 50, 0.5) solid 1px;
   overflow-x: auto;
