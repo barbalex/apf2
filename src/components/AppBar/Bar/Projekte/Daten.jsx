5ea7316b6aed081909cd0026881fc06a98df5c83
Alexander Gabriel <alex.barbalex@gmail.com>
2024-12-07 22:57:26 +0100

    remove forwardRef

--- src/components/AppBar/Bar/Projekte/Daten.jsx
+++ src/components/AppBar/Bar/Projekte/Daten.jsx
@@ -1,4 +1,4 @@
-import { memo, useCallback, forwardRef } from 'react'
+import { memo, useCallback } from 'react'
 import Button from '@mui/material/Button'
 import remove from 'lodash/remove'
 import styled from '@emotion/styled'
@@ -12,45 +12,43 @@ import { isDesktopViewAtom } from '../../../../JotaiStore/index.js'
 
 const isMobileView = window.innerWidth <= constants.mobileViewMaxWidth
 
-export const Daten = memo(
-  forwardRef(({ treeNr = '', hide = false,  }, ref) => {
-    const [projekteTabs, setProjekteTabs] = useProjekteTabs()
-    const isDaten = projekteTabs.includes(`daten${treeNr}`)
-    const isTree = projekteTabs.includes(`tree${treeNr}`)
-    
-    const [isDesktopView] = useAtom(isDesktopViewAtom)
+export const Daten = memo(({ treeNr = '', hide = false, ref }) => {
+  const [projekteTabs, setProjekteTabs] = useProjekteTabs()
+  const isDaten = projekteTabs.includes(`daten${treeNr}`)
+  const isTree = projekteTabs.includes(`tree${treeNr}`)
 
-    const onClickButton = useCallback(() => {
-      const copyOfProjekteTabs = [...projekteTabs]
-      if (isDesktopView) {
-        if (copyOfProjekteTabs.includes(`daten${treeNr}`)) {
-          remove(copyOfProjekteTabs, (el) => el === `daten${treeNr}`)
-        } else {
-          copyOfProjekteTabs.push(`daten${treeNr}`)
-        }
-        setProjekteTabs(copyOfProjekteTabs)
+  const [isDesktopView] = useAtom(isDesktopViewAtom)
+
+  const onClickButton = useCallback(() => {
+    const copyOfProjekteTabs = [...projekteTabs]
+    if (isDesktopView) {
+      if (copyOfProjekteTabs.includes(`daten${treeNr}`)) {
+        remove(copyOfProjekteTabs, (el) => el === `daten${treeNr}`)
       } else {
-        // show one tab only
-        setProjekteTabs([`daten${treeNr}`])
+        copyOfProjekteTabs.push(`daten${treeNr}`)
       }
-    }, [projekteTabs, setProjekteTabs, treeNr])
-
-    let followed = projekteTabs.includes('filter')
-    if (treeNr === '2') {
-      followed = projekteTabs.includes('filter2')
+      setProjekteTabs(copyOfProjekteTabs)
+    } else {
+      // show one tab only
+      setProjekteTabs([`daten${treeNr}`])
     }
+  }, [projekteTabs, setProjekteTabs, treeNr])
 
-    return (
-      <StyledButton
-        ref={ref}
-        variant={isDaten ? 'outlined' : 'text'}
-        preceded={isDesktopView ? isTree.toString() : 'false'}
-        followed={followed.toString()}
-        onClick={onClickButton}
-        data-id={`nav-daten${treeNr || 1}`}
-      >
-        {`Daten${treeNr === '2' ? ' 2' : ''}`}
-      </StyledButton>
-    )
-  }),
-)
+  let followed = projekteTabs.includes('filter')
+  if (treeNr === '2') {
+    followed = projekteTabs.includes('filter2')
+  }
+
+  return (
+    <StyledButton
+      ref={ref}
+      variant={isDaten ? 'outlined' : 'text'}
+      preceded={isDesktopView ? isTree.toString() : 'false'}
+      followed={followed.toString()}
+      onClick={onClickButton}
+      data-id={`nav-daten${treeNr || 1}`}
+    >
+      {`Daten${treeNr === '2' ? ' 2' : ''}`}
+    </StyledButton>
+  )
+})
