b465ff1d89529f6507627c2b2499f617f3f693a5
Alexander Gabriel <alex.barbalex@gmail.com>
2024-12-07 22:57:12 +0100

    remove forwardRef

--- src/components/Bookmarks/Bookmark/Menu/Title/FilterInput.jsx
+++ src/components/Bookmarks/Bookmark/Menu/Title/FilterInput.jsx
@@ -5,7 +5,6 @@ import {
   useCallback,
   useEffect,
   useRef,
-  forwardRef,
 } from 'react'
 import { observer } from 'mobx-react-lite'
 import { useDebouncedCallback } from 'use-debounce'
@@ -30,93 +29,91 @@ const StyledTextField = styled(TextField)`
 `
 
 export const FilterInput = memo(
-  observer(
-    forwardRef(({ width, filterInputIsVisible }, inputRef) => {
-      const store = useContext(StoreContext)
-      const { nodeLabelFilter, activeFilterTable } = store.tree
+  observer(({ width, filterInputIsVisible, ref: inputRef }) => {
+    const store = useContext(StoreContext)
+    const { nodeLabelFilter, activeFilterTable } = store.tree
 
-      const { setKey: setNodeLabelFilterKey, isFiltered: runIsFiltered } =
-        nodeLabelFilter
-      const isFiltered = runIsFiltered()
-      const showFilter = isFiltered
+    const { setKey: setNodeLabelFilterKey, isFiltered: runIsFiltered } =
+      nodeLabelFilter
+    const isFiltered = runIsFiltered()
+    const showFilter = isFiltered
 
-      const filterValue = nodeLabelFilter?.[activeFilterTable] ?? ''
-      const [value, setValue] = useState(filterValue)
-      useEffect(() => {
-        if (filterValue === value) return
-        setValue(filterValue)
-      }, [filterValue])
+    const filterValue = nodeLabelFilter?.[activeFilterTable] ?? ''
+    const [value, setValue] = useState(filterValue)
+    useEffect(() => {
+      if (filterValue === value) return
+      setValue(filterValue)
+    }, [filterValue])
 
-      const setNodeLabelFilterAfterChange = useCallback(
-        (val) => {
-          setNodeLabelFilterKey({
-            value: val,
-            key: activeFilterTable,
-          })
-        },
-        [setNodeLabelFilterKey, activeFilterTable],
-      )
-      const setNodeLabelFilterDebounced = useDebouncedCallback(
-        setNodeLabelFilterAfterChange,
-        600,
-      )
+    const setNodeLabelFilterAfterChange = useCallback(
+      (val) => {
+        setNodeLabelFilterKey({
+          value: val,
+          key: activeFilterTable,
+        })
+      },
+      [setNodeLabelFilterKey, activeFilterTable],
+    )
+    const setNodeLabelFilterDebounced = useDebouncedCallback(
+      setNodeLabelFilterAfterChange,
+      600,
+    )
 
-      const onChange = useCallback(
-        (e) => {
-          // remove some values as they can cause exceptions in regular expressions
-          const val = e.target.value.replaceAll('(', '').replaceAll(')', '')
+    const onChange = useCallback(
+      (e) => {
+        // remove some values as they can cause exceptions in regular expressions
+        const val = e.target.value.replaceAll('(', '').replaceAll(')', '')
 
-          setValue(val)
-          setNodeLabelFilterDebounced(val)
-        },
-        [setNodeLabelFilterDebounced],
-      )
+        setValue(val)
+        setNodeLabelFilterDebounced(val)
+      },
+      [setNodeLabelFilterDebounced],
+    )
 
-      const onClickEmpty = useCallback(() => {
-        setValue('')
-        setNodeLabelFilterAfterChange('')
-        setTimeout(() => inputRef?.current?.focus?.(), 0)
-      }, [setNodeLabelFilterAfterChange])
+    const onClickEmpty = useCallback(() => {
+      setValue('')
+      setNodeLabelFilterAfterChange('')
+      setTimeout(() => inputRef?.current?.focus?.(), 0)
+    }, [setNodeLabelFilterAfterChange])
 
-      // if no activeFilterTable, show nothing
-      if (!activeFilterTable) return null
+    // if no activeFilterTable, show nothing
+    if (!activeFilterTable) return null
 
-      return (
-        <Container show={filterInputIsVisible.toString()}>
-          <StyledTextField
-            inputRef={inputRef}
-            label="Filter"
-            variant="standard"
-            width={width}
-            value={value}
-            onChange={onChange}
-            spellCheck="false"
-            autoComplete="off"
-            autoCorrect="off"
-            autoCapitalize="off"
-            // autofocus leads to focus being stolen from other filter inputs
-            // autoFocus={true}
-            slotProps={{
-              input: {
-                endAdornment:
-                  showFilter ?
-                    <InputAdornment position="end">
-                      <Tooltip title="Filter entfernen">
-                        <IconButton
-                          aria-label="Filter entfernen"
-                          onClick={onClickEmpty}
-                          fontSize="small"
-                        >
-                          <FaTimes />
-                        </IconButton>
-                      </Tooltip>
-                    </InputAdornment>
-                  : null,
-              },
-            }}
-          />
-        </Container>
-      )
-    }),
-  ),
+    return (
+      <Container show={filterInputIsVisible.toString()}>
+        <StyledTextField
+          inputRef={inputRef}
+          label="Filter"
+          variant="standard"
+          width={width}
+          value={value}
+          onChange={onChange}
+          spellCheck="false"
+          autoComplete="off"
+          autoCorrect="off"
+          autoCapitalize="off"
+          // autofocus leads to focus being stolen from other filter inputs
+          // autoFocus={true}
+          slotProps={{
+            input: {
+              endAdornment:
+                showFilter ?
+                  <InputAdornment position="end">
+                    <Tooltip title="Filter entfernen">
+                      <IconButton
+                        aria-label="Filter entfernen"
+                        onClick={onClickEmpty}
+                        fontSize="small"
+                      >
+                        <FaTimes />
+                      </IconButton>
+                    </Tooltip>
+                  </InputAdornment>
+                : null,
+            },
+          }}
+        />
+      </Container>
+    )
+  }),
 )
