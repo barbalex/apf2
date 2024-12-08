4e6e7d1de7414c978a0367a7d2a5aee172450152
Alexander Gabriel <alex.barbalex@gmail.com>
2024-12-08 11:16:13 +0100

    animate tree

--- src/components/Projekte/TreeContainer/Tree/Root/Projekt/ApFolder/Aps/Folders/BeobNichtZuzuordnen/BeobNichtZuzuordnens.jsx
+++ src/components/Projekte/TreeContainer/Tree/Root/Projekt/ApFolder/Aps/Folders/BeobNichtZuzuordnen/BeobNichtZuzuordnens.jsx
@@ -1,36 +1,18 @@
 import { memo } from 'react'
 
-import { Row } from '../../../../../../Row.jsx'
 import { useBeobNichtZuzuordnensNavData } from '../../../../../../../../../../modules/useBeobNichtZuzuordnensNavData.js'
+import { BeobNichtZuzuordnen } from './BeobNichtZuzuordnen.jsx'
 
-export const BeobNichtZuzuordnens = memo(({ projekt, ap }) => {
+export const BeobNichtZuzuordnens = memo(({ projekt, ap, in: inProp }) => {
   const { navData } = useBeobNichtZuzuordnensNavData({ apId: ap.id })
 
-  return navData.menus.map((el) => {
-    const node = {
-      nodeType: 'table',
-      menuType: 'beobNichtZuzuordnen',
-      id: el.id,
-      parentId: ap.id,
-      parentTableId: ap.id,
-      urlLabel: el.id,
-      label: el.label,
-      url: [
-        'Projekte',
-        projekt.id,
-        'Arten',
-        ap.id,
-        'nicht-zuzuordnende-Beobachtungen',
-        el.id,
-      ],
-      hasChildren: false,
-    }
-
-    return (
-      <Row
-        key={el.id}
-        node={node}
-      />
-    )
-  })
+  return navData.menus.map((menu) => (
+    <BeobNichtZuzuordnen
+      key={menu.id}
+      projekt={projekt}
+      ap={ap}
+      inProp={inProp}
+      menu={menu}
+    />
+  ))
 })
--- src/components/Projekte/TreeContainer/Tree/Root/Projekt/ApFolder/Aps/Folders/BeobNichtZuzuordnen/index.jsx
+++ src/components/Projekte/TreeContainer/Tree/Root/Projekt/ApFolder/Aps/Folders/BeobNichtZuzuordnen/index.jsx
@@ -1,5 +1,6 @@
 import { memo, useContext } from 'react'
 import { observer } from 'mobx-react-lite'
+import { TransitionGroup } from 'react-transition-group'
 
 import { Row } from '../../../../../../Row.jsx'
 import { StoreContext } from '../../../../../../../../../../storeContext.js'
@@ -40,12 +41,14 @@ export const BeobNichtZuzuordnenFolder = memo(
     return (
       <>
         <Row node={node} />
-        {isOpen && (
-          <BeobNichtZuzuordnens
-            projekt={projekt}
-            ap={ap}
-          />
-        )}
+        <TransitionGroup component={null}>
+          {isOpen && (
+            <BeobNichtZuzuordnens
+              projekt={projekt}
+              ap={ap}
+            />
+          )}
+        </TransitionGroup>
       </>
     )
   }),
