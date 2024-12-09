2613682ae1205ee6159c33ae85b76446597ffdbf
Alexander Gabriel <alex.barbalex@gmail.com>
2024-12-09 16:18:19 +0100

    animate tree

---
+++ src/components/Projekte/TreeContainer/Tree/Root/Projekt/ApFolder/Aps/Folders/PopFolder/Pops/Pop.jsx
@@ -0,0 +1,65 @@
+import { memo, useContext, useRef } from 'react'
+import { observer } from 'mobx-react-lite'
+import { Transition } from 'react-transition-group'
+
+import { Row } from '../../../../../../../Row.jsx'
+import { MobxContext } from '../../../../../../../../../../../mobxContext.js'
+import { PopFolders } from './PopFolders/index.jsx'
+
+export const Pop = memo(
+  observer(({ projekt, ap, menu, inProp }) => {
+    const store = useContext(MobxContext)
+
+    const node = {
+      nodeType: 'table',
+      menuType: 'pop',
+      id: menu.id,
+      parentId: `${ap.id}PopFolder`,
+      parentTableId: ap.id,
+      urlLabel: menu.id,
+      label: menu.label,
+      status: menu.status,
+      url: ['Projekte', projekt.id, 'Arten', ap.id, 'Populationen', menu.id],
+      hasChildren: true,
+    }
+
+    const ref = useRef(null)
+
+    const isOpen =
+      store.tree.openNodes.filter(
+        (n) =>
+          n.length > 5 &&
+          n[1] === projekt.id &&
+          n[3] === ap.id &&
+          n[4] === 'Populationen' &&
+          n[5] === menu.id,
+      ).length > 0
+
+    return (
+      <Transition
+        in={inProp}
+        timeout={300}
+        mountOnEnter
+        unmountOnExit
+        nodeRef={ref}
+      >
+        {(state) => (
+          <>
+            <Row
+              node={node}
+              transitionState={state}
+              ref={ref}
+            />
+            {isOpen && (
+              <PopFolders
+                projekt={projekt}
+                ap={ap}
+                pop={menu}
+              />
+            )}
+          </>
+        )}
+      </Transition>
+    )
+  }),
+)
--- src/components/Projekte/TreeContainer/Tree/Root/Projekt/ApFolder/Aps/Folders/PopFolder/Pops/index.jsx
+++ src/components/Projekte/TreeContainer/Tree/Root/Projekt/ApFolder/Aps/Folders/PopFolder/Pops/index.jsx
@@ -5,49 +5,23 @@ import { Row } from '../../../../../../../Row.jsx'
 import { MobxContext } from '../../../../../../../../../../../mobxContext.js'
 import { PopFolders } from './PopFolders/index.jsx'
 import { usePopsNavData } from '../../../../../../../../../../../modules/usePopsNavData.js'
+import { Pop } from './Pop.jsx'
 
 export const Pops = memo(
-  observer(({ projekt, ap }) => {
+  observer(({ projekt, ap, in: inProp }) => {
     const store = useContext(MobxContext)
     const { popGqlFilterForTree } = store.tree
 
     const { navData } = usePopsNavData({ projId: projekt.id, apId: ap.id })
 
-    return navData.menus.map((menu) => {
-      const node = {
-        nodeType: 'table',
-        menuType: 'pop',
-        id: menu.id,
-        parentId: `${ap.id}PopFolder`,
-        parentTableId: ap.id,
-        urlLabel: menu.id,
-        label: menu.label,
-        status: menu.status,
-        url: ['Projekte', projekt.id, 'Arten', ap.id, 'Populationen', menu.id],
-        hasChildren: true,
-      }
-      const isOpen =
-        store.tree.openNodes.filter(
-          (n) =>
-            n.length > 5 &&
-            n[1] === projekt.id &&
-            n[3] === ap.id &&
-            n[4] === 'Populationen' &&
-            n[5] === menu.id,
-        ).length > 0
-
-      return (
-        <div key={menu.id}>
-          <Row node={node} />
-          {isOpen && (
-            <PopFolders
-              projekt={projekt}
-              ap={ap}
-              pop={menu}
-            />
-          )}
-        </div>
-      )
-    })
+    return navData.menus.map((menu) => (
+      <Pop
+        key={menu.id}
+        projekt={projekt}
+        ap={ap}
+        menu={menu}
+        inProp={inProp}
+      />
+    ))
   }),
 )
