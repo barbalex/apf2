c5340ec21fb04bac06ca7be47613273280390a07
Alexander Gabriel <alex.barbalex@gmail.com>
2024-12-14 00:22:18 +0100

    show list only on mobile

--- src/components/Projekte/Daten/Zieljahrs/index.jsx
+++ src/components/Projekte/Daten/Zieljahrs/index.jsx
@@ -1,44 +1,21 @@
-import { memo, useContext, useMemo } from 'react'
-import { useApolloClient, gql } from '@apollo/client'
-import { useQuery } from '@tanstack/react-query'
-import { observer } from 'mobx-react-lite'
-import { useParams } from 'react-router'
-import union from 'lodash/union'
+import { memo } from 'react'
 import { useAtom } from 'jotai'
 
-import { MobxContext } from '../../../../mobxContext.js'
-import { useZieljahrsNavData } from '../../../../modules/useZieljahrsNavData.js'
-import { List } from '../../../shared/List/index.jsx'
-import { Menu } from './Menu.jsx'
-import { Spinner } from '../../../shared/Spinner.jsx'
-import { Error } from '../../../shared/Error.jsx'
 import { isDesktopViewAtom } from '../../../../JotaiStore/index.js'
 import { Component as Ap } from '../Ap/index.jsx'
+import { List } from './List.jsx'
 
-export const Component = memo(
-  observer(() => {
-    const [isDesktopView] = useAtom(isDesktopViewAtom)
+export const Component = memo(() => {
+  const [isDesktopView] = useAtom(isDesktopViewAtom)
 
-    const { apId } = useParams()
-    const apolloClient = useApolloClient()
-    const store = useContext(MobxContext)
-    const { zielGqlFilterForTree, nodeLabelFilter } = store.tree
+  if (isDesktopView) return <Ap />
 
-    const { navData, isLoading, error } = useZieljahrsNavData()
-
-    if (isLoading) return <Spinner />
-
-    if (error) return <Error error={error} />
-
-    if (isDesktopView) return <Ap />
-
-    return (
-      <List
-        items={navData.menus}
-        title={navData.label}
-        menuBar={<Menu />}
-        highlightSearchString={nodeLabelFilter.ziel}
-      />
-    )
-  }),
-)
+  return (
+    <List
+      items={navData.menus}
+      title={navData.label}
+      menuBar={<Menu />}
+      highlightSearchString={nodeLabelFilter.ziel}
+    />
+  )
+})
