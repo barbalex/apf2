d5d016199f34bdfc275dd1c9a9021c85e9afb0c6
Alexander Gabriel <alex.barbalex@gmail.com>
2024-12-13 20:52:34 +0100

    show list only on mobile

---
+++ src/components/Projekte/Daten/CurrentIssues/List.jsx
@@ -0,0 +1,23 @@
+import { memo } from 'react'
+
+import { useCurrentissuesNavData } from '../../../../modules/useCurrentissuesNavData.js'
+import { List as SharedList } from '../../../shared/List/index.jsx'
+import { Spinner } from '../../../shared/Spinner.jsx'
+import { Error } from '../../../shared/Error.jsx'
+
+export const List = memo(() => {
+  const { navData, isLoading, error } = useCurrentissuesNavData()
+  const currentissues = navData?.data?.allCurrentissues?.nodes ?? []
+  const totalCount = navData?.data?.allCurrentissues?.totalCount ?? 0
+
+  if (isLoading) return <Spinner />
+
+  if (error) return <Error error={error} />
+
+  return (
+    <SharedList
+      items={navData.menus}
+      title={navData.label}
+    />
+  )
+})
--- src/components/Projekte/Daten/CurrentIssues/index.jsx
+++ src/components/Projekte/Daten/CurrentIssues/index.jsx
@@ -1,27 +1,13 @@
 import { memo } from 'react'
 import { useAtom } from 'jotai'
 
-import { useCurrentissuesNavData } from '../../../../modules/useCurrentissuesNavData.js'
-import { List } from '../../../shared/List/index.jsx'
-import { Spinner } from '../../../shared/Spinner.jsx'
-import { Error } from '../../../shared/Error.jsx'
 import { isDesktopViewAtom } from '../../../../JotaiStore/index.js'
+import { List } from './List.jsx'
 
 export const Component = memo(() => {
   const [isDesktopView] = useAtom(isDesktopViewAtom)
 
-  const { navData, isLoading, error } = useCurrentissuesNavData()
-  const currentissues = navData?.data?.allCurrentissues?.nodes ?? []
-  const totalCount = navData?.data?.allCurrentissues?.totalCount ?? 0
+  if (isDesktopView) return null
 
-  if (isLoading) return <Spinner />
-
-  if (error) return <Error error={error} />
-
-  return (
-    <List
-      items={navData.menus}
-      title={navData.label}
-    />
-  )
+  return <List />
 })
