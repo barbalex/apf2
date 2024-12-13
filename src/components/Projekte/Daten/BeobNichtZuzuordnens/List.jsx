8b29629f8b51506e9f7eaf5cc45732b2045ad54f
Alexander Gabriel <alex.barbalex@gmail.com>
2024-12-14 00:01:11 +0100

    show list only on mobile

---
+++ src/components/Projekte/Daten/BeobNichtZuzuordnens/List.jsx
@@ -0,0 +1,31 @@
+import { memo, useContext } from 'react'
+import { observer } from 'mobx-react-lite'
+
+import { MobxContext } from '../../../../mobxContext.js'
+import { useBeobNichtZuzuordnensNavData } from '../../../../modules/useBeobNichtZuzuordnensNavData.js'
+import { List as SharedList } from '../../../shared/List/index.jsx'
+import { Menu } from '../BeobNichtBeurteilts/Menu.jsx'
+import { Spinner } from '../../../shared/Spinner.jsx'
+import { Error } from '../../../shared/Error.jsx'
+
+export const List = memo(
+  observer(() => {
+    const store = useContext(MobxContext)
+    const { nodeLabelFilter } = store.tree
+
+    const { navData, isLoading, error } = useBeobNichtZuzuordnensNavData()
+
+    if (isLoading) return <Spinner />
+
+    if (error) return <Error error={error} />
+
+    return (
+      <SharedList
+        items={navData.menus}
+        title={navData.label}
+        menuBar={<Menu apfloraLayer="beobNichtZuzuordnen" />}
+        highlightSearchString={nodeLabelFilter.beob}
+      />
+    )
+  }),
+)
