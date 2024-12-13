773a581264ba3d769e80642ffbdbfe79924a6d4a
Alexander Gabriel <alex.barbalex@gmail.com>
2024-12-14 00:05:37 +0100

    show list only on mobile

---
+++ src/components/Projekte/Daten/Aparts/List.jsx
@@ -0,0 +1,31 @@
+import { memo, useContext } from 'react'
+import { observer } from 'mobx-react-lite'
+
+import { MobxContext } from '../../../../mobxContext.js'
+import { useApartsNavData } from '../../../../modules/useApartsNavData.js'
+import { List as SharedList } from '../../../shared/List/index.jsx'
+import { Menu } from './Menu.jsx'
+import { Spinner } from '../../../shared/Spinner.jsx'
+import { Error } from '../../../shared/Error.jsx'
+
+export const List = memo(
+  observer(() => {
+    const store = useContext(MobxContext)
+    const { nodeLabelFilter } = store.tree
+
+    const { navData, isLoading, error } = useApartsNavData()
+
+    if (isLoading) return <Spinner />
+
+    if (error) return <Error error={error} />
+
+    return (
+      <SharedList
+        items={navData.menus}
+        title={navData.label}
+        menuBar={<Menu />}
+        highlightSearchString={nodeLabelFilter.apart}
+      />
+    )
+  }),
+)
