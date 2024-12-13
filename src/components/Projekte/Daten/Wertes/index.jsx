29fc9d62df664a16cd88447532b505f7d9fe5307
Alexander Gabriel <alex.barbalex@gmail.com>
2024-12-13 20:50:45 +0100

    show list only on mobile

--- src/components/Projekte/Daten/Wertes/index.jsx
+++ src/components/Projekte/Daten/Wertes/index.jsx
@@ -1,25 +1,13 @@
 import { memo } from 'react'
 import { useAtom } from 'jotai'
 
-import { List } from '../../../shared/List/index.jsx'
-import { Spinner } from '../../../shared/Spinner.jsx'
-import { Error } from '../../../shared/Error.jsx'
-import { useWertesNavData } from '../../../../modules/useWertesNavData.js'
 import { isDesktopViewAtom } from '../../../../JotaiStore/index.js'
+import { List } from './List.jsx'
 
 export const Component = memo(() => {
   const [isDesktopView] = useAtom(isDesktopViewAtom)
-  
-  const { navData, isLoading, error } = useWertesNavData()
 
-  if (isLoading) return <Spinner />
+  if (isDesktopView) return null
 
-  if (error) return <Error error={error} />
-
-  return (
-    <List
-      items={navData?.menus}
-      title={navData?.label}
-    />
-  )
+  return <List />
 })
