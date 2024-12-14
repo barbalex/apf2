30b619e6644725093a323b5e35652bdae88f493a
Alexander Gabriel <alex.barbalex@gmail.com>
2024-12-14 16:37:22 +0100

    correct

--- src/components/Projekte/TreeContainer/Tree/Root/Projekt/ApFolder/Aps/Folders/PopFolder/Pops/Folders/TpopFolder/Tpops/Tpop.jsx
+++ src/components/Projekte/TreeContainer/Tree/Root/Projekt/ApFolder/Aps/Folders/PopFolder/Pops/Folders/TpopFolder/Tpops/Tpop.jsx
@@ -1,73 +1,84 @@
-import { memo, useRef } from 'react'
+import { memo, useRef, useContext } from 'react'
+import { observer, Observer } from 'mobx-react-lite'
 import { Transition, TransitionGroup } from 'react-transition-group'
 
+import { MobxContext } from '../../../../../../../../../../../../../../mobxContext.js'
 import { Row } from '../../../../../../../../../../Row.jsx'
 import { TpopFolders } from './Folders/index.jsx'
 
-export const Tpop = memo(({ projekt, ap, pop, menu, inProp }) => {
-  const isOpen =
-    store.tree.openNodes.filter(
-      (n) =>
-        n.length > 5 &&
-        n[1] === projekt.id &&
-        n[3] === ap.id &&
-        n[4] === 'Populationen' &&
-        n[5] === pop.id &&
-        n[6] === 'Teil-Populationen' &&
-        n[7] === menu.id,
-    ).length > 0
+export const Tpop = memo(
+  observer(({ projekt, ap, pop, menu, inProp }) => {
+    const store = useContext(MobxContext)
 
-  const node = {
-    nodeType: 'table',
-    menuType: 'tpop',
-    id: menu.id,
-    parentId: `${pop.id}TpopFolder`,
-    parentTableId: pop.id,
-    urlLabel: menu.id,
-    label: menu.label,
-    status: menu.status,
-    url: [
-      'Projekte',
-      projekt.id,
-      'Arten',
-      ap.id,
-      'Populationen',
-      pop.id,
-      'Teil-Populationen',
-      menu.id,
-    ],
-    hasChildren: true,
-  }
+    const isOpen =
+      store.tree.openNodes.filter(
+        (n) =>
+          n.length > 5 &&
+          n[1] === projekt.id &&
+          n[3] === ap.id &&
+          n[4] === 'Populationen' &&
+          n[5] === pop.id &&
+          n[6] === 'Teil-Populationen' &&
+          n[7] === menu.id,
+      ).length > 0
 
-  const ref = useRef(null)
+    const node = {
+      nodeType: 'table',
+      menuType: 'tpop',
+      id: menu.id,
+      parentId: `${pop.id}TpopFolder`,
+      parentTableId: pop.id,
+      urlLabel: menu.id,
+      label: menu.label,
+      status: menu.status,
+      url: [
+        'Projekte',
+        projekt.id,
+        'Arten',
+        ap.id,
+        'Populationen',
+        pop.id,
+        'Teil-Populationen',
+        menu.id,
+      ],
+      hasChildren: true,
+    }
 
-  return (
-    <Transition
-      in={inProp}
-      timeout={300}
-      mountOnEnter
-      unmountOnExit
-      nodeRef={ref}
-    >
-      {(state) => (
-        <>
-          <Row
-            node={node}
-            ref={ref}
-            transitionState={state}
-          />
-          <TransitionGroup component={null}>
-            {isOpen && (
-              <TpopFolders
-                projekt={projekt}
-                ap={ap}
-                pop={pop}
-                tpop={menu}
+    const ref = useRef(null)
+
+    console.log('Tpop', { menu, inProp, isOpen })
+
+    return (
+      <Transition
+        in={inProp}
+        timeout={300}
+        mountOnEnter
+        unmountOnExit
+        nodeRef={ref}
+      >
+        {(state) => {
+          console.log('Tpop state', state)
+          return (
+            <>
+              <Row
+                node={node}
+                ref={ref}
+                transitionState={state}
               />
-            )}
-          </TransitionGroup>
-        </>
-      )}
-    </Transition>
-  )
-})
+              <TransitionGroup component={null}>
+                {isOpen && (
+                  <TpopFolders
+                    projekt={projekt}
+                    ap={ap}
+                    pop={pop}
+                    tpop={menu}
+                  />
+                )}
+              </TransitionGroup>
+            </>
+          )
+        }}
+      </Transition>
+    )
+  }),
+)
