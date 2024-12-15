e45c863a85a4f010e8a56975f5fd87b659e74f98
Alexander Gabriel <alex.barbalex@gmail.com>
2024-12-15 22:39:43 +0100

    describe better

--- src/components/AppBar/Bar/Projekte/More/AlwaysShowBookmarks.jsx
+++ src/components/AppBar/Bar/Projekte/More/AlwaysShowBookmarks.jsx
@@ -22,7 +22,7 @@ export const AlwaysShowBookmarks = memo(() => {
 
   return (
     <Tooltip
-      title={`Bookmarks dienen der Navigation auf Mobilgeräten. Daher werden sie normalerweise nur auf schmalen Bildschirmen angezeigt (unter ${constants.mobileViewMaxWidth + 1} Pixeln)`}
+      title={`Bookmarks dienen der Navigation auf Mobilgeräten. Daher werden sie normalerweise nur auf schmalen Bildschirmen angezeigt (unter ${constants.mobileViewMaxWidth + 1} Pixeln Breite).`}
       placement={window.innerWidth > 730 ? 'left' : 'bottom'}
     >
       <StyledFormControlLabel
