bfb280301b89bf08ba33409f27b1902464fc389a
Alexander Gabriel <alex.barbalex@gmail.com>
2024-12-15 14:54:11 +0100

    work on idealbiotop

--- src/components/Projekte/TreeContainer/Tree/Root/Projekt/ApFolder/Aps/Folders/IdealbiotopFolder/index.jsx
+++ src/components/Projekte/TreeContainer/Tree/Root/Projekt/ApFolder/Aps/Folders/IdealbiotopFolder/index.jsx
@@ -1,38 +1,60 @@
-import { memo, useMemo } from 'react'
+import { memo, useMemo, useContext } from 'react'
+import { observer } from 'mobx-react-lite'
 
 import { Row } from '../../../../../../Row.jsx'
 import { ChildlessFolder } from './ChildlessFolder.jsx'
 import { useIdealbiotopNavData } from '../../../../../../../../../../modules/useIdealbiotopNavData.js'
+import { MobxContext } from '../../../../../../../../../../mobxContext.js'
 
-export const IdealbiotopFolder = memo(({ projekt, ap }) => {
-  const url = ['Projekte', projekt.id, 'Arten', ap.id, 'Idealbiotop']
+export const IdealbiotopFolder = memo(
+  observer(({ projekt, ap }) => {
+    const store = useContext(MobxContext)
+    const { openNodes } = store.tree
 
-  const { navData } = useIdealbiotopNavData({ projId: projekt.id, apId: ap.id })
+    const url = ['Projekte', projekt.id, 'Arten', ap.id, 'Idealbiotop']
 
-  const node = {
-    nodeType: 'folder',
-    menuType: 'idealbiotopFolder',
-    id: `${ap.id}IdealbiotopFolder`,
-    tableId: ap.id,
-    urlLabel: 'Idealbiotop',
-    label: 'Idealbiotop',
-    url,
-    hasChildren: false,
-  }
+    const { navData } = useIdealbiotopNavData({
+      projId: projekt.id,
+      apId: ap.id,
+    })
 
-  const dateienMenu = useMemo(
-    () => navData?.menus.find((m) => m.id === 'Dateien'),
-    [navData],
-  )
+    const node = {
+      nodeType: 'folder',
+      menuType: 'idealbiotopFolder',
+      id: `${ap.id}IdealbiotopFolder`,
+      tableId: ap.id,
+      urlLabel: 'Idealbiotop',
+      label: 'Idealbiotop',
+      url,
+      hasChildren: true,
+    }
 
-  return (
-    <>
-      <Row node={node} />
-      <ChildlessFolder
-        projekt={projekt}
-        ap={ap}
-        menu={dateienMenu}
-      />
-    </>
-  )
-})
+    const isOpen =
+      openNodes.filter(
+        (n) =>
+          n[0] === 'Projekte' &&
+          n[1] === projekt.id &&
+          n[2] === 'Arten' &&
+          n[3] === ap.id &&
+          n[4] === 'Idealbiotop',
+      ).length > 0
+
+    const dateienMenu = useMemo(
+      () => navData?.menus.find((m) => m.id === 'Dateien'),
+      [navData],
+    )
+
+    return (
+      <>
+        <Row node={node} />
+        {isOpen && (
+          <ChildlessFolder
+            projekt={projekt}
+            ap={ap}
+            menu={dateienMenu}
+          />
+        )}
+      </>
+    )
+  }),
+)
