f062ce3a5f0dc74ce71dba8ef6c484b8993e1c68
Alexander Gabriel <alex.barbalex@gmail.com>
2024-12-20 21:58:37 +0100

    style

--- src/components/Docs/MobileDoc.jsx
+++ src/components/Docs/MobileDoc.jsx
@@ -3,9 +3,13 @@ import { Outlet } from 'react-router'
 
 import { Container, Doku } from './DesktopDocs.jsx'
 
+const dokuStyle = {
+  padding: '0 10px 10px 10px',
+}
+
 export const Component = memo(() => (
   <Container>
-    <Doku>
+    <Doku stye={dokuStyle}>
       <Outlet />
     </Doku>
   </Container>
