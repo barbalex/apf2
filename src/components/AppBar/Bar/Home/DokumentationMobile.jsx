53e1a8a2093a02b2486f865069eab5813eb23d64
Alexander Gabriel <alex.barbalex@gmail.com>
2024-12-20 12:50:58 +0100

    work on mobile docs

--- src/components/AppBar/Bar/Home/DokumentationMobile.jsx
+++ src/components/AppBar/Bar/Home/DokumentationMobile.jsx
@@ -1,6 +1,7 @@
-import { memo } from 'react'
-import styled from '@emotion/styled'
+import { memo, useState } from 'react'
+import Menu from '@mui/material/Menu'
 import { useLocation, Link } from 'react-router'
+import styled from '@emotion/styled'
 
 import { StyledButton } from './index.jsx'
 
@@ -10,10 +11,20 @@ export const DokumentationMobile = memo(() => {
   const { pathname, search } = useLocation()
   const isDocs = pathname.startsWith('/Dokumentation')
 
+  const [anchorEl, setAnchorEl] = useState(null)
+  const open = Boolean(anchorEl)
+
+  const handleClick = (event) => setAnchorEl(event.currentTarget)
+  const handleClose = () => setAnchorEl(null)
+
   return (
     <>
       <StyledButton
-        id="docsbutton"
+        id="docs-button"
+        aria-controls={open ? 'docs-button' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
         variant={isDocs ? 'outlined' : 'text'}
         component={Link}
         to={`/Dokumentation/${search}`}
@@ -22,6 +33,17 @@ export const DokumentationMobile = memo(() => {
       >
         Dokumentation
       </StyledButton>
+      <Menu
+        id="docs-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'docs-button',
+        }}
+      >
+        content
+      </Menu>
     </>
   )
 })
