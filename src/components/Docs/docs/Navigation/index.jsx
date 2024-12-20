875e9b2d5e949b12c517cbded5489176fcc38e12
Alexander Gabriel <alex.barbalex@gmail.com>
2024-12-20 23:35:30 +0100

    document

--- src/components/Docs/docs/Navigation/index.jsx
+++ src/components/Docs/docs/Navigation/index.jsx
@@ -5,61 +5,69 @@ export const Component = memo(() => (
   <>
     <h1>Navigation</h1>
     <DokuDate>20.12.2024</DokuDate>
-    <h2>1. Mobil vs. Desktop</h2>
     <p>
-      Bis Ende 2024 war apflora v.a. für die Arbeit auf Desktops ausgelegt. Ende
-      2024 wurde es für die Arbeit auf Handys und Tablets erweitert.
+      Früher war apflora primär für die Arbeit auf Desktops ausgelegt. Ende 2024
+      wurde apflora für die Bedienung auf Handys und Tablets erweitert.
     </p>
     <p>
-      apflora hat eine tiefe, hierarchische Struktur. Das macht die Navigation
+      apflora hat eine tiefe hierarchische Struktur. Das macht die Navigation
       anspruchsvoll und eine gute Umsetzung wichtig. Darum gibt es eine&nbsp;
       <b>Mobil-Ansicht</b> und eine <b>Desktop-Ansicht</b> mit jeweils eigener
-      Navigation.
+      Navigation:
+    </p>
+    <br />
+    <ul>
+      <li>
+        Auf dem Desktop ist der <b>Navigationsbaum</b>&nbsp;das Werkzeug der
+        Wahl.
+      </li>
+      <li>
+        Auf Mobilgeräten gibt es <b>Bookmarks</b> und&nbsp;
+        <b>Navigationslisten</b>.
+      </li>
+    </ul>
+    <p>
+      In der Mobil-Ansicht wird grundsätzlich immer nur eine Oberfläche
+      angezeigt. Mehr (z.B. Navigationsbaum neben Formular) ist nur in der
+      Desktop-Ansicht möglich.
     </p>
     <p>
-      Auf dem Desktop ist der <b>Navigationsbaum</b>&nbsp; das Werkzeug der
-      Wahl.
+      Der Navigationsbaum enthält Kontext-Menüs. Diese sind auf primär
+      Finger-Gesteuerten Geräten deaktiviert, weil sie sich nicht für die
+      Bedienung mit Fingern eignen und die Gefahr besteht, sie ungewollt zu
+      aktivieren (v.a. bei Nässe). Neu gibt es Menüs in den Formularköpfen, die
+      sich dynamisch an die verfügbare Breite anpassen (sie sind auch auf dem
+      Desktop verfügbar).
     </p>
     <p>
-      Auf Mobilgeräten gibt es <b>Bookmarks</b> und&nbsp;
-      <b>Navigationslisten</b>. In den Bookmarks können auch Menüs eingeblendet
-      werden (Option im Mehr-Menü).
+      Mobile und Desktop sind nicht immer klar zu bestimmen. Es gibt sehr grosse
+      Bildschirme mit Touch-Steuerung. Einige Bildschirme mit Touch-Steuerung
+      werden primär mit der Maus bedient. Handys wiederum können z.T. sehr hohe
+      Auflösung haben.
     </p>
     <p>
-      Der Navigationsbaum enthält Kontext-Menüs. Diese sind in der Mobil-Ansicht
-      deaktiviert, weil sie sich nicht für die Bedienung mit Fingern eignen.
-      Stattdessen gibt es Menüs in den Formularköpfen, die sich dynamisch an die
-      verfügbare Breite anpassen (sie sind auch auf dem Desktop verfügbar).
+      Aktuell ist die Grenze zwischen Mobil- und Desktop-Navigation in apflora
+      bei einer Bildschirmbreite von 1000 Pixeln festgelegt. Wegen der unklaren
+      Grenze zwischen Mobile und Desktop gibt es im Mehr-Menü die folgenden
+      Optionen:
     </p>
+    <br />
+    <ul>
+      <li>
+        Der Navigationsbaum kann auch auf Mobilgeräten angeboten werden. Man
+        kann dann mit dem Top-Menü seine Anzeige steuern.
+      </li>
+      <li>Die Mobil-Navigation kann erzwungen werden.</li>
+      <li>
+        Die Desktop-Navigation kann erzwungen werden. In diesem Fall können auch
+        auf kleinen Bildschirmen mehrere Oberflächen nebeneinander angezeigt
+        werden.
+      </li>
+    </ul>
     <p>
-      Der Navigationsbaum <em>kann</em> auch auf Mobilgeräten eingeblendet
-      werden. Auf dem Desktop <em>kann</em> die Mobil-Navigation erzwungen
-      werden (Optionen im Mehr-Menü).
+      Die bisherigen Tabs (z.B. Auswertung, Dateien, Biotop, Historien) wurden
+      bei der Implementation der Mobil-Navigation in die normale Navigation
+      (Navigationsbaum bzw. Bookmarks & Navigationslisten) gezügelt.
     </p>
-    <p>
-      Was ein Mobilgerät ist, ist leider nicht endgültig und zufriedenstellend
-      definierbar. Es gibt sehr grosse Bildschirme mit Touch-Steuerung. Es gibt
-      Bildschirme mit Touch-Steuerung, die primär mit der Maus bedient werden.
-      Handys wiederum können z.T. sehr hohe Auflösung haben. Aktuell ist die
-      Grenze zwischen Mobil- und Desktop-Navigation in apflora bei einer
-      Bildschirmbreite von 1000 Pixeln festgelegt.
-    </p>
-    Ich habe selber kein Tablet. Bin froh um Rückmeldungen falls die aktuelle
-    Einstellung dort nicht optimal sein sollte. Seit unserer Besprechung wurde
-    implementiert: - In der Mobilansicht sind die Kontextmenüs im
-    Navigationsbaum deaktiviert. - Navigationslisten werden mobil auch bei
-    einzelnen Objekten (Art, Projekt, Teilprojekt, Feldkontrolle) anstelle des
-    Daten-Formulars angezeigt. Um ins Objekt zu gelangen gibt es einen
-    entsprechenden Eintrag in der Liste. Wenn man nicht mehr tiefer navigieren
-    kann, wird direkt das Daten-Formular geöffnet. - Die Navigation von zuoberst
-    in der Hierarchie überspringt in der Mobilansicht die Projekt-Liste, da es
-    nur ein Projekt gibt. - Die bisherigen Tabs wurden in die normale Navigation
-    (Navigationsbaum, Bookmarks & Navigationslisten) gezügelt (Desktop und
-    mobil). - In der Desktopansicht werden (wieder) grundsätzlich nur
-    Daten-Formulare angezeigt. - Bei den Bookmarks scheinen mir die Befehle an
-    deren rechten Rand wenig(er) nützlich, da jetzt immer Navigationslisten zur
-    Verfügung stehen. Ich habe sie ausbeglendet (Vorteile: übersichtlicher, mehr
-    Platz für den Text). In den Optionen gibt es einen Befehl, um sie
-    einzublenden. Sagt mir doch bitte, ob ihr meint, sie seien noch nötig.
   </>
 ))
