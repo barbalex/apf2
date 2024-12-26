4ce31701d0af3fe1920343e0859a1decba15ef06
Alexander Gabriel <alex.barbalex@gmail.com>
2024-12-26 10:33:12 +0100

    implement general table folder

--- src/components/Projekte/TreeContainer/Tree/Root/Projekt/ApFolder/Aps/Folders/ApzieljahrFolder/Zieljahr/Ziels.jsx
+++ src/components/Projekte/TreeContainer/Tree/Root/Projekt/ApFolder/Aps/Folders/ApzieljahrFolder/Zieljahr/Ziels.jsx
@@ -1,13 +1,13 @@
 import { memo } from 'react'
 
 import { useZielsOfJahrNavData } from '../../../../../../../../../../../modules/useZielsOfJahrNavData.js'
-import { Ziel } from './Ziel/index.jsx'
+import { NodeListFolderTransitioned } from '../../../../../../../NodeListFolderTransitioned.jsx'
 
 export const Ziels = memo(({ in: inProp, menu }) => {
   const { navData } = useZielsOfJahrNavData(menu.fetcherParams)
 
   return navData.menus.map((menu) => (
-    <Ziel
+    <NodeListFolderTransitioned
       key={menu.id}
       menu={menu}
       inProp={inProp}
