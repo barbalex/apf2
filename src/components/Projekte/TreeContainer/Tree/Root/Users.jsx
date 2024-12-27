c00360c4abf69ffed3fa934ea3ba795d24cab63d
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2024-12-27 22:02:13 +0100

    generalize tree

--- src/components/Projekte/TreeContainer/Tree/Root/Users.jsx
+++ src/components/Projekte/TreeContainer/Tree/Root/Users.jsx
@@ -1,18 +1,14 @@
 import { memo, useContext } from 'react'
 import { observer } from 'mobx-react-lite'
-import { TransitionGroup } from 'react-transition-group'
 
-import { Row } from '../Row.jsx'
 import { MobxContext } from '../../../../../mobxContext.js'
-import { Users } from './Users.jsx'
 import { useUsersNavData } from '../../../../../modules/useUsersNavData.js'
 import { nodeFromMenu } from '../nodeFromMenu.js'
 import { checkIfIsOpen } from '../checkIfIsOpen.js'
 import { NodeWithList } from '../NodeWithList.jsx'
-import { is } from 'date-fns/locale'
 
 export const UsersFolder = memo(
-  observer(({ count, countFiltered, isLoading, usersFilter }) => {
+  observer(() => {
     const store = useContext(MobxContext)
 
     const { navData } = useUsersNavData()
@@ -21,14 +17,5 @@ export const UsersFolder = memo(
     const isOpen = checkIfIsOpen({ store, menu: navData })
 
     return <NodeWithList menu={navData} />
-
-    return (
-      <>
-        <Row node={node} />
-        <TransitionGroup component={null}>
-          {isOpen && <Users usersFilter={usersFilter} />}
-        </TransitionGroup>
-      </>
-    )
   }),
 )
