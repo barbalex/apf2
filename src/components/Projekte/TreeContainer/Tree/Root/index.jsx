af56b8c550a5dd72fe9e0d937fcba03a4eb5ce1a
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2024-12-28 18:22:47 +0100

    generalize tree

--- src/components/Projekte/TreeContainer/Tree/Root/index.jsx
+++ src/components/Projekte/TreeContainer/Tree/Root/index.jsx
@@ -1,6 +1,4 @@
 import { memo, useContext } from 'react'
-import { gql, useApolloClient } from '@apollo/client'
-import { useQuery } from '@tanstack/react-query'
 import { observer } from 'mobx-react-lite'
 import { jwtDecode } from 'jwt-decode'
 
@@ -16,99 +14,25 @@ import { useWertesNavData } from '../../../../../modules/useWertesNavData.js'
 
 export const Root = memo(
   observer(() => {
-    const client = useApolloClient()
+    
 
     const store = useContext(MobxContext)
-    const { projectIsOpen, nodeLabelFilter, apGqlFilterForTree } = store.tree
     const token = store.user?.token
     const role = token ? jwtDecode(token).role : null
 
-    const apberuebersichtsFilter = {
-      projId: { in: ['e57f56f4-4376-11e8-ab21-4314b6749d13'] },
-    }
-    if (nodeLabelFilter.apberuebersicht) {
-      apberuebersichtsFilter.label = {
-        includesInsensitive: nodeLabelFilter.apberuebersicht,
-      }
-    }
-    const apsFilter = apGqlFilterForTree
 
-    const { data, isLoading } = useQuery({
-      queryKey: [
-        'treeRoot',
-        projectIsOpen,
-        apsFilter,
-        nodeLabelFilter.apberuebersicht,
-      ],
-      // This query is re-run under certain circumstances
-      // when focus was out of app and comes back in
-      // EVENT THOUGH component is not re-rendered
-      // Seems that was a feature of tanstack-query:
-      // https://tanstack.com/query/latest/docs/react/guides/window-focus-refetching
-      queryFn: () =>
-        client.query({
-          query: gql`
-            query TreeRootQuery(
-              $apsFilter: ApFilter!
-              $apberuebersichtsFilter: ApberuebersichtFilter!
-              $projectIsOpen: Boolean!
-            ) {
-              allProjekts {
-                nodes {
-                  id
-                  label
-                  apberuebersichtsByProjId @include(if: $projectIsOpen) {
-                    totalCount
-                  }
-                  filteredApberuebersichts: apberuebersichtsByProjId(
-                    filter: $apberuebersichtsFilter
-                  ) @include(if: $projectIsOpen) {
-                    totalCount
-                  }
-                  apsByProjId @include(if: $projectIsOpen) {
-                    totalCount
-                  }
-                  apsFiltered: apsByProjId(filter: $apsFilter)
-                    @include(if: $projectIsOpen) {
-                    totalCount
-                  }
-                }
-              }
-            }
-          `,
-          variables: {
-            apsFilter,
-            apberuebersichtsFilter,
-            projectIsOpen,
-          },
-          fetchPolicy: 'no-cache',
-        }),
-    })
 
-    // console.log('TreeComponents rendering', {
-    //   data,
-    //   apsFilter,
-    //   apberuebersichtsFilter,
-    //   projectIsOpen,
-    //   nodeLabelFilter,
-    // })
-
-    //console.log('Tree, height:', { height, initialTopMostIndex })
-
-    if (!data) return null
 
     return (
       <>
-        <Projekt
+        {/* <Projekt
           projekt={data?.data?.allProjekts?.nodes?.[0]}
           isLoading={isLoading}
           projectIsOpen={projectIsOpen}
-        />
+        /> */}
         {/* TODO: renders twice, on second render fetcher is undefined. IMPOSSIBLE */}
         <RootNode fetcher={useProjektNavData} />
         <RootNode fetcher={useUsersNavData} />
-        {/* {role === 'apflora_manager' && <WerteFolder />} */}
-        {/* TODO: renders twice, on second render fetcher is undefined. IMPOSSIBLE */}
         {role === 'apflora_manager' && <RootNode fetcher={useWertesNavData} />}
         <RootNode fetcher={useMessagesNavData} />
         <RootNode fetcher={useCurrentissuesNavData} />
