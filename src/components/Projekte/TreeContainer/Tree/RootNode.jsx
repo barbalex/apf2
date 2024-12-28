97a587a54cad95cc32c35224ae97cc7c1beb4869
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2024-12-28 18:02:59 +0100

    refactor: simplify navData extraction in RootNode component

--- src/components/Projekte/TreeContainer/Tree/RootNode.jsx
+++ src/components/Projekte/TreeContainer/Tree/RootNode.jsx
@@ -1,7 +1,8 @@
 import { memo } from 'react'
 
 export const RootNode = memo(({ fetcher }) => {
-  const { navData } = fetcher?.() ?? {}
+  const res = fetcher?.()
+  const navData = res?.navData
   console.log('RootNode', { fetcher, navData })
 
   if (!navData) return null
