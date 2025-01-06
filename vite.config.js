28a02b7ac7cb3fd2db746a10841c203e1965e0fd
Alexander Gabriel <alex.barbalex@gmail.com>
2025-01-06 15:33:02 +0100

    remove plugin-react-swc

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 import svgrPlugin from 'vite-plugin-svgr'
