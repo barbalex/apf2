57b879b66f0ddf3c934954f24b5b4c177417c95f
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2025-02-09 22:13:41 +0100

    work on loading data per row

---
+++ src/components/EkPlan/Table/Row/Visible.jsx
@@ -0,0 +1,116 @@
+import { memo, useRef, useContext } from 'react'
+import styled from '@emotion/styled'
+import { gql, useQuery } from '@apollo/client'
+import { observer } from 'mobx-react-lite'
+
+import { MobxContext } from '../../../../mobxContext.js'
+import { yearColumnWidth } from './yearColumnWidth.js'
+import { CellForYearTitle } from '../CellForYearTitle.jsx'
+import { CellForEkfrequenz } from '../CellForEkfrequenz/index.jsx'
+import { CellForEkfrequenzStartjahr } from '../CellForEkfrequenzStartjahr/index.jsx'
+import { CellForEkfrequenzAbweichend } from '../CellForEkfrequenzAbweichend.jsx'
+import { CellForTpopLink } from '../CellForTpopLink.jsx'
+import { CellForValue } from '../CellForValue.jsx'
+import { CellForYear } from '../CellForYear/index.jsx'
+import { ErrorBoundary } from '../../../shared/ErrorBoundary.jsx'
+import { queryRow } from './queryRow.js'
+import { tpopRowFromTpop } from './tpopRowFromTpop.js'
+
+export const Visible = memo(
+  observer(({ tpop, index, setProcessing, years }) => {
+    const store = useContext(MobxContext)
+
+    // const { data, loading, error } = useQuery(queryRow, {
+    //   variables: { apIds: store.ekPlan.apValues, tpopId: tpop.id },
+    // })
+    // console.log('Row', { data, loading, error })
+    const { row, tpopColumns } = tpopRowFromTpop({ tpop, index, years, store })
+
+    return (
+      <ErrorBoundary>
+        {tpopColumns.map((tpopColumn, columnIndex) => {
+          const value = row[tpopColumn.name]
+          const column = tpopColumn.name
+          const width = tpopColumn.width
+
+          if (value.name === 'yearTitle') {
+            return <CellForYearTitle key={value.name} row={row} width={width} />
+          }
+          if (value.name === 'ekAbrechnungstyp') {
+            return (
+              <CellForValue
+                key={value.name}
+                field={value}
+                row={row}
+                firstChild={columnIndex === 0}
+                width={width}
+              />
+            )
+          }
+          if (value.name === 'ekfrequenz') {
+            return (
+              <CellForEkfrequenz
+                key={value.name}
+                row={row}
+                field={value}
+                setProcessing={setProcessing}
+                width={width}
+              />
+            )
+          }
+          if (value.name === 'ekfrequenzStartjahr') {
+            return (
+              <CellForEkfrequenzStartjahr
+                key={value.name}
+                row={row}
+                setProcessing={setProcessing}
+                width={width}
+              />
+            )
+          }
+          if (value.name === 'ekfrequenzAbweichend') {
+            return (
+              <CellForEkfrequenzAbweichend
+                key={value.name}
+                row={row}
+                field={value}
+                width={width}
+              />
+            )
+          }
+          if (value.name === 'link') {
+            return (
+              <CellForTpopLink
+                key={value.name}
+                field={value}
+                row={row}
+                width={width}
+              />
+            )
+          }
+          return (
+            <CellForValue
+              key={value.label}
+              field={value}
+              row={row}
+              firstChild={columnIndex === 0}
+              width={width}
+            />
+          )
+        })}
+        {years.map((year, columnIndex) => {
+          const value = row[year]
+
+          return (
+            <CellForYear
+              key={value.name}
+              row={row}
+              field={value}
+              width={yearColumnWidth}
+            />
+          )
+        })}
+      </ErrorBoundary>
+    )
+  }),
+)
--- src/components/EkPlan/Table/Row/index.jsx
+++ src/components/EkPlan/Table/Row/index.jsx
@@ -1,22 +1,9 @@
-import { memo, useRef, useContext } from 'react'
+import { memo, useRef } from 'react'
 import styled from '@emotion/styled'
-import Button from '@mui/material/Button'
-import { gql, useQuery } from '@apollo/client'
-import { observer } from 'mobx-react-lite'
 
-import { MobxContext } from '../../../../mobxContext.js'
-import { yearColumnWidth } from './yearColumnWidth.js'
-import { CellForYearTitle } from '../CellForYearTitle.jsx'
-import { CellForEkfrequenz } from '../CellForEkfrequenz/index.jsx'
-import { CellForEkfrequenzStartjahr } from '../CellForEkfrequenzStartjahr/index.jsx'
-import { CellForEkfrequenzAbweichend } from '../CellForEkfrequenzAbweichend.jsx'
-import { CellForTpopLink } from '../CellForTpopLink.jsx'
-import { CellForValue } from '../CellForValue.jsx'
-import { CellForYear } from '../CellForYear/index.jsx'
 import { ErrorBoundary } from '../../../shared/ErrorBoundary.jsx'
 import { useOnScreen } from '../../../../modules/useOnScreen.js'
-import { queryRow } from './queryRow.js'
-import { tpopRowFromTpop } from './tpopRowFromTpop.js'
+import { Visible } from './Visible.jsx'
 
 const RowContainer = styled.div`
   display: flex;
@@ -24,109 +11,22 @@ const RowContainer = styled.div`
   min-height: 60px;
 `
 
-export const TpopRow = memo(
-  observer(({ tpop, index, setProcessing, years }) => {
-    const store = useContext(MobxContext)
-    const ref = useRef(null)
-    const isVisible = useOnScreen(ref)
+export const TpopRow = memo(({ tpop, index, setProcessing, years }) => {
+  const ref = useRef(null)
+  const isVisible = useOnScreen(ref)
 
-    // const { data, loading, error } = useQuery(queryRow, {
-    //   variables: { apIds: store.ekPlan.apValues, tpopId: tpop.id },
-    // })
-    // console.log('Row', { data, loading, error })
-    const { row, tpopColumns } = tpopRowFromTpop({ tpop, index, years, store })
-
-    return (
-      <ErrorBoundary>
-        <RowContainer ref={ref}>
-          {isVisible &&
-            tpopColumns.map((tpopColumn, columnIndex) => {
-              const value = row[tpopColumn.name]
-              const column = tpopColumn.name
-              const width = tpopColumn.width
-
-              if (value.name === 'yearTitle') {
-                return (
-                  <CellForYearTitle key={value.name} row={row} width={width} />
-                )
-              }
-              if (value.name === 'ekAbrechnungstyp') {
-                return (
-                  <CellForValue
-                    key={value.name}
-                    field={value}
-                    row={row}
-                    firstChild={columnIndex === 0}
-                    width={width}
-                  />
-                )
-              }
-              if (value.name === 'ekfrequenz') {
-                return (
-                  <CellForEkfrequenz
-                    key={value.name}
-                    row={row}
-                    field={value}
-                    setProcessing={setProcessing}
-                    width={width}
-                  />
-                )
-              }
-              if (value.name === 'ekfrequenzStartjahr') {
-                return (
-                  <CellForEkfrequenzStartjahr
-                    key={value.name}
-                    row={row}
-                    setProcessing={setProcessing}
-                    width={width}
-                  />
-                )
-              }
-              if (value.name === 'ekfrequenzAbweichend') {
-                return (
-                  <CellForEkfrequenzAbweichend
-                    key={value.name}
-                    row={row}
-                    field={value}
-                    width={width}
-                  />
-                )
-              }
-              if (value.name === 'link') {
-                return (
-                  <CellForTpopLink
-                    key={value.name}
-                    field={value}
-                    row={row}
-                    width={width}
-                  />
-                )
-              }
-              return (
-                <CellForValue
-                  key={value.label}
-                  field={value}
-                  row={row}
-                  firstChild={columnIndex === 0}
-                  width={width}
-                />
-              )
-            })}
-          {isVisible &&
-            years.map((year, columnIndex) => {
-              const value = row[year]
-
-              return (
-                <CellForYear
-                  key={value.name}
-                  row={row}
-                  field={value}
-                  width={yearColumnWidth}
-                />
-              )
-            })}
-        </RowContainer>
-      </ErrorBoundary>
-    )
-  }),
-)
+  return (
+    <ErrorBoundary>
+      <RowContainer ref={ref}>
+        {isVisible && (
+          <Visible
+            tpop={tpop}
+            index={index}
+            setProcessing={setProcessing}
+            years={years}
+          />
+        )}
+      </RowContainer>
+    </ErrorBoundary>
+  )
+})
