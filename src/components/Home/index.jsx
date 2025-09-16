2c0b666a9704004faf72422885d5c43f6830fb10
Alexander Gabriel <alex.barbalex@gmail.com>
2025-09-16 18:24:56 +0200

    optimize

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -114,6 +114,7 @@ export const Component = () => (
         srcSet="home_700.webp 700w, home_1000.webp 1000w, home_1400.webp 1400w, home_2000.webp 2000w, home_2500.webp 2500w"
         sizes="100vw"
         alt="Spinnen-Ragwurz"
+        fetchPriority="high"
       />
     </picture>
     <ScrollContainer>
