9bcb89572e39205fdeaa269532d6670b8fdebb1f
Alexander Gabriel <alex.barbalex@gmail.com>
2025-09-25 13:40:53 +0200

    rename

--- src/components/Projekte/Exporte/Populationen/LetzterPopBericht.jsx
+++ src/components/Projekte/Exporte/Populationen/LetzterPopBericht.jsx
@@ -11,9 +11,10 @@ import { DownloadCardButton, StyledProgressText } from '../index.jsx'
 
 export const LetzterPopBericht = memo(
   observer(() => {
-    const client = useApolloClient()
     const store = useContext(MobxContext)
     const { enqueNotification } = store
+
+    const apolloClient = useApolloClient()
 
     const [queryState, setQueryState] = useState()
 
@@ -25,7 +26,7 @@ export const LetzterPopBericht = memo(
           setQueryState('lade Daten...')
           let result
           try {
-            result = await client.query({
+            result = await apolloClient.query({
               query: gql`
                 query popMitLetzterPopbersQuery {
                   allPops(filter: { vPopMitLetzterPopbersByPopIdExist: true }) {
