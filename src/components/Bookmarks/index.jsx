bfd704a8cdde2d98629082ec1a0208152eed4bf5
Alexander Gabriel <alex.barbalex@gmail.com>
2025-10-24 15:38:24 +0200

    de-memoize

--- src/components/Bookmarks/index.jsx
+++ src/components/Bookmarks/index.jsx
@@ -1,6 +1,6 @@
 import { useMatches, useLocation } from 'react-router'
 
-import { NavTo } from './NavTo/index.jsx'
+// import { NavTo } from './NavTo/index.jsx'
 import { Bookmarks as Bookmarkss } from './Bookmarks/index.jsx'
 
 // this file is bypassed for now
