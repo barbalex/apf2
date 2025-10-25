fe878ca96e966a51ea66824716e401f431e38cea
Alexander Gabriel <alex.barbalex@gmail.com>
2025-10-25 17:17:48 +0200

    de-memoize

--- src/components/Projekte/Exporte/Teilpopulationen/TPopOhneApberRelevant.jsx
+++ src/components/Projekte/Exporte/Teilpopulationen/TPopOhneApberRelevant.jsx
@@ -1,4 +1,4 @@
-import { memo, useContext, useState } from 'react'
+import { useContext, useState } from 'react'
 import { observer } from 'mobx-react-lite'
 import { gql } from '@apollo/client'
 
@@ -8,75 +8,73 @@ import { exportModule } from '../../../../modules/export.js'
 import { MobxContext } from '../../../../mobxContext.js'
 import { DownloadCardButton, StyledProgressText } from '../index.jsx'
 
-export const TPopOhneApberRelevant = memo(
-  observer(() => {
-    const store = useContext(MobxContext)
-    const { enqueNotification } = store
+export const TPopOhneApberRelevant = observer(() => {
+  const store = useContext(MobxContext)
+  const { enqueNotification } = store
 
-    const apolloClient = useApolloClient()
+  const apolloClient = useApolloClient()
 
-    const [queryState, setQueryState] = useState()
+  const [queryState, setQueryState] = useState()
 
-    return (
-      <DownloadCardButton
-        color="inherit"
-        disabled={!!queryState}
-        onClick={async () => {
-          setQueryState('lade Daten...')
-          let result
-          try {
-            result = await apolloClient.query({
-              query: gql`
-                query viewTpopOhneapberichtrelevants {
-                  allVTpopOhneapberichtrelevants {
-                    nodes {
-                      artname
-                      pop_nr: popNr
-                      pop_name: popName
-                      id
-                      nr
-                      gemeinde
-                      flurname
-                      apber_relevant: apberRelevant
-                      apber_relevant_grund: apberRelevantGrund
-                      lv95X: x
-                      lv95Y: y
-                    }
+  return (
+    <DownloadCardButton
+      color="inherit"
+      disabled={!!queryState}
+      onClick={async () => {
+        setQueryState('lade Daten...')
+        let result
+        try {
+          result = await apolloClient.query({
+            query: gql`
+              query viewTpopOhneapberichtrelevants {
+                allVTpopOhneapberichtrelevants {
+                  nodes {
+                    artname
+                    pop_nr: popNr
+                    pop_name: popName
+                    id
+                    nr
+                    gemeinde
+                    flurname
+                    apber_relevant: apberRelevant
+                    apber_relevant_grund: apberRelevantGrund
+                    lv95X: x
+                    lv95Y: y
                   }
                 }
-              `,
-            })
-          } catch (error) {
-            enqueNotification({
-              message: error.message,
-              options: { variant: 'error' },
-            })
-          }
-          setQueryState('verarbeite...')
-          const rows = result.data?.allVTpopOhneapberichtrelevants?.nodes ?? []
-          if (rows.length === 0) {
-            setQueryState(undefined)
-            return enqueNotification({
-              message: 'Die Abfrage retournierte 0 Datensätze',
-              options: {
-                variant: 'warning',
-              },
-            })
-          }
-          exportModule({
-            data: rows,
-            fileName: 'TeilpopulationenOhneApBerichtRelevant',
-            store,
-            apolloClient,
+              }
+            `,
           })
+        } catch (error) {
+          enqueNotification({
+            message: error.message,
+            options: { variant: 'error' },
+          })
+        }
+        setQueryState('verarbeite...')
+        const rows = result.data?.allVTpopOhneapberichtrelevants?.nodes ?? []
+        if (rows.length === 0) {
           setQueryState(undefined)
-        }}
-      >
-        {'Teilpopulationen ohne Eintrag im Feld "Für AP-Bericht relevant"'}
-        {queryState ?
-          <StyledProgressText>{queryState}</StyledProgressText>
-        : null}
-      </DownloadCardButton>
-    )
-  }),
-)
+          return enqueNotification({
+            message: 'Die Abfrage retournierte 0 Datensätze',
+            options: {
+              variant: 'warning',
+            },
+          })
+        }
+        exportModule({
+          data: rows,
+          fileName: 'TeilpopulationenOhneApBerichtRelevant',
+          store,
+          apolloClient,
+        })
+        setQueryState(undefined)
+      }}
+    >
+      {'Teilpopulationen ohne Eintrag im Feld "Für AP-Bericht relevant"'}
+      {queryState ?
+        <StyledProgressText>{queryState}</StyledProgressText>
+      : null}
+    </DownloadCardButton>
+  )
+})
--- src/components/Projekte/Exporte/Teilpopulationen/TPopOhneBekanntSeit.jsx
+++ src/components/Projekte/Exporte/Teilpopulationen/TPopOhneBekanntSeit.jsx
@@ -1,4 +1,4 @@
-import { memo, useContext, useState } from 'react'
+import { useContext, useState } from 'react'
 import { observer } from 'mobx-react-lite'
 import { gql } from '@apollo/client'
 
@@ -8,75 +8,73 @@ import { exportModule } from '../../../../modules/export.js'
 import { MobxContext } from '../../../../mobxContext.js'
 import { DownloadCardButton, StyledProgressText } from '../index.jsx'
 
-export const TPopOhneBekanntSeit = memo(
-  observer(() => {
-    const store = useContext(MobxContext)
-    const { enqueNotification } = store
+export const TPopOhneBekanntSeit = observer(() => {
+  const store = useContext(MobxContext)
+  const { enqueNotification } = store
 
-    const apolloClient = useApolloClient()
+  const apolloClient = useApolloClient()
 
-    const [queryState, setQueryState] = useState()
+  const [queryState, setQueryState] = useState()
 
-    return (
-      <DownloadCardButton
-        color="inherit"
-        disabled={!!queryState}
-        onClick={async () => {
-          setQueryState('lade Daten...')
-          let result
-          try {
-            result = await apolloClient.query({
-              query: gql`
-                query viewTpopOhnebekanntseits {
-                  allVTpopOhnebekanntseits {
-                    nodes {
-                      artname
-                      ap_bearbeitung: apBearbeitung
-                      pop_nr: popNr
-                      pop_name: popName
-                      id
-                      nr
-                      gemeinde
-                      flurname
-                      bekannt_seit: bekanntSeit
-                      lv95X: x
-                      lv95Y: y
-                    }
+  return (
+    <DownloadCardButton
+      color="inherit"
+      disabled={!!queryState}
+      onClick={async () => {
+        setQueryState('lade Daten...')
+        let result
+        try {
+          result = await apolloClient.query({
+            query: gql`
+              query viewTpopOhnebekanntseits {
+                allVTpopOhnebekanntseits {
+                  nodes {
+                    artname
+                    ap_bearbeitung: apBearbeitung
+                    pop_nr: popNr
+                    pop_name: popName
+                    id
+                    nr
+                    gemeinde
+                    flurname
+                    bekannt_seit: bekanntSeit
+                    lv95X: x
+                    lv95Y: y
                   }
                 }
-              `,
-            })
-          } catch (error) {
-            enqueNotification({
-              message: error.message,
-              options: { variant: 'error' },
-            })
-          }
-          setQueryState('verarbeite...')
-          const rows = result.data?.allVTpopOhnebekanntseits?.nodes ?? []
-          if (rows.length === 0) {
-            setQueryState(undefined)
-            return enqueNotification({
-              message: 'Die Abfrage retournierte 0 Datensätze',
-              options: {
-                variant: 'warning',
-              },
-            })
-          }
-          exportModule({
-            data: rows,
-            fileName: 'TeilpopulationenVonApArtenOhneBekanntSeit',
-            store,
-            apolloClient,
+              }
+            `,
           })
+        } catch (error) {
+          enqueNotification({
+            message: error.message,
+            options: { variant: 'error' },
+          })
+        }
+        setQueryState('verarbeite...')
+        const rows = result.data?.allVTpopOhnebekanntseits?.nodes ?? []
+        if (rows.length === 0) {
           setQueryState(undefined)
-        }}
-      >
-        {'Teilpopulationen von AP-Arten ohne "Bekannt seit"'}
-        {queryState ?
-          <StyledProgressText>{queryState}</StyledProgressText>
-        : null}
-      </DownloadCardButton>
-    )
-  }),
-)
+          return enqueNotification({
+            message: 'Die Abfrage retournierte 0 Datensätze',
+            options: {
+              variant: 'warning',
+            },
+          })
+        }
+        exportModule({
+          data: rows,
+          fileName: 'TeilpopulationenVonApArtenOhneBekanntSeit',
+          store,
+          apolloClient,
+        })
+        setQueryState(undefined)
+      }}
+    >
+      {'Teilpopulationen von AP-Arten ohne "Bekannt seit"'}
+      {queryState ?
+        <StyledProgressText>{queryState}</StyledProgressText>
+      : null}
+    </DownloadCardButton>
+  )
+})
