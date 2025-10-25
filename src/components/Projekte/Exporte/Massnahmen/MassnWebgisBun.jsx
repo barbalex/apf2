1ed6333537ecb67265b0c78e065a51909e13d6ae
Alexander Gabriel <alex.barbalex@gmail.com>
2025-10-25 17:11:05 +0200

    de-memoize

--- src/components/Projekte/Exporte/Massnahmen/MassnWebgisBun.jsx
+++ src/components/Projekte/Exporte/Massnahmen/MassnWebgisBun.jsx
@@ -1,4 +1,4 @@
-import { memo, useContext, useState } from 'react'
+import { useContext, useState } from 'react'
 import { observer } from 'mobx-react-lite'
 import { gql } from '@apollo/client'
 
@@ -8,101 +8,99 @@ import { exportModule } from '../../../../modules/export.js'
 import { MobxContext } from '../../../../mobxContext.js'
 import { DownloadCardButton, StyledProgressText } from '../index.jsx'
 
-export const MassnWebgisBun = memo(
-  observer(() => {
-    const store = useContext(MobxContext)
-    const { enqueNotification } = store
+export const MassnWebgisBun = observer(() => {
+  const store = useContext(MobxContext)
+  const { enqueNotification } = store
 
-    const apolloClient = useApolloClient()
+  const apolloClient = useApolloClient()
 
-    const [queryState, setQueryState] = useState()
+  const [queryState, setQueryState] = useState()
 
-    return (
-      <DownloadCardButton
-        color="inherit"
-        disabled={!!queryState}
-        onClick={async () => {
-          setQueryState('lade Daten...')
-          let result
-          try {
-            result = await apolloClient.query({
-              query: gql`
-                query viewMassnWebgisbuns {
-                  allVMassnWebgisbuns {
-                    nodes {
-                      APARTID: apartid
-                      APART: apart
-                      POPGUID: popguid
-                      POPNR: popnr
-                      TPOPGUID: tpopguid
-                      TPOPNR: tpopnr
-                      TPOP_X: tpopX
-                      TPOP_Y: tpopY
-                      TPOPSTATUS: tpopstatus
-                      tpopapberrelevant: tPopApberRelevant
-                      tpopapberrelevantgrund: tPopApberRelevantGrund
-                      MASSNGUID: massnguid
-                      MASSNJAHR: massnjahr
-                      MASSNDAT: massndat
-                      MASSTYP: masstyp
-                      MASSNMASSNAHME: massnmassnahme
-                      MASSNBEARBEITER: massnbearbeiter
-                      MASSNBEMERKUNG: massnbemerkung
-                      MASSNPLAN: massnplan
-                      MASSPLANBEZ: massplanbez
-                      MASSNFLAECHE: massnflaeche
-                      MASSNFORMANSIEDL: massnformansiedl
-                      MASSNPFLANZANORDNUNG: massnpflanzanordnung
-                      MASSNMARKIERUNG: massnmarkierung
-                      MASSNANZTRIEBE: massnanztriebe
-                      MASSNANZPFLANZEN: massnanzpflanzen
-                      MASSNANZPFLANZSTELLEN: massnanzpflanzstellen
-                      MASSNZIELEINHEITEINHEIT: massnzieleinheiteinheit
-                      MASSNZIELEINHEITANZAHL: massnzieleinheitanzahl
-                      MASSNWIRTSPFLANZEN: massnwirtspflanzen
-                      MASSNHERKUNFTSPOP: massnherkunftspop
-                      MASSNSAMMELDAT: massnsammeldat
-                      MASSNVONANZAHLINDIVIDUEN: massnvonanzahlindividuen
-                      MASSNCHANGEDAT: massnchangedat
-                      MASSNCHANGEBY: massnchangeby
-                    }
+  return (
+    <DownloadCardButton
+      color="inherit"
+      disabled={!!queryState}
+      onClick={async () => {
+        setQueryState('lade Daten...')
+        let result
+        try {
+          result = await apolloClient.query({
+            query: gql`
+              query viewMassnWebgisbuns {
+                allVMassnWebgisbuns {
+                  nodes {
+                    APARTID: apartid
+                    APART: apart
+                    POPGUID: popguid
+                    POPNR: popnr
+                    TPOPGUID: tpopguid
+                    TPOPNR: tpopnr
+                    TPOP_X: tpopX
+                    TPOP_Y: tpopY
+                    TPOPSTATUS: tpopstatus
+                    tpopapberrelevant: tPopApberRelevant
+                    tpopapberrelevantgrund: tPopApberRelevantGrund
+                    MASSNGUID: massnguid
+                    MASSNJAHR: massnjahr
+                    MASSNDAT: massndat
+                    MASSTYP: masstyp
+                    MASSNMASSNAHME: massnmassnahme
+                    MASSNBEARBEITER: massnbearbeiter
+                    MASSNBEMERKUNG: massnbemerkung
+                    MASSNPLAN: massnplan
+                    MASSPLANBEZ: massplanbez
+                    MASSNFLAECHE: massnflaeche
+                    MASSNFORMANSIEDL: massnformansiedl
+                    MASSNPFLANZANORDNUNG: massnpflanzanordnung
+                    MASSNMARKIERUNG: massnmarkierung
+                    MASSNANZTRIEBE: massnanztriebe
+                    MASSNANZPFLANZEN: massnanzpflanzen
+                    MASSNANZPFLANZSTELLEN: massnanzpflanzstellen
+                    MASSNZIELEINHEITEINHEIT: massnzieleinheiteinheit
+                    MASSNZIELEINHEITANZAHL: massnzieleinheitanzahl
+                    MASSNWIRTSPFLANZEN: massnwirtspflanzen
+                    MASSNHERKUNFTSPOP: massnherkunftspop
+                    MASSNSAMMELDAT: massnsammeldat
+                    MASSNVONANZAHLINDIVIDUEN: massnvonanzahlindividuen
+                    MASSNCHANGEDAT: massnchangedat
+                    MASSNCHANGEBY: massnchangeby
                   }
                 }
-              `,
-            })
-          } catch (error) {
-            enqueNotification({
-              message: error.message,
-              options: {
-                variant: 'error',
-              },
-            })
-          }
-          setQueryState('verarbeite...')
-          const rows = result.data?.allVMassnWebgisbuns.nodes ?? []
-          if (rows.length === 0) {
-            setQueryState(undefined)
-            return enqueNotification({
-              message: 'Die Abfrage retournierte 0 Datensätze',
-              options: {
-                variant: 'warning',
-              },
-            })
-          }
-          exportModule({
-            data: rows,
-            fileName: 'MassnahmenWebGisBun',
-            store,
-            apolloClient,
+              }
+            `,
           })
+        } catch (error) {
+          enqueNotification({
+            message: error.message,
+            options: {
+              variant: 'error',
+            },
+          })
+        }
+        setQueryState('verarbeite...')
+        const rows = result.data?.allVMassnWebgisbuns.nodes ?? []
+        if (rows.length === 0) {
           setQueryState(undefined)
-        }}
-      >
-        Massnahmen für WebGIS BUN
-        {queryState ?
-          <StyledProgressText>{queryState}</StyledProgressText>
-        : null}
-      </DownloadCardButton>
-    )
-  }),
-)
+          return enqueNotification({
+            message: 'Die Abfrage retournierte 0 Datensätze',
+            options: {
+              variant: 'warning',
+            },
+          })
+        }
+        exportModule({
+          data: rows,
+          fileName: 'MassnahmenWebGisBun',
+          store,
+          apolloClient,
+        })
+        setQueryState(undefined)
+      }}
+    >
+      Massnahmen für WebGIS BUN
+      {queryState ?
+        <StyledProgressText>{queryState}</StyledProgressText>
+      : null}
+    </DownloadCardButton>
+  )
+})
--- src/components/Projekte/Exporte/Populationen/AnzKontrProPop.jsx
+++ src/components/Projekte/Exporte/Populationen/AnzKontrProPop.jsx
@@ -1,4 +1,4 @@
-import { memo, useContext, useState } from 'react'
+import { useContext, useState } from 'react'
 import { sortBy } from 'es-toolkit'
 import { observer } from 'mobx-react-lite'
 import { gql } from '@apollo/client'
@@ -9,105 +9,102 @@ import { exportModule } from '../../../../modules/export.js'
 import { MobxContext } from '../../../../mobxContext.js'
 import { DownloadCardButton, StyledProgressText } from '../index.jsx'
 
-export const AnzKontrProPop = memo(
-  observer(() => {
-    const store = useContext(MobxContext)
-    const { enqueNotification } = store
+export const AnzKontrProPop = observer(() => {
+  const store = useContext(MobxContext)
+  const { enqueNotification } = store
 
-    const apolloClient = useApolloClient()
+  const apolloClient = useApolloClient()
 
-    const [queryState, setQueryState] = useState()
+  const [queryState, setQueryState] = useState()
 
-    return (
-      <DownloadCardButton
-        color="inherit"
-        disabled={!!queryState}
-        onClick={async () => {
-          setQueryState('lade Daten...')
-          let result
-          try {
-            result = await apolloClient.query({
-              query: gql`
-                query popAnzKontrsQuery {
-                  allPops {
-                    nodes {
-                      id
-                      vPopAnzkontrsById {
-                        nodes {
-                          apId
-                          artname
-                          apBearbeitung
-                          apStartJahr
-                          apUmsetzung
-                          id
-                          nr
-                          name
-                          status
-                          bekanntSeit
-                          statusUnklar
-                          statusUnklarBegruendung
-                          x
-                          y
-                          anzahlKontrollen
-                        }
+  return (
+    <DownloadCardButton
+      color="inherit"
+      disabled={!!queryState}
+      onClick={async () => {
+        setQueryState('lade Daten...')
+        let result
+        try {
+          result = await apolloClient.query({
+            query: gql`
+              query popAnzKontrsQuery {
+                allPops {
+                  nodes {
+                    id
+                    vPopAnzkontrsById {
+                      nodes {
+                        apId
+                        artname
+                        apBearbeitung
+                        apStartJahr
+                        apUmsetzung
+                        id
+                        nr
+                        name
+                        status
+                        bekanntSeit
+                        statusUnklar
+                        statusUnklarBegruendung
+                        x
+                        y
+                        anzahlKontrollen
                       }
                     }
                   }
                 }
-              `,
-            })
-          } catch (error) {
-            enqueNotification({
-              message: error.message,
-              options: {
-                variant: 'error',
-              },
-            })
-          }
-          setQueryState('verarbeite...')
-          const rows = (result?.data?.allPops?.nodes ?? []).map((z) => ({
-            ap_id: z?.vPopAnzkontrsById?.nodes?.[0]?.apId ?? '',
-            artname: z?.vPopAnzkontrsById?.nodes?.[0]?.artname ?? '',
-            ap_bearbeitung:
-              z?.vPopAnzkontrsById?.nodes?.[0]?.apBearbeitung ?? '',
-            ap_start_jahr: z?.vPopAnzkontrsById?.nodes?.[0]?.apStartJahr ?? '',
-            ap_umsetzung: z?.vPopAnzkontrsById?.nodes?.[0]?.apUmsetzung ?? '',
-            id: z?.vPopAnzkontrsById?.nodes?.[0]?.id ?? '',
-            nr: z?.vPopAnzkontrsById?.nodes?.[0]?.nr ?? '',
-            name: z?.vPopAnzkontrsById?.nodes?.[0]?.name ?? '',
-            status: z?.vPopAnzkontrsById?.nodes?.[0]?.status ?? '',
-            bekannt_seit: z?.vPopAnzkontrsById?.nodes?.[0]?.bekanntSeit ?? '',
-            status_unklar: z?.vPopAnzkontrsById?.nodes?.[0]?.statusUnklar ?? '',
-            status_unklar_begruendung:
-              z?.vPopAnzkontrsById?.nodes?.[0]?.statusUnklarBegruendung ?? '',
-            x: z?.vPopAnzkontrsById?.nodes?.[0]?.x ?? '',
-            y: z?.vPopAnzkontrsById?.nodes?.[0]?.y ?? '',
-            anzahl_kontrollen:
-              z?.vPopAnzkontrsById?.nodes?.[0]?.anzahlKontrollen ?? '',
-          }))
-          if (rows.length === 0) {
-            setQueryState(undefined)
-            return enqueNotification({
-              message: 'Die Abfrage retournierte 0 Datensätze',
-              options: {
-                variant: 'warning',
-              },
-            })
-          }
-          exportModule({
-            data: sortBy(rows, ['artname', 'nr']),
-            fileName: 'PopulationenAnzahlKontrollen',
-            store,
-            apolloClient,
+              }
+            `,
           })
+        } catch (error) {
+          enqueNotification({
+            message: error.message,
+            options: {
+              variant: 'error',
+            },
+          })
+        }
+        setQueryState('verarbeite...')
+        const rows = (result?.data?.allPops?.nodes ?? []).map((z) => ({
+          ap_id: z?.vPopAnzkontrsById?.nodes?.[0]?.apId ?? '',
+          artname: z?.vPopAnzkontrsById?.nodes?.[0]?.artname ?? '',
+          ap_bearbeitung: z?.vPopAnzkontrsById?.nodes?.[0]?.apBearbeitung ?? '',
+          ap_start_jahr: z?.vPopAnzkontrsById?.nodes?.[0]?.apStartJahr ?? '',
+          ap_umsetzung: z?.vPopAnzkontrsById?.nodes?.[0]?.apUmsetzung ?? '',
+          id: z?.vPopAnzkontrsById?.nodes?.[0]?.id ?? '',
+          nr: z?.vPopAnzkontrsById?.nodes?.[0]?.nr ?? '',
+          name: z?.vPopAnzkontrsById?.nodes?.[0]?.name ?? '',
+          status: z?.vPopAnzkontrsById?.nodes?.[0]?.status ?? '',
+          bekannt_seit: z?.vPopAnzkontrsById?.nodes?.[0]?.bekanntSeit ?? '',
+          status_unklar: z?.vPopAnzkontrsById?.nodes?.[0]?.statusUnklar ?? '',
+          status_unklar_begruendung:
+            z?.vPopAnzkontrsById?.nodes?.[0]?.statusUnklarBegruendung ?? '',
+          x: z?.vPopAnzkontrsById?.nodes?.[0]?.x ?? '',
+          y: z?.vPopAnzkontrsById?.nodes?.[0]?.y ?? '',
+          anzahl_kontrollen:
+            z?.vPopAnzkontrsById?.nodes?.[0]?.anzahlKontrollen ?? '',
+        }))
+        if (rows.length === 0) {
           setQueryState(undefined)
-        }}
-      >
-        Anzahl Kontrollen pro Population
-        {queryState ?
-          <StyledProgressText>{queryState}</StyledProgressText>
-        : null}
-      </DownloadCardButton>
-    )
-  }),
-)
+          return enqueNotification({
+            message: 'Die Abfrage retournierte 0 Datensätze',
+            options: {
+              variant: 'warning',
+            },
+          })
+        }
+        exportModule({
+          data: sortBy(rows, ['artname', 'nr']),
+          fileName: 'PopulationenAnzahlKontrollen',
+          store,
+          apolloClient,
+        })
+        setQueryState(undefined)
+      }}
+    >
+      Anzahl Kontrollen pro Population
+      {queryState ?
+        <StyledProgressText>{queryState}</StyledProgressText>
+      : null}
+    </DownloadCardButton>
+  )
+})
