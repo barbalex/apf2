9d31ec77a447e3567651e8244a58fc6f2d55ee81
Alexander Gabriel <alex.barbalex@gmail.com>
2025-10-25 17:35:15 +0200

    de-memoize

--- src/components/Projekte/Karte/layers/BeobNichtBeurteilt/Marker.jsx
+++ src/components/Projekte/Karte/layers/BeobNichtBeurteilt/Marker.jsx
@@ -1,4 +1,4 @@
-import { memo, useContext, useCallback } from 'react'
+import { useContext } from 'react'
 import { Marker as LeafletMarker, Popup } from 'react-leaflet'
 import { format } from 'date-fns/format'
 import { isValid } from 'date-fns/isValid'
@@ -33,182 +33,168 @@ const AbsenzDiv = styled.div`
   font-weight: bold;
 `
 
-export const Marker = memo(
-  observer(({ beob }) => {
-    const { apId, projId, beobId } = useParams()
-    const navigate = useNavigate()
-    const { search } = useLocation()
+export const Marker = observer(({ beob }) => {
+  const { apId, projId, beobId } = useParams()
+  const navigate = useNavigate()
+  const { search } = useLocation()
 
-    const store = useContext(MobxContext)
-    const { assigningBeob, openTree2WithActiveNodeArray } = store
+  const store = useContext(MobxContext)
+  const { assigningBeob, openTree2WithActiveNodeArray } = store
 
-    const tsQueryClient = useQueryClient()
-    const apolloClient = useApolloClient()
+  const tsQueryClient = useQueryClient()
+  const apolloClient = useApolloClient()
 
-    const isHighlighted = beobId === beob.id
-    const isAbsenz = beob.absenz
-    const latLng = new window.L.LatLng(beob.wgs84Lat, beob.wgs84Long)
-    const icon = window.L.divIcon({
-      html:
-        isHighlighted ?
-          isAbsenz ? beobIconHighlightedAbsenzString
-          : beobIconHighlightedString
-        : isAbsenz ? beobIconAbsenzString
-        : beobIconString,
-      className: isHighlighted ? 'beobIconHighlighted' : 'beobIcon',
-    })
-    // some dates are not valid
-    // need to account for that
-    let datum = '(kein Datum)'
-    if (!isValid(new Date(beob.datum))) {
-      datum = '(ungültiges Datum)'
-    } else if (beob.datum) {
-      datum = format(new Date(beob.datum), 'yyyy.MM.dd')
-    }
-    const autor = beob.autor ?? '(kein Autor)'
-    const quelle = beob?.quelle ?? ''
-    const label = `${datum}: ${autor} (${quelle})`
+  const isHighlighted = beobId === beob.id
+  const isAbsenz = beob.absenz
+  const latLng = new window.L.LatLng(beob.wgs84Lat, beob.wgs84Long)
+  const icon = window.L.divIcon({
+    html:
+      isHighlighted ?
+        isAbsenz ? beobIconHighlightedAbsenzString
+        : beobIconHighlightedString
+      : isAbsenz ? beobIconAbsenzString
+      : beobIconString,
+    className: isHighlighted ? 'beobIconHighlighted' : 'beobIcon',
+  })
+  // some dates are not valid
+  // need to account for that
+  let datum = '(kein Datum)'
+  if (!isValid(new Date(beob.datum))) {
+    datum = '(ungültiges Datum)'
+  } else if (beob.datum) {
+    datum = format(new Date(beob.datum), 'yyyy.MM.dd')
+  }
+  const autor = beob.autor ?? '(kein Autor)'
+  const quelle = beob?.quelle ?? ''
+  const label = `${datum}: ${autor} (${quelle})`
 
-    const onMoveend = useCallback(
-      async (event) => {
-        /**
-         * assign to nearest tpop
-         * point url to moved beob
-         */
-        const nearestTpop = await getNearestTpop({
-          apId,
-          latLng: event.target._latlng,
-          apolloClient,
-        })
-        await apolloClient.mutate({
-          mutation: updateBeobById,
-          variables: {
-            id: beob.id,
-            tpopId: nearestTpop.id,
-          },
-        })
-        navigate(
-          `/Daten/Projekte/${projId}/Arten/${apId}/Populationen/${nearestTpop.popId}/Teil-Populationen/${nearestTpop.id}/Beobachtungen/${beob.id}${search}`,
-        )
-        tsQueryClient.invalidateQueries({
-          queryKey: [`treeQuery`],
-        })
-        tsQueryClient.invalidateQueries({
-          queryKey: [`BeobZugeordnetForMapQuery`],
-        })
-        tsQueryClient.invalidateQueries({
-          queryKey: [`BeobNichtBeurteiltForMapQuery`],
-        })
-        tsQueryClient.invalidateQueries({
-          queryKey: [`BeobAssignLinesQuery`],
-        })
-        tsQueryClient.invalidateQueries({
-          queryKey: [`treeBeobZugeordnet`],
-        })
-        tsQueryClient.invalidateQueries({
-          queryKey: [`treeBeobNichtZuzuordnen`],
-        })
-        tsQueryClient.invalidateQueries({
-          queryKey: [`treeBeobNichtBeurteilt`],
-        })
-        tsQueryClient.invalidateQueries({
-          queryKey: [`treeAp`],
-        })
-        store.tree.setLastTouchedNode([
-          'Projekte',
-          projId,
-          'Arten',
-          apId,
-          'Populationen',
-          nearestTpop.popId,
-          'Teil-Populationen',
-          nearestTpop.id,
-          'Beobachtungen',
-          beob.id,
-        ])
-      },
-      [apId, beob.id, apolloClient, navigate, projId, tsQueryClient, search],
-    )
-
-    const [projekteTabs, setProjekteTabs] = useProjekteTabs()
-    const openBeobInTree2 = useCallback(() => {
-      openTree2WithActiveNodeArray({
-        activeNodeArray: [
-          'Projekte',
-          projId,
-          'Arten',
-          apId,
-          'nicht-beurteilte-Beobachtungen',
-          beob.id,
-        ],
-        search,
-        projekteTabs,
-        setProjekteTabs,
-      })
-    }, [
+  const onMoveend = async (event) => {
+    /**
+     * assign to nearest tpop
+     * point url to moved beob
+     */
+    const nearestTpop = await getNearestTpop({
       apId,
-      beob.id,
-      openTree2WithActiveNodeArray,
-      projId,
-      projekteTabs,
-      search,
-      setProjekteTabs,
-    ])
-
-    const openBeobInTab = useCallback(() => {
-      const url = `${appBaseUrl()}Daten/Projekte/${projId}/Arten/${apId}/nicht-beurteilte-Beobachtungen/${
-        beob.id
-      }`
-      if (window.matchMedia('(display-mode: standalone)').matches) {
-        return window.open(url, '_blank', 'toolbar=no')
-      }
-      window.open(url)
-    }, [apId, beob.id, projId])
-
-    return (
-      <LeafletMarker
-        position={latLng}
-        icon={icon}
-        title={label}
-        draggable={assigningBeob}
-        eventHandlers={{ moveend: onMoveend }}
-      >
-        <Popup>
-          <>
-            <div>{`Beobachtung von ${
-              beob?.aeTaxonomyByArtId?.artname ?? ''
-            }`}</div>
-            {beob?.absenz ?
-              <AbsenzDiv>Absenzmeldung</AbsenzDiv>
-            : null}
-            <StyledH3>{label}</StyledH3>
-            <div>
-              {`Koordinaten: ${beob.lv95X?.toLocaleString(
-                'de-ch',
-              )} / ${beob.lv95Y?.toLocaleString('de-ch')}`}
-            </div>
-            <StyledButton
-              size="small"
-              variant="text"
-              onClick={openBeobInTab}
-              color="inherit"
-              fullWidth
-            >
-              Formular in neuem Fenster öffnen
-            </StyledButton>
-            <StyledButton
-              size="small"
-              variant="text"
-              onClick={openBeobInTree2}
-              color="inherit"
-              fullWidth
-            >
-              Formular in Navigationsbaum 2 öffnen
-            </StyledButton>
-            <Data id={beob.id} />
-          </>
-        </Popup>
-      </LeafletMarker>
+      latLng: event.target._latlng,
+      apolloClient,
+    })
+    await apolloClient.mutate({
+      mutation: updateBeobById,
+      variables: {
+        id: beob.id,
+        tpopId: nearestTpop.id,
+      },
+    })
+    navigate(
+      `/Daten/Projekte/${projId}/Arten/${apId}/Populationen/${nearestTpop.popId}/Teil-Populationen/${nearestTpop.id}/Beobachtungen/${beob.id}${search}`,
     )
-  }),
-)
+    tsQueryClient.invalidateQueries({
+      queryKey: [`treeQuery`],
+    })
+    tsQueryClient.invalidateQueries({
+      queryKey: [`BeobZugeordnetForMapQuery`],
+    })
+    tsQueryClient.invalidateQueries({
+      queryKey: [`BeobNichtBeurteiltForMapQuery`],
+    })
+    tsQueryClient.invalidateQueries({
+      queryKey: [`BeobAssignLinesQuery`],
+    })
+    tsQueryClient.invalidateQueries({
+      queryKey: [`treeBeobZugeordnet`],
+    })
+    tsQueryClient.invalidateQueries({
+      queryKey: [`treeBeobNichtZuzuordnen`],
+    })
+    tsQueryClient.invalidateQueries({
+      queryKey: [`treeBeobNichtBeurteilt`],
+    })
+    tsQueryClient.invalidateQueries({
+      queryKey: [`treeAp`],
+    })
+    store.tree.setLastTouchedNode([
+      'Projekte',
+      projId,
+      'Arten',
+      apId,
+      'Populationen',
+      nearestTpop.popId,
+      'Teil-Populationen',
+      nearestTpop.id,
+      'Beobachtungen',
+      beob.id,
+    ])
+  }
+
+  const [projekteTabs, setProjekteTabs] = useProjekteTabs()
+  const openBeobInTree2 = () =>
+    openTree2WithActiveNodeArray({
+      activeNodeArray: [
+        'Projekte',
+        projId,
+        'Arten',
+        apId,
+        'nicht-beurteilte-Beobachtungen',
+        beob.id,
+      ],
+      search,
+      projekteTabs,
+      setProjekteTabs,
+    })
+
+  const openBeobInTab = () => {
+    const url = `${appBaseUrl()}Daten/Projekte/${projId}/Arten/${apId}/nicht-beurteilte-Beobachtungen/${
+      beob.id
+    }`
+    if (window.matchMedia('(display-mode: standalone)').matches) {
+      return window.open(url, '_blank', 'toolbar=no')
+    }
+    window.open(url)
+  }
+
+  return (
+    <LeafletMarker
+      position={latLng}
+      icon={icon}
+      title={label}
+      draggable={assigningBeob}
+      eventHandlers={{ moveend: onMoveend }}
+    >
+      <Popup>
+        <>
+          <div>{`Beobachtung von ${
+            beob?.aeTaxonomyByArtId?.artname ?? ''
+          }`}</div>
+          {beob?.absenz ?
+            <AbsenzDiv>Absenzmeldung</AbsenzDiv>
+          : null}
+          <StyledH3>{label}</StyledH3>
+          <div>
+            {`Koordinaten: ${beob.lv95X?.toLocaleString(
+              'de-ch',
+            )} / ${beob.lv95Y?.toLocaleString('de-ch')}`}
+          </div>
+          <StyledButton
+            size="small"
+            variant="text"
+            onClick={openBeobInTab}
+            color="inherit"
+            fullWidth
+          >
+            Formular in neuem Fenster öffnen
+          </StyledButton>
+          <StyledButton
+            size="small"
+            variant="text"
+            onClick={openBeobInTree2}
+            color="inherit"
+            fullWidth
+          >
+            Formular in Navigationsbaum 2 öffnen
+          </StyledButton>
+          <Data id={beob.id} />
+        </>
+      </Popup>
+    </LeafletMarker>
+  )
+})
