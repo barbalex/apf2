fd147761df58be3e65b6ec55276f862f69157bb3
Alexander Gabriel <alex.barbalex@gmail.com>
2025-10-25 17:13:46 +0200

    de-memoize

--- src/components/Projekte/Exporte/Populationen/PopsForGEArtname.jsx
+++ src/components/Projekte/Exporte/Populationen/PopsForGEArtname.jsx
@@ -1,4 +1,4 @@
-import { memo, useContext, useState } from 'react'
+import { useContext, useState } from 'react'
 import { sortBy } from 'es-toolkit'
 import { observer } from 'mobx-react-lite'
 import { gql } from '@apollo/client'
@@ -9,87 +9,85 @@ import { exportModule } from '../../../../modules/export.js'
 import { MobxContext } from '../../../../mobxContext.js'
 import { DownloadCardButton, StyledProgressText } from '../index.jsx'
 
-export const PopsForGEArtname = memo(
-  observer(() => {
-    const store = useContext(MobxContext)
-    const { enqueNotification } = store
+export const PopsForGEArtname = observer(() => {
+  const store = useContext(MobxContext)
+  const { enqueNotification } = store
 
-    const apolloClient = useApolloClient()
+  const apolloClient = useApolloClient()
 
-    const [queryState, setQueryState] = useState()
+  const [queryState, setQueryState] = useState()
 
-    return (
-      <DownloadCardButton
-        color="inherit"
-        disabled={!!queryState}
-        onClick={async () => {
-          setQueryState('lade Daten...')
-          let result
-          try {
-            result = await apolloClient.query({
-              query: gql`
-                query popKmlNamenQuery {
-                  allPops(filter: { vPopKmlnamenByIdExist: true }) {
-                    nodes {
-                      id
-                      vPopKmlnamenById {
-                        nodes {
-                          art
-                          label
-                          inhalte
-                          id
-                          wgs84Lat
-                          wgs84Long
-                          url
-                        }
+  return (
+    <DownloadCardButton
+      color="inherit"
+      disabled={!!queryState}
+      onClick={async () => {
+        setQueryState('lade Daten...')
+        let result
+        try {
+          result = await apolloClient.query({
+            query: gql`
+              query popKmlNamenQuery {
+                allPops(filter: { vPopKmlnamenByIdExist: true }) {
+                  nodes {
+                    id
+                    vPopKmlnamenById {
+                      nodes {
+                        art
+                        label
+                        inhalte
+                        id
+                        wgs84Lat
+                        wgs84Long
+                        url
                       }
                     }
                   }
                 }
-              `,
-            })
-          } catch (error) {
-            enqueNotification({
-              message: error.message,
-              options: {
-                variant: 'error',
-              },
-            })
-          }
-          setQueryState('verarbeite...')
-          const rows = (result?.data?.allPops?.nodes ?? []).map((z) => ({
-            art: z?.vPopKmlnamenById?.nodes?.[0]?.art ?? '',
-            label: z?.vPopKmlnamenById?.nodes?.[0]?.label ?? '',
-            inhalte: z?.vPopKmlnamenById?.nodes?.[0]?.inhalte ?? '',
-            id: z?.vPopKmlnamenById?.nodes?.[0]?.id ?? '',
-            wgs84Lat: z?.vPopKmlnamenById?.nodes?.[0]?.wgs84Lat ?? '',
-            wgs84Long: z?.vPopKmlnamenById?.nodes?.[0]?.wgs84Long ?? '',
-            url: z?.vPopKmlnamenById?.nodes?.[0]?.url ?? '',
-          }))
-          if (rows.length === 0) {
-            setQueryState(undefined)
-            return enqueNotification({
-              message: 'Die Abfrage retournierte 0 Datensätze',
-              options: {
-                variant: 'warning',
-              },
-            })
-          }
-          exportModule({
-            data: sortBy(rows, ['art', 'label']),
-            fileName: 'PopulationenNachNamen',
-            store,
-            kml: true,
-            apolloClient,
+              }
+            `,
           })
+        } catch (error) {
+          enqueNotification({
+            message: error.message,
+            options: {
+              variant: 'error',
+            },
+          })
+        }
+        setQueryState('verarbeite...')
+        const rows = (result?.data?.allPops?.nodes ?? []).map((z) => ({
+          art: z?.vPopKmlnamenById?.nodes?.[0]?.art ?? '',
+          label: z?.vPopKmlnamenById?.nodes?.[0]?.label ?? '',
+          inhalte: z?.vPopKmlnamenById?.nodes?.[0]?.inhalte ?? '',
+          id: z?.vPopKmlnamenById?.nodes?.[0]?.id ?? '',
+          wgs84Lat: z?.vPopKmlnamenById?.nodes?.[0]?.wgs84Lat ?? '',
+          wgs84Long: z?.vPopKmlnamenById?.nodes?.[0]?.wgs84Long ?? '',
+          url: z?.vPopKmlnamenById?.nodes?.[0]?.url ?? '',
+        }))
+        if (rows.length === 0) {
           setQueryState(undefined)
-        }}
-      >
-        {`Populationen für Google Earth (beschriftet mit Artname, PopNr)`}
-        {queryState ?
-          <StyledProgressText>{queryState}</StyledProgressText>
-        : null}
-      </DownloadCardButton>
-    )
-  }),
-)
+          return enqueNotification({
+            message: 'Die Abfrage retournierte 0 Datensätze',
+            options: {
+              variant: 'warning',
+            },
+          })
+        }
+        exportModule({
+          data: sortBy(rows, ['art', 'label']),
+          fileName: 'PopulationenNachNamen',
+          store,
+          kml: true,
+          apolloClient,
+        })
+        setQueryState(undefined)
+      }}
+    >
+      {`Populationen für Google Earth (beschriftet mit Artname, PopNr)`}
+      {queryState ?
+        <StyledProgressText>{queryState}</StyledProgressText>
+      : null}
+    </DownloadCardButton>
+  )
+})
--- src/components/Projekte/Exporte/Populationen/PopsForGoogleEarth.jsx
+++ src/components/Projekte/Exporte/Populationen/PopsForGoogleEarth.jsx
@@ -1,4 +1,4 @@
-import { memo, useContext, useState } from 'react'
+import { useContext, useState } from 'react'
 import { sortBy } from 'es-toolkit'
 import { observer } from 'mobx-react-lite'
 import { gql } from '@apollo/client'
@@ -9,87 +9,85 @@ import { exportModule } from '../../../../modules/export.js'
 import { MobxContext } from '../../../../mobxContext.js'
 import { DownloadCardButton, StyledProgressText } from '../index.jsx'
 
-export const PopsForGoogleEarth = memo(
-  observer(() => {
-    const store = useContext(MobxContext)
-    const { enqueNotification } = store
+export const PopsForGoogleEarth = observer(() => {
+  const store = useContext(MobxContext)
+  const { enqueNotification } = store
 
-    const apolloClient = useApolloClient()
+  const apolloClient = useApolloClient()
 
-    const [queryState, setQueryState] = useState()
+  const [queryState, setQueryState] = useState()
 
-    return (
-      <DownloadCardButton
-        color="inherit"
-        disabled={!!queryState}
-        onClick={async () => {
-          setQueryState('lade Daten...')
-          let result
-          try {
-            result = await apolloClient.query({
-              query: gql`
-                query popKmlQuery {
-                  allPops(filter: { vPopKmlsByIdExist: true }) {
-                    nodes {
-                      id
-                      vPopKmlsById {
-                        nodes {
-                          art
-                          label
-                          inhalte
-                          id
-                          wgs84Lat
-                          wgs84Long
-                          url
-                        }
+  return (
+    <DownloadCardButton
+      color="inherit"
+      disabled={!!queryState}
+      onClick={async () => {
+        setQueryState('lade Daten...')
+        let result
+        try {
+          result = await apolloClient.query({
+            query: gql`
+              query popKmlQuery {
+                allPops(filter: { vPopKmlsByIdExist: true }) {
+                  nodes {
+                    id
+                    vPopKmlsById {
+                      nodes {
+                        art
+                        label
+                        inhalte
+                        id
+                        wgs84Lat
+                        wgs84Long
+                        url
                       }
                     }
                   }
                 }
-              `,
-            })
-          } catch (error) {
-            enqueNotification({
-              message: error.message,
-              options: {
-                variant: 'error',
-              },
-            })
-          }
-          setQueryState('verarbeite...')
-          const rows = (result?.data?.allPops?.nodes ?? []).map((z) => ({
-            art: z?.vPopKmlsById?.nodes?.[0]?.art ?? '',
-            label: z?.vPopKmlsById?.nodes?.[0]?.label ?? '',
-            inhalte: z?.vPopKmlsById?.nodes?.[0]?.inhalte ?? '',
-            id: z?.vPopKmlsById?.nodes?.[0]?.id ?? '',
-            wgs84Lat: z?.vPopKmlsById?.nodes?.[0]?.wgs84Lat ?? '',
-            wgs84Long: z?.vPopKmlsById?.nodes?.[0]?.wgs84Long ?? '',
-            url: z?.vPopKmlsById?.nodes?.[0]?.url ?? '',
-          }))
-          if (rows.length === 0) {
-            setQueryState(undefined)
-            return enqueNotification({
-              message: 'Die Abfrage retournierte 0 Datensätze',
-              options: {
-                variant: 'warning',
-              },
-            })
-          }
-          exportModule({
-            data: sortBy(rows, ['art', 'label']),
-            fileName: 'Populationen',
-            store,
-            kml: true,
-            apolloClient,
+              }
+            `,
           })
+        } catch (error) {
+          enqueNotification({
+            message: error.message,
+            options: {
+              variant: 'error',
+            },
+          })
+        }
+        setQueryState('verarbeite...')
+        const rows = (result?.data?.allPops?.nodes ?? []).map((z) => ({
+          art: z?.vPopKmlsById?.nodes?.[0]?.art ?? '',
+          label: z?.vPopKmlsById?.nodes?.[0]?.label ?? '',
+          inhalte: z?.vPopKmlsById?.nodes?.[0]?.inhalte ?? '',
+          id: z?.vPopKmlsById?.nodes?.[0]?.id ?? '',
+          wgs84Lat: z?.vPopKmlsById?.nodes?.[0]?.wgs84Lat ?? '',
+          wgs84Long: z?.vPopKmlsById?.nodes?.[0]?.wgs84Long ?? '',
+          url: z?.vPopKmlsById?.nodes?.[0]?.url ?? '',
+        }))
+        if (rows.length === 0) {
           setQueryState(undefined)
-        }}
-      >
-        {`Populationen für Google Earth (beschriftet mit PopNr)`}
-        {queryState ?
-          <StyledProgressText>{queryState}</StyledProgressText>
-        : null}
-      </DownloadCardButton>
-    )
-  }),
-)
+          return enqueNotification({
+            message: 'Die Abfrage retournierte 0 Datensätze',
+            options: {
+              variant: 'warning',
+            },
+          })
+        }
+        exportModule({
+          data: sortBy(rows, ['art', 'label']),
+          fileName: 'Populationen',
+          store,
+          kml: true,
+          apolloClient,
+        })
+        setQueryState(undefined)
+      }}
+    >
+      {`Populationen für Google Earth (beschriftet mit PopNr)`}
+      {queryState ?
+        <StyledProgressText>{queryState}</StyledProgressText>
+      : null}
+    </DownloadCardButton>
+  )
+})
