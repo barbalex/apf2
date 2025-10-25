9ab8a5e4cfac1d8f06b6ba94a91855a194e6876e
Alexander Gabriel <alex.barbalex@gmail.com>
2025-10-25 17:08:07 +0200

    de-memoize

--- src/components/Projekte/Exporte/Beobachtungen/BeobArtChanged.jsx
+++ src/components/Projekte/Exporte/Beobachtungen/BeobArtChanged.jsx
@@ -1,4 +1,4 @@
-import { memo, useContext, useState, useCallback } from 'react'
+import { useContext, useState } from 'react'
 import { observer } from 'mobx-react-lite'
 import { gql } from '@apollo/client'
 
@@ -8,144 +8,142 @@ import { exportModule } from '../../../../modules/export.js'
 import { MobxContext } from '../../../../mobxContext.js'
 import { DownloadCardButton, StyledProgressText } from '../index.jsx'
 
-export const BeobArtChanged = memo(
-  observer(() => {
-    const store = useContext(MobxContext)
-    const { enqueNotification } = store
-    const { mapFilter } = store.tree
+export const BeobArtChanged = observer(() => {
+  const store = useContext(MobxContext)
+  const { enqueNotification } = store
+  const { mapFilter } = store.tree
 
-    const apolloClient = useApolloClient()
+  const apolloClient = useApolloClient()
 
-    const [queryState, setQueryState] = useState()
+  const [queryState, setQueryState] = useState()
 
-    const onClickButton = useCallback(async () => {
-      setQueryState('lade Daten...')
-      let result
-      try {
-        // view: v_beob_art_changed
-        result =
-          mapFilter ?
-            await apolloClient.query({
-              query: gql`
-                query allBeobsArtChangedFilteredByMap {
-                  allVBeobArtChangeds {
-                    nodes {
-                      id
-                      quelle
-                      id_field: idField
-                      original_id: originalId
-                      art_id_original: artIdOriginal
-                      artname_original: artnameOriginal
-                      taxonomie_id_original: taxonomieIdOriginal
-                      art_id: artId
-                      artname
-                      taxonomie_id: taxonomieId
-                      pop_id: popId
-                      pop_nr: popNr
-                      tpop_id: tpopId
-                      tpop_nr: tpopNr
-                      tpop_status: tpopStatus
-                      tpop_gemeinde: tpopGemeinde
-                      tpop_flurname: tpopFlurname
-                      lv95X: x
-                      lv95Y: y
-                      distanz_zur_teilpopulation: distanzZurTeilpopulation
-                      datum
-                      autor
-                      nicht_zuordnen: nichtZuordnen
-                      bemerkungen
-                      created_at: createdAt
-                      updated_at: updatedAt
-                      changed_by: changedBy
-                    }
+  const onClickButton = async () => {
+    setQueryState('lade Daten...')
+    let result
+    try {
+      // view: v_beob_art_changed
+      result =
+        mapFilter ?
+          await apolloClient.query({
+            query: gql`
+              query allBeobsArtChangedFilteredByMap {
+                allVBeobArtChangeds {
+                  nodes {
+                    id
+                    quelle
+                    id_field: idField
+                    original_id: originalId
+                    art_id_original: artIdOriginal
+                    artname_original: artnameOriginal
+                    taxonomie_id_original: taxonomieIdOriginal
+                    art_id: artId
+                    artname
+                    taxonomie_id: taxonomieId
+                    pop_id: popId
+                    pop_nr: popNr
+                    tpop_id: tpopId
+                    tpop_nr: tpopNr
+                    tpop_status: tpopStatus
+                    tpop_gemeinde: tpopGemeinde
+                    tpop_flurname: tpopFlurname
+                    lv95X: x
+                    lv95Y: y
+                    distanz_zur_teilpopulation: distanzZurTeilpopulation
+                    datum
+                    autor
+                    nicht_zuordnen: nichtZuordnen
+                    bemerkungen
+                    created_at: createdAt
+                    updated_at: updatedAt
+                    changed_by: changedBy
                   }
                 }
-              `,
-              variables: {
-                filter: {
-                  geomPoint: {
-                    coveredBy: mapFilter,
-                  },
+              }
+            `,
+            variables: {
+              filter: {
+                geomPoint: {
+                  coveredBy: mapFilter,
                 },
               },
-            })
-          : await apolloClient.query({
-              query: gql`
-                query allBeobsArtChanged {
-                  allVBeobArtChangeds {
-                    nodes {
-                      id
-                      quelle
-                      id_field: idField
-                      original_id: originalId
-                      art_id_original: artIdOriginal
-                      artname_original: artnameOriginal
-                      taxonomie_id_original: taxonomieIdOriginal
-                      art_id: artId
-                      artname
-                      taxonomie_id: taxonomieId
-                      pop_id: popId
-                      pop_nr: popNr
-                      tpop_id: tpopId
-                      tpop_nr: tpopNr
-                      tpop_status: tpopStatus
-                      tpop_gemeinde: tpopGemeinde
-                      tpop_flurname: tpopFlurname
-                      lv95X: x
-                      lv95Y: y
-                      distanz_zur_teilpopulation: distanzZurTeilpopulation
-                      datum
-                      autor
-                      nicht_zuordnen: nichtZuordnen
-                      bemerkungen
-                      created_at: createdAt
-                      updated_at: updatedAt
-                      changed_by: changedBy
-                    }
+            },
+          })
+        : await apolloClient.query({
+            query: gql`
+              query allBeobsArtChanged {
+                allVBeobArtChangeds {
+                  nodes {
+                    id
+                    quelle
+                    id_field: idField
+                    original_id: originalId
+                    art_id_original: artIdOriginal
+                    artname_original: artnameOriginal
+                    taxonomie_id_original: taxonomieIdOriginal
+                    art_id: artId
+                    artname
+                    taxonomie_id: taxonomieId
+                    pop_id: popId
+                    pop_nr: popNr
+                    tpop_id: tpopId
+                    tpop_nr: tpopNr
+                    tpop_status: tpopStatus
+                    tpop_gemeinde: tpopGemeinde
+                    tpop_flurname: tpopFlurname
+                    lv95X: x
+                    lv95Y: y
+                    distanz_zur_teilpopulation: distanzZurTeilpopulation
+                    datum
+                    autor
+                    nicht_zuordnen: nichtZuordnen
+                    bemerkungen
+                    created_at: createdAt
+                    updated_at: updatedAt
+                    changed_by: changedBy
                   }
                 }
-              `,
-            })
-      } catch (error) {
-        setQueryState(undefined)
-        return enqueNotification({
-          message: error.message,
-          options: {
-            variant: 'error',
-          },
-        })
-      }
-      setQueryState('verarbeite...')
-      const rows = result.data?.allVBeobArtChangeds?.nodes ?? []
-      if (rows.length === 0) {
-        setQueryState(undefined)
-        return enqueNotification({
-          message: 'Die Abfrage retournierte 0 Datensätze',
-          options: {
-            variant: 'warning',
-          },
-        })
-      }
-      exportModule({
-        data: rows,
-        fileName: 'BeobachtungenArtVeraendert',
-        store,
-        apolloClient,
-      })
+              }
+            `,
+          })
+    } catch (error) {
       setQueryState(undefined)
-    }, [apolloClient, enqueNotification, mapFilter, store])
+      return enqueNotification({
+        message: error.message,
+        options: {
+          variant: 'error',
+        },
+      })
+    }
+    setQueryState('verarbeite...')
+    const rows = result.data?.allVBeobArtChangeds?.nodes ?? []
+    if (rows.length === 0) {
+      setQueryState(undefined)
+      return enqueNotification({
+        message: 'Die Abfrage retournierte 0 Datensätze',
+        options: {
+          variant: 'warning',
+        },
+      })
+    }
+    exportModule({
+      data: rows,
+      fileName: 'BeobachtungenArtVeraendert',
+      store,
+      apolloClient,
+    })
+    setQueryState(undefined)
+  }
 
-    return (
-      <DownloadCardButton
-        onClick={onClickButton}
-        color="inherit"
-        disabled={!!queryState}
-      >
-        Alle Beobachtungen, bei denen die Art verändert wurde
-        {queryState ?
-          <StyledProgressText>{queryState}</StyledProgressText>
-        : null}
-      </DownloadCardButton>
-    )
-  }),
-)
+  return (
+    <DownloadCardButton
+      onClick={onClickButton}
+      color="inherit"
+      disabled={!!queryState}
+    >
+      Alle Beobachtungen, bei denen die Art verändert wurde
+      {queryState ?
+        <StyledProgressText>{queryState}</StyledProgressText>
+      : null}
+    </DownloadCardButton>
+  )
+})
--- src/components/Projekte/Exporte/Beobachtungen/BeobNichtZuzuordnen.jsx
+++ src/components/Projekte/Exporte/Beobachtungen/BeobNichtZuzuordnen.jsx
@@ -1,4 +1,4 @@
-import { memo, useContext, useState } from 'react'
+import { useContext, useState } from 'react'
 import { observer } from 'mobx-react-lite'
 import { gql } from '@apollo/client'
 
@@ -8,81 +8,79 @@ import { exportModule } from '../../../../modules/export.js'
 import { MobxContext } from '../../../../mobxContext.js'
 import { DownloadCardButton, StyledProgressText } from '../index.jsx'
 
-export const BeobNichtZuzuordnen = memo(
-  observer(() => {
-    const store = useContext(MobxContext)
-    const { enqueNotification } = store
+export const BeobNichtZuzuordnen = observer(() => {
+  const store = useContext(MobxContext)
+  const { enqueNotification } = store
 
-    const apolloClient = useApolloClient()
+  const apolloClient = useApolloClient()
 
-    const [queryState, setQueryState] = useState()
+  const [queryState, setQueryState] = useState()
 
-    return (
-      <DownloadCardButton
-        color="inherit"
-        disabled={!!queryState}
-        onClick={async () => {
-          setQueryState('lade Daten...')
-          let result
-          try {
-            result = await apolloClient.query({
-              query: gql`
-                query allBeobsNichtZuzuordnenForExport {
-                  allVBeobNichtZuzuordnens {
-                    nodes {
-                      id
-                      quelle
-                      id_field: idField
-                      original_id: originalId
-                      art_id: artId
-                      art_id_original: artIdOriginal
-                      artname
-                      pop_id: popId
-                      pop_nr: popNr
-                      tpop_id: tpopId
-                      tpop_nr: tpopNr
-                      tpop_status: tpopStatus
-                      tpop_gemeinde: tpopGemeinde
-                      tpop_flurname: tpopFlurname
-                      lv95X: x
-                      lv95Y: y
-                      distanz_zur_teilpopulation: distanzZurTeilpopulation
-                      datum
-                      autor
-                      nicht_zuordnen: nichtZuordnen
-                      bemerkungen
-                      created_at: createdAt
-                      updated_at: updatedAt
-                      changed_by: changedBy
-                    }
+  return (
+    <DownloadCardButton
+      color="inherit"
+      disabled={!!queryState}
+      onClick={async () => {
+        setQueryState('lade Daten...')
+        let result
+        try {
+          result = await apolloClient.query({
+            query: gql`
+              query allBeobsNichtZuzuordnenForExport {
+                allVBeobNichtZuzuordnens {
+                  nodes {
+                    id
+                    quelle
+                    id_field: idField
+                    original_id: originalId
+                    art_id: artId
+                    art_id_original: artIdOriginal
+                    artname
+                    pop_id: popId
+                    pop_nr: popNr
+                    tpop_id: tpopId
+                    tpop_nr: tpopNr
+                    tpop_status: tpopStatus
+                    tpop_gemeinde: tpopGemeinde
+                    tpop_flurname: tpopFlurname
+                    lv95X: x
+                    lv95Y: y
+                    distanz_zur_teilpopulation: distanzZurTeilpopulation
+                    datum
+                    autor
+                    nicht_zuordnen: nichtZuordnen
+                    bemerkungen
+                    created_at: createdAt
+                    updated_at: updatedAt
+                    changed_by: changedBy
                   }
                 }
-              `,
-            })
-          } catch (error) {
-            setQueryState(undefined)
-            return enqueNotification({
-              message: error.message,
-              options: {
-                variant: 'error',
-              },
-            })
-          }
-          setQueryState('verarbeite...')
-          exportModule({
-            data: result?.data?.allVBeobNichtZuzuordnens?.nodes ?? [],
-            fileName: 'Beobachtungen',
-            store,
-            apolloClient,
+              }
+            `,
           })
+        } catch (error) {
           setQueryState(undefined)
-        }}
-      >
-        Alle nicht zuzuordnenden Beobachtungen
-        {queryState ?
-          <StyledProgressText>{queryState}</StyledProgressText>
-        : null}
-      </DownloadCardButton>
-    )
-  }),
-)
+          return enqueNotification({
+            message: error.message,
+            options: {
+              variant: 'error',
+            },
+          })
+        }
+        setQueryState('verarbeite...')
+        exportModule({
+          data: result?.data?.allVBeobNichtZuzuordnens?.nodes ?? [],
+          fileName: 'Beobachtungen',
+          store,
+          apolloClient,
+        })
+        setQueryState(undefined)
+      }}
+    >
+      Alle nicht zuzuordnenden Beobachtungen
+      {queryState ?
+        <StyledProgressText>{queryState}</StyledProgressText>
+      : null}
+    </DownloadCardButton>
+  )
+})
