23305cf7184634d4048f7568c571598881c53b0e
Alexander Gabriel <alex.barbalex@gmail.com>
2025-10-25 17:36:54 +0200

    de-memoize

--- src/components/Projekte/Karte/layers/BeobNichtZuzuordnen/index.jsx
+++ src/components/Projekte/Karte/layers/BeobNichtZuzuordnen/index.jsx
@@ -1,4 +1,4 @@
-import { memo, useContext } from 'react'
+import { useContext } from 'react'
 import { observer } from 'mobx-react-lite'
 import { useQuery } from '@tanstack/react-query'
 import { useApolloClient } from '@apollo/client/react'
@@ -27,94 +27,90 @@ const iconCreateFunction = function (cluster) {
   })
 }
 
-const BeobNichtZuzuordnenMarker = memo(
-  observer(({ clustered }) => {
-    // const leafletMap = useMap()
-    const store = useContext(MobxContext)
-    const { enqueNotification } = store
-    const tree = store.tree
-    const { beobGqlFilter } = tree
-    const apolloClient = useApolloClient()
+const BeobNichtZuzuordnenMarker = observer(({ clustered }) => {
+  // const leafletMap = useMap()
+  const store = useContext(MobxContext)
+  const { enqueNotification } = store
+  const tree = store.tree
+  const { beobGqlFilter } = tree
+  const apolloClient = useApolloClient()
 
-    const { data, error } = useQuery({
-      queryKey: [
-        'KarteBeobNichtZuzuordnenQuery',
-        beobGqlFilter('nichtZuzuordnen').filtered,
-      ],
-      queryFn: () =>
-        apolloClient.query({
-          query,
-          variables: {
-            beobFilter: beobGqlFilter('nichtZuzuordnen').filtered,
-          },
-          fetchPolicy: 'no-cache',
-        }),
-    })
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    // const [refetchProvoker, setRefetchProvoker] = useState(1)
-    // useEffect(() => {
-    //   // DO NOT use:
-    //   // leafletMap.on('zoomend dragend', refetch
-    //   // see: https://github.com/apollographql/apollo-client/issues/1291#issuecomment-367911441
-    //   // Also: leafletMap.on('zoomend dragend', ()=> refetch()) never refetches!!??
-    //   // Also: use dragend, not moveend because moveend fires on zoomend as well
-    //   leafletMap.on('zoomend dragend', () => setRefetchProvoker(Math.random()))
-    //   return () => {
-    //     leafletMap.off('zoomend dragend', () => setRefetchProvoker(Math.random()))
-    //   }
-    // }, [leafletMap])
-
-    if (error) {
-      enqueNotification({
-        message: `Fehler beim Laden der Nicht zuzuordnenden Beobachtungen für die Karte: ${error.message}`,
-        options: {
-          variant: 'error',
+  const { data, error } = useQuery({
+    queryKey: [
+      'KarteBeobNichtZuzuordnenQuery',
+      beobGqlFilter('nichtZuzuordnen').filtered,
+    ],
+    queryFn: () =>
+      apolloClient.query({
+        query,
+        variables: {
+          beobFilter: beobGqlFilter('nichtZuzuordnen').filtered,
         },
-      })
-    }
+        fetchPolicy: 'no-cache',
+      }),
+  })
 
-    const beobMarkers = (data?.data?.allBeobs?.nodes ?? []).map((beob) => (
-      <Marker
-        key={beob.id}
-        beob={beob}
-      />
-    ))
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  // const [refetchProvoker, setRefetchProvoker] = useState(1)
+  // useEffect(() => {
+  //   // DO NOT use:
+  //   // leafletMap.on('zoomend dragend', refetch
+  //   // see: https://github.com/apollographql/apollo-client/issues/1291#issuecomment-367911441
+  //   // Also: leafletMap.on('zoomend dragend', ()=> refetch()) never refetches!!??
+  //   // Also: use dragend, not moveend because moveend fires on zoomend as well
+  //   leafletMap.on('zoomend dragend', () => setRefetchProvoker(Math.random()))
+  //   return () => {
+  //     leafletMap.off('zoomend dragend', () => setRefetchProvoker(Math.random()))
+  //   }
+  // }, [leafletMap])
 
-    if (clustered) {
-      return (
-        <MarkerClusterGroup
-          key={beobMarkers.toString()} // to force rerendering when data changes, see https://github.com/barbalex/apf2/issues/750
-          maxClusterRadius={66}
-          iconCreateFunction={iconCreateFunction}
-        >
-          {beobMarkers}
-        </MarkerClusterGroup>
-      )
-    }
-    return beobMarkers
-  }),
-)
+  if (error) {
+    enqueNotification({
+      message: `Fehler beim Laden der Nicht zuzuordnenden Beobachtungen für die Karte: ${error.message}`,
+      options: {
+        variant: 'error',
+      },
+    })
+  }
 
-export const BeobNichtZuzuordnen = memo(
-  observer(({ clustered }) => {
-    const store = useContext(MobxContext)
-    const tree = store.tree
-    const { beobGqlFilter } = tree
+  const beobMarkers = (data?.data?.allBeobs?.nodes ?? []).map((beob) => (
+    <Marker
+      key={beob.id}
+      beob={beob}
+    />
+  ))
 
-    const { apId } = useParams()
+  if (clustered) {
+    return (
+      <MarkerClusterGroup
+        key={beobMarkers.toString()} // to force rerendering when data changes, see https://github.com/barbalex/apf2/issues/750
+        maxClusterRadius={66}
+        iconCreateFunction={iconCreateFunction}
+      >
+        {beobMarkers}
+      </MarkerClusterGroup>
+    )
+  }
+  return beobMarkers
+})
 
-    // Problem: gqlFilter updates AFTER apId
-    // if navigating from ap to pop, apId is set before gqlFilter
-    // thus query fetches data for all aps
-    // Solution: do not return pop if apId exists but gqlFilter does not contain it (yet)
-    const gqlFilterHasApId =
-      !!beobGqlFilter('nichtZuzuordnen').filtered?.aeTaxonomyByArtId
-        ?.apartsByArtId?.some?.apId
-    const apIdExistsButGqlFilterDoesNotKnowYet = !!apId && !gqlFilterHasApId
+export const BeobNichtZuzuordnen = observer(({ clustered }) => {
+  const store = useContext(MobxContext)
+  const tree = store.tree
+  const { beobGqlFilter } = tree
 
-    if (apIdExistsButGqlFilterDoesNotKnowYet) return null
+  const { apId } = useParams()
 
-    return <BeobNichtZuzuordnenMarker clustered={clustered} />
-  }),
-)
+  // Problem: gqlFilter updates AFTER apId
+  // if navigating from ap to pop, apId is set before gqlFilter
+  // thus query fetches data for all aps
+  // Solution: do not return pop if apId exists but gqlFilter does not contain it (yet)
+  const gqlFilterHasApId =
+    !!beobGqlFilter('nichtZuzuordnen').filtered?.aeTaxonomyByArtId
+      ?.apartsByArtId?.some?.apId
+  const apIdExistsButGqlFilterDoesNotKnowYet = !!apId && !gqlFilterHasApId
+
+  if (apIdExistsButGqlFilterDoesNotKnowYet) return null
+
+  return <BeobNichtZuzuordnenMarker clustered={clustered} />
+})
--- src/components/Projekte/Karte/layers/BeobZugeordnet/index.jsx
+++ src/components/Projekte/Karte/layers/BeobZugeordnet/index.jsx
@@ -1,4 +1,4 @@
-import { memo, useContext } from 'react'
+import { useContext } from 'react'
 import { observer } from 'mobx-react-lite'
 import { useQuery } from '@tanstack/react-query'
 import { useApolloClient } from '@apollo/client/react'
@@ -27,93 +27,89 @@ const iconCreateFunction = function (cluster) {
   })
 }
 
-const BeobZugeordnetMarker = memo(
-  observer(({ clustered }) => {
-    // const leafletMap = useMap()
-    const store = useContext(MobxContext)
-    const { enqueNotification } = store
-    const tree = store.tree
-    const { beobGqlFilter } = tree
+const BeobZugeordnetMarker = observer(({ clustered }) => {
+  // const leafletMap = useMap()
+  const store = useContext(MobxContext)
+  const { enqueNotification } = store
+  const tree = store.tree
+  const { beobGqlFilter } = tree
 
-    const apolloClient = useApolloClient()
+  const apolloClient = useApolloClient()
 
-    const { data, error } = useQuery({
-      queryKey: [
-        'BeobZugeordnetForMapQuery',
-        beobGqlFilter('zugeordnet').filtered,
-      ],
-      queryFn: async () =>
-        apolloClient.query({
-          query: query,
-          variables: { beobFilter: beobGqlFilter('zugeordnet').filtered },
-          fetchPolicy: 'no-cache',
-        }),
+  const { data, error } = useQuery({
+    queryKey: [
+      'BeobZugeordnetForMapQuery',
+      beobGqlFilter('zugeordnet').filtered,
+    ],
+    queryFn: async () =>
+      apolloClient.query({
+        query: query,
+        variables: { beobFilter: beobGqlFilter('zugeordnet').filtered },
+        fetchPolicy: 'no-cache',
+      }),
+  })
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  // const [refetchProvoker, setRefetchProvoker] = useState(1)
+  // useEffect(() => {
+  //   // DO NOT use:
+  //   // leafletMap.on('zoomend dragend', refetch
+  //   // see: https://github.com/apollographql/apollo-client/issues/1291#issuecomment-367911441
+  //   // Also: leafletMap.on('zoomend dragend', ()=> refetch()) never refetches!!??
+  //   // Also: use dragend, not moveend because moveend fires on zoomend as well
+  //   leafletMap.on('zoomend dragend', () => setRefetchProvoker(Math.random()))
+  //   return () => {
+  //     leafletMap.off('zoomend dragend', () => setRefetchProvoker(Math.random()))
+  //   }
+  // }, [leafletMap])
+
+  if (error) {
+    enqueNotification({
+      message: `Fehler beim Laden der Nicht zugeordneten Beobachtungen für die Karte: ${error.message}`,
+      options: {
+        variant: 'error',
+      },
     })
+  }
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    // const [refetchProvoker, setRefetchProvoker] = useState(1)
-    // useEffect(() => {
-    //   // DO NOT use:
-    //   // leafletMap.on('zoomend dragend', refetch
-    //   // see: https://github.com/apollographql/apollo-client/issues/1291#issuecomment-367911441
-    //   // Also: leafletMap.on('zoomend dragend', ()=> refetch()) never refetches!!??
-    //   // Also: use dragend, not moveend because moveend fires on zoomend as well
-    //   leafletMap.on('zoomend dragend', () => setRefetchProvoker(Math.random()))
-    //   return () => {
-    //     leafletMap.off('zoomend dragend', () => setRefetchProvoker(Math.random()))
-    //   }
-    // }, [leafletMap])
+  const beobMarkers = (data?.data?.allBeobs?.nodes ?? []).map((beob) => (
+    <Marker
+      key={beob.id}
+      beob={beob}
+    />
+  ))
 
-    if (error) {
-      enqueNotification({
-        message: `Fehler beim Laden der Nicht zugeordneten Beobachtungen für die Karte: ${error.message}`,
-        options: {
-          variant: 'error',
-        },
-      })
-    }
+  if (clustered) {
+    return (
+      <MarkerClusterGroup
+        key={beobMarkers.toString()} // to force rerendering when data changes, see https://github.com/barbalex/apf2/issues/750
+        maxClusterRadius={66}
+        iconCreateFunction={iconCreateFunction}
+      >
+        {beobMarkers}
+      </MarkerClusterGroup>
+    )
+  }
+  return beobMarkers
+})
 
-    const beobMarkers = (data?.data?.allBeobs?.nodes ?? []).map((beob) => (
-      <Marker
-        key={beob.id}
-        beob={beob}
-      />
-    ))
+export const BeobZugeordnet = observer(({ clustered }) => {
+  const store = useContext(MobxContext)
+  const tree = store.tree
+  const { beobGqlFilter } = tree
 
-    if (clustered) {
-      return (
-        <MarkerClusterGroup
-          key={beobMarkers.toString()} // to force rerendering when data changes, see https://github.com/barbalex/apf2/issues/750
-          maxClusterRadius={66}
-          iconCreateFunction={iconCreateFunction}
-        >
-          {beobMarkers}
-        </MarkerClusterGroup>
-      )
-    }
-    return beobMarkers
-  }),
-)
+  const { apId } = useParams()
 
-export const BeobZugeordnet = memo(
-  observer(({ clustered }) => {
-    const store = useContext(MobxContext)
-    const tree = store.tree
-    const { beobGqlFilter } = tree
+  // Problem: gqlFilter updates AFTER apId
+  // if navigating from ap to pop, apId is set before gqlFilter
+  // thus query fetches data for all aps
+  // Solution: do not return pop if apId exists but gqlFilter does not contain it (yet)
+  const gqlFilterHasApId =
+    !!beobGqlFilter('zugeordnet').filtered?.aeTaxonomyByArtId?.apartsByArtId
+      ?.some?.apId
+  const apIdExistsButGqlFilterDoesNotKnowYet = !!apId && !gqlFilterHasApId
 
-    const { apId } = useParams()
+  if (apIdExistsButGqlFilterDoesNotKnowYet) return null
 
-    // Problem: gqlFilter updates AFTER apId
-    // if navigating from ap to pop, apId is set before gqlFilter
-    // thus query fetches data for all aps
-    // Solution: do not return pop if apId exists but gqlFilter does not contain it (yet)
-    const gqlFilterHasApId =
-      !!beobGqlFilter('zugeordnet').filtered?.aeTaxonomyByArtId?.apartsByArtId
-        ?.some?.apId
-    const apIdExistsButGqlFilterDoesNotKnowYet = !!apId && !gqlFilterHasApId
-
-    if (apIdExistsButGqlFilterDoesNotKnowYet) return null
-
-    return <BeobZugeordnetMarker clustered={clustered} />
-  }),
-)
+  return <BeobZugeordnetMarker clustered={clustered} />
+})
