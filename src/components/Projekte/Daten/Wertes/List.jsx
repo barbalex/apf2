2de95238bd49a24d43245f93063dd0e1a2273621
Alexander Gabriel <alex.barbalex@gmail.com>
2025-10-26 20:30:42 +0100

    use suspense

--- src/components/Projekte/Daten/Wertes/List.jsx
+++ src/components/Projekte/Daten/Wertes/List.jsx
@@ -1,14 +1,18 @@
+import { Suspense } from 'react'
+
 import { List as SharedList } from '../../../shared/List/index.jsx'
 import { Spinner } from '../../../shared/Spinner.jsx'
 import { Error } from '../../../shared/Error.jsx'
 import { useWertesNavData } from '../../../../modules/useWertesNavData.js'
 
 export const List = () => {
-  const { navData, isLoading, error } = useWertesNavData()
-
-  if (isLoading) return <Spinner />
+  const { navData, error } = useWertesNavData()
 
   if (error) return <Error error={error} />
 
-  return <SharedList navData={navData} />
+  return (
+    <Suspense fallback={<Spinner />}>
+      <SharedList navData={navData} />
+    </Suspense>
+  )
 }
