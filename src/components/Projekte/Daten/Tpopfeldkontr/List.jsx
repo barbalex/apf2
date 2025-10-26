a986c95b1508b00cdec047efabaf59fc06f12c00
Alexander Gabriel <alex.barbalex@gmail.com>
2025-10-26 18:53:21 +0100

    use suspense

--- src/components/Projekte/Daten/Tpopfeldkontr/List.jsx
+++ src/components/Projekte/Daten/Tpopfeldkontr/List.jsx
@@ -1,3 +1,5 @@
+import { Suspense } from 'react'
+
 import { List as SharedList } from '../../../shared/List/index.jsx'
 import { Menu } from './Menu.jsx'
 import { Spinner } from '../../../shared/Spinner.jsx'
@@ -5,17 +7,17 @@ import { Error } from '../../../shared/Error.jsx'
 import { useTpopfeldkontrNavData } from '../../../../modules/useTpopfeldkontrNavData.js'
 
 export const List = () => {
-  const { navData, isLoading, error } = useTpopfeldkontrNavData()
-
-  if (isLoading) return <Spinner />
+  const { navData, error } = useTpopfeldkontrNavData()
 
   if (error) return <Error error={error} />
 
   return (
-    <SharedList
-      navData={navData}
-      MenuBarComponent={Menu}
-      menuBarProps={{ row: navData }}
-    />
+    <Suspense fallback={<Spinner />}>
+      <SharedList
+        navData={navData}
+        MenuBarComponent={Menu}
+        menuBarProps={{ row: navData }}
+      />
+    </Suspense>
   )
 }
--- src/components/Projekte/Daten/Tpopfreiwkontr/List.jsx
+++ src/components/Projekte/Daten/Tpopfreiwkontr/List.jsx
@@ -1,3 +1,5 @@
+import { Suspense } from 'react'
+
 import { List as SharedList } from '../../../shared/List/index.jsx'
 import { Menu } from './Menu.jsx'
 import { Spinner } from '../../../shared/Spinner.jsx'
@@ -5,7 +7,7 @@ import { Error } from '../../../shared/Error.jsx'
 import { useTpopfreiwkontrNavData } from '../../../../modules/useTpopfreiwkontrNavData.js'
 
 export const List = () => {
-  const { navData, isLoading, error } = useTpopfreiwkontrNavData()
+  const { navData, error } = useTpopfreiwkontrNavData()
 
   // BEWARE: Zählungen need to be hidden in this list
   const navDataToPass = {
@@ -13,15 +15,15 @@ export const List = () => {
     menus: navData.menus.filter((m) => !m.hideInNavList),
   }
 
-  if (isLoading) return <Spinner />
-
   if (error) return <Error error={error} />
 
   return (
-    <SharedList
-      navData={navData}
-      MenuBarComponent={Menu}
-      menuBarProps={{ row: navData }}
-    />
+    <Suspense fallback={<Spinner />}>
+      <SharedList
+        navData={navData}
+        MenuBarComponent={Menu}
+        menuBarProps={{ row: navData }}
+      />
+    </Suspense>
   )
 }
