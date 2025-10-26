2c92be0c3ba0c934c6cb78ac9bde7e79734e13e1
Alexander Gabriel <alex.barbalex@gmail.com>
2025-10-26 18:53:00 +0100

    use suspense

--- src/components/Projekte/Daten/Pop/List.jsx
+++ src/components/Projekte/Daten/Pop/List.jsx
@@ -1,3 +1,5 @@
+import { Suspense } from 'react'
+
 import { List as SharedList } from '../../../shared/List/index.jsx'
 import { Menu } from './Menu.jsx'
 import { Spinner } from '../../../shared/Spinner.jsx'
@@ -5,17 +7,17 @@ import { Error } from '../../../shared/Error.jsx'
 import { usePopNavData } from '../../../../modules/usePopNavData.js'
 
 export const List = () => {
-  const { navData, isLoading, error } = usePopNavData()
-
-  if (isLoading) return <Spinner />
+  const { navData, error } = usePopNavData()
 
   if (error) return <Error error={error} />
 
   return (
-    <SharedList
-      navData={navData}
-      MenuBarComponent={Menu}
-      menuBarProps={{ row: navData }}
-    />
+    <Suspense fallback={<Spinner />}>
+      <SharedList
+        navData={navData}
+        MenuBarComponent={Menu}
+        menuBarProps={{ row: navData }}
+      />
+    </Suspense>
   )
 }
