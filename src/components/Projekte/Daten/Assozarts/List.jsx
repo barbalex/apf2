b7853ba53d743cd3be99c6f84f30a46786df292f
Alexander Gabriel <alex.barbalex@gmail.com>
2025-10-26 18:53:26 +0100

    use suspense

--- src/components/Projekte/Daten/Assozarts/List.jsx
+++ src/components/Projekte/Daten/Assozarts/List.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, Suspense } from 'react'
 import { observer } from 'mobx-react-lite'
 
 import { MobxContext } from '../../../../mobxContext.js'
@@ -12,17 +12,17 @@ export const List = observer(() => {
   const store = useContext(MobxContext)
   const { nodeLabelFilter } = store.tree
 
-  const { navData, isLoading, error } = useAssozartsNavData()
-
-  if (isLoading) return <Spinner />
+  const { navData, error } = useAssozartsNavData()
 
   if (error) return <Error error={error} />
 
   return (
-    <SharedList
-      navData={navData}
-      MenuBarComponent={Menu}
-      highlightSearchString={nodeLabelFilter.assozart}
-    />
+    <Suspense fallback={<Spinner />}>
+      <SharedList
+        navData={navData}
+        MenuBarComponent={Menu}
+        highlightSearchString={nodeLabelFilter.assozart}
+      />
+    </Suspense>
   )
 })
--- src/components/Projekte/Daten/Tpopfeldkontrs/List.jsx
+++ src/components/Projekte/Daten/Tpopfeldkontrs/List.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, Suspense } from 'react'
 import { observer } from 'mobx-react-lite'
 
 import { MobxContext } from '../../../../mobxContext.js'
@@ -12,17 +12,17 @@ export const List = observer(() => {
   const store = useContext(MobxContext)
   const { nodeLabelFilter } = store.tree
 
-  const { navData, isLoading, error } = useTpopfeldkontrsNavData()
-
-  if (isLoading) return <Spinner />
+  const { navData, error } = useTpopfeldkontrsNavData()
 
   if (error) return <Error error={error} />
 
   return (
-    <SharedList
-      navData={navData}
-      MenuBarComponent={Menu}
-      highlightSearchString={nodeLabelFilter.tpopkontr}
-    />
+    <Suspense fallback={<Spinner />}>
+      <SharedList
+        navData={navData}
+        MenuBarComponent={Menu}
+        highlightSearchString={nodeLabelFilter.tpopkontr}
+      />
+    </Suspense>
   )
 })
