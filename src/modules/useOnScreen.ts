a6ae4891db8ecd21544b37529cf1d4ae4ae41e37
Alexander Gabriel <alex.barbalex@gmail.com>
2025-10-26 11:44:42 +0100

    de-memoize

--- src/modules/useOnScreen.ts
+++ src/modules/useOnScreen.ts
@@ -1,15 +1,12 @@
-import { useEffect, useMemo, useState, RefObject } from 'react'
+import { useEffect, useState, RefObject } from 'react'
+
 // https://stackoverflow.com/a/65008608/712005
 export const useOnScreen = (ref: RefObject<HTMLElement>) => {
   const [isIntersecting, setIntersecting] = useState(false)
 
-  const observer = useMemo(
-    () =>
-      new IntersectionObserver(
-        ([entry]) => setIntersecting(entry.isIntersecting),
-        { threshold: 0, rootMargin: '120px 0px 120px 0px' },
-      ),
-    [ref],
+  const observer = new IntersectionObserver(
+    ([entry]) => setIntersecting(entry.isIntersecting),
+    { threshold: 0, rootMargin: '120px 0px 120px 0px' },
   )
 
   useEffect(() => {
