a50d6bd62e7842a40f937efe3c922ce6cf337ef8
Alexander Gabriel <alex.barbalex@gmail.com>
2025-10-26 18:53:05 +0100

    use suspense

--- src/components/Projekte/Daten/CurrentIssues/List.jsx
+++ src/components/Projekte/Daten/CurrentIssues/List.jsx
@@ -1,16 +1,18 @@
+import { Suspense } from 'react'
+
 import { useCurrentissuesNavData } from '../../../../modules/useCurrentissuesNavData.js'
 import { List as SharedList } from '../../../shared/List/index.jsx'
 import { Spinner } from '../../../shared/Spinner.jsx'
 import { Error } from '../../../shared/Error.jsx'
 
 export const List = () => {
-  const { navData, isLoading, error } = useCurrentissuesNavData()
-  const currentissues = navData?.data?.allCurrentissues?.nodes ?? []
-  const totalCount = navData?.data?.allCurrentissues?.totalCount ?? 0
-
-  if (isLoading) return <Spinner />
+  const { navData, error } = useCurrentissuesNavData()
 
   if (error) return <Error error={error} />
 
-  return <SharedList navData={navData} />
+  return (
+    <Suspense fallback={<Spinner />}>
+      <SharedList navData={navData} />
+    </Suspense>
+  )
 }
--- src/components/Projekte/Daten/Popmassnbers/List.jsx
+++ src/components/Projekte/Daten/Popmassnbers/List.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, Suspense } from 'react'
 import { observer } from 'mobx-react-lite'
 
 import { MobxContext } from '../../../../mobxContext.js'
@@ -12,17 +12,17 @@ export const List = observer(() => {
   const store = useContext(MobxContext)
   const { nodeLabelFilter } = store.tree
 
-  const { navData, isLoading, error } = usePopmassnbersNavData()
-
-  if (isLoading) return <Spinner />
+  const { navData, error } = usePopmassnbersNavData()
 
   if (error) return <Error error={error} />
 
   return (
-    <SharedList
-      navData={navData}
-      MenuBarComponent={Menu}
-      highlightSearchString={nodeLabelFilter.popmassnber}
-    />
+    <Suspense fallback={<Spinner />}>
+      <SharedList
+        navData={navData}
+        MenuBarComponent={Menu}
+        highlightSearchString={nodeLabelFilter.popmassnber}
+      />
+    </Suspense>
   )
 })
