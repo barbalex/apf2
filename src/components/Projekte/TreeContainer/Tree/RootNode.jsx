7047889ad72dab520ac8fd29c6e53e360f0190f2
Alexander Gabriel <alex.barbalex@gmail.com>
2025-10-26 13:11:52 +0100

    use suspense

--- src/components/Projekte/TreeContainer/Tree/RootNode.jsx
+++ src/components/Projekte/TreeContainer/Tree/RootNode.jsx
@@ -1,10 +1,13 @@
+import { Suspense } from 'react'
 import { observer } from 'mobx-react-lite'
 
 export const RootNode = observer(({ fetcher }) => {
   const res = fetcher?.()
   const navData = res?.navData
 
-  if (!navData) return null
-
-  return <navData.component menu={navData} />
+  return (
+    <Suspense fallback={null}>
+      <navData.component menu={navData} />
+    </Suspense>
+  )
 })
