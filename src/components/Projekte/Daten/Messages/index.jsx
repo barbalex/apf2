79439a50a8553a82a7aec8a9ba9744636100dd7d
Alexander Gabriel <alex.barbalex@gmail.com>
2025-10-28 23:06:56 +0100

    use css module

--- src/components/Projekte/Daten/Messages/index.jsx
+++ src/components/Projekte/Daten/Messages/index.jsx
@@ -1,4 +1,3 @@
-import styled from '@emotion/styled'
 import { useQuery } from '@apollo/client/react'
 import Linkify from 'react-linkify'
 import { DateTime } from 'luxon'
@@ -8,46 +7,14 @@ import { ErrorBoundary } from '../../../shared/ErrorBoundary.jsx'
 import { Error } from '../../../shared/Error.jsx'
 import { Spinner } from '../../../shared/Spinner.jsx'
 
-const Container = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  overflow: hidden;
-`
-const ScrollContainer = styled.div`
-  flex-grow: 1;
-  display: flex;
-  flex-direction: column;
-  overflow: hidden;
-  overflow-y: auto;
-  scrollbar-width: thin;
-  padding: 10px;
-  height: 100%;
-`
-const MessageRow = styled.div`
-  display: flex;
-  padding: 10px;
-  border-bottom: solid #f0f0f0;
-`
-const Date = styled.div`
-  flex-basis: 110px;
-  flex-shrink: 0;
-`
-const Message = styled.div``
-const TitleRow = styled.div`
-  display: flex;
-  justify-content: space-between;
-  background-color: #388e3c;
-  padding-bottom: 10px;
-  flex-shrink: 0;
-  display: flex;
-  flex-direction: column;
-`
-const Title = styled.div`
-  padding: 10px 10px 0 10px;
-  color: white;
-  font-weight: bold;
-`
+import {
+  container,
+  scrollContainer,
+  messageRow,
+  date as dateClass,
+  titleRow,
+  title,
+} from './index.module.css'
 
 export const Component = () => {
   const { data, loading, error } = useQuery(query)
@@ -60,27 +27,35 @@ export const Component = () => {
 
   return (
     <ErrorBoundary>
-      <Container>
-        <TitleRow>
-          <Title data-id="form-title">Mitteilungen</Title>
-        </TitleRow>
-        <ScrollContainer>
+      <div className={container}>
+        <div className={titleRow}>
+          <div
+            className={title}
+            data-id="form-title"
+          >
+            Mitteilungen
+          </div>
+        </div>
+        <div className={scrollContainer}>
           {rows.map((m) => {
             const date = DateTime.fromISO(m.time).toFormat('yyyy.LL.dd')
 
             return (
-              <MessageRow key={m.id}>
-                <Date>{date}</Date>
-                <Message>
+              <div
+                className={messageRow}
+                key={m.id}
+              >
+                <div className={dateClass}>{date}</div>
+                <div>
                   <Linkify properties={{ target: '_blank' }}>
                     {m.message}
                   </Linkify>
-                </Message>
-              </MessageRow>
+                </div>
+              </div>
             )
           })}
-        </ScrollContainer>
-      </Container>
+        </div>
+      </div>
     </ErrorBoundary>
   )
 }
