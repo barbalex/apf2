eddd1d6c1d7672e7f1ae84c456616f034ee2a27b
Alexander Gabriel <alex.barbalex@gmail.com>
2025-10-28 17:32:13 +0100

    use css module

--- src/components/Projekte/Daten/ApAuswertung/PopMenge/CustomTooltip.jsx
+++ src/components/Projekte/Daten/ApAuswertung/PopMenge/CustomTooltip.jsx
@@ -1,30 +1,8 @@
-import styled from '@emotion/styled'
 import { sortBy } from 'es-toolkit'
 
 import { exists } from '../../../../../modules/exists.js'
+import { popup, title, row, label } from './CustomTooltip.module.css'
 
-const Popup = styled.div`
-  background-color: white;
-  border: 1px solid #2e7d32;
-  opacity: 0.8;
-  padding: 8px;
-`
-const Title = styled.div`
-  font-size: 1em;
-  font-weight: 700;
-`
-const Row = styled.div`
-  font-size: 0.8em;
-  font-weight: 700;
-  display: flex;
-  flex-wrap: nowrap;
-  justify-content: space-between;
-  color: ${(props) => props.color};
-`
-const Label = styled.div`
-  padding-right: 5px;
-`
-const Value = styled.div``
 const colorUrspruenglich = '#2e7d32'
 const colorAngesiedelt = 'rgba(245,141,66,1)'
 
@@ -38,18 +16,21 @@ export const CustomTooltip = ({ payload = [], label, active, popsData }) => {
   ])
 
   return (
-    <Popup>
-      <Title>{label}</Title>
+    <div className={popup}>
+      <div className={title}>{label}</div>
       {payloadSorted.map((p, i) => {
         const pop = popsData.find((d) => d.id === p.dataKey)
+
         let label = p.dataKey
         if (pop) {
           label = `${pop.nr || '(keine Nr)'}: ${pop.name || '(kein Name)'}`
         }
+
         const value =
           exists(p.value) && p.value?.toLocaleString ?
             p.value?.toLocaleString('de-ch')
           : null
+
         let color
         if (!pop) {
           color = 'grey'
@@ -57,16 +38,18 @@ export const CustomTooltip = ({ payload = [], label, active, popsData }) => {
           const isUrspruenglich = pop?.status < 200
           color = isUrspruenglich ? colorUrspruenglich : colorAngesiedelt
         }
+
         return (
-          <Row
+          <div
+            className={row}
             key={p.dataKey}
-            color={color}
+            style={{ color }}
           >
-            <Label>{`${label}:`}</Label>
-            <Value>{value}</Value>
-          </Row>
+            <div className={label}>{`${label}:`}</div>
+            <div>{value}</div>
+          </div>
         )
       })}
-    </Popup>
+    </div>
   )
 }
