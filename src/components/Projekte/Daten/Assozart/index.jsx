3d9118ea69791aaf68dafa2fcdfe5533d4371b49
Alexander Gabriel <alex.barbalex@gmail.com>
2025-10-28 19:36:31 +0100

    use css module

--- src/components/Projekte/Daten/Assozart/index.jsx
+++ src/components/Projekte/Daten/Assozart/index.jsx
@@ -1,5 +1,4 @@
 import { useContext, useState } from 'react'
-import styled from '@emotion/styled'
 import { observer } from 'mobx-react-lite'
 import { gql } from '@apollo/client'
 import { useApolloClient, useQuery } from '@apollo/client/react'
@@ -19,21 +18,7 @@ import { Error } from '../../../shared/Error.jsx'
 import { Spinner } from '../../../shared/Spinner.jsx'
 import { Menu } from './Menu.jsx'
 
-const Container = styled.div`
-  flex-grow: 1;
-  display: flex;
-  flex-direction: column;
-  overflow: hidden;
-`
-const FormContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  flex-grow: 1;
-  overflow: hidden;
-  overflow-y: auto;
-  scrollbar-width: thin;
-  padding: 10px;
-`
+import { container, formContainer } from './index.module.css'
 
 const fieldTypes = {
   bemerkungen: 'String',
@@ -138,12 +123,15 @@ export const Component = observer(() => {
 
   return (
     <ErrorBoundary>
-      <Container data-id="assozart">
+      <div
+        className={container}
+        data-id="assozart"
+      >
         <FormTitle
           title="assoziierte Art"
           MenuBarComponent={Menu}
         />
-        <FormContainer>
+        <div className={formContainer}>
           <SelectLoadingOptions
             field="aeId"
             valueLabelPath="aeTaxonomyByAeId.taxArtName"
@@ -164,8 +152,8 @@ export const Component = observer(() => {
             saveToDb={saveToDb}
             error={fieldErrors.bemerkungen}
           />
-        </FormContainer>
-      </Container>
+        </div>
+      </div>
     </ErrorBoundary>
   )
 })
