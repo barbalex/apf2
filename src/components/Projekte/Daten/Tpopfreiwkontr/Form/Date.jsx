da4cb62acf27e63abca1d5470a28dbdf74dff7f0
Alexander Gabriel <alex.barbalex@gmail.com>
2025-10-29 21:04:31 +0100

    use css module

--- src/components/Projekte/Daten/Tpopfreiwkontr/Form/Date.jsx
+++ src/components/Projekte/Daten/Tpopfreiwkontr/Form/Date.jsx
@@ -1,43 +1,10 @@
-import styled from '@emotion/styled'
 import { DateField as DateFieldComponent } from '../../../../shared/Date.jsx'
-
-const Area = styled.div`
-  border: 1px solid rgba(0, 0, 0, 0.5);
-  border-radius: 6px;
-  padding: 10px;
-  break-inside: avoid;
-`
-const Container = styled(Area)`
-  grid-area: date;
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-template-areas: 'dateLabel dateVal dateVal';
-  grid-column-gap: 10px;
-  align-items: center;
-`
-const Label = styled.div`
-  font-weight: 700;
-`
-const DateLabel = styled(Label)`
-  grid-area: dateLabel;
-`
-const DateVal = styled.div`
-  grid-area: dateVal;
-  > div {
-    margin-top: 5px;
-    padding-bottom: 0 !important;
-  }
-  @media print {
-    input {
-      font-size: 11px;
-    }
-  }
-`
+import { container, label, val } from './Date.module.css'
 
 export const DateField = ({ saveToDb, row, errors }) => (
-  <Container>
-    <DateLabel>Aufnahme-datum</DateLabel>
-    <DateVal>
+  <div className={container}>
+    <div className={label}>Aufnahme-datum</div>
+    <div className={val}>
       <DateFieldComponent
         key={`${row.id}datum`}
         name="datum"
@@ -45,6 +12,6 @@ export const DateField = ({ saveToDb, row, errors }) => (
         saveToDb={saveToDb}
         error={errors.datum}
       />
-    </DateVal>
-  </Container>
+    </div>
+  </div>
 )
