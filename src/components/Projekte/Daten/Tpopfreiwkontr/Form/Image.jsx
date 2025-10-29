70e9d6e0af2cde95e5651514e79ac60f6fe6faea
Alexander Gabriel <alex.barbalex@gmail.com>
2025-10-29 21:14:52 +0100

    use css module

--- src/components/Projekte/Daten/Tpopfreiwkontr/Form/Image.jsx
+++ src/components/Projekte/Daten/Tpopfreiwkontr/Form/Image.jsx
@@ -1,45 +1,12 @@
 import { useState } from 'react'
-import styled from '@emotion/styled'
 
-const Container = styled.div`
-  grid-area: image;
-  border: 1px solid rgba(0, 0, 0, 0.5);
-  border-radius: 6px;
-  padding: 10px;
-  break-inside: avoid;
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-template-rows: 1fr 20fr;
-  grid-template-areas:
-    'title'
-    'myImage';
-  container-type: size;
-  min-height: 250px;
-  @media print {
-    -webkit-print-color-adjust: exact;
-  }
-`
-
-const ImageContainer = styled.div`
-  grid-area: myImage;
-  height: calc(100cqh - 20px);
-`
-const Img = styled.img`
-  height: 100%;
-  width: 100%;
-  object-fit: contain;
-`
-const Title = styled.div`
-  grid-area: title;
-  font-weight: 700;
-  justify-self: center;
-`
-const NotifContainer = styled.div`
-  grid-area: myImage;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`
+import {
+  container,
+  imageContainer,
+  img,
+  title,
+  notifContainer,
+} from './Image.module.css'
 
 export const Image = ({ artname, apId }) => {
   const [notif, setNotif] = useState(null)
@@ -50,24 +17,25 @@ export const Image = ({ artname, apId }) => {
   const onError = () => setNotif(`Für ${artname} wurde kein Bild gefunden`)
 
   return (
-    <Container>
-      <Title>{artname}</Title>
+    <div className={container}>
+      <div className={title}>{artname}</div>
       {!notif && (
-        <ImageContainer>
+        <div className={imageContainer}>
           <picture>
             <source
               srcSet={`/${apId}.avif`}
               type="image/avif"
             />
-            <Img
+            <img
+              className={img}
               src={`/${apId}.webp`}
               onError={onError}
               alt={artname}
             />
           </picture>
-        </ImageContainer>
+        </div>
       )}
-      {!!notif && <NotifContainer>{notif}</NotifContainer>}
-    </Container>
+      {!!notif && <div className={notifContainer}>{notif}</div>}
+    </div>
   )
 }
