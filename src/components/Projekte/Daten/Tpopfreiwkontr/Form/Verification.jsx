b9701beb4c327bb13029f59b5b6c34a2cf83f639
Alexander Gabriel <alex.barbalex@gmail.com>
2025-10-29 22:44:35 +0100

    use css module

--- src/components/Projekte/Daten/Tpopfreiwkontr/Form/Verification.jsx
+++ src/components/Projekte/Daten/Tpopfreiwkontr/Form/Verification.jsx
@@ -1,48 +1,11 @@
-import styled from '@emotion/styled'
-
 import { RadioButton } from '../../../../shared/RadioButton.jsx'
 import { TextField2 } from '../../../../shared/TextField2.jsx'
 
-const Area = styled.div`
-  border: 1px solid rgba(0, 0, 0, 0.5);
-  border-radius: 6px;
-  border-color: red;
-  padding: 10px;
-  break-inside: avoid;
-  legend,
-  label {
-    color: rgba(0, 0, 0, 0.87) !important;
-    font-weight: bold;
-  }
-`
-const Container = styled(Area)`
-  grid-area: verification;
-  display: grid;
-  grid-template-columns: 220px 1fr;
-  grid-template-areas:
-    'title title'
-    'relevant grund';
-  grid-column-gap: 10px;
-`
-const Relevant = styled.div`
-  grid-area: relevant;
-  fieldset {
-    border-collapse: separate;
-  }
-  fieldset > legend {
-    padding-top: 0 !important;
-  }
-`
-const Grund = styled.div`
-  grid-area: grund;
-  > div {
-    margin-bottom: -25px;
-  }
-`
+import { container, relevant, grund } from './Verification.module.css'
 
 export const Verification = ({ saveToDb, row, errors }) => (
-  <Container>
-    <Relevant>
+  <div className={container}>
+    <div className={relevant}>
       <RadioButton
         key={`${row.id}apberNichtRelevant`}
         name="apberNichtRelevant"
@@ -51,8 +14,8 @@ export const Verification = ({ saveToDb, row, errors }) => (
         saveToDb={saveToDb}
         error={errors.apberNichtRelevant}
       />
-    </Relevant>
-    <Grund>
+    </div>
+    <div className={grund}>
       <TextField2
         key={`${row.id}apberNichtRelevantGrund`}
         name="apberNichtRelevantGrund"
@@ -63,6 +26,6 @@ export const Verification = ({ saveToDb, row, errors }) => (
         saveToDb={saveToDb}
         errors={errors}
       />
-    </Grund>
-  </Container>
+    </div>
+  </div>
 )
