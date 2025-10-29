dd3e176600092d87113b78fff77617305166115c
Alexander Gabriel <alex.barbalex@gmail.com>
2025-10-29 21:10:41 +0100

    use css module

--- src/components/Projekte/Daten/Tpopfreiwkontr/Form/Files.jsx
+++ src/components/Projekte/Daten/Tpopfreiwkontr/Form/Files.jsx
@@ -1,30 +1,14 @@
-import styled from '@emotion/styled'
-
 import { FilesRouter } from '../../../../shared/Files/index.jsx'
-
-const Container = styled.div`
-  border: 1px solid rgba(0, 0, 0, 0.5);
-  border-radius: 6px;
-  padding: 10px;
-  break-inside: avoid;
-  grid-area: files;
-`
-const RemarksLabel = styled.div`
-  font-weight: 700;
-  margin-bottom: 10px;
-`
-const RemarksVal = styled.div`
-  margin-bottom: -10px;
-`
+import { container, label, val } from './Files.module.css'
 
 export const Files = ({ row }) => (
-  <Container>
-    <RemarksLabel>Dateien</RemarksLabel>
-    <RemarksVal>
+  <div className={container}>
+    <div className={label}>Dateien</div>
+    <div className={val}>
       <FilesRouter
         parentId={row.id}
         parent="tpopkontr"
       />
-    </RemarksVal>
-  </Container>
+    </div>
+  </div>
 )
