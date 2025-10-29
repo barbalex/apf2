8d8b332e0e8bac394ae178940e5d26eb9b7064c6
Alexander Gabriel <alex.barbalex@gmail.com>
2025-10-29 11:39:28 +0100

    use css module

--- src/components/Projekte/Daten/Popmassnber/index.jsx
+++ src/components/Projekte/Daten/Popmassnber/index.jsx
@@ -1,5 +1,4 @@
 import { useContext, useState } from 'react'
-import styled from '@emotion/styled'
 import { observer } from 'mobx-react-lite'
 import { gql } from '@apollo/client'
 import { useApolloClient, useQuery } from '@apollo/client/react'
@@ -22,21 +21,7 @@ import {
 } from '../../../shared/fragments.js'
 import { Menu } from './Menu.jsx'
 
-const Container = styled.div`
-  flex-grow: 1;
-  display: flex;
-  flex-direction: column;
-  overflow: hidden;
-`
-const FormContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  flex-grow: 1;
-  overflow: hidden;
-  overflow-y: auto;
-  scrollbar-width: thin;
-  padding: 10px;
-`
+import { container, formContainer } from './index.module.css'
 
 const fieldTypes = {
   popId: 'UUID',
@@ -123,12 +108,12 @@ export const Component = observer(() => {
 
   return (
     <ErrorBoundary>
-      <Container>
+      <div className={container}>
         <FormTitle
           title="Massnahmen-Bericht Population"
           MenuBarComponent={Menu}
         />
-        <FormContainer>
+        <div className={formContainer}>
           <TextField
             name="jahr"
             label="Jahr"
@@ -155,8 +140,8 @@ export const Component = observer(() => {
             saveToDb={saveToDb}
             error={fieldErrors.bemerkungen}
           />
-        </FormContainer>
-      </Container>
+        </div>
+      </div>
     </ErrorBoundary>
   )
 })
