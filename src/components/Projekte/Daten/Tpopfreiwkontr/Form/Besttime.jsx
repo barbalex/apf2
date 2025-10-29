b5fa8ec56a4be225c8b624c0fc7805fe9d9f69ff
Alexander Gabriel <alex.barbalex@gmail.com>
2025-10-29 20:41:15 +0100

    use css module

--- src/components/Projekte/Daten/Tpopfreiwkontr/Form/Besttime.jsx
+++ src/components/Projekte/Daten/Tpopfreiwkontr/Form/Besttime.jsx
@@ -1,34 +1,10 @@
-import styled from '@emotion/styled'
-
-const Area = styled.div`
-  border: 1px solid rgba(0, 0, 0, 0.5);
-  border-radius: 6px;
-  padding: 10px;
-  break-inside: avoid;
-`
-const Container = styled(Area)`
-  grid-area: besttime;
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-template-areas: 'besttimeLabel besttimeVal besttimeVal';
-  grid-column-gap: 10px;
-  align-items: center;
-`
-const Label = styled.div`
-  font-weight: 700;
-`
-const BesttimeLabel = styled(Label)`
-  grid-area: besttimeLabel;
-`
-const BesttimeVal = styled.div`
-  grid-area: besttimeVal;
-`
+import { container, label, val } from './Besttime.module.css'
 
 export const Besttime = ({ row }) => (
-  <Container>
-    <BesttimeLabel>bester Beobachtungs-Zeitpunkt</BesttimeLabel>
-    <BesttimeVal>
+  <div className={container}>
+    <div className={label}>Bester Beobachtungs-Zeitpunkt</div>
+    <div className={val}>
       {row?.tpopByTpopId?.popByPopId?.apByApId?.ekfBeobachtungszeitpunkt ?? ''}
-    </BesttimeVal>
-  </Container>
+    </div>
+  </div>
 )
