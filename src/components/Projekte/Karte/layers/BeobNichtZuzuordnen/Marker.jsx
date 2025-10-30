7620109b336b455eee379de49e6d77cb6a6db4e9
Alexander Gabriel <alex.barbalex@gmail.com>
2025-10-30 18:12:24 +0100

    use css module

--- src/components/Projekte/Karte/layers/BeobNichtZuzuordnen/Marker.jsx
+++ src/components/Projekte/Karte/layers/BeobNichtZuzuordnen/Marker.jsx
@@ -2,7 +2,6 @@ import { useContext } from 'react'
 import { Marker as LeafletMarker, Popup } from 'react-leaflet'
 import { format } from 'date-fns/format'
 import { isValid } from 'date-fns/isValid'
-import styled from '@emotion/styled'
 import { observer } from 'mobx-react-lite'
 import Button from '@mui/material/Button'
 import { useParams, useLocation } from 'react-router'
@@ -16,18 +15,7 @@ import { appBaseUrl } from '../../../../../modules/appBaseUrl.js'
 import { useProjekteTabs } from '../../../../../modules/useProjekteTabs.js'
 import { Data } from '../BeobData/index.jsx'
 
-const StyledH3 = styled.h3`
-  margin: 7px 0;
-`
-const StyledButton = styled(Button)`
-  text-transform: none;
-  justify-content: left;
-  padding: 2px 0;
-`
-const AbsenzDiv = styled.div`
-  color: red;
-  font-weight: bold;
-`
+import { h3, button, absenz } from '../BeobNichtBeurteilt/Marker.module.css'
 
 export const Marker = observer(({ beob }) => {
   const { apId, projId, beobId } = useParams()
@@ -99,32 +87,34 @@ export const Marker = observer(({ beob }) => {
             beob?.aeTaxonomyByArtId?.artname ?? ''
           }`}</div>
           {beob?.absenz ?
-            <AbsenzDiv>Absenzmeldung</AbsenzDiv>
+            <div className={absenz}>Absenzmeldung</div>
           : null}
-          <StyledH3>{label}</StyledH3>
+          <h3 className={h3}>{label}</h3>
           <div>
             {`Koordinaten: ${beob.lv95X?.toLocaleString(
               'de-ch',
             )} / ${beob.lv95Y?.toLocaleString('de-ch')}`}
           </div>
-          <StyledButton
+          <Button
             size="small"
             variant="text"
             onClick={openBeobInTab}
             color="inherit"
             fullWidth
+            className={button}
           >
             Formular in neuem Fenster öffnen
-          </StyledButton>
-          <StyledButton
+          </Button>
+          <Button
             size="small"
             variant="text"
             onClick={openBeobInTree2}
             color="inherit"
             fullWidth
+            className={button}
           >
             Formular in Navigationsbaum 2 öffnen
-          </StyledButton>
+          </Button>
           <Data id={beob.id} />
         </>
       </Popup>
