69f4055d12730ac20f8eb5bea02c8c396566cc4e
Alexander Gabriel <alex.barbalex@gmail.com>
2025-10-30 17:48:32 +0100

    use css module

--- src/components/Projekte/Karte/CoordinatesControl/ShowCoordinates.jsx
+++ src/components/Projekte/Karte/CoordinatesControl/ShowCoordinates.jsx
@@ -1,30 +1,9 @@
 import { useContext } from 'react'
 import 'leaflet'
-import styled from '@emotion/styled'
 import { observer } from 'mobx-react-lite'
 
 import { MobxContext } from '../../../../mobxContext.js'
-
-const StyledDiv = styled.div`
-  background-color: transparent;
-  color: rgb(48, 48, 48);
-  font-weight: 700;
-  text-shadow:
-    0 1px 0 white,
-    -0 -1px 0 white,
-    1px 0 0 white,
-    -1px 0 0 white,
-    0 2px 1px white,
-    -0 -2px 1px white,
-    2px 0 1px white,
-    -2px 0 1px white,
-    0 3px 2px white,
-    -0 -3px 2px white,
-    3px 0 2px white,
-    -3px 0 2px white;
-  cursor: pointer;
-  margin-bottom: 2px !important;
-`
+import { div } from './ShowCoordinates.module.css'
 
 export const ShowCoordinates = observer(({ setControlType }) => {
   const { mapMouseCoordinates } = useContext(MobxContext)
@@ -34,11 +13,12 @@ export const ShowCoordinates = observer(({ setControlType }) => {
   const onClick = () => setControlType('goto')
 
   return (
-    <StyledDiv
+    <div
+      className={div}
       onClick={onClick}
       title="Klicken um Koordinaten zu suchen"
     >
       {`${x}, ${y}`}
-    </StyledDiv>
+    </div>
   )
 })
