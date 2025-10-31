31d5ade4a9233b149ee58783bd5cc3bd6bbe5c43
Alexander Gabriel <alex.barbalex@gmail.com>
2025-10-31 18:23:24 +0100

    use css module

--- src/components/Projekte/Karte/PngControl.jsx
+++ src/components/Projekte/Karte/PngControl.jsx
@@ -2,30 +2,12 @@ import { useState, useEffect, useContext } from 'react'
 import 'leaflet'
 import { useMap } from 'react-leaflet'
 import 'leaflet-easyprint'
-import styled from '@emotion/styled'
 import { MdGetApp } from 'react-icons/md'
 import { observer } from 'mobx-react-lite'
 
 import { MobxContext } from '../../../mobxContext.js'
 
-const FileDownloadIcon = styled(MdGetApp)`
-  font-size: 1.5rem;
-`
-const StyledButton = styled.button`
-  background-color: white;
-  width: 34px;
-  height: 34px;
-  border-radius: 4px;
-  border: 2px solid rgba(0, 0, 0, 0.2);
-  background-clip: padding-box;
-  cursor: pointer;
-  svg {
-    margin-left: -2px;
-    padding-top: 2px;
-    color: rgba(0, 0, 0, 0.7) !important;
-  }
-  margin-top: 7px;
-`
+import { icon, button } from './PngControl.module.css'
 
 const options = {
   hidden: true,
@@ -67,11 +49,12 @@ export const PngControl = observer(() => {
   }
 
   return (
-    <StyledButton
+    <button
       onClick={savePng}
       title="Karte als png speichern"
+      className={button}
     >
-      <FileDownloadIcon />
-    </StyledButton>
+      <MdGetApp className={icon} />
+    </button>
   )
 })
