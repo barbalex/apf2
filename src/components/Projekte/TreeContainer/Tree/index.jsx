471733004a4c279f414af89cd27988b3f3eca9e2
Alexander Gabriel <alex.barbalex@gmail.com>
2025-10-31 18:35:37 +0100

    use css module

--- src/components/Projekte/TreeContainer/Tree/index.jsx
+++ src/components/Projekte/TreeContainer/Tree/index.jsx
@@ -1,27 +1,13 @@
-import styled from '@emotion/styled'
-
 import { Root } from './Root.jsx'
 import { IntoViewScroller } from './IntoViewScroller.jsx'
 import { Menu } from './Menu.jsx'
 
-const Container = styled.div`
-  height: calc(100% - 53px - 8px);
-  width: 100%;
-  overflow: auto;
-  scrollbar-width: thin;
-  position: relative;
-  contain: paint layout style;
-  ul {
-    margin: 0;
-    list-style: none;
-    padding: 0 0 0 1.1em;
-  }
-`
+import { container } from './index.module.css'
 
 export const TreeComponent = () => (
-  <Container>
+  <div className={container}>
     <Root />
     <IntoViewScroller />
     <Menu />
-  </Container>
+  </div>
 )
