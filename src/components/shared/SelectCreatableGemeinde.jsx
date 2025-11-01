ad50a60fcb6fae00e0d25a3f5f409ca80d083475
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-01 10:36:28 +0100

    use css module

--- src/components/shared/SelectCreatableGemeinde.jsx
+++ src/components/shared/SelectCreatableGemeinde.jsx
@@ -1,85 +1,19 @@
 import { useState, useEffect } from 'react'
 import CreatableSelect from 'react-select/creatable'
-import styled from '@emotion/styled'
 import IconButton from '@mui/material/IconButton'
 import Tooltip from '@mui/material/Tooltip'
 import { IoMdLocate } from 'react-icons/io'
 
 import { exists } from '../../modules/exists.js'
-
-const AddLocationIcon = styled(IoMdLocate)`
-  font-size: 1.5rem;
-`
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 12px;
-`
-const Label = styled.div`
-  font-size: 12px;
-  color: ${(props) => (props.error ? '#f44336' : 'rgb(0, 0, 0, 0.54)')};
-`
-const Error = styled.div`
-  font-size: 12px;
-  color: #f44336;
-`
-const Field = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-content: stretch;
-`
-const StyledSelect = styled(CreatableSelect)`
-  width: 100%;
-  .react-select__control {
-    background-color: rgba(0, 0, 0, 0) !important;
-    border-bottom-color: rgba(0, 0, 0, 0.1);
-    border-top: none;
-    border-left: none;
-    border-right: none;
-    border-radius: 0;
-  }
-  .react-select__control:hover {
-    border-bottom-width: 2px;
-  }
-  .react-select__control:focus-within {
-    border-bottom-color: rgba(28, 80, 31, 1) !important;
-    box-shadow: none;
-  }
-  .react-select__value-container {
-    padding-left: 0;
-  }
-  .react-select__indicators {
-    @media print {
-      display: none;
-    }
-  }
-  .react-select__clear-indicator {
-    /* ability to hide caret when not enough space */
-    padding-right: ${(props) => (props.nocaret ? '0' : '8px')};
-  }
-  .react-select__dropdown-indicator {
-    /* ability to hide caret when not enough space */
-    display: ${(props) => (props.nocaret ? 'none' : 'flex')};
-  }
-  .react-select__indicator-separator {
-    /* ability to hide caret when not enough space */
-    width: ${(props) => (props.nocaret ? '0' : '1px')};
-  }
-  input {
-    @media print {
-      padding-top: 3px;
-      padding-bottom: 0;
-    }
-  }
-  .react-select__menu,
-  .react-select__menu-list {
-    height: 130px;
-    height: ${(props) => (props.maxheight ? `${props.maxheight}px` : 'unset')};
-  }
-`
-const StyledIconButton = styled(IconButton)`
-  margin-top: -5px !important;
-`
+import {
+  addLocationIcon,
+  container,
+  labelClass,
+  errorClass,
+  field,
+  select,
+  iconButton,
+} from './SelectCreatableGemeinde.module.css'
 
 export const SelectCreatableGemeinde = ({
   label,
@@ -147,13 +81,21 @@ export const SelectCreatableGemeinde = ({
     maxHeight ? { '--react-select-menu-list-max-height': `${maxHeight}px` } : {}
 
   return (
-    <Container
+    <div
+      className={container}
       data-id={name}
       style={styleMeantForSelect}
     >
-      {label && <Label error={!!error}>{label}</Label>}
-      <Field>
-        <StyledSelect
+      {label && (
+        <div
+          className={labelClass}
+          style={{ color: error ? '#f44336' : 'rgb(0, 0, 0, 0.54)' }}
+        >
+          {label}
+        </div>
+      )}
+      <div className={field}>
+        <CreatableSelect
           id={name}
           name={name}
           value={selectValue}
@@ -169,20 +111,21 @@ export const SelectCreatableGemeinde = ({
           maxheight={maxHeight}
           classNamePrefix="react-select"
           nocaret={noCaret}
-          className={`select-height-limited ${noCaret ? 'select-nocaret' : ''}`}
+          className={`select-height-limited ${noCaret ? 'select-nocaret' : ''} ${select}`}
         />
         {showLocate && (
           <Tooltip title="Mit Hilfe der Koordinaten automatisch setzen">
-            <StyledIconButton
+            <IconButton
               aria-label="Mit Hilfe der Koordinaten automatisch setzen"
               onClick={onClickLocate}
+              className={iconButton}
             >
-              <AddLocationIcon />
-            </StyledIconButton>
+              <IoMdLocate className={addLocationIcon} />
+            </IconButton>
           </Tooltip>
         )}
-      </Field>
-      {error && <Error>{error}</Error>}
-    </Container>
+      </div>
+      {error && <div className={errorClass}>{error}</div>}
+    </div>
   )
 }
