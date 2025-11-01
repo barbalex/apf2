5823aeb55152857e824bcf2930c5f41c9d756e38
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-01 10:27:46 +0100

    use css module

--- src/components/shared/SelectCreatable.jsx
+++ src/components/shared/SelectCreatable.jsx
@@ -1,75 +1,10 @@
 // seems not in use
 import { useState, useEffect } from 'react'
 import CreatableSelect from 'react-select/creatable'
-import styled from '@emotion/styled'
 import { observer } from 'mobx-react-lite'
 
 import { exists } from '../../modules/exists.js'
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 12px;
-`
-const Label = styled.div`
-  font-size: 12px;
-  color: rgb(0, 0, 0, 0.54);
-`
-const Error = styled.div`
-  font-size: 12px;
-  color: red;
-`
-const StyledSelect = styled(CreatableSelect)`
-  .react-select__control {
-    background-color: rgba(0, 0, 0, 0) !important;
-    border-bottom-color: rgba(0, 0, 0, 0.1);
-    border-top: none;
-    border-left: none;
-    border-right: none;
-    border-radius: 0;
-  }
-  .react-select__control:hover {
-    border-bottom-width: 2px;
-  }
-  .react-select__control:focus-within {
-    border-bottom-color: rgba(28, 80, 31, 1) !important;
-    box-shadow: none;
-  }
-  .react-select__value-container {
-    padding-left: 0;
-  }
-  .react-select__indicators {
-    @media print {
-      display: none;
-    }
-  }
-  .react-select__clear-indicator {
-    /* ability to hide caret when not enough space */
-    padding-right: ${(props) => (props.nocaret ? '0' : '8px')};
-  }
-  .react-select__dropdown-indicator {
-    /* ability to hide caret when not enough space */
-    display: ${(props) => (props.nocaret ? 'none' : 'flex')};
-  }
-  .react-select__indicator-separator {
-    /* ability to hide caret when not enough space */
-    width: ${(props) => (props.nocaret ? '0' : '1px')};
-  } /*
-  > div > div > div {
-    margin-left: 0;
-  }*/
-  input {
-    @media print {
-      padding-top: 3px;
-      padding-bottom: 0;
-    }
-  }
-  .react-select__menu,
-  .react-select__menu-list {
-    height: 130px;
-    height: ${(props) => (props.maxheight ? `${props.maxheight}px` : 'unset')};
-  }
-`
+import { container, labelClass, errorClass, select } from './Select.module.css'
 
 export const SelectCreatable = observer(
   ({
@@ -139,9 +74,12 @@ export const SelectCreatable = observer(
       : {}
 
     return (
-      <Container style={styleMeantForSelect}>
-        {label && <Label>{label}</Label>}
-        <StyledSelect
+      <div
+        className={container}
+        style={styleMeantForSelect}
+      >
+        {label && <div className={labelClass}>{label}</div>}
+        <CreatableSelect
           id={field}
           name={field}
           defaultValue={selectValue}
@@ -154,13 +92,11 @@ export const SelectCreatable = observer(
           isClearable
           isSearchable
           noOptionsMessage={() => '(keine)'}
-          maxheight={maxHeight}
           classNamePrefix="react-select"
-          nocaret={noCaret}
-          className={`select-height-limited ${noCaret ? 'select-nocaret' : ''}`}
+          className={`select-height-limited ${noCaret ? 'select-nocaret' : ''} ${select}`}
         />
-        {error && <Error>{error}</Error>}
-      </Container>
+        {error && <div className={errorClass}>{error}</div>}
+      </div>
     )
   },
 )
