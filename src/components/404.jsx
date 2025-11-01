71a002174b640d672525e117e88a074320886d1c
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-01 11:18:14 +0100

    use css module

--- src/components/404.jsx
+++ src/components/404.jsx
@@ -1,100 +1,17 @@
 import Typography from '@mui/material/Typography'
-import styled from '@emotion/styled'
 import Button from '@mui/material/Button'
 import { useNavigate, useLocation } from 'react-router'
 
-const Img = styled.img`
-  display: block;
-  height: 100%;
-  width: 100%;
-  object-fit: cover;
-`
-const OuterContainer = styled.div`
-  height: 100%;
-  width: 100%;
-  position: relative;
-  overflow: hidden;
-`
-const InnerContainer = styled.div`
-  height: 100%;
-  width: 100%;
-  position: absolute;
-  top: 0;
-  overflow-y: auto;
-  scrollbar-width: thin;
-  /* prevent layout shift when scrollbar appears */
-  scrollbar-gutter: stable;
-  color: black !important;
-`
-const CardContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 15px;
-  position: relative;
-  @media (min-width: 700px) {
-    padding: 20px;
-  }
-  @media (min-width: 1200px) {
-    padding: 25px;
-  }
-  @media (min-width: 1700px) {
-    padding: 25px;
-  }
-  p {
-    margin-bottom: 10px !important;
-  }
-  p:last-of-type {
-    margin-bottom: 0 !important;
-    margin-top: 10px !important;
-  }
-`
-const PageTitle = styled(Typography)`
-  font-size: 2em !important;
-  padding-top: 15px;
-  padding-bottom: 0;
-  font-weight: 700 !important;
-  text-shadow:
-    2px 2px 3px white,
-    -2px -2px 3px white,
-    2px -2px 3px white,
-    -2px 2px 3px white;
-  @media (min-width: 700px) {
-    padding-top: 20px;
-    padding-bottom: 5;
-  }
-  @media (min-width: 1200px) {
-    padding-top: 25px;
-    padding-bottom: 10px;
-  }
-  @media (min-width: 1700px) {
-    padding-top: 30px;
-    padding-bottom: 15px;
-  }
-`
-const TextContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  font-weight: 700 !important;
-  text-shadow:
-    2px 2px 3px white,
-    -2px -2px 3px white,
-    2px -2px 3px white,
-    -2px 2px 3px white;
-`
-const Text = styled(Typography)`
-  font-size: 1.5em !important;
-  padding: 15px;
-  font-weight: 700 !important;
-`
-const StyledButton = styled(Button)`
-  text-shadow:
-    2px 2px 3px white,
-    -2px -2px 3px white,
-    2px -2px 3px white,
-    -2px 2px 3px white;
-  border-color: white !important;
-  margin-top: 10px !important;
-`
+import {
+  img,
+  outerContainer,
+  innerContainer,
+  cardContainer,
+  pageTitle,
+  textContainer,
+  text,
+  button,
+} from './404.module.css'
 
 export const Component = () => {
   const { search } = useLocation()
@@ -102,55 +19,60 @@ export const Component = () => {
   const onClickBack = () => navigate(`/${search}`)
 
   return (
-    <OuterContainer>
+    <div className={outerContainer}>
       <picture>
         <source
           srcSet="home_700.avif 700w, home_1000.avif 1000w, home_1400.avif 1400w, home_2000.avif 2000w, home_2500.avif 2500w"
           type="image/avif"
         />
-        <Img
+        <img
+          className={img}
           src="home_700.webp"
           srcSet="home_700.webp 700w, home_1000.webp 1000w, home_1400.webp 1400w, home_2000.webp 2000w, home_2500.webp 2500w"
           sizes="100vw"
           alt="Spinnen-Ragwurz"
         />
       </picture>
-      <InnerContainer>
-        <PageTitle
+      <div className={innerContainer}>
+        <Typography
           align="center"
           variant="h6"
           color="inherit"
+          className={pageTitle}
         >
           Bedrohte Pflanzenarten fördern
-        </PageTitle>
-        <CardContainer>
-          <TextContainer>
-            <PageTitle
+        </Typography>
+        <div className={cardContainer}>
+          <div className={textContainer}>
+            <Typography
               align="center"
               variant="h6"
+              className={pageTitle}
             >
               Oh je
-            </PageTitle>
-          </TextContainer>
-          <TextContainer>
-            <Text
+            </Typography>
+          </div>
+          <div className={textContainer}>
+            <Typography
               align="center"
               variant="h6"
+              className={text}
             >
               Diese Seite ist nicht verfügbar.
-            </Text>
-          </TextContainer>
-          <TextContainer>
-            <StyledButton
+            </Typography>
+          </div>
+          <div className={textContainer}>
+            <Button
               variant="outlined"
               onClick={onClickBack}
               color="inherit"
+              className={button}
             >
-              Zurück zur Startseite
-            </StyledButton>
-          </TextContainer>
-        </CardContainer>
-      </InnerContainer>
-    </OuterContainer>
+              Zur Startseite
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
