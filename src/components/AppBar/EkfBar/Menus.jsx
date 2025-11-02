e2bbc193e12805d3d47a0a788ee22aa9bcd4ec63
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-02 23:51:36 +0100

    use css module

--- src/components/AppBar/EkfBar/Menus.jsx
+++ src/components/AppBar/EkfBar/Menus.jsx
@@ -1,5 +1,4 @@
 import { useContext, useState } from 'react'
-import styled from '@emotion/styled'
 import { keyframes } from '@emotion/react'
 import { jwtDecode } from 'jwt-decode'
 import { observer } from 'mobx-react-lite'
