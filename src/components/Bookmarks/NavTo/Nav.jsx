4240a77abfd8e12289469cd2e5af803a2ce5ed7f
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-03 13:37:50 +0100

    use css module

--- src/components/Bookmarks/NavTo/Nav.jsx
+++ src/components/Bookmarks/NavTo/Nav.jsx
@@ -1,22 +1,7 @@
 import { Link, useLocation } from 'react-router'
 import Tooltip from '@mui/material/Tooltip'
-import styled from '@emotion/styled'
 
-const StyledLink = styled(Link)`
-  text-decoration: none;
-  padding: 0 9px;
-  min-width: 50px;
-  max-width: 150px;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  border-right: ${(props) =>
-    props.borderright === 'true' ? 'rgba(46, 125, 50, 0.5) solid 1px' : 'none'};
-  &:hover {
-    text-decoration: underline;
-    text-decoration-color: rgba(55, 118, 28, 0.5);
-  }
-`
+import { link } from './Nav.module.css'
 
 export const Nav = ({ item, baseUrl, needsBorderRight = false }) => {
   const { pathname, search } = useLocation()
@@ -26,15 +11,19 @@ export const Nav = ({ item, baseUrl, needsBorderRight = false }) => {
 
   return (
     <Tooltip title={item.label}>
-      <StyledLink
+      <Link
         to={{
           pathname: `${baseUrl ?? pathnameWithoutLastSlash}/${item.id}`,
           search,
         }}
-        borderright={needsBorderRight.toString()}
+        style={{
+          borderRight:
+            needsBorderRight ? 'rgba(46, 125, 50, 0.5) solid 1px' : 'none',
+        }}
+        className={link}
       >
         {item.label}
-      </StyledLink>
+      </Link>
     </Tooltip>
   )
 }
