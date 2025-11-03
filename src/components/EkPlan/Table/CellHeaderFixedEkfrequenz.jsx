8114e00feff5240461133e02990de75d14cd073b
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-03 10:53:00 +0100

    use css module

--- src/components/EkPlan/Table/CellHeaderFixedEkfrequenz.jsx
+++ src/components/EkPlan/Table/CellHeaderFixedEkfrequenz.jsx
@@ -8,6 +8,15 @@ import { observer } from 'mobx-react-lite'
 import { MobxContext } from '../../../mobxContext.js'
 import { TextFilter } from './CellHeaderFixed/TextFilter.jsx'
 
+import {
+  cell,
+  title,
+  dropdown,
+  faFilter,
+  textFilterContainer,
+} from './CellHeaderFixedEkfrequenz.module.css'
+
+// TODO: replace imports
 export const StyledCell = styled.div`
   display: flex;
   justify-content: space-between;
@@ -22,33 +31,11 @@ export const StyledCell = styled.div`
   padding: 0 4px;
   cursor: pointer;
   box-sizing: border-box;
-  width: ${(props) => props.width}px;
-  min-width: ${(props) => props.width}px;
   height: 60px;
   &.column-hovered {
     background: hsla(120, 25%, 82%, 1) !important;
     font-weight: 700 !important;
   }
-`
-const Title = styled.div`
-  text-align: left;
-  display: inline-block;
-  vertical-align: middle;
-  line-height: normal;
-  padding: 2px 0;
-  margin-top: auto;
-  margin-bottom: auto;
-  user-select: none;
-`
-const Dropdown = styled.div`
-  font-size: 1.3em;
-  padding-left: 2px;
-`
-const StyledFaFilter = styled(FaFilter)`
-  font-size: 0.8em;
-`
-const TextFilterContainer = styled.div`
-  padding: 0 16px;
 `
 const StyledMenu = styled(Menu)`
   & .MuiPaper-root {
@@ -84,19 +71,24 @@ export const CellHeaderFixedEkfrequenz = observer(({ column }) => {
 
   return (
     <>
-      <StyledCell
+      <div
+        className={cell}
         aria-controls="ekfrequenzHeaderMenu"
         aria-haspopup="true"
         onClick={onClickCell}
         width={column.width}
+        style={{
+          width: column.width,
+          minWidth: column.width,
+        }}
       >
-        <Title>{label}</Title>
-        <Dropdown>
+        <div className={title}>{label}</div>
+        <div className={dropdown}>
           {filterEkfrequenzEmpty || filterEkfrequenz ?
-            <StyledFaFilter />
+            <FaFilter className={faFilter} />
           : <Caret />}
-        </Dropdown>
-      </StyledCell>
+        </div>
+      </div>
       <StyledMenu
         anchorEl={anchorEl}
         open={Boolean(anchorEl)}
@@ -111,12 +103,12 @@ export const CellHeaderFixedEkfrequenz = observer(({ column }) => {
             'nicht Leerwerte filtern'
           : 'Leerwerte filtern'}
         </MenuItem>
-        <TextFilterContainer>
+        <div className={textFilterContainer}>
           <TextFilter
             column={column}
             closeMenu={closeMenu}
           />
-        </TextFilterContainer>
+        </div>
       </StyledMenu>
     </>
   )
