4dc06e5a8bb55edab8b455420489325391eaf61d
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-03 18:45:06 +0100

    use css module

--- src/components/Ekf/List/Item.jsx
+++ src/components/Ekf/List/Item.jsx
@@ -1,34 +1,6 @@
-import styled from '@emotion/styled'
 import { useNavigate, useParams, useLocation } from 'react-router'
 
-const OuterContainer = styled.div`
-  box-sizing: border-box;
-  cursor: pointer;
-  background-color: ${(props) =>
-    props.active ? 'rgb(255, 250, 198)' : 'unset'};
-  outline: 1px solid rgba(46, 125, 50, 0.5);
-  height: ${(props) => props.height}px;
-  &:hover {
-    background-color: rgb(255, 250, 198);
-    /* outline: 2px solid rgba(46, 125, 50, 0.5); */
-  }
-  &:last-of-type {
-    border-bottom: 1px solid rgba(46, 125, 50, 0.5);
-  }
-`
-const InnerContainer = styled.div`
-  height: ${(props) => props.height}px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  padding: 15px;
-  box-sizing: border-box;
-  > div {
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-  }
-`
+import { outerContainer, innerContainer } from './Item.module.css'
 
 export const Item = ({ projektCount, row }) => {
   const { search } = useLocation()
@@ -40,17 +12,23 @@ export const Item = ({ projektCount, row }) => {
     navigate(`/Daten/Benutzer/${userId}/EKF/${ekfYear}/${row.id}${search}`)
 
   return (
-    <OuterContainer
+    <div
+      className={outerContainer}
       onClick={onClick}
-      active={ekfId === row.id}
-      height={innerContainerHeight}
+      style={{
+        backgroundColor: ekfId === row.id ? 'rgb(255, 250, 198)' : 'unset',
+        height: innerContainerHeight,
+      }}
     >
-      <InnerContainer height={innerContainerHeight}>
+      <div
+        className={innerContainer}
+        style={{ height: innerContainerHeight }}
+      >
         {projektCount > 1 && <div>{row.projekt}</div>}
         <div>{row.art}</div>
         <div>{row.pop}</div>
         <div>{row.tpop}</div>
-      </InnerContainer>
-    </OuterContainer>
+      </div>
+    </div>
   )
 }
