b034f005da22a46869b913536b01035c5030905e
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-04 22:08:48 +0100

    use css module

--- src/components/EkPlan/Table/index.jsx
+++ src/components/EkPlan/Table/index.jsx
@@ -61,15 +61,6 @@ export const StyledCellForSelect = styled(StyledTableCell)`
     border: solid orange 3px;
   }
 `
-export const InfoRow = styled.div`
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  overflow: hidden;
-  height: 19px;
-  min-height: 19px;
-  display: flex;
-  align-items: center;
-`
 
 const getTpopFilter = ({
   apValues,
