4488ab74adba53f1960c071faed246f84371b04e
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-05 22:21:07 +0100

    use css module

--- src/components/Print/ApberForAp/BMengen.jsx
+++ src/components/Print/ApberForAp/BMengen.jsx
@@ -1,52 +1,15 @@
-import styled from '@emotion/styled'
-
-const Container = styled.div`
-  padding: 0.2cm 0;
-  break-inside: avoid;
-`
-const Title = styled.h3`
-  font-size: 16px;
-  font-weight: 700;
-  margin-bottom: -18px;
-`
-const Row = styled.div`
-  display: flex;
-  padding: 0.05cm 0;
-  border-bottom: 1px solid rgba(0, 0, 0, 0.1) !important;
-  page-break-inside: avoid;
-`
-const YearRow = styled.div`
-  display: flex;
-  padding: 0.05cm 0;
-`
-const LabelRow = styled(Row)`
-  font-size: 12px;
-`
-const Year = styled.div`
-  position: relative;
-  left: 10.9cm;
-  width: 2cm;
-`
-const YearSince = styled.div`
-  position: relative;
-  left: 11.45cm;
-  width: 2cm;
-`
-const Label1 = styled.div`
-  min-width: 10cm;
-  max-width: 10cm;
-`
-const Number = styled.div`
-  min-width: 1cm;
-  max-width: 1cm;
-  text-align: right;
-`
-const PopBerJahr = styled(Number)``
-const TpopBerJahr = styled(Number)``
-const PopSeit = styled(Number)`
-  margin-left: 1cm;
-`
-const TpopSeit = styled(Number)``
+import {
+  container,
+  title,
+  row,
+  yearRow,
+  labelRow,
+  year,
+  yearSince,
+  label1,
+  number,
+  popSeit,
+} from './BMengen.module.css'
 
 export const BMengen = ({ jahr, loading, node }) => {
   const b1LPop = node?.b1LPop
@@ -56,26 +19,28 @@ export const BMengen = ({ jahr, loading, node }) => {
   const b1FirstYear = node?.b1FirstYear
 
   return (
-    <Container>
-      <Title>B. Bestandesentwicklung</Title>
-      <YearRow>
-        <Year>{jahr}</Year>
-        <YearSince>{`Seit ${loading ? '...' : b1FirstYear}`}</YearSince>
-      </YearRow>
-      <LabelRow>
-        <Label1 />
-        <PopBerJahr>Pop</PopBerJahr>
-        <TpopBerJahr>TPop</TpopBerJahr>
-        <PopSeit>Pop</PopSeit>
-        <TpopSeit>TPop</TpopSeit>
-      </LabelRow>
-      <Row>
-        <Label1>kontrolliert (inkl. Ansaatversuche)</Label1>
-        <PopBerJahr>{loading ? '...' : b1LPop}</PopBerJahr>
-        <TpopBerJahr>{loading ? '...' : b1LTpop}</TpopBerJahr>
-        <PopSeit>{loading ? '...' : b1RPop}</PopSeit>
-        <TpopSeit>{loading ? '...' : b1RTpop}</TpopSeit>
-      </Row>
-    </Container>
+    <div className={container}>
+      <h3 className={title}>B. Bestandesentwicklung</h3>
+      <div className={yearRow}>
+        <div className={year}>{jahr}</div>
+        <div
+          className={yearSince}
+        >{`Seit ${loading ? '...' : b1FirstYear}`}</div>
+      </div>
+      <div className={labelRow}>
+        <div className={label1} />
+        <div className={number}>Pop</div>
+        <div className={number}>TPop</div>
+        <div className={popSeit}>Pop</div>
+        <div className={number}>TPop</div>
+      </div>
+      <div className={row}>
+        <div className={label1}>kontrolliert (inkl. Ansaatversuche)</div>
+        <div className={number}>{loading ? '...' : b1LPop}</div>
+        <div className={number}>{loading ? '...' : b1LTpop}</div>
+        <div className={popSeit}>{loading ? '...' : b1RPop}</div>
+        <div className={number}>{loading ? '...' : b1RTpop}</div>
+      </div>
+    </div>
   )
 }
