f3e4d3a72adccc501643b467388f7d4dcd6a5f6a
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-05 14:16:42 +0100

    use css module

--- src/components/Messages/Messages/index.jsx
+++ src/components/Messages/Messages/index.jsx
@@ -1,18 +1,8 @@
-import styled from '@emotion/styled'
-
 import { Messages } from './Messages.jsx'
+import { scrollContainer } from './index.module.css'
 
-const ScrollContainer = styled.div`
-  min-width: 80% !important;
-  flex-grow: 1;
-  overflow: auto;
-  scrollbar-width: thin;
-`
-
-export const MessagesList = ({ unreadMessages }) => {
-  return (
-    <ScrollContainer>
-      <Messages unreadMessages={unreadMessages} />
-    </ScrollContainer>
-  )
-}
+export const MessagesList = ({ unreadMessages }) => (
+  <div className={scrollContainer}>
+    <Messages unreadMessages={unreadMessages} />
+  </div>
+)
