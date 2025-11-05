129a5af8b1a758a88221bd5a893edce7b3ba30e8
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-05 17:59:31 +0100

    use css module

--- src/components/NavElements/MovingIcon.jsx
+++ src/components/NavElements/MovingIcon.jsx
@@ -1,21 +1,13 @@
-import styled from '@emotion/styled'
 import { MdOutlineMoveDown } from 'react-icons/md'
 
-import { IconContainer } from './IconContainer.jsx'
-
-const StyledIcon = styled(MdOutlineMoveDown)`
-  padding-left: 0.2em;
-  height: 20px !important;
-  color: rgb(255, 90, 0) !important;
-  font-size: 1.5rem;
-  align-self: flex-start;
-`
+import { iconContainer } from './index.module.css'
+import { icon } from './MovingIcon.module.css'
 
 export const MovingIcon = () => (
-  <IconContainer
+  <div
     title="zum Verschieben gemerkt, bereit um in einer anderen Art einzufügen"
-    className="iconContainer"
+    className={iconContainer}
   >
-    <StyledIcon />
-  </IconContainer>
+    <MdOutlineMoveDown className={icon} />
+  </div>
 )
