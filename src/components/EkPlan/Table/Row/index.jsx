689fca880b4368a4e4660439ee8858ae1b90963b
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-05 13:58:47 +0100

    use css module

--- src/components/EkPlan/Table/Row/index.jsx
+++ src/components/EkPlan/Table/Row/index.jsx
@@ -1,23 +1,19 @@
 import { useRef } from 'react'
-import styled from '@emotion/styled'
 
 import { useOnScreen } from '../../../../modules/useOnScreen.js'
 import { Visible } from './Visible.jsx'
 
-const RowContainer = styled.div`
-  display: flex;
-  height: 60px;
-  min-height: 60px;
-  content-visibility: auto;
-  contain-intrinsic-size: auto 60px;
-`
+import { container } from './index.module.css'
 
 export const TpopRow = ({ tpopId, index, setProcessing, years }) => {
   const ref = useRef(null)
   const isVisible = useOnScreen(ref)
 
   return (
-    <RowContainer ref={ref}>
+    <div
+      className={container}
+      ref={ref}
+    >
       {isVisible && (
         <Visible
           tpopId={tpopId}
@@ -26,6 +22,6 @@ export const TpopRow = ({ tpopId, index, setProcessing, years }) => {
           years={years}
         />
       )}
-    </RowContainer>
+    </div>
   )
 }
