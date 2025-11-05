8ad67d5d6935faef0d3da03d49ac2413ec031978
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-05 18:44:04 +0100

    use css module

--- src/components/Projekte/Karte/layers/Tpop/tpopHighlighted.jsx
+++ src/components/Projekte/Karte/layers/Tpop/tpopHighlighted.jsx
@@ -1,5 +1,5 @@
 import Icon from './tpopHighlighted.svg'
-import { container } from '../../iconContainer.module.css'
+import { container } from '../iconContainer.module.css'
 
 export const TpopIconHighlighted = () => (
   <div className={container}>
