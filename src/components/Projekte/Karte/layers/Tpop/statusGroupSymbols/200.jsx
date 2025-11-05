930c2e7d3c9ab8e41ba45e8b930037816c17110c
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-05 18:33:13 +0100

    use css module

--- src/components/Projekte/Karte/layers/Tpop/statusGroupSymbols/200.jsx
+++ src/components/Projekte/Karte/layers/Tpop/statusGroupSymbols/200.jsx
@@ -1,8 +1,8 @@
 import Icon from './200.svg'
-import { IconContainer } from '../../../../../NavElements/IconContainer.jsx'
+import { iconContainer } from '../../iconContainer.module.css'
 
 export const TpopIcon200 = () => (
-  <IconContainer className="iconContainer">
+  <div className={iconContainer}>
     <Icon />
-  </IconContainer>
+  </div>
 )
