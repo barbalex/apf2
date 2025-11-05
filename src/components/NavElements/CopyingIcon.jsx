0f2839fecdd2c2854f3532d340fcf352b15bd071
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-05 17:57:32 +0100

    use css module

--- src/components/NavElements/CopyingIcon.jsx
+++ src/components/NavElements/CopyingIcon.jsx
@@ -1,21 +1,13 @@
-import styled from '@emotion/styled'
 import { MdContentCopy } from 'react-icons/md'
 
-import { IconContainer } from './IconContainer.jsx'
-
-const StyledIcon = styled(MdContentCopy)`
-  padding-left: 0.2em;
-  height: 20px !important;
-  color: rgb(255, 90, 0) !important;
-  font-size: 1.5rem;
-  align-self: flex-start;
-`
+import { iconContainer } from './index.module.css'
+import { icon } from './CopyingIcon.module.css'
 
 export const CopyingIcon = () => (
-  <IconContainer
+  <div
     title="kopiert, bereit zum Einfügen"
-    className="iconContainer"
+    className={iconContainer}
   >
-    <StyledIcon />
-  </IconContainer>
+    <MdContentCopy className={icon} />
+  </div>
 )
