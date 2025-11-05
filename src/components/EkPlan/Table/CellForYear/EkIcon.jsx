7af7a3ee20fc3551f77bf50dad981a9b8314b2c1
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-05 10:39:09 +0100

    use css module

--- src/components/EkPlan/Table/CellForYear/EkIcon.jsx
+++ src/components/EkPlan/Table/CellForYear/EkIcon.jsx
@@ -1,63 +1,24 @@
 import { useContext } from 'react'
-import styled from '@emotion/styled'
 import { sum } from 'es-toolkit'
 import { observer } from 'mobx-react-lite'
 
 import { MobxContext } from '../../../../mobxContext.js'
-
-const Container = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-wrap: nowrap;
-`
-
-const Icon = styled.svg`
-  fill: none;
-  stroke: #2e7d32;
-  stroke-width: 3px;
-`
-const Checkbox = styled.div`
-  position: relative;
-  width: 17px;
-  height: 17px;
-  border-radius: 3px;
-  transition: all 150ms;
-  flex-grow: 0;
-  flex-basis: 19px;
-  flex-shrink: 0;
-`
-const NrOfEk = styled.div`
-  font-weight: 700;
-  font-size: smaller;
-  position: absolute;
-  bottom: -2px;
-  right: 0;
-  color: red;
-`
-const SumCounted = styled.div`
-  margin-left: 3px;
-  align-self: center;
-  flex-grow: 0;
-  flex-basis: 32px;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  overflow: hidden;
-`
+import {
+  container,
+  icon,
+  checkbox,
+  nrOfEk,
+  sumCounted,
+} from './EkIcon.module.css'
 
 export const EkIcon = observer(({ planned, eks, einheits }) => {
   const store = useContext(MobxContext)
   const { showCount, showEkCount } = store.ekPlan
 
-  //console.log('EkIcon', { planned, eks, einheits })
-
-  const containerStyle = {
-    justifyContent: showCount ? 'space-between' : 'center',
-  }
-
   if (!planned && !eks.length) {
-    return <Container>&nbsp;</Container>
+    return <div className={container}>&nbsp;</div>
   }
+
   let sumCounted = null
   let eksHaveCountedZielrelevanteEinheits = false
   if (einheits && einheits.length) {
@@ -91,23 +52,36 @@ export const EkIcon = observer(({ planned, eks, einheits }) => {
   }
 
   return (
-    <Container style={containerStyle}>
-      <Checkbox
+    <div
+      className={container}
+      style={{
+        justifyContent: showCount ? 'space-between' : 'center',
+      }}
+    >
+      <div
+        className={checkbox}
         style={{
           background: planned ? 'rgba(46, 125, 50, 0.05)' : 'none',
           border: planned ? '1px solid #2e7d32' : 'none',
         }}
       >
         {!!eks.length && (
-          <Icon viewBox="0 0 24 24">
+          <svg
+            viewBox="0 0 24 24"
+            className={icon}
+          >
             <polyline points="20 6 9 17 4 12" />
-          </Icon>
+          </svg>
         )}
-        {showEkCount && eks.length > 1 && <NrOfEk>{eks.length}</NrOfEk>}
-      </Checkbox>
+        {showEkCount && eks.length > 1 && (
+          <div className={nrOfEk}>{eks.length}</div>
+        )}
+      </div>
       {showCount && (
-        <SumCounted>{sumCounted !== null ? sumCounted : ' '}</SumCounted>
+        <div className={sumCounted}>
+          {sumCounted !== null ? sumCounted : ' '}
+        </div>
       )}
-    </Container>
+    </div>
   )
 })
