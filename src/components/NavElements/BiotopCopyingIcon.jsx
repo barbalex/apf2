0c595e9fbfbefc0022302c53045224939bbad700
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-05 17:55:05 +0100

    use css module

--- src/components/NavElements/BiotopCopyingIcon.jsx
+++ src/components/NavElements/BiotopCopyingIcon.jsx
@@ -1,21 +1,13 @@
-import styled from '@emotion/styled'
 import { MdPhotoLibrary } from 'react-icons/md'
 
-import { IconContainer } from './IconContainer.jsx'
-
-const StyledIcon = styled(MdPhotoLibrary)`
-  padding-left: 0.2em;
-  height: 20px !important;
-  color: rgb(255, 90, 0) !important;
-  font-size: 1.5rem;
-  align-self: flex-start;
-`
+import { iconContainer } from './index.module.css'
+import { copyingIcon } from './BiotopCopyingIcon.module.css'
 
 export const BiotopCopyingIcon = () => (
-  <IconContainer
+  <div
     title="Biotop kopiert, bereit zum Einfügen"
-    className="iconContainer"
+    className={iconContainer}
   >
-    <StyledIcon />
-  </IconContainer>
+    <MdPhotoLibrary className={copyingIcon} />
+  </div>
 )
