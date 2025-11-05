41ca083e7fa28935b09dda998a33d7aa66f90f26
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-05 18:34:04 +0100

    use css module

--- src/components/Projekte/Karte/layers/Pop/statusGroupSymbols/300Highlighted.jsx
+++ src/components/Projekte/Karte/layers/Pop/statusGroupSymbols/300Highlighted.jsx
@@ -1,8 +1,8 @@
 import Icon from './300Highlighted.svg'
-import { IconContainer } from '../../../../../NavElements/IconContainer.jsx'
+import { container } from '../../iconContainer.module.css'
 
 export const PopIcon300Highlighted = () => (
-  <IconContainer className="iconContainer">
+  <div className={container}>
     <Icon />
-  </IconContainer>
+  </div>
 )
