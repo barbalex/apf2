6c3d5e22be06410d8c9a0e646cf66ef4f0b1a557
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-05 18:48:23 +0100

    use css module

--- src/components/NavElements/MapIcon.jsx
+++
@@ -1,9 +0,0 @@
-import { MdLocalFlorist } from 'react-icons/md'
-import styled from '@emotion/styled'
-
-export const MapIcon = styled(MdLocalFlorist)`
-  margin-left: -2px;
-  height: 1.2rem !important;
-  font-size: 1.4rem;
-  align-self: flex-start;
-`
