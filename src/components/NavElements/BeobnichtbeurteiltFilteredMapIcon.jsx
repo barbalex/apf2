30fd48d67bd92d364fe8ce7f0e22948af2bef8d7
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-05 14:27:45 +0100

    use css module

--- src/components/NavElements/BeobnichtbeurteiltFilteredMapIcon.jsx
+++ src/components/NavElements/BeobnichtbeurteiltFilteredMapIcon.jsx
@@ -1,20 +1,14 @@
-import styled from '@emotion/styled'
-
 import { MapIcon } from './MapIcon.jsx'
 import { IconContainer } from './IconContainer.jsx'
 
-const StyledMapIcon = styled(MapIcon)`
-  color: #9a009a !important;
-  paint-order: stroke;
-  stroke-width: 8px;
-  stroke: #fff900;
-`
+import { mapIcon } from './BeobnichtbeurteiltFilteredMapIcon.module.css'
+import { iconContainer } from './index.module.css'
 
 export const BeobnichtbeurteiltFilteredMapIcon = () => (
-  <IconContainer
+  <div
     title="Beobachtung in Karte hervorgehoben"
-    className="iconContainer"
+    className={iconContainer}
   >
-    <StyledMapIcon />
-  </IconContainer>
+    <MapIcon className={mapIcon} />
+  </div>
 )
