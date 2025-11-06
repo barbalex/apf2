9ec093d84540ef30fe20e2ab59d3ec75863797dc
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-06 11:08:41 +0100

    use css module

--- src/components/Print/ApberForAp/Ziele.jsx
+++ src/components/Print/ApberForAp/Ziele.jsx
@@ -1,59 +1,42 @@
-import styled from '@emotion/styled'
-
-const Container = styled.div`
-  padding: 0.2cm 0;
-  break-inside: avoid;
-`
-const Title = styled.div`
-  border-bottom: 1px solid rgba(0, 0, 0, 0.1) !important;
-  font-weight: 700;
-`
-const Row = styled.div`
-  display: flex;
-  padding: 0.15cm 0;
-  border-bottom: 1px solid rgba(0, 0, 0, 0.1) !important;
-`
-const ZielColumn = styled.div`
-  width: 100%;
-`
-const TitleRow = styled(Row)`
-  color: grey;
-`
-const Typ = styled.div`
-  width: 5.5cm;
-  padding-right: 0.5cm;
-`
-const Goal = styled.div`
-  width: 100%;
-`
-const Opinion = styled.div`
-  width: 100%;
-  padding-top: 0.1cm;
-`
+import {
+  container,
+  title,
+  row,
+  zielColumn,
+  titleRow,
+  typ,
+  goal,
+  opinion,
+} from './Ziele.module.css'
 
 export const Ziele = ({ ziele }) => (
-  <Container>
-    <Title>Ziele im Berichtsjahr:</Title>
-    <TitleRow>
-      <Typ>Typ</Typ>
-      <Goal>Ziel</Goal>
-    </TitleRow>
+  <div className={container}>
+    <div className={title}>Ziele im Berichtsjahr:</div>
+    <div className={titleRow}>
+      <div className={typ}>Typ</div>
+      <div className={goal}>Ziel</div>
+    </div>
     {ziele.map((z) => {
       const hasZielBer = Boolean(z.erreichung || z.bemerkungen)
 
       return (
-        <Row key={z.id}>
-          <Typ>{z?.zielTypWerteByTyp?.text ?? ''}</Typ>
-          <ZielColumn>
-            <Goal>{z.bezeichnung || ''}</Goal>
+        <div
+          className={row}
+          key={z.id}
+        >
+          <div className={typ}>{z?.zielTypWerteByTyp?.text ?? ''}</div>
+          <div className={zielColumn}>
+            <div className={goal}>{z.bezeichnung || ''}</div>
             {hasZielBer && (
-              <Opinion>{`Beurteilung: ${z.erreichung || '(keine)'}${
+              <div
+                className={opinion}
+              >{`Beurteilung: ${z.erreichung || '(keine)'}${
                 z.bemerkungen ? '; ' : ''
-              }${z.bemerkungen || ''}`}</Opinion>
+              }${z.bemerkungen || ''}`}</div>
             )}
-          </ZielColumn>
-        </Row>
+          </div>
+        </div>
       )
     })}
-  </Container>
+  </div>
 )
