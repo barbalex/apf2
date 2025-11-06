7b89d2cdb3f5ba39121837ee0b9b714310b5eb9a
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-06 22:15:24 +0100

    use css module

--- src/components/Projekte/Daten/Beob/index.jsx
+++ src/components/Projekte/Daten/Beob/index.jsx
@@ -1,5 +1,4 @@
 import { useContext, useEffect, Suspense } from 'react'
-import styled from '@emotion/styled'
 import { observer } from 'mobx-react-lite'
 import { useApolloClient } from '@apollo/client/react'
 import { useParams } from 'react-router'
