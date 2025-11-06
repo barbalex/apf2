e849e42917964048a60e6f2b29e045042b961edf
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-06 11:02:45 +0100

    use css module

--- src/components/Print/ApberForAp/Massnahmen.jsx
+++ src/components/Print/ApberForAp/Massnahmen.jsx
@@ -1,4 +1,3 @@
-import styled from '@emotion/styled'
 import { DateTime } from 'luxon'
 
 import {
@@ -20,129 +19,47 @@ import {
   massnBeschreibung,
 } from './Massnahmen.module.css'
 
-const Container = styled.div`
-  padding: 0.2cm 0;
-  margin-bottom: 0.4cm;
-  max-width: 18cm;
-  font-size: 12px;
-`
-const Title = styled.div`
-  border-bottom: 1px solid rgba(0, 0, 0, 0.1) !important;
-  font-weight: 700;
-  break-after: avoid;
-  page-break-after: avoid;
-  page-break-inside: avoid;
-`
-const Row = styled.div`
-  display: flex;
-  padding: 0.05cm 0;
-  border-bottom: 1px solid rgba(0, 0, 0, 0.1) !important;
-`
-const TitleRow = styled.div`
-  padding: 0.05cm 0;
-  border-bottom: 1px solid rgba(0, 0, 0, 0.1) !important;
-  display: flex;
-  flex-direction: column;
-  color: grey;
-  max-width: 18cm;
-  break-after: avoid;
-  page-break-after: avoid;
-  page-break-inside: avoid;
-`
-const TitleSubRow = styled.div`
-  display: flex;
-  max-width: 18cm;
-`
-const PopNr = styled.div`
-  min-width: 0.6cm;
-  max-width: 0.6cm;
-  padding-right: 0.2cm;
-`
-const PopName = styled.div`
-  min-width: 3.5cm;
-  max-width: 3.5cm;
-  padding-right: 0.2cm;
-`
-const PopNrName = styled.div`
-  min-width: 4.1cm;
-  max-width: 4.1cm;
-  padding-right: 0.2cm;
-`
-const TpopNr = styled.div`
-  min-width: 0.8cm;
-  max-width: 0.8cm;
-  padding-right: 0.2cm;
-`
-const TpopFlurname = styled.div`
-  min-width: 3.5cm;
-  max-width: 3.5cm;
-  padding-right: 0.2cm;
-`
-const TpopNrFlurname = styled.div`
-  min-width: 4.3cm;
-  max-width: 4.3cm;
-  padding-right: 0.2cm;
-`
-const MassnDatum = styled.div`
-  min-width: 1cm;
-  max-width: 1cm;
-  padding-right: 0.2cm;
-`
-const MassnDatumTitle = styled.div`
-  min-width: 1cm;
-  max-width: 1cm;
-  padding-right: 0.2cm;
-  padding-top: 3px;
-  font-size: 10px;
-`
-const MassnTyp = styled.div`
-  min-width: 3.3cm;
-  max-width: 3.3cm;
-  padding-right: 0.2cm;
-`
-const MassnDatumTyp = styled.div`
-  min-width: 4.3cm;
-  max-width: 4.3cm;
-  padding-right: 0.2cm;
-`
-const MassnBeschreibung = styled.div`
-  width: 100%;
-`
-
 export const Massnahmen = ({ massns }) => (
-  <Container>
-    <Title>Massnahmen im Berichtsjahr:</Title>
-    <TitleRow>
-      <TitleSubRow>
-        <PopNrName>Population</PopNrName>
-        <TpopNrFlurname>Teil-Population</TpopNrFlurname>
-        <MassnDatumTyp>Massnahme</MassnDatumTyp>
-        <MassnBeschreibung />
-      </TitleSubRow>
-      <TitleSubRow>
-        <PopNr>Nr.</PopNr>
-        <PopName>Name</PopName>
-        <TpopNr>Nr.</TpopNr>
-        <TpopFlurname>Flurname</TpopFlurname>
-        <MassnDatumTitle>Datum</MassnDatumTitle>
-        <MassnTyp>Typ</MassnTyp>
-        <MassnBeschreibung>Massnahme</MassnBeschreibung>
-      </TitleSubRow>
-    </TitleRow>
+  <div className={container}>
+    <div className={title}>Massnahmen im Berichtsjahr:</div>
+    <div className={titleRow}>
+      <div className={titleSubRow}>
+        <div className={popNrName}>Population</div>
+        <div className={tpopNrFlurname}>Teil-Population</div>
+        <div className={massnDatumTyp}>Massnahme</div>
+        <div className={massnBeschreibung} />
+      </div>
+      <div className={titleSubRow}>
+        <div className={popNr}>Nr.</div>
+        <div className={popName}>Name</div>
+        <div className={tpopNr}>Nr.</div>
+        <div className={tpopFlurname}>Flurname</div>
+        <div className={massnDatumTitle}>Datum</div>
+        <div className={massnTyp}>Typ</div>
+        <div className={massnBeschreibung}>Massnahme</div>
+      </div>
+    </div>
     {massns.map((m) => {
       const mDatum = m.datum ? DateTime.fromSQL(m.datum).toFormat('dd.LL') : ''
 
       return (
-        <Row key={m.id}>
-          <PopNr>{m?.tpopByTpopId?.popByPopId?.nr ?? ''}</PopNr>
-          <PopName>{m?.tpopByTpopId?.popByPopId?.name ?? ''}</PopName>
-          <TpopNr>{m?.tpopByTpopId?.nr ?? ''}</TpopNr>
-          <TpopFlurname>{m?.tpopByTpopId?.flurname ?? ''}</TpopFlurname>
-          <MassnDatum>{mDatum}</MassnDatum>
-          <MassnTyp>{m?.tpopmassnTypWerteByTyp?.text ?? ''}</MassnTyp>
-          <MassnBeschreibung>{m?.beschreibung ?? ''}</MassnBeschreibung>
-        </Row>
+        <div
+          className={row}
+          key={m.id}
+        >
+          <div className={popNr}>{m?.tpopByTpopId?.popByPopId?.nr ?? ''}</div>
+          <div className={popName}>
+            {m?.tpopByTpopId?.popByPopId?.name ?? ''}
+          </div>
+          <div className={tpopNr}>{m?.tpopByTpopId?.nr ?? ''}</div>
+          <div className={tpopFlurname}>{m?.tpopByTpopId?.flurname ?? ''}</div>
+          <div className={massnDatum}>{mDatum}</div>
+          <div className={massnTyp}>
+            {m?.tpopmassnTypWerteByTyp?.text ?? ''}
+          </div>
+          <div className={massnBeschreibung}>{m?.beschreibung ?? ''}</div>
+        </div>
       )
     })}
-  </Container>
+  </div>
 )
