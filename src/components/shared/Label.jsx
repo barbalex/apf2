b215e970750e15270fc6c15d2b8144a5aa7a9515
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-07 20:16:34 +0100

    use modules for css

--- src/components/shared/Label.jsx
+++ src/components/shared/Label.jsx
@@ -1,15 +1,10 @@
-import styled from '@emotion/styled'
-
-const StyledLabel = styled.div`
-  margin-top: 10px;
-  cursor: text;
-  font-size: 12px;
-  color: ${(props) => props.color};
-  pointer-events: none;
-  user-select: none;
-  padding-bottom: 4px;
-`
+import { labelClass } from './Label.module.css'
 
 export const Label = ({ label, color = 'rgba(0, 0, 0, 0.5)' }) => (
-  <StyledLabel color={color}>{label}</StyledLabel>
+  <div
+    style={{ color }}
+    className={labelClass}
+  >
+    {label}
+  </div>
 )
