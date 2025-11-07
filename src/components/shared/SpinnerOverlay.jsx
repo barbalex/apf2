3105481ce61b325fe45d27f6e360cb53a22fed06
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-07 22:50:24 +0100

    use modules for css

--- src/components/shared/SpinnerOverlay.jsx
+++ src/components/shared/SpinnerOverlay.jsx
@@ -1,19 +1,7 @@
-import styled from '@emotion/styled'
 import CircularProgress from '@mui/material/CircularProgress'
 import Dialog from '@mui/material/Dialog'
 
-const SpinnerContainer = styled.div`
-  min-height: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding-top: 10px;
-`
-const SpinnerText = styled.div`
-  padding: 10px;
-`
+import { container, text } from './SpinnerOverlay.module.css'
 
 export const SpinnerOverlay = ({ message, onClose }) => (
   <Dialog
@@ -21,9 +9,9 @@ export const SpinnerOverlay = ({ message, onClose }) => (
     onClose={onClose}
     disableEscapeKeyDown={true}
   >
-    <SpinnerContainer>
+    <div className={container}>
       <CircularProgress />
-      {!!message && <SpinnerText>{message}</SpinnerText>}
-    </SpinnerContainer>
+      {!!message && <div className={text}>{message}</div>}
+    </div>
   </Dialog>
 )
