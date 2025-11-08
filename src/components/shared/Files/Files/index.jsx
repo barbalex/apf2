df14c63cbb2c3952a8499593300c4ca46b895759
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-08 10:51:39 +0100

    use modules for css

--- src/components/shared/Files/Files/index.jsx
+++ src/components/shared/Files/Files/index.jsx
@@ -1,25 +1,16 @@
-import styled from '@emotion/styled'
 import { useOutletContext } from 'react-router'
 
 import { ErrorBoundary } from '../../ErrorBoundary.jsx'
-
 import { File } from './File.jsx'
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  background-color: ${(props) => (props.showfilter ? '#ffd3a7' : 'unset')};
-  padding: 10px;
-  overflow-y: auto;
-  scrollbar-width: thin;
-`
+import { container } from './index.module.css'
 
 export const Component = () => {
   const { parent, files, refetch } = useOutletContext()
 
   return (
     <ErrorBoundary>
-      <Container>
+      <div className={container}>
         {files.map((file) => (
           <File
             key={file.fileId}
@@ -28,7 +19,7 @@ export const Component = () => {
             refetch={refetch}
           />
         ))}
-      </Container>
+      </div>
     </ErrorBoundary>
   )
 }
