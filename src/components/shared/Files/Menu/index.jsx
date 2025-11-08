14b072b44f51195befdd9de24bd56fc0a2453eb5
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-08 11:13:33 +0100

    use modules for css

--- src/components/shared/Files/Menu/index.jsx
+++ src/components/shared/Files/Menu/index.jsx
@@ -1,22 +1,10 @@
 import { useLocation, useParams } from 'react-router'
-import styled from '@emotion/styled'
 
 import { ErrorBoundary } from '../../ErrorBoundary.jsx'
 import { MenuBar } from '../../MenuBar/index.jsx'
 import { Title } from './Title.jsx'
 import { ListMenus } from './ListMenus.jsx'
 import { PreviewMenus } from './PreviewMenus.jsx'
-
-export const MenuTitle = styled.h3`
-  padding-top: 2px;
-  padding-left: 15px;
-  padding-right: 16px;
-  padding-bottom: 0;
-  margin-bottom: 3px;
-  &:focus {
-    outline: none;
-  }
-`
 
 export const Menu = ({ parent, files, refetch, containerRef }) => {
   const { fileId } = useParams()
