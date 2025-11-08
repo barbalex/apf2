b1d8270e5dcec2978c99bfa550ad8fd1bed8e58a
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-08 12:09:28 +0100

    use modules for css

--- src/components/shared/List/index.jsx
+++ src/components/shared/List/index.jsx
@@ -1,43 +1,11 @@
 import { useNavigate, useLocation } from 'react-router'
-import styled from '@emotion/styled'
 import Highlighter from 'react-highlight-words'
 
 import { FormTitle } from '../FormTitle/index.jsx'
 import { ErrorBoundary } from '../ErrorBoundary.jsx'
 import { navData } from '../../Bookmarks/NavTo/Navs/Projects.jsx'
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  flex-grow: 1;
-  overflow: hidden;
-  content-visibility: auto;
-  contain-intrinsic-size: auto 45px;
-`
-const ListContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  flex-grow: 1;
-  overflow: auto;
-  scrollbar-width: thin;
-`
-const Row = styled.div`
-  padding: 5px 10px;
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  flex-grow: 0;
-  min-height: 35px;
-  cursor: pointer;
-  user-select: none;
-  &:hover {
-    background-color: rgba(103, 216, 101, 0.07);
-  }
-  border-top: rgba(46, 125, 50, 0.5) solid 1px;
-  &:last-child {
-    border-bottom: rgba(46, 125, 50, 0.5) solid 1px;
-  }
-`
+import { container, list, row } from './index.module.css'
 
 export const List = ({
   navData,
@@ -52,19 +20,20 @@ export const List = ({
 
   return (
     <ErrorBoundary>
-      <Container>
+      <div className={container}>
         <FormTitle
           title={navData.label}
           listFilter={navData.listFilter}
           MenuBarComponent={MenuBarComponent}
           menuBarProps={menuBarProps}
         />
-        <ListContainer>
+        <div className={list}>
           {navData.menus.map((item) => {
             const label = item.label ?? item.labelEkf ?? item.labelEk
 
             return (
-              <Row
+              <div
+                className={row}
                 key={item.id}
                 onClick={onClickRow.bind(this, item)}
               >
@@ -80,11 +49,11 @@ export const List = ({
                   item.labelRightElements.map((El, index) => (
                     <El key={index} />
                   ))}
-              </Row>
+              </div>
             )
           })}
-        </ListContainer>
-      </Container>
+        </div>
+      </div>
     </ErrorBoundary>
   )
 }
