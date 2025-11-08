d02f7a0d1338c1839281a55c376d6b1ff1a472be
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-08 12:06:22 +0100

    use modules for css

--- src/components/shared/History/index.jsx
+++ src/components/shared/History/index.jsx
@@ -1,27 +1,15 @@
-import styled from '@emotion/styled'
-
 import { Data } from './Data.jsx'
 import { ErrorBoundary } from '../ErrorBoundary.jsx'
 
-const Container = styled.div`
-  padding: 10px 0;
-`
-const Title = styled.h4`
-  margin-bottom: 0;
-`
-const TitleRow = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin-bottom: 10px;
-`
+import { container, title, titleRow } from './index.module.css'
 
 export const History = ({ year, dataArray }) => (
   <ErrorBoundary>
-    <Container>
-      <TitleRow>
-        <Title>{year}</Title>
-      </TitleRow>
+    <div className={container}>
+      <div className={titleRow}>
+        <h4 className={title}>{year}</h4>
+      </div>
       <Data dataArray={dataArray} />
-    </Container>
+    </div>
   </ErrorBoundary>
 )
