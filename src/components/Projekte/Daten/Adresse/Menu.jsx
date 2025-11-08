0677222d9028e0ceec25a818b5b712bf002784de
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-08 10:57:21 +0100

    use modules for css

--- src/components/Projekte/Daten/Adresse/Menu.jsx
+++ src/components/Projekte/Daten/Adresse/Menu.jsx
@@ -15,7 +15,9 @@ import { isEqual } from 'es-toolkit'
 import { MenuBar } from '../../../shared/MenuBar/index.jsx'
 import { ErrorBoundary } from '../../../shared/ErrorBoundary.jsx'
 import { MobxContext } from '../../../../mobxContext.js'
-import { MenuTitle } from '../../../shared/Files/Menu/index.jsx'
+
+import { menuTitle } from '../../../shared/Files/Menu/index.module.css'
+
 const iconStyle = { color: 'white' }
 
 export const Menu = observer(() => {
@@ -129,7 +131,7 @@ export const Menu = observer(() => {
         open={delMenuOpen}
         onClose={() => setDelMenuAnchorEl(null)}
       >
-        <MenuTitle>löschen?</MenuTitle>
+        <h3 className={menuTitle}>löschen?</h3>
         <MenuItem onClick={onClickDelete}>ja</MenuItem>
         <MenuItem onClick={() => setDelMenuAnchorEl(null)}>nein</MenuItem>
       </MuiMenu>
