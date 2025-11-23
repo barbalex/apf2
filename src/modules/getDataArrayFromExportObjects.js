e61ee22c3b456b5c053572767875139b171071d2
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-23 21:06:01 +0100

    remove no more used

--- src/modules/getDataArrayFromExportObjects.js
+++ src/modules/getDataArrayFromExportObjects.js
@@ -3,19 +3,6 @@ export const getDataArrayFromExportObjects = (exportObjects) => {
   // first the field names:
   dataArray.push(Object.keys(exportObjects[0]))
   // then the field values
-  exportObjects.forEach((object) =>
-    dataArray.push(
-      Object.keys(object).map((key, index) => {
-        /**
-         * exceljs errors out if first member of array is null
-         * see: https://github.com/guyonroche/exceljs/issues/111
-         */
-        if (object[key] === null && index === 0) {
-          return ''
-        }
-        return object[key]
-      }),
-    ),
-  )
+  exportObjects.forEach((object) => dataArray.push(Object.values(object)))
   return dataArray
 }
