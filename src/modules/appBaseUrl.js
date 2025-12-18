f5e58556fd340a9d0cbfbf36e3e3923a15bccb07
Alexander Gabriel <alex.barbalex@gmail.com>
2025-12-18 14:56:59 +0100

    detect electron app

--- src/modules/appBaseUrl.js
+++ src/modules/appBaseUrl.js
@@ -1,9 +1,15 @@
 // in development should return local path
+import { isElectron } from './isElectron.js'
 
 export const appBaseUrl = () => {
   const hostnameWithoutWww = window.location.hostname.replace('www.', '')
   const isLocalhost = hostnameWithoutWww === 'localhost'
   const hostname = isLocalhost ? 'localhost' : window.location.hostname
+
+  const isElectronApp = isElectron()
+  if (isElectronApp) {
+    return `https://apflora.ch/`
+  }
 
   const appHost =
     isLocalhost ?
