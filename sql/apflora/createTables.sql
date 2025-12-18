39cf42ae7a98c0a388a1365b734426e8264d1ef1
Alexander Gabriel <alex.barbalex@gmail.com>
2025-12-18 20:39:05 +0100

    optimize

--- sql/apflora/createTables.sql
+++ sql/apflora/createTables.sql
@@ -1703,34 +1703,36 @@ DROP POLICY IF EXISTS reader ON apflora.idealbiotop_file;
 --                   user_name = current_user_name()))));
 
 CREATE POLICY reader ON apflora.idealbiotop_file
-  USING (CURRENT_USER IN ('apflora_manager', 'apflora_ap_writer', 'apflora_reader', 'apflora_freiwillig')
-    OR (CURRENT_USER IN ('apflora_ap_reader') AND idealbiotop_id IN (
-      SELECT
-        id
-      FROM
-        apflora.idealbiotop
-      WHERE
-        ap_id IN (
-          SELECT
-            ap_id
-          FROM
-            apflora.ap_user
-          WHERE
-            user_name = current_user_name()))))
-          WITH CHECK (CURRENT_USER IN ('apflora_manager', 'apflora_freiwillig')
-          OR (CURRENT_USER IN ('apflora_ap_writer') AND idealbiotop_id IN (
-            SELECT
-              id
-            FROM
-              apflora.idealbiotop
-            WHERE
-              ap_id IN (
-                SELECT
-                  ap_id
-                FROM
-                  apflora.ap_user
-                WHERE
-                  user_name = current_user_name()))));
+USING (
+  CURRENT_USER IN ('apflora_manager', 'apflora_ap_writer', 'apflora_reader', 'apflora_freiwillig')
+  OR (
+    CURRENT_USER IN ('apflora_ap_reader') 
+    AND idealbiotop_id IN (
+      SELECT id
+      FROM apflora.idealbiotop
+      WHERE ap_id IN (
+        SELECT ap_id
+        FROM apflora.ap_user
+        WHERE user_name = current_user_name()
+      )
+    )
+  )
+)
+WITH CHECK (
+  CURRENT_USER IN ('apflora_manager', 'apflora_freiwillig')
+  OR (
+    CURRENT_USER IN ('apflora_ap_writer') 
+    AND idealbiotop_id IN (
+      SELECT id
+      FROM apflora.idealbiotop
+      WHERE ap_id IN (
+        SELECT ap_id
+        FROM apflora.ap_user
+        WHERE user_name = current_user_name()
+      )
+    )
+  )
+);
 
 -- pop
 DROP TABLE IF EXISTS apflora.pop;
