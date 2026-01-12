bb318ecaf8d7a61137de46a43b88196be4adf1b0
Alexander Gabriel <alex.barbalex@gmail.com>
2026-01-12 14:54:27 +0100

    up version

--- src/components/AppBar/Bar/Projekte/More/index.jsx
+++ src/components/AppBar/Bar/Projekte/More/index.jsx
@@ -140,7 +140,7 @@ export const More = observer(
           <MenuItem onClick={onClickUptime}>
             Verfügbarkeit der Server von apflora.ch
           </MenuItem>
-          <div className={version}>Version: 1.124.51 vom 7.1.2026</div>
+          <div className={version}>Version: 1.124.52 vom 12.1.2026</div>
         </Menu>
       </Tooltip>
     )
