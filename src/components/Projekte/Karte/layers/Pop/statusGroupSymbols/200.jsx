7f1b3e7d5ba2c986161b7405fd481fbb7af30c5f
Alexander Gabriel <alex.barbalex@gmail.com>
2026-01-13 14:11:36 +0100

    style without importing single classes

--- src/components/Projekte/Karte/layers/Pop/statusGroupSymbols/200.jsx
+++ src/components/Projekte/Karte/layers/Pop/statusGroupSymbols/200.jsx
@@ -1,8 +1,8 @@
 import Icon from './200.svg'
-import { container } from '../../iconContainer.module.css'
+import styles from '../../iconContainer.module.css'
 
 export const PopIcon200 = () => (
-  <div className={container}>
+  <div className={styles.container}>
     <Icon />
   </div>
 )
