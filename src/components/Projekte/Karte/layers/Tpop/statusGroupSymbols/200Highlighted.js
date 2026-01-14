b2eff5788d64564c1f524bfff6129e6de38302bc
Alexander Gabriel <alex.barbalex@gmail.com>
2026-01-14 02:13:10 +0100

    type

--- src/components/Projekte/Karte/layers/Tpop/statusGroupSymbols/200Highlighted.js
+++
@@ -1,1 +0,0 @@
-export const svg200Highlighted = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#ff0" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.249" d="M.654.654h22.694v22.784H.654z"/></svg>`
