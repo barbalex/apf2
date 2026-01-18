c758d6c6c5324fe7dec28bc5c4adac0c5bbd1be1
Alexander Gabriel <alex.barbalex@gmail.com>
2026-01-18 22:35:14 +0100

    use react query and suspense

--- src/components/EkPlan/Table/CellForYear/index.tsx
+++ src/components/EkPlan/Table/CellForYear/index.tsx
@@ -1,6 +1,5 @@
 import { useContext } from 'react'
 import { observer } from 'mobx-react-lite'
-import { useQuery } from '@apollo/client/react'
 
 import { EkIcon } from './EkIcon.tsx'
 import { MassnIcon } from './MassnIcon.tsx'
