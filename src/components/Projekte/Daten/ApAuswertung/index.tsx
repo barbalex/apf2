60a77d4d78f0016850935516b5c1ccfabc47eb3b
Alexander Gabriel <alex.barbalex@gmail.com>
2026-01-18 22:50:49 +0100

    use react query and suspense

--- src/components/Projekte/Daten/ApAuswertung/index.tsx
+++ src/components/Projekte/Daten/ApAuswertung/index.tsx
@@ -1,7 +1,7 @@
 import { useParams } from 'react-router'
 import { gql } from '@apollo/client'
-
-import { useQuery } from '@apollo/client/react'
+import { useApolloClient } from '@apollo/client/react'
+import { useQuery } from '@tanstack/react-query'
 
 import { ApErfolg } from './ApErfolg/index.tsx'
 import { PopStatus } from './PopStatus/index.tsx'
@@ -37,12 +37,24 @@ interface ApAuswertungQueryResult {
 }
 
 export const Component = () => {
+  const apolloClient = useApolloClient()
+
   const { apId } = useParams<{ apId: string }>()
-  const { data } = useQuery<ApAuswertungQueryResult>(apAuswertungQuery, {
-    variables: { apId },
+  const { data } = useQuery({
+    queryKey: ['apAuswertung', apId],
+    queryFn: async () => {
+      const result = await apolloClient.query<ApAuswertungQueryResult>({
+        query: apAuswertungQuery,
+        variables: { apId },
+        fetchPolicy: 'no-cache',
+      })
+      if (result.error) throw result.error
+      return result
+    },
+    suspense: true,
   })
 
-  const artname = data?.apById?.aeTaxonomyByArtId?.artname ?? 'Art'
+  const artname = data?.data?.apById?.aeTaxonomyByArtId?.artname ?? 'Art'
 
   return (
     <>
