7e3728e250d08e29e67dd9688cf2a6918e82a061
Alexander Gabriel <alex.barbalex@gmail.com>
2026-01-19 10:58:31 +0100

    use react query and suspense

--- src/components/Projekte/Daten/Pop/Dateien.tsx
+++ src/components/Projekte/Daten/Pop/Dateien.tsx
@@ -1,7 +1,7 @@
 import { useParams } from 'react-router'
 import { gql } from '@apollo/client'
-
-import { useQuery } from '@apollo/client/react'
+import { useApolloClient } from '@apollo/client/react'
+import { useQuery } from '@tanstack/react-query'
 
 import { FilesRouter } from '../../../shared/Files/index.tsx'
 import { FormTitle } from '../../../shared/FormTitle/index.tsx'
@@ -9,30 +9,39 @@ import { FormTitle } from '../../../shared/FormTitle/index.tsx'
 import type { PopId } from '../../../../models/apflora/index.tsx'
 
 interface PopQueryResult {
-  data?: {
-    popById?: {
-      id: PopId
-      label: string | null
-    }
+  popById?: {
+    id: PopId
+    label: string | null
   }
 }
 
+const query = gql`
+  query popByIdForPopDateienQuery($id: UUID!) {
+    popById(id: $id) {
+      id
+      label
+    }
+  }
+`
+
 export const Component = () => {
   const { popId } = useParams()
+  const apolloClient = useApolloClient()
 
-  const { data } = useQuery<PopQueryResult>(
-    gql`
-      query popByIdForPopDateienQuery($id: UUID!) {
-        popById(id: $id) {
-          id
-          label
-        }
-      }
-    `,
-    {
-      variables: { id: popId },
+  const { data } = useQuery({
+    queryKey: ['pop', popId, 'label'],
+    queryFn: async () => {
+      const result = await apolloClient.query<PopQueryResult>({
+        query,
+        variables: { id: popId },
+        fetchPolicy: 'no-cache',
+      })
+      if (result.error) throw result.error
+      return result.data
     },
-  )
+    suspense: true,
+  })
+
   const label = data?.popById?.label ?? 'Population'
 
   return (
