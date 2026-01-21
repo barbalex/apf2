1cc1982700afb6f3566a48782eeacd98b2a46268
Alexander Gabriel <alex.barbalex@gmail.com>
2026-01-21 18:34:13 +0100

    migrate ekplan

--- src/components/EkPlan/Table/CellForValue.tsx
+++ src/components/EkPlan/Table/CellForValue.tsx
@@ -9,38 +9,36 @@ import {
 import styles from './CellForValue.module.css'
 import indexStyles from './index.module.css'
 
-export const CellForValue = (
-  ({ field, width, row, isOdd, firstChild }) => {
-    const hovered = useAtomValue(ekPlanHoveredAtom)
-    const setHoveredTpopId = useSetAtom(ekPlanSetHoveredTpopIdAtom)
-    const resetHovered = useSetAtom(ekPlanResetHoveredAtom)
+export const CellForValue = ({ field, width, row, isOdd, firstChild }) => {
+  const hovered = useAtomValue(ekPlanHoveredAtom)
+  const setHoveredTpopId = useSetAtom(ekPlanSetHoveredTpopIdAtom)
+  const resetHovered = useSetAtom(ekPlanResetHoveredAtom)
 
-    const { value } = field
+  const { value } = field
 
-    const isHovered = hovered.tpopId === row.id
-    const onMouseEnter = () => setHoveredTpopId(row.id)
+  const isHovered = hovered.tpopId === row.id
+  const onMouseEnter = () => setHoveredTpopId(row.id)
 
-    const tableCellStyle = {
-      width,
-      minWidth: width,
-      paddingLeft: firstChild ? 10 : 2,
-      backgroundColor:
-        isHovered ? 'hsla(45, 100%, 90%, 1)'
-        : isOdd ? 'rgb(255, 255, 252)'
-        : 'unset',
-    }
+  const tableCellStyle = {
+    width,
+    minWidth: width,
+    paddingLeft: firstChild ? 10 : 2,
+    backgroundColor:
+      isHovered ? 'hsla(45, 100%, 90%, 1)'
+      : isOdd ? 'rgb(255, 255, 252)'
+      : 'unset',
+  }
 
-    return (
-      <div
-        onMouseEnter={onMouseEnter}
-        onMouseLeave={resetHovered}
-        className={indexStyles.tableCell}
-        style={tableCellStyle}
-      >
-        <div className={styles.container}>
-          <div>{value}</div>
-        </div>
+  return (
+    <div
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={resetHovered}
+      className={indexStyles.tableCell}
+      style={tableCellStyle}
+    >
+      <div className={styles.container}>
+        <div>{value}</div>
       </div>
-    )
-  },
-)
+    </div>
+  )
+}
