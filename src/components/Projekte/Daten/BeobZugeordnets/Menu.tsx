1520f27a819c5dc912dac9e5061fd00b5f897df4
Alexander Gabriel <alex.barbalex@gmail.com>
2026-01-21 22:25:01 +0100

    migrate

--- src/components/Projekte/Daten/BeobZugeordnets/Menu.tsx
+++ src/components/Projekte/Daten/BeobZugeordnets/Menu.tsx
@@ -1,16 +1,18 @@
-import { useContext } from 'react'
 import { useQueryClient } from '@tanstack/react-query'
 import { FaMap } from 'react-icons/fa6'
 import IconButton from '@mui/material/IconButton'
 import Tooltip from '@mui/material/Tooltip'
 import { uniq } from 'es-toolkit'
-import { observer } from 'mobx-react-lite'
+import { useAtomValue, useSetAtom } from 'jotai'
 
 import { MenuBar } from '../../../shared/MenuBar/index.tsx'
 import { FilterButton } from '../../../shared/MenuBar/FilterButton.tsx'
 import { ErrorBoundary } from '../../../shared/ErrorBoundary.tsx'
-import { useProjekteTabs } from '../../../../modules/useProjekteTabs.ts'
-import { MobxContext } from '../../../../mobxContext.ts'
+import { useProjekteTabs } from '../../../../modules/useProjekteTaps.ts'
+import {
+  activeApfloraLayersAtom,
+  setActiveApfloraLayersAtom,
+} from '../../../../JotaiStore/index.ts'
 
 interface MenuProps {
   apfloraLayer?: string
@@ -21,39 +23,37 @@ const iconStyle = { color: 'white' }
 
 // TODO: how to pass both props?
 // TODO: need to add menu to other beobs to enable filtering
-export const Menu = observer(
-  ({ apfloraLayer, toggleFilterInput }: MenuProps) => {
-    const tsQueryClient = useQueryClient()
+export const Menu = ({ apfloraLayer, toggleFilterInput }: MenuProps) => {
+  const tsQueryClient = useQueryClient()
 
-    const store = useContext(MobxContext)
-    const { setActiveApfloraLayers, activeApfloraLayers } = store
+  const activeApfloraLayers = useAtomValue(activeApfloraLayersAtom)
+  const setActiveApfloraLayers = useSetAtom(setActiveApfloraLayersAtom)
 
-    const [projekteTabs, setProjekteTabs] = useProjekteTabs()
-    const showMapIfNotYetVisible = (projekteTabs: string[]) => {
-      const isVisible = projekteTabs.includes('karte')
-      if (!isVisible) {
-        setProjekteTabs([...projekteTabs, 'karte'])
-      }
+  const [projekteTabs, setProjekteTabs] = useProjekteTabs()
+  const showMapIfNotYetVisible = (projekteTabs: string[]) => {
+    const isVisible = projekteTabs.includes('karte')
+    if (!isVisible) {
+      setProjekteTabs([...projekteTabs, 'karte'])
     }
+  }
 
-    const onClickShowOnMap = () => {
-      showMapIfNotYetVisible(projekteTabs)
-      setActiveApfloraLayers(uniq([...activeApfloraLayers, apfloraLayer]))
-    }
+  const onClickShowOnMap = () => {
+    showMapIfNotYetVisible(projekteTabs)
+    setActiveApfloraLayers(uniq([...activeApfloraLayers, apfloraLayer]))
+  }
 
-    return (
-      <ErrorBoundary>
-        <MenuBar>
-          {!!toggleFilterInput && (
-            <FilterButton toggleFilterInput={toggleFilterInput} />
-          )}
-          <Tooltip title="Zeige auf Karte">
-            <IconButton onClick={onClickShowOnMap}>
-              <FaMap style={iconStyle} />
-            </IconButton>
-          </Tooltip>
-        </MenuBar>
-      </ErrorBoundary>
-    )
-  },
-)
+  return (
+    <ErrorBoundary>
+      <MenuBar>
+        {!!toggleFilterInput && (
+          <FilterButton toggleFilterInput={toggleFilterInput} />
+        )}
+        <Tooltip title="Zeige auf Karte">
+          <IconButton onClick={onClickShowOnMap}>
+            <FaMap style={iconStyle} />
+          </IconButton>
+        </Tooltip>
+      </MenuBar>
+    </ErrorBoundary>
+  )
+}
