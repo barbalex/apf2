f0b8bfbfe033bb23b2df847f883e53212bec4f2e
Alexander Gabriel <alex.barbalex@gmail.com>
2026-01-22 23:57:27 +0100

    remove store

--- src/components/Bookmarks/Bookmark/Label.tsx
+++ src/components/Bookmarks/Bookmark/Label.tsx
@@ -1,72 +1,65 @@
-import { useContext } from 'react'
 import { useLocation } from 'react-router'
 import Tooltip from '@mui/material/Tooltip'
-import { observer } from 'mobx-react-lite'
 
 import { toggleNodeSymbol } from '../../Projekte/TreeContainer/Tree/toggleNodeSymbol.ts'
-import { MobxContext } from '../../../mobxContext.ts'
 import { PrefetchLink } from '../../shared/PrefetchLink.tsx'
 
 import styles from './Label.module.css'
 
-export const Label = observer(
-  ({ navData, outerContainerRef, labelStyle, ref }) => {
-    const { pathname, search } = useLocation()
-    const store = useContext(MobxContext)
+export const Label = ({ navData, outerContainerRef, labelStyle, ref }) => {
+  const { pathname, search } = useLocation()
 
-    // issue: relative paths are not working!!!???
-    // also: need to decode pathname (Zähleinheiten...)
-    const pathnameDecoded = decodeURIComponent(pathname)
-    const pathnameWithoutLastSlash = pathnameDecoded.replace(/\/$/, '')
-    const linksToSomewhereElse = !pathnameWithoutLastSlash.endsWith(navData.url)
+  // issue: relative paths are not working!!!???
+  // also: need to decode pathname (Zähleinheiten...)
+  const pathnameDecoded = decodeURIComponent(pathname)
+  const pathnameWithoutLastSlash = pathnameDecoded.replace(/\/$/, '')
+  const linksToSomewhereElse = !pathnameWithoutLastSlash.endsWith(navData.url)
 
-    const onClick = () => {
-      // 1. ensure the clicked element is visible
-      const element = outerContainerRef.current
-      if (!element) return
-      // the timeout needs to be rather long to wait for the transition to finish
-      setTimeout(() => {
-        element.scrollIntoView({
-          inline: 'start',
-        })
-      }, 1000)
-      // 2. sync tree openNodes
-      toggleNodeSymbol({
-        node: {
-          url: navData.url
-            .split('/')
-            .filter((e) => !!e)
-            .slice(1),
-        },
-        store,
-        search,
+  const onClick = () => {
+    // 1. ensure the clicked element is visible
+    const element = outerContainerRef.current
+    if (!element) return
+    // the timeout needs to be rather long to wait for the transition to finish
+    setTimeout(() => {
+      element.scrollIntoView({
+        inline: 'start',
       })
-    }
+    }, 1000)
+    // 2. sync tree openNodes
+    toggleNodeSymbol({
+      node: {
+        url: navData.url
+          .split('/')
+          .filter((e) => !!e)
+          .slice(1),
+      },
+      search,
+    })
+  }
 
-    const label =
-      linksToSomewhereElse ?
-        <PrefetchLink
-          to={{ pathname: navData.url, search }}
-          onClick={onClick}
-          ref={ref}
-          style={{ ...labelStyle }}
-          className={styles.link}
-        >
-          {navData.labelShort ?? navData.label}
-        </PrefetchLink>
-      : <div
-          className={styles.text}
-          ref={ref}
-          style={{ ...labelStyle }}
-        >
-          {navData.labelShort ?? navData.label}
-        </div>
+  const label =
+    linksToSomewhereElse ?
+      <PrefetchLink
+        to={{ pathname: navData.url, search }}
+        onClick={onClick}
+        ref={ref}
+        style={{ ...labelStyle }}
+        className={styles.link}
+      >
+        {navData.labelShort ?? navData.label}
+      </PrefetchLink>
+    : <div
+        className={styles.text}
+        ref={ref}
+        style={{ ...labelStyle }}
+      >
+        {navData.labelShort ?? navData.label}
+      </div>
 
-    // tooltip can mess with touch, so hide it on touch devices
-    if (!matchMedia('(pointer: coarse)').matches) {
-      return <Tooltip title={navData.label}>{label}</Tooltip>
-    }
+  // tooltip can mess with touch, so hide it on touch devices
+  if (!matchMedia('(pointer: coarse)').matches) {
+    return <Tooltip title={navData.label}>{label}</Tooltip>
+  }
 
-    return label
-  },
-)
+  return label
+}
