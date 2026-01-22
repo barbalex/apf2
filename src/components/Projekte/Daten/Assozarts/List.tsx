e4777aad1f3d8cdb244624a5e29fa6d3466b0a14
Alexander Gabriel <alex.barbalex@gmail.com>
2026-01-22 15:17:19 +0100

    migrate tree nodeLabelFilter

--- src/components/Projekte/Daten/Assozarts/List.tsx
+++ src/components/Projekte/Daten/Assozarts/List.tsx
@@ -1,15 +1,14 @@
-import { useContext, Suspense } from 'react'
-import { observer } from 'mobx-react-lite'
+import { Suspense } from 'react'
+import { useAtomValue } from 'jotai'
 
-import { MobxContext } from '../../../../mobxContext.ts'
+import { treeNodeLabelFilterAtom } from '../../../../JotaiStore/index.ts'
 import { useAssozartsNavData } from '../../../../modules/useAssozartsNavData.ts'
 import { List as SharedList } from '../../../shared/List/index.tsx'
 import { Menu } from './Menu.tsx'
 import { Spinner } from '../../../shared/Spinner.tsx'
 
-export const List = observer(() => {
-  const store = useContext(MobxContext)
-  const { nodeLabelFilter } = store.tree
+export const List = () => {
+  const nodeLabelFilter = useAtomValue(treeNodeLabelFilterAtom)
 
   const navData = useAssozartsNavData()
 
@@ -22,4 +21,4 @@ export const List = observer(() => {
       />
     </Suspense>
   )
-})
+}
