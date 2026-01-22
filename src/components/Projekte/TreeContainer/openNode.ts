fac0c06fbe8d00a47f2c77d4d0b76afdb4effa65
Alexander Gabriel <alex.barbalex@gmail.com>
2026-01-22 11:24:16 +0100

    migrate resetting nodelabelfilter

--- src/components/Projekte/TreeContainer/openNode.ts
+++ src/components/Projekte/TreeContainer/openNode.ts
@@ -1,7 +1,6 @@
 import { isNodeOpen } from './isNodeOpen.ts'
 
 export const openNode = async ({ node, openNodes, store }) => {
-  const { treeNodeLabelFilterResetExceptAp } = store
   // make sure this node's url is not yet contained
   // otherwise same nodes will be added multiple times!
   if (isNodeOpen({ openNodes, url: node.url })) return
@@ -17,6 +16,34 @@ export const openNode = async ({ node, openNodes, store }) => {
   if (node.menuType === 'ap') {
     // if ap is changed, need to empty nodeLabelFilter,
     // with exception of the ap key
-    treeNodeLabelFilterResetExceptAp()
+    store.tree.nodeLabelFilter = {
+      ap: store.tree.nodeLabelFilter.ap,
+      pop: null,
+      tpop: null,
+      tpopkontr: null,
+      tpopfeldkontr: null,
+      tpopfreiwkontr: null,
+      tpopkontrzaehl: null,
+      tpopmassn: null,
+      ziel: null,
+      erfkrit: null,
+      apber: null,
+      apberuebersicht: null,
+      idealbiotop: null,
+      assozart: null,
+      ekzaehleinheit: null,
+      ekfrequenz: null,
+      popber: null,
+      popmassnber: null,
+      tpopber: null,
+      tpopmassnber: null,
+      apart: null,
+      projekt: null,
+      beob: null,
+      beobprojekt: null,
+      adresse: null,
+      gemeinde: null,
+      user: null,
+    }
   }
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -2,41 +2,6 @@ import { types } from 'mobx-state-tree'
 
 import { Tree, defaultValue as defaultTree } from './Tree/index.ts'
 
-export const MobxStore = types
-  .model({
-    tree: types.optional(Tree, defaultTree),
-  })
-  .actions((self) => ({
-    // TODO: move to modules where possible
-    treeNodeLabelFilterResetExceptAp() {
-      self.tree.nodeLabelFilter = {
-        ap: self.tree.nodeLabelFilter.ap,
-        pop: null,
-        tpop: null,
-        tpopkontr: null,
-        tpopfeldkontr: null,
-        tpopfreiwkontr: null,
-        tpopkontrzaehl: null,
-        tpopmassn: null,
-        ziel: null,
-        erfkrit: null,
-        apber: null,
-        apberuebersicht: null,
-        idealbiotop: null,
-        assozart: null,
-        ekzaehleinheit: null,
-        ekfrequenz: null,
-        popber: null,
-        popmassnber: null,
-        tpopber: null,
-        tpopmassnber: null,
-        apart: null,
-        projekt: null,
-        beob: null,
-        beobprojekt: null,
-        adresse: null,
-        gemeinde: null,
-        user: null,
-      }
-    },
-  }))
+export const MobxStore = types.model({
+  tree: types.optional(Tree, defaultTree),
+})
