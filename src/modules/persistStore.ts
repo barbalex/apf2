ecab9156904f5eb4e2669fa01da02b22b7834cd6
Alexander Gabriel <alex.barbalex@gmail.com>
2026-01-23 00:43:51 +0100

    clean

--- src/modules/persistStore.ts
+++ src/modules/persistStore.ts
@@ -1,57 +1,28 @@
 import localForage from 'localforage'
-//import { onPatch } from 'mobx-state-tree'
-import { persist } from 'mst-persist'
 
-import { isObject } from './isObject.ts'
 import {
   store as jotaiStore,
   navigateAtom,
   userNameAtom,
   treeActiveNodeArrayAtom,
-  treeDataFilterAtom,
-  treeDataFilterEmptyAtom,
 } from '../JotaiStore/index.ts'
 
 const blacklist = [
   'overlays', // 2022.10.26 added overlay. Need to refresh or users will not get new ones
 ]
 
-export const persistStore = (store) => {
+export const persistStore = () => {
   const username = jotaiStore.get(userNameAtom)
   const navigate = jotaiStore.get(navigateAtom)
 
   const visitedTopDomain = window.location.pathname === '/'
 
-  persist('store', store, {
-    storage: localForage,
-    jsonify: false,
-    blacklist,
-  }).then(async () => {
-    /**
-     * TODO:
-     * This is temporary after rebuilding the structure of dataFilter
-     * Goal: prevent errors because previous persisted structure was invalid
-     * Idea: test if is object. Only then empty
-     */
-    const dataFilter = jotaiStore.get(treeDataFilterAtom)
-    if (
-      isObject(dataFilter.ap) ||
-      isObject(dataFilter.pop) ||
-      isObject(dataFilter.tpop) ||
-      isObject(dataFilter.tpopmassn) ||
-      isObject(dataFilter.tpopfeldkontr) ||
-      isObject(dataFilter.tpopfreiwkontr)
-    ) {
-      jotaiStore.set(treeDataFilterEmptyAtom)
-    }
+  const isUser = !!username
 
-    const isUser = !!username
-
-    // set last activeNodeArray
-    // only if top domain was visited
-    if (isUser && visitedTopDomain) {
-      const activeNodeArray = jotaiStore.get(treeActiveNodeArrayAtom)
-      return navigate?.(`/Daten/${activeNodeArray.join('/')}`)
-    }
-  })
+  // set last activeNodeArray
+  // only if top domain was visited
+  if (isUser && visitedTopDomain) {
+    const activeNodeArray = jotaiStore.get(treeActiveNodeArrayAtom)
+    return navigate?.(`/Daten/${activeNodeArray.join('/')}`)
+  }
 }
