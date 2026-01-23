7903976a9e71909d360ca74aab398f213de3bb62
Alexander Gabriel <alex.barbalex@gmail.com>
2026-01-23 01:38:30 +0100

    clean

--- src/modules/useTpopNavData.ts
+++ src/modules/useTpopNavData.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useContext, useState } from 'react'
 import { gql } from '@apollo/client'
 import { useApolloClient } from '@apollo/client/react'
 import { useQuery } from '@tanstack/react-query'
@@ -52,7 +52,6 @@ export const useTpopNavData = (props) => {
   const popId = props?.popId ?? params.popId
   const tpopId = props?.tpopId ?? params.tpopId
 
-  const store = useContext(MobxContext)
   const copying = useAtomValue(copyingAtom)
   const moving = useAtomValue(movingAtom)
   const tpopmassnGqlFilterForTree = useAtomValue(
