8b0b2f302c71c00af48cb68954833cd4a8ac6ce5
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2026-01-23 17:46:32 +0100

    removing mobx

--- src/store/Tree/DataFilter/pop.ts
+++ src/store/Tree/DataFilter/pop.ts
@@ -1,20 +1,3 @@
-import { types } from 'mobx-state-tree'
-
-export const type = types.model({
-  nr: types.optional(types.maybeNull(types.number), null),
-  name: types.optional(
-    types.maybeNull(types.union(types.string, types.number)),
-    null,
-  ),
-  status: types.optional(types.maybeNull(types.number), null),
-  statusUnklar: types.optional(types.maybeNull(types.boolean), null),
-  statusUnklarBegruendung: types.optional(
-    types.maybeNull(types.union(types.string, types.number)),
-    null,
-  ),
-  bekanntSeit: types.optional(types.maybeNull(types.number), null),
-})
-
 export const initial = {
   nr: null,
   name: null,
