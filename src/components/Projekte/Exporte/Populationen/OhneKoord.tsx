efae4fb07c173b69de6e74cdf72a5444937024df
Alexander Gabriel <alex.barbalex@gmail.com>
2026-01-23 01:47:23 +0100

    clean

--- src/components/Projekte/Exporte/Populationen/OhneKoord.tsx
+++ src/components/Projekte/Exporte/Populationen/OhneKoord.tsx
@@ -1,13 +1,11 @@
-import { useContext, useState } from 'react'
+import { useState } from 'react'
 import { useSetAtom } from 'jotai'
 import { sortBy } from 'es-toolkit'
-import { observer } from 'mobx-react-lite'
 import { gql } from '@apollo/client'
 import Button from '@mui/material/Button'
 import { useApolloClient } from '@apollo/client/react'
 
 import { exportModule } from '../../../../modules/export.ts'
-import { MobxContext } from '../../../../mobxContext.ts'
 
 import type { ApId } from '../../../../models/apflora/public/ApId.ts'
 import type { PopId } from '../../../../models/apflora/public/PopId.ts'
@@ -45,9 +43,8 @@ interface PopOhneKoordsQueryResult {
   }
 }
 
-export const OhneKoord = observer(() => {
+export const OhneKoord = () => {
   const addNotification = useSetAtom(addNotificationAtom)
-  const store = useContext(MobxContext)
   const apolloClient = useApolloClient()
 
   const [queryState, setQueryState] = useState()
@@ -145,4 +142,4 @@ export const OhneKoord = observer(() => {
       : null}
     </Button>
   )
-})
+}
