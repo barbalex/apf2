e3c2e7297079ca8c6cd0f43eb151c67584106d82
Alexander Gabriel <alex.barbalex@gmail.com>
2026-02-03 10:05:25 +0100

    up version

--- src/components/AppBar/Bar/Projekte/More/index.tsx
+++ src/components/AppBar/Bar/Projekte/More/index.tsx
@@ -137,7 +137,7 @@ export const More = ({ onClickExporte: passedOnClickExporte, role }) => {
         <MenuItem onClick={onClickUptime}>
           Verfügbarkeit der Server von apflora.ch
         </MenuItem>
-        <div className={styles.version}>Version: 1.124.71 vom 3.2.2026</div>
+        <div className={styles.version}>Version: 1.124.72 vom 3.2.2026</div>
       </Menu>
     </Tooltip>
   )
