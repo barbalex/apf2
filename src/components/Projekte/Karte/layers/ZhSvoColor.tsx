09a0d9c2cc72a4c7fe319a3d708b6355f6969186
Alexander Gabriel <alex.barbalex@gmail.com>
2026-06-05 09:54:38 +0200

    comment

--- src/components/Projekte/Karte/layers/ZhSvoColor.tsx
+++ src/components/Projekte/Karte/layers/ZhSvoColor.tsx
@@ -1,10 +1,12 @@
 import { WMSTileLayer } from 'react-leaflet'
 
 // https://wms.zh.ch/FnsSchutzanordnungenZH?SERVICE=WMS&Request=GetCapabilities
+//
+// before 2026.06.05:
+// wmsUrl: '//wms.zh.ch/FnsSVOZHWMS',
+// layers="zonen-schutzverordnungen,ueberlagernde-schutzzonen,schutzverordnungsobjekte,svo-zonen-labels,schutzverordnungsobjekt-nr"
 export const ZhSvoColor = () => (
   <WMSTileLayer
-    // url="//wms.zh.ch/FnsSVOZHWMS"
-    // layers="zonen-schutzverordnungen,ueberlagernde-schutzzonen,schutzverordnungsobjekte,svo-zonen-labels,schutzverordnungsobjekt-nr"
     url="//wms.zh.ch/FnsSchutzanordnungenZH"
     layers="objekt-umriss,schutzzonen-farbig,zonen-nach-schutzkategorien,ueberlagernde-schutzzonen-naturschutz,schutztyp-naturschutz,ueberlagernde-schutzzonen,schutzzonen-labels" // altrechtliche-schutzverordnungen-a,altrechtliche-schutzverordnungen-c,
     opacity={0.5}
