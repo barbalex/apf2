96bf7db5be59aa656a12210e32d767f5154d8204
Alexander Gabriel <alex.barbalex@gmail.com>
2026-09-15 12:43:57 +0200

    type NavElements tree: default CSS-module imports instead of named imports, fix lint warnings

--- src/components/NavElements/BeobnichtbeurteiltAbsenzMapIcon.tsx
+++ src/components/NavElements/BeobnichtbeurteiltAbsenzMapIcon.tsx
@@ -1,17 +1,17 @@
 import { beobIconAbsenzString } from '../Projekte/Karte/layers/BeobNichtBeurteilt/beobIconAbsenzString.ts'
 
-import { iconContainer } from './index.module.css'
-import { absenzIcon } from './absenzIcon.module.css'
+import indexStyles from './index.module.css'
+import absenzIconStyles from './absenzIcon.module.css'
 import styles from './BeobnichtbeurteiltAbsenzMapIcon.module.css'
 
 export const BeobnichtbeurteiltAbsenzMapIcon = () => (
   <div
     title="Absenz-Beobachtung"
-    className={iconContainer}
+    className={indexStyles.iconContainer}
   >
     <div
       dangerouslySetInnerHTML={{ __html: beobIconAbsenzString }}
-      className={`${absenzIcon} ${styles.icon}`}
+      className={`${absenzIconStyles.absenzIcon} ${styles.icon}`}
     />
   </div>
 )
